8fbbd838f07d50d03bc8cd0cdec5e1d592cea396
David A.D. Morano <david.a.morano@gmail.com>
2023-12-11 16:43:00 -0500

    comments
    }factorial| + |fibonacci|

---
+++ include/factorial.h
@@ -0,0 +1,40 @@
+/* factorial INCLUDE */
+/* lang=C20 */
+
+/* calculate factorial */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright (c) 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FACTORIAL_INCLUDE
+#define	FACTORIAL_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<utypedefs.h>
+#include	<clanguage.h>
+#include	<char.h>
+
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+extern long factorial(int) noex ;
+
+#ifdef	__cplusplus
+}
+#endif
+
+
+#endif /* FACTORIAL_INCLUDE */
+
+
