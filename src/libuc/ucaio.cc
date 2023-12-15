444886091750cb821ab4721f299a512d34357417
David A.D. Morano <david.a.morano@gmail.com>
2023-12-15 17:08:11 -0500

    consolidation to |ucaio|
    consolidated all AIO to |ucaio|

--- src/libuc/ucaio.cc
+++ src/libuc/ucaio.cc
@@ -20,16 +20,24 @@
 	Names:
 	uc_aioread
 	uc_aiowrite
+	uc_aiocancel
+	uc_aioreturn
+	uc_aiolist
 
 	Description:
 	Get some amount of data and time it also so that we can
 	abort if it times out.
 
 	Synopsis:
-	int uc_aioread(AIOCB *aiobp) noex
+	int uc_aioread(AIOCB *aiocbp) noex
+	int uc_aiowrite(AIOCB *aiocbp) noex
+	int uc_aiocancel(AIOCB *aiocbp,int fd) noex
+	int uc_aioreturn(AIOCB *aiocbp) noex
+	int uc_aiolist(int mode,AIOCB *const *aiocbpp,int n,SIGEVENT *sep) noex
 
 	Arguments:
-	aiobp		AIO block pointer
+	aiocbp		AIO block pointer
+	fd		file-descriptor
 
 	Returns:
 	>=0		amount of data returned
@@ -44,7 +52,6 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>
 #include	<unistd.h>
-#include	<poll.h>
 #include	<aio.h>
 #include	<cerrno>
 #include	<usystem.h>
@@ -71,25 +78,22 @@
 
 namespace {
     struct ucaio ;
-    typedef int (ucaio::*mem_f)(int,void *,int,int) noex ;
+    typedef int (ucaio::*mem_f)(AIOCB *) noex ;
     struct ucaio {
 	mem_f		m ;
-	MSQIDDS		*buf ;
-	key_t		key ;
-	sysvmsgtype	msgtype ;
-	int		cmd ;
+	CTIMESPEC	*tsp ;
+	int		fd ;
+	int		ne ;
 	ucaio() noex { } ;
-	ucaio(key_t k) noex : key(k) { } ;
-	ucaio(int c,MSQIDDS *b) noex : buf(b), cmd(c) { } ;
-	ucaio(sysvmsgtype t) noex : msgtype(t) { } ;
-	int get(int,void *,int,int) noex ;
-	int ctl(int,void *,int,int) noex ;
-	int rcv(int,void *,int,int) noex ;
-	int snd(int,void *,int,int) noex ;
-	int operator () (int,void *,int,int) noex ;
+	ucaio(int f) noex : fd(f) { } ;
+	ucaio(int n,CTIMESPEC *t) noex : ne(n), tsp(t) { } ;
+	int read(AIOCB *) noex ;
+	int write(AIOCB *) noex ;
+	int cancel(AIOCB *) noex ;
+	int suspend(AIOCB *) noex ;
+	int operator () (AIOCB *) noex ;
     } ; /* end struct (ucaio) */
 }
-
 
 
 /* forward references */
@@ -98,26 +102,26 @@ namespace {
 /* exported subroutines */
 
 int uc_aioread(AIOCB *aiocbp) noex {
-	uiaio		ao ;
+	ucaio		ao ;
 	ao.m = &ucaio::read ;
 	return ao(aiocbp) ;
 }
 
 int uc_aiowrite(AIOCB *aiocbp) noex {
-	uiaio		ao ;
+	ucaio		ao ;
 	ao.m = &ucaio::write ;
 	return ao(aiocbp) ;
 }
 
 int uc_aiocancel(AIOCB *aiocbp,int fd) noex {
-	uiaio		ao(fd) ;
+	ucaio		ao(fd) ;
 	ao.m = &ucaio::cancel ;
 	return ao(aiocbp) ;
 }
 
 int uc_aioreturn(AIOCB *aiocbp) noex {
 	int	rs SR_FAULT ;
-	if (aiobbp) {
+	if (aiocbp) {
 	    int		ec ;
 	    errno = 0 ;
 	    rs = SR_OK ;
@@ -137,13 +141,36 @@ int uc_aioreturn(AIOCB *aiocbp) noex {
 }
 /* end subroutine (uc_aioreturn) */
 
+int uc_aiolist(int mode,AIOCB *const *aiocbpp,int n,SIGEVENT *sep) noex {
+	int		rs = SR_FAULT ;
+	if (aiocbpp) {
+	    errno = 0 ;
+	    if ((rs = lio_listio(mode,aiocbpp,n,sep)) == -1) {
+	        rs = (- errno) ;
+	    }
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (uc_aiolist) */
+
+int uc_aiosuspend(const AIOCB *const *aiocbpp,int n,CTIMESPEC *tsp) noex {
+	AIOCB		*aiocbp = (AIOCB *) aiocbpp ;
+	int		rs = SR_FAULT ;
+	if (aiocbpp && tsp) {
+	    ucaio	ao(n,tsp) ;
+	    ao.m = &ucaio::suspend;
+	    rs = ao(aiocbp) ;
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (uc_aiosuspend) */
 
 
 /* local subroutines */
 
 int ucaio::operator () (AIOCB *aiocbp) noex {
 	int		rs = SR_FAULT ;
-	if (aiobp) {
+	if (aiocbp) {
 	    int		to_nomem = utimeout[uto_nomem] ;
 	    int		to_again = utimeout[uto_again] ;
 	    bool	f_exit = false ;
@@ -152,7 +179,7 @@ int ucaio::operator () (AIOCB *aiocbp) noex {
 	        if ((rs = (this->*m)(aiocbp)) < 0) {
 	            switch (rs) {
 	            case SR_NOMEM:
-	                if (to_nomwm-- > 0) {
+	                if (to_nomem-- > 0) {
 		            msleep(1000) ;
 		        } else {
 		            f_exit = true ;
@@ -178,20 +205,20 @@ int ucaio::operator () (AIOCB *aiocbp) noex {
 }
 /* end subroutine (ucaio::operator) */
 
-int uiaio::read(AIOCB *aiocbp) noex {
+int ucaio::read(AIOCB *aiocbp) noex {
 	int		rs ;
 	errno = 0 ;
-	if ((rs = aio_read(aiobp)) == -1) {
+	if ((rs = aio_read(aiocbp)) == -1) {
 	    rs = (- errno) ;
 	}
 	return rs ;
 }
 /* end method (ucaio::read) */
 
-int uiaio::write(AIOCB *aiocbp) noex {
+int ucaio::write(AIOCB *aiocbp) noex {
 	int		rs ;
 	errno = 0 ;
-	if ((rs = aio_write(aiobp)) == -1) {
+	if ((rs = aio_write(aiocbp)) == -1) {
 	    rs = (- errno) ;
 	}
 	return rs ;
@@ -201,58 +228,22 @@ int uiaio::write(AIOCB *aiocbp) noex {
 int ucaio::cancel(AIOCB *aiocbp) noex {
 	int		rs ;
 	errno = 0 ;
-	if ((rs = aio_cancel(fd,aiobp)) == -1) {
+	if ((rs = aio_cancel(fd,aiocbp)) == -1) {
 	    rs = (- errno) ;
 	}
 	return rs ;
 }
 /* end method (ucaio::cancel) */
 
-
-
-
-int uc_aiolist(int mode,AIOCB *const *aiocbpp,int n,SIGEVENT *sep) noex {
-	int		rs = SR_FAULT ;
-	if (aiocbp) {
-	    errno = 0 ;
-	    if ((rs = lio_listio(mode,aiocbpp,n,sep)) == -1) {
-	        rs = (- errno) ;
-	    }
-	} /* end if (non-null) */
-	return rs ;
-}
-/* end subroutine (uc_aiolist) */
-
-int uc_aiosuspend(AIOCB *const *aiocbpp,int n,TIMESPEC *tsp) noex {
+int ucaio::suspend(AIOCB *aiocbp) noex {
+	const AIOCB *const *aiocbpp = (AIOCB *const *) aiocbp ;
 	int		rs ;
-	int		to_nomem = TO_NOMEM ;
-	int		f_exit = FALSE ;
-
-	if (aiobp == NULL) return SR_FAULT ;
-
-	repeat {
-	    errno = 0 ;
-	    if ((rs = aio_suspend(aiobp,n,tsp)) == -1) rs = (- errno) ;
-	    if (rs < 0) {
-	        switch (rs) {
-	        case SR_NOMEM:
-	            if (to_nomem-- > 0) {
-		        msleep(1000) ;
-		    } else {
-		        f_exit = TRUE ;
-		    }
-	            break ;
-	        case SR_INTR:
-	            break ;
-    		default:
-		    f_exit = TRUE ;
-		    break ;
-	        } /* end switch */
-	    } /* end if (error) */
-	} until ((rs >= 0) || f_exit) ;
-
+	errno = 0 ;
+	if ((rs = aio_suspend(aiocbpp,ne,tsp)) == -1) {
+	    rs = (- errno) ;
+	}
 	return rs ;
 }
-/* end subroutine (uc_aiosuspend) */
+/* end subroutine (ucaio::suspend) */
 
 
