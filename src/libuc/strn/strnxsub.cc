68051c196f605bbf07f7958df2092a8ad89a3d1d
David A.D. Morano <david.a.morano@gmail.com>
2023-12-20 07:57:15 -0500

    enhancement to use standard
    enhanced to (compile-time optionally) use the standard in two places

--- src/libuc/strn/strnxsub.cc
+++ src/libuc/strn/strnxsub.cc
@@ -4,15 +4,29 @@
 /* find a substring within a larger string */
 /* version %I% last-modified %G% */
 
+#define	CF_STRNSTR	1		/* used standard |strnstr(3c)| */
+#define	CF_STRCASESTR	1		/* used standard |strcasestr(3c)| */
 
 /* revision history:
 
 	= 1998-03-23, David A­D­ Morano
 	This subroutine was originally written.
 
+	= 2023-12-20, David A­D­ Morano
+	Everyone (!) has |strnstr(3c)| now. After all of these
+	years, I finally discovered that. I must have actually
+	written this subroutine back in the 1980s or so when
+	|strnstr(3c)| did not yet exist. So I am adding a compile-time
+	option (see above in this file) to use the UNIX® C-lang
+	Standard-Library subroutine |strnstr(3c)| when calling our
+	own |strnsub(3uc)|. Oops! Note that the function signature 
+	on the standard |strnstr(3c)| is NOT the same as our own
+	old |strnsub(3uc)|, so I adapt it to match. Everything diddo
+	w/ the (now) standard |strcasestr(3c)|.
+
 */
 
-/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998,2023 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
@@ -23,7 +37,7 @@
 	This subroutine determines if the parameter string (argument
 	's2') is or is not a substring specified by the first two
 	arguments.  This subroutine either returns a pointer to the
-	the begining of the found substring or nullptr if not found.
+	the begining of the found substring or NULL if not found.
 
 	Synopsis:
 	char *strn{x}sub(cchar *sp,int sl,cchar *ss) noex
@@ -35,12 +49,12 @@
 
 	Returns:
 	-	pointer to found substring
-	nullptr	substring was not found
+	NULL	substring was not found
 
 *******************************************************************************/
 
 #include	<envstandards.h>
-#include	<cstring>
+#include	<cstring>		/* <- |strstr(3c)| + |strnstr(3c)| */
 #include	<utypedefs.h>
 #include	<clanguage.h>
 #include	<char.h>
@@ -52,13 +66,33 @@
 
 /* local defines */
 
+#ifndef	CF_STRNSTR
+#define	CF_STRNSTR	0
+#endif
+
+#ifndef	CF_STRCASESTR
+#define	CF_STRCASESTR	0
+#endif
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+typedef char	*charp ;
+
 
 /* forward references */
 
 static char	*strnsub_local(cchar *,int,cchar *) noex ;
+static char	*strncasesub_local(cchar *,int,cchar *,int) noex ;
 
 
 /* local variables */
+
+constexpr bool	f_strnstr = CF_STRNSTR ;
+constexpr bool	f_strcasestr = CF_STRCASESTR ;
 
 
 /* exported subroutines */
@@ -67,7 +101,11 @@ char *strnsub(cchar *sp,int sl,cchar *ss) noex {
 	char		*rp = nullptr ;
 	if (sp && ss) {
 	    if (sl >= 0) {
-	        rp = strnsub_local(sp,sl,ss) ;
+		if constexpr (f_strnstr) {
+		    rp = strnstr(sp,ss,sl) ;
+		} else {
+	            rp = strnsub_local(sp,sl,ss) ;
+		}
 	    } else {
 	        rp = strstr(sp,ss) ;
 	    }
@@ -80,24 +118,17 @@ char *strncasesub(cchar *sp,int sl,cchar *s2) noex {
 	char		*rp = nullptr ;
 	if (sp && s2) {
 	    cint	s2len = strlen(s2) ;
-	    rp = (char *) sp ;
+	    rp = charp(sp) ;
 	    if (s2len > 0) {
-	        if (sl < 0) sl = strlen(sp) ;
-	        if (s2len <= sl) {
-		    cint	s2lead = CHAR_TOLC(s2[0]) ;
-	            int		i ;
-		    int		f = false ;
-	            for (i = 0 ; i <= (sl-s2len) ; i += 1) {
-	                if (CHAR_TOLC(sp[i]) == s2lead) {
-	                    cint	m = nleadcasestr((sp+i),s2,s2len) ;
-	                    f = (m == s2len) ;
-			    if (f) break ;
-	                } /* end if */
-	            } /* end for */
-	            rp = (char *) ((f) ? (sp+i) : nullptr) ;
-	        } else {
-	            rp = nullptr ;
-	        }
+		if (sl >= 0) {
+		    rp = strncasesub_local(sp,sl,s2,s2len) ;
+		} else {
+		    if constexpr (f_strcasestr) {
+		        rp = strcasestr(sp,s2) ;
+		    } else {
+		        rp = strncasesub_local(sp,sl,s2,s2len) ;
+		    } /* end if-constexpr) */
+		} /* end if */
 	    } /* end if (positive) */
 	} /* end if (non-null) */
 	return rp ;
@@ -120,7 +151,7 @@ char *strnsub_local(cchar *sp,int sl,cchar *s2) noex {
 	            f = f && (strncmp((sp+i),s2,s2len) == 0) ;
 		    if (f) break ;
 	        } /* end for */
-	        rp = (char *) ((f) ? (sp+i) : nullptr) ;
+	        rp = charp((f) ? (sp+i) : nullptr) ;
 	    } else {
 	        rp = nullptr ;
 	    }
@@ -128,5 +159,25 @@ char *strnsub_local(cchar *sp,int sl,cchar *s2) noex {
 	return rp ;
 }
 /* end subroutine (strnsub_local) */
+
+char *strncasesub_local(cchar *sp,int sl,cchar *ss,int s2len) noex {
+	char		*rp = nullptr ;
+        if (sl < 0) sl = strlen(sp) ;
+        if (s2len <= sl) {
+            cint        s2lead = CHAR_TOLC(ss[0]) ;
+            int         i ;
+            bool        f = false ;
+            for (i = 0 ; i <= (sl-s2len) ; i += 1) {
+                if (CHAR_TOLC(sp[i]) == s2lead) {
+                    cint        m = nleadcasestr((sp+i),ss,s2len) ;
+                    f = (m == s2len) ;
+                    if (f) break ;
+                } /* end if */
+            } /* end for */
+            rp = charp((f) ? (sp+i) : nullptr) ;
+        } /* end if */
+	return rp ;
+}
+/* end subroutine (strncasesub_local) */
 
 
