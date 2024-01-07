8597cf28a84899d38c423e8f92e370eed69a0157
David A.D. Morano <david.a.morano@gmail.com>
2024-01-07 16:34:19 -0500

    new function |syswords|
    for special system-related words

---
+++ src/libuc/syswords.cc
@@ -0,0 +1,41 @@
+/* syswords SUPPORT */
+/* lang=C++20 */
+
+/* this is a database of commonly used system words */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2001-04-11, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2001 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	This object contains various commonly used system-related
+	words.
+
+*******************************************************************************/
+
+#include	<envstandards.h>
+#include	<sys/types.h>
+#include	<usystem.h>
+#include	<localmisc.h>
+
+#include	"syswords.hh"
+
+
+const char syswords::w_export[] =	"export" ;
+const char syswords::w_path[] =		"path" ;
+const char syswords::w_fpath[] =	"fpath" ;
+const char syswords::w_cdpath[] =	"cdpath" ;
+const char syswords::w_libpath[] =	"libpath" ;
+const char syswords::w_manpath[] =	"manpath" ;
+const char syswords::w_incpath[] =	"incpath" ;
+const char syswords::w_infopath[] =	"infopath" ;
+
+
