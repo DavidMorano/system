4c8b0e347a08c860180a03021109485976884ea1
David A.D. Morano <david.a.morano@gmail.com>
2024-01-26 22:47:39 -0500

    loaded into include-dir
    should have already been there

---
+++ include/sntmtime.h
@@ -0,0 +1,37 @@
+/* sntmtime HEADER */
+/* lang=C20 */
+
+/* counted c-string operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SNTMTIME_INCLUDE
+#define	SNTMTIME_INCLUDE
+
+
+#include	<envstandards.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<clanguage.h>
+#include	<tmtime.h>		/* <- the money shot! */
+
+
+EXTERNC_begin
+
+extern int sntmtime(char *,int,TMTIME *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* SNTMTIME_INCLUDE */
+
+
