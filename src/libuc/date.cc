a3b7efba8fa579cd92647a33b0028467d10ef0a5
David A.D. Morano <david.a.morano@gmail.com>
2024-01-29 03:13:21 -0500

    filre re-org |tmz|
    -

--- src/libuc/date.cc
+++ src/libuc/date.cc
@@ -1,16 +1,15 @@
-/* date */
+/* date SUPPORT */
+/* lang=C++20 */
 
 /* general date object */
-
-
-#define	CF_DEBUGS	0		/* non-switchable debug print-outs */
+/* version %I% last-modified %G% */
 
 
 /* revision history:
 
 	= 1998-05-01, David A­D­ Morano
-        Originally created due to frustration with various other "fuzzy" date
-        conversion subroutines.
+	Originally created due to frustration with various other
+	"fuzzy" date conversion subroutines.
 
 */
 
@@ -18,42 +17,39 @@
 
 /*******************************************************************************
  
-        This object can be used to create dates from various input data
-        including strings.
+	This object can be used to create dates from various input
+	data including strings.
 
 	Note:
-
-        The timezone offset value in 'struct timeb' is the minutes west of GMT.
-        This is a positive value for timezones that are west of Greenwich. It is
-        negative for timezones east of Greenwich. This is opposite from what you
-        will see in email headers (for example). Our number here must be
-        subtracted from GMT in order to get the local time.
+	The timezone offset value in 'struct timeb' is the minutes
+	west of GMT.  This is a positive value for timezones that
+	are west of Greenwich. It is negative for timezones east
+	of Greenwich. This is opposite from what you will see in
+	email headers (for example). Our number here must be
+	subtracted from GMT in order to get the local time.
 
 	Frustration note:
-
-        What an incredible pain this time/date handling stuff all is? This file
-        doesn't even do justice to a small fraction of the real problems
-        associated with data management ! The problem is that the date changes
-        as time progresses. Changes are due to timezone differences and year
-        leaps and leap seconds. The fact that timezone data is not a part of
-        many dates only complicates matters worse as we then have to try and
-        figure out a reasonable timezone when they are not supplied.
-
+	What an incredible pain this time-date handling stuff all
+	is? This file does not even do justice to a small fraction
+	of the real problems associated with data management!  The
+	problem is that the date changes as time progresses.  Changes
+	are due to timezone differences and year leaps and leap
+	seconds.  The fact that timezone data is not a part of many
+	dates only complicates matters worse as we then have to try
+	and figure out a reasonable timezone when they are not
+	supplied.
 
 *******************************************************************************/
 
-#define	DATE_MASTER	0
-
 #include	<envstandards.h>	/* must be first (to configure) */
-
-#include	<sys/types.h>
 #include	<sys/timeb.h>
-#include	<time.h>
-#include	<stdlib.h>
-#include	<string.h>
+#include	<cstdlib>
+#include	<cstring>
+#include	<ctime>
 #include	<tzfile.h>		/* for TM_YEAR_BASE */
-
 #include	<usystem.h>
+#include	<snwcpy.h>
+#include	<strn.h>
 #include	<localmisc.h>
 
 #include	"date.h"
@@ -77,18 +73,13 @@
 #endif
 
 
+/* local namespaces */
+
+
+/* local typedefs */
+
+
 /* external subroutines */
-
-extern int	snwcpy(char *,int,const char *,int) ;
-
-extern char	*strwcpy(char *,const char *,int) ;
-extern char	*strncpylc(char *,const char *,int) ;
-extern char	*strncpyuc(char *,const char *,int) ;
-extern char	*strnncpy(char *,const char *,int,int) ;
-
-extern char	*timestr_gmlog(time_t,char *) ;
-extern char	*timestr_log(time_t,char *) ;
-extern char	*timestr_logz(time_t,char *) ;
 
 
 /* external variables */
@@ -106,8 +97,7 @@ extern char	*timestr_logz(time_t,char *) ;
 /* exported subroutines */
 
 
-int date_start(DATE *dp,time_t t,int zoff,int isdst,const char *zbuf,int zlen)
-{
+int date_start(DATE *dp,time_t t,int zoff,int isdst,cchar *zbuf,int zlen) noex {
 	int		rs ;
 	if (dp == NULL) return SR_FAULT ;
 	if (zbuf == NULL) return SR_FAULT ;
@@ -120,21 +110,14 @@ int date_start(DATE *dp,time_t t,int zoff,int isdst,const char *zbuf,int zlen)
 }
 /* end subroutine (date_start) */
 
-
-int date_finish(DATE *dp)
-{
-
+int date_finish(DATE *dp) noex {
 	if (dp == NULL) return SR_FAULT ;
 	dp->time = 0 ;
 	return SR_OK ;
 }
 /* end subroutine (date_finish) */
 
-
-/* copy a DATE object */
-int date_copy(DATE *dp,DATE *d2p)
-{
-
+int date_copy(DATE *dp,DATE *d2p) noex {
 	if (dp == NULL) return SR_FAULT ;
 	if (d2p == NULL) return SR_FAULT ;
 	memcpy(dp,d2p,sizeof(DATE)) ;
@@ -142,10 +125,7 @@ int date_copy(DATE *dp,DATE *d2p)
 }
 /* end subroutine (date_copy) */
 
-
-int date_gettime(DATE *dp,time_t *tp)
-{
-
+int date_gettime(DATE *dp,time_t *tp) noex {
 	if (dp == NULL) return SR_FAULT ;
 	if (tp == NULL) return SR_FAULT ;
 	*tp = (time_t) dp->time ;
@@ -153,10 +133,7 @@ int date_gettime(DATE *dp,time_t *tp)
 }
 /* end subroutine (date_gettime) */
 
-
-int date_getzoff(DATE *dp,int *zop)
-{
-
+int date_getzoff(DATE *dp,int *zop) noex {
 	if (dp == NULL) return SR_FAULT ;
 	if (zop == NULL) return SR_FAULT ;
 	*zop = (time_t) dp->zoff ;
@@ -164,10 +141,7 @@ int date_getzoff(DATE *dp,int *zop)
 }
 /* end subroutine (date_getzoff) */
 
-
-int date_getisdst(DATE *dp,int *dstp)
-{
-
+int date_getisdst(DATE *dp,int *dstp) noex {
 	if (dp == NULL) return SR_FAULT ;
 	if (dstp == NULL) return SR_FAULT ;
 	*dstp = (time_t) dp->isdst ;
@@ -175,9 +149,7 @@ int date_getisdst(DATE *dp,int *dstp)
 }
 /* end subroutine (date_getisdst) */
 
-
-int date_getzname(DATE *dp,char *zbuf,int zlen)
-{
+int date_getzname(DATE *dp,char *zbuf,int zlen) noex {
 	int		rs ;
 	if (dp == NULL) return SR_FAULT ;
 	if (zbuf == NULL) return SR_FAULT ;
