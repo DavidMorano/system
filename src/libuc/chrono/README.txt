047222f3a8c37482dd8bdeec8ada9d7bbb9f37d9
David A.D. Morano <david.a.morano@gmail.com>
2024-02-08 11:15:17 -0500

    file re-org
    creation of |chrono| group

---
+++ src/libuc/chrono/README.txt
@@ -0,0 +1,4 @@
+CHRONO
+
+This directory contains time related objects and functions.
+
---
+++ src/libuc/chrono/chrono.h
@@ -0,0 +1,36 @@
+/* chrono HEADER */
+/* lang=C20 */
+
+/* convert a single character to a given case */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright (c) 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CHRONO_INCLUDE
+#define	CHRONO_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<utypedefs.h>
+#include	<clanguage.h>
+
+#include	<tmstrs.h>
+#include	<zdb.h>
+#include	<tmz.h>
+#include	<zos.h>
+#include	<date.h>
+#include	<tmtime.h>
+#include	<dater.h>
+
+
+#endif /* CHRONO_INCLUDE */
+
+
