b4e819ecead51ba5374120e5402c0f1b646c4725
David A.D. Morano <david.a.morano@gmail.com>
2024-02-15 12:56:04 -0500

    refactor |readfileline|
    ok

---
+++ src/libuc/readfileline.cc
@@ -0,0 +1,119 @@
+/* readfileline SUPPORT */
+/* lang=C++20 */
+
+/* we read one line from a file! */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This subroutine was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	readfileline
+
+	Description:
+	This subroutine reads a single line from a file (the string
+	of which means something to someone).
+
+	Synopsis:
+	int readfileline(cc *fname,char *rbuf,int rlen) noex
+
+	Arguments:
+	fname		file to read
+	rbuf		user supplied buffer to hold result
+	rlen		length of user supplied buffer
+
+	Returns:
+	>=0		length of returned string
+	<0		error (system-return)
+
+*******************************************************************************/
+
+#include	<envstandards.h>
+#include	<unistd.h>
+#include	<fcntl.h>
+#include	<cstdlib>
+#include	<cstring>
+#include	<usystem.h>
+#include	<mallocxx.h>
+#include	<filebuf.h>
+#include	<sfx.h>
+#include	<sncpyxw.h>
+#include	<char.h>
+#include	<localmisc.h>
+
+#include	"readfileline.h"
+
+
+/* local defines */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported subroutines */
+
+int readfileline(cchar *fn,char *rbuf,int rlen) noex {
+	cint		to = -1 ;
+	int		rs = SR_FAULT ;
+	int		rs1 ;
+	int		len = 0 ;
+	if (fn && rbuf) {
+	    rs = SR_INVALID ;
+	    rbuf[0] = '\0' ;
+	    if (fn[0]) {
+		char	*lbuf{} ;
+		if ((rs = malloc_ml(&lbuf)) >= 0) {
+		    cint	llen = rs ;
+		    cint	of = O_RDONLY ;
+		    cmode	om = 0666 ;
+	            if ((rs = uc_open(fn,of,om)) >= 0) {
+	                filebuf		b ;
+	                int		fd = rs ;
+	                if ((rs = filebuf_start(&b,fd,0L,512,0)) >= 0) {
+	                    while ((rs = filebuf_readln(&b,lbuf,llen,to)) > 0) {
+				int	cl ;
+		                cchar	*cp{} ;
+		                if ((cl = sfcontent(lbuf,rs,&cp)) > 0) {
+	                            rs = sncpy1w(rbuf,rlen,cp,cl) ;
+	                            len = rs ;
+	                        }
+	                        if (len > 0) break ;
+	                        if (rs < 0) break ;
+	                    } /* end while (reading lines) */
+	                    rs1 = filebuf_finish(&b) ;
+		            if (rs >= 0) rs = rs1 ;
+	                } /* end if (filebuf) */
+	                rs1 = u_close(fd) ;
+	                if (rs >= 0) rs = rs1 ;
+	            } /* end if (uc_open) */
+	            rs1 = uc_free(lbuf) ;
+	            if (rs >= 0) rs = rs1 ;
+		} /* end if (m-a-f) */
+	    } /* end if (valid) */
+	} /* end if (non-null) */
+	return (rs >= 0) ? len : rs ;
+}
+/* end subroutine (readfileline) */
+
+
