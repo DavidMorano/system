96074d8c30012f335b133663967a2ab13327dfc0
David A.D. Morano <david.a.morano@gmail.com>
2024-03-18 14:05:55 -0400

    comments |ipow|
    ok

--- src/libuc/ipow.cc
+++ src/libuc/ipow.cc
@@ -1,12 +1,10 @@
-/* ipow */
+/* ipow SUPPORT */
+/* lang=C++20 */
 
 /* return integer-power */
 /* version %I% last-modified %G% */
 
-
-#define	CF_DEBUGS	0		/* non-switchable debug print-outs */
 #define	CF_DYNAMIC	1		/* dynamic programming */
-
 
 /* revision history:
 
@@ -23,33 +21,34 @@
         base.
 
 	Synopsis:
-
-	int ipow(b,p)
-	int	b, p ;
+	int ipow(int b,int p) noex
 
 	Arguments:
-
 	b	base
 	p	power
 
 	Returns:
-
 	-	result (b**p)
 
 	Notes: 
-
 	Only defined for positive exponents.
-
 
 *******************************************************************************/
 
-
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/types.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<clanguage.h>
 #include	<localmisc.h>		/* 'LONG' type */
+
+#include	"ipow.h"
 
 
 /* local defines */
+
+#ifndef	CF_DYNAMIC
+#define	CF_DYNAMIC	1		/* dynamic programming */
+#endif
 
 
 /* external subroutines */
@@ -66,44 +65,36 @@
 
 /* local variables */
 
+constexpt bool		f_dynamic = CF_DYNAMIC ;
+
+
+/* exported variables */
+
 
 /* exported subroutines */
 
-
-#if	CF_DYNAMIC
-
-int ipow(int b,int n)
-{
+int ipow(int b,int n) noex {
 	int		r = 1 ;
-	if (n == 1) {
-	    r = b ;
-	} else if (n == 2) { /* common case */
-	    r = b*b ;
-	} else if (n > 2) {
-	    int	t = ipow(b,(n/2)) ;
-	    if ((n&1) == 0) {
-		r = (t*t) ;
-	    } else {
-		r = b*(t*t) ;
+	if constexpr (f_dynamic) {
+	    if (n == 1) {
+	        r = b ;
+	    } else if (n == 2) { /* common case */
+	        r = b*b ;
+	    } else if (n > 2) {
+	        int	t = ipow(b,(n/2)) ;
+	        if ((n&1) == 0) {
+		    r = (t*t) ;
+	        } else {
+		    r = b*(t*t) ;
+	        }
 	    }
-	}
+	} else {
+	    for (int i = 0 ; i < n ; i += 1) {
+	        r *= b ;
+	    }
+	} /* end if-constexpr (f_dynamic) */
 	return r ;
 }
 /* end subroutine (ipow) */
-
-#else /* CF_DYNAMIC */
-
-int ipow(int b,int n)
-{
-	int		r = 1 ;
-	int		i ;
-	for (i = 0 ; i < n ; i += 1) {
-	    r *= b ;
-	}
-	return r ;
-}
-/* end subroutine (ipow) */
-
-#endif /* CF_DYNAMIC */
 
 
---
+++ src/libuc/ipow.h
@@ -0,0 +1,35 @@
+/* ipow HEADER */
+/* lang=C20 */
+
+/* calculate the integer expoential of an integer */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	IPOW_INCLUDE
+#define	IPOW_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<utypedefs.h>
+#include	<clanguage.h>
+
+
+EXTERNC_begin
+
+extern int	ipow(int,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* IPOW_INCLUDE */
+
+
