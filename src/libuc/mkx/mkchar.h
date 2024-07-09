e9ddb1afda86d3e57521d6b55f018edd81fa9b05
David A.D. Morano <david.a.morano@gmail.com>
2024-07-08 23:36:02 -0400

    symbol name changes

--- src/libuc/mkx/mkchar.h
+++ src/libuc/mkx/mkchar.h
@@ -26,8 +26,9 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<utypedefs.h>
 #include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
 
 
 #ifndef	MKCHAR
