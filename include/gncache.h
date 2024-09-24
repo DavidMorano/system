3b5885ee600a498556ec8cb1f576b745c970d3a9
David A.D. Morano <david.a.morano@gmail.com>
2024-09-24 18:44:45 -0400

    robust

--- include/gncache.h
+++ include/gncache.h
@@ -24,8 +24,8 @@
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
+#include	<usysdefs.h>
 #include	<usysrets.h>
-#include	<utypealiases.h>
 #include	<vechand.h>
 #include	<cq.h>
 #include	<localmisc.h>		/* |GROUPNAMELEN| */
