76fb3cb54510a763d31e5cb612800877c1065b86
David A.D. Morano <david.a.morano@gmail.com>
2025-04-10 00:41:06 -0400

    new |funmode(3dam)|

---
+++ src/libdam/msgidx/funmode.hh
@@ -0,0 +1,40 @@
+/* funmode HEADER */
+/* encoding=ISO8859-1 */
+/* lang=C20 */
+
+/* Function-Mode */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FUNMODE_INCLUDE
+#define	FUNMODE_INCLUDE
+#ifdef	__cpluplsus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+enum struct funmode : int {
+    	wr,
+	rd,
+	overlast
+} ;
+
+
+#endif /* __cpluplsus */
+#endif /* FUNMODE_INCLUDE */
+
+
