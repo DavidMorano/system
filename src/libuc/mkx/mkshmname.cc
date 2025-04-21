9668d5b64981f251f8a91082a05831cf4bb1032e
David A.D. Morano <david.a.morano@gmail.com>
2025-04-21 00:46:55 -0400

    new |mkshmname(3dam)|

---
+++ src/libuc/mkx/mkshmname.cc
@@ -0,0 +1,124 @@
+/* mkshmname SUPPORT */
+/* encoding=ISO8859-1 */
+/* lang=C++20 */
+
+/* make a shared-object filename from components */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2001-12-03, David A­D­ Morano
+	This code was born out of frustration with cleaning up bad
+	legacy code (of which there is quite a bit -- like almost
+	all of it).
+
+*/
+
+/* Copyright © 2001 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	mkshmname
+
+	Description:
+	This subroutine constructs a shared-memory (SHM) name 
+	from various specified
+	components.
+
+	Synopsis:
+	int mkshmname(char *shmbuf,cchar *fp,int fl,cchar *dp,int dl) noex
+
+	Arguments:
+	shmbuf		result buffer pointer
+	fp		something pointer
+	fl		something length
+	dp		something pointer
+	dl		something length
+
+	Returns:
+	>=0		length of resulting c-string
+	<0		error (system-return)
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<usystem.h>
+#include	<bufsizevar.hh>		/* |maxnamelen(3uc)| */
+#include	<storebuf.h>
+#include	<strn.h>		/* |strnnlen(3uc)| */
+#include	<localmisc.h>
+
+#include	"mkx.h"
+
+
+/* local defines */
+
+#ifndef	SHMPREFIXLEN
+#define	SHMPREFIXLEN	8
+#endif
+#ifndef	SHMPOSTFIXLEN
+#define	SHMPOSTFIXLEN	4
+#endif
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local strutures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+static bufsizevar	maxnamelen(getbufsize_mn) ;
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int mkshmname(char *shmbuf,cchar *fp,int fl,cchar *dp,int dl) noex {
+	int		rs ;
+	int		len = 0 ;
+	if ((rs = maxnamelen) >= 0) {
+	    storebuf	sb(shmbuf,rs) ;
+	    int		ml ;
+	    if ((rs = sb.chr('/')) >= 0) {
+	        if (fp[0] == '/') {
+	            if (fl < 0) fl = xstrlen(fp) ;
+	            fp += 1 ;
+	            fl -= 1 ;
+	        }
+	        ml = strnnlen(fp,fl,SHMPREFIXLEN) ;
+	        rs = sb.strw(fp,ml) ;
+	    }
+	    if (rs >= 0) {
+	        rs = sb.chr('$') ;
+	    }
+	    if (rs >= 0) {
+	        ml = strnnlen(dp,dl,SHMPOSTFIXLEN) ;
+	        rs = sb.strw(dp,ml) ;
+	        len = sb.idx ;
+	    }
+	} /* end if (maxnamelen) */
+	return (rs >= 0) ? len : rs ;
+}
+/* end subroutine (mkshmname) */
+
+
