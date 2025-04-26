f6aa182013e74554ede004c69b9784cfea060836
David A.D. Morano <david.a.morano@gmail.com>
2025-04-26 00:34:03 -0400

    update |strkeyx(3uc)|

---
+++ src/libuc/strkeyx.h
@@ -0,0 +1,26 @@
+/* strxcmp HEADER */
+/* encoding=ISO8859-1 */
+/* lang=C20 */
+
+/* c-string comparisons */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STRXCMP_INCLUDE
+#define	STRXCMP_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+#include	<strkeycmp.h>
+
+
+#endif /* STRXCMP_INCLUDE */
+
+
