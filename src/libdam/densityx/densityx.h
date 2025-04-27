eaa472990859ba7977a6ac52f21c03164cfb7dd9
David A.D. Morano <david.a.morano@gmail.com>
2025-04-27 01:05:41 -0400

    inc |densityx(3dam)|

---
+++ src/libdam/densityx/densityx.h
@@ -0,0 +1,25 @@
+/* densityx HEADER */
+/* encoding=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* various probability-density interfaces */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	DENSITYX_INCLUDE
+#define	DENSITYX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+
+#include	<density.h>
+#include	<densitydb.h>
+#include	<densitydbe.h>
+#include	<densitystat.h>
+
+
+#endif /* DENSITYX_INCLUDE */
+
+
