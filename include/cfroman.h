e0d33ace8af85118b389a6346a2089cbd8d5d857
David A.D. Morano <david.a.morano@gmail.com>
2025-05-03 00:22:48 -0400

    sync

--- include/cfroman.h
+++ include/cfroman.h
@@ -1,7 +1,9 @@
-/* cfroman */
+/* cfroman HEADER */
+/* encoding=ISO8859-1 */
 /* lang=C++11 */
 
 /* convert from Roman Numeral to binary integer */
+/* version %I% last-modified %G% */
 
 
 /* revision history:
@@ -14,35 +16,54 @@
 /* Copyright © 2017 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	CFROMAN_INCLUDE
-#define	CFROMAN_INCLUDE	1
+#define	CFROMAN_INCLUDE
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/types.h>
-#include	<localmisc.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<stdintx.h>
 
 
-#if	(! defined(CFROMAN_MASTER)) || (CFROMAN_MASTER == 0)
+EXTERNC_begin
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
+extern int cfromani(cchar *,int,int *) noex ;
+extern int cfromanl(cchar *,int,long *) noex ;
+extern int cfromanll(cchar *,int,longlong *) noex ;
 
-extern int cfroman(cchar *,int,* int) ;
+extern int cfromanui(cchar *,int,uint *) noex ;
+extern int cfromanul(cchar *,int,ulong *) noex ;
+extern int cfromanull(cchar *,int,ulonglong *) noex ;
 
-extern int cfromani(cchar *,int,* int) ;
-extern int cfromanl(cchar *,int,* long) ;
-extern int cfromanll(cchar *,int,* longlong) ;
-
-extern int cfromanui(cchar *,int,* uint) ;
-extern int cfromanul(cchar *,int,* ulong) ;
-extern int cfromanull(cchar *,int,* ulonglong) ;
-
-#ifdef	__cplusplus
+static inline int cfroman(cchar *sp,int sl,int *rp) noex {
+	return cfromani(sp,sl,rp) ;
 }
-#endif
 
-#endif /* CFROMAN_MASTER */
+EXTERNC_end
+
+#ifdef	__cplusplus
+
+inline int cfroman(cchar *sp,int sl,long *rp) noex {
+	return cfromanl(sp,sl,rp) ;
+}
+inline int cfroman(cchar *sp,int sl,longlong *rp) noex {
+	return cfromanll(sp,sl,rp) ;
+}
+inline int cfroman(cchar *sp,int sl,uint *rp) noex {
+	return cfromanui(sp,sl,rp) ;
+}
+inline int cfroman(cchar *sp,int sl,ulong *rp) noex {
+	return cfromanul(sp,sl,rp) ;
+}
+inline int cfroman(cchar *sp,int sl,ulonglong *rp) noex {
+	return cfromanull(sp,sl,rp) ;
+}
+
+#endif /* __cplusplus */
+
 
 #endif /* CFROMAN_INCLUDE */
 
