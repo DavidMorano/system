1367630a7e1ee2b3be93d8876af6a5d25eb7ae3a
David A.D. Morano <david.a.morano@gmail.com>
2025-05-19 01:43:08 -0400

    sync

--- include/fmtsub.hh
+++ include/fmtsub.hh
@@ -1,4 +1,4 @@
-/* fmtsub HEADER */
+/* fmtsub MODULE (primary-module-interface) */
 /* encoding=ISO8859-1 */
 /* lang=C++20 */
 
@@ -20,51 +20,52 @@
 
 /*******************************************************************************
 
+  	Object:
+	fmtsub
+
+	Description:
 	This (FMTSUB) is a helper object for the FMTSTR facility.
 
 *******************************************************************************/
 
-#ifndef	FMTSUB_INCLUDE
-#define	FMTSUB_INCLUDE
-#ifdef	__cplusplus
-
+module ;
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
 
 #include	<fmtspec.hh>
-#include	<fmtstrdata.h>
 
+export module fmtsub ;
 
-#define	FMTSUB		struct fmtsub_head
-#define	FMTSUB_FL	struct fmtsub_flags
+import fmtstrdata ;
 
+#define	FMTSUB		fmtsub_head
+#define	FMTSUB_FL	fmtsub_flags
 
-struct fmtsub_flags {
+export {
+    struct fmtsub_flags {
 	uint		ov:1 ;		/* overflow */
 	uint		mclean:1 ;	/* mode: clean-up */
 	uint		mnooverr:1 ;	/* mode: return-error */
-} ;
-
-struct fmtsub_head {
+    } ;
+    struct fmtsub_head {
 	char		*ubuf ;		/* user buffer */
-	FMTSUB_FL	f ;		/* flags */
+	FMTSUB_FL	fl ;		/* flags */
 	int		ulen ;		/* buffer length */
-	int		len ;		/* current usage count */
+	int		len ;		/* current buffer usage count */
 	int		mode ;		/* format mode */
-} ;
-
-enum fmtsubmems {
+    } ;
+    enum fmtsubmems {
 	fmtsubmem_audit,
 	fmtsubmem_finish,
 	fmtsubmem_overlast
-} ;
-
-struct fmtsub ;
-
-struct fmtsub_co {
+    } ;
+    struct fmtsub ;
+    struct fmtsub_co {
 	fmtsub		*op = nullptr ;
 	int		w = -1 ;
 	void operator () (fmtsub *p,int m) noex {
@@ -75,9 +76,8 @@ struct fmtsub_co {
 	int operator () () noex { 
 	    return operator int () ;
 	} ;
-} ; /* end struct (fmtsub_co) */
-
-struct fmtsub : fmtsub_head {
+    } ; /* end struct (fmtsub_co) */
+    struct fmtsub : fmtsub_head {
 	fmtsub_co	audit ;
 	fmtsub_co	finish ;
 	fmtsub() noex : fmtsub_head{} {
@@ -90,34 +90,29 @@ struct fmtsub : fmtsub_head {
 	int cleanstrw(cchar *,int = -1) noex ;
 	int strw(cchar *,int = -1) noex ;
 	int chr(int) noex ;
+	int blanks(int = 1) noex ;
 	int emit(fmtspec *,cchar *,int) noex ;
 	int formstr(fmtspec *,fmtstrdata *) noex ;
 	void dtor() noex ;
 	~fmtsub() {
 	    dtor() ;
 	} ;
-} ; /* end struct (fmtsub) */
+    } ; /* end struct (fmtsub) */
+    typedef	FMTSUB_FL	fmtsub_fl ;
+} /* end export */
 
-typedef	FMTSUB_FL	fmtsub_fl ;
-
-EXTERNC_begin
-
-extern int fmtsub_start(fmtsub *,char *,int,int) noex ;
-extern int fmtsub_finish(fmtsub *) noex ;
-extern int fmtsub_strw(fmtsub *,cchar *,int) noex ;
-extern int fmtsub_chr(fmtsub *,int) noex ;
-extern int fmtsub_blanks(fmtsub *,int) noex ;
-extern int fmtsub_cleanstrw(fmtsub *,cchar *,int) noex ;
-extern int fmtsub_emit(fmtsub *,fmtspec *,cchar *,int) noex ;
-extern int fmtsub_formstr(fmtsub *,fmtspec *,fmtstrdata *) noex ;
-extern int fmtsub_reserve(fmtsub *,int) noex ;
-extern int fmtsub_audit(fmtsub *) noex ;
-extern int fmtsub_float(fmtsub *,int,double,int,int,int,char *) noex ;
-
-EXTERNC_end
-
-
-#endif /* __cplusplus */
-#endif /* FMTSUB_INCLUDE */
+export {
+    int fmtsub_start(fmtsub *,char *,int,int) noex ;
+    int fmtsub_finish(fmtsub *) noex ;
+    int fmtsub_strw(fmtsub *,cchar *,int) noex ;
+    int fmtsub_chr(fmtsub *,int) noex ;
+    int fmtsub_blanks(fmtsub *,int) noex ;
+    int fmtsub_cleanstrw(fmtsub *,cchar *,int) noex ;
+    int fmtsub_emit(fmtsub *,fmtspec *,cchar *,int) noex ;
+    int fmtsub_formstr(fmtsub *,fmtspec *,fmtstrdata *) noex ;
+    int fmtsub_reserve(fmtsub *,int) noex ;
+    int fmtsub_audit(fmtsub *) noex ;
+    int fmtsub_floater(fmtsub *,int,double,int,int,int,char *) noex ;
+} /* end export */
 
 
