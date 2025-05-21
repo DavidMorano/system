ff7c64b161a4c99c4cba5c61fff0f23f9d2b8e76
David A.D. Morano <david.a.morano@gmail.com>
2025-05-21 01:45:07 -0400

    sync

--- include/mknpathx.h
+++ include/mknpathx.h
@@ -20,7 +20,11 @@
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
 
 #include	<mknpathxw.h>
 
