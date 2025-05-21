f7856f2719c3d72b08fa4956335d4a3f5d5f81a3
David A.D. Morano <david.a.morano@gmail.com>
2025-05-21 01:45:45 -0400

    sync

--- include/psem.h
+++ include/psem.h
@@ -1,8 +1,8 @@
-/* psem HEADER */
+/* psem HEADER ("real" POSIX® Semaphore) */
 /* encoding=ISO8859-1 */
 /* lang=C20 */
 
-/* POSIX® unnamed-Semaphore (PSEM) */
+/* POSIX© unnamed Semaphore (PSEM) */
 /* version %I% last-modified %G% */
 
 
@@ -13,31 +13,107 @@
 
 */
 
+/* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
+
 /*******************************************************************************
 
   	Object:
 	psem
 
 	Description:
-	These are unnamed POSIX® semaphores.  See the corresponding
-	main C-language source file for more information on what
-	is going on here.
+	This obect implements a (real or emulated) "unnamed" POSIX®
+	semaphore.
+
+	Notes:
+	1. Yes.  Apple-Darwin does not support unnamed POSIX® 
+	semaphores.  So when on Apple-Darwin, I substitute my own
+	emulated POSIX® semaphores.  On a normal operating system,
+	I used real POSIX® semaphores.
 
 *******************************************************************************/
 
-/* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
+#ifndef	PSEM_INCLUDE
+#define	PSEM_INCLUDE
+
 
 #include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
 
 
-#if	defined(SYSHAS_PSEM) && (SYSHAS_PSEM > 0)
+#define	PSEM		struct psem_head
 
-#include	<rpsem.h>		/* "real" POSIX® semaphores */
+struct psem_head {
+    	void		*subobj ;
+} ;
 
-#else /* defined(SYSHAS_PSEM) && (SYSHAS_PSEM > 0) */
+#ifdef	__cplusplus
+enum psemmems {
+	psemmem_wait,
+	psemmem_waiter,
+	psemmem_trywait,
+	psemmem_post,
+	psemmem_count,
+    	psemmem_destroy,
+	psemmem_overlast
+} ; /* end enum */
+struct psem ;
+struct psem_co {
+	psem		*op = nullptr ;
+	int		w = -1 ;
+	void operator () (psem *p,int m) noex {
+	    op = p ;
+	    w = m ;
+	} ;
+	int operator () (int = -1) noex ;
+	operator int () noex {
+	    return operator () () ;
+	} ;
+} ; /* end struct (psem_co) */
+struct psem : psem_head {
+	psem_co		wait ;
+	psem_co		waiter ;
+	psem_co		trywait ;
+	psem_co		post ;
+	psem_co		count ;
+	psem_co		destroy ;
+	psem() noex {
+	    wait(this,psemmem_wait) ;
+	    waiter(this,psemmem_waiter) ;
+	    trywait(this,psemmem_trywait) ;
+	    post(this,psemmem_post) ;
+	    count(this,psemmem_count) ;
+	    destroy(this,psemmem_destroy) ;
+	} ;
+	psem(const psem &) = delete ;
+	psem &operator = (const psem &) = delete ;
+	int create(int = 0,int = -1) noex ;
+	operator int () noex ;
+	void dtor() noex ;
+	destruct psem() {
+	    dtor() ;
+	} ;
+} ; /* end struct (psem) */
+#else	/* __cplusplus */
+typedef PSEM		psem ;
+#endif /* __cplusplus */
 
-#include	<epsem.h>		/* "emulated" POSIX® semaphores */
+EXTERNC_begin
 
-#endif /* defined(SYSHAS_PSEM) && (SYSHAS_PSEM > 0) */
+extern int	psem_create(psem *,int,int) noex ;
+extern int	psem_destroy(psem *) noex ;
+extern int	psem_wait(psem *) noex ;
+extern int	psem_waiter(psem *,int) noex ;
+extern int	psem_trywait(psem *) noex ;
+extern int	psem_post(psem *) noex ;
+extern int	psem_count(psem *) noex ;
+
+EXTERNC_end
+
+
+#endif /* PSEM_INCLUDE */
 
 
