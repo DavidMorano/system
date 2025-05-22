560913d55428b49aa4daf5238a3f180f307830b2
David A.D. Morano <david.a.morano@gmail.com>
2025-05-22 00:54:12 -0400

    sync

--- include/ucsigset.h
+++ include/ucsigset.h
@@ -20,7 +20,11 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<signal.h>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
 
 
 EXTERNC_begin
