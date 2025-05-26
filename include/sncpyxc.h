1929f7e76b99a63d919269f6361e83c82d26d71c
David A.D. Morano <david.a.morano@gmail.com>
2025-05-26 00:36:45 -0400

    sync

--- include/sncpyxc.h
+++ include/sncpyxc.h
@@ -22,6 +22,7 @@
 #include	<envstandards.h>	/* MUST be ordered first to configure */
 #include	<clanguage.h>
 #include	<utypedefs.h>
+#include	<utypealiases.h>
 #include	<usysdefs.h>
 #include	<usysrets.h>
 
