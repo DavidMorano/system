2e4bd92e2ee780e86a528a1b95284af90ab097d4
David A.D. Morano <david.a.morano@gmail.com>
2025-05-26 00:37:07 -0400

    sync

--- include/termcharset.h
+++ include/termcharset.h
@@ -1,4 +1,5 @@
 /* termcharset HEADER */
+/* encoding=ISO8859-1 */
 /* lang=C20 */
 
 /* terminal-character-setter */
@@ -22,6 +23,7 @@
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
+#include	<usysdefs.h>
 #include	<usysrets.h>
 
 
