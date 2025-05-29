bd153750859e8aa31a9ff0321ae4148a0aab33da
David A.D. Morano <david.a.morano@gmail.com>
2025-05-29 01:38:09 -0400

    comments

--- src/clanguage.h
+++ src/clanguage.h
@@ -33,25 +33,49 @@
 #endif
 #endif /* noex */
 
+#ifndef	scharconv
+#define	scharconv		schar
+#endif
+
 #ifndef	charconv
 #define	charconv		char
 #endif
-
 #ifndef	shortconv
 #define	shortconv		short
 #endif
-
 #ifndef	intconv
 #define	intconv			int
 #endif
-
 #ifndef	longconv
 #define	longconv		long
+#endif
+
+#ifndef	ucharconv
+#define	ucharconv		uchar
+#endif
+#ifndef	ushortconv
+#define	ushortconv		ushort
+#endif
+#ifndef	uintconv
+#define	uintconv		uint
+#endif
+#ifndef	uintlongconv
+#define	ulongconv		ulong
+#endif
+
+#ifndef	floatconv
+#define	floatconv		float
 #endif
 
 #ifdef	__cplusplus
 #ifndef	restruct
 #define	restrict		__restrict__
+#endif
+#endif /* __cplusplus */
+
+#ifdef	__cplusplus
+#ifndef	typealias
+#define	typealias		typedef
 #endif
 #endif /* __cplusplus */
 
@@ -124,6 +148,12 @@
 #define	cast_bit		bit_cast
 #define	cast_saturate		saturate_cast
 #define	cast_any		any_cast
+#endif
+#endif /* __cplusplus */
+
+#ifdef	__cplusplus
+#ifndef	assert_contract
+#define	assert_contract		contract_assert
 #define	assert_static		static_assert
 #endif
 #endif /* __cplusplus */
