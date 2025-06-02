fa67196ea88b3fe7972cb1668f99c3eeed29d57b
David A.D. Morano <david.a.morano@gmail.com>
2025-06-02 00:52:51 -0400

    restore

---
+++ src/libu/syscontain/syscontain.h
@@ -0,0 +1,32 @@
+/* contain HEADER */
+/* encoding=ISO8859-1 */
+/* lang=C20 (conformance reviewed) */
+
+/* Integer-Remaider */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CONTAIN_INCLUDE
+#define	CONTAIN_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* CONTAIN_INCLUDE */
+
+
---
+++ src/libu/syscontain/vecbool1.cc
@@ -0,0 +1,157 @@
+/* vecbool1 MODULE */
+/* encoding=ISO8859-1 */
+/* lang=C++20 */
+
+/* manage a variable length bit-array */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-09-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+	= 2020-08-13, David A­D­ Morano
+	This code has been modularized (w/ C++20 modules).
+
+*/
+
+/* Copyright © 1998,2020 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Object:
+	vecbool
+
+	Description:
+	This object implements an array of bits (bools).
+
+*******************************************************************************/
+
+module ;
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<new>			/* |nothrow(3c++)| */
+#include	<vector>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<ulogerror.h>
+#include	<localmisc.h>
+
+module vecbool ;
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* forward refernces */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
+/* local subroutines */
+
+int vecbool::resize(int n) noex {
+	    int		rs = SR_INVALID ;
+	    if (n >= 0) {
+		try {
+	            csize nsize = size_t(n) ;
+	            vector<bool>::resize(nsize,false) ;
+	            rs = SR_OK ;
+		} catch (...) {
+		    rs = SR_NOMEM ;
+		}
+	    }
+	    return rs ;
+} /* end method (vecbool::resize) */
+
+int vecbool::adj(int idx) noex {
+	int		rs = SR_OK ;
+	try {
+	    csize	nsize = size_t(idx + 1) ;
+	    vector<bool>::resize(nsize,false) ;
+	} catch (...) {
+	    rs = SR_NOMEM ;
+	}
+	return rs ;
+} /* end method (vecbool::adj) */
+
+vecbool_co::operator int () noex {
+	int		rs = SR_BUGCHECK ;
+	if (op) {
+	    switch (w) {
+	    case vecboolmem_start:
+	        rs = SR_OK ;
+	        break ;
+	    case vecboolmem_finish:
+	        rs = SR_OK ;
+	        break ;
+	    case vecboolmem_extent:
+	        rs = int(op->capacity()) ;
+	        break ;
+	    case vecboolmem_count:
+	        rs = int(op->size()) ;
+	        break ;
+	    default:
+		rs = SR_INVALID ;
+		break ;
+	    } /* end switch */
+	} /* end if (non-null) */
+	return rs ;
+} /* end method (vecbool_co) */
+
+int vecbool_co::operator [] (int idx) noex {
+    	int		rs = SR_BUGCHECK ;
+	bool		f = false ;
+	if (op) {
+	    rs = SR_INVALID ;
+	    if (idx >= 0) {
+	        if ((rs = op->adj(idx)) >= 0) {
+                    switch (w) {
+                    case vecboolmem_set:
+                        (*op)[idx] = true ;
+                        break ;
+                    case vecboolmem_clr:
+                        (*op)[idx] = false ;
+                        break ;
+                    case vecboolmem_tst:
+                        f = (*op)[idx] ;
+                        break ;
+	            default:
+		        rs = SR_INVALID ;
+		        break ;
+                    } /* end switch */
+		    if (rs >= 0) rs = int(f) ;
+	        } /* end if (adj) */
+	    } /* end if (valid) */
+	} /* end if (non-null) */
+	return rs ;
+} /* end method (vecbool_co) */
+
+
