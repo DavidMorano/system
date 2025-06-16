bc4556bbb143d8b9d3a82b1fc440a881f4b12cf3
David A.D. Morano <david.a.morano@gmail.com>
2025-06-16 00:04:50 -0400

    restore

---
+++ src/libuc/strx/straltwchar.h
@@ -0,0 +1,31 @@
+/* straltwchar HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* counted-string copy while compacting white-space from the source */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STRALTWCHAR_INCLUDE
+#define	STRALTWCHAR_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern cchar	*straltwchar(int) noex ;
+
+EXTERNC_end
+
+
+#endif /* STRALTWCHAR_INCLUDE */
+
+
