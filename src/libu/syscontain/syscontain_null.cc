2998619dd35777b6443d14fed1396a98629d4028
David A.D. Morano <david.a.morano@gmail.com>
2025-06-15 23:48:29 -0400

    restore

---
+++ src/libu/syscontain/syscontain_null.cc
@@ -0,0 +1,57 @@
+/* syscontain_null SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* convert a single character to a given case */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+        This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/******************************************************************************* 
+  	Group:
+	syscontain_null
+
+	Description:
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<localmisc.h>
+
+#include	"syscontain_null.h"
+
+
+/* local defines */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
