d1b0aac22c3fe54b8cca16823b05f4efbd8bdb7d
David A.D. Morano <david.a.morano@gmail.com>
2025-06-17 00:56:47 -0400

    sync

--- include/calmgr.h
+++ include/calmgr.h
@@ -1,5 +1,5 @@
 /* calmgr HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* calendar manager object */
--- include/ccfile.hh
+++ include/ccfile.hh
@@ -1,5 +1,5 @@
 /* ccfile HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* C++ file I-O operations */
