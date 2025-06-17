9f275ce5a7a2cf4be321f78393b020b9c26dd662
David A.D. Morano <david.a.morano@gmail.com>
2025-06-17 01:02:15 -0400

    sync

--- include/getnfile.h
+++ include/getnfile.h
@@ -1,5 +1,5 @@
 /* getnfile HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* calculate column values */
--- include/getnodename.h
+++ include/getnodename.h
@@ -1,5 +1,5 @@
 /* getnodename HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* get program root directory */
