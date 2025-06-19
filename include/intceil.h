153def648b8d05612650f99993af9b89108b81ab
David A.D. Morano <david.a.morano@gmail.com>
2025-06-19 03:54:27 -0400

    sync

--- include/intceil.h
+++ include/intceil.h
@@ -1,5 +1,5 @@
 /* intceil HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 (conformance reviewed) */
 
 /* Integer-Ceiling */
--- include/intfloor.h
+++ include/intfloor.h
@@ -1,5 +1,5 @@
 /* intfloor HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 (conformance reviewed) */
 
 /* Integer-Flooring */
