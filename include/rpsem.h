ec3f0c0795419b225716758e0dd293d35d691572
David A.D. Morano <david.a.morano@gmail.com>
2025-06-20 00:42:09 -0400

    sync

--- include/rpsem.h
+++ include/rpsem.h
@@ -1,5 +1,5 @@
 /* rpsem HEADER ("real" POSIX® Semaphore) */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* POSIX© unnamed Semaphore (RPSEM) */
--- include/sbuf.h
+++ include/sbuf.h
@@ -1,5 +1,5 @@
 /* sbuf HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* storage buffer (SBuf) object */
