59a67cb87f3aa9c7cf22b4d19175f8b83e25448e
David A.D. Morano <david.a.morano@gmail.com>
2025-06-20 00:41:09 -0400

    sync

--- include/pow.h
+++ include/pow.h
@@ -1,5 +1,5 @@
 /* pow HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* calculate the integer exponential of an integer */
--- include/proglog.hh
+++ include/proglog.hh
@@ -1,5 +1,5 @@
 /* proglog HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* program-logging */
