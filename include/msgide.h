42f4d3b054f3e31f4f09a35370f9ef779aa615e2
David A.D. Morano <david.a.morano@gmail.com>
2025-06-20 00:39:00 -0400

    comments + update

--- include/msgide.h
+++ include/msgide.h
@@ -1,5 +1,5 @@
 /* msgide HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* message identification (MSG-ID) entry */
--- include/msgidx.h
+++ include/msgidx.h
@@ -1,5 +1,5 @@
 /* msgdix HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* message identification (MSG-ID) entry */
