027288f3cee5cee1ddc558f2c2800c30c04cff7e
David A.D. Morano <david.a.morano@gmail.com>
2025-06-20 00:37:55 -0400

    comments + update

--- include/matparam.h
+++ include/matparam.h
@@ -1,5 +1,5 @@
 /* matparam HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* does the parameter array have a given value? */
--- include/memfile.h
+++ include/memfile.h
@@ -1,5 +1,5 @@
 /* memfile HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* provides a memory-mapped file for writing */
