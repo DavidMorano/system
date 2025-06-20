d428f6e100e317b7782cd0fd30090073b86f5e8e
David A.D. Morano <david.a.morano@gmail.com>
2025-06-20 00:45:52 -0400

    sync

--- include/syshelper.h
+++ include/syshelper.h
@@ -1,5 +1,5 @@
 /* syshelper HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 (conformance reviewed) */
 
 /* syshelper operations */
--- include/sysusers.h
+++ include/sysusers.h
@@ -1,5 +1,5 @@
 /* sysusers HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* system user-entry enumeration */
