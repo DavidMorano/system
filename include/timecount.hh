50af39047b3749c4b4fed25fc34c342d25485509
David A.D. Morano <david.a.morano@gmail.com>
2025-06-21 02:24:10 -0400

    sync

--- include/timecount.hh
+++ include/timecount.hh
@@ -1,5 +1,5 @@
 /* timecount HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 (conformance reviewed) */
 
 /* count-down timer */
--- include/tmpmailboxes.h
+++ include/tmpmailboxes.h
@@ -1,5 +1,5 @@
 /* tmpmailboxes HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* do something w/ temporary mailboxes */
