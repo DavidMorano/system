54383cfbda3f8107fb12d46e4ac5af1ad6f6c2f8
David A.D. Morano <david.a.morano@gmail.com>
2025-06-21 02:23:55 -0400

    sync

--- include/terment.h
+++ include/terment.h
@@ -1,5 +1,5 @@
 /* terment HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* methods for the TERMENT object */
--- include/termout.h
+++ include/termout.h
@@ -1,5 +1,5 @@
 /* termout HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* terminal-output management */
