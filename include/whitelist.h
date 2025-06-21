a30abd219fd98bc889361094c91a6ea435334515
David A.D. Morano <david.a.morano@gmail.com>
2025-06-21 02:30:35 -0400

    sync

--- include/whitelist.h
+++ include/whitelist.h
@@ -1,5 +1,5 @@
 /* whitelist HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* whitelist mail management */
--- include/wsix.h
+++ include/wsix.h
@@ -1,5 +1,5 @@
 /* wsix HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* wide-c-string find-index operations */
