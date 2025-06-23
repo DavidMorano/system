3e58134a4de15b26738d9e8e66218df845a91abe
David A.D. Morano <david.a.morano@gmail.com>
2025-06-23 01:47:59 -0400

    comments + update

--- src/libdam/ba.cc
+++ src/libdam/ba.cc
@@ -1,5 +1,5 @@
 /* ba SUPPORT (BitArray) */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* perform some bit-array type operations */
--- src/libdam/bookx/bcspec.cc
+++ src/libdam/bookx/bcspec.cc
@@ -1,5 +1,5 @@
 /* bcspec SUPPORT */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* load a Bible-Citation-Specification */
