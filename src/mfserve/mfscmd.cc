411d4d5e0171cbbfb004c5babe200e69aefd63f2
David A.D. Morano <david.a.morano@gmail.com>
2025-06-26 00:55:09 -0400

    restore

---
+++ src/mfserve/mfscmd.cc
@@ -0,0 +1,431 @@
+/* mfs-cmd SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* client-side code to initiate commands to the server */
+/* version %I% last-modified %G% */
+
+#define	CF_DEBUGS	0		/* compile-time debugging */
+#define	CF_DEBUG	0		/* switchable debug print-outs */
+
+/* revision history:
+
+	= 2008-09-01, David A­D­ Morano
+	This subroutine was adopted from the DWD program. I may not
+	have changed all of the comments correctly though!
+
+	= 2017-08-10, David A­D­ Morano
+	This subroutine was borrowed to code MFSERVE.
+
+*/
+
+/* Copyright © 2008,2017 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Description:
+	These subroutines handle some IPC related functions. This
+	is client-side code which interacts with the server code
+	(within the server (MFSERVE).
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+
+#if	defined(SFIO) && (SFIO > 0)
+#define	CF_SFIO	1
+#else
+#define	CF_SFIO	0
+#endif
+
+#if	(defined(KSHBUILTIN) && (KSHBUILTIN > 0))
+#include	<shell.h>
+#endif
+
+#include	<sys/types.h>
+#include	<sys/param.h>
+#include	<sys/stat.h>
+#include	<sys/socket.h>
+#include	<netinet/in.h>
+#include	<unistd.h>
+#include	<fcntl.h>
+#include	<climits>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<cstring>
+#include	<usystem.h>
+#include	<timestr.h>
+#include	<localmisc.h>
+
+#include	"shio.h"
+#include	"mfsmsg.h"
+#include	"mfsmain.h"
+#include	"mfsc.h"
+#include	"mfslocinfo.h"
+#include	"mfslog.h"
+#include	"defs.h"
+
+
+/* local defines */
+
+
+/* external subroutines */
+
+extern int	snddd(char *,int,uint,uint) ;
+extern int	mkpath2(char *,cchar *,cchar *) ;
+extern int	mkpath3(char *,cchar *,cchar *,cchar *) ;
+extern int	sfshrink(cchar *,int,cchar **) ;
+extern int	sfbasename(cchar *,int,cchar **) ;
+extern int	sfdirname(cchar *,int,cchar **) ;
+extern int	matostr(cchar **,int,cchar *,int) ;
+extern int	isNotPresent(int) ;
+extern int	isBadSend(int) ;
+
+extern int	progtmpdir(PROGINFO *,char *) ;
+extern int	progcheckdir(PROGINFO *,char *,int,int) ;
+extern int	progreqfile(PROGINFO *) ;
+
+#if	CF_DEBUGS || CF_DEBUG
+extern int	debugprintf(cchar *,...) ;
+extern int	strlinelen(cchar *,int,int) ;
+#endif
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+static int	mfscmder(PROGINFO *,MFSC *,SHIO *) ;
+
+static int	mfscmd_status(PROGINFO *,MFSC *,SHIO *) ;
+static int	mfscmd_help(PROGINFO *,MFSC *,SHIO *) ;
+static int	mfscmd_mark(PROGINFO *,MFSC *,SHIO *) ;
+static int	mfscmd_exit(PROGINFO *,MFSC *,SHIO *) ;
+static int	mfscmd_listeners(PROGINFO *,MFSC *,SHIO *) ;
+
+
+/* local variables */
+
+static cchar	*cmds[] = {
+	"status",
+	"help",
+	"getval",
+	"mark",
+	"exit",
+	"listeners",
+	NULL
+} ;
+
+enum cmds {
+	cmd_status,
+	cmd_help,
+	cmd_getval,
+	cmd_mark,
+	cmd_exit,
+	cmd_listeners,
+	cmd_overlast
+} ;
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int mfscmd(PROGINFO *pip,cchar *ofn) {
+	LOCINFO		*lip = pip->lip ;
+	int		rs ;
+	int		rs1 ;
+	int		wlen = 0 ;
+
+	if ((rs = locinfo_cmdscount(lip)) > 0) {
+	    SHIO	ofile, *ofp = &ofile ;
+	    cchar	*pn = pip->progname ;
+	    cchar	*fmt ;
+
+	    if ((ofn == NULL) || (ofn[0] == '\0'))
+	        ofn = STDFNOUT ;
+
+	    if ((rs = shio_open(ofp,ofn,"wct",0666)) >= 0) {
+		MFSC		c ;
+		const int	to = TO_OPENSERVE ;
+		cchar		*pr = pip->pr ;
+		if ((rs = mfsc_open(&c,pr,to)) > 0) {
+		    {
+			rs = mfscmder(pip,&c,ofp) ;
+			wlen += rs ;
+		    }
+	    	    rs1 = mfsc_close(&c) ;
+	    	    if (rs >= 0) rs = rs1 ;
+		} else if (rs == 0) {
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	debugprintf("mfscmd: SN rs=%d\n",rs) ;
+#endif
+	    	    fmt = "%s: server not available (%d)\n" ;
+	    	    shio_printf(pip->efp,fmt,pn,rs) ;
+		} else if (isNotPresent(rs)) {
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	debugprintf("mfscmd: SF rs=%d\n",rs) ;
+#endif
+	    	    fmt = "%s: server failure (%d)\n" ;
+	    	    shio_printf(pip->efp,fmt,pn,rs) ;
+		} /* end if (mfsc) */
+		rs1 = shio_close(ofp) ;
+		if (rs >= 0) rs = rs1 ;
+	   } else {
+		fmt = "%s: inaccessible output (%d)\n" ;
+		shio_printf(pip->efp,fmt,pn,rs) ;
+		shio_printf(pip->efp,"%s: ofile=%s\n",pn,ofn) ;
+	   } /* end if (file-output) */
+	} /* end if (positive) */
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	debugprintf("mfscmd: ret rs=%d wlen=%u\n",rs,wlen) ;
+#endif
+
+	return (rs >= 0) ? wlen : rs ;
+}
+/* end subroutine (mfscmd) */
+
+
+int mfscmd_svcname(PROGINFO *pip,int idx,cchar **rpp)
+{
+	const int	nidx = nelem(cmds) ;
+	int		rs = SR_OK ;
+	int		len = 0 ;
+
+	if (pip == NULL) return SR_FAULT ;
+
+	if ((idx >= 0) && (idx < nidx)) {
+	    if (cmds[idx] != NULL) {
+	        len = strlen(cmds[idx]) ;
+	    }
+	} else {
+	    rs = SR_NOTFOUND ;
+	}
+
+	if (rpp != NULL) {
+	    *rpp = (rs >= 0) ? cmds[idx] : NULL ;
+	}
+
+	return (rs >= 0) ? len : rs ;
+}
+/* end subroutine (mfscmd_svcname) */
+
+
+/* local subroutines */
+
+
+static int mfscmder(PROGINFO *pip,MFSC *pcp,SHIO *ofp)
+{
+	LOCINFO		*lip = pip->lip ;
+	KEYOPT		*kop ;
+	KEYOPT_CUR	kcur ;
+	int		rs ;
+	int		c = 0 ;
+
+	kop = &lip->cmds ;
+	if ((rs = keyopt_curbegin(kop,&kcur)) >= 0) {
+	    int		ci ;
+	    int		kl ;
+	    cchar	*kp ;
+	    cchar	*pn = pip->progname ;
+	    cchar	*fmt ;
+
+	    while (rs >= 0) {
+	        kl = keyopt_enumkeys(kop,&kcur,&kp) ;
+	        if (kl == SR_NOTFOUND) break ;
+	        rs = kl ;
+
+	        if (rs > 0) {
+	            if ((ci = matostr(cmds,1,kp,kl)) >= 0) {
+	                c += 1 ;
+	                switch (ci) {
+	                case cmd_status:
+	                    rs = mfscmd_status(pip,pcp,ofp) ;
+	                    break ;
+	                case cmd_help:
+	                    rs = mfscmd_help(pip,pcp,ofp) ;
+	                    break ;
+	                case cmd_mark:
+	                    rs = mfscmd_mark(pip,pcp,ofp) ;
+	                    break ;
+	                case cmd_exit:
+	                    rs = mfscmd_exit(pip,pcp,ofp) ;
+	                    break ;
+	                case cmd_listeners:
+	                    rs = mfscmd_listeners(pip,pcp,ofp) ;
+	                    break ;
+	                } /* end switch */
+		    } else {
+	                    fmt = "%s: unknown cmd=%r\n" ;
+	                    shio_printf(pip->efp,fmt,pn,kp,kl) ;
+			    {
+				char	tbuf[TIMEBUFLEN+1] ;
+				timestr_logz(pip->daytime,tbuf) ;
+				logprintf(pip,"%s unknown cmd=%r",tbuf,kp,kl) ;
+			    }
+	            } /* end if (valid) */
+	        } /* end if (positive) */
+
+	    } /* end while */
+
+	    keyopt_curend(kop,&kcur) ;
+	} /* end if (keyopt-cur) */
+
+	return (rs >= 0) ? c : rs ;
+}
+/* end subroutine (proccmd) */
+
+
+static int mfscmd_status(PROGINFO *pip,MFSC *pcp,SHIO *ofp)
+{
+	MFSC_STATUS	stat ;
+	int		rs ;
+	int		wlen = 0 ;
+	cchar		*fmt ;
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("mfscmd_status: ent\n") ;
+#endif
+
+	if (pip == NULL) return SR_FAULT ;
+
+	if ((rs = mfsc_status(pcp,&stat)) >= 0) {
+	    fmt = "server rc=%u pid=%u queries=%u\n" ;
+#if	CF_DEBUG
+	    if (DEBUGLEVEL(4))
+	        debugprintf("mfscmd_status: mfsc_status() rs=%d\n",rs) ;
+#endif
+	    rs = shio_printf(ofp,fmt,rs,stat.pid,stat.queries) ;
+	    wlen += rs ;
+	} else if (isBadSend(rs)) {
+	    fmt = "server failure (%d)\n" ;
+#if	CF_DEBUG
+	    if (DEBUGLEVEL(4))
+	        debugprintf("mfscmd_status: mfsc_status() rs=%d\n",rs) ;
+#endif
+	    rs = shio_printf(ofp,fmt,rs) ;
+	    wlen += rs ;
+	} /* end if (mfsc_status) */
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("mfscmd_status: ret rs=%d wlen=%u\n",rs,wlen) ;
+#endif
+
+	return (rs >= 0) ? wlen : rs ;
+}
+/* end subroutine (mfscmd_status) */
+
+
+static int mfscmd_help(PROGINFO *pip,MFSC *pcp,SHIO *ofp)
+{
+	const int	rlen = MAXNAMELEN ;
+	int		rs ;
+	int		rs1 ;
+	int		wlen = 0 ;
+	char		*rbuf ;
+
+	if (pip == NULL) return SR_FAULT ;
+
+	if ((rs = uc_malloc((rlen+1),&rbuf)) >= 0) {
+	    int		i ;
+	    for (i = 0 ; rs >= 0 ; i += 1) {
+	        if ((rs = mfsc_help(pcp,rbuf,rlen,i)) > 0) {
+		    rs = shio_print(ofp,rbuf,rs) ;
+		    wlen += rs ;
+		}
+		if (rs == 0) break ;
+	    } /* end for */
+	    rs1 = uc_free(rbuf) ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if (m-a-f) */
+
+	return (rs >= 0) ? wlen : rs ;
+}
+/* end subroutine (mfscmd_help) */
+
+
+static int mfscmd_mark(PROGINFO *pip,MFSC *pcp,SHIO *ofp)
+{
+	int		rs ;
+	int		wlen = 0 ;
+	cchar		*fmt ;
+
+	if (pip == NULL) return SR_FAULT ;
+
+	if ((rs = mfsc_mark(pcp)) >= 0) {
+	    fmt = "marking (%d)\n" ;
+	    rs = shio_printf(ofp,fmt,rs) ;
+	    wlen += rs ;
+	} else if (isBadSend(rs)) {
+	    fmt = "server failure (%d)\n" ;
+	    rs = shio_printf(ofp,fmt,rs) ;
+	    wlen += rs ;
+	} /* end if (mfsc_mark) */
+
+	return (rs >= 0) ? wlen : rs ;
+}
+/* end subroutine (mfscmd_mark) */
+
+
+static int mfscmd_exit(PROGINFO *pip,MFSC *pcp,SHIO *ofp)
+{
+	int		rs ;
+	int		wlen = 0 ;
+	cchar		*fmt ;
+
+	if (pip == NULL) return SR_FAULT ;
+
+	if ((rs = mfsc_exit(pcp,"extern")) >= 0) {
+	    fmt = "exiting (%d)\n" ;
+	    rs = shio_printf(ofp,fmt,rs) ;
+	    wlen += rs ;
+	} else if (isBadSend(rs)) {
+	    fmt = "server failure (%d)\n" ;
+	    rs = shio_printf(ofp,fmt,rs) ;
+	    wlen += rs ;
+	} /* end if (mfsc_exit) */
+
+	return (rs >= 0) ? wlen : rs ;
+}
+/* end subroutine (mfscmd_exit) */
+
+
+static int mfscmd_listeners(PROGINFO *pip,MFSC *pcp,SHIO *ofp)
+{
+	const int	rlen = (2*MAXPATHLEN) ;
+	int		rs ;
+	int		rs1 ;
+	int		wlen = 0 ;
+	char		*rbuf ;
+
+	if (pip == NULL) return SR_FAULT ;
+
+	if ((rs = uc_malloc((rlen+1),&rbuf)) >= 0) {
+	    int		i ;
+	    for (i = 0 ; rs >= 0 ; i += 1) {
+	        if ((rs = mfsc_listener(pcp,rbuf,rlen,i)) > 0) {
+		    rs = shio_print(ofp,rbuf,rs) ;
+		    wlen += rs ;
+		}
+		if (rs == 0) break ;
+	    } /* end for */
+	    rs1 = uc_free(rbuf) ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if (m-a-f) */
+
+	return (rs >= 0) ? wlen : rs ;
+}
+/* end subroutine (mfscmd_listeners) */
+
+
---
+++ src/mfserve/mfsconfig.cc
@@ -0,0 +1,612 @@
+/* mfs-config SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* handle MFS configuration functions */
+/* version %I% last-modified %G% */
+
+#define	CF_DEBUGS	0		/* non-switchable debug print-outs */
+#define	CF_DEBUG	0		/* switchable at invocation */
+
+/* revision history:
+
+	= 2004-03-01, David A­D­ Morano
+	This code was originally written.  
+
+	= 2017-08-10, David A­D­ Morano
+	This subroutine was borrowed to code MFSERVE.
+
+*/
+
+/* Copyright © 2004,2017 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Description:
+	These subroutines form part of the MFS program (yes, getting
+	a little bit more complicated every day now).
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<sys/types.h>
+#include	<sys/param.h>
+#include	<sys/stat.h>
+#include	<unistd.h>
+#include	<climits>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<cstring>
+#include	<usystem.h>
+#include	<vecstr.h>
+#include	<paramfile.h>
+#include	<expcook.h>
+#include	<logfile.h>
+#include	<prmkfname.h>
+#include	<strwcpy.h>
+#include	<strwcmp.h>
+#include	<localmisc.h>
+
+#include	"mfsmain.h"
+#include	"mfsconfig.h"
+#include	"mfslocinfo.h"
+#include	"mfslisten.h"
+#include	"defs.h"
+
+
+/* local typedefs */
+
+#ifndef	TYPEDEF_CCHAR
+#define	TYPEDEF_CCHAR	1
+typedef cchar	cchar ;
+#endif
+
+
+/* local defines */
+
+#ifndef	PBUFLEN
+#define	PBUFLEN		(4 * MAXPATHLEN)
+#endif
+
+#ifndef	VBUFLEN
+#define	VBUFLEN		(4 * MAXPATHLEN)
+#endif
+
+#ifndef	EBUFLEN
+#define	EBUFLEN		(3 * MAXPATHLEN)
+#endif
+
+#ifndef	DIGBUFLEN
+#define	DIGBUFLEN	40		/* can hold int128_t in decimal */
+#endif
+
+#define	DEBUGFNAME	"/tmp/mfs.deb"
+
+#define	MFS_REQCNAME	"req"
+
+
+/* external subroutines */
+
+extern int	snsd(char *,int,cchar *,uint) ;
+extern int	snsds(char *,int,cchar *,cchar *) ;
+extern int	sncpy1(char *,int,cchar *) ;
+extern int	sncpy2(char *,int,cchar *,cchar *) ;
+extern int	sncpy3(char *,int,cchar *,cchar *,cchar *) ;
+extern int	mkfnamesuf1(char *,cchar *,cchar *) ;
+extern int	mkpath1w(char *,cchar *,int) ;
+extern int	mkpath1(char *,cchar *) ;
+extern int	mkpath2(char *,cchar *,cchar *) ;
+extern int	mkpath3(char *,cchar *,cchar *,cchar *) ;
+extern int	sfdirname(cchar *,int,cchar **) ;
+extern int	sfshrink(cchar *,int,cchar **) ;
+extern int	matstr(cchar **,cchar *,int) ;
+extern int	matostr(cchar **,int,cchar *,int) ;
+extern int	cfdeci(cchar *,int,int *) ;
+extern int	cfdecui(cchar *,int,uint *) ;
+extern int	cfdecti(cchar *,int,int *) ;
+extern int	cfdecmfi(cchar *,int,int *) ;
+extern int	ctdeci(char *,int,int) ;
+extern int	optbool(cchar *,int) ;
+
+#if	CF_DEBUGS || CF_DEBUG
+extern int	debugprintf(cchar *,...) ;
+extern int	strlinelen(cchar *,int,int) ;
+#endif
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+static int	config_reader(CONFIG *,MFSLISTEN_ACQ *,char *,char *,char *) ;
+
+
+/* local variables */
+
+static cchar	*params[] = {
+	"cmd",
+	"logsize",
+	"msfile",
+	"pidfile",
+	"logfile",
+	"reqfile",
+	"builtin",
+	"svctab",
+	"acctab",
+	"runtime",
+	"runint",
+	"mspoll",
+	"pollint",
+	"markint",
+	"lockint",
+	"speedint",
+	"speedname",
+	"listen",
+	"svctype",
+	"users",
+	NULL
+} ;
+
+enum params {
+	param_cmd,
+	param_logsize,
+	param_msfile,
+	param_pidfile,
+	param_logfile,
+	param_reqfile,
+	param_builtin,
+	param_svctab,
+	param_acctab,
+	param_runtime,
+	param_intrun,
+	param_mspoll,
+	param_intpoll,
+	param_intmark,
+	param_intlock,
+	param_intspeed,
+	param_speedname,
+	param_listen,
+	param_svctype,
+	param_users,
+	param_overlast
+} ;
+
+
+/* exported subroutines */
+
+
+int config_start(CONFIG *cfp,PROGINFO *pip,cchar *cfname,int intcheck)
+{
+	LOCINFO		*lip = pip->lip ;
+	int		rs = SR_OK ;
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("config_start: ent cfname=%s intcheck=%d\n",
+			cfname,intcheck) ;
+#endif
+
+	if (cfp == NULL) return SR_FAULT ;
+	if (pip == NULL) return SR_FAULT ;
+	if (cfname == NULL) return SR_FAULT ;
+
+	if (cfname[0] == '\0') return SR_INVALID ;
+
+/* start in */
+
+	memset(cfp,0,sizeof(CONFIG)) ;
+	cfp->pip = pip ;
+	cfp->intcheck = intcheck ;
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4)) {
+	    debugprintf("config_start: cfname=%s\n",cfname) ;
+	    debugprintf("config_start: nodename=%s\n",pip->nodename) ;
+	    debugprintf("config_start: domainname=%s\n",pip->domainname) ;
+	}
+#endif
+
+	if (lip->open.cooks) {
+	    PARAMFILE	*pfp = &cfp->p ;
+	    if ((rs = paramfile_open(pfp,pip->envv,cfname)) >= 0) {
+#if	CF_DEBUG
+	        if (DEBUGLEVEL(4))
+	            debugprintf("config_start: paramfile_open rs=%d\n",rs) ;
+#endif
+	        cfp->f.p = TRUE ;
+	        rs = config_read(cfp) ;
+#if	CF_DEBUG
+	        if (DEBUGLEVEL(4))
+	            debugprintf("config_start: config_read rs=%d\n",rs) ;
+#endif
+		if (rs < 0) {
+		    cfp->f.p = FALSE ;
+		    paramfile_close(pfp) ;
+		}
+	    }
+	} else {
+	    rs = SR_BUGCHECK ;
+	}
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("config_start: ret rs=%d\n",rs) ;
+#endif
+
+	return rs ;
+}
+/* end subroutine (config_start) */
+
+
+int config_finish(CONFIG *cfp)
+{
+	PROGINFO	*pip ;
+	int		rs = SR_OK ;
+	int		rs1 ;
+
+#if	CF_DEBUGS
+	debugprintf("config_finish: ent\n") ;
+#endif
+
+	if (cfp == NULL) return SR_FAULT ;
+
+	pip = cfp->pip ;
+	if (pip == NULL) return SR_FAULT ;
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("config_finish: ent %c\n",
+		((cfp->f.p) ? '¥' : '_')) ;
+#endif
+
+	if (cfp->f.p) {
+	    PARAMFILE	*pfp = &cfp->p ;
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("config_finish: fins\n") ;
+#endif
+	    rs1 = paramfile_close(pfp) ;
+	    if (rs >= 0) rs = rs1 ;
+	} else
+	    rs = SR_NOTOPEN ;
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("config_finish: ret rs=%d\n",rs) ;
+#endif
+
+	return rs ;
+}
+/* end subroutine (config_finish) */
+
+
+int config_check(CONFIG *cfp)
+{
+	PROGINFO	*pip ;
+	int		rs = SR_OK ;
+	int		f_changed = FALSE ;
+
+	if (cfp == NULL) return SR_FAULT ;
+	pip = cfp->pip ;
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("mfsmain/config_check: ent f_p=%u\n",cfp->f.p) ;
+#endif
+
+	if (cfp->f.p) {
+	    const time_t	dt = pip->daytime ;
+	    const int		intcheck = cfp->intcheck ;
+	    int			f_check = TRUE ;
+
+	    f_check = f_check && (intcheck > 0) ;
+	    f_check = f_check && ((dt - cfp->ti_lastcheck) >= intcheck) ;
+	    if (f_check) {
+	        cfp->ti_lastcheck = dt ;
+
+#if	CF_DEBUG
+	        if (DEBUGLEVEL(4))
+	            debugprintf("mfsmain/config_check: paramfile_check()\n") ;
+#endif
+
+	        if ((rs = paramfile_check(&cfp->p,dt)) > 0) {
+	            f_changed = TRUE ;
+	            rs = config_read(cfp) ;
+
+#if	CF_DEBUG
+	            if (DEBUGLEVEL(4))
+	                debugprintf("mfsmain/config_check: "
+	                    "config_read() rs=%d\n",rs) ;
+#endif
+
+	        } /* end if (parameter file changed) */
+	    } /* end if (needed a check) */
+
+	} else {
+	    rs = SR_NOTOPEN ;
+	}
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("mfsmain/config_check: ret rs=%d f=%u\n",
+		rs,f_changed) ;
+#endif
+
+	return (rs >= 0) ? f_changed : rs ;
+}
+/* end subroutine (config_check) */
+
+
+int config_read(CONFIG *cfp)
+{
+	PROGINFO	*pip = cfp->pip ;
+	int		rs = SR_OK ;
+	int		rs1 ;
+	if (cfp->f.p) {
+	    MFSLISTEN_ACQ	acq ;
+	    if ((rs = mfslisten_acqbegin(pip,&acq)) >= 0) {
+	        int	size = 0 ;
+	        char	*a ;
+	        size += (PBUFLEN+1) ;
+	        size += (EBUFLEN+1) ;
+	        size += (MAXPATHLEN+1) ;
+	        if ((rs = uc_malloc(size,&a)) >= 0) {
+		    {
+			char	*bp = a ;
+		        char	*pbuf ;
+		        char	*ebuf ;
+		        char	*tbuf ;
+			pbuf = bp ; bp += (PBUFLEN+1) ;
+			ebuf = bp ; bp += (EBUFLEN+1) ;
+			tbuf = bp ; bp += (MAXPATHLEN+1) ;
+		        rs = config_reader(cfp,&acq,pbuf,ebuf,tbuf) ;
+	            }
+		    rs1 = uc_free(a) ;
+		    if (rs >= 0) rs = rs1 ;
+	        } /* end if (m-a-f) */
+	        rs1 = mfslisten_acqend(pip,&acq) ;
+	        if (rs >= 0) rs = rs1 ;
+	    } /* end if (mfslisten-acq) */
+	} /* end if (active) */
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("config_read: ret rs=%d\n",rs) ;
+#endif
+	return rs ;
+}
+/* end subroutine (config_read) */
+
+
+/* private subroutines */
+
+
+int config_reader(CONFIG *cfp,MFSLISTEN_ACQ *acp,
+		char *pbuf,char *ebuf,char *tbuf)
+{
+	PROGINFO	*pip = cfp->pip ;
+	LOCINFO		*lip ;
+	PARAMFILE	*pfp = &cfp->p ;
+	PARAMFILE_CUR	cur ;
+	PARAMFILE_ENT	pe ;
+	int		rs ;
+	int		rs1 ;
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("config_reader: ent f_p=%u\n",cfp->f.p) ;
+#endif
+
+	lip = pip->lip ;
+	if ((rs = paramfile_curbegin(pfp,&cur)) >= 0) {
+	    EXPCOOK	*ecp = &lip->cooks ;
+	    const int	elen = EBUFLEN ;
+	    const int	plen = PBUFLEN ;
+	    int		pi ;
+	    int		kl ;
+	    int		ml, vl, el ;
+	    int		v ;
+	    cchar	*pr = pip->pr ;
+	    cchar	*ccp ;
+	    cchar	*kp, *vp ;
+
+	    while (rs >= 0) {
+	        kl = paramfile_enum(pfp,&cur,&pe,pbuf,plen) ;
+	        if (kl == SR_NOTFOUND) break ;
+	        rs = kl ;
+	        if (rs < 0) break ;
+
+	        kp = pe.key ;
+	        vp = pe.value ;
+	        vl = pe.vlen ;
+
+	        pi = matostr(params,2,kp,kl) ;
+	        if (pi < 0) continue ;
+
+	        ebuf[0] = '\0' ;
+	        el = 0 ;
+	        if (vl > 0) {
+	            if ((el = expcook_exp(ecp,0,ebuf,elen,vp,vl)) >= 0) {
+	                ebuf[el] = '\0' ;
+		    }
+	        } /* end if */
+	        if (el < 0) continue ;
+
+#if	CF_DEBUG
+	        if (DEBUGLEVEL(4)) {
+	            debugprintf("config_read: ebuf=>%r<\n",ebuf,el) ;
+	            debugprintf("config_read: param=%s(%u)\n",
+	                params[pi],pi) ;
+	        }
+#endif
+
+
+	        switch (pi) {
+	        case param_logsize:
+	            if (el > 0) {
+	                rs1 = cfdecmfi(ebuf,el,&v) ;
+	                if (rs1 >= 0)
+	                    pip->logsize = v ;
+	            }
+	            break ;
+	        case param_intrun:
+	        case param_intpoll:
+	        case param_intmark:
+	        case param_intlock:
+	        case param_intspeed:
+	            if ((rs = cfdecti(ebuf,el,&v)) >= 0) {
+	                switch (pi) {
+	                case param_intrun:
+	                    if (! pip->final.intrun) pip->intrun = v ;
+	                    break ;
+	                case param_mspoll:
+	                case param_intpoll:
+	                    if (! pip->final.intpoll) pip->intpoll = v ;
+	                    break ;
+	                case param_intmark:
+	                    if (! pip->final.intmark) pip->intmark = v ;
+	                    break ;
+	                case param_intlock:
+	                    if (! pip->final.intlock) pip->intlock = v ;
+	                    break ;
+	                case param_intspeed:
+			    if (! lip->final.intspeed) lip->intspeed = v ;
+	                    break ;
+	                } /* end switch */
+	            } /* end if (cfdectinumber) */
+	            break ;
+	        case param_msfile:
+	            if (! lip->final.msfname) {
+	                lip->have.msfname = TRUE ;
+	                rs1 = prsetfname(pr,tbuf,ebuf,el,TRUE,
+	                    MSDNAME,MSFNAME,"") ;
+	                ccp = lip->msfname ;
+	                if ((ccp == NULL) ||
+	                    (strcmp(ccp,tbuf) != 0)) {
+			    cchar	**vpp = &lip->msfname ;
+	                    lip->changed.msfname = TRUE ;
+	                    rs = locinfo_setentry(lip,vpp,tbuf,rs1) ;
+	                }
+	            }
+	            break ;
+	        case param_pidfile:
+	            if (! pip->final.pidfname) {
+	                pip->have.pidfname = TRUE ;
+	                rs1 = prsetfname(pr,tbuf,ebuf,el,TRUE,
+	                    RUNDNAME,pip->nodename,PIDFNAME) ;
+	                ccp = pip->pidfname ;
+	                if ((ccp == NULL) ||
+	                    (strcmp(ccp,tbuf) != 0)) {
+			    cchar	**vpp = &pip->pidfname ;
+	                    pip->changed.pidfname = TRUE ;
+	                    rs = proginfo_setentry(pip,vpp,tbuf,rs1) ;
+	                }
+	            }
+	            break ;
+	        case param_logfile:
+	            if (! pip->final.logprog) {
+	                pip->have.logprog = TRUE ;
+	                rs1 = prsetfname(pr,tbuf,ebuf,el,TRUE,
+	                    LOGDNAME,pip->searchname,"") ;
+	                ccp = pip->lfname ;
+	                if ((ccp == NULL) ||
+	                    (strcmp(ccp,tbuf) != 0)) {
+			    cchar	**vpp = &pip->lfname ;
+	                    pip->changed.logprog = TRUE ;
+	                    rs = proginfo_setentry(pip,vpp,tbuf,rs1) ;
+	                }
+	            } /* end if */
+	            break ;
+	        case param_reqfile:
+	            if (! lip->final.reqfname) {
+	                lip->have.reqfname = TRUE ;
+#ifdef	COMMENT
+	                rs1 = prsetfname(pr,tbuf,ebuf,el,TRUE,
+	                    LOGDNAME,pip->searchname,"") ;
+#else			    
+	                rs1 = mkpath1w(tbuf,ebuf,el) ;
+#endif /* COMMENT */
+	                ccp = lip->reqfname ;
+	                if ((ccp == NULL) ||
+	                    (strcmp(ccp,tbuf) != 0)) {
+			    cchar	**vpp = &lip->reqfname ;
+	                    lip->changed.reqfname = TRUE ;
+	                    rs = locinfo_setentry(lip,vpp,tbuf,rs1) ;
+	                }
+	            } /* end if */
+	            break ;
+	        case param_svctab:
+	            if (! lip->final.svcfname) {
+			cchar	**vpp = &lip->svcfname ;
+	                lip->final.svcfname = TRUE ;
+	                rs = locinfo_setentry(lip,vpp,ebuf,el) ;
+	            }
+	            break ;
+	        case param_acctab:
+	            if (! lip->final.accfname) {
+			cchar	**vpp = &lip->accfname ;
+	                lip->final.accfname = TRUE ;
+	                rs = locinfo_setentry(lip,vpp,ebuf,el) ;
+	            }
+	            break ;
+	        case param_cmd:
+	            ml = MIN(LOGIDLEN,el) ;
+	            if (ml && (lip->cmd[0] == '\0')) {
+	                strwcpy(lip->cmd,ebuf,ml) ;
+		    }
+	            break ;
+	        case param_speedname:
+	            if (! lip->final.speedname) {
+	                lip->have.speedname = TRUE ;
+	                ccp = lip->speedname ;
+	                if ((ccp == NULL) ||
+	                    (strwcmp(ccp,ebuf,el) != 0)) {
+			    cchar	**vpp = &lip->speedname ;
+	                    lip->changed.speedname = TRUE ;
+	                    rs = locinfo_setentry(lip,vpp,ebuf,el) ;
+	                }
+	            } /* end if */
+	            break ;
+	       case param_listen:
+	            if (pip->f.daemon && (el > 0)) {
+	                rs = mfslisten_acqadd(pip,acp,ebuf,el) ;
+		    }
+		    break ;
+	       case param_svctype:
+	            if (pip->f.daemon && (el > 0)) {
+			if (! lip->final.svctype) {
+	                    rs = locinfo_svctype(lip,ebuf,el) ;
+			}
+		    }
+		    break ;
+	       case param_users:
+	            if (! lip->final.users) {
+			lip->final.users = TRUE ;
+			lip->f.users = TRUE ;
+			if (el > 0) {
+	                    rs = optbool(ebuf,el) ;
+			    lip->f.users = (rs > 0) ;
+			}
+		    }
+		    break ;
+	        } /* end switch */
+
+	    } /* end while (fetching) */
+
+	    rs1 = paramfile_curend(&cfp->p,&cur) ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if (paramfile-cur) */
+
+#if	CF_DEBUG
+	if (DEBUGLEVEL(4))
+	    debugprintf("config_reader: ret rs=%d\n",rs) ;
+#endif
+
+	return rs ;
+}
+/* end subroutine (config_reader) */
+
+
