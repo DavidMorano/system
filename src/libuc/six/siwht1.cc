40c840078be97d1aa8c8020f36f2fb90ea279945
David A.D. Morano <david.a.morano@gmail.com>
2025-06-27 00:36:59 -0400

    restore

---
+++ src/libuc/six/siwht1.cc
@@ -0,0 +1,126 @@
+/* siwht1 MODULE (module-implementation-unit) */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* subroutine to find the index of the first character not something */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-23, David A­D­ Morano
+	This code was originally written.
+
+	= 2020-03-22, David A­D­ Morano
+	I modularized this.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	siwht{xxx}
+
+	Description:
+	This subroutine searchs for an a character that is not part
+	of a certain character class.
+
+	Synopsis:
+	int siwht{xxx}(cchar *sp,int sl) noex
+
+	Arguments:
+	xxx	one of: oct, dec, hex
+	sp	string to be examined
+	sl	length of string of break character to break on
+
+	Returns:
+	>=0	index of found character
+	<0	the search character was not found
+
+*******************************************************************************/
+
+module ;
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<strn.h>
+#include	<localmisc.h>
+
+module siwht ;
+
+import strnwht ;			/* |strnwhtbrk(3uc)| */
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+extern "C" {
+    int siwht(cchar *sp,int sl) noex {
+    	cnullptr	np{} ;
+    	int		si = -1 ;
+	if (cchar *tp ; (tp = strnwht(sp,sl)) != np) {
+	    si = intconv(tp - sp) ;
+	}
+    	return si ;
+    } /* end subroutine (siwht) */
+    int siwhtbrk(cchar *sp,int sl,cchar *ss) noex {
+    	cnullptr	np{} ;
+    	int		si = -1 ;
+	if (cchar *tp ; (tp = strnwhtbrk(sp,sl,ss)) != np) {
+	    si = intconv(tp - sp) ;
+	}
+    	return si ;
+    } /* end subroutine (siwhtbrk) */
+    int siwhtchr(cchar *sp,int sl,int sch) noex {
+    	cnullptr	np{} ;
+    	int		si = -1 ;
+	if (cchar *tp ; (tp = strnwhtchr(sp,sl,sch)) != np) {
+	    si = intconv(tp - sp) ;
+	}
+    	return si ;
+    } /* end subroutine (siwhtchr) */
+} /* end extern */
+
+extern "C++" {
+    int siwhtbrk(cchar *sp,int sl,const chrset &sset) noex {
+    	int		si = -1 ;
+	if (cchar *tp ; (tp = strnwhtbrk(sp,sl,sset)) != nullptr) {
+	    si = intconv(tp - sp) ;
+	}
+    	return si ;
+    } /* end subroutine (siwhtbrk) */
+} /* end extern */
+
+
---
+++ src/libuc/six/sixbrk.cc
@@ -0,0 +1,184 @@
+/* sixbrk SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* subroutine to find a character in a given string */
+/* version %I% last-modified %G% */
+
+#define	CF_STRCHR	1		/* use |strchr(3c)| */
+
+/* revision history:
+
+	= 1998-03-23, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	siobrk
+	sirbrk
+
+	Description:
+	This subroutine determines if any of the characters from
+	the parameter string (argument 'ss') are found in the primary
+	string buffer specified ('s').  
+
+	Synopsis:
+	int siobrk(cchar *sp,int sl,cchar *ss) noex
+	int sirbrk(cchar *sp,int sl,cchar *ss) noex
+
+	Arguments:
+	sp	string to be examined pointer
+	sl	string to be examined length
+	ss	NUL-terminated substring to search for
+
+	Returns:
+	>=0	position (in 's') of a character from 'ss'
+	<0	no characters from string 'ss' were present in 's'
+
+	Notes:
+	This subroutine is almost identical to the |strcspn(3c)|
+	subroutine except that this subroutine returns (-1) if there
+	is a failure to match a charater, while |strcspn(3c)| returns
+	the index of the NUL character of the source string for a
+	failure to match.  The real problem with |strcspn(3c)| is
+	that it does not take a counted source c-string as an
+	argument, which is what we want here.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<cstring>		/* |strchr(3c)| */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<mkchar.h>
+#include	<localmisc.h>
+
+#include	"sixbrk.h"
+
+import libutil ;
+
+/* local defines */
+
+#ifndef	CF_STRCHR
+#define	CF_STRCHR	1		/* use |strchr(3c)| */
+#endif
+
+
+/* imported namespaces */
+
+using std::nullptr_t ;			/* type */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* forward references */
+
+
+/* local variables */
+
+constexpr bool		f_strchr = CF_STRCHR ;
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int siobrk(cchar *sp,int sl,cchar *ss) noex {
+	int		i = 0 ; /* return-value */
+	bool		f = false ;
+	if (sp && ss) {
+	    if (sl >= 0) {
+	        if_constexpr (f_strchr) {
+	            cnullptr	np{} ;
+	            for (i = 0 ; sl && sp[i] ; i += 1) {
+		        cint	ch = mkchar(sp[i]) ;
+		        f = (strchr(ss,ch) != np) ;
+	                if (f) break ;
+	                sl -= 1 ;
+	            } /* end for */
+	        } else {
+	            for (i = 0 ; sl && sp[i] ; i += 1) {
+	                for (int j = 0 ; ss[j] ; j += 1) {
+	                    f = (sp[i] == ss[j]) ;
+		            if (f) break ;
+	                } /* end for */
+	                if (f) break ;
+	                sl -= 1 ;
+	            } /* end for */
+	        } /* end if_constexpr (f_strchr) */
+	    } else {
+	        f = ((i = xstrcspn(sp,ss)) , (sp[i] != '\0')) ;
+	    }
+	} /* end if (non-null) */
+	return (f) ? i : -1 ;
+}
+/* end subroutine (siobrk) */
+
+int siobrk(cchar *sp,int sl,const chrset &sset) noex {
+	int		i = 0 ; /* return-value */
+	bool		f = false ;
+	if (sp) {
+	    if (sl < 0) sl = xstrlen(sp) ;
+	    if (sl >= 0) {
+	        for (i = 0 ; sl && sp[i] ; i += 1) {
+		    cint	ch = mkchar(sp[i]) ;
+		    if ((f = sset.tst(ch))) break ;
+	            sl -= 1 ;
+	        } /* end for */
+	    } /* end if (valid) */
+	} /* end if (non-null) */
+	return (f) ? i : -1 ;
+}
+/* end subroutine (siobrk) */
+
+int sirbrk(cchar *sp,int sl,cchar *ss) noex {
+	int		i = 0 ; /* return-value */
+	bool		f = false ;
+	if (sp && ss) {
+	    if (sl < 0) sl = xstrlen(sp) ;
+	    if (sl > 0) {
+	        cnullptr	np{} ;
+	        for (i = (sl - 1) ; i >= 0 ; i -= 1) {
+		    cint	ch = mkchar(sp[i]) ;
+		    if ((f = (strchr(ss,ch) != np))) break ;
+	        } /* end for */
+	    } /* end if */
+	} /* end if (non-null) */
+	return (f) ? i : -1 ;
+}
+/* end subroutine (sirbrk) */
+
+int sirbrk(cchar *sp,int sl,const chrset &sset) noex {
+	int		i = 0 ; /* return-value */
+	bool		f = false ;
+	if (sp) {
+	    if (sl < 0) sl = xstrlen(sp) ;
+	    if (sl > 0) {
+	        for (i = (sl - 1) ; i >= 0 ; i -= 1) {
+		    cint	ch = mkchar(sp[i]) ;
+		    if ((f = sset.tst(ch))) break ;
+	        } /* end for */
+	    } /* end if */
+	} /* end if (non-null) */
+	return (f) ? i : -1 ;
+}
+/* end subroutine (sirbrk) */
+
+
