dc10f1a7aab93c95bb71efde57e938596bbe0927
David A.D. Morano <david.a.morano@gmail.com>
2025-06-27 00:37:26 -0400

    restore

---
+++ src/libuc/sif.h
@@ -0,0 +1,36 @@
+/* sif HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* String-Interate-Find (SIF) object */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SIF_INCLUDE
+#define	SIF_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* __cplusplus */
+#endif /* SIF_INCLUDE */
+
+
