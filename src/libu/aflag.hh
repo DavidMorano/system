6989e8de3dd5b23ed1e113a1fea56216480aef67
David A.D. Morano <david.a.morano@gmail.com>
2025-06-28 00:20:35 -0400

    name-change + comments

--- src/libu/aflag.hh
+++ src/libu/aflag.hh
@@ -1,5 +1,5 @@
 /* aflag HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 (conformance reviewed) */
 
 /* atomic-flag */
--- src/libu/endian.cc
+++ src/libu/endian.cc
@@ -1,5 +1,5 @@
 /* endian SUPPORT */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* machine endian management */
