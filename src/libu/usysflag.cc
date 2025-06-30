eff98d068f4f4638c9e35e4647385f39a8b66ab3
David A.D. Morano <david.a.morano@gmail.com>
2025-06-30 01:01:37 -0400

    comments

--- src/libu/usysflag.cc
+++ src/libu/usysflag.cc
@@ -1,5 +1,5 @@
 /* usysflag SUPPORT */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* system flags */
--- src/libu/usysop.h
+++ src/libu/usysop.h
@@ -1,5 +1,5 @@
 /* usysop HEADER (UNIX® system operations) */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* translation layer interface for UNIX® equivalents */
