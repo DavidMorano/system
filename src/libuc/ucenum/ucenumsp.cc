5a38e1d2f59d90a97ae7b872bb2f57983ee416b5
David A.D. Morano <david.a.morano@gmail.com>
2025-06-30 00:55:11 -0400

    comments

--- src/libuc/ucenum/ucenumsp.cc
+++ src/libuc/ucenum/ucenumsp.cc
@@ -1,5 +1,5 @@
 /* ucenumsp SUPPORT */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* enumerate system SYSDB-XX entries */
@@ -32,7 +32,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<usystem.h>
-#include	<sysdbfname.h>
+#include	<sysdbfn.h>
 #include	<localmisc.h>
 
 #include	"ucenumxx.h"
@@ -74,9 +74,9 @@ constexpr sysdbfiles	w = sysdbfile_sp ;
 int ucenumsp_open(ucenumsp *op,cchar *efname) noex {
 	ucenumxxco<ucenumsp_ent>	ueo(op,mxx) ;
 	int		rs ;
-	if (cchar *rp{} ; (rs = sysdbfnameget(w,efname,&rp)) >= 0) {
+	if (cchar *rp ; (rs = sysdbfn_get(w,efname,&rp)) >= 0) {
 	    rs = ueo.open(rp) ;
-	} /* end if (sysdbfnameget) */
+	} /* end if (sysdbfn_get) */
 	return rs ;
 }
 /* end if (ucenumsp_open) */
--- src/libuc/ucenum/ucenumsv.h
+++ src/libuc/ucenum/ucenumsv.h
@@ -1,5 +1,5 @@
 /* ucenumsv HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* enumerate system SYSDB-XX entries */
