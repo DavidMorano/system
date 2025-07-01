1330427809e601e146921866ba585134660279a7
David A.D. Morano <david.a.morano@gmail.com>
2025-07-01 00:49:31 -0400

    comments

--- src/libuc/chrono/tmtime.hh
+++ src/libuc/chrono/tmtime.hh
@@ -1,5 +1,5 @@
 /* tmtime HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* this is supposed to provide OS-independent time management operations */
--- src/libuc/chrono/zdb.h
+++ src/libuc/chrono/zdb.h
@@ -1,5 +1,5 @@
 /* zdb HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* time-zone database management */
