e3be8422b94fad768446d49c5af23107d9cfc267
David A.D. Morano <david.a.morano@gmail.com>
2025-07-01 00:52:18 -0400

    comments

--- src/libuc/ema/ema_haveaddr.cc
+++ src/libuc/ema/ema_haveaddr.cc
@@ -1,5 +1,5 @@
 /* ema_haveaddr SUPPORT */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* EMA have a given address */
--- src/libuc/envs.h
+++ src/libuc/envs.h
@@ -1,5 +1,5 @@
 /* envs HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* environment-variable list container */
