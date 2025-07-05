d2f71901c69768cf88cff00311aa249188c12fcd
David A.D. Morano <david.a.morano@gmail.com>
2025-07-05 01:05:37 -0400

    comments + update

--- src/libuc/strcpy.h
+++ src/libuc/strcpy.h
@@ -1,5 +1,5 @@
 /* strcpy HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* copy a c-string to destination buffer (ad other functions) */
--- src/libuc/strcpyx.h
+++ src/libuc/strcpyx.h
@@ -1,5 +1,5 @@
 /* strcpyx HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* copy a c-string to destination buffer w/ variations on the theme */
