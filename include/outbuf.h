12514668a6b1da266679b0566de2f586ad976225
David A.D. Morano <david.a.morano@gmail.com>
2025-07-05 00:57:45 -0400

    sync

--- include/outbuf.h
+++ include/outbuf.h
@@ -29,12 +29,49 @@ struct outbuf_head {
 	int		f_alloc ;
 } ;
 
+#ifdef	__cplusplus
+enum outbufmems {
+	outbufmem_finish,
+	outbufmem_overlast
+} ;
+struct outbuf ;
+struct outbuf_co {
+	outbuf		*op = nullptr ;
+	int		w = -1 ;
+	void operator () (outbuf *p,int m) noex {
+	    op = p ;
+	    w = m ;
+	} ;
+	operator int () noex ;
+	int operator () () noex { 
+	    return operator int () ;
+	} ;
+} ; /* end struct (outbuf_co) */
+struct outbuf : outbuf_head {
+	outbuf_co	finish ;
+	outbuf() noex {
+	    finish(this,outbufmem_finish) ;
+	    obuf = nullptr ;
+	    f_alloc = false ;
+	} ;
+	outbuf(const outbuf &) = delete ;
+	outbuf &operator = (const outbuf &) = delete ;
+	int start(cchar *,int = -1) noex ;
+	int get(cchar **) noex ;
+	void dtor() noex ;
+	operator int () noex ;
+	destruct outbuf() {
+	    if (obuf || f_alloc) dtor() ;
+	} ;
+} ; /* end struct (outbuf) */
+#else	/* __cplusplus */
 typedef OUTBUF		outbuf ;
+#endif /* __cplusplus */
 
 EXTERNC_begin
 
 extern int	outbuf_start(outbuf *,char *,int) noex ;
-extern int	outbuf_get(outbuf *,char **) noex ;
+extern int	outbuf_get(outbuf *,cchar **) noex ;
 extern int	outbuf_finish(outbuf *) noex ;
 
 EXTERNC_end
