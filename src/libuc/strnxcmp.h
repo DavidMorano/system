4fc0ed2e62f61a7ab23dba43f2c72da37e462681
David A.D. Morano <david.a.morano@gmail.com>
2025-07-05 01:04:11 -0400

    comments + update

--- src/libuc/strnxcmp.h
+++ src/libuc/strnxcmp.h
@@ -1,5 +1,5 @@
 /* strnxcmp HEADER */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* find a character in a counted c-string */
--- src/libuc/strnxcmp/strnfoldcmp.cc
+++ src/libuc/strnxcmp/strnfoldcmp.cc
@@ -1,5 +1,5 @@
 /* strnfoldcmp SUPPORT */
-/* encoding=ISO8859-1 */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* string comparison w/ folded case */
