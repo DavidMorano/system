4f0ddce3791b8c04c552679f2ada755c292907e3
David A.D. Morano <david.a.morano@gmail.com>
2025-07-06 02:17:38 -0400

    comments

--- src/libpr/prmktmpdir.cc
+++ src/libpr/prmktmpdir.cc
@@ -51,7 +51,6 @@
 #include	<sys/stat.h>
 #include	<unistd.h>
 #include	<fcntl.h>
-#include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<cstring>
 #include	<usystem.h>
@@ -170,7 +169,7 @@ int prmktmpdir(cchar *pr,char *rbuf,cc *tmpdname,cc *dname,mode_t m) noex {
 			} /* end if (stat) */
 	            } /* end if (mkpath) */
 	        } else {
-	            len = strlen(rbuf) ;
+	            len = lenstr(rbuf) ;
 	 	}
 	        if ((rs >= 0) && (! f_prtmp)) {
 	            rs = subinfo_chown(sip,prtmpdname) ;
