d88a25f7ddec44212dfb13ac9e2ed2c2c4a3f7c8
David A.D. Morano <david.a.morano@gmail.com>
2025-07-13 00:54:10 -0400

    update

--- src/libdam/conslog.cc
+++ src/libdam/conslog.cc
@@ -18,8 +18,11 @@
 
 /*******************************************************************************
 
-	These are the subroutines in this module:
+  	Object:
+	conslog
 
+	Description:
+	These are the subroutines in this module:
 		conslog_open
 		conslog_write
 		conslog_printf
@@ -30,9 +33,6 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/types.h>
-#include	<sys/param.h>
-#include	<sys/stat.h>
 #include	<sys/log.h>		/* for LOG_MAXPS */
 #include	<sys/strlog.h>		/* interface definitions */
 #include	<sys/syslog.h>		/* for all other 'LOG_xxx' */
@@ -44,10 +44,12 @@
 #include	<cstdarg>
 #include	<cstring>
 #include	<usystem.h>
+#include	<mallocxx.h>
 #include	<ascii.h>
+#include	<strn.h>
+#include	<strdcpy.h>
 #include	<char.h>
 #include	<ischarx.h>
-#include	<strdcpy.h>
 #include	<localmisc.h>
 
 #include	"conslog.h"
@@ -87,9 +89,40 @@ struct colstate {
 
 /* forward references */
 
-static int	conslog_fileopen(CONSLOG *) noex ;
-static int	conslog_fileclose(CONSLOG *) noex ;
-static int	conslog_logdevice(CONSLOG *,int,cchar *,int) noex ;
+template<typename ... Args>
+static int conslog_ctor(conslog *op,Args ... args) noex {
+    	CONSLOG		*hop = op ;
+	int		rs = SR_FAULT ;
+	if (op && (args && ...)) {
+	    rs = memclear(hop) ;
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (conslog_ctor) */
+
+static int conslog_dtor(conslog *op) noex {
+	int		rs = SR_FAULT ;
+	if (op) {
+	    rs = SR_OK ;
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (conslog_dtor) */
+
+template<typename ... Args>
+static inline int conslog_magic(conslog *op,Args ... args) noex {
+	int		rs = SR_FAULT ;
+	if (op && (args && ...)) {
+	    rs = (op->magic == CONSLOG_MAGIC) ? SR_OK : SR_NOTOPEN ;
+	}
+	return rs ;
+}
+/* end subroutine (conslog_magic) */
+
+static int	conslog_fileready(conslog *) noex ;
+static int	conslog_fileopen(conslog *) noex ;
+static int	conslog_fileclose(conslog *) noex ;
+static int	conslog_logdevice(conslog *,int,cchar *,int) noex ;
 
 static boolt	isLogFac(int) noex ;
 
@@ -116,177 +149,136 @@ static cint	logfacs[] = {
 	LOG_LOCAL6,
 	LOG_LOCAL7,
 	-1
-} ;
+} ; /* end array (logfacs) */
+
+
+/* exported variables */
 
 
 /* exported subroutines */
 
-
-int conslog_open(CONSLOG *op,int logfac)
-{
-	int		rs = SR_OK ;
-
-	if (op == nullptr) return SR_FAULT ;
-
-	if (! isLogFac(logfac)) return SR_INVALID ;
-
-	memclear(op) ;
-	op->lfd = -1 ;
-
-	op->magic = CONSLOG_MAGIC ;
+int conslog_open(conslog *op,int logfac) noex {
+	int		rs ;
+	if ((rs = conslog_ctor(op)) >= 0) {
+	    rs = SR_INVALID ;
+	    memclear(op) ;
+	    if (isLogFac(logfac)) {
+	        op->lfd = -1 ;
+	        op->magic = CONSLOG_MAGIC ;
+	    } /* end if (valid) */
+	    if (rs < 0) {
+		conslog_dtor(op) ;
+	    }
+	} /* end if (conslog_ctor) */
 	return rs ;
 }
 /* end subroutine (conslog_open) */
 
-
-int conslog_close(CONSLOG *op)
-{
-	int		rs = SR_OK ;
+int conslog_close(conslog *op) noex {
+	int		rs ;
 	int		rs1 ;
-
-	if (op == nullptr) return SR_FAULT ;
-
-	if (op->magic != CONSLOG_MAGIC) return SR_NOTOPEN ;
-
-	rs1 = conslog_fileclose(op) ;
-	if (rs >= 0) rs = rs1 ;
-
-	op->magic = 0 ;
+	if ((rs = conslog_magic(op)) >= 0) {
+	    {
+	        rs1 = conslog_fileclose(op) ;
+	        if (rs >= 0) rs = rs1 ;
+	    }
+	    {
+	        rs1 = conslog_dtor(op) ;
+	        if (rs >= 0) rs = rs1 ;
+	    }
+	    op->magic = 0 ;
+	} /* end if (magic) */
 	return rs ;
 }
 /* end subroutine (conslog_close) */
 
-
-/* make a log entry */
-int conslog_printf(CONSLOG *op,int logpri,cchar fmt[],...)
-{
+int conslog_printf(conslog *op,int logpri,cchar *fmt,...) noex {
+	va_list		ap ;
 	int		rs ;
-
-	{
-	    va_list	ap ;
+	if ((rs = conslog_magic(op,fmt)) >= 0) {
 	    va_begin(ap,fmt) ;
 	    rs = conslog_vprintf(op,logpri,fmt,ap) ;
 	    va_end(ap) ;
-	} /* end block */
-
+	} /* end if (magic) */
 	return rs ;
 }
 /* end subroutine (conslog_printf) */
 
-
-/* make a log entry */
-int conslog_vprintf(CONSLOG *op,int logpri,cchar *fmt,va_list ap)
-{
-	int		rs = SR_OK ;
-	int		sl ;
-	int		ol ;
-	int		len = 0 ;
-	cchar	*tp, *sp ;
-	char		outbuf[OUTBUFLEN + 1] ;
-
-#if	CF_DEBUGS
-	debugprintf("conslog_printf: ent\n") ;
-#endif
-
-	if (op == nullptr) return SR_FAULT ;
-
-	if (op->magic != CONSLOG_MAGIC) return SR_BADF ;
-
-/* format the user's portion */
-
-	ol = bufvprintf(outbuf,OUTBUFLEN,fmt,ap) ;
-	if (ol < 0) ol = 0 ;
-
-#ifdef	COMMENT
-	if (ol > CONSLOG_USERLEN)
-	    ol = CONSLOG_USERLEN ;
-#endif
-
-	sp = outbuf ;
-	sl = ol ;
-	while ((tp = strnchr(sp,sl,'\n')) != nullptr) {
-
-	    rs = conslog_write(op,logpri,sp,(tp - sp)) ;
-	    len += rs ;
-
-	    sl -= ((tp + 1) - sp) ;
-	    sp = (tp + 1) ;
-
-	    if (rs < 0) break ;
-	} /* end while */
-
-	if ((rs >= 0) && (sl > 0)) {
-	    rs = conslog_write(op,logpri,sp,sl) ;
-	    len += rs ;
-	}
-
+int conslog_vprintf(conslog *op,int logpri,cchar *fmt,va_list ap) noex {
+    	cnullptr	np{} ;
+	int		rs ;
+	int		len = 0 ; /* return-value */
+	if ((rs = conslog_magic(op,fmt,ap)) >= 0) {
+	    if (char *lbuf ; (rs = malloc_ml(&lbuf)) >= 0) {
+		cint	llen = rs ;
+		if ((rs = snwvprintf(lbuf,llen,fmt,ap)) >= 0) {
+		    int		sl = rs ;
+		    cchar	*sp = lbuf ;
+	            for (cchar *tp ; (tp = strnchr(sp,sl,'\n')) != np ; ) {
+	                {
+		            cint tl = intconv(tp - sp) ;
+	                    rs = conslog_write(op,logpri,sp,tl) ;
+	                    len += rs ;
+	                }
+	                sl -= intconv((tp + 1) - sp) ;
+	                sp = (tp + 1) ;
+	                if (rs < 0) break ;
+		    } /* end while */
+		    if ((rs >= 0) && (sl > 0)) {
+	    	        rs = conslog_write(op,logpri,sp,sl) ;
+	    	        len += rs ;
+		    }
+		} /* end if (snwvprintf) */
+		rs = rsfree(rs,lbuf) ;
+	    } /* end if (m-a-f) */
+	} /* end if (magic) */
 	return (rs >= 0) ? len : rs ;
 }
 /* end subroutine (conslog_vprintf) */
 
-
-int conslog_check(CONSLOG *op,time_t dt)
-{
-	int		rs = SR_OK ;
-
-	if (op == nullptr) return SR_FAULT ;
-
-	if (op->magic != CONSLOG_MAGIC) return SR_NOTOPEN ;
-
-	if (op->lfd >= 0) {
-	    int		f = false ;
-	    f = f || ((dt - op->ti_write) >= TO_WRITE) ;
-	    f = f || ((dt - op->ti_open) >= TO_OPEN) ;
-	    if (f) {
-	        rs = conslog_fileclose(op) ;
-	    }
-	} /* end if (open) */
-
+int conslog_check(conslog *op,time_t dt) noex {
+	int		rs ;
+	if ((rs = conslog_magic(op)) >= 0) {
+	    if (op->lfd >= 0) {
+	        bool	f = false ;
+	        f = f || ((dt - op->ti_write) >= TO_WRITE) ;
+	        f = f || ((dt - op->ti_open) >= TO_OPEN) ;
+	        if (f) {
+	            rs = conslog_fileclose(op) ;
+	        }
+	    } /* end if (open) */
+	} /* end if (magic) */
 	return rs ;
 }
 /* end subroutine (conslog_check) */
 
-
-int conslog_write(CONSLOG *op,int logpri,cchar *wbuf,int wlen)
-{
-	int		rs = SR_OK ;
-
-	if (op == nullptr) return SR_FAULT ;
-	if (wbuf == nullptr) return SR_FAULT ;
-
-	if (wlen < 0)
-	    wlen = lenstr(wbuf) ;
-
-	while (wlen && iseol(wbuf[wlen-1])) {
-	    wlen -= 1 ;
-	}
-
-	while (wlen && CHAR_ISWHITE(wbuf[wlen-1])) {
-	    wlen -= 1 ;
-	}
-
-	if (wlen > 0) {
-	    if ((rs = conslog_logdevice(op,logpri,wbuf,wlen)) >= 0) {
-	        op->c += 1 ;
+int conslog_write(conslog *op,int logpri,cchar *wbuf,int wlen) noex {
+	int		rs ;
+	int		len = 0 ; /* return-value */
+	if ((rs = conslog_magic(op,wbuf)) >= 0) {
+	    if (wlen < 0) wlen = lenstr(wbuf) ;
+	    while (wlen && iseol(wbuf[wlen-1])) {
+	        wlen -= 1 ;
 	    }
-	}
-
-#if	CF_DEBUGS
-	debugprintf("conslog_printf: ret rs=%d\n",rs) ;
-#endif
-
-	return rs ;
+	    while (wlen && CHAR_ISWHITE(wbuf[wlen-1])) {
+	        wlen -= 1 ;
+	    }
+	    if (wlen > 0) {
+		if ((rs = conslog_logdevice(op,logpri,wbuf,wlen)) >= 0) {
+		    len = rs ;
+		    op->c += 1 ;
+		}
+	    }
+	} /* end if (magic) */
+	return (rs >= 0) ? len : rs ;
 }
 /* end subroutine (conslog_write) */
 
-
-int conslog_count(CONSLOG *op)
-{
+int conslog_count(conslog *op) noex {
 	int		rs ;
-
-	if (op == nullptr) return SR_FAULT ;
-
-	rs = op->c ;
+	if ((rs = conslog_magic(op)) >= 0) {
+	    rs = op->c ;
+	} /* end if (magic) */
 	return rs ;
 }
 /* end subroutine (conslog_count) */
@@ -294,7 +286,15 @@ int conslog_count(CONSLOG *op)
 
 /* private subroutines */
 
-static int conslog_fileopen(CONSLOG *op) noex {
+static int conslog_fileready(conslog *op) noex {
+    	int		rs = SR_OK ;
+	if (op->lfd < 0) {
+	    rs = conslog_fileopen(op) ;
+	}
+	return rs ;
+}
+
+static int conslog_fileopen(conslog *op) noex {
 	int		rs = SR_OK ;
 	if (op->lfd < 0) {
 	    cint	of = O_WRONLY ;
@@ -314,7 +314,7 @@ static int conslog_fileopen(CONSLOG *op) noex {
 }
 /* end subroutine (conslog_fileopen) */
 
-static int conslog_fileclose(CONSLOG *op) noex {
+static int conslog_fileclose(conslog *op) noex {
 	int		rs = SR_OK ;
 	int		rs1 ;
 	if (op->lfd >= 0) {
@@ -326,15 +326,12 @@ static int conslog_fileclose(CONSLOG *op) noex {
 }
 /* end subroutine (conslog_fileclose) */
 
-static int conslog_logdevice(CONSLOG *op,int logpri,cchar *bp,int bl) noex {
-	struct strbuf	cmsg ;
-	struct strbuf	dmsg ;
+static int conslog_logdevice(conslog *op,int logpri,cchar *bp,int bl) noex {
 	struct log_ctl	lc{} ;
-	int		rs = SR_OK ;
-
-	if (op->lfd < 0) rs = conslog_fileopen(op) ;
-
-	if (rs >= 0) {
+	STRBUF		cmsg ;
+	STRBUF		dmsg ;
+	int		rs ;
+	if ((rs = conslog_fileready(op)) >= 0) {
 	    logpri &= LOG_PRIMASK ;		/* truncate any garbage */
 	    /* write it to the LOG device */
 	    lc.flags = SL_CONSOLE ;
@@ -344,16 +341,12 @@ static int conslog_logdevice(CONSLOG *op,int logpri,cchar *bp,int bl) noex {
 	    cmsg.maxlen = szof(struct log_ctl) ;
 	    cmsg.len = szof(struct log_ctl) ;
 	    cmsg.buf = (caddr_t) &lc ;
-
 	    dmsg.maxlen = (bl+1) ;
 	    dmsg.len = bl ;
 	    dmsg.buf = (char *) bp ;
-
-/* output the message to the local logger */
+	    /* output the message to the local logger */
 	    rs = u_putmsg(op->lfd,&cmsg,&dmsg,0) ;
-
 	} /* end if (ok) */
-
 	return rs ;
 }
 /* end subroutine (conslog_logdevice) */
