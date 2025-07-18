0e3eb99988c086a6b159499e090e4ef7646b9d5d
David A.D. Morano <david.a.morano@gmail.com>
2025-07-18 00:30:07 -0400

    sync

---
+++ src/libuc/addrset/addrset.h
@@ -0,0 +1,32 @@
+/* addrset HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* string-index operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	ADDRSET_INCLUDE
+#define	ADDRSET_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* ADDRSET_INCLUDE */
+
+
