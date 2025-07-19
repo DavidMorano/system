db04cdd1abfc4ecbce1fa4ae839bf15fa79c506d
David A.D. Morano <david.a.morano@gmail.com>
2025-07-19 00:10:07 -0400

    sync

--- include/listenspec.h
+++ include/listenspec.h
@@ -1,4 +1,5 @@
 /* listenspec HEADER */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* this object holds a "listen" specification */
@@ -22,6 +23,8 @@
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
 #include	<sockaddress.h>
 #include	<localmisc.h>		/* |MAXNAMELEN| + |MAXPATHLEN| */
 
