ee15dac0e8b8a567dfbc4ad63f15b842efbccff1
David A.D. Morano <david.a.morano@gmail.com>
2025-07-21 00:35:50 -0400

    develop

---
+++ include/usysconf.h
@@ -0,0 +1,32 @@
+/* usysconf HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* File-Once management */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USYSCONF_INCLUDE
+#define	USYSCONF_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* USYSCONF_INCLUDE */
+
+
