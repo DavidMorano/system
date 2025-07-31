6741e6ac87dc77a80606bc96e8cda23e0d17bd8c
David A.D. Morano <david.a.morano@gmail.com>
2025-07-31 01:23:31 -0400

    comments + update

--- src/libdam/instr/instrrev.cc
+++ src/libdam/instr/instrrev.cc
@@ -1,34 +1,52 @@
-/* mkrevstr SUPPORT */
+/* instrrev SUPPORT */
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
-/* reverse the characters in a string in place */
+/* reverse the characters in a c-string in-place */
 /* version %I% last-modified %G% */
 
 
 /* revision history:
 
-	= 1998-02-01, David A­D­ Morano
-	This code was originally written.
+	= 1992-05-14, David A­D­ Morano
+	This was originally written for a UNIX-based hardware 
+	support tool.
 
 */
 
-/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1992 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
   	Name:
-	mkrevstr
+	instrrev
 
 	Description:
-	Reverse the characters of a string in place.
+	Reverse the characters of a string in-place.
+
+	Synopsis:
+	int instrrev(char *bp,int bl) noex
+
+	Arguments:
+	rp		source and destination buffer
+	rl		length of source string
+
+	Returns:
+	>=0		length of result
+	<0		error (system-return)
+
 
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<localmisc.h>
 
 #include	"instrrev.h"
 
@@ -50,6 +68,12 @@ import libutil ;
 
 /* forward references */
 
+static inline void swapends(char *bp,int bl,int i) noex {
+	cchar chr = bp[i] ;
+	bp[i] = bp[bl-i-1] ;
+	bp[bl-i-1] = chr ;
+} /* end subroutine (swapends) */
+
 
 /* local variables */
 
@@ -59,18 +83,17 @@ import libutil ;
 
 /* exported subroutines */
 
-int mkrevstr(char *bp,int bl) noex {
+int instrrev(char *bp,int bl) noex {
 	int		rs = SR_FAULT ;
 	if (bp) ylikely {
+	    rs = SR_OK ;
 	    if (bl < 0) bl = lenstr(bp) ;
 	    for (int i = 0 ; i < (bl/2) ; i += 1) {
-	        int	ch = bp[i] ;
-	        bp[i] = bp[bl-i-1] ;
-	        bp[bl-i-1] = char(ch) ;
+		swapends(bp,bl,i) ;
 	    } /* end for */
 	} /* end if (non-null) */
 	return (rs >= 0) ? bl : rs ;
 }
-/* end subroutine (mkrevstr) */
+/* end subroutine (instrrev) */
 
 
