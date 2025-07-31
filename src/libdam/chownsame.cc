f9961593f0b07496dda3ed0f2601c976ffe9530d
David A.D. Morano <david.a.morano@gmail.com>
2025-07-31 01:22:22 -0400

    comments + update

--- src/libdam/chownsame.cc
+++ src/libdam/chownsame.cc
@@ -5,7 +5,6 @@
 /* make all directories in a directory path */
 /* version %I% last-modified %G% */
 
-#define	CF_DEBUGS	0		/* compile-time debugging */
 
 /* revision history:
 
@@ -17,6 +16,9 @@
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
+
+  	Name:
+	chownsame
 
   	Description:
 	This subroutine changes the owner of a file (or directory)
@@ -38,16 +40,16 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-
-#include	<sys/types.h>
-#include	<sys/param.h>
 #include	<sys/stat.h>
-#include	<unistd.h>
+#include	<unistd.h>		/* |_PC_{xx}| */
 #include	<fcntl.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
 #include	<cstring>
-
 #include	<usystem.h>
 #include	<localmisc.h>
+
+#include	"chownsame.h"
 
 
 /* local defines */
@@ -59,41 +61,42 @@
 /* external variables */
 
 
+/* local structures */
+
+
 /* forward references */
 
 
 /* local variables */
 
 
+/* exported variables */
+
+
 /* external subroutines */
 
-
-int chownsame(cchar *dname,cchar *ref)
-{
-	const int	n = _PC_CHOWN_RESTRICTED ;
-	int		rs ;
-	int		f = FALSE ;
-
-	if (dname == NULL) return SR_FAULT ;
-	if (ref == NULL) return SR_FAULT ;
-
-	if (dname[0] == NULL) return SR_INVALID ;
-	if (ref[0] == NULL) return SR_INVALID ;
-
-#if	CF_DEBUGS
-	debugprintf("chownsame: ent dname=%s\n",dname) ;
-#endif
-
-	if ((rs = uc_pathconf(dname,n,NULL)) == 0) {
-	    USTAT	sb ;
-	    if ((rs = uc_stat(ref,&sb)) >= 0) {
-	        f = TRUE ;
-	        rs = uc_chown(dname,sb.st_uid,sb.st_gid) ;
-	    } /* end if (uc_stat) */
-	} /* end if (uc_pathconf) */
-
+int chownsame(cchar *dname,cchar *ref) noex {
+    	cnullptr	np{} ;
+	cint		cmd = _PC_CHOWN_RESTRICTED ;
+	int		rs = SR_FAULT ;
+	int		f = false ; /* return-value */
+	if (dname && ref) {
+	    rs = SR_INVALID ;
+	    if (dname[0] && ref[0]) {
+	        if ((rs = uc_pathconf(dname,cmd,np)) == 0) {
+	            if (ustat sb ; (rs = uc_stat(ref,&sb)) >= 0) {
+	                f = true ;
+	                rs = uc_chown(dname,sb.st_uid,sb.st_gid) ;
+	            } /* end if (uc_stat) */
+	        } /* end if (uc_pathconf) */
+	    } /* end if (valid) */
+	} /* end if (non-null) */
 	return (rs >= 0) ? f : rs ;
 }
 /* end subroutine (chownsame) */
+
+int chownref(cchar *dname,cchar *ref) noex {
+    	return chownsame(dname,ref) ;
+}
 
 
