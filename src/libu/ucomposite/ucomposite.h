5a94b48736d3402c90182dc856c12b0894c08d7e
David A.D. Morano <david.a.morano@gmail.com>
2025-08-07 00:12:25 -0400

    restore

---
+++ src/libu/ucomposite/ucomposite.h
@@ -0,0 +1,50 @@
+/* ucomposte HEADER (UNIX® file operations) */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* translation layer interface for UNIX® equivalents */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services
+	(RNS).
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Names:
+	u_copy
+
+	Description:
+	All of the UNIX® system calls that use (operate on) a
+	file-descroptor.
+
+*******************************************************************************/
+
+#ifndef	UCOPOSITE_INCLUDE
+#define	UCOPOSITE_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<unistd.h>
+#include	<fcntl.h>
+#include	<stddef.h>		/* |size_t| */
+#include	<stdint.h>		/* |uint32_t| */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+#include	<ucopy.h>
+
+
+#endif /* UCOPOSITE_INCLUDE */
+
+
