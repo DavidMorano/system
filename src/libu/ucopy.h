6d17db71e986ecad80ead2edb678a86f0fb8a603
David A.D. Morano <david.a.morano@gmail.com>
2025-08-07 00:12:34 -0400

    restore

---
+++ src/libu/ucopy.h
@@ -0,0 +1,41 @@
+/* ucopy HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* convert a single character to a given case */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCOPY_INCLUDE
+#define	UCOPY_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+EXTERNC_begin
+
+extern int	u_writefd(int,int) noex ;
+extern int	u_writefile(int,cchar *) noex ;
+extern int	u_copy(cchar *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCOPY_INCLUDE */
+
+
