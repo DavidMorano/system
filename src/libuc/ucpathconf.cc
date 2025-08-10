c5155204bd8c65392dfccad7f4da0ffa28371c55
David A.D. Morano <david.a.morano@gmail.com>
2025-08-10 00:49:50 -0400

    comments + update

--- src/libuc/ucpathconf.cc
+++ src/libuc/ucpathconf.cc
@@ -17,23 +17,18 @@
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/types.h>
-#include	<sys/param.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
 #include	<usystem.h>
-#include	<utypedefs.h>
-#include	<clanguage.h>
 #include	<getbufsize.h>
+#include	<mkx.h>
+#include	<localmisc.h>
 
 
 /* local defines */
 
 
 /* external subroutines */
-
-extern "C" {
-    extern int	uc_pathconf(cchar *,int,long *) noex ;
-    extern int	mkexpandpath(char *,cchar *,int) noex ;
-}
 
 
 /* external variables */
@@ -59,9 +54,8 @@ int uc_pathconf(cchar *fname,int name,long *rp) noex {
 	int		c = 0 ;
 	if ((rs = getbufsize(getbufsize_mp)) >= 0) {
 	    cint	elen = rs ;
-	    char	*ebuf ;
-	    if ((rs = uc_libmalloc((elen+1),&ebuf)) >= 0) {
-	        if ((rs = mkexpandpath(ebuf,fname,-1)) > 0) {
+	    if (char *ebuf ; (rs = uc_libmalloc((elen+1),&ebuf)) >= 0) {
+	        if ((rs = mkpathexp(ebuf,fname,-1)) > 0) {
 		    rs = u_pathconf(ebuf,name,rp) ;
 		    c = rs ;
 	        } else if (rs == 0) {
