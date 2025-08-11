c095ee429ae5e84ee5cdb068efe9019b003cdb17
David A.D. Morano <david.a.morano@gmail.com>
2025-08-11 03:15:20 -0400

    comments + update

--- src/libuc/envx/envlist.cc
+++ src/libuc/envx/envlist.cc
@@ -29,11 +29,10 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<cstring>		/* |strnchr(3c)| */
 #include	<new>			/* |nothrow(3c++)| */
 #include	<usystem.h>
 #include	<strpack.h>
-#include	<strn.h>
+#include	<strn.h>		/* |strnchr(3uc)| */
 #include	<strwcpy.h>
 #include	<localmisc.h>
 
@@ -43,21 +42,21 @@ import libutil ;
 
 /* local defines */
 
-#define	ENVLIST_DEFENT	10
+#define	EL_DEFENT	10
+#define	EL_CHUNKSIZE	ENVLIST_CHUNKSIZE
 
-#define	ENVLIST_DBINIT(op,n,at,hash,cmp)	\
+#define	EL_DBINIT(op,n,at,hash,cmp)	\
 					hdb_start((op),(n),(at),(hash),(cmp))
-#define	ENVLIST_DBSTORE(op,k,v)		hdb_store((op),(k),(v))
-#define	ENVLIST_DBFETCH(op,k,c,vp)	hdb_fetch((op),(k),(c),(vp))
-#define	ENVLIST_DBCOUNT(op)		hdb_count((op))
-#define	ENVLIST_DBFREE(op)		hdb_finish((op))
+#define	EL_DBSTORE(op,k,v)		hdb_store((op),(k),(v))
+#define	EL_DBFETCH(op,k,c,vp)	hdb_fetch((op),(k),(c),(vp))
+#define	EL_DBCOUNT(op)		hdb_count((op))
+#define	EL_DBFREE(op)		hdb_finish((op))
 
-#define	ENVLIST_DBDATA			HDB_DATUM
+#define	EL_DBDATA			HDB_DATUM
 
 
 /* imported namespaces */
 
-using std::nullptr_t ;			/* type */
 using std::nothrow ;			/* constant */
 
 
@@ -79,9 +78,9 @@ typedef strpack	*	strpackp ;
 
 template<typename ... Args>
 static int envlist_ctor(envlist *op,Args ... args) noex {
+	cnullptr	np{} ;
 	int		rs = SR_FAULT ;
 	if (op && (args && ...)) {
-	    cnullptr	np{} ;
 	    rs = SR_NOMEM ;
 	    op->store = nullptr ;
 	    if ((op->elp = new(nothrow) hdb) != np) {
@@ -89,8 +88,7 @@ static int envlist_ctor(envlist *op,Args ... args) noex {
 	    }
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (envlist_ctor) */
+} /* end subroutine (envlist_ctor) */
 
 static int envlist_dtor(envlist *op) noex {
 	int		rs = SR_FAULT ;
@@ -102,8 +100,7 @@ static int envlist_dtor(envlist *op) noex {
 	    }
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (clusterdb_dtor) */
+} /* end subroutine (envlist_dtor) */
 
 static int	envlist_store(envlist *,cchar *,int) noex ;
 static int	envlist_storer(envlist *) noex ;
@@ -120,7 +117,7 @@ static int	envlist_storer(envlist *) noex ;
 int envlist_start(envlist *op,int ne) noex {
 	int		rs ;
 	if ((rs = envlist_ctor(op)) >= 0) {
-	    rs = ENVLIST_DBINIT(op->elp,ne,0,nullptr,nullptr) ;
+	    rs = EL_DBINIT(op->elp,ne,0,nullptr,nullptr) ;
 	    if (rs < 0) {
 		envlist_dtor(op) ;
 	    }
@@ -147,7 +144,7 @@ int envlist_finish(envlist *op) noex {
 	        op->store = nullptr ;
 	    } /* end if (store) */
 	    {
-	        rs1 = ENVLIST_DBFREE(op->elp) ;
+	        rs1 = EL_DBFREE(op->elp) ;
 	        if (rs >= 0) rs = rs1 ;
 	    }
 	    {
@@ -166,7 +163,7 @@ int envlist_addkeyval(envlist *op,cchar *kp,cchar *vp,int vl) noex {
 	if (op && kp) {
 	    int		kl = lenstr(kp) ;
 	    int		sz = 1 ;
-	    sz += (kl+1) ;
+	    sz += (kl + 1) ;
 	    if (vp) {
 	        if (vl < 0) vl = lenstr(vp) ;
 	        sz += vl ;
@@ -193,8 +190,8 @@ int envlist_add(envlist *op,cchar *sp,int sl) noex {
 	int		rs = SR_FAULT ;
 	int		ridx = 0 ;
 	if (op && sp) {
-	    ENVLIST_DBDATA	key ;
-	    ENVLIST_DBDATA	val ;
+	    EL_DBDATA	key ;
+	    EL_DBDATA	val ;
 	    int			kl ;
 	    if (sl < 0) sl = lenstr(sp) ;
 	    kl = sl ;
@@ -205,7 +202,7 @@ int envlist_add(envlist *op,cchar *sp,int sl) noex {
 	    key.len = kl ;
 	    val.buf = sp ;
 	    val.len = sl ;
-	    rs = ENVLIST_DBSTORE(op->elp,key,val) ;
+	    rs = EL_DBSTORE(op->elp,key,val) ;
 	    ridx = rs ;
 	} /* end if (non-null) */
 	return (rs >= 0) ? ridx : rs ;
@@ -215,7 +212,7 @@ int envlist_add(envlist *op,cchar *sp,int sl) noex {
 int envlist_count(envlist *op) noex {
 	int		rs = SR_FAULT ;
 	if (op) {
-	    rs = ENVLIST_DBCOUNT(op->elp) ;
+	    rs = EL_DBCOUNT(op->elp) ;
 	} /* end if (non-null) */
 	return rs ;
 }
@@ -225,8 +222,8 @@ int envlist_present(envlist *op,cchar *sp,int sl,cchar **rpp) noex {
 	int		rs = SR_FAULT ;
 	int		vl = 0 ;
 	if (op && sp) {
-	    ENVLIST_DBDATA	key ;
-	    ENVLIST_DBDATA	val{} ;
+	    EL_DBDATA	key ;
+	    EL_DBDATA	val{} ;
 	    int			kl = 0 ;
 	    if (sl < 0) sl = lenstr(sp) ;
 	    kl += sl ;
@@ -235,7 +232,7 @@ int envlist_present(envlist *op,cchar *sp,int sl,cchar **rpp) noex {
 	    }
 	    key.buf = sp ;
 	    key.len = kl ;
-	    if ((rs = ENVLIST_DBFETCH(op->elp,key,nullptr,&val)) >= 0) {
+	    if ((rs = EL_DBFETCH(op->elp,key,nullptr,&val)) >= 0) {
 		vl = val.len ;
 	    }
 	    if (rpp) {
@@ -269,7 +266,7 @@ static int envlist_storer(envlist *op) noex {
 	    void	*vp{} ;
 	    if ((rs = uc_malloc(osize,&vp)) >= 0) {
 	        strpack		*spp = cast_static<strpackp>(vp) ;
-	        cint		csz = ENVLIST_CHUNKSIZE ;
+	        cint		csz = EL_CHUNKSIZE ;
 		op->store = vp ;
 	        rs = strpack_start(spp,csz) ;
 		if (rs < 0) {
