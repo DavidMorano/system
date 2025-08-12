65ae069c1d1fb2313f96d4b89f5f2e735090a6be
David A.D. Morano <david.a.morano@gmail.com>
2025-08-12 01:08:03 -0400

    comments + update

--- src/libuc/dial/dialuss.cc
+++ src/libuc/dial/dialuss.cc
@@ -29,7 +29,7 @@
 
 	Arguments:
 	dst		path to UNIX® domain socket to dial to
-	to		to ('>=0' mean use one, '-1' means don't)
+	to		to ('>=0' mean use one, '-1' means do not)
 	dot		any dial options
 
 	Returns:
