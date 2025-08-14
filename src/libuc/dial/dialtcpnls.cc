e4cd6b9edb37bd546a6e03e0fcb5b06b5eba6081
David A.D. Morano <david.a.morano@gmail.com>
2025-08-14 00:19:17 -0400

    comments + update

--- src/libuc/dial/dialtcpnls.cc
+++ src/libuc/dial/dialtcpnls.cc
@@ -33,7 +33,7 @@
 	ps		port specification to use
 	af		address family
 	svc		service specification
-	to		to ('>0' means use one, '-1' means don't)
+	to		to ('>0' means use one, '-1' means do not)
 	dot		any dial options
 
 	Returns:
