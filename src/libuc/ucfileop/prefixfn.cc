f7d2faafeb2bee7f767ed06abbcf670237bbbfd2
David A.D. Morano <david.a.morano@gmail.com>
2025-08-14 00:17:48 -0400

    comments + update

--- src/libuc/ucfileop/prefixfn.cc
+++ src/libuc/ucfileop/prefixfn.cc
@@ -31,17 +31,17 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<usystem.h>
-#include	<mallocxx.h>
-#include	<getpwd.h>
-#include	<strwcpy.h>
-#include	<pathadd.h>
+#include	<libmallocxx.h>
+#include	<mkx.h>			/* |mkpathexp(3uc)| */
+#include	<mkchar.h>
+#include	<ascii.h>
 #include	<localmisc.h>
 
 #include	"prefixfn.h"
 
 #pragma		GCC dependency	"mod/libutil.ccm"
 
-import libutil ;			/* |lenstr(3u)| */
+import libutil ;			/* |getlenstr(3u)| */
 
 /* local defines */
 
@@ -58,13 +58,26 @@ import libutil ;			/* |lenstr(3u)| */
 /* external variables */
 
 
+/* local structures */
+
+
 /* forward references */
 
-static int	prefixfn_loadpwd(prefixfn *,cchar *,int,cchar **) noex ;
-static int	prefixfn_loadnul(prefixfn *,cchar *,int,cchar **) noex ;
+local int	prefixfn_loadexp(prefixfn *,cchar *,int,cchar **) noex ;
+local int	prefixfn_loadnul(prefixfn *,cchar *,int,cchar **) noex ;
+
+local bool isprefix(int ch) noex {
+    	cint	ch_tilde	= CH_TILDE ;
+    	cint	ch_var		= mkchar('¬') ;
+    	return ((ch == ch_tilde) || (ch == ch_var)) ;
+} /* end subroutine (isprefix) */
 
 
 /* local variables */
+
+constexpr cauto		mem_almp = libmalloc_mp ;
+constexpr cauto		mem_alst = uc_libmallocstrw ;
+constexpr cauto		mem_free = uc_libfree ;
 
 
 /* exported variables */
@@ -79,10 +92,11 @@ int prefixfn_start(prefixfn *op,cchar *sp,int µsl,cchar **rpp) noex {
 	    rs = SR_INVALID ;
 	    op->as = nullptr ;
 	    if ((sl = getlenstr(sp,µsl)) > 0) {
+		cint	chx_prefix = mkchar(sp[0]) ;
 	        rs = SR_OK ;
 	        *rpp = sp ;
-	        if (sp[0] != '/') {
-		    rs = prefixfn_loadpwd(op,sp,sl,rpp) ;
+	        if (isprefix(chx_prefix)) {
+		    rs = prefixfn_loadexp(op,sp,sl,rpp) ;
 		    sl = rs ;
 		} else if (sp[sl] != '\0') {
 		    rs = prefixfn_loadnul(op,sp,sl,rpp) ;
@@ -101,11 +115,10 @@ int prefixfn_finish(prefixfn *op) noex {
 	    rs = SR_OK ;
 	    if (op->as) {
 		char *bp = cast_const<charp>(op->as) ;
-	        rs1 = uc_libfree(bp) ;
+	        rs1 = mem_free(bp) ;
 	        if (rs >= 0) rs = rs1 ;
 	        op->as = nullptr ;
 	    }
-	    op->buf[0] = '\0' ;
 	} /* end if (non-null) */
 	return rs ;
 }
@@ -114,44 +127,38 @@ int prefixfn_finish(prefixfn *op) noex {
 
 /* local subroutines */
 
-static int prefixfn_loadpwd(prefixfn *op,cchar *sp,int sl,cchar **rpp) noex {
+local int prefixfn_loadexp(prefixfn *op,cchar *sp,int sl,cchar **rpp) noex {
 	int		rs ;
 	int		rs1 ;
-	if (char *pbuf ; (rs = malloc_mp(&pbuf)) >= 0) ylikely {
-	    cint	plen = rs ;
-	    if ((rs = getpwd(pbuf,plen)) >= 0) ylikely {
-	 	if ((rs = pathaddw(pbuf,rs,sp,sl)) >= 0) ylikely {
+	if (char *pbuf ; (rs = mem_almp(&pbuf)) >= 0) ylikely {
+	    if ((rs = mkpathexp(pbuf,sp,sl)) > 0) {
+		if (cchar *cp ; (rs = mem_alst(pbuf,rs,&cp)) >= 0) {
 		    sl = rs ;
-	            if (sl > PREFIXFN_SHORTLEN) {
-	                if (cc *cp ; (rs = uc_libmallocstrw(sp,sl,&cp)) >= 0) {
-	                    *rpp = cp ;
-	                    op->as = cp ;
-	                } /* end if (memory-allocation) */
-	            } else {
-	                *rpp = op->buf ;
-	                strwcpy(op->buf,sp,sl) ;
-	            } /* end if */
-	        } /* end if (pathadd) */
-	    } /* end if (getpwd) */
-	    rs1 = uc_free(pbuf) ;
+		    op->as = cp ;
+		    *rpp = cp ;
+		}
+	    } else if (rs == 0) {
+		if (sp[sl] != '\0') {
+		    rs = prefixfn_loadnul(op,sp,sl,rpp) ;
+		    sl = rs ;
+		} else {
+		    *rpp = sp ;
+		}
+	    } /* end if (mkpathexp) */
+	    rs1 = mem_free(pbuf) ;
 	    if (rs >= 0) rs = rs1 ;
 	} /* end if (m-a-f) */
 	return (rs >= 0) ? sl : rs ;
 }
-/* end subroutine (prefixfn_loadpwd) */
+/* end subroutine (prefixfn_loadexp) */
 
-static int prefixfn_loadnul(prefixfn *op,cchar *sp,int sl,cchar **rpp) noex {
-	int		rs = SR_OK ;
-	if (sl > PREFIXFN_SHORTLEN) {
-	    if (cchar *cp ; (rs = uc_libmallocstrw(sp,sl,&cp)) >= 0) {
-		sl = rs ;
-		*rpp = cp ;
-		op->as = cp ;
-	    } /* end if (memory-allocation) */
-	} else {
-	    *rpp = op->buf ;
-	    strwcpy(op->buf,sp,sl) ;
-	} /* end if */
+local int prefixfn_loadnul(prefixfn *op,cchar *sp,int sl,cchar **rpp) noex {
+	int		rs ;
+	if (cchar *cp ; (rs = mem_alst(sp,sl,&cp)) >= 0) {
+	    sl = rs ;
+	    op->as = cp ;
+	    *rpp = cp ;
+	} /* end if (memory-allocation) */
 	return (rs >= 0) ? sl : rs ;
 }
 /* end subroutine (prefixfn_loadnul) */
