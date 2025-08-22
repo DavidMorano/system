09d3e969f416d65759e9d07b9cb01fc7ef4b4aba
David A.D. Morano <david.a.morano@gmail.com>
2025-08-22 00:04:33 -0400

    comments + update

--- src/libdam/readn.cc
+++ src/libdam/readn.cc
@@ -1,22 +1,66 @@
-/* readn */
+/* readn SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* read from a file into a buffer */
+/* version %I% last-modified %G% */
 
 
+/* revision history:
 
-extern int	uc_readn(int,void *,int) ;
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+/*******************************************************************************
+
+  	Name:
+	readn
+
+	Description:
+	This ruboutine reads data from a file into a specified buffer.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<usystem.h>
+#include	<localmisc.h>
+
+/* local defines */
 
 
+/* local namespaces */
 
 
-
-int readn(fd,buf,len)
-int	fd ;
-char	buf[] ;
-int	len ;
-{
+/* local typedefs */
 
 
-	return uc_readn(fd,buf,len) ;
-}
+/* external subroutines */
 
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int readn(int fd,void *rbuf,int rlen) noex {
+	return uc_readn(fd,rbuf,rlen) ;
+} /* end subroutine (readn) */
 
 
