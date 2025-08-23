c592819dd70f056ee2d9f0d8c888475afb970c2c
David A.D. Morano <david.a.morano@gmail.com>
2025-08-23 00:27:23 -0400

    comments + update

--- src/libdam/langx/langproc.h
+++ src/libdam/langx/langproc.h
@@ -34,7 +34,7 @@
 
 
 struct langproc_head {
-	langstate	*lsp ;		/* LANGSTAT pointer */
+	langstate	*lsp ;		/* LANGSTATE pointer */
 	void		*lvp ;		/* line-value-pointer */
 	uint		magic ;
 } ;
@@ -43,7 +43,52 @@ struct langproc_cur {
     	int		i ;
 } ;
 
+#ifdef	__cplusplus
+enum langprocmems {
+    	langprocmem_start,
+	langprocmem_finish,
+	langprocmem_count,
+	langprocmem_overlast
+} ;
+struct langproc ;
+struct langproc_co {
+	langproc	*op = nullptr ;
+	int		w = -1 ;
+	void operator () (langproc *p,int m) noex {
+	    op = p ;
+	    w = m ;
+	} ;
+	operator int () noex ;
+	int operator () () noex { 
+	    return operator int () ;
+	} ;
+} ; /* end struct (langproc_co) */
+struct langproc : langproc_head {
+	langproc_co	start ;
+	langproc_co	finish ;
+	langproc_co	count ;
+	langproc() noex {
+	    start	(this,langprocmem_start) ;
+	    finish	(this,langprocmem_finish) ;
+	    count	(this,langprocmem_count) ;
+	    magic = 0 ;
+	} ;
+	langproc(const langproc &) = delete ;
+	langproc &operator = (const langproc &) = delete ;
+	int proc(int,cchar *,int = -1) noex ;
+	int curbegin(langproc_cur *) noex ;
+	int curend(langproc_cur *) noex ;
+	int curenum(langproc_cur *,cchar **) noex ;
+	operator int () noex ;
+	void dtor() noex ;
+	destruct langproc() {
+	    if (magic) dtor() ;
+	} ;
+} ; /* end struct (langproc) */
+#else	/* __cplusplus */
 typedef LANGPROC	langproc ;
+#endif /* __cplusplus */
+
 typedef LANGPROC_CUR	langproc_cur ;
 
 EXTERNC_begin
@@ -52,8 +97,8 @@ extern int langproc_start(langproc *,cchar *) noex ;
 extern int langproc_proc(langproc *,int,cchar *,int) noex ;
 extern int langproc_count(langproc *) noex ;
 extern int langproc_curbegin(langproc *,langproc_cur *) noex ;
-extern int langproc_curenum(langproc *,langproc_cur *,cchar **) noex ;
 extern int langproc_curend(langproc *,langproc_cur *) noex ;
+extern int langproc_curenum(langproc *,langproc_cur *,cchar **) noex ;
 extern int langproc_finish(langproc *) noex ;
 
 EXTERNC_end
