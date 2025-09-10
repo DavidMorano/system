577f4e008e93a926ca93e990eeaae311794bbdaa
David A.D. Morano <david.a.morano@gmail.com>
2025-09-10 00:37:57 -0400

    restore

---
+++ src/libuc/termx/termx.h
@@ -0,0 +1,41 @@
+/* termx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* string-index operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	TERMX_INCLUDE
+#define	TERMX_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+#include	<termcharset.h>
+#include	<termcmd.h>
+#include	<termconseq.h>
+#include	<termdevice.h>
+#include	<termescseq.h>
+#include	<termios_cf.h>
+#include	<termstr.h>
+#include	<termtypemat.h>
+
+
+#endif /* TERMX_INCLUDE */
+
+
