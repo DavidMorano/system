78296bb5fa0b21b54c1be9e851e096650652f6f5
David A.D. Morano <david.a.morano@gmail.com>
2025-09-10 00:44:39 -0400

    restore

---
+++ src/libuc/colx/colx.h
@@ -0,0 +1,35 @@
+/* colx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* calculate column values */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	COLX_INCLUDE
+#define	COLX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+#include	<ncol.h>
+#include	<nlinecols.h>
+#include	<tabcols.h>
+
+
+#endif /* COLX_INCLUDE */
+
+
