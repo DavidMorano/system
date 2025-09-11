e8ebd6a62dfaac74f29d94b43c02938979ba2502
David A.D. Morano <david.a.morano@gmail.com>
2025-09-11 01:08:22 -0400

    comments + update

--- src/libuc/vec/vecpstr_ext.cc
+++ src/libuc/vec/vecpstr_ext.cc
@@ -31,7 +31,12 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
 #include	<localmisc.h>
 
 #include	"vecpstr.h"
