8d9befe455c809124a63ae5e3315de51ad379e70
David A.D. Morano <david.a.morano@gmail.com>
2025-09-11 00:40:34 -0400

    restore

---
+++ src/libu/ulibvals/ulibvals.h
@@ -0,0 +1,32 @@
+/* ulibvals HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* UNIXRO library values (constants) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	ULIBVALS_INCLUDE
+#define	ULIBVALS_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* ULIBVALS_INCLUDE */
+
+
