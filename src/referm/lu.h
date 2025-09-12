b41e7946417da03f22d7f834ba62e3f0860c4d1a
David A.D. Morano <david.a.morano@gmail.com>
2025-09-12 12:22:39 -0400

    comments + update

--- src/referm/lu.h
+++ src/referm/lu.h
@@ -1,62 +1,72 @@
-/* lu */
+/* lu HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* lookup a reference in the databases */
+/* version %I% last-modified %G% */
 
 
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	LU_INCLUDE
-#define	LU_INCLUDE	1
+#define	LU_INCLUDE
 
 
-
-#include	<sys/types.h>
-
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
 #include	<bfile.h>
 #include	<hdb.h>
 
 
+#define	LU		struct lu_head
+#define	LU_FL		struct lu_flags
+#define	LU_CUR		struct hdb_cursor
+#define	LU_VAL		struct hdb_value
 
 
 struct lu_flags {
-	uint	open : 1 ;
+	uint		open:1 ;
 } ;
 
 struct lu_value {
-	struct lu_flags	f ;
-	bfile			infile, outfile, errfile ;
+	bfile		infile ;
+	bfile		outfile ;
+	bfile		errfile ;
+	LU_FL		fl ;
 } ;
 
 struct lu_head {
-	int		magic ;
-	HDB_HEAD	lh ;
+	hdb		lh ;
+	uint		magic ;
 } ;
 
+typedef LU		lu ;
+typedef LU_FL		lu_fl ;
+typedef LU_CUR		lu_cur ;
+typedef LU_VAL		lu_val ;
 
+EXTERNC_begin
 
-typedef struct lu_head	lu ;
+extern int	lu_init(lu *) noex ;
+extern int	lu_free(lu *) noex ;
+extern int	lu_count(lu *) noex ;
+extern int	lu_getkey(lu *,lu_cur *,cchar **) noex ;
+extern int	lu_delcur(lu *,lu_cur *) noex ;
 
-
-
-#define	LU		struct lu_head
-
-#define	LU_HEAD		struct lu_head
-#define	LU_CUR		struct hdb_cur
-#define	LU_CUR	struct hdb_cur
-#define	LU_VALUE	struct hdb_value
-
-
-
-
-#ifndef	LU_MASTER
-
-extern int	bdbinit(LU *) ;
-extern int	bdbfree(LU *) ;
-extern int	bdbcount(LU *) ;
-extern int	bdbgetkey(LU *,LU_CUR *,char **) ;
-extern int	bdbdelcursor(LU *,LU_CUR *) ;
-
-#endif /* LU_MASTER */
+EXTERNC_end
 
 
 #endif /* LU_INCLUDE */
-
 
 
