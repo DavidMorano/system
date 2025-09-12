87bf7530add3637c5b1364a055cff98ef37209d2
David A.D. Morano <david.a.morano@gmail.com>
2025-09-12 12:20:19 -0400

    comments + update

--- src/shiftargs/main.cc
+++ src/shiftargs/main.cc
@@ -178,7 +178,7 @@ char	*envv[] ;
 
 	if (bopen(&errfile,BFILE_STDERR,"dwca",0666) >= 0) {
 	    pip->efp = &errfile ;
-	    pip->f.errfile = TRUE ;
+	    pip->fl.errfile = TRUE ;
 	    bcontrol(&errfile,BC_LINEBUF,0) ;
 	}
 
@@ -186,7 +186,7 @@ char	*envv[] ;
 
 	pip->debuglevel = 0 ;
 	pip->verboselevel = 1 ;
-	pip->f.quiet = FALSE ;
+	pip->fl.quiet = FALSE ;
 	pip->pr = NULL ;
 
 /* start parsing the arguments */
@@ -383,7 +383,7 @@ char	*envv[] ;
 
 /* quiet mode */
 	                        case 'q':
-	                            pip->f.quiet = TRUE ;
+	                            pip->fl.quiet = TRUE ;
 	                            break ;
 
 /* verbose mode */
