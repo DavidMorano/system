6a2ee740753389887bab175233564bd97b73d301
David A.D. Morano <david.a.morano@gmail.com>
2025-09-12 12:19:51 -0400

    comments + update

--- src/smservices/smservices.h
+++ src/smservices/smservices.h
@@ -1,63 +1,58 @@
-/* smservices */
+/* smservices HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 
 #ifndef	SMSERVICES_INCLUDE
 #define	SMSERVICES_INCLUDE
 
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<vecelem.h>
 
-
-/* object defines */
 
 #define	SMSERVICES		struct smservices_head
-#define	SMSERVICES_CUR	struct smservices_c
-#define	SMSERVICES_ENT	struct smservices_e
-
-
+#define	SMSERVICES_CUR		struct smservices_cursor
+#define	SMSERVICES_ENT		struct smservices_entry
 #define	SMSERVICES_SVCLEN	32
 
 
-
-#include	<sys/types.h>
-
-#include	<vecelem.h>
-
-#include	"localmisc.h"
-
-
-
-
 struct smservices_head {
-	unsigned long	magic ;
-	vecelem		entries ;
 	char		*fname ;
+	vecelem		entries ;
+	uint		magic ;
 } ;
 
-struct smservices_e {
-	uint	svc ;
-	char	canonname[SMSERVICES_SVCLEN + 1] ;
-	char	svcname[SMSERVICES_SVCLEN + 1] ;
+struct smservices_enrty {
+	uint		svc ;
+	char		canonname[SMSERVICES_SVCLEN + 1] ;
+	char		svcname[SMSERVICES_SVCLEN + 1] ;
 } ;
 
-struct smservices_c {
-	int	i ;
+struct smservices_cursor {
+	int		i ;
 } ;
 
+typedef	SMSERVICES	smservices ;
+typedef	SMSERVICES_CUR	smservices_cur ;
+typedef	SMSERVICES_ENT	smservices_ent ;
 
+EXTERNC_begin
 
-
-#if	(! defined(SMSERVICES_MASTER)) || (SMSERVICES_MASTER == 0)
-
-extern int smservices_open(SMSERVICES *,char *) ;
-extern int smservices_close(SMSERVICES *) ;
-extern int smservices_curbegin(SMSERVICES *,SMSERVICES_CUR *) ;
-extern int smservices_curend(SMSERVICES *,SMSERVICES_CUR *) ;
+extern int smservices_open(SMSERVICES *,char *) noex ;
+extern int smservices_close(SMSERVICES *) noex ;
+extern int smservices_curbegin(SMSERVICES *,SMSERVICES_CUR *) noex ;
+extern int smservices_curend(SMSERVICES *,SMSERVICES_CUR *) noex ;
 extern int smservices_fetch(SMSERVICES *,SMSERVICES_CUR *,
-		SMSERVICES_ENT *) ;
+		SMSERVICES_ENT *) noex ;
 
-#endif /* SMSERVICES_MASTER */
+EXTERNC_end
 
 
 #endif /* SMSERVICES_INCLUDE */
-
 
 
