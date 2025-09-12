a5858e100d869ac8439e289fa817b399a4ce29aa
David A.D. Morano <david.a.morano@gmail.com>
2025-09-12 12:19:59 -0400

    comments + update

--- src/smservices/recsvc.h
+++ src/smservices/recsvc.h
@@ -1,66 +1,58 @@
-/* recsvc */
+/* recsvc HEADER */
 
 
 
 #ifndef	RECSVC_INCLUDE
-#define	RECSVC_INCLUDE	1
+#define	RECSVC_INCLUDE
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-
-#include	<sys/types.h>
-
-#include	"localmisc.h"
-
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
 
 
 /* object defines */
-
 #define	RECSVC			struct recsvc_head
-
+#define	RECSVC_ENT		struct recsvc_entry
 #define	RECSVC_STARTNUM	100	/* starting number records */
-
-
 /* branch types */
-
 #define	RECSVC_BTFWD		1	/* forward */
 #define	RECSVC_BTSSH		2	/* Simple Single-sided Hammock */
 
 
-
-struct recsvc_ent {
+struct recsvc_entry {
 	uint	canon ;		/* cannonical name */
 	uint	name ;		/* service name */
 	uint	svc ;		/* service code */
 } ;
 
 struct recsvc_head {
-	unsigned long		magic ;
-	struct recsvc_ent	*rectab ;
+	RECSVC_ENT	*rectab ;
+	uint		magic ;
 	int	i ;		/* current length */
 	int	e ;		/* current buffer extent */
 	int	c ;		/* count */
 } ;
 
+EXTERNC_begin
 
+extern int	recsvc_init(recsvc *,int) noex ;
+extern int	recsvc_free(recsvc *) noex ;
+extern int	recsvc_add(recsvc *,uint,int,int) noex ;
+extern int	recsvc_already(recsvc *,uint,int,int) noex ;
+extern int	recsvc_gettab(recsvc *,uint **) noex ;
+extern int	recsvc_rtlen(recsvc *) noex ;
+extern int	recsvc_count(recsvc *) noex ;
+extern int	recsvc_countindex(recsvc *) noex ;
+extern int	recsvc_sizeindex(recsvc *) noex ;
+extern int	recsvc_mkindex(recsvc *,uint [][2],int) noex ;
 
-#if	(! defined(RECSVC_MASTER)) || (RECSVC_MASTER == 0)
-
-extern int	recsvc_init(RECSVC *,int) ;
-extern int	recsvc_free(RECSVC *) ;
-extern int	recsvc_add(RECSVC *,uint,int,int) ;
-extern int	recsvc_already(RECSVC *,uint,int,int) ;
-extern int	recsvc_gettab(RECSVC *,uint **) ;
-extern int	recsvc_rtlen(RECSVC *) ;
-extern int	recsvc_count(RECSVC *) ;
-extern int	recsvc_countindex(RECSVC *) ;
-extern int	recsvc_sizeindex(RECSVC *) ;
-extern int	recsvc_mkindex(RECSVC *,uint [][2],int) ;
-
-#endif /* RECSVC_MASTER */
+EXTERNC_end
 
 
 #endif /* RECSVC_INCLUDE */
-
 
 
