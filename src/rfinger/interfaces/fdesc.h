69c4963d7fe7683e4dd79eb0704a07836b65b158
David A.D. Morano <david.a.morano@gmail.com>
2025-09-12 12:22:29 -0400

    comments + update

--- src/rfinger/interfaces/fdesc.h
+++ src/rfinger/interfaces/fdesc.h
@@ -1,33 +1,33 @@
-/* fdesc */
+/* fdesc HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 
 #ifndef	FDESC_INCLUDE
-#define	FDESC_INCLUDE	1
-
-
+#define	FDESC_INCLUDE
 
 
 #define	FDESC		struct fdesc_head
-#define	FDESC_ARGS	struct fdesc_a
+#define	FDESC_A		struct fdesc_args
 
 
-
-
-struct fdesc_a {
-	int	fd ;
-	int	pid ;
-	int	flags ;
+struct fdesc_args {
+	int		fd ;
+	int		pid ;
+	int		flags ;
 } ;
 
 struct fdesc_head {
-	unsigned long	magic ;
-	int	fd ;
-	int	pid ;
-	int	flags ;
+	uint		magic ;
+	int		fd ;
+	int		pid ;
+	int		flags ;
 } ;
+
+typedef	FDESC		fdesc ;
+typedef	FDESC_A		fdesc_a ;
 
 
 #endif /* FDESC_INCLUDE */
-
 
 
