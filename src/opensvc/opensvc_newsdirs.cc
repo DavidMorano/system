785f98bcf8fcacb7c290dd99b383d466ab9697c0
David A.D. Morano <david.a.morano@gmail.com>
2025-09-13 00:36:20 -0400

    comments + update

--- src/opensvc/opensvc_newsdirs.cc
+++ src/opensvc/opensvc_newsdirs.cc
@@ -377,12 +377,12 @@ int		to ;
 
 /* specify blocks as output rather than GigiBytes */
 	                    case 'b':
-	                        sip->f.blocks = TRUE ;
+	                        sip->fl.blocks = TRUE ;
 	                        if (f_optequal) {
 	                            f_optequal = FALSE ;
 	                            if (avl) {
 	                                rs = optbool(avp,avl) ;
-					sip->f.blocks = (rs > 0) ;
+					sip->fl.blocks = (rs > 0) ;
 				    }
 	                        }
 	                        break ;
