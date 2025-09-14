4919a6990d010d1d0d8427a1ee4cf1c26ab13b46
David A.D. Morano <david.a.morano@gmail.com>
2025-09-14 00:21:37 -0400

    comments + update

--- src/dialerusd/usd.h
+++ src/dialerusd/usd.h
@@ -1,14 +1,15 @@
-/* usd */
+/* usd HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 
 #ifndef	USD_INCLUDE
-#define	USD_INCLUDE	1
+#define	USD_INCLUDE
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>
 #include	<logfile.h>
-#include	<localmisc.h>
 
 #include	"sysdialer.h"
 
@@ -23,38 +24,30 @@ struct usd_flags {
 } ;
 
 struct usd_head {
-	uint		magic ;
 	LOGFILE		lh ;
-	USD_FL		f ;
+	USD_FL		fl ;
+	uint		magic ;
 	int		tlen ;
 	int		fd ;
 } ;
 
-
-#if	(! defined(USD_MASTER)) || (USD_MASTER == 0)
-
-#ifdef	__cplusplus
-extern "C" {
-#endif
+EXTERNC_begin
 
 extern int usd_open(USD *,SYSDIALER_ARGS *,
-		const char *,const char *,const char **) ;
-extern int usd_reade(USD *,char *,int,int,int) ;
-extern int usd_recve(USD *,char *,int,int,int,int) ;
-extern int usd_recvfrome(USD *,char *,int,int,void *,int *,int,int) ;
-extern int usd_recvmsge(USD *,struct msghdr *,int,int,int) ;
-extern int usd_write(USD *,const char *,int) ;
-extern int usd_send(USD *,const char *,int,int) ;
-extern int usd_sendto(USD *,const char *,int,int,void *,int) ;
-extern int usd_sendmsg(USD *,struct msghdr *,int) ;
-extern int usd_shutdown(USD *,int) ;
-extern int usd_close(USD *) ;
+		cchar *,cchar *,cchar **) noex ;
+extern int usd_reade(USD *,char *,int,int,int) noex ;
+extern int usd_recve(USD *,char *,int,int,int,int) noex ;
+extern int usd_recvfrome(USD *,char *,int,int,void *,int *,int,int) noex ;
+extern int usd_recvmsge(USD *,MSGHDR *,int,int,int) noex ;
+extern int usd_write(USD *,cchar *,int) noex ;
+extern int usd_send(USD *,cchar *,int,int) noex ;
+extern int usd_sendto(USD *,cchar *,int,int,void *,int) noex ;
+extern int usd_sendmsg(USD *,MSGHDR *,int) noex ;
+extern int usd_shutdown(USD *,int) noex ;
+extern int usd_close(USD *) noex ;
 
-#ifdef	__cplusplus
-}
-#endif
+EXTERNC_end
 
-#endif /* USD_MASTER */
 
 #endif /* USD_INCLUDE */
 
