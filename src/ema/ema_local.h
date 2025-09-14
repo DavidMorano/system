6be142a0ab51141aba4c0ee5eec5e03877b16c29
David A.D. Morano <david.a.morano@gmail.com>
2025-09-14 00:24:10 -0400

    comments + update

--- src/ema/ema_local.h
+++ src/ema/ema_local.h
@@ -1,17 +1,24 @@
-/* ema_local */
+/* ema_local HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 
 #ifndef	EMALOCAL_INCLUDE
-#define	EMALOCAL_INCLUDE	1
+#define	EMALOCAL_INCLUDE
 
 
-#include	<envstandards.h>
-#include	<localmisc.h>			/* additional types */
+#include	<envstandards.h>	/* must be ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
 
 
 #define	CMD_FL		struct cmd_flags
 #define	CMD_AFL		struct cmd_aflags
-#define	CMD_LOCAL	struct cmd_local
+#define	CMD_LOC		struct cmd_local
 
 
 struct cmd_flags {
@@ -26,14 +33,18 @@ struct cmd_aflags {
 	uint		comment:1 ;
 	uint		best:1 ;
 	uint		original:1 ;
-	uint		any:1 ;
+	uint		anyone:1 ;
 } ;
 
 struct cmd_local {
-	CMD_FL		f ;
-	CMD_AFL		af ;
+	CMD_FL		fl ;
+	CMD_AFL		afl ;
 	int		spc ;
 } ;
+
+typedef	CMD_FL		cmd_fl ;
+typedef	CMD_AFL		cmd_afl ;
+typedef	CMD_LOC		cmd_loc ;
 
 
 #endif /* EMALOCAL_INCLUDE */
