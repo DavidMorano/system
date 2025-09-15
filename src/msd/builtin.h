69540a1cd83dfc8801248e843605acbcb74e666d
David A.D. Morano <david.a.morano@gmail.com>
2025-09-15 10:24:38 -0400

    comments + update

--- src/msd/builtin.h
+++ src/msd/builtin.h
@@ -12,10 +12,14 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>
 #include	<time.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
 #include	<vecstr.h>
 #include	<svcfile.h>
 #include	<connection.h>
-#include	<localmisc.h>
 
 #include	"config.h"
 #include	"defs.h"
@@ -24,32 +28,34 @@
 
 
 #define	BUILTIN_MAGIC	0x12345678
-#define	BUILTIN		struct builtin
-#define	BUILTIN_CACHE	struct builtin_cache
+#define	BUILTIN		struct builtin_head
+#define	BUILTIN_CA	struct builtin_cache
 
 
 struct builtin_cache {
 	time_t		boottime ;
 } ;
 
-struct builtin {
-	unsigned long	magic ;
+struct builtin_head {
+	svcfile		*sfp ;
+	proginfo	*pip ;
 	BUILTIN_CACHE	c ;
-	SVCFILE		*sfp ;
-	PROGINFO	*pip ;
+	uint		magic ;
 	uint		providerid ;
 	int		nentries ;
 	int		hostnamelen ;
 } ;
 
+typedef	BUILTIN		builtin ;
+typedef	BUILTIN_CA	builtin_ca ;
+
 EXTERNC_begin
 
-extern int builtin_start(BUILTIN *,PROGINFO *) noex ;
-extern int builtin_finish(BUILTIN *) noex ;
-extern int builtin_match(BUILTIN *,const char *) noex ;
-extern int builtin_enum(BUILTIN *,int,const char **) noex ;
-extern int builtin_execute(BUILTIN *,STANDING *,CLIENTINFO *,int,
-		cchar **) noex ;
+extern int builtin_start(builtin *,proginfo *) noex ;
+extern int builtin_finish(builtin *) noex ;
+extern int builtin_match(builtin *,cchar *) noex ;
+extern int builtin_enum(builtin *,int,cchar **) noex ;
+extern int builtin_execute(builtin *,standing *,clientinfo *,int,cc **) noex ;
 
 EXTERNC_end
 
