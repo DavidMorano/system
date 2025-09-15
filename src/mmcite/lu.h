8fca1d74fd8f83ebedaf25fdb063c568afa5bf4c
David A.D. Morano <david.a.morano@gmail.com>
2025-09-15 10:25:15 -0400

    comments + update

--- src/mmcite/lu.h
+++ src/mmcite/lu.h
@@ -1,47 +1,38 @@
-/* lu */
+/* lu HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* lookup a reference in the databases */
+/* version %I% last-modified %G% */
 
 
 /* revision history:
 
-	= 1998-09-10, David A­D­ Morano
-	This module was changed to serve in the REFERM program.
-
-	= 2005-10-01, David A­D­ Morano
-        This was changed to work in the MMCITE program. The old REFERM program
-        is really obsolete. It used a database lookup strategy to remote
-        databases. The high-level problem is: what to do if the cited BIB entry
-        isn't found? How does a maintainer of the present (local) document know
-        what that BIB entry was? The new strategy (implemented by the MMCITE
-        program) is more like what is done with BibTeX in the TeX (or LaTeX)
-        world. All BIB databases are really expected to be maintained by the
-        document creator -- not some centralized entiry. The older centralized
-        model reflected more the use in the corporate world (where different
-        people create BIB entries) than in the more "modern"
-        personal-responsibility type of world! :-) Anyway, this is the way the
-        gods seem to now want to do things. Deal with it!
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
 
 */
 
-/* Copyright © 1998,2005 David A­D­ Morano.  All rights reserved. */
-
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	LU_INCLUDE
-#define	LU_INCLUDE	1
+#define	LU_INCLUDE
 
 
-#include	<sys/types.h>
-
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
 #include	<bfile.h>
 #include	<hdb.h>
-#include	<localmisc.h>
 
 
 #define	LU		struct lu_head
-#define	LU_HEAD		struct lu_head
 #define	LU_FL		struct lu_flags
-#define	LU_CUR		struct hdb_cur
-#define	LU_CURSOR	struct hdb_cur
-#define	LU_VALUE	struct hdb_value
+#define	LU_CUR		struct hdb_cursor
+#define	LU_VAL		struct hdb_value
 
 
 struct lu_flags {
@@ -49,28 +40,31 @@ struct lu_flags {
 } ;
 
 struct lu_value {
-	LU_FL		f ;
-	bfile		infile, outfile, errfile ;
+	bfile		infile ;
+	bfile		outfile ;
+	bfile		errfile ;
+	LU_FL		fl ;
 } ;
 
 struct lu_head {
-	ulong		magic ;
-	HDB_HEAD	lh ;
+	hdb		lh ;
+	uint		magic ;
 } ;
 
+typedef LU		lu ;
+typedef LU_FL		lu_fl ;
+typedef LU_CUR		lu_cur ;
+typedef LU_VAL		lu_val ;
 
-typedef struct lu_head	lu ;
+EXTERNC_begin
 
+extern int	lu_init(lu *) noex ;
+extern int	lu_free(lu *) noex ;
+extern int	lu_count(lu *) noex ;
+extern int	lu_getkey(lu *,lu_cur *,cchar **) noex ;
+extern int	lu_delcur(lu *,lu_cur *) noex ;
 
-#ifndef	LU_MASTER
-
-extern int	lu_init(LU *) ;
-extern int	lu_free(LU *) ;
-extern int	lu_count(LU *) ;
-extern int	lu_getkey(LU *,LU_CUR *,char **) ;
-extern int	lu_delcur(LU *,LU_CUR *) ;
-
-#endif /* LU_MASTER */
+EXTERNC_end
 
 
 #endif /* LU_INCLUDE */
