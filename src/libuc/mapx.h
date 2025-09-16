62ade842e832b5f2c505ae3feb5cfef9922c00d0
David A.D. Morano <david.a.morano@gmail.com>
2025-09-16 00:23:09 -0400

    restore

---
+++ src/libuc/mapx.h
@@ -0,0 +1,35 @@
+/* mapx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* map containers */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MAPX_INCLUDE
+#define	MAPX_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+#include	<mapstrs.h>
+#include	<mapstrint.h>
+
+
+#endif /* MAPX_INCLUDE */
+
+
