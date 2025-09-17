f936f1569724286839270b2cc404613a0f3637b6
David A.D. Morano <david.a.morano@gmail.com>
2025-09-17 00:23:49 -0400

    comments + update

--- src/ksh/shobj.h
+++ src/ksh/shobj.h
@@ -1,16 +1,23 @@
-/* shobj */
+/* shobj HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 
 #ifndef	SHOBJ_INCLUDE
-#define	SHOBJ_INCLUDE	1
+#define	SHOBJ_INCLUDE
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
 #include	<vecobj.h>
 
 
 #define	SHOBJ		struct shobj_head
-#define	SHOBJ_CALLS	struct shobj_calls
+#define	SHOBJ_CA	struct shobj_calls
 
 
 struct shobj_module {
@@ -30,9 +37,12 @@ struct shobj_ent {
 } ;
 
 struct shobj_head {
-	unsigned long	magic ;
 	vecobj		entries ;
+	uint		magic ;
 } ;
+
+typedef	SHOBJ		shobj ;
+typedef	SHOBJ_CA	shobj_ca ;
 
 
 #endif /* SHOBJ_INCLUDE */
