0de1f9d50c01933e6cf6d0711e2e3638cfecafd1
David A.D. Morano <david.a.morano@gmail.com>
2025-09-18 00:36:18 -0400

    comments + update

--- src/libuc/uclibmem.h
+++ src/libuc/uclibmem.h
@@ -44,17 +44,48 @@ EXTERNC_end
 
 namespace libuc {
     struct libmems {
-	int mallocstrw(cchar *,int,cchar **) noex ;
-	int mallocitem(cvoid *,int,voidpp) noex ;
-	int malloc(int,void *) noex ;
-	int valloc(int,void *) noex ;
-	int calloc(int,int,void *) noex ;
-	int ralloc(void *,int,void *) noex ;
-	int free(void *) noex ;
-	int rsfree(int,void *) noex ;
+	int mallocstrw	(cchar *,int,cchar **) noex ;
+	int mallocitem	(cvoid *,int,voidpp) noex ;
+	int malloc	(int,void *) noex ;
+	int valloc	(int,void *) noex ;
+	int calloc	(int,int,void *) noex ;
+	int ralloc	(void *,int,void *) noex ;
+	int strw	(cchar *,int,cchar **) noex ;
+	int item	(cvoid *,int,voidpp) noex ;
+	int mall	(int,void *) noex ;
+	int vall	(int,void *) noex ;
+	int call	(int,int,void *) noex ;
+	int rall	(void *,int,void *) noex ;
+	int free	(void *) noex ;
+	int rsfree	(int,void *) noex ;
     } ; /* end struct (libmems) */
     extern libmems	libmem ;
 } /* end namespace (libuc) */
+
+inline int lm_strw	(cchar *sp,int sl,cchar **rpp) noex {
+    	return libuc::libmem.strw(sp,sl,rpp) ;
+}
+inline int lm_item	(cvoid *sp,int sl,void **rpp) noex {
+    	return libuc::libmem.item(sp,sl,rpp) ;
+}
+inline int lm_mall	(int sz,void *vp) noex {
+    	return libuc::libmem.mall(sz,vp) ;
+}
+inline int lm_vall	(int sz,void *vp) noex {
+    	return libuc::libmem.vall(sz,vp) ;
+}
+inline int lm_call	(int ne,int esz,void *vp) noex {
+    	return libuc::libmem.call(ne,esz,vp) ;
+}
+inline int lm_rall	(void *cp,int nsz,void *vp) noex {
+    	return libuc::libmem.rall(cp,nsz,vp) ;
+}
+inline int lm_free	(void *vp) noex {
+    	return libuc::libmem.free(vp) ;
+}
+inline int lm_rsfree	(int rs,void *vp) noex {
+    	return libuc::libmem.rsfree(rs,vp) ;
+}
 
 #endif /* __cplusplus */
 
