797a1778119cec149c203c75705a5d8e3f7423fa
David A.D. Morano <david.a.morano@gmail.com>
2025-09-18 00:40:50 -0400

    comments + update

--- src/libuc/queue/piq.cc
+++ src/libuc/queue/piq.cc
@@ -17,6 +17,10 @@
 
 /******************************************************************************
 
+  	Object:
+	piq
+
+	Description:
 	This is a pointer Q. The caller must supply entries with
 	the first two (pointer) compnents of the entry available
 	for pointer use by this object.
@@ -26,9 +30,16 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+#include	<uclibmem.h>
 #include	<ptm.h>
 #include	<pq.h>
+#include	<localmisc.h>
 
 #include	"piq.h"
 
@@ -38,7 +49,7 @@
 
 /* imported namespaces */
 
-using std::nullptr_t ;			/* type */
+using libuc::libmem ;			/* variable */
 using std::nothrow ;			/* constant */
 
 
@@ -58,12 +69,12 @@ using std::nothrow ;			/* constant */
 
 template<typename ... Args>
 static inline int piq_ctor(piq *op,Args ... args) noex {
+	cnullptr	np{} ;
 	int		rs = SR_FAULT ;
 	if (op && (args && ...)) {
-	    const nullptr_t	np{} ;
 	    rs = SR_NOMEM ;
 	    if ((op->mxp = new(nothrow) ptm) != np) {
-	        if ((op->freep = new(nothrow) pq) != np) {
+	        if ((op->qlp = new(nothrow) pq) != np) {
 		    rs = SR_OK ;
 	        } /* end if (new-pq) */
 		if (rs < 0) {
@@ -73,16 +84,15 @@ static inline int piq_ctor(piq *op,Args ... args) noex {
 	    } /* end if (new-ptm) */
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (piq_ctor) */
+} /* end subroutine (piq_ctor) */
 
 static inline int piq_dtor(piq *op) noex {
 	int		rs = SR_FAULT ;
 	if (op) {
 	    rs = SR_OK ;
-	    if (op->freep) {
-		delete op->freep ;
-		op->freep = nullptr ;
+	    if (op->qlp) {
+		delete op->qlp ;
+		op->qlp = nullptr ;
 	    }
 	    if (op->mxp) {
 		delete op->mxp ;
@@ -90,8 +100,7 @@ static inline int piq_dtor(piq *op) noex {
 	    }
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (piq_dtor) */
+} /* end subroutine (piq_dtor) */
 
 template<typename ... Args>
 static inline int piq_magic(piq *op,Args ... args) noex {
@@ -100,9 +109,10 @@ static inline int piq_magic(piq *op,Args ... args) noex {
 	    rs = (op->magic == PIQ_MAGIC) ? SR_OK : SR_NOTOPEN ;
 	}
 	return rs ;
-}
+} /* end subroutine (piq_magic) */
 
-static int	pq_finishup(pq *) noex ;
+
+/* local variables */
 
 
 /* exported variables */
@@ -114,7 +124,7 @@ int piq_start(piq *op) noex {
 	int		rs = SR_FAULT ;
 	if ((rs = piq_ctor(op)) >= 0) {
 	    if ((rs = ptm_create(op->mxp,nullptr)) >= 0) {
-	        if ((rs = pq_start(op->freep)) >= 0) {
+	        if ((rs = pq_start(op->qlp)) >= 0) {
 		    op->magic = PIQ_MAGIC ;
 	        }
 	        if (rs < 0)
@@ -133,7 +143,7 @@ int piq_finish(piq *op) noex {
 	int		rs1 ;
 	if ((rs = piq_magic(op)) >= 0) {
 	    {
-	        rs1 = pq_finishup(op->freep) ;
+	        rs1 = pq_finish(op->qlp) ;
 	        if (rs >= 0) rs = rs1 ;
 	    }
 	    {
@@ -158,7 +168,7 @@ int piq_ins(piq *op,void *vp) noex {
 	        if ((rs = ptm_lock(op->mxp)) >= 0) {
 	            {
 		        pq_ent	*pep = (pq_ent *) vp ;
-	                rs = pq_ins(op->freep,pep) ;
+	                rs = pq_ins(op->qlp,pep) ;
 	                c = rs ;
 	            }
 	            rs1 = ptm_unlock(op->mxp) ;
@@ -174,15 +184,14 @@ int piq_rem(piq *op,void *vrp) noex {
 	int		rs1 ;
 	int		c = 0 ;
 	if ((rs = piq_magic(op)) >= 0) {
-	    pq_ent	*pep{} ;
 	    void	**vpp = (void **) vrp ;
 	    if ((rs = ptm_lock(op->mxp)) >= 0) {
-	            if ((rs = pq_remtail(op->freep,&pep)) >= 0) {
-	                c = rs ;
-	                if (vpp) *vpp = pep ;
-	            }
-	            rs1 = ptm_unlock(op->mxp) ;
-		    if (rs >= 0) rs = rs1 ;
+	        if (pq_ent *pep{} ; (rs = pq_remtail(op->qlp,&pep)) >= 0) {
+	            c = rs ;
+	            if (vpp) *vpp = pep ;
+	        }
+	        rs1 = ptm_unlock(op->mxp) ;
+		if (rs >= 0) rs = rs1 ;
 	    } /* end if (mutex) */
 	} /* end if (magic) */
 	return (rs >= 0) ? c : rs ;
@@ -196,7 +205,7 @@ int piq_count(piq *op) noex {
 	if ((rs = piq_magic(op)) >= 0) {
 	        if ((rs = ptm_lock(op->mxp)) >= 0) {
 	            {
-	                rs = pq_count(op->freep) ;
+	                rs = pq_count(op->qlp) ;
 	                c = rs ;
 	            }
 	            rs1 = ptm_unlock(op->mxp) ;
@@ -214,7 +223,7 @@ int piq_audit(piq *op) noex {
 	if ((rs = piq_magic(op)) >= 0) {
 	        if ((rs = ptm_lock(op->mxp)) >= 0) {
 	            {
-	                rs = pq_audit(op->freep) ;
+	                rs = pq_audit(op->qlp) ;
 		        c = rs ;
     	            }
 	            rs1 = ptm_unlock(op->mxp) ;
@@ -224,27 +233,5 @@ int piq_audit(piq *op) noex {
 	return (rs >= 0) ? c : rs ;
 }
 /* end subroutine (piq_audit) */
-
-
-/* private subroutines */
-
-static int pq_finishup(pq *qp) noex {
-	int		rs = SR_OK ;
-	int		rs1 ;
-	{
-	    pq_ent	*pep{} ;
-	    while ((rs1 = pq_rem(qp,&pep)) >= 0) {
-	        rs1 = uc_free(pep) ;
-	        if (rs >= 0) rs = rs1 ;
-	    }
-	    if ((rs >= 0) && (rs1 != SR_EMPTY)) rs = rs1 ;
-	}
-	{
-	    rs1 = pq_finish(qp) ;
-	    if (rs >= 0) rs = rs1 ;
-	}
-	return rs ;
-}
-/* end subroutine (pq_finishup) */
 
 
