273c34e906e48ea0d592f946a0a3d219d6c5fb2b
David A.D. Morano <david.a.morano@gmail.com>
2025-09-19 00:22:25 -0400

    comments + update

--- src/libdam/calmgr.cc
+++ src/libdam/calmgr.cc
@@ -38,7 +38,6 @@
 #include	<cstring>		/* |lenstr(3c)| */
 #include	<algorithm>		/* |min(3c++)| + |max(3c++)| */
 #include	<usystem.h>
-#include	<ucmallreg.h>
 #include	<getbufsize.h>
 #include	<getusername.h>
 #include	<mallocxx.h>
