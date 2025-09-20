07a1e09a717889bb08b6a4c1a61e8ee89f203d29
David A.D. Morano <david.a.morano@gmail.com>
2025-09-20 00:17:00 -0400

    comments + update

--- src/libdam/percache.h
+++ src/libdam/percache.h
@@ -43,12 +43,12 @@ enum pertypes {
 	pertype_netload,
 	pertype_systat,
 	pertype_overlast
-} ;
+} ; /* end enum (pertypes) */
 
 struct percache_dataitem {
     	char		*data ;
-	uint		t ;
-	uint		v ;
+	uint		t ;		/* time (of acquisition) */
+	uint		v ;		/* value (acquired) */
 } ;
 
 struct percache_head {
@@ -57,27 +57,27 @@ struct percache_head {
 	volatile uint	f_init ;
 	volatile uint	f_initdone ;
 	volatile uint	f_finireg ;
-} ;
+} ; /* end struct (percache_head) */
 
 typedef PERCACHE	percache ;
 typedef	PERCACHE_ITEM	percache_item ;
 
 EXTERNC_begin
 
-extern int	percache_init(percache *) noex ;
-extern int	percache_finireg(percache *) noex ;
-extern int	percache_invalidate(percache *) noex ;
-extern int	percache_gethostid(percache *,time_t,uint *) noex ;
-extern int	percache_getbtime(percache *,time_t,time_t *) noex ;
-extern int	percache_getnprocs(percache *,time_t) noex ;
-extern int	percache_getrunlevel(percache *,time_t) noex ;
-extern int	percache_getnusers(percache *,time_t) noex ;
-extern int	percache_getnodename(percache *,time_t,cchar **) noex ;
-extern int	percache_getnetdomain(percache *,time_t,cchar **) noex ;
-extern int	percache_getnisdomain(percache *,time_t,cchar **) noex ;
-extern int	percache_getnetload(percache *,time_t,cchar *,cchar **) noex ;
-extern int	percache_getsystat(percache *,time_t,cchar *,cchar **) noex ;
-extern int	percache_fini(percache *) noex ;
+extern int percache_init	(percache *) noex ;
+extern int percache_finireg	(percache *) noex ;
+extern int percache_invalidate	(percache *) noex ;
+extern int percache_gethostid	(percache *,time_t,uint *) noex ;
+extern int percache_getbtime	(percache *,time_t,time_t *) noex ;
+extern int percache_getnprocs	(percache *,time_t) noex ;
+extern int percache_getrunlevel	(percache *,time_t) noex ;
+extern int percache_getnusers	(percache *,time_t) noex ;
+extern int percache_getnodename	(percache *,time_t,cchar **) noex ;
+extern int percache_getnetdomain(percache *,time_t,cchar **) noex ;
+extern int percache_getnisdomain(percache *,time_t,cchar **) noex ;
+extern int percache_getnetload	(percache *,time_t,cchar *,cchar **) noex ;
+extern int percache_getsystat	(percache *,time_t,cchar *,cchar **) noex ;
+extern int percache_fini	(percache *) noex ;
 
 EXTERNC_end
 
