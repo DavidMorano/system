456f9030f35fec5fe644fc56bc2f7f0196aab439
David A.D. Morano <david.a.morano@gmail.com>
2025-09-21 00:50:28 -0400

    comments + update

--- src/libuc/buffer/buffer_strquote.cc
+++ src/libuc/buffer/buffer_strquote.cc
@@ -42,8 +42,13 @@
 #include	<envstandards.h>	/* MUST be ordered first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<cstring>		/* |strlen(3c)| */
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+#include	<uclibmem.h>
 #include	<ascii.h>
 #include	<strn.h>
 #include	<strwcpy.h>
@@ -51,9 +56,19 @@
 
 #include	"buffer.h"
 
-import libutil ;
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |getlenstr(3u) */
 
 /* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+using libuc::libmem ;			/* variable */
 
 
 /* external subroutines */
@@ -80,44 +95,45 @@ extern "C" {
 
 /* exported subroutines */
 
-int buffer_strquote(buffer *op,cchar *sp,int sl) noex {
+int buffer_strquote(buffer *op,cchar *sp,int µsl) noex {
+    	cnullptr	np{} ;
 	cint		qch = CH_DQUOTE ;
-	int		rs ;
+	int		rs = SR_FAULT ;
 	int		rs1 ;
-	int		len = 0 ;
-	if (sl < 0) sl = lenstr(sp) ;
-	if (strnbrk(sp,sl," \t\r\n\v\f\b\"\\") != nullptr) {
-	    cint	sz = ((2 * sl) + 3) ;
-	    if (char *ap ; (rs = uc_malloc(sz,&ap)) >= 0) {
-	        cchar	*tp ;
-		char	*bp = ap ;
-		{
-		    *bp++ = qch ;
-		    while ((tp = strnbrk(sp,sl,"\"\\")) != nullptr) {
-			cint	tl = intconv(tp - sp) ;
-		        bp = strwcpy(bp,sp,tl) ;
-		        *bp++ = CH_BSLASH ;
-		        *bp++ = *tp ;
-		        sl -= intconv((tp + 1) - sp) ;
-		        sp = (tp+1) ;
-		    } /* end while */
-		    if (sl > 0) {
-		        bp = strwcpy(bp,sp,sl) ;
-		    }
-		    *bp++ = qch ;
+	int		len = 0 ; /* return-value */
+	if (int sl ; (sl = getlenstr(sp,µsl)) >= 0) {
+	    if (strnbrk(sp,sl," \t\r\n\v\f\b\"\\") != np) {
+	        cint	sz = ((2 * sl) + 3) ;
+	        if (char *ap ; (rs = libmem.mall(sz,&ap)) >= 0) {
+		    char	*bp = ap ;
 		    {
-		        cint	tl = intconv(bp - ap) ;
-		        rs = buffer_strw(op,ap,tl) ;
-		    }
-		    len = rs ;
-		} /* end block */
-		rs1 = uc_free(ap) ;
-		if (rs >= 0) rs = rs1 ;
-	    } /* end if (m-a-f) */
-	} else {
-	    rs = buffer_strw(op,sp,sl) ;
-	    len = rs ;
-	}
+		        *bp++ = qch ;
+		        for (cc *tp ; (tp = strnbrk(sp,sl,"\"\\")) != np ; ) {
+			    cint	tl = intconv(tp - sp) ;
+		            bp = strwcpy(bp,sp,tl) ;
+		            *bp++ = CH_BSLASH ;
+		            *bp++ = *tp ;
+		            sl -= intconv((tp + 1) - sp) ;
+		            sp = (tp+1) ;
+		        } /* end while */
+		        if (sl > 0) {
+		            bp = strwcpy(bp,sp,sl) ;
+		        }
+		        *bp++ = qch ;
+		        {
+		            cint	tl = intconv(bp - ap) ;
+		            rs = buffer_strw(op,ap,tl) ;
+		        }
+		        len = rs ;
+		    } /* end block */
+		    rs1 = libmem.free(ap) ;
+		    if (rs >= 0) rs = rs1 ;
+	        } /* end if (m-a-f) */
+	    } else {
+	        rs = buffer_strw(op,sp,sl) ;
+	        len = rs ;
+	    }
+	} /* end if (getlenstr) */
 	return (rs >= 0) ? len : rs ;
 }
 /* end subroutine (buffer_strquote) */
