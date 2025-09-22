a24b1290c3e3a22c6339d6d1581568b25f8c2653
David A.D. Morano <david.a.morano@gmail.com>
2025-09-22 00:22:19 -0400

    comments + update

--- src/libf/fprint.cc
+++ src/libf/fprint.cc
@@ -1,17 +1,15 @@
-/* fprint */
+/* fprint SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* print some (binary) data out to a STDIO stream */
-
-
-#define	CF_DEBUGS	0		/* compile-time debug print-outs */
+/* version %I% last-modified %G% */
 
 
 /* revision history:
 
 	= 1998-08-17, David A­D­ Morano
-
 	This code was originally written.
-
 
 */
 
@@ -19,19 +17,32 @@
 
 /******************************************************************************
 
+  	Name:
+	fprint
+
+	Description:
 	This is a knock-off of the 'fwrite(3)' subroutine, only
 	sensible!
 
-
 ******************************************************************************/
 
-
 #include	<envstandards.h>
-
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
 #include	<cstdio>
 #include	<cstring>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+#include	<intsat.h>
+#include	<localmisc.h>
 
-#include	<usystem.h>
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |getlenstr(3u)| */
 
 
 /* local defines */
@@ -39,39 +50,41 @@
 
 /* external subroutines */
 
-extern int	fbwrite(FILE *,const void *,int) ;
+extern "C++" {
+    int fwrite(FILE *,cvoid *,int) noex ;
+}
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
 
 
 /* exported subroutines */
 
-
-int fprint(fp,lbuf,llen)
-FILE		*fp ;
-const char	lbuf[] ;
-int		llen ;
-{
-	int	rs = SR_OK ;
-	int	wlen = 0 ;
-
-
-	if (lbuf == NULL)
-	    return SR_FAULT ;
-
-	if (llen < 0) llen = lenstr(lbuf) ;
-
-	if (llen > 0) {
-	    rs = fbwrite(fp,lbuf,llen) ;
-	    wlen += rs ;
-	}
-
-	if ((rs >= 0) && ((llen == 0) || (lbuf[llen-1] != '\n'))) {
-	    rs = fbwrite(fp,"\n",1) ;
-	    wlen += rs ;
-	}
-
+int fprint(FILE *fp,cchar *lbuf,int µllen) noex {
+	int		rs = SR_OK ;
+	int		wlen = 0 ; /* return-value */
+	if(fp && lbuf) {
+	    if (int llen ; (llen = getlenstr(lbuf,µllen)) >= 0) {
+	        if (llen > 0) {
+	            rs = fwrite(fp,lbuf,llen) ;
+	            wlen += rs ;
+		}
+	        if ((rs >= 0) && ((llen == 0) || (lbuf[llen-1] != '\n'))) {
+	            rs = fwrite(fp,"\n",1) ;
+	            wlen += rs ;
+	        }
+	    } /* end if (valid) */
+	} /* end if (non-null) */
 	return (rs >= 0) ? wlen : rs ;
-}
-/* end subroutine (fprint) */
-
+} /* end subroutine (fprint) */
 
 
