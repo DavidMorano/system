58b48d28e932910ff770440da7d6b053216add72
David A.D. Morano <david.a.morano@gmail.com>
2025-09-22 00:21:38 -0400

    restore

---
+++ src/libdam/mathx/mathdam.h
@@ -0,0 +1,35 @@
+/* mathdam HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* calculate (integer) factorial */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-09-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MATHDAM_INCLUDE
+#define	MATHDAM_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+#include	<binexp.h>
+#include	<factorial.h>
+#include	<fibonacci.h>
+
+
+#endif /* MATHDAM_INCLUDE */
+
+
