ee1e686c056793376cfa18f922922beab1ef7ba3
David A.D. Morano <david.a.morano@gmail.com>
2025-09-23 01:10:09 -0400

    comments + update

--- src/libu/libutil.h
+++ src/libu/libutil.h
@@ -2,13 +2,13 @@
 /* charset=ISO8859-1 */
 /* lang=C20 */
 
-/* LIBU miscellaneous interfaces */
+/* UNIX® kernel support subroutines */
 /* version %I% last-modified %G% */
 
 
 /* revision history:
 
-	= 1998-04-10, David A­D­ Morano
+	= 1998-11-01, David A­D­ Morano
 	This subroutine was written for Rightcore Network Services.
 
 */
@@ -19,7 +19,7 @@
 #define	LIBUTIL_INCLUDE
 
 
-#include	<envstandards.h>	/* MUST be first to configure */
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
