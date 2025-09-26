c2f238a03cf191c0b6b61a6e56b85ff48d8cadb9
David A.D. Morano <david.a.morano@gmail.com>
2025-09-26 00:31:01 -0400

    comments + update

--- src/liblkcmd/babycalcs.h
+++ src/liblkcmd/babycalcs.h
@@ -1,44 +1,49 @@
-/* babycalcs */
+/* babycalcs HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* baby calculator */
+/* version %I% last-modified %G% */
 
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	BABYCALCS_INCLUDE
-#define	BABYCALCS_INCLUDE	1
+#define	BABYCALCS_INCLUDE
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-
-#include	<sys/types.h>
-#include	<sys/param.h>
-
+#include	<sys/types.h>		/* |time_t| */
 #include	<ptm.h>
-#include	<localmisc.h>
-
-#include	"babiesfu.h"
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<babieshdr.h>
 
 
 #define	BABYCALCS_DBNAME	"babies"
 #define	BABYCALCS_MAGIC		0x43628199
 #define	BABYCALCS		struct babycalcs_head
-#define	BABYCALCS_OBJ		struct babycalcs_obj
-#define	BABYCALCS_INFO		struct babycalcs_i
 #define	BABYCALCS_FL		struct babycalcs_flags
-#define	BABYCALCS_ENT		struct babycalcs_e
+#define	BABYCALCS_OBJ		struct babycalcs_object
+#define	BABYCALCS_INFO		struct babycalcs_information
+#define	BABYCALCS_ENT		struct babycalcs_entry
 
 
-struct babycalcs_obj {
-	const char	*name ;
+struct babycalcs_object {
+	cchar		*name ;
 	uint		objsize ;
 } ;
 
-struct babycalcs_i {
+struct babycalcs_information {
 	time_t		wtime ;
 	time_t		atime ;
 	uint		acount ;
 } ;
 
-struct babycalcs_e {
+struct babycalcs_entry {
 	time_t		date ;
 	uint		count ;
 } ;
@@ -51,43 +56,41 @@ struct babycalcs_flags {
 } ;
 
 struct babycalcs_head {
-	uint		magic ;
-	BABYCALCS_FL	f ;
 	BABYCALCS_ENT	*table ;
-	const char	*pr ;
-	const char	*dbfname ;
-	const char	*shmname ;
+	cchar		*pr ;
+	cchar		*dbfname ;
+	cchar		*shmname ;
 	caddr_t		mapdata ;	/* SHM data */
-	PTM		*mp ;		/* pointer to SHM mutex */
-	BABIESFU	hf ;
+	ptm		*mxp ;		/* pointer to SHM mutex */
+	BABIESHDR	hf ;
 	time_t		ti_mdb ;	/* db-mtime */
 	time_t		ti_map ;	/* map-time */
 	time_t		ti_lastcheck ;
 	size_t		mapsize ;	/* SHM map-size */
+	BABYCALCS_FL	fl ;
+	uint		magic ;
 	int		pagesize ;
 	int		dbsize ;
 	int		shmsize ;
 	int		nentries ;	
 } ;
 
+typedef	BABYCALCS		babycalcs ;
+typedef	BABYCALCS_FL		babycalcs_fl ;
+typedef	BABYCALCS_OBJ		babycalcs_obj ;
+typedef	BABYCALCS_INFO		babycalcs_info ;
+typedef	BABYCALCS_ENT		babycalcs_ent ;
 
-#if	(! defined(BABYCALCS_MASTER)) || (BABYCALCS_MASTER == 0)
+EXTERNC_begin
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
+extern int	babycalcs_open(babycalcs *,cchar *,cchar *) noex ;
+extern int	babycalcs_check(babycalcs *,time_t) noex ;
+extern int	babycalcs_lookup(babycalcs *,time_t,uint *) noex ;
+extern int	babycalcs_getinfo(babycalcs *,babycalcs_info *) noex ;
+extern int	babycalcs_close(babycalcs *) noex ;
 
-extern int	babycalcs_open(BABYCALCS *,cchar *,cchar *) ;
-extern int	babycalcs_check(BABYCALCS *,time_t) ;
-extern int	babycalcs_lookup(BABYCALCS *,time_t,uint *) ;
-extern int	babycalcs_info(BABYCALCS *,BABYCALCS_INFO *) ;
-extern int	babycalcs_close(BABYCALCS *) ;
+EXTERNC_end
 
-#ifdef	__cplusplus
-}
-#endif
-
-#endif /* BABYCALCS_MASTER */
 
 #endif /* BABYCALCS_INCLUDE */
 
