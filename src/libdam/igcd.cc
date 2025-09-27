1606035eba37e64277a0177cb6e3e901715e56c8
David A.D. Morano <david.a.morano@gmail.com>
2025-09-27 00:18:07 -0400

    github-garbage

--- src/libdam/igcd.cc
+++ src/libdam/igcd.cc
@@ -1,45 +1,46 @@
-/* igcd */
+/* igcd SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* integer Greatest Common Divisor (GCD) */
 /* version %I% last-modified %G% */
 
-
 #define	CF_DEBUGS	0		/* non-switchable debug print-outs */
-
 
 /* revision history:
 
-	= 2004-03-01, David AÂ­DÂ­ Morano
+	= 2004-03-01, David A­D­ Morano
 	This code was originally written.  
 
 */
 
-/* Copyright Â© 2004 David AÂ­DÂ­ Morano.  All rights reserved. */
+/* Copyright © 2004 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
+  	Name:
+	igcd
+
+	Description:
         This subroutine calculates the Greastest Common Divisor (GCD) of
         two integer numbers.
 
 	Synopsis:
-
-	int igcd(int a,int b)
+	int igcd(int a,int b) noex
 
 	Arguments:
-
 	a	number
 	b	number
 
 	Returns:
-
 	-	GCD(a,b)
-
 
 *******************************************************************************/
 
-
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
 #include	<localmisc.h>		/* more types */
 
 
@@ -61,13 +62,13 @@
 /* local variables */
 
 
+/* exported variables */
+
+
 /* exported subroutines */
 
-
-int igcd(int a,int b)
-{
-	int		r ;
-	while ((r = (a % b)) > 0) {
+int igcd(int a,int b) noex {
+	for (int r ; (r = (a % b)) > 0 ; ) {
 	    a = b ;
 	    b = r ;
 	} /* end while */
