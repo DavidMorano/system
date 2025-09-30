cdf1ac257c8f54c0d733cb1288aa4b7ee2ce0c23
David A.D. Morano <david.a.morano@gmail.com>
2025-09-30 00:21:11 -0400

    comments + update

--- src/libuc/uc_sockjoin.cc
+++ src/libuc/uc_sockjoin.cc
@@ -17,6 +17,10 @@
 
 /*******************************************************************************
 
+  	Name:
+	uc_sockjoin
+
+  	Description:
 	This subroutine joins a UNIX® socket of the datagram variety.
 
 *******************************************************************************/
@@ -24,6 +28,8 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>
 #include	<sys/param.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
 #include	<cstring>
 #include	<usystem.h>
 #include	<sockaddress.h>
@@ -35,13 +41,8 @@
 
 /* external subroutines */
 
-extern int	mkpath1(char *,const char *,const char *) ;
-extern int	mkpath2(char *,const char *,const char *) ;
-extern int	opentmpusd(const char *,int,mode_t,char *) ;
 
-extern char	*strwcpy(char *,const char *,int) ;
-extern char	*strdcpy1(char *,int,const char *) ;
-extern char	*strdcpy1w(char *,int,const char *,int) ;
+/* external variables */
 
 
 /* local structures */
@@ -53,29 +54,34 @@ extern char	*strdcpy1w(char *,int,const char *,int) ;
 /* local variables */
 
 
+/* exported variables */
+
+
 /* exported subroutines */
 
 int uc_sockjoin(int fd,SOCKADDR *sap,int sal,int of,mode_t om) noex {
-	int		rs = SR_OK ;
+	int		rs = SR_BADF ;
 	cint		f_create = ((of&O_CREAT)?1:0) ;
-	if (fd < 0) return SR_INVALID ;
-	if (f_create) {
-	    if ((rs = u_bind(fd,sap,sal)) >= 0) {
-		SOCKADDRESS	*sa = (SOCKADDRESS *) sap ;
-		const int	af = AF_UNIX ;
-		if ((rs = sockaddress_getaf(sa)) == af) {
-		    const int	slen = MAXPATHLEN ;
-		    char	sbuf[MAXPATHLEN+1] ;
-		    if ((rs = sockaddress_getaddr(sa,sbuf,slen)) >= 0) {
-		        rs = u_chmod(sbuf,om) ;
-		        if (rs < 0)
-			    uc_unlink(sbuf) ;
-		    } /* end if (sockaddress_getaddr) */
-		} /* end if (UNIX® address family) */
-	    } /* end if (u_bind) */
-	} else {
-	    rs = u_connect(fd,sap,sal) ;
-	}
+	if (fd >= 0) {
+	    if (f_create) {
+	        if ((rs = u_bind(fd,sap,sal)) >= 0) {
+		    SOCKADDRESS	*sa = (SOCKADDRESS *) sap ;
+		    cint	af = AF_UNIX ;
+		    if ((rs = sockaddress_getaf(sa)) == af) {
+		        cint	slen = MAXPATHLEN ;
+		        char	sbuf[MAXPATHLEN+1] ;
+		        if ((rs = sockaddress_getaddr(sa,sbuf,slen)) >= 0) {
+		            rs = u_chmod(sbuf,om) ;
+		            if (rs < 0) {
+			        uc_unlink(sbuf) ;
+			    }
+		        } /* end if (sockaddress_getaddr) */
+		    } /* end if (UNIX® address family) */
+	        } /* end if (u_bind) */
+	    } else {
+	        rs = u_connect(fd,sap,sal) ;
+	    }
+	} /* end if (valid) */
 	return rs ;
 }
 /* end subroutine (uc_sockjoin) */
