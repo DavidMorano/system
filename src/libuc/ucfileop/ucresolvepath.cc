594928d1e0f171ee2e1c26f096072b64d8e87383
David A.D. Morano <david.a.morano@gmail.com>
2025-09-30 00:21:06 -0400

    comments + update

--- src/libuc/ucfileop/ucresolvepath.cc
+++ src/libuc/ucfileop/ucresolvepath.cc
@@ -2,19 +2,19 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
-/* interface component for UNIX®Â® library-3c */
+/* interface component for UNIX® library-3c */
 /* resolve a path without symbolic components */
 /* version %I% last-modified %G% */
 
 
 /* revision history:
 
-	= 1998-04-13, David AÂ­DÂ­ Morano
+	= 1998-04-13, David A­D­ Morano
 	Originally written for Rightcore Network Services.
 
 */
 
-/* Copyright Â© 1998 David AÂ­DÂ­ Morano.  All rights reserved. */
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
