3bf7214c913f865956e64494e3431e472d46c22f
David A.D. Morano <david.a.morano@gmail.com>
2025-09-30 00:21:42 -0400

    comments + update

--- src/libuc/fdt.h
+++ src/libuc/fdt.h
@@ -2,6 +2,19 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 (conformance reviewed) */
 
+/* File-Descriptor-Table */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services
+	(RNS).
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	FDT_INCLUDE
 #define	FDT_INCLUDE
