49bf28133bc66256b986d05c0758d5b429fb363d
David A.D. Morano <david.a.morano@gmail.com>
2025-10-03 00:12:33 -0400

    restore

---
+++ src/libuc/six/sileadchr.h
@@ -0,0 +1,39 @@
+/* sileadchr HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* find index to first gicen character after skipping white-space */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-23, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SILEADCHR_INCLUDE
+#define	SILEADCHR_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+EXTERNC_begin
+
+extern int	sileadchr(cchar *,int,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* SILEADCHR_INCLUDE */
+
+
