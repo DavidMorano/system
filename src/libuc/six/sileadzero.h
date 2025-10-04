0878fa2b3cb6193af41ef4318908a47cf2fd134f
David A.D. Morano <david.a.morano@gmail.com>
2025-10-04 00:53:09 -0400

    restore

---
+++ src/libuc/six/sileadzero.h
@@ -0,0 +1,39 @@
+/* sileadzero HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* find index to first gicen character after skipping white-space */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-23, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SILEADZERO_INCLUDE
+#define	SILEADZERO_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+EXTERNC_begin
+
+extern int	sileadzero(cchar *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* SILEADZERO_INCLUDE */
+
+
