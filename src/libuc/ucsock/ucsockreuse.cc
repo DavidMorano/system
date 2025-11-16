3cef583682fd676b8e21a6f5907fe23282021dd4
David A.D. Morano <david.a.morano@gmail.com>
2025-11-16 00:26:03 -0500

    restore

---
+++ src/libuc/ucsock/ucsockreuse.cc
@@ -0,0 +1,63 @@
+/* ucsockreuse SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* interface component for UNIX® library-3c */
+/* reuse an existing (perhaps) socket address */
+
+
+/* revision history:
+
+	= 1998-04-13, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#include	<envstandards.h>
+#include	<sys/socket.h>
+#include	<uclibsubs.h>
+#include	<localmisc.h>
+
+#include	"ucsock.h"
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int uc_sockreuse(int fd) noex {
+	cint		optlen = szof(int) ;
+	cint		sol = SOL_SOCKET ;
+	cint		so = SO_REUSEADDR ;
+	int		one = 1 ; /* <- must be mutable */
+	return u_setsockopt(fd,sol,so,&one,optlen) ;
+}
+/* end subroutine (uc_sockreuse) */
+
+
