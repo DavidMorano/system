a5b4f1f92a954041a97da057980d6be0bc2604b2
David A.D. Morano <david.a.morano@gmail.com>
2025-11-17 00:11:50 -0500

    restore

---
+++ src/libdam/mkui/mkbangname.cc
@@ -0,0 +1,78 @@
+/* mkbangname SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+#define	CF_FULLNAME	0		/* use full-name */
+#define	CF_MAILNAME	1		/* use mail-name */
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	mkbangname
+
+	Description:
+	This subroutine create a name that combines the nodename
+	and the username into a "bangname."
+
+	Synopsis:
+	int mkbangname(char *nbuf,int nlen,userinfo *uip) noex
+
+	Arguments:
+	nbuf		buffer to receive resulting name
+	nlen		length of supplied buffer
+	uip		pointer to USERINFO object
+
+	Returns:
+	>=0		OK
+	<0		error (system-return)
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<usystem.h>
+#include	<userinfo.h>
+#include	<localmisc.h>
+#include	<mkui.h>		/* |mkuibang(3dam)| */
+
+#include	"mkbangname.h"
+
+/* local defines */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int mkbangname(char *rbuf,int rlen,userinfo *uip) noex {
+    	return mkuibang(rbuf,rlen,uip) ;
+}
+/* end subroutine (mkbangname) */
+
+
