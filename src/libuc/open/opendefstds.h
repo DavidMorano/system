96d96215245df7c613134905190fea4cc20da9a6
David A.D. Morano <david.a.morano@gmail.com>
2025-11-22 00:02:15 -0500

    restore

---
+++ src/libuc/open/opendefstds.h
@@ -0,0 +1,40 @@
+/* openddefstads HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* open default standard files */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services
+	(RNS).
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	OPENDEFSTDS_INCLUDE
+#define	OPENDEFSTDS_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+EXTERNC_begin
+
+extern int opendial(int,int,cc *,cc *,cc *,mainv,mainv,int,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* OPENDEFSTDS_INCLUDE */
+
+
