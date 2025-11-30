3aab421d29a9b95ed59a29cb3078181de5bd6b9a
David A.D. Morano <david.a.morano@gmail.com>
2025-11-30 00:49:20 -0500

    rename

---
+++ src/libuc/ncolx.h
@@ -0,0 +1,36 @@
+/* ncolx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* calculate column values */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	NCOLX_INCLUDE
+#define	NCOLX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+#include	<ncol.h>
+#include	<ncolline.h>
+#include	<tabcols.h>
+
+
+#endif /* NCOLX_INCLUDE */
+
+
