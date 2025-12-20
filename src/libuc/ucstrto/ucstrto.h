22ae4166c07d5c6f8a035c62bcdba49ce5d28049
David A.D. Morano <david.a.morano@gmail.com>
2025-12-20 00:51:55 -0500

    restore

---
+++ src/libuc/ucstrto/ucstrto.h
@@ -0,0 +1,35 @@
+/* ucstrto HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* string-to-value conversion operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCSTRTO_INCLUDE
+#define	UCSTRTO_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+#include	<ucstrtox.h>
+#include	<ucstrtof.h>
+
+
+#endif /* UCSTRTO_INCLUDE */
+
+
