26a0357bb41c5916fcc5de3b25b4119315249580
David A.D. Morano <david.a.morano@gmail.com>
2025-12-25 00:54:23 -0500

    restore

---
+++ src/libuc/hasx/hasprint.cc
@@ -0,0 +1,124 @@
+/* hasprint SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* has a counted c-string some characteristic? */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-10-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Group:
+	hasprint{x}
+
+	Namess:
+	hasprintlatin
+	hasprintbad
+
+	Description:
+	These subroutines check if a specified c-string has any of
+	some characteristic we are looking for.
+
+	Synopsis:
+	bool hasprintlatin(cchar *sp,int sl) noex
+	bool hasprintbad(cchar *sp,int sl) noex ;
+
+	Arguments:
+	sp		test c-string pointer
+	sl		test c-string length
+
+	Returns:
+	true		true (empty)
+	false		false (not empty)
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<mkchar.h>
+#include	<ischarx.h>
+#include	<localmisc.h>		/* |UC(3dam)| */
+
+#include	"hasprint.h"
+
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |getlenstr(3u)| + |lenstr(3u)| */
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+bool hasprintlatin(cchar *sp,int sl) noex {
+	bool		f = false ;
+	if (sp) ylikely {
+	    while (sl && *sp) {
+	        cint	ch = mkchar(*sp) ;
+	        f = isprintlatin(ch) ;
+	        if (f) break ;
+	        sp += 1 ;
+	        sl -= 1 ;
+	    } /* end while */
+	} /* end if (non-null) */
+	return f ;
+}
+/* end subroutine (hasprintlatin) */
+
+bool hasprintbad(cchar *sp,int sl) noex {
+	bool		f = false ;
+	if (sp) ylikely {
+	    while (sl && *sp) {
+	        cint	ch = mkchar(*sp) ;
+	        f = isprintbad(ch) ;
+	        if (f) break ;
+	        sp += 1 ;
+	        sl -= 1 ;
+	    } /* end while */
+	} /* end if (non-null) */
+	return f ;
+}
+/* end subroutine (hasprintbad) */
+
+
+/* local subroutines */
+
+
