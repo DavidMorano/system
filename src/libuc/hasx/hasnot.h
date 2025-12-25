78fc4722392959a3e948b57727e76ad48e13326b
David A.D. Morano <david.a.morano@gmail.com>
2025-12-25 00:54:29 -0500

    restore

---
+++ src/libuc/hasx/hasnot.h
@@ -0,0 +1,46 @@
+/* hasnot HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* has a counted c-string some characteristic? */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright (c) 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	HASNOT_INCLUDE
+#define	HASNOT_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern bool hasnotdots(cchar *,int) noex ;
+extern bool hasnotempty(cchar *,int) noex ;
+
+static inline bool hasNotDots(cchar *sp,int sl) noex {
+	return hasnotdots(sp,sl) ;
+}
+static inline bool hasNotEmpty(cchar *sp,int sl) noex {
+	return hasnotempty(sp,sl) ;
+}
+
+EXTERNC_end
+
+
+#endif /* HASNOT_INCLUDE */
+
+
