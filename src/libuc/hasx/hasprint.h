d94a0d5bc847e6fbe443915f9547717360d26413
David A.D. Morano <david.a.morano@gmail.com>
2025-12-25 00:54:35 -0500

    restore

---
+++ src/libuc/hasx/hasprint.h
@@ -0,0 +1,39 @@
+/* hasprint HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* has a counted c-string some characteristic? */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright (c) 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	HASPRINT_INCLUDE
+#define	HASPRINT_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern bool hasprintlatin(cchar *,int) noex ;
+extern bool hasprintbad(cchar *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* HASPRINT_INCLUDE */
+
+
