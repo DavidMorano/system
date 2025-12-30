38adb522203985a2f6243505b43aa6c77ada5696
David A.D. Morano <david.a.morano@gmail.com>
2025-12-30 12:04:56 -0500

    restore

---
+++ src/libcx/strcase.h
@@ -0,0 +1,39 @@
+/* libcx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 (conformance reviewed) */
+
+/* C-language library-extension subroutines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright (c) 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef STRCASE_INCLUDE
+#define	STRCASE_INCLUDE
+
+#include	<envstandards.h>	/* order first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern char *strcasestr(cchar *,cchar *) noex ;
+
+extern int strncasecmp(cchar *,cchar *,int) noex ;
+
+EXTERNC_end
+
+
+#endif	/* STRCASE_INCLUDE */
+
+
