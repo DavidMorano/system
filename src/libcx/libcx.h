60bb7947df9ca68fadd994ccec6e33608b3fe7b4
David A.D. Morano <david.a.morano@gmail.com>
2025-12-30 12:04:41 -0500

    restore

---
+++ src/libcx/libcx.h
@@ -0,0 +1,32 @@
+/* libcx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 (conformance reviewed) */
+
+/* C-language library-extension subroutines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright (c) 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef LIBCX_INCLUDE
+#define	LIBCX_INCLUDE
+
+#include	<envstandards.h>	/* order first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+#include	<ischartype.h>
+
+
+#endif	/* LIBCX_INCLUDE */
+
+
