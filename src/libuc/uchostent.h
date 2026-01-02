e397fc0dfa9c1ebb13174009b21fd09e8d73b1eb
David A.D. Morano <david.a.morano@gmail.com>
2026-01-02 02:27:31 -0500

    restore

---
+++ src/libuc/uchostent.h
@@ -0,0 +1,38 @@
+/* uchostent HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* subroutines to retrieve various system information items */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-23, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCHOSTENT_INCLUDE
+#define	UCHOSTENT_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/types.h>		/* system types */
+#include	<netdb.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	uc_hostentfree(HOSTENT *) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCHOSTENT_INCLUDE */
+
+
