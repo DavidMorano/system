94ee9f86712081178e1dfb39b55b273923d43a3d
David A.D. Morano <david.a.morano@gmail.com>
2026-01-02 02:21:43 -0500

    restore

---
+++ src/libuc/memtrack.h
@@ -0,0 +1,36 @@
+/* memtrack HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* String-Interate-Find (MEMTRACK) object */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MEMTRACK_INCLUDE
+#define	MEMTRACK_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* __cplusplus */
+#endif /* MEMTRACK_INCLUDE */
+
+
