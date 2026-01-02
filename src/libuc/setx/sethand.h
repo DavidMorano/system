9bf4d0607dac63b3664ce13ce5c8229a3767e7d6
David A.D. Morano <david.a.morano@gmail.com>
2026-01-02 02:24:24 -0500

    restore

---
+++ src/libuc/setx/sethand.h
@@ -0,0 +1,29 @@
+/* sethand HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* set handler */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-12-01, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SETHAND_INCLUDE
+#define	SETHAND_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#endif /* SETHAND_INCLUDE */
+
+
