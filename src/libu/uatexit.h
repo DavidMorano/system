6de1c0e3743f72e93b37b36d77a074c82fd7cd76
David A.D. Morano <david.a.morano@gmail.com>
2026-01-02 02:16:41 -0500

    restore

---
+++ src/libu/uatexit.h
@@ -0,0 +1,40 @@
+/* uatexit HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* record and expunge fork hanlers */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UATEXIT_INCLUDE
+#define	UATEXIT_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#ifndef	ATEXITF_SUBROUTINE
+#define	ATEXITF_SUBROUTINE
+EXTERNC_begin
+typedef void	(*atexit_f)(void) noex ;
+EXTERNC_end
+#endif /* ATEXITF_SUBROUTINE */
+
+
+EXTERNC_begin
+
+extern int u_atexit(atexit_f f) noex ;
+
+EXTERNC_end
+
+
+#endif /* UATEXIT_INCLUDE */
+
+
