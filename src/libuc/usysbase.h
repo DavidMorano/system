bd823e68add5b411f41edbc2bf7ec702210450c2
David A.D. Morano <david.a.morano@gmail.com>
2026-01-03 00:51:14 -0500

    restore

---
+++ src/libuc/usysbase.h
@@ -0,0 +1,33 @@
+/* usysbase HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* version %I% last-modified %G% */
+/* virtual-system definitions */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USYSBASE_INCLUDE
+#define	USYSBASE_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<ubufdefs.h>
+#include	<usys.h>
+
+
+#endif /* USYSBASE_INCLUDE */
+
+
