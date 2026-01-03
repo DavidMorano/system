79043bd60db567b022b8cb39089a67ea0958a593
David A.D. Morano <david.a.morano@gmail.com>
2026-01-03 00:58:28 -0500

    restore

---
+++ src/libuc/filex/fileprintable.h
@@ -0,0 +1,29 @@
+/* fileprintable HEADER (file-is-printable) */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* is the given file totally printable? */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FILEPRINTABLE_INCLUDE
+#define	FILEPRINTABLE_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	fileprintable(cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* FILEPRINTABLE_INCLUDE */
+
+
