8f0710949e0a5025d01ec58e3f2c39508969c8e1
David A.D. Morano <david.a.morano@gmail.com>
2026-01-03 00:57:52 -0500

    restore

---
+++ src/libuc/filex/filex.h
@@ -0,0 +1,37 @@
+/* filex HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* read stuff from a file */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FILEX_INCLUDE
+#define	FILEX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	<filebinary.h>
+#include	<fileliner.h>
+#include	<filelines.h>
+#include	<fileobject.h>
+#include	<fileprintable.h>
+#include	<filereadln.h>
+#include	<filereadstrs.h>
+
+
+#endif /* FILEX_INCLUDE */
+
+
