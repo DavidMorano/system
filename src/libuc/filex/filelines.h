5d2aa26d5a5a39a9348551e7cb401918604b8ca9
David A.D. Morano <david.a.morano@gmail.com>
2026-01-03 00:59:17 -0500

    restore

---
+++ src/libuc/filex/filelines.h
@@ -0,0 +1,36 @@
+/* fileines HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* calculate the number of lines in a file */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2023-12-17, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2023 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FILELINES_INCLUDE
+#define	FILELINES_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	filelines(cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* FILELINES_INCLUDE */
+
+
