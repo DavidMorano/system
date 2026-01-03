b993c3c96fdb617f9cc1a21b7bb26c904f9edbbd
David A.D. Morano <david.a.morano@gmail.com>
2026-01-03 00:59:22 -0500

    restore

---
+++ src/libuc/filex/filereadln.h
@@ -0,0 +1,36 @@
+/* filereadln HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* read a single line from a file */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FILEREADLN_INCLUDE
+#define	FILEREADLN_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	filereadln(cchar *,char *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* FILEREADLN_INCLUDE */
+
+
