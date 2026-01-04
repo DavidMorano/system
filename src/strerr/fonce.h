6527edb8e7fa6b2bdea133535fb1063d0c83ff91
David A.D. Morano <david.a.morano@gmail.com>
2026-01-04 00:15:56 -0500

    restore

---
+++ src/strerr/fonce.h
@@ -0,0 +1,32 @@
+/* fonce HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* File-Once management */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FONCE_INCLUDE
+#define	FONCE_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* FONCE_INCLUDE */
+
+
