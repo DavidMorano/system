653222e83956119a9bf88b23b39d48855c2ca404
David A.D. Morano <david.a.morano@gmail.com>
2026-01-04 00:18:27 -0500

    restore

---
+++ src/strerr/argmgr.h
@@ -0,0 +1,42 @@
+/* argmgr HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* Argument-Manager */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	ARGMGR_INCLUDE
+#define	ARGMGR_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<debprintf.h>
+
+
+#define DEBPR(FMT, ...) ({						\
+	int rc = 0 ;							\
+	if_constexpr (f_debug) {					\
+	    debprintf(__func__, FMT __VA_OPT__(,) __VA_ARGS__) ;	\
+	}								\
+	rc ;								\
+})
+
+
+#endif /* ARGMGR_INCLUDE */
+
+
