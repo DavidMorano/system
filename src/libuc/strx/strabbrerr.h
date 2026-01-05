910c04f868ef3b83bcb59546502b94fc38ae7698
David A.D. Morano <david.a.morano@gmail.com>
2026-01-05 00:07:56 -0500

    restore

---
+++ src/libuc/strx/strabbrerr.h
@@ -0,0 +1,35 @@
+/* strabbrerr HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* string-error (strerr) abbreviation retrieval */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STRABBRERR_INCLUDE
+#define	STRABBRERR_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern cchar *strabbrerr(int) noex ;
+
+local inline cchar *strerrabbr(int v) noex {
+    return strabbrerr(v) ;
+}
+
+EXTERNC_end
+
+
+#endif /* STRABBRERR_INCLUDE */
+
+
