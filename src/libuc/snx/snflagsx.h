ae56b78919db754748b39583fe1abeb77e6aeb16
David A.D. Morano <david.a.morano@gmail.com>
2026-01-06 00:56:33 -0500

    restore

---
+++ src/libuc/snx/snflagsx.h
@@ -0,0 +1,41 @@
+/* snflagsx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* counted c-string operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SNFLAGSX_INCLUDE
+#define	SNFLAGSX_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+EXTERNC_begin
+
+extern int snflagsfs(char *,int,ulong) noex ;
+extern int snflagsopen(char *,int,ulong) noex ;
+extern int snflagspoll(char *,int,ulong) noex ;
+
+EXTERNC_end
+
+
+#endif /* SNFLAGSX_INCLUDE */
+
+
