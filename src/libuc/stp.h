9f2a9055ddfab2ab606917442f21672f93baca71
David A.D. Morano <david.a.morano@gmail.com>
2026-01-08 00:14:25 -0500

    restore

---
+++ src/libuc/stp.h
@@ -0,0 +1,32 @@
+/* stp HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* string-index operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STP_INCLUDE
+#define	STP_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	<stpcpyxc.h>
+#include	<stpncpyxc.h>
+
+
+#endif /* STP_INCLUDE */
+
+
