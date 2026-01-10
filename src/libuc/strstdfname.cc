9e04a048586cf4f3755354796a064139e4ea02a7
David A.D. Morano <david.a.morano@gmail.com>
2026-01-10 00:24:48 -0500

    update

--- src/libuc/strstdfname.cc
+++ src/libuc/strstdfname.cc
@@ -36,7 +36,8 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<localmisc.h>
 
 #include	"strstdfname.h"
