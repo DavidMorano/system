3527bfcd395194cb4fb371c1296575084a7e670c
David A.D. Morano <david.a.morano@gmail.com>
2026-01-12 00:29:41 -0500

    update

--- src/libuc/ucent/ucentsp.h
+++ src/libuc/ucent/ucentsp.h
@@ -1,6 +1,6 @@
 /* ucentsp HEADER */
 /* charset=ISO8859-1 */
-/* lang=C++20 */
+/* lang=C20 */
 
 /* UCENTSP object management */
 /* version %I% last-modified %G% */
@@ -21,10 +21,8 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
+#include	<ucsys.h>
 #include	<spwd.h>		/* <- the money shot! */
 
 
