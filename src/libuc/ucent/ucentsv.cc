a22d70efd9bf2a1e75ae664ab948e09c2ddd9b4a
David A.D. Morano <david.a.morano@gmail.com>
2026-01-12 00:29:26 -0500

    update

--- src/libuc/ucent/ucentsv.cc
+++ src/libuc/ucent/ucentsv.cc
@@ -1,6 +1,6 @@
 /* ucentsv SUPPORT */
 /* charset=ISO8859-1 */
-/* lang=C++20 */
+/* lang=C++20 (conformance reviewed) */
 
 /* UCENTSV object management */
 /* version %I% last-modified %G% */
@@ -27,15 +27,12 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/types.h>
-#include	<sys/param.h>
-#include	<unistd.h>
-#include	<cerrno>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<new>			/* |nothrow(3c++)| */
 #include	<algorithm>		/* |min(3c++)| + |max(3c++)| */
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<storeitem.h>
 #include	<sbuf.h>
 #include	<vechand.h>
@@ -46,10 +43,13 @@
 #include	<cfdec.h>
 #include	<localmisc.h>
 
+#include	"ucgetsv.h"		/* |uc_getsv{x}(3uc)| */
 #include	"ucentsv.h"
 #include	"ucentxx.hh"
 
-import libutil ;
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |lenstr(3u)| */
 
 /* local defines */
 
