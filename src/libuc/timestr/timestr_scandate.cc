dd4a9a6a7df523d4baab954086f668471e660484
David A.D. Morano <david.a.morano@gmail.com>
2026-01-12 00:26:28 -0500

    update

--- src/libuc/timestr/timestr_scandate.cc
+++ src/libuc/timestr/timestr_scandate.cc
@@ -43,7 +43,8 @@
 #include	<ctime>			/* |time_t| */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<usupport.h>		/* |ulogerror(3u)| */
 #include	<tmtime.hh>
 #include	<sntmtime.h>
