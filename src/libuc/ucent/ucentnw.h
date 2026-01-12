aed294a7231483dadda045d7e179170461df1a2f
David A.D. Morano <david.a.morano@gmail.com>
2026-01-12 00:28:42 -0500

    update

--- src/libuc/ucent/ucentnw.h
+++ src/libuc/ucent/ucentnw.h
@@ -1,6 +1,6 @@
 /* ucentnw HEADER */
 /* charset=ISO8859-1 */
-/* lang=C++20 */
+/* lang=C20 */
 
 /* UCENTNW object management */
 /* version %I% last-modified %G% */
@@ -22,10 +22,7 @@
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<stdint.h>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<netdb.h>		/* <- is that the money shot? */
 
 
