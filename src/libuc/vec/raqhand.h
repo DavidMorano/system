73a2c081dbb82f4509de08d4cdfb4476e2ea669b
David A.D. Morano <david.a.morano@gmail.com>
2026-01-12 00:32:23 -0500

    update

--- src/libuc/vec/raqhand.h
+++ src/libuc/vec/raqhand.h
@@ -21,26 +21,56 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 #define	RAQHAND_DEFENTS	10
 #define	RAQHAND		struct raqhand_head
 #define	RAQHAND_FL	struct raqhand_flags
 
-/* options */
-#define	RAQHAND_ODEFAULT	0
-#define	RAQHAND_OREUSE		(1 << 0)	/* reuse empty slots */
-#define	RAQHAND_OCOMPACT	(1 << 1)	/* means NOHOLES */
-#define	RAQHAND_OSWAP		(1 << 2)	/* use swapping */
-#define	RAQHAND_OSTATIONARY	(1 << 3)	/* entries should not move */
-#define	RAQHAND_OCONSERVE	(1 << 4)	/* conserve space */
-#define	RAQHAND_OSORTED		(1 << 5)	/* keep sorted */
-#define	RAQHAND_OORDERED	(1 << 6)	/* keep ordered */
+/**** options
+reuse		= reuse empty slots
+compact		= do not allow for holes
+swap		= use swapping for empty slot management
+stationary	= entries do not move
+conserve	= conserve space where possible
+sorted		= maintain a sorted list
+ordered		= maintain an ordered list
+****/
 
+enum raqhandos {
+    raqhando_reuse,
+    raqhando_compact,
+    raqhando_swap,
+    raqhando_stationary,
+    raqhando_conserve,
+    raqhando_sorted,
+    raqhando_ordered,
+    raqhando_overlast
+} ; /* end enum (raqhandos) */
+
+#ifdef	__cplusplus	/* C++ only! */
+
+struct raqhandms {
+    constexpr static int	reuse		= (1 << raqhando_reuse) ;
+    constexpr static int	compact		= (1 << raqhando_compact) ;
+    constexpr static int	swap		= (1 << raqhando_swap) ;
+    constexpr static int	stationary	= (1 << raqhando_stationary) ;
+    constexpr static int	conserve	= (1 << raqhando_conserve) ;
+    constexpr static int	sorted		= (1 << raqhando_sorted) ;
+    constexpr static int	ordered		= (1 << raqhando_ordered) ;
+} ; /* end struct (raqhandms) */
+
+#endif /* __cplusplus */
+
+#define	RAQHAND_ODEFAULT	0
+#define	RAQHAND_OREUSE		(1 << raqhando_reuse)
+#define	RAQHAND_OCOMPACT	(1 << raqhando_compact)
+#define	RAQHAND_OSWAP		(1 << raqhando_swap)
+#define	RAQHAND_OSTATIONARY	(1 << raqhando_stationary)
+#define	RAQHAND_OCONSERVE	(1 << raqhando_conserve)
+#define	RAQHAND_OSORTED		(1 << raqhando_sorted)
+#define	RAQHAND_OORDERED	(1 << raqhando_ordered)
 
 struct raqhand_flags {
 	uint		issorted:1 ;
@@ -52,7 +82,7 @@ struct raqhand_flags {
 	uint		osorted:1 ;
 	uint		oordered:1 ;
 	uint		oconserve:1 ;
-} ;
+} ; /* end struct (raqhand_flags) */
 
 struct raqhand_head {
 	cvoid		**va ;
@@ -61,26 +91,30 @@ struct raqhand_head {
 	int		n ;		/* extent of array */
 	int		hi ;		/* head */
 	int		ti ;		/* tail */
-} ;
+} ; /* end struct (raqhand_head) */
 
 typedef RAQHAND		raqhand ;
 typedef RAQHAND_FL	raqhand_fl ;
 
 EXTERNC_begin
 
-extern int raqhand_start(raqhand *,int,int) noex ;
-extern int raqhand_ins(raqhand *,cvoid *) noex ;
-extern int raqhand_rem(raqhand *,void **) noex ;
-extern int raqhand_del(raqhand *,int) noex ;
-extern int raqhand_delall(raqhand *) noex ;
-extern int raqhand_count(raqhand *) noex ;
-extern int raqhand_acc(raqhand *,int,void **) noex ; 
-extern int raqhand_acclast(raqhand *,void **) noex ; 
-extern int raqhand_get(raqhand *,int,void **) noex ; 
-extern int raqhand_ent(raqhand *,cvoid *) noex ;
-extern int raqhand_finish(raqhand *) noex ;
+extern int raqhand_start	(raqhand *,int,int) noex ;
+extern int raqhand_ins		(raqhand *,cvoid *) noex ;
+extern int raqhand_rem		(raqhand *,void **) noex ;
+extern int raqhand_del		(raqhand *,int) noex ;
+extern int raqhand_delall	(raqhand *) noex ;
+extern int raqhand_count	(raqhand *) noex ;
+extern int raqhand_acc		(raqhand *,int,void **) noex ; 
+extern int raqhand_acclast	(raqhand *,void **) noex ; 
+extern int raqhand_get		(raqhand *,int,void **) noex ; 
+extern int raqhand_ent		(raqhand *,cvoid *) noex ;
+extern int raqhand_finish	(raqhand *) noex ;
 
 EXTERNC_end
+
+#ifdef	__cplusplus
+extern const raqhandms	raqhandm ;
+#endif /* __cplusplus */
 
 
 #endif /* RAQHAND_INCLUDE */
