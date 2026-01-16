425df513b48f26213e05e017ecf078e83090f1cc
David A.D. Morano <david.a.morano@gmail.com>
2026-01-16 00:42:43 -0500

    restore

---
+++ src/libuc/hasx/hasnon.h
@@ -0,0 +1,38 @@
+/* hasnon HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* does a counted c-string some characteristic? */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright (c) 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	HASNON_INCLUDE
+#define	HASNON_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern bool hasnonwhite(cchar *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* HASNON_INCLUDE */
+
+
