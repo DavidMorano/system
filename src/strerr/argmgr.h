4c2eed17154fc36b9e25cb2877bd166aea94ca9e
David A.D. Morano <david.a.morano@gmail.com>
2026-01-16 00:29:21 -0500

    update

--- src/strerr/argmgr.h
+++ src/strerr/argmgr.h
@@ -15,6 +15,27 @@
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
+/*******************************************************************************
+
+	Object:
+	argmgr
+	
+	Descpriotn:
+	This provides a helper-debug macro for debugging the
+	ARGMGR object.
+
+	Notes:
+	1. Note the use of the GCC extension for providing compound
+	statement-list expressions in a special way.  This extension
+	is more formerly named a "statement-expression" by the GCC
+	people.  Note that both CLANG and the Intel-C++ compilers
+	also support this extension.  These statement-expressions
+	are very useful within preprocessor macros, and that is
+	exactly how I used one below.  I love this extension and
+	feel that it should be incorporated into the C++ standard.
+
+*******************************************************************************/
+
 #ifndef	ARGMGR_INCLUDE
 #define	ARGMGR_INCLUDE
 
@@ -29,11 +50,11 @@
 
 
 #define DEBPR(FMT, ...) ({						\
-	int rc = 0 ;							\
+	int debrc = 0 ;							\
 	if_constexpr (f_debug) {					\
-	    debprintf(__func__, FMT __VA_OPT__(,) __VA_ARGS__) ;	\
+	    debrc = debprintf(__func__, FMT __VA_OPT__(,) __VA_ARGS__) ;\
 	}								\
-	rc ;								\
+	debrc ;								\
 })
 
 
