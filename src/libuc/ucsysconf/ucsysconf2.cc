91b6ed3f58ad59c07df8fbdecc6229dde0c0bd77
David A.D. Morano <david.a.morano@gmail.com>
2026-01-16 00:39:24 -0500

    update

--- src/libuc/ucsysconf/ucsysconf2.cc
+++ src/libuc/ucsysconf/ucsysconf2.cc
@@ -65,9 +65,15 @@ module ;
 #include	<unistd.h>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>		/* |{xx}buflen| + |ustd_{x}(3u)| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<utimeout.h>		/* |uto_{x}| */
+#include	<ustd.h>		/* |ustd_conf{x}| */
+#include	<intsat.h>
 #include	<sysconfcmds.h>
 #include	<localmisc.h>
+
+#include	"ucsysconf.h"
 
 module ucsysconf ;
 
