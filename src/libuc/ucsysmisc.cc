e57a984b2424bf69645e6aa7a0e58654c996297b
David A.D. Morano <david.a.morano@gmail.com>
2026-01-17 00:40:21 -0500

    update

--- src/libuc/ucsysmisc.cc
+++ src/libuc/ucsysmisc.cc
@@ -62,9 +62,9 @@
 
 	Notes:
 	1. In the old days, people actually cared about what this
-	value was. Now-a-days, the omly (pretty much) real use for
+	value was.  Now-a-days, the omly (pretty much) real use for
 	this value is in evaluating the results of the |times(2)|
-	system call. But that call has been depracated.
+	system call.  But that call has been depracated.
 
 
 	Name:
@@ -75,19 +75,93 @@
 	virtual-memory (VM) facility is doing about both total and
 	available memory pages.
 
+
+	Name:
+	uc_ftime
+
+  	Description:
+	This was originally supposed to implement the (so-called)
+	|ftime(3c)| subroutine.  But that subroutine is *broken* by
+	design.  Why is it broken?  It is broken because it always
+	returns the standard time-zone offset from GMT in its
+	'timezone' structure member.  This is essentually useless
+	in real life.  The only useful thing to return there is the
+	*current* time-zone offset as indicated by the 'dstflag'
+	member.  So this is what we do instead of the original
+	behavior.  We instead return the current time-zone offset
+	in the 'timezone' variable, as indicated by the 'dstflag'
+	member variable.
+
+	Use of this subroutine (|uc_ftime(3dam)|) and its UNIX®
+	inspired piece of crap (|ftime(3c)|) should be eliminated
+	from all code as soon as time permits.
+
+	A 'timeb' structure looks like:
+
+	struct timeb {
+		time_t	time ;		// time, seconds since the epoch
+		ushort 	millitm ;	// 1000 msec of additional accuracy
+		short	timezone ;	// timezone, minutes west of GMT
+		short	dstflag ;	// DST flag
+	} ;
+
+	Note:
+	Only Apple Darwin still fully supports this subroutine.
+	Solaris® supports it only partially (not filling in the
+	POSIX® optional fields).  Linux does not (no longer) supports
+	this subroutine at all.
+
+	Synopsis:
+	int uc_ftime(TIMEB *tbp) noex
+
+	Arguments:
+	tbp		time-buffer-pointer
+
+	Returns:
+	>=0		OK
+	<0		error (system-return)
+
+
+  	Name:
+	uc_gettimeofday
+
+	Description:
+	This routine calls the system's (library) 'gettimeofday'
+	subroutine.
+
+	Synopsis:
+	int uc_gettimeofday(struct timeval *tvp,void *np) noex
+	
+	Arguments:
+	tvp		pointer to TIMEVAL object to hold result
+	np		NULL pointer (currently required)
+
+	Returns:
+	>=0		success
+	<0		error (system-return)
+
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>
 #include	<sys/param.h>
+#include	<sys/timeb.h>
 #include	<unistd.h>
+#include	<climits>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<climits>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<ucsysconf.h>
+#include	<tmtime.hh>
+#include	<localmisc.h>
 
 #include	"ucsysmisc.h"
 
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |memclear(3u)| */
 
 /* local defines */
 
@@ -161,6 +235,7 @@
 
 /* local variables */
 
+constexpr bool		f_darwin	= F_DARWIN ;
 constexpr bool		f_procavail 	= F_PROCAVAIL ;
 constexpr bool		f_procall 	= F_PROCALL ;
 constexpr bool		f_hz		= F_HZ ;
@@ -180,7 +255,7 @@ libuc::ucpagesizer	ucpagesize ;
 int uc_nprocessors(int w) noex {
 	int		rs = SR_NOTSUP ;
 	int		cmd = -1 ;
-	int		n = 0 ;
+	int		n = 0 ; /* return-value */
 	switch (w) {
 	case 0:
 	    if_constexpr (f_procavail) {
@@ -199,7 +274,7 @@ int uc_nprocessors(int w) noex {
 	    break ;
         } /* end switch */
 	if ((rs >= 0) && (cmd >= 0)) {
-	    if (long rn{} ; (rs = uc_sysconfval(cmd,&rn)) >= 0) {
+	    if (long rn{} ; (rs = uc_sysconfval(cmd,&rn)) >= 0) ylikely {
 	        n = intsat(rn) ;
 	    } else if (rs == SR_INVALID) {
 	        rs = SR_NOTSUP ;
@@ -238,7 +313,7 @@ int uc_gethz(int w) noex {
 int uc_syspages(int w) noex {
 	int		rs = SR_NOTSUP ;
 	int		cmd = -1 ;
-	int		n = 0 ;
+	int		n = 0 ; /* return-value */
 	switch (w) {
 	case 0:
 	    if_constexpr (f_pagesavail) {
@@ -257,7 +332,7 @@ int uc_syspages(int w) noex {
 	    break ;
         } /* end switch */
 	if ((rs >= 0) && (cmd >= 0)) {
-	    if (long rn{} ; (rs = uc_sysconfval(cmd,&rn)) >= 0) {
+	    if (long rn{} ; (rs = uc_sysconfval(cmd,&rn)) >= 0) ylikely {
 	        n = intsat(rn) ;
 	    } else if (rs == SR_INVALID) {
 		rs = SR_NOTSUP ;
@@ -273,15 +348,52 @@ int uc_pagesize() noex {
 }
 /* end subroutine (uc_pagesize) */
 
+int uc_ftime(TIMEB *tbp) noex {
+    	cnullptr	np{} ;
+	int		rs = SR_FAULT ;
+	if (tbp) ylikely {
+	    if_constexpr (f_darwin) {
+		ftime(tbp) ;
+		rs = SR_OK ;
+	    } else {
+	        memclear(tbp) ;
+	        if (TIMEVAL tv ; (rs = uc_gettimeofday(&tv,np)) >= 0) ylikely {
+		    custime 	t = tbp->time ;
+	            tbp->time = tv.tv_sec ;
+	            tbp->millitm = ushortconv(tv.tv_usec / 1000) ;
+	            if (tmtime tmt ; (rs = tmtime_localtime(&tmt,t)) >= 0) {
+	                tbp->timezone = shortconv(tmt.gmtoff / 60) ;
+	                tbp->dstflag = shortconv(tmt.isdst) ;
+	                rs = (tmt.isdst > 0) ;
+	            } /* end if (tmtime_localtime) */
+	        } /* end if (uc_gettimeofday) */
+	    } /* end if_constexpr (f_darwin) */
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (uc_ftime) */
+
+int uc_gettimeofday(TIMEVAL *tvp,void *dp) noex {
+	int		rs = SR_FAULT ;
+	if (tvp) {
+	    rs = SR_OK ;
+	    if (gettimeofday(tvp,dp) == -1) {
+		rs = (- errno) ;
+	    }
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (uc_gettimeofday) */
+
 
 /* local subrouties */
 
 namespace libuc {
     ucpagesizer::operator int () noex {
     	int		rs ;
-	if ((rs = pagesize) == 0) {
+	if ((rs = pagesize) == 0) ylikely {
 	    cint	cmd = _SC_PAGESIZE ;
-	    if ((rs = uc_sysconfval(cmd,nullptr)) >= 0) {
+	    if ((rs = uc_sysconfval(cmd,nullptr)) >= 0) ylikely {
 		pagesize = rs ;
 	    }
 	}
