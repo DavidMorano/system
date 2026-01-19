50f6de0780f479172cd68bdde015e737785de508
David A.D. Morano <david.a.morano@gmail.com>
2026-01-19 01:22:58 -0500

    update

--- src/libuc/ucsig.h
+++ src/libuc/ucsig.h
@@ -20,11 +20,7 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<usyscalls.h>
+#include	<usysbase.h>
 
 #include	<ucsigset.h>
 #include	<ucsigx.h>
