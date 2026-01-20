973813b151c9c5560f5b8fee68406a76ae142636
David A.D. Morano <david.a.morano@gmail.com>
2026-01-20 01:33:10 -0500

    update

--- src/libuc/ucgetx/ucgetx.h
+++ src/libuc/ucgetx/ucgetx.h
@@ -23,10 +23,7 @@
 #include	<sys/types.h>		/* system types */
 #include	<netdb.h>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 #include	<ucgetloadavg.h>
 #include	<ucgetpid.h>
