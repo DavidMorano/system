7a012d04cf6c0240cac4debe1a8ecc91d8194a3b
David A.D. Morano <david.a.morano@gmail.com>
2026-01-20 01:32:27 -0500

    update

--- src/libuc/ucgetx/ucgetrusage.cc
+++ src/libuc/ucgetx/ucgetrusage.cc
@@ -31,7 +31,9 @@
 #include	<cerrno>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
 #include	<localmisc.h>
 
 
