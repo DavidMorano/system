abed41917ba7d93d18550b0387a0ffab8dcb423f
David A.D. Morano <david.a.morano@gmail.com>
2026-01-21 00:35:15 -0500

    update

--- src/libuc/ucfminmod.cc
+++ src/libuc/ucfminmod.cc
@@ -1,11 +1,9 @@
-/* uc_fminmod */
+/* ucfminmod */
 /* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* interface component for UNIX® library-3c */
 /* set (ensure) a minimum mode on a file */
-
-
-#define	CF_DEBUGS	0		/* compile-time debugging */
 
 
 /* revision history:
@@ -17,21 +15,43 @@
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
-
 #include	<envstandards.h>	/* MUST be first to configure */
-
 #include	<sys/types.h>
 #include	<sys/stat.h>
 #include	<sys/uio.h>
 #include	<unistd.h>
 #include	<fcntl.h>
 #include	<poll.h>
-
-#include	<usystem.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
 #include	<localmisc.h>
 
 
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
 /* forward references */
+
+
+/* local variables */
 
 
 /* exported variables */
@@ -40,21 +60,20 @@
 /* exported subroutines */
 
 int uc_fminmod(int fd,mode_t mm) noex {
-	USTAT		sb ;
 	int		rs = SR_BADFD ;
-	int		f_changed = false ;
+	int		fchanged = false ;
 	if (fd >= 0) {
 	    mm &= (~ S_IFMT) ;
-	    if ((rs = u_fstat(fd,&sb)) >= 0) {
+	    if (ustat sb ; (rs = u_fstat(fd,&sb)) >= 0) {
 	        cmode	cm = (sb.st_mode & (~ S_IFMT)) ;
 	        if ((cm & mm) != mm) {
-	            f_changed = true ;
+	            fchanged = true ;
 	            mm |= cm ;
 	            rs = u_fchmod(fd,mm) ;
 	        } /* end if (needed a change) */
 	    } /* end if (successful stat) */
 	} /* end if (valid) */
-	return (rs >= 0) ? f_changed : rs ;
+	return (rs >= 0) ? fchanged : rs ;
 }
 /* end subroutine (uc_fminmod) */
 
