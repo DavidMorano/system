5589b372d32dafa0a5fe1d0f36dcf63a4c56dbce
David A.D. Morano <david.a.morano@gmail.com>
2026-01-21 00:39:12 -0500

    update

--- src/libuc/strn/strnset.cc
+++ src/libuc/strn/strnset.cc
@@ -68,9 +68,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<usysbase.h>
 #include	<localmisc.h>
 
 #include	"strn.h"
