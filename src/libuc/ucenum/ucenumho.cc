6257b9c5ea90fcfe283417b3e891342106c5fb31
David A.D. Morano <david.a.morano@gmail.com>
2026-01-21 00:35:59 -0500

    update

--- src/libuc/ucenum/ucenumho.cc
+++ src/libuc/ucenum/ucenumho.cc
@@ -28,10 +28,11 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<climits>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>		/* |ulogerror(3u)| */
 #include	<sysdbfn.h>
 #include	<localmisc.h>
 
