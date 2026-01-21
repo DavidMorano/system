01739e5164cc3f990c07c0bf985007eebb471ee9
David A.D. Morano <david.a.morano@gmail.com>
2026-01-21 00:34:21 -0500

    update

--- src/libuc/ucfileop/ucfileop.h
+++ src/libuc/ucfileop/ucfileop.h
@@ -88,10 +88,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 EXTERNC_begin
