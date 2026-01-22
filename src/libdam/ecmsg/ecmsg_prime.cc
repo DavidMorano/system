1387afb830f6755541f1b6758cd7d148878549b3
David A.D. Morano <david.a.morano@gmail.com>
2026-01-22 01:03:32 -0500

    restore

---
+++ src/libdam/ecmsg/ecmsg_prime.cc
@@ -0,0 +1,130 @@
+/* ecmsg_prime SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* email-cover subchannel message */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1999-06-13, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services (RNS).
+
+*/
+
+/* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	ecmsg
+
+	Description:
+	This little dittie provides a little way to help manage the
+	subchannel message for the email-cover algorithm.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<uclibmem.h>
+#include	<strwcpy.h>
+#include	<localmisc.h>
+
+#include	"ecmsg.h"
+
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |lenstr(3u)| */
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int ecmsg_start(ecmsg *op) noex {
+	int		rs = SR_FAULT ;
+	if (op) {
+	    rs = memclear(op) ;
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (ecmsg_start) */
+
+int ecmsg_finish(ecmsg *op) noex {
+	int		rs = SR_FAULT ;
+	int		rs1 ;
+	if (op) {
+	    rs = SR_OK ;
+	    if (op->ebuf) {
+	        rs1 = lm_free(op->ebuf) ;
+	        if (rs >= 0) rs = rs1 ;
+	    }
+	    memclear(op) ;
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (ecmsg_finish) */
+
+int ecmsg_loadbuf(ecmsg *op,cchar *mbuf,int mlen) noex {
+	int		rs = SR_FAULT ;
+	if (op && mbuf) {
+	    rs = SR_OK ;
+	    if (mlen < 0) mlen = lenstr(mbuf) ;
+	    if (mlen > ECMSG_MAXBUFLEN) mlen = ECMSG_MAXBUFLEN ;
+	    if (op->ebuf) {
+	        lm_free(op->ebuf) ;
+	        op->ebuf = nullptr ;
+	        op->elen = 0 ;
+	    }
+	    if (mlen >= 0) {
+	        if (char *bp ; (rs = lm_mall((mlen + 1),&bp)) >= 0) {
+		    op->ebuf = bp ;
+	            strwcpy(bp,mbuf,mlen) ;
+	            op->elen = mlen ;
+	        } /* end if (memory-allocation) */
+	    } /* end if (size) */
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (ecmsg_loadbuf) */
+
+int ecmsg_already(ecmsg *op) noex {
+	int		rs = SR_FAULT ;
+	if (op) {
+	    rs = (op->ebuf != nullptr) ;
+	}
+	return rs ;
+}
+/* end subroutine (ecmsg_already) */
+
+
