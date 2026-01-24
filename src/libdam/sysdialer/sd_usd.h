713f528ada1c255cb861dd9d88e1dee9d2ac52ae
David A.D. Morano <david.a.morano@gmail.com>
2026-01-24 00:54:56 -0500

    restore

---
+++ src/libdam/sysdialer/sd_usd.h
@@ -0,0 +1,67 @@
+/* usd HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* SYSDIALER "usd" dialer */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2003-11-04, David A­D­ Morano
+	This was created as one of the first dialer modules for the
+	DIALER object.
+
+*/
+
+/* Copyright © 2003 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USD_INCLUDE
+#define	USD_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<logfile.h>
+#include	<sysdialer.h>
+
+
+#define	USD_MAGIC	31415926
+#define	USD		struct usd_head
+#define	USD_FL		struct usd_flags
+
+
+struct usd_flags {
+	uint		log:1 ;
+} ; /* end struct */
+
+struct usd_head {
+	logfile		lh ;
+	USD_FL		fl ;
+	uint		magic ;
+	int		tlen ;
+	int		fd ;
+} ; /* end struct */
+
+EXTERNC_begin
+
+extern int usd_open(USD *,SYSDIALER_ARGS *,
+		cchar *,cchar *,cchar **) noex ;
+extern int usd_reade(USD *,char *,int,int,int) noex ;
+extern int usd_recve(USD *,char *,int,int,int,int) noex ;
+extern int usd_recvfrome(USD *,char *,int,int,void *,int *,int,int) noex ;
+extern int usd_recvmsge(USD *,MSGHDR *,int,int,int) noex ;
+extern int usd_write(USD *,cchar *,int) noex ;
+extern int usd_send(USD *,cchar *,int,int) noex ;
+extern int usd_sendto(USD *,cchar *,int,int,void *,int) noex ;
+extern int usd_sendmsg(USD *,MSGHDR *,int) noex ;
+extern int usd_shutdown(USD *,int) noex ;
+extern int usd_close(USD *) noex ;
+
+EXTERNC_end
+
+
+#endif /* USD_INCLUDE */
+
+
