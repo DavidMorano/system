730c8caca9332161b827475f2f0f8648201535c7
David A.D. Morano <david.a.morano@gmail.com>
2026-01-24 00:51:18 -0500

    restore

---
+++ src/libdam/retustime.h
@@ -0,0 +1,39 @@
+/* retusime HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* Connection-Manager */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This subroutine was adopted for use from the DWD program.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	RETUSTIME_INCLUDE
+#define	RETUSTIME_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+EXTERNC_begin
+
+extern ustime	retestime(void) noex ;
+
+EXTERNC_end
+
+
+#endif /* RETUSTIME_INCLUDE */
+
+
