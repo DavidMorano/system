5ac00803806bd0c0ce2392b90ff450e261b76749
David A.D. Morano <david.a.morano@gmail.com>
2026-01-24 00:57:25 -0500

    restore

---
+++ src/libdam/sysdialer/sysdialer_vars.cc
@@ -0,0 +1,88 @@
+/* sysdialer_vars SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* system-dialer storage object */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	- 2003-11-04, David A­D­ Morano
+	This subroutine was adopted for use from the DWD program.
+
+*/
+
+/* Copyright © 2003 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	sysdialer
+
+	Description:
+	This object manages what system-dialers (SYSDIALER) have
+	been loaded so far.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<ctime>
+#include	<climits>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<usupport.h>		/* |getustime(3u)| */
+#include	<uclibmem.h>
+#include	<getbufsize.h>
+#include	<localmisc.h>
+
+#include	"sysdialer.h"
+
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |memclear(3u)| */
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+ 
+
+/* local structures */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+namespace sysdialer_util {
+    vars		var ;
+} /* end namespace (sysdialer_util) */
+
+
+/* exported subroutines */
+
+namespace sysdialer_util {
+    vars::operator int () noex {
+    	int		rs ;
+	if ((rs = getbufsize(bufsize_mp)) >= 0) ylikely {
+	    maxpathlen = rs ;
+	}
+	return rs ;
+    } /* end method (vars::operator) */
+} /* end namespace (sysdialer_util) */
+
+
