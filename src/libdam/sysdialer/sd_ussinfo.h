4b26d48d00bef85c1ba834720ef55da8b7b0fd35
David A.D. Morano <david.a.morano@gmail.com>
2026-01-24 00:55:32 -0500

    restore

---
+++ src/libdam/sysdialer/sd_ussinfo.h
@@ -0,0 +1,129 @@
+/* ussinfo HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 (conformance reviewed) */
+
+/* USSINFO helper object */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2003-11-04, David A­D­ Morano
+	This is a helper object for the various USS-type SYSDIALERs.
+
+*/
+
+/* Copyright © 2003 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USSINFO_INCLUDE
+#define	USSINFO_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<sys/types.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<ids.h>
+#include	<vecstr.h>
+#include	<expcook.h>
+#include	<userinfo.h>
+#include	<keyopt.h>
+#include	<sysdialer.h>
+
+
+#define	USSINFO		struct ussinfo_head
+#define	USSINFO_FL	struct ussinfo_flags
+#define	USSINFO_A	struct ussinfo_allocs
+
+
+struct ussinfo_flags {
+	uint		stores:1 ;
+	uint		progdash:1 ;
+	uint		ids:1 ;
+	uint		userinfo:1 ;
+	uint		ignore:1 ;
+	uint		log:1 ;
+} ; /* end struct */
+
+struct ussinfo_allocs {
+	cchar		*node ;
+	cchar		*svc ;
+	cchar		*pr ;
+	cchar		*portspec ;
+} ; /* end struct */
+
+struct ussinfo_head {
+	mainv		argv ;
+	mainv		envv ;
+	cchar		*pr ;
+	cchar		*prn ;
+	cchar		*searchname ;
+	cchar		*version ;
+	cchar		*afspec ;
+	cchar		*hostname ;
+	cchar		*portspec ;
+	cchar		*svcname ;
+	cchar		*pvfname ;
+	cchar		*dfname ;
+	cchar		*xfname ;
+	cchar		*efname ;
+	cchar		*architecture ;		/* machine architecture */
+	cchar		*umachine ;		/* UNAME machine name */
+	cchar		*usysname ;		/* UNAME OS system-name */
+	cchar		*urelease ;		/* UNAME OS release */
+	cchar		*uversion ;		/* UNAME OS version */
+	cchar		*hz ;			/* OS HZ */
+	cchar		*nodename ;		/* USERINFO */
+	cchar		*domainname ;		/* USERINFO */
+	cchar		*username ;		/* USERINFO */
+	cchar		*homedname ;		/* USERINFO */
+	cchar		*shell ;		/* USERINFO */
+	cchar		*organization ;		/* USERINFO */
+	cchar		*gecosname ; 		/* USERINFO */
+	cchar		*realname ;		/* USERINFO */
+	cchar		*name ;			/* USERINFO */
+	cchar		*tz ;			/* USERINFO */
+	cchar		*groupname ;
+	cchar		*tmpdname ;
+	cchar		*maildname ;
+	cchar		*hfname ;
+	cchar		*lfname ;
+	cchar		*paramfname ;
+	cchar		*logid ;
+	cchar		*defprog ;
+	void		*op ;
+	SYSDIALER_ARGS	*ap ;
+	ids		id ;
+	vecstr		aenvs ;
+	vecstr		stores ;
+	userinfo	u ;
+	USSINFO_A	a ;
+	USSINFO_FL	fl, init, open ;
+	uid_t		uid ;
+	gid_t		gid ;
+	int		argc ;
+	int		argi ;
+	int		ncpu ;
+	int		af ;
+	int		to ;
+} ; /* end struct */
+
+typedef	USSINFO		ussinfo ;
+
+EXTERNC_begin
+
+extern int	ussinfo_start(ussinfo *,void *,SYSDIALER_INFO *,
+			SYSDIALER_ARGS *,cchar *,cchar *) noex ;
+extern int	ussinfo_finish(ussinfo *) noex ;
+extern int	ussinfo_procargs(ussinfo *) noex ;
+extern int	ussinfo_procopts(ussinfo *,keyopt *) noex ;
+extern int	ussinfo_defaults(ussinfo *) noex ;
+extern int	ussinfo_addrparse(ussinfo *) noex ;
+extern int	ussinfo_logfile(ussinfo *) noex ;
+
+EXTERNC_end
+
+
+#endif /* USSINFO_INCLUDE */
+
+
