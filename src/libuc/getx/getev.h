d27d8d3a334d353ab5198630ad9b185ccdb70a8b
David A.D. Morano <david.a.morano@gmail.com>
2026-01-25 00:39:11 -0500

    update

--- src/libuc/getx/getev.h
+++ src/libuc/getx/getev.h
@@ -35,10 +35,7 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 EXTERNC_begin
