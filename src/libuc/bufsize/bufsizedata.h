554218c4046346750ec8dcd00f046b6927a8f3c1
David A.D. Morano <david.a.morano@gmail.com>
2026-01-25 00:04:37 -0500

    restore

---
+++ src/libuc/bufsize/bufsizedata.h
@@ -0,0 +1,40 @@
+/* bufsizedata HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* list the various system buffer-size names (as strings) */
+/* version %I% last-modified %G% */
+
+/* revision history:
+
+	= 2001-04-11, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2001 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	bufsizedata
+
+	Description:
+	This file contains the string names (enumeration names) for
+	the various system buffer-sizes, and the array interface
+	to access them.
+
+*******************************************************************************/
+
+#ifndef	BUFSIZEDATA_INCLUDE
+#define	BUFSIZEDATA_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#endif /* BUFSIZEDATA_INCLUDE */
+
+
