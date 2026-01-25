d76f478ddfb2b398effdc67c70256d99a8262bd8
David A.D. Morano <david.a.morano@gmail.com>
2026-01-25 00:02:23 -0500

    restore

---
+++ src/libuc/dirs.h
@@ -0,0 +1,37 @@
+/* dirs HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* operate on series of possibly hierarchical directories */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright (c) 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	DIRS_INCLUDE
+#define	DIRS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+#include	<mkdirs.h>
+#include	<removes.h>
+#include	<rmdirfiles.h>
+#include	<dirempty.h>
+
+
+#endif /* DIRS_INCLUDE */
+
+
