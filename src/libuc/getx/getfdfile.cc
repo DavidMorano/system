d778b0d2c6e883a4a60acee5ca216a58be4280f8
David A.D. Morano <david.a.morano@gmail.com>
2026-01-25 00:39:16 -0500

    update

--- src/libuc/getx/getfdfile.cc
+++ src/libuc/getx/getfdfile.cc
@@ -45,7 +45,8 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<stdfnames.h>
 #include	<cfdec.h>
 #include	<matstr.h>
