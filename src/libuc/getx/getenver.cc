266cdf6c33173ab1dcd3dce3205138018da3b28e
David A.D. Morano <david.a.morano@gmail.com>
2026-01-25 00:38:43 -0500

    restore

--- src/libuc/getx/getenver.cc
+++ src/libuc/getx/getenver.cc
@@ -47,9 +47,7 @@
 #include	<cstdlib>
 #include	<cstring>		/* |strchr(3c)| */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<usysbase.h>
 #include	<strn.h>		/* |strnchr(3uc)| */
 #include	<matkeystr.h>
 
