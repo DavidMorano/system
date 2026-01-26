c64c2eeceec235eeb0b8697997d245599ded0d2b
David A.D. Morano <david.a.morano@gmail.com>
2026-01-26 00:15:27 -0500

    update

--- src/libu/usys/usys_plock.h
+++ src/libu/usys/usys_plock.h
@@ -17,11 +17,20 @@
 
 /*******************************************************************************
 
+  	Name:
+	usys_plock
+
   	Description:
 	This file contains the UNIX® system types that the brain-damaged
 	MacOS operating system does NOT have.  We are trying in a very
 	small way to make up for some of the immense brain-damage within
 	the Apple Darwin operating system.
+
+	Commands:
+	MLOCKP_NON		unlock everything
+	MLOCKP_ALL		lock-all
+	MLOCKP_TXT		lock-text-segment
+	MLOCKP_DAT		lock_data-segment
 
 *******************************************************************************/
 
@@ -30,7 +39,7 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/time.h>		/* for all others |gettimeofday(3c)| */
+#include	<sys/mman.h>
 #include	<unistd.h>		/* for Solaris® */
 #include	<clanguage.h>
 #include	<utypedefs.h>
@@ -39,19 +48,27 @@
 
 /* MEMPLOCK begin */
 #if	defined(SYSHAS_MEMPLOCK) && (SYSHAS_MEMPLOCK > 0)
+/******************************************************************************/
+
 
 /* SunOS (Solaris®) already has the |plock(2)| system call */
 
-#else /* defined(SYSHAS_MEMPLOCK) && (SYSHAS_MEMPLOCK > 0) */
-#ifdef	__cplusplus /* C++ only! */
 
-#ifndef	SUBROUTINE_STIME
-#define	SUBROUTINE_STIME
+/******************************************************************************/
+#else /* defined(SYSHAS_MEMPLOCK) && (SYSHAS_MEMPLOCK > 0) */
+#ifdef	__cplusplus /* C++ only */
+/******************************************************************************/
+
+
+#ifndef	SUBROUTINE_PLOCK
+#define	SUBROUTINE_PLOCK
 namespace usys {
     extern unixret_t usys_plock(int) noex ;
 }
-#endif /* SUBROUTINE_STIME */
+#endif /* SUBROUTINE_PLOCK */
 
+
+/******************************************************************************/
 #endif /* __cplusplus (C++ only) */
 #endif /* defined(SYSHAS_MEMPLOCK) && (SYSHAS_MEMPLOCK > 0) */
 /* MEMPLOCK end */
