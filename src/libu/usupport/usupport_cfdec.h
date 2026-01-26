37626d37c22a6c8519d7fd242b406de07ca6c513
David A.D. Morano <david.a.morano@gmail.com>
2026-01-26 00:13:40 -0500

    update

--- src/libu/usupport/usupport_cfdec.h
+++ src/libu/usupport/usupport_cfdec.h
@@ -17,10 +17,7 @@
 #include	<stdlib.h>
 #include	<stdint.h>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<stdintx.h>
 
 
