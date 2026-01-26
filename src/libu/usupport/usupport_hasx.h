14cfc5dddcd3d1c4ed3016a88e015928218c03b2
David A.D. Morano <david.a.morano@gmail.com>
2026-01-26 00:13:48 -0500

    update

--- src/libu/usupport/usupport_hasx.h
+++ src/libu/usupport/usupport_hasx.h
@@ -14,9 +14,7 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<usysbase.h>
 
 
 #ifdef	__cplusplus
