726e56681ac6df179c5bf44f7df9c8c1590b03e3
David A.D. Morano <david.a.morano@gmail.com>
2026-01-27 01:15:06 -0500

    restore

---
+++ src/libuc/mkx/mkshlibname.h
@@ -0,0 +1,36 @@
+/* mkshlibname HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* make a program-root file-path */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MKSHLIBNAME_INCLUDE
+#define	MKSHLIBNAME_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	mkshlibname(char *,cchar *,cchar *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* MKSHLIBNAME_INCLUDE */
+
+
