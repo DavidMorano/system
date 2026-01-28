1e9859a584a87eeb863ecfd54a44837ea3adca53
David A.D. Morano <david.a.morano@gmail.com>
2026-01-28 00:11:23 -0500

    restore

---
+++ src/libuc/opendialer/opendialer.h
@@ -0,0 +1,42 @@
+/* opendialer HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* Open-Dialer modules */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	OPENDIALER_INCLUDE
+#define	OPENDIALER_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+
+#include	<opendialer_finger.h>
+#include	<opendialer_hello.h>
+#include	<opendialer_prog.h>
+#include	<opendialer_tcp.h>
+#include	<opendialer_tcpmux.h>
+#include	<opendialer_tcpnls.h>
+#include	<opendialer_ticotsord.h>
+#include	<opendialer_uss.h>
+
+
+#endif /* OPENDIALER_INCLUDE */
+
+
