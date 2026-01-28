99b8ab84a7dc26292698cc0925f129e9222fa917
David A.D. Morano <david.a.morano@gmail.com>
2026-01-28 00:07:45 -0500

    update

--- src/libuc/envx/envmgr.h
+++ src/libuc/envx/envmgr.h
@@ -21,10 +21,7 @@
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usyscalls.h>		/* |ulogerror(3u)| */
 #include	<vecstr.h>
 #include	<vechand.h>
 
