497aeeb653ca7e8034a20e48d0b15b181a81ef7c
David A.D. Morano <david.a.morano@gmail.com>
2026-01-29 00:35:26 -0500

    update

--- src/libuc/snx/snx.h
+++ src/libuc/snx/snx.h
@@ -22,21 +22,22 @@
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>		/* system types (see below) */
 #include        <pthread.h>		/* |pthread_t| */
-#include	<stdarg.h>		/* |va_list| */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
 #include	<usysdefs.h>
 #include	<usysrets.h>
 
+#include	<snabbr.h>
+#include	<snadd.h>
 #include	<sncpy.h>
 #include	<sncpyx.h>		/* repeat? */
 #include	<sncpyxc.h>		/* repeat? */
 #include	<sncpyxw.h>		/* repeat? */
-#include	<snxxx.h>
-#include	<snadd.h>
-#include	<sntmtime.h>
 #include	<snflags.h>
+#include	<snflagsx.h>
+#include	<snxxx.h>
+#include	<sntmtime.h>
 #include	<snuuid.h>
 #include	<snclean.h>
 #include	<snwx.h>
@@ -44,17 +45,11 @@
 
 EXTERNC_begin
 
-extern int snfsflags(char *,int,ulong) noex ;
-extern int snopenflags(char *,int,int) noex ;
-extern int snpollflags(char *,int,int) noex ;
 extern int snxtilook(char *,int,int) noex ;
 extern int sninetaddr(char *,int,int,cchar *) noex ;
-extern int snsigabbr(char *,int,uint) noex ;
-extern int snabbr(char *,int,cchar *,int) noex ;
 extern int snshellunder(char *,int,pid_t,cchar *) noex ;
 extern int snfilemode(char *,int,mode_t) noex ;
 extern int sntid(char *,int,pthread_t) noex ;
-extern int snerrabbr(char *,int,int) noex ;
 extern int snrealname(char *,int,mainv,int) noex ;
 extern int snloadavg(char *,int,uint,int,int,int) noex ;
 extern int snkeyval(char *,int,cchar *,int,cchar *,int) noex ;
