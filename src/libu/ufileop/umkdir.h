bbc5c7d8033ebf3ba24f95d3fa696608f234a469
David A.D. Morano <david.a.morano@gmail.com>
2026-02-01 00:42:27 -0500

    restore

---
+++ src/libu/ufileop/umkdir.h
@@ -0,0 +1,47 @@
+/* umkdir HEADER (UNIX® file operations) */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* translation layer interface for UNIX® equivalents */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services
+	(RNS).
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Names:
+
+	Description:
+	All of the UNIX® system calls that use (operate on) a
+	file-descroptor.
+
+*******************************************************************************/
+
+#ifndef	UMKDIR_INCLUDE
+#define	UMKDIR_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int u_mkdir(cchar *,mode_t) noex ;
+
+EXTERNC_end
+
+
+#endif /* UMKDIR_INCLUDE */
+
+
