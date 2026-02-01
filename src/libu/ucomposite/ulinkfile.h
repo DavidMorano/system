5ec2097b0972e757ecbbdcf75fcd59f20c854ac0
David A.D. Morano <david.a.morano@gmail.com>
2026-02-01 00:40:42 -0500

    restore

---
+++ src/libu/ucomposite/ulinkfile.h
@@ -0,0 +1,36 @@
+/* ulinkfile HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* link a file as it should be */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	ULINKFILE_INCLUDE
+#define	ULINKFILE_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	u_linkfile(cchar *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* ULINKFILE_INCLUDE */
+
+
