d066195521495b5545d5b580472c4813c08a1790
David A.D. Morano <david.a.morano@gmail.com>
2026-02-01 00:42:53 -0500

    restore

---
+++ src/libu/ufileop/ufiletimes.cc
@@ -0,0 +1,108 @@
+/* ufiletimes SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* translation layer interface for UNIX® equivalents */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services (RNS).
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Group:
+	u_filetime
+	u_filetimes
+
+	Description:
+	This system call (|utime(2)|) is the older obsolete form
+	of the newer |utimes(2)| call.  On some (newer) operating
+	systems (Apple Darwin, or others), this call is emulated
+	with the newer |utimes(2)| call.
+
+	Notes:
+	1. The fields of the UTIMBUF object are:
+		actime 		- access time in seconds
+		modtime 	- modification time in seconds
+	2. In the Apple-Darwin operating system, this system-call,
+	it is supported (both its include file and the call itself)
+	but as a library call rather than a system-call.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<sys/time.h>		/* |utimes(2)| */
+#include	<utime.h>		/* |utime(2)| */
+#include	<cerrno>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<localmisc.h>
+
+#include	"ufiletimes.h"
+
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int u_filetime(cchar *fname,CUTIMBUF *utp) noex {
+	int		rs = SR_FAULT ;
+	if (fname && utp) {
+	    rs = SR_OK ;
+	    repeat {
+	        if (utime(fname,utp) < 0) {
+		    rs = (- errno) ;
+		}
+	    } until (rs != SR_INTR) ;
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (u_filetime) */
+
+int u_filetimes(cchar *fname,CTIMEVAL *tvp) noex {
+	int		rs = SR_FAULT ;
+	if (fname && tvp) {
+	    rs = SR_OK ;
+	    repeat {
+	        if (utimes(fname,tvp) < 0) {
+		    rs = (- errno) ;
+		}
+	    } until (rs != SR_INTR) ;
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (u_filetimes) */
+
+
