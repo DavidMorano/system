2796c86f04e23bf3850c9343c612977f44b75dc1
David A.D. Morano <david.a.morano@gmail.com>
2026-02-02 10:24:12 -0500

    update

--- src/libuc/getx/getpwentry.h
+++ src/libuc/getx/getpwentry.h
@@ -15,10 +15,7 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>		/* |uid_t| */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<pwentry.h>
 
 
