7ba22504cca2f0d48405a0ca4a829821a4485f79
David A.D. Morano <david.a.morano@gmail.com>
2026-02-02 00:20:33 -0500

    restore

---
+++ src/libpcs/pcsgetserial.h
@@ -0,0 +1,27 @@
+/* pcsgetserial HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* PCS user-account-database file management */
+/* version %I% last-modified %G% */
+
+
+#ifndef	PCSGETSERIAL_INCLUDE
+#define	PCSGETSERIAL_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int 	pcsgetserial(cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* PCSGETSERIAL_INCLUDE */
+
+
