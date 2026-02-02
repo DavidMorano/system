b213a50394e3e836851991601f8b4a7257350769
David A.D. Morano <david.a.morano@gmail.com>
2026-02-02 10:23:31 -0500

    update

--- src/libuc/getx/getportnum.h
+++ src/libuc/getx/getportnum.h
@@ -32,10 +32,7 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 EXTERNC_begin
