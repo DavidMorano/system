63cccd6c2130da124e9d4f13265df81bffd5dce3
David A.D. Morano <david.a.morano@gmail.com>
2026-02-02 10:27:59 -0500

    update

--- src/libuc/getutmp/getutmpterm.h
+++ src/libuc/getutmp/getutmpterm.h
@@ -29,10 +29,7 @@
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>		/* system types */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 EXTERNC_begin
