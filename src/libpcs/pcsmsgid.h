b4f5cd2c385fa8feb882ac5b26e786b949a6dca0
David A.D. Morano <david.a.morano@gmail.com>
2026-02-02 00:20:39 -0500

    restore

---
+++ src/libpcs/pcsmsgid.h
@@ -0,0 +1,27 @@
+/* pcsmsgid HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* PCS user-account-database file management */
+/* version %I% last-modified %G% */
+
+
+#ifndef	PCSMSGID_INCLUDE
+#define	PCSMSGID_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int pcsuserfile(cc *,cc *,cc *,cc *,cc *) noex ;
+
+EXTERNC_end
+
+
+#endif /* PCSMSGID_INCLUDE */
+
+
