ee7dbaca0d5c6b821e2b7618b276663db7fc2675
David A.D. Morano <david.a.morano@gmail.com>
2026-02-02 10:14:13 -0500

    update

--- src/libpcs/datestr_envelope.cc
+++ src/libpcs/datestr_envelope.cc
@@ -1,13 +1,16 @@
-/* datestr_envelope */
+/* datestr_envelope SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* create a date-str for the UNIX mail envelope */
+/* version %I% last-modified %G% */
 
 
 /* revision history:
 
 	= 1998-01-10, David A­D­ Morano
-        This subroutine was written to replace the previous one. This version
-        simply calls 'timestr_edate()'.
+	This subroutine was written to replace the previous one.
+	This version simply calls |timestr_edate()|.
 
 */
 
@@ -15,6 +18,10 @@
 
 /*******************************************************************************
 
+  	Name:
+	datestr_envelope
+
+	Description:
 	  Places today's date and time into the string "datestr".  
 	  The format is RFC822 standard is:
 		   27 Nov 1981 13:17 EST
@@ -22,22 +29,22 @@
 
 	This kind of date string is used in the DATE header of the message.
 
-
 *******************************************************************************/
 
-
 #include	<envstandards.h>	/* MUST be first to configure */
-
 #include	<sys/types.h>
-#include	<time.h>
-
+#include	<ctime>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<timestr.h>
 #include	<localmisc.h>
+
+#include	"datestr.h"
 
 
 /* external subroutines */
-
-extern char	*timestr_edate(time_t,char *) ;
-extern char	*timestr_hdate(time_t,char *) ;
 
 
 /* external variables */
@@ -46,12 +53,12 @@ extern char	*timestr_hdate(time_t,char *) ;
 /* local (static) variables */
 
 
+/* exported variables */
+
+
 /* exported subroutines */
 
-
-char *datestr_envelope(time_t date,char *datestr)
-{
-
+char *datestr_envelope(time_t date,char *datestr) noex {
 	return timestr_edate(date,datestr) ;
 }
 /* end subroutine (datestr_envelope) */
