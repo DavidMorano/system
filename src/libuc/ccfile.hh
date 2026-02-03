82492677b6174ba306801b095329af0c9c54efeb
David A.D. Morano <david.a.morano@gmail.com>
2026-02-03 00:32:44 -0500

    update

--- src/libuc/ccfile.hh
+++ src/libuc/ccfile.hh
@@ -26,10 +26,7 @@
 #include	<string>
 #include	<string_view>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 enum ccfilemems {
