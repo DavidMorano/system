54ef38c559e64282884b5cbd71157b795747f409
David A.D. Morano <david.a.morano@gmail.com>
2026-02-03 00:31:24 -0500

    update

--- src/libuc/cfx/cfhexstr.h
+++ src/libuc/cfx/cfhexstr.h
@@ -21,11 +21,8 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-
+#include	<usysbase.h>
+#include	<stdintx.h>
 
 EXTERNC_begin
 
