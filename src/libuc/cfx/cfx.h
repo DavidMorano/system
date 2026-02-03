7db5e5f8951c6438d6da938d1d914bb350e8ff3a
David A.D. Morano <david.a.morano@gmail.com>
2026-02-03 00:32:38 -0500

    update

--- src/libuc/cfx/cfx.h
+++ src/libuc/cfx/cfx.h
@@ -21,10 +21,7 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<stdintx.h>
 
 #include	<cfbin.h>
