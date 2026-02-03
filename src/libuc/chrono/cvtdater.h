a6014c5cb66a2caf5fe96bf838bab62776902bf5
David A.D. Morano <david.a.morano@gmail.com>
2026-02-03 00:34:37 -0500

    update

--- src/libuc/chrono/cvtdater.h
+++ src/libuc/chrono/cvtdater.h
@@ -15,10 +15,7 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<time.h>		/* |time_t| */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 #define	CVTDATER	struct cvtdater_head
