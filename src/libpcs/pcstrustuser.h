d875a25019c43005fca2932af403135d1b25f3a9
David A.D. Morano <david.a.morano@gmail.com>
2026-02-04 00:39:39 -0500

    restore

---
+++ src/libpcs/pcstrustuser.h
@@ -0,0 +1,28 @@
+/* pcstrustusers HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* PCS get mail-host for a given user */
+/* version %I% last-modified %G% */
+
+
+#ifndef	PCSTRUSTUSER_INCLUDE
+#define	PCSTRUSTUSER_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<vecstr.h>
+
+
+EXTERNC_begin
+
+extern int	pcstrustuser(cchar *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* PCSTRUSTUSER_INCLUDE */
+
+
