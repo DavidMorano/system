1e3fe64c1d0e98b7135f1f5e9934881aec54a495
David A.D. Morano <david.a.morano@gmail.com>
2026-02-05 01:55:01 -0500

    update

--- src/libdam/argmgr/argmgr1.cc
+++ src/libdam/argmgr/argmgr1.cc
@@ -34,13 +34,13 @@ module ;
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<cstring>		/* |strchr(3c)| */
-#include	<new>			/* |nothrow(3c++)| */
 #include	<string_view>
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
 #include	<usysdefs.h>
 #include	<usysrets.h>
+#include	<usyscalls.h>		/* |msleep(3u)| */
 #include	<ulogerror.h>
 #include	<sfx.h>
 #include	<strn.h>
@@ -48,6 +48,8 @@ module ;
 #include	<mkchar.h>
 #include	<ischarx.h>
 #include	<localmisc.h>
+
+#include	"argmgr.h"
 
 #pragma		GCC dependency	"mod/libutil.ccm"
 #pragma		GCC dependency	"mod/debug.ccm"
@@ -75,23 +77,22 @@ typedef string_view		strview ;
 /* external subroutines */
 
 
+/* external variables */
+
+
 /* local structures */
 
 
 /* forward references */
 
-
-/* forward refernces */
-
 template<typename ... Args>
-static inline int argmgr_magic(argmgr *op,Args ... args) noex {
+local inline int argmgr_magic(argmgr *op,Args ... args) noex {
 	int		rs = SR_FAULT ;
 	if (op && (args && ...)) {
 	    rs = (op->magic == ARGMGR_MAGIC) ? SR_OK : SR_NOTOPEN ;
 	}
 	return rs ;
-}
-/* end subroutine (argmgr_magic) */
+} /* end subroutine (argmgr_magic) */
 
 
 /* local variables */
@@ -145,114 +146,120 @@ int argmgr::iarg() noex {
 
 int argmgr::argopt(cchar **rpp) noex {
     	int		rs ;
+	int		klen = 0 ; /* return-value */
+	DEBPR("ent ai=%d c=%d\n",ai,cntpos) ;
 	if ((rs = argmgr_magic(this)) >= 0) ylikely {
-	    if_constexpr (f_debug) {
-	        debprintf(__func__,"ent ai=%d c=%d\n",ai,cntpos) ;
-	    }
-	    if (ai < argc) ylikely {
+	    DEBPR("ai=%d c=%d\n",ai,cntpos) ;
+	    if (ai < argc) ylikely { /* valid argument range */
 	        cchar	*ap = argv[ai] ;
+	        DEBPR("arg=>%s<\n",ap) ;
 	        if (isplusminus(ap[0])) {
 		    fl.plus = (ap[0] == '+') ;
 		    if (cint ch = mkchar(ap[1]) ; ch) {
 		        rs = iargopt(ap,ch,rpp) ;
+			klen = rs ;
 		    } /* end if (have option) */
-	        } /* end if (isplusminus) */
-	        if (rs > 0) ylikely {
-		    if_constexpr (f_debug) {
-		        strnul sk((ap+1),rs) ;
-		        debprintf(__func__,"kp=>%s<\n",ccp(sk)) ;
-		    }
-		    if (valp) {
-		        cint vall= lenstr(valp) ;
-			if_constexpr (f_debug) {
-		            strnul sv (valp,vall) ;
-		            debprintf(__func__,"vp=>%s<\n",ccp(sv)) ;
-			}
-		    }
-	        } /* end if (non-zero positive) */
+	        } /* end if (isplusminus) */ 
+		/**** debug block begin */
 		if_constexpr (f_debug) {
-	            debprintf(__func__,"ret rs=%d ai=%d c=%d\n",rs,ai,cntpos) ;
-		}
-	    } /* end if (valid) */
+	            if (rs >= 0) ylikely {
+		        strnul sk((ap+1),rs) ; /* string-key */
+		        DEBPR("kp=>%s<\n",ccp(sk)) ;
+		        if (valp) {
+		            cint vall = lenstr(valp) ;
+			    if_constexpr (f_debug) {
+		                strnul sv(valp,vall) ; /* string-val */
+		                DEBPR("vp=>%s<\n",ccp(sv)) ;
+			    }
+		        } /* end if (had non-null 'valp') */
+		    } /* end if (ok) */
+		} /* end if_constexpr (f_debug) */
+		/**** debug block end */
+	        DEBPR("done rs=%d ai=%d c=%d\n",rs,ai,cntpos) ;
+	    } /* end if (valid argument range) */
 	} /* end if (magic) */
-	return rs ;
+	DEBPR("ret rs=%d klen=%d\n",rs,klen) ;
+	return (rs >= 0) ? klen : rs ;
 } /* end method (argmgr::argopt) */
 
-int argmgr::iargopt(cchar *ap,int ch,cchar **rpp) noex {
+int argmgr::iargopt(cchar *ap,int ch,cchar **kpp) noex {
     	cnullptr	np{} ;
     	int		rs = SR_OK ;
+	int		klen = 0 ; /* return-value ("key-length"?) */
+	DEBPR("ent ai=%d c=%d\n",ai,cntpos) ;
 	if (isalphalatin(ch)) ylikely {
-	    if ((rs = amap.set[ai]) >= 0) {
-		if (rpp) *rpp = (ap + 1) ;
+	    DEBPR("latin ch=%02X\n",ch) ;
+	    if ((rs = amap.set[ai]) >= 0) { /* "set" means not pos-arg */
+		if (kpp) *kpp = (ap + 1) ;
 		cntpos -= 1 ;
 		if (cc *tp ; (tp = strchr((ap + 1),'=')) != np) {
 		    valp = (tp + 1) ;
-		    haveargoptval = true ;
-		    rs = intconv(tp - (ap + 1)) ;
+		    fargoptval = true ; /* public flag */
+		    klen = intconv(tp - (ap + 1)) ;
 		} else {
 		    valp = nullptr ;
-		    haveargoptval = false ;
-		    rs = lenstr(ap + 1) ;
-		}
-	    } /* end if (amap,set) */
+		    fargoptval = false ; /* public flag */
+		    klen = lenstr(ap + 1) ;
+		} /* end if */
+	    } /* end if (amap.set) */
 	} else if (isdigitlatin(ch)) {
 	    digvalp = (ap + 1) ;
 	} /* end if (isalphalatin) */
-	return rs ;
+	DEBPR("ret rs=%d\n",rs) ;
+	return (rs >= 0) ? klen : rs ;
 } /* end method (argmgr::iargopt) */
 
-int argmgr::argoptlong(cchar **rpp) noex {
+int argmgr::argoptlong(cchar **kpp) noex {
     	int		rs ;
+	int		klen = 0 ; /* return-value ("key-length") */
 	if ((rs = argmgr_magic(this)) >= 0) ylikely {
-	    if (ai < argc) ylikely {
+	    if (ai < argc) ylikely { /* valid argumen range */
 	        cchar	*ap = argv[ai] ;
 	        if ((ap[0] == '-') && (ap[1] == '-')) {
 		    if (cint ch = mkchar(ap[2]) ; ch) {
 		        if (isalphalatin(ch)) ylikely {
 			    if ((rs = amap.set[ai]) >= 0) {
 			        cntpos -= 1 ;
-		                if (rpp) *rpp = (ap + 2) ;
-		                rs = lenstr(ap + 2) ;
+		                if (kpp) *kpp = (ap + 2) ;
+		                klen = lenstr(ap + 2) ;
 			    }
 		        } else {
-		            argoptdone = true ;
+		            fargoptdone = true ; /* public flag */
 		            aie = ai ;
 		        }
 		    } else {
-		            argoptdone = true ;
+		            fargoptdone = true ; /* public flag */
 		            aie = ai ;
 			    cntpos -= 1 ;
 		    }
-	        } /* end if (ch) */
-	    } /* end if (valid) */
+	        } /* end if (long-opt) */
+	    } /* end if (valid argument range) */
 	} /* end if (magic) */
-	return rs ;
+	return (rs >= 0) ? klen : rs ;
 } /* end method (argmgr::argoptlong) */
 
 /* this gets the next value, whether the next arg or an arg-opt-val */
-int argmgr::argval(cchar **rpp) noex {
+int argmgr::argval(cchar **vpp) noex {
     	int		rs ;
-	int		al = 0 ; /* return-value */
-	if_constexpr (f_debug) {
-	    debprintf(__func__,"ent\n") ;
-	}
+	int		vlen = 0 ; /* return-value ("val-length") */
+	DEBPR("ent\n") ;
 	if ((rs =  argmgr_magic(this)) >= 0) ylikely {
     	    rs = SR_INVALID ;
-	    if (ai < argc) ylikely {
+	    if (ai < argc) ylikely { /* valid arguement range */
 	        cchar *rp = nullptr ;
 	        if (valp) {
 		    rs = SR_OK ;
 		    rp = valp ;
-		    al = lenstr(valp) ;
+		    vlen = lenstr(valp) ;
 		    valp = nullptr ;
-		    haveargoptval = false ;
+		    fargoptval = false ; /* public flag */
 	        } else {
 	            if ((rs = get((ai + 1),&rp)) > 0) {
 		        ai = rs ;
 		        if (rp) {
 			    if ((rs = amap.set[ai]) >= 0) {
 			        cntpos -= 1 ;
-		                al = lenstr(rp) ;
+		                vlen = lenstr(rp) ;
 			    }
 		        } else {
 			    rs = SR_INVALID ;
@@ -260,54 +267,58 @@ int argmgr::argval(cchar **rpp) noex {
 		    } else if (rs == 0) {
 		        rs = SR_INVALID ;
 		    }
-	        } /* end if */
-	        if (rpp) *rpp = rp ;
+	        } /* end if (existing or new value) */
+	        if (vpp) *vpp = rp ;
 	    } /* end if (within possible range) */
 	} /* end if (magic) */
-	if_constexpr (f_debug) {
-	    debprintf(__func__,"rs=%d al=%d\n",rs,al) ;
-	}
-	return (rs >= 0) ? al : rs ;
+	DEBPR("ret rs=%d vlen=%d\n",rs,vlen) ;
+	return (rs >= 0) ? vlen : rs ;
 } /* end method (argmgr::argval) */
 
-int argmgr::get(int i,ccharpp rpp) noex {
+int argmgr::get(int ri,ccharpp rpp) noex {
     	int		rs = SR_OK ;
 	bool		f = false ;
-	if_constexpr (f_debug) {
-	    debprintf(__func__,"ent i=%d\n",i) ;
-	}
-	while ((rs >= 0) && (i < argc) && (! f)) {
+	DEBPR("ent ri=%d aie=%d\n",ri,aie) ;
+	while ((rs >= 0) && (ri < argc) && (! f)) {
+	    DEBPR("loop-top ri=%d\n",ri) ;
 	    if (aie > 0) {
-		if (i < aie) {
-		    if ((rs = amap.tst[i]) > 0) {
-		        i += 1 ;
+	        DEBPR("arg-end-case ri=%d\n",ri) ;
+		if (ri < aie) {
+	            DEBPR("request-before ri=%d\n",ri) ;
+		    if ((rs = amap.tst[ri]) > 0) {
+		        ri += 1 ;
 		    } else if (rs == 0) {
 		        f = true ;
 		    }
-		} else if (i > aie) {
-		    f = true ;
-		}
+		} else if (ri == aie) {
+	            DEBPR("request-deadon ri=%d\n",ri) ;
+		    ri += 1 ;
+		} else if (ri > aie) {
+	            DEBPR("request-after ri=%d\n",ri) ;
+		    f = true ; /* <- automatically assumed pos-arg */
+		} /* end if (type of request) */
 	    } else {
-	        if_constexpr (f_debug) {
-		    debprintf(__func__,"reg i=%d\n",i) ;
-	        }
-		if ((rs = amap.tst[i]) > 0) {
-		    i += 1 ;
+		DEBPR("reg-case ri=%d\n",ri) ;
+		if ((rs = amap.tst[ri]) > 0) {
+		    ri += 1 ;
 		} else if (rs == 0) {
 		    f = true ;
 		}
-	        if_constexpr (f_debug) {
-	            debprintf(__func__,"reg rs=%d\n",rs) ;
-		}
-	    } /* end if */
+	        DEBPR("reg-case rs=%d\n",rs) ;
+	    } /* end if (ehether "arg-end" present) */
 	} /* end while */
 	if (rs >= 0) ylikely {
-	    if (rpp) *rpp = (f) ? argv[i] : nullptr ;
-	}
-	if_constexpr (f_debug) {
-	    debprintf(__func__,"rs=%d i=%d f=%u\n",rs,i,f) ;
-	}
-	return (rs >= 0) ? i : rs ;
+	    if (rpp) {
+		if (f) {
+		    *rpp = argv[ri] ;
+		} else {
+		    *rpp = nullptr ;
+		    ri = 0 ;
+		}
+	    } /* end if (result disposition) */
+	} /* end if (finish-up) */
+	DEBPR("ret rs=%d ri=%d f=%u\n",rs,ri,f) ;
+	return (rs >= 0) ? ri : rs ;
 } /* end method (argmgr::get) */
 
 int argmgr::present(int i) noex {
