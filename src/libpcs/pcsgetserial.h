17c52e579af710b5f6f86d93fb9c3116b4a4dc41
David A.D. Morano <david.a.morano@gmail.com>
2026-02-06 00:38:30 -0500

    update

--- src/libpcs/pcsgetserial.h
+++ src/libpcs/pcsgetserial.h
@@ -6,6 +6,15 @@
 /* version %I% last-modified %G% */
 
 
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This program was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
 #ifndef	PCSGETSERIAL_INCLUDE
 #define	PCSGETSERIAL_INCLUDE
 
