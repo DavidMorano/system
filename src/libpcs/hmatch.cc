b6637ff5273e1db0b81538e329e57241782c6c2d
David A.D. Morano <david.a.morano@gmail.com>
2026-02-06 00:37:10 -0500

    update

--- src/libpcs/hmatch.cc
+++ src/libpcs/hmatch.cc
@@ -9,10 +9,10 @@
 /* revision history:
 
 	= 1995-04-01, David A­D­ Morano
-	This is part of our cleanup-compatibility effort.
+	This is part of my cleanup-compatibility effort.
 
 	= 1998-11-22, David A­D­ Morano
-        I did some clean-up.
+        I did some more clean-up.
 
 */
 
@@ -24,21 +24,21 @@
 	hmatch
 
   	Description:
-	Is the initial substring of 'buf' the specified 'header'
-	string?  Return 0 if there is no match, else we return the
-	character position of the header value string.  The match
-	is case independent.
+	Is the initial non-white-space substring of the given
+	c-string to be tested the specified 'header' string?  Return
+	0 if there is no match, else we return the character position
+	of the header-value string.  The match is case independent.
 
 	Synopsis:
-	int hmatch(cchar *header,cchar *sp) noex
+	int hmatch(cchar *header,cchar *str) noex
 
 	Arguments:
 	- header	header string that we are looking for
 	- str		string value to test for header
 
 	Returns:
-	>0		index to first non-white-space value character
-	0		no header match
+	>0		index to first non-white-space header-value character
+	==0		no header match
 	<0		error (system-return)
 
 *******************************************************************************/
@@ -88,22 +88,31 @@ int hmatch(cchar *header,cchar *str) noex {
 	int		rc = 0 ; /* return-value */
 	if (header && str) {
 	    cchar	*hp = header ;
-	    cchar	*bp = str ;
-	    bool	f = false ;
-	    while (*hp && (*hp != ':')) {
-	        f = (CHAR_TOLC(*bp) != CHAR_TOLC(*hp)) ;
-	        if (f) break ;
-	        bp += 1 ;
+	    cchar	*sp = str ;
+	    bool	fmat = false ;
+	    rs = SR_OK ;
+	    while (CHAR_ISWHT(*sp)) {
+		sp += 1 ;
+	    }
+	    while (*sp && *hp && (*sp != ':') && (! CHAR_ISWHT(*sp))) {
+	        fmat = (CHAR_TOLC(*sp) == CHAR_TOLC(*hp)) ;
+	        if (! fmat) break ;
+	        sp += 1 ;
 	        hp += 1 ;
 	    } /* end while */
-	    if (! f) {
-	        while (CHAR_ISWHT(*bp)) {
-		    bp += 1 ;
+	    if (fmat) {
+		fmat = (hp[0] == '\0') ; /* all of header-key matched */
+	    }
+	    if (fmat) {
+	        while (CHAR_ISWHT(*sp)) {
+		    sp += 1 ;
 	        }
-	        if (*bp++ == ':')  {
-	            while (CHAR_ISWHT(*bp)) bp += 1 ;
-	            rc = intconv(bp - str) ;
-	        }
+	        if (*sp++ == ':')  {
+	            while (CHAR_ISWHT(*sp)) {
+			sp += 1 ;
+		    }
+	            rc = intconv(sp - str) ;
+	        } /* end if (have colon) */
 	    } /* end if */
 	} /* end if (non-null) */
 	return (rs >= 0) ? rc : rs ;
