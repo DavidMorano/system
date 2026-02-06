34a31d59f37e3d5253c99b39bfe398ed20245aea
David A.D. Morano <david.a.morano@gmail.com>
2026-02-06 00:31:50 -0500

    restore

---
+++ src/mkindexing/pikeys.h
@@ -0,0 +1,37 @@
+/* pikeys HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* handle the keys while processing a file */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	KEYS_INCLUDE
+#define	KEYS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<sys/types.h>		/* system types */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<bfile.h>
+#include	<hdb.h>
+#include	<ptm.h>
+
+#include	"proginfo.h"
+
+
+EXTERNC_begin
+
+extern int keys_begin(proginfo *,hdb *,int) noex ;
+extern int keys_add(proginfo *,hdb *,cchar *,int) noex ;
+extern int keys_end(proginfo *,hdb *,bfile *,ptm *,cchar *,off_t,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* KEYS_INCLUDE */
+
+
