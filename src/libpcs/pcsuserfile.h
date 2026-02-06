547077e3f900768fa842ce44b78b81ef3e30c820
David A.D. Morano <david.a.morano@gmail.com>
2026-02-06 00:40:01 -0500

    update

--- src/libpcs/pcsuserfile.h
+++ src/libpcs/pcsuserfile.h
@@ -6,6 +6,15 @@
 /* version %I% last-modified %G% */
 
 
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
 #ifndef	PCSUSERFILE_INCLUDE
 #define	PCSUSERFILE_INCLUDE
 
