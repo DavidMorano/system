64c10a8c3d9ef8f56c6d619d96142c7af847697a
David A.D. Morano <david.a.morano@gmail.com>
2026-02-06 00:29:38 -0500

    restore

---
+++ src/libuc/six/sinext.h
@@ -0,0 +1,36 @@
+/* sinext HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* string-index operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-23, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SINEXT_INCLUDE
+#define	SINEXT_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int sinext(cchar *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* SINEXT_INCLUDE */
+
+
