50a53ac1367f984fa0ecfddaff2520b6036c6e01
David A.D. Morano <david.a.morano@gmail.com>
2026-02-06 00:30:59 -0500

    restore

---
+++ src/libuc/ucmincore.h
@@ -0,0 +1,36 @@
+/* ucincore HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* in-core memory management */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-05, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCMINCORE_INCLUDE
+#define	UCMINCORE_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	uc_mincoreset(void *,size_t,char *) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCMINCORE_INCLUDE */
+
+
