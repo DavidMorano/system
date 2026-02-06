b500a1163105e48db3a4219efb95fc1d86600a7b
David A.D. Morano <david.a.morano@gmail.com>
2026-02-06 00:37:05 -0500

    update

--- src/libpcs/errfile.h
+++ src/libpcs/errfile.h
@@ -6,6 +6,17 @@
 /* version %I% last-modified %G% */
 
 
+/* revision history:
+
+	= 1994-06-01, David A­D­ Morano
+	This module was created so that a file to store debugging
+	or error information could be accessed independent from the
+	regular I-O of the program.
+
+*/
+
+/* Copyright © 1994 David A­D­ Morano.  All rights reserved. */
+
 #ifndef	ERRFILE_INCLUDE
 #define	ERRFILE_INCLUDE
 
