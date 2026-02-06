c4354d31ae74b4a8539692767334c0260fc4e149
David A.D. Morano <david.a.morano@gmail.com>
2026-02-06 00:39:49 -0500

    update

--- src/libpcs/pcstrustuser.h
+++ src/libpcs/pcstrustuser.h
@@ -2,9 +2,19 @@
 /* charset=ISO8859-1 */
 /* lang=C20 */
 
-/* PCS get mail-host for a given user */
+/* is a user a PCS-trusted user */
 /* version %I% last-modified %G% */
 
+
+/* revision history:
+
+	= 1998-05-01, David A­D­ Morano
+	This code module was completely rewritten to replace any
+	original garbage that was here before.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	PCSTRUSTUSER_INCLUDE
 #define	PCSTRUSTUSER_INCLUDE
