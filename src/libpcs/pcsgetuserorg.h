7022bc3af5919bc2a56a61471a1bab17b6a0203d
David A.D. Morano <david.a.morano@gmail.com>
2026-02-06 00:38:38 -0500

    update

--- src/libpcs/pcsgetuserorg.h
+++ src/libpcs/pcsgetuserorg.h
@@ -1,9 +1,17 @@
 /* pcsgetuserorg HEADER */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* get the best approximation of the username of the current user */
 /* version %I% last-modified %G% */
 
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This code was originally written.
+
+*/
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
