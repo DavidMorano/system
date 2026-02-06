fa6c508d27b09d037c308a15d9ebb354ba597e5c
David A.D. Morano <david.a.morano@gmail.com>
2026-02-06 00:38:56 -0500

    update

--- src/libpcs/pcsmailhost.h
+++ src/libpcs/pcsmailhost.h
@@ -6,6 +6,16 @@
 /* version %I% last-modified %G% */
 
 
+/* revision history:
+
+	= 1998-05-01, David A­D­ Morano
+	This code module was completely rewritten to replace any
+	original garbage that was here before.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
 #ifndef	PCSMAILHOST_INCLUDE
 #define	PCSMAILHOST_INCLUDE
 
@@ -13,7 +23,6 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<clanguage.h>
 #include	<usysbase.h>
-#include	<vecstr.h>
 
 
 EXTERNC_begin
