51110a8826113e46a8eb2c9d1c17eddeddd97006
David A.D. Morano <david.a.morano@gmail.com>
2026-02-08 00:11:49 -0500

    restore

---
+++ src/libuc/strxcmp/stremacmp.h
@@ -0,0 +1,38 @@
+/* stremacmp HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* find a substring within a larger string */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STREMACMP_INCLUDE
+#define	STREMACMP_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern int	stremacmp(cchar *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* STREMACMP_INCLUDE */
+
+
