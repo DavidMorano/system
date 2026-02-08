5793361840fdde5c1a0b2b9b0ab9062391ab9658
David A.D. Morano <david.a.morano@gmail.com>
2026-02-08 00:16:08 -0500

    update

--- src/opensvc/opensvc_pcsmotd.cc
+++ src/opensvc/opensvc_pcsmotd.cc
@@ -92,18 +92,6 @@
 
 /* external subroutines */
 
-extern int	sncpy1(char *,int,const char *) ;
-extern int	sncpy2(char *,int,const char *,const char *) ;
-extern int	matkeystr(const char **,const char *,int) ;
-extern int	openshmtmp(char *,int,mode_t) ;
-extern int	getgroupname(char *,int,gid_t) ;
-extern int	getuserhome(char *,int,const char *) ;
-extern int	localgetorg(const char *,char *,int,const char *) ;
-extern int	bufprintf(char *,int,const char *,...) ;
-extern int	cfdeci(const char *,int,int *) ;
-extern int	mkpr(char *,int,const char *,const char *) ;
-extern int	isNotPresent(int) ;
-
 #if	CF_DEBUGS || CF_DEBUGN
 extern int	debugprintf(cchar *,...) ;
 extern int	strlinelen(cchar *,int,int) ;
@@ -112,10 +100,6 @@ extern int	strlinelen(cchar *,int,int) ;
 #if	CF_DEBUGN
 extern int	nprintf(cchar *,...) ;
 #endif
-
-extern cchar	*getourenv(cchar **,cchar *) ;
-
-extern char	*strwcpy(char *,const char *,int) ;
 
 
 /* local structures */
