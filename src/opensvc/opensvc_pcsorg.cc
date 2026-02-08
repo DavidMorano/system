9d27166ca20f54f42833c8ee39fb252ced0acf27
David A.D. Morano <david.a.morano@gmail.com>
2026-02-08 00:16:13 -0500

    update

--- src/opensvc/opensvc_pcsorg.cc
+++ src/opensvc/opensvc_pcsorg.cc
@@ -81,20 +81,6 @@
 
 /* external subroutines */
 
-extern int	sncpy1(char *,int,const char *) ;
-extern int	sncpy2(char *,int,const char *,const char *) ;
-extern int	matkeystr(const char **,const char *,int) ;
-extern int	getuserhome(char *,int,const char *) ;
-extern int	getuserorg(char *,int,const char *) ;
-extern int	localgetorg(const char *,char *,int,const char *) ;
-extern int	pcsgetorg(const char *,char *,int,const char *) ;
-extern int	cfdeci(const char *,int,int *) ;
-extern int	isNotPresent(int) ;
-
-extern cchar	*getourenv(cchar **,cchar *) ;
-
-extern char	*strwcpy(char *,const char *,int) ;
-
 
 /* local structures */
 
