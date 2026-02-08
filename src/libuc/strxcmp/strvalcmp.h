5396a2cc99eb9e0a14e572a35860a147d32bec1f
David A.D. Morano <david.a.morano@gmail.com>
2026-02-08 00:11:33 -0500

    restore

---
+++ src/libuc/strxcmp/strvalcmp.h
@@ -0,0 +1,38 @@
+/* strvalcmp HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* find a substring within a larger string */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STRVALCMP_INCLUDE
+#define	STRVALCMP_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern int	strvalcmp(cchar *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* STRVALCMP_INCLUDE */
+
+
