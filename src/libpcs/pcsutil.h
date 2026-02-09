716d4533dee5083bdd6eef875f17c93ce7ec5e45
David A.D. Morano <david.a.morano@gmail.com>
2026-02-09 00:38:40 -0500

    restore

---
+++ src/libpcs/pcsutil.h
@@ -0,0 +1,29 @@
+/* pcsutil HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* PCS utilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This file was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	PCSUTIL_INCLUDE
+#define	PCSUTIL_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysnase.h>
+
+
+#endif /* PCSUTIL_INCLUDE */
+
+
