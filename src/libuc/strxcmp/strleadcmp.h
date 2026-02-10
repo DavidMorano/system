85c8679befa07021fee2884bce5697fbc0a12878
David A.D. Morano <david.a.morano@gmail.com>
2026-02-10 12:22:51 -0500

    restore

---
+++ src/libuc/strxcmp/strleadcmp.h
@@ -0,0 +1,31 @@
+/* strleadcmp HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* c-string comparisons */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STRLEADCMP_INCLUDE
+#define	STRLEADCMP_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern int	strleadcmp(cchar *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* STRLEADCMP_INCLUDE */
+
+
