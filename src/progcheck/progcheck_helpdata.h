e0b6b6a7dfb4011b1ad07996c6eb99e2cee53a02
David A.D. Morano <david.a.morano@gmail.com>
2026-02-10 12:24:21 -0500

    restore

---
+++ src/progcheck/progcheck_helpdata.h
@@ -0,0 +1,25 @@
+/* progcheck_helpdata HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* help-usage date */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	HELPDATA_INCLUDE
+#define	HELPDATA_INCLUDE
+
+extern const cpcchar	helpdata[] ;
+
+#endif /* HELPDATA_INCLUDE */
+
+
