bb50db35abcc63b0272d60765f0b1f19fd978916
David A.D. Morano <david.a.morano@gmail.com>
2026-02-11 00:28:35 -0500

    restore

---
+++ src/libu/uopen/udup.h
@@ -0,0 +1,58 @@
+/* udup HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* UNIX® File-Descriptor Dupplication */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	This file contains the subroutine declarations for various
+	UNIX® system Memory-Management subroutines.
+
+	Names:
+	u_mdup
+	u_mdup2
+
+	Descrptor:
+	I duplication file-descriptors.
+
+*******************************************************************************/
+
+#ifndef	UDUP_INCLUDE
+#define	UDUP_INCLUDE
+
+
+#include	<envstandards.h>
+#include	<sys/types.h>
+#include	<sys/mman.h>
+#include	<unistd.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<clanguage.h>
+
+#include	<usys.h>
+
+
+EXTERNC_begin
+
+extern int u_dup(int) noex ;
+extern int u_dups(int,int) noex ;
+extern int u_dup2(int,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* UDUP_INCLUDE */
+
+
