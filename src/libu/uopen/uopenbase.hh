af2e3bf54a4a70141c55656ffe99981ae5797581
David A.D. Morano <david.a.morano@gmail.com>
2026-02-12 01:01:55 -0500

    restore

---
+++ src/libu/uopen/uopenbase.hh
@@ -0,0 +1,42 @@
+/* uopenbase HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* UNIX® support for opening files or devices */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Objects:
+	uopenbase
+
+	Description:
+	This module provides support for opening files or devices.
+
+*******************************************************************************/
+
+#ifndef	UOPENBASE_INCLUDE
+#define	UOPENBASE_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+
+#endif /* __cplusplus */
+#endif /* UOPENBASE_INCLUDE */
+
+
