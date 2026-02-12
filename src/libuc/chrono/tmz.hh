4713d80ff7990f90c6336db006e572fe51222c48
David A.D. Morano <david.a.morano@gmail.com>
2026-02-12 00:54:31 -0500

    update

--- src/libuc/chrono/tmz.hh
+++ src/libuc/chrono/tmz.hh
@@ -22,21 +22,19 @@
 
 #include	<envstandards.h>	/* first to configure */
 #include	<time.h>		/* |TM| */
+#include	<limits.h>		/* |SHORT_MIN| */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 #define	TMZ		tmz
-#define	TMZ_FLAGS	tmz_flags
+#define	TMZ_FL		tmz_flags
 
 
 struct tmz_flags {
 	uint		zoff:1 ;	/* zone offset is present */
 	uint		year:1 ;	/* year is present */
-} ;
+} ; /* end struct */
 
 enum tmzmems {
 	tmzmem_clear,
@@ -46,7 +44,7 @@ enum tmzmems {
 	tmzmem_haszoff,
 	tmzmem_haszone,
 	tmzmem_getdst,
-	tmzmem_getzoff,
+	tmzmem_fini,
 	tmzmem_overlast
 } ; /* end enum (tmzmems) */
 
@@ -74,57 +72,66 @@ struct tmz {
 	tmz_co		haszoff ;
 	tmz_co		haszone ;
 	tmz_co		getdst ;
-	tmz_co		getzoff ;
+	tmz_co		fini ;
 	TM		st ;
-	TMZ_FLAGS	fl ;
+	TMZ_FL		fl ;
 	short		zoff ;		/* minutes west of GMT */
 	tmz() noex {
-	    clear(this,tmzmem_clear) ;
-	    init(this,tmzmem_init) ;
-	    isset(this,tmzmem_isset) ;
-	    hasyear(this,tmzmem_hasyear) ;
-	    haszoff(this,tmzmem_haszoff) ;
-	    haszone(this,tmzmem_haszone) ;
-	    getdst(this,tmzmem_getdst) ;
-	    getzoff(this,tmzmem_getzoff) ;
-	} ;
-	int	xstd(cchar *,int) noex ;
-	int	xmsg(cchar *,int) noex ;
-	int	xtouch(cchar *,int) noex ;
-	int	xtoucht(cchar *,int) noex ;
-	int	xstrdig(cchar *,int) noex ;
-	int	xlogz(cchar *,int) noex ;
-	int	xday(cchar *,int) noex ;
-	int	setday(int,int,int) noex ;
-	int	setyear(int) noex ;
-	int	setzone(cchar *,int) noex ;
-	int	gettm(TM *) noex ;
+	    clear	(this,tmzmem_clear) ;
+	    init	(this,tmzmem_init) ;
+	    isset	(this,tmzmem_isset) ;
+	    hasyear	(this,tmzmem_hasyear) ;
+	    haszoff	(this,tmzmem_haszoff) ;
+	    haszone	(this,tmzmem_haszone) ;
+	    getdst	(this,tmzmem_getdst) ;
+	    fini	(this,tmzmem_fini) ;
+	    st = {} ;
+	    fl = {} ;
+	    zoff = SHORT_MIN ;
+	} ; /* end ctor */
+	int xstd	(cchar *,int) noex ;
+	int xmsg	(cchar *,int) noex ;
+	int xtouch	(cchar *,int) noex ;
+	int xtoucht	(cchar *,int) noex ;
+	int xstrdig	(cchar *,int) noex ;
+	int xlogz	(cchar *,int) noex ;
+	int xday	(cchar *,int) noex ;
+	int setday	(int,int,int) noex ;
+	int setyear	(int) noex ;
+	int setzone	(cchar *,int) noex ;
+	int gettm	(TM *) noex ;
+	int getzoff	(short *) noex ;
+	int getzone	(char *,int) noex ;
 	void dtor() noex ;
 	destruct tmz() {
 	    if (zname) dtor() ;
 	} ;
 } ; /* end struct (tmz) */
 
+typedef TMZ_FL		tmz_fl ;
+
 EXTERNC_begin
 
-extern int	tmz_init(tmz *) noex ;
-extern int	tmz_xstd(tmz *,cchar *,int) noex ;
-extern int	tmz_xmsg(tmz *,cchar *,int) noex ;
-extern int	tmz_xtouch(tmz *,cchar *,int) noex ;
-extern int	tmz_xtoucht(tmz *,cchar *,int) noex ;
-extern int	tmz_xstrdig(tmz *,cchar *,int) noex ;
-extern int	tmz_xlogz(tmz *,cchar *,int) noex ;
-extern int	tmz_xday(tmz *,cchar *,int) noex ;
-extern int	tmz_isset(tmz *) noex ;
-extern int	tmz_hasyear(tmz *) noex ;
-extern int	tmz_haszoff(tmz *) noex ;
-extern int	tmz_haszone(tmz *) noex ;
-extern int	tmz_setday(tmz *,int,int,int) noex ;
-extern int	tmz_setyear(tmz *,int) noex ;
-extern int	tmz_setzone(tmz *,cchar *,int) noex ;
-extern int	tmz_gettm(tmz *,TM *) noex ;
-extern int	tmz_getdst(tmz *) noex ;
-extern int	tmz_getzoff(tmz *) noex ;
+extern int	tmz_init	(tmz *) noex ;
+extern int	tmz_xstd	(tmz *,cchar *,int) noex ;
+extern int	tmz_xmsg	(tmz *,cchar *,int) noex ;
+extern int	tmz_xtouch	(tmz *,cchar *,int) noex ;
+extern int	tmz_xtoucht	(tmz *,cchar *,int) noex ;
+extern int	tmz_xstrdig	(tmz *,cchar *,int) noex ;
+extern int	tmz_xlogz	(tmz *,cchar *,int) noex ;
+extern int	tmz_xday	(tmz *,cchar *,int) noex ;
+extern int	tmz_isset	(tmz *) noex ;
+extern int	tmz_hasyear	(tmz *) noex ;
+extern int	tmz_haszoff	(tmz *) noex ;
+extern int	tmz_haszone	(tmz *) noex ;
+extern int	tmz_setday	(tmz *,int,int,int) noex ;
+extern int	tmz_setyear	(tmz *,int) noex ;
+extern int	tmz_setzone	(tmz *,cchar *,int) noex ;
+extern int	tmz_gettm	(tmz *,TM *) noex ;
+extern int	tmz_getdst	(tmz *) noex ;
+extern int	tmz_getzoff	(tmz *,short *) noex ;
+extern int	tmz_getzone	(tmz *,char *,int) noex ;
+extern int	tmz_fini	(tmz *) noex ;
 
 EXTERNC_end
 
