243daa5da82ed27a7e01a0b2e128ebe1ba5c183e
David A.D. Morano <david.a.morano@gmail.com>
2026-02-12 00:53:07 -0500

    update

--- src/libuc/char.h
+++ src/libuc/char.h
@@ -21,8 +21,32 @@
 
 /******************************************************************************
 
-	Name:
+	Group:
 	char
+
+	Names:
+	CHAR_ISALPHA
+	CHAR_ISALNUM
+	CHAR_ISDIGIT
+	CHAR_ISDIGEX
+	CHAR_ISOCTAL
+	CHAR_ISWHITE
+	CHAR_ISBLANK
+	CHAR_ISLC
+	CHAR_ISUC
+	CHAR_ISFC
+	CHAR_ISDIG
+	CHAR_ISOCT
+	CHAR_ISDEC
+	CHAR_ISHEX
+	CHAR_ISWHT
+	CHAR_ISSPACETAB
+	CHAR_DICTORDER
+	CHAR_TOBC
+	CHAR_TOLC
+	CHAR_TOUC
+	CHAR_TOFC
+	CHAR_TOVAL
 
 	Description:
 	This file (the header file) essentially supplies macros
@@ -60,12 +84,21 @@ extern const unsigned char	chardata_touc[] ;
 extern const unsigned char	chardata_tofc[] ;
 
 /* test character attribute routines */
-#define	CHAR_ISWHT(c)		char_iswhite(c)
+#define	CHAR_ISALPHA(c)		char_isalpha(c)
+#define	CHAR_ISALNUM(c)		char_isalnum(c)
+#define	CHAR_ISDIGIT(c)		char_isdigit(c)
+#define	CHAR_ISDIGEX(c)		char_isdigex(c)
+#define	CHAR_ISOCTAL(c)		char_isoctal(c)
 #define	CHAR_ISWHITE(c)		char_iswhite(c)
+#define	CHAR_ISBLANK(c)		char_isblank(c)
 #define	CHAR_ISLC(c)		char_islc(c)
 #define	CHAR_ISUC(c)		char_isuc(c)
 #define	CHAR_ISFC(c)		char_isfc(c)
 #define	CHAR_ISDIG(c)		char_isdig(c)
+#define	CHAR_ISOCT(c)		char_isoct(c)
+#define	CHAR_ISDEC(c)		char_isdec(c)
+#define	CHAR_ISHEX(c)		char_ishex(c)
+#define	CHAR_ISWHT(c)		char_iswht(c)
 #define	CHAR_ISSPACETAB(c)	(((c) == ' ') || ((c) == '\t'))
 
 /* dictionary-collating-ordinal */
@@ -80,29 +113,64 @@ extern const unsigned char	chardata_tofc[] ;
 
 EXTERNC_begin
 
+extern bool char_isalpha(int) noex ;
+extern bool char_isalnum(int) noex ;
+local inline bool char_isdigit(int ch) noex {
+	return ((ch >= '0') && (ch <= '9')) ;
+}
+local inline bool char_isdigex(int ch) noex {
+    	bool f = false ;
+	f = f || ((ch >= '0') && (ch <= '9')) ;
+	f = f || ((ch >= 'A') && (ch <= 'Z')) ;
+	f = f || ((ch >= 'a') && (ch <= 'z')) ;
+	return f ;
+}
+local inline bool char_isoctal(int ch) noex {
+	return ((ch >= '0') && (ch <= '7')) ;
+}
+local inline bool char_isblank(int ch) noex {
+	return ((ch == ' ') && (ch == '\t')) ;
+}
 extern bool char_iswhite(int) noex ;
 extern bool char_islc(int) noex ;
 extern bool char_isuc(int) noex ;
 extern bool char_isfc(int) noex ;
-static inline bool char_isdig(int ch) noex {
+
+local inline bool char_isdig(int ch) noex {
 	return ((ch >= '0') && (ch <= '9')) ;
 }
-static inline bool char_isspacetab(int ch) noex {
+local inline bool char_isoct(int ch) noex {
+	return ((ch >= '0') && (ch <= '7')) ;
+}
+local inline bool char_isdec(int ch) noex {
+	return ((ch >= '0') && (ch <= '9')) ;
+}
+local inline bool char_ishex(int ch) noex {
+    	bool f = false ;
+	f = f || ((ch >= '0') && (ch <= '9')) ;
+	f = f || ((ch >= 'A') && (ch <= 'Z')) ;
+	f = f || ((ch >= 'a') && (ch <= 'z')) ;
+	return f ;
+}
+local inline bool char_iswht(int ch) noex {
+    	return char_iswhite(ch) ;
+}
+local inline bool char_isspacetab(int ch) noex {
 	return CHAR_ISSPACETAB(ch) ;
 }
-static inline short char_dictorder(int ch) noex {
+local inline short char_dictorder(int ch) noex {
 	return chardata_dictorder[ch & UCHAR_MAX] ;
 }
-static inline uchar char_tobc(int ch) noex {
+local inline uchar char_tobc(int ch) noex {
 	return uchar(ch & UCHAR_MAX) ;
 }
-static inline uchar char_tolc(int ch) noex {
+local inline uchar char_tolc(int ch) noex {
 	return chardata_tolc[ch & UCHAR_MAX] ;
 }
-static inline uchar char_touc(int ch) noex {
+local inline uchar char_touc(int ch) noex {
 	return chardata_touc[ch & UCHAR_MAX] ;
 }
-static inline uchar char_tofc(int ch) noex {
+local inline uchar char_tofc(int ch) noex {
 	return chardata_tofc[ch & UCHAR_MAX] ;
 }
 extern int char_toval(int) noex ;
