55171e9a53d163405d01532dd43e79f44b3f3e55
David A.D. Morano <david.a.morano@gmail.com>
2026-02-12 00:54:22 -0500

    update

--- src/libuc/chrono/tmtime.cc
+++ src/libuc/chrono/tmtime.cc
@@ -54,13 +54,15 @@
 #include	<cstdlib>
 #include	<clanguage.h>
 #include	<usysbase.h>
-#include	<usysflag.h>
 #include	<usyscalls.h>
+#include	<usupport.h>		/* |geustime(3u)| */
 #include	<uclibmem.h>
 #include	<uctimeconv.h>
 #include	<bufsizevar.hh>
 #include	<strn.h>		/* |strnwcpy(3uc)| */
+#include	<strdcpy.h>
 #include	<strwcpy.h>
+#include	<sncpyx.h>
 #include	<altzone.h>		/* <- special for bad systems */
 #include	<localmisc.h>
 
@@ -91,23 +93,37 @@ import libutil ;			/* |memclear(3u)| */
 /* forward references */
 
 template<typename ... Args>
-local int tmtime_ctor(tmtime *op,Args ... args) noex {
+local int tmtime_zinit(tmtime *op,Args ... args) noex {
 	int		rs = SR_FAULT ;
 	if (op && (args && ...)) ylikely {
 	    rs = SR_OK ;
-	    if (op->zname == nullptr) ylikely {
+	    if (op->znbuf == nullptr) ylikely {
 	        if (char *a ; (rs = lm_zn(&a)) >= 0) ylikely {
-		    op->zname = a ;
+		    op->znbuf = a ;
 		    memclear(a,rs) ;
 		    a[rs] = '\0' ;
 	        } /* end if (memory-allocation) */
 	    } /* end if (NULL zname) */
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (tmtime_ctor) */
+} /* end subroutine (tmtime_zinit) */
+
+local int tmtime_zfini(tmtime *op) noex {
+	int		rs = SR_FAULT ;
+	int		rs1 ;
+	if (op) {
+	    rs = SR_OK ;
+	    if (op->znbuf) {
+		rs1 = lm_free(op->znbuf) ;
+		if (rs >= 0) rs = rs1 ;
+		op->znbuf = nullptr ;
+	    }
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (tmtime_zfini) */
 
 local int	tmtime_mktimer(tmtime *,int,time_t *) noex ;
+local int	getznlen() noex ;
 
 
 /* local variables */
@@ -119,6 +135,8 @@ constexpr bool		f_darwin = F_DARWIN ;
 
 /* exported variables */
 
+cint tmtime::znlen  = 	getznlen() ;
+
 
 /* exported subroutines */
 
@@ -126,62 +144,64 @@ int tmtime_ztime(tmtime *op,bool fz,time_t t) noex {
 	int		rs = SR_FAULT ;
 	if (op) ylikely {
 	    if (fz) {
-	        rs = tmtime_localtime(op,t) ;
+	        rs = tmtime_timelocal(op,t) ;
 	    } else {
-	        rs = tmtime_gmtime(op,t) ;
+	        rs = tmtime_timegm(op,t) ;
 	    }
 	} /* end if (non-null) */
 	return rs ;
 }
 /* end subroutine (tmtime_ztime) */
 
-int tmtime_gmtime(tmtime *op,time_t t) noex {
+int tmtime_timegm(tmtime *op,time_t t) noex {
 	int		rs ;
-	if ((rs = tmtime_ctor(op)) >= 0) ylikely {
-	    if (t == 0) t = time(nullptr) ;
-	    if (TM tmd ; (rs = uc_gmtime(&t,&tmd)) >= 0) ylikely {
+	if ((rs = tmtime_zinit(op)) >= 0) ylikely {
+	    if (t == 0) t = getustime ;
+	    if (TM tmd ; (rs = uc_timegm(&t,&tmd)) >= 0) ylikely {
 	        if ((rs = tmtime_insert(op,&tmd)) >= 0) ylikely {
-	            op->gmtoff = 0 ;
-	            rs = intconv(strwcpy(op->zname,"GMT",znlen) - op->zname) ;
-	        }
+		    cchar *p = strwcpy(op->znbuf,"GMT",znlen) ;
+	            op->gmtoff = 0 ; /* <- GMT */
+	            rs = intconv(p - op->znbuf) ;
+	        } /* end if (tmtime_insert) */
 	    } /* end if */
 	    if (rs < 0) {
 		op->dtor() ;
 	    }
-	} /* end if (tmtime_ctor) */
+	} /* end if (tmtime_zinit) */
 	return rs ;
 }
-/* end subroutine (tmtime_gmtime) */
+/* end subroutine (tmtime_timegm) */
 
-int tmtime_localtime(tmtime *op,time_t t) noex {
+int tmtime_timelocal(tmtime *op,time_t t) noex {
 	int		rs ;
-	if ((rs = tmtime_ctor(op)) >= 0) ylikely {
-	    if (t == 0) t = time(nullptr) ;
-	    if (TM tmd ; (rs = uc_localtime(&t,&tmd)) >= 0) ylikely {
+	if ((rs = tmtime_zinit(op)) >= 0) ylikely {
+	    if (t == 0) t = getustime ;
+	    if (TM tmd ; (rs = uc_timelocal(&t,&tmd)) >= 0) ylikely {
 	        rs = tmtime_insert(op,&tmd) ;
 	    }
 	    if (rs < 0) {
 		op->dtor() ;
 	    }
-	} /* end if (tmtime_ctor) */
+	} /* end if (tmtime_zinit) */
 	return rs ;
 }
-/* end subroutine (tmtime_localtime) */
+/* end subroutine (tmtime_timelocal) */
 
+/* tmtime <- tm */
 int tmtime_insert(tmtime *op,CTM *tmp) noex {
 	int		rs ;
-	if ((rs = tmtime_ctor(op,tmp)) >= 0) ylikely {
+	if ((rs = tmtime_zinit(op,tmp)) >= 0) ylikely {
 	    TM		tc = *tmp ;
-	    op->gmtoff = 0 ;
-	    op->sec = tmp->tm_sec ;
-	    op->min = tmp->tm_min ;
-	    op->hour = tmp->tm_hour ;
-	    op->mday = tmp->tm_mday ;
-	    op->mon = tmp->tm_mon ;
-	    op->year = tmp->tm_year ;
-	    op->wday = tmp->tm_wday ;
-	    op->yday = tmp->tm_yday ;
-	    op->isdst = tmp->tm_isdst ;
+	    op->gmtoff = -1 ;
+	    op->sec	= tmp->tm_sec ;
+	    op->min	= tmp->tm_min ;
+	    op->hour	= tmp->tm_hour ;
+	    op->mday	= tmp->tm_mday ;
+	    op->mon	= tmp->tm_mon ;
+	    op->year	= tmp->tm_year ;
+	    op->wday	= tmp->tm_wday ;
+	    op->yday	= tmp->tm_yday ;
+	    op->isdst	= tmp->tm_isdst ;
 	    if (tmp->tm_isdst < 0) {
 	        time_t	t ; /* dummy */
 	        rs = uc_mktime(&tc,&t) ;
@@ -196,36 +216,37 @@ int tmtime_insert(tmtime *op,CTM *tmp) noex {
 	            op->gmtoff = (f_isdst) ? altzone : timezone ;
 	            zp = (f_isdst) ? tzname[1] : tzname[0] ;
 	        } /* end if_constexpr (f_darwin) */
-	        rs = intconv(strwcpy(op->zname,zp,znlen) - op->zname) ;
+	        rs = intconv(strwcpy(op->znbuf,zp,znlen) - op->znbuf) ;
 	    } /* end if (getting zone-name) */
 	    if (rs < 0) {
 		op->dtor() ;
 	    }
-	} /* end if (tmtime_ctor) */
+	} /* end if (tmtime_zinit) */
 	return rs ;
 }
 /* end subroutine (tmtime_insert) */
 
+/* tmtime -> tm */
 int tmtime_extract(tmtime *op,TM *tmp) noex {
 	int		rs ;
-	if ((rs = tmtime_ctor(op,tmp)) >= 0) ylikely {
-	    tmp->tm_sec = op->sec ;
-	    tmp->tm_min = op->min ;
-	    tmp->tm_hour = op->hour ;
-	    tmp->tm_mday = op->mday ;
-	    tmp->tm_mon = op->mon ;
-	    tmp->tm_year = op->year ;
-	    tmp->tm_wday = op->wday ;
-	    tmp->tm_yday = op->yday ;
-	    tmp->tm_isdst = op->isdst ;
+	if ((rs = tmtime_zinit(op,tmp)) >= 0) ylikely {
+	    tmp->tm_sec		= op->sec ;
+	    tmp->tm_min		= op->min ;
+	    tmp->tm_hour	= op->hour ;
+	    tmp->tm_mday	= op->mday ;
+	    tmp->tm_mon		= op->mon ;
+	    tmp->tm_year	= op->year ;
+	    tmp->tm_wday	= op->wday ;
+	    tmp->tm_yday	= op->yday ;
+	    tmp->tm_isdst	= op->isdst ;
 	    if_constexpr (f_darwin) {
-	        tmp->tm_gmtoff = op->gmtoff ;
-	        tmp->tm_zone = op->zname ;
+	        tmp->tm_gmtoff = long(op->gmtoff) ;
+	        tmp->tm_zone = op->znbuf ;
 	    } /* end if_constexpr (f_darwin) */
 	    if (rs < 0) {
 		op->dtor() ;
 	    }
-	} /* end if (tmtime_ctor) */
+	} /* end if (tmtime_zinit) */
 	return rs ;
 }
 /* end subroutine (tmtime_extract) */
@@ -240,12 +261,34 @@ int tmtime_adjtime(tmtime *op,time_t *tp) noex {
 }
 /* end subroutine (tmtime_adjtime) */
 
+int tmtime_getzn(tmtime *op,char *rbuf,int rlen) noex {
+    	int		rs ;
+	if (op && rbuf) {
+    	    rs = SR_NOTOPEN ;
+	    if (op->znbuf) {
+	        rs = sncpy(rbuf,rlen,op->znbuf) ;
+	    }
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end subroutine (tmtime_getzn) */
+
+int tmtime_loadzn(tmtime *op,cchar *sp,int sl) noex {
+    	int		rs ;
+	if ((rs = tmtime_zinit(op,sp)) >= 0) ylikely {
+	    cchar *p = strdcpy1w(op->znbuf,op->znlen,sp,sl) ;
+	    rs = intconv(p - op->znbuf) ;
+	} /* end if (tmtime_zinit) */
+	return rs ;
+}
+/* end subroutine (tmtime_loadzn) */
+
 
 /* local subroutines */
 
 local int tmtime_mktimer(tmtime *op,int fadj,time_t *tp) noex {
 	int		rs ;
-	if ((rs = tmtime_ctor(op)) >= 0) ylikely {
+	if ((rs = tmtime_zinit(op)) >= 0) ylikely {
 	    time_t	t = 0 ;
 	    if (TM tmd ; (tmtime_extract(op,&tmd)) >= 0) ylikely {
 	        if ((rs = uc_mktime(&tmd,&t)) >= 0) ylikely {
@@ -270,21 +313,21 @@ local int tmtime_mktimer(tmtime *op,int fadj,time_t *tp) noex {
 	    if (tp) {
 	        *tp = (rs >= 0) ? t : 0 ;
 	    }
-	} /* end if (tmtime_ctor) */
+	} /* end if (tmtime_zinit) */
 	return rs ;
 }
 /* end subroutine (tmtime_mktimer) */
 
-int tmtime::ztime(bool fz,time_t t) noex {
-	return tmtime_ztime(this,fz,t) ;
+int tmtime::timex(bool fz,time_t t) noex {
+	return tmtime_timex(this,fz,t) ;
 }
 
-int tmtime::gmtime(time_t t) noex {
-	return tmtime_gmtime(this,t) ;
+int tmtime::timegm(time_t t) noex {
+	return tmtime_timegm(this,t) ;
 }
 
-int tmtime::localtime(time_t t) noex {
-	return tmtime_localtime(this,t) ;
+int tmtime::timelocal(time_t t) noex {
+	return tmtime_timelocal(this,t) ;
 }
 
 int tmtime::insert(CTM *tmp) noex {
@@ -303,18 +346,26 @@ int tmtime::adjtime(time_t *tp) noex {
 	return tmtime_adjtime(this,tp) ;
 }
 
+int tmtime::getzn(char *rbuf,int rlen) noex {
+	return tmtime_getzn(this,rbuf,rlen) ;
+}
+
+int tmtime::loadzn(cchar *sp,int sl) noex {
+	return tmtime_loadzn(this,sp,sl) ;
+}
+
 void tmtime::dtor() noex {
-    	int		rs = SR_OK ;
-	int		rs1 ;
-    	if (zname) ylikely {
-	    rs1 = lm_free(zname) ;
-	    if (rs >= 0) rs = rs1 ;
-	    zname = nullptr ;
-	}
-	if (rs < 0) {
+	if (cint rs = tmtime_zfini(this) ; rs < 0) {
 	    ulogerror("tmtime",rs,"dtor-free") ;
 	}
-}
-/* end method (tmtime::dtor) */
+} /* end method (tmtime::dtor) */
+
+local int getznlen() noex {
+    	int		rs ;
+	if ((rs = znlen) < 0) {
+	    ulogerror("tmtime",rs,"getznlen") ;
+	} /* end if (znlen) */
+	return rs ;
+} /* end subroutine (getznlen) */
 
 
