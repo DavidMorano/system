ab6a667868dbd66eede78f39c7f71d1c1b35b5c8
David A.D. Morano <david.a.morano@gmail.com>
2026-02-14 00:58:23 -0500

    restore

---
+++ src/libuc/fileobj/fileobj.h
@@ -0,0 +1,32 @@
+/* fileobj HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* read stuff from a file */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FILEOBJ_INCLUDE
+#define	FILEOBJ_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	<filegrp.h>
+#include	<filelinker.h>
+
+
+#endif /* FILEOBJ_INCLUDE */
+
+
