403e8b8f976a953db47fe283ece9d511daa97a5f
David A.D. Morano <david.a.morano@gmail.com>
2026-02-14 01:02:20 -0500

    update

--- src/libuc/getbufsize.h
+++ src/libuc/getbufsize.h
@@ -34,12 +34,8 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-
-#include	<bufsizenames.h>
+#include	<usysbase.h>
+#include	<bufsizes.h>
 
 
 #define	GETBUFSIZE_CONF		"/etc/default/bufsize"
