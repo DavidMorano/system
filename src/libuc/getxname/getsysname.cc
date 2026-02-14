4294759c7740f89b0e877b47dbab300b74db2eeb
David A.D. Morano <david.a.morano@gmail.com>
2026-02-14 01:05:33 -0500

    update

--- src/libuc/getxname/getsysname.cc
+++ src/libuc/getxname/getsysname.cc
@@ -39,7 +39,8 @@
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>		/* |getenv(3c)| */
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<uinfo.h>
 #include	<sfx.h>
 #include	<snwcpy.h>
