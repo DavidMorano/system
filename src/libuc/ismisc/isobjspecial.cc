ab0d9e7c50c2b0558a8b6b519a3d091b77b712c3
David A.D. Morano <david.a.morano@gmail.com>
2026-02-15 01:51:00 -0500

    update

--- src/libuc/ismisc/isobjspecial.cc
+++ src/libuc/ismisc/isobjspecial.cc
@@ -11,9 +11,15 @@
 	= 1998-07-01, David A­D­ Morano
 	This code was originally written.
 
+	= 2020-10-22, David A-D- Morano
+	I tried to make the constant array of RTLD_{xx} values (see
+	the code below) 'constexpr' but C++ does not allow it since
+	it does not allow casting integers to pointers (even to
+	pointers to void) in a 'constexpr' context.
+
 */
 
-/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998,2020 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
@@ -35,16 +41,23 @@
 	FALSE		shared-object is regular
 
 	Notes:
-	MacOS (Darwin) and possibly other OSes do not have the handle
-	of RTLD_PROBE.
+	1. MacOS (Darwin) and possibly other OSes do not have the
+	handle of RTLD_PROBE.
+
+	2. In the data array below (w/ variable name 'objs') I note
+	that the constant-data array cannot be make 'constexpr'
+	because f*ck-*ass C++ does not let integers to be cast to
+	a pointer (even a pointer-to-void) in a 'onstexpr' variable
+	definition.  So the array defiition has to remain simply
+	'static void *const []'.  The 'RTLD_{xx}' preprocessor defines
+	are defined by the system implementation -- not by me --
+	so they cannot be changed (defined differently).  So I have
+	to work with what is given to me.
 
 *******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<sys/param.h>
-#include	<unistd.h>
-#include	<dlfcn.h>
+#include	<dlfcn.h>		/* |RTLD_{xxx}| */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
@@ -61,7 +74,7 @@
 /* local defines */
 
 #ifndef	RTLD_PROBE
-#define	RTLD_PROBE		voidp(1)	/* real vals are neg */
+#define	RTLD_PROBE		voidp(1)	/* real vals are negative */
 #endif
 
 
@@ -79,13 +92,14 @@
 
 /* local variables */
 
-static const void	*objs[] = {
-	RTLD_DEFAULT,
-	RTLD_NEXT,
-	RTLD_SELF,
-	RTLD_PROBE,
+/* <- cannot do 'constexpr' cast of integer to ptr (f*ck up of C++) */
+static void	*const objs[] = { 
+	RTLD_DEFAULT,			/* <- integer cast to pointer-to-void */
+	RTLD_NEXT,			/* <- integer cast to pointer-to-void */
+	RTLD_SELF,			/* <- integer cast to pointer-to-void */
+	RTLD_PROBE,			/* <- integer cast to pointer-to-void */
 	nullptr
-} ;
+} ; /* end array (objs) */
 
 
 /* exported variables */
