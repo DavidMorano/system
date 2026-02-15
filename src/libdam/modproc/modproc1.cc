4129832616f209b58294acb1b1bc8f851b934bce
David A.D. Morano <david.a.morano@gmail.com>
2026-02-15 01:14:43 -0500

    update

--- src/libdam/modproc/modproc1.cc
+++ src/libdam/modproc/modproc1.cc
@@ -54,7 +54,6 @@ module ;
 #include	<climits>		/* |INT_MAX| */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<string>
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
@@ -62,16 +61,18 @@ module ;
 #include	<usysrets.h>
 #include	<usyscalls.h>
 #include	<strnul.hh>
-#include	<langproc.h>
+#include	<langparse.h>
 #include	<ascii.h>
+#include	<strn.h>		/* |strnchr(3uc)| */
 #include	<six.h>			/* |sispanwht(3uc)| */
-#include	<hasx.h>		/* |hasmodname(3uc)| */
 #include	<strmgr.h>
 #include	<strop.h>
 #include	<strwcmp.h>
+#include	<ascii.h>		/* |CH_NL| */
 #include	<mkchar.h>
-#include	<ischarx.h>		/* |isalnumlatin(3cu)| */
+#include	<hasx.h>		/* |headlead(3uc)| */
 #include	<localmisc.h>
+#include	<debprintf.h>		/* |DEBPRINTF(3u)| */
 
 #pragma		GCC dependency		"mod/modproc.ccm"
 #pragma		GCC dependency		"mod/libutil.ccm"
@@ -86,11 +87,14 @@ import ulibvals ;			/* |ulibval(3u)| + |pagesz| */
 
 #define	ALTLNMULT	5		/* multplier to pagesz for alt-line */
 
+#define CDEBPR(FMT, ...) \
+	if_constexpr (f_debug) \
+    	debprintf(__func__, FMT __VA_OPT__(,) __VA_ARGS__)
+
 
 /* imported namespaces */
 
-using std::string ;			/* type */
-using libu::umem ;			/* variable */
+using libu::um ;			/* variable */
 
 
 /* local typedefs */
@@ -108,19 +112,22 @@ namespace {
     struct modmgr {
 	vecstr		*vop ;
 	char		*lbuf{} ;
-	langproc	proc ;
+	short		*sbuf{} ;
+	langparse	procer ;
 	strmgr		lnmgr ;
 	int		llen = 0 ;
+	int		slen = 0 ;
 	int		li = 0 ;
-	bool		lnalt = false ;
+	bool		flnalt = false ;
 	modmgr(vecstr *p) noex : vop(p) { } ;
 	int operator () (cchar *) noex ;
 	int start() noex ;
 	int finish() noex ;
 	int fileload(cchar *) noex ;
 	int fileproc() noex ;
+	int filter(char *,int,int) noex ;
 	int liner(int,size_t) noex ;
-	int checkln(int,cchar *,int) noex ;
+	int checker(int,cchar *,int) noex ;
 	int procln(int,cchar *,int) noex ;
     private:
 	int lnbegin() noex ;
@@ -128,19 +135,16 @@ namespace {
 	int lnready() noex ;
 	int lnadd(cchar *,int) noex ;
 	int lncheck(int) noex ;
+	int swbegin() noex ;
+	int swend() noex ;
     } ; /* end struct (modmgr) */
 } /* end namespace */
 
 
 /* forward references */
 
-static bool haspound(cchar *sp,int µsl) noex {
-    	bool		f = false ;
-    	if (int sl ; (sl = getlenstr(sp,µsl)) > 0) {
-    	    cint	si = sispanwht(sp,sl) ;
-	    f = (si >= 0) && (si < sl) && (sp[si] == '#') ;
-	} /* end if (getlenstr) */
-    	return f ;
+local bool haspound(cchar *sp,int sl) noex {
+    	return haslead(sp,sl,'#') ;
 } /* end subroutine (haspound) */
 
 
@@ -156,15 +160,14 @@ cchar			istr[] = "import" ;
 
 int modprocload(vecstr *op,cchar *fname) noex {
     	int		rs = SR_FAULT ;
-	if_constexpr (f_debug) debprintf(__func__,"ent fn=%s\n",fname) ;
+	CDEBPR("ent fn=%s\n",fname) ;
 	if (op && fname) ylikely {
     	    modmgr	mgr(op) ;
 	    rs = mgr(fname) ;
 	} /* end if (non-null) */
-	if_constexpr (f_debug) debprintf(__func__,"ret rs=%d\n",rs) ;
+	CDEBPR("ret rs=%d\n",rs) ;
 	return rs ;
-}
-/* end subroutine (modprocload) */
+} /* end subroutine (modprocload) */
 
 
 /* local subroutines */
@@ -173,28 +176,43 @@ int modmgr::operator () (cchar *fname) noex {
     	int		rs ;
 	int		rs1 ;
 	int		rv = 0 ; /* return-value */
+	CDEBPR("ent fn=%s\n",fname) ;
 	if ((rs = start()) >= 0) {
-	    if ((rs = proc.start) >= 0) {
+	    if ((rs = procer.start) >= 0) {
 	        if ((rs = fileload(fname)) >= 0) {
 		    rs = fileproc() ;
 		    rv = rs ;
+	            CDEBPR("fileproc() rs=%d\n",rs) ;
 	        } /* end if (fileload) */
-	        rs1 = proc.finish ;
+	        rs1 = procer.finish ;
 	        if (rs >= 0) rs = rs1 ;
+	        CDEBPR("procer.finish() rs=%d\n",rs) ;
 	    } /* end if (langproc) */
 	    rs1 = finish() ;
 	    if (rs >= 0) rs = rs1 ;
+	    CDEBPR("finish() rs=%d\n",rs) ;
 	} /* end if (start-finish) */
+	CDEBPR("ret rs=%d rc=%d\n",rs,rv) ;
     	return (rs >= 0) ? rv : rs ;
 } /* end method (modmgr::operator) */
 
 int modmgr::start() noex {
-    	return SR_OK ;
-}
+	return swbegin() ;
+} /* end method (modmgr::start) */
 
 int modmgr::finish() noex {
-    	return lnend() ;
-}
+    	int		rs = SR_OK ;
+	int		rs1 ;
+	if (lbuf) {
+    	    rs1 = lnend() ;
+	    if (rs >= 0) rs = rs1 ;
+	}
+	if (sbuf) {
+	    rs1 = swend() ;
+	    if (rs >= 0) rs = rs1 ;
+	}
+	return rs ;
+} /* end method (modmgr::finish) */
 
 int modmgr::fileload(cchar *fname) noex {
 	int		rs = SR_FAULT ;
@@ -232,9 +250,7 @@ int modmgr::liner(int fd,csize ms) noex {
 	int		rs ;
 	int		rs1 ;
 	int		nmods = 0 ; /* return-value */
-	if_constexpr (f_debug) {
-	    debprintf(__func__,"ent\n") ;
-	}
+	CDEBPR("ent\n") ;
 	if (void *md ; (rs = u_mmapbegin(np,ms,mp,mf,fd,0z,&md)) >= 0) {
 	    cint	cmd = MADV_SEQUENTIAL ;
 	    if ((rs = u_madvise(md,ms,cmd)) >= 0) ylikely {
@@ -252,13 +268,13 @@ int modmgr::liner(int fd,csize ms) noex {
 			    if (sp[sl - 1] == CH_BSLASH) {
 			        rs = lnadd(sp,(sl - 1)) ;
 			    } else {
-			        if (lnalt) {
+			        if (flnalt) {
 			            if ((rs = lnadd(sp,sl)) >= 0) {
 				        rs = lncheck(ln++) ;
 		                        nmods += rs ;
 				    }
 			        } else {
-		                    rs = checkln(ln++,sp,sl) ;
+		                    rs = checker(ln++,sp,sl) ;
 		                    nmods += rs ;
 			        }
 			    } /* end if (continuation or regular) */
@@ -270,75 +286,91 @@ int modmgr::liner(int fd,csize ms) noex {
 		} /* end for */
 		if ((rs >= 0) && (ll > 0)) {
 		    cint	sl = intconv(ll) ;
-		    rs = checkln(ln++,lp,sl) ;
+		    rs = checker(ln++,lp,sl) ;
 		    nmods += rs ;
 		} /* end if (trailing line */
 	    } /* end if (memory-advise) */
 	    rs1 = u_mmapend(md,ms) ;
 	    if (rs >= 0) rs = rs1 ;
 	} /* end if (map-file) */
-	if_constexpr (f_debug) {
-	    debprintf(__func__,"ret rs=%d c=%d\n",rs,nmods) ;
-	}
+	CDEBPR("ret rs=%d c=%d\n",rs,nmods) ;
 	return (rs >= 0) ? nmods : rs ;
 } /* end method (modmgr::liner) */
 
-int modmgr::checkln(int ln,cchar *lp,int ll) noex {
+int modmgr::checker(int ln,cchar *lp,int ll) noex {
     	int		rs = SR_OK ;
 	if_constexpr (f_debug) {
 	    strnul s(lp,ll) ;
-	    debprintf(__func__,"ent ln=%d l=>%s<\n",ln,ccp(s)) ;
+	    CDEBPR("ent ln=%d l=>%s<\n",ln,ccp(s)) ;
 	}
 	if ((ll > 0) && (! haspound(lp,ll))) {
 	    if ((ll > 0) && (lp[ll - 1] == '\r')) {
 		ll -= 1 ;
 	    }
 	    if (ll > 0) {
-	        rs = proc.proc(ln,lp,ll) ;
-	        debprintf(__func__,"proc.proc() ret rs=%d\n",rs) ;
+	        rs = procer.load(lp,ll) ;
+	        CDEBPR("procer.proc() rs=%d\n",rs) ;
 	    }
 	} /* end if (not preprocessor directive) */
-	debprintf(__func__,"ret rs=%d\n",rs) ;
+	CDEBPR("ret rs=%d\n",rs) ;
     	return rs ; 
-} /* end method (modmgr::checkln) */
+} /* end method (modmgr::checker) */
 
 int modmgr::fileproc() noex {
-    	cint		rsn = SR_NOTFOUND ;
+    	cnullptr	np{} ;
+    	cint		chnl = CH_NL ;
     	int		rs ;
-	int		rs1{} ;
+	int		rs1 ;
 	int		rv = 0 ; /* return-value */
-	debprintf(__func__,"ent\n") ;
-	if (langproc_cur cur ; (rs = proc.curbegin(&cur)) >= 0) {
-	    int		ln = 1 ;
-	    debprintf(__func__,"while-cur\n") ;
-	    for (cchar *lp ; (rs1 = proc.curenum(&cur,&lp)) >= 0 ; ) {
-		cint ll = rs1 ;
-		if (lp) {
-		    {
-			strnul s(lp,ll) ;
-		        debprintf(__func__,"enum s=>%s<\n",ccp(s)) ;
-		    }
-		    rs = procln(ln++,lp,ll) ;
-		    rv += rs ;
-		}
-		if (rs < 0) break ;
-	    } /* end for (langproc_curenum) */
-	    debprintf(__func__,"while-cur rs=%d rs1=%d\n",rs,rs1) ;
-	    if ((rs >= 0) && (rs1 != rsn)) rs = rs1 ;
-	    rs1 = proc.curend(&cur) ;
+	CDEBPR("ent\n") ;
+	if (char *tbuf ; (rs = um.mall((slen + 1),&tbuf)) >= 0) {
+	    cint tlen = rs ;
+	    for (int ln = 1 ; (rs = procer.remread(sbuf,slen)) > 0 ; ) {
+		if ((rs = filter(tbuf,tlen,rs)) >= 0) {
+		    int		ll = rs ;
+		    cchar	*lp = tbuf ;
+		    for (cchar *tp ; (tp = strnchr(lp,ll,chnl)) != np ; ) {
+			cint tl = intconv(tp - lp) ;
+			CDEBPR("bef tl=%d\n",tl) ;
+	                rs = procln(ln++,lp,tl) ;
+	                rv += rs ;
+	                if (rs < 0) break ;
+			ll -= intconv((tp + 1) - lp) ;
+			lp = (tp + 1) ;
+		    } /* end for */
+		    if ((rs >= 0) && (ll > 0)) {
+			CDEBPR("aft ll=%d\n",ll) ;
+	                rs = procln(ln++,lp,ll) ;
+	                rv += rs ;
+		    } /* end if (reminder) */
+		} /* end if (filter) */
+	    } /* end for */
+	    rs1 = um.free(tbuf) ;
 	    if (rs >= 0) rs = rs1 ;
-	} /* end if (langproc_cur) */
-	debprintf(__func__,"ret rs=%d rv=%d\n",rs,rv) ;
+	} /* end if (m-a-f) */
+	CDEBPR("ret rs=%d rv=%d\n",rs,rv) ;
     	return (rs >= 0) ? rv : rs ;
 } /* end method (modmgr::fileproc) */
+
+int modmgr::filter(char *tbuf,int tlen,int sl) noex {
+    	cauto		&lm = langparsem ;
+    	int		rs = SR_OK ;
+	int		rl = 0 ;
+	for (int i = 0 ; (i < sl) && (i < tlen) ; i += 1) {
+	    if (cshort cw = sbuf[i] ; (! (cw & lm.comment))) {
+		tbuf[rl++] = char(cw) ;
+	    }
+	} /* end for */
+	return (rs >= 0) ? rl : rs ;
+} /* end method (modmgr::filter) */
 
 int modmgr::procln(int ln,cchar *lp,int ll) noex {
     	int		rs ;
 	int		rs1 ;
 	int		c = 0 ; /* return-value -- number of names found */
-	if_constexpr (f_debug) {
+	{
 	    strnul st(lp,ll) ;
-	    debprintf(__func__,"ent ln=>%s<\n",ccp(st)) ;
+	    CDEBPR("ent ln=>%s<\n",ccp(st)) ;
 	}
 	(void) ln ;
     	if (strop s ; (rs = s.start(lp,ll)) >= 0) ylikely {
@@ -346,16 +378,16 @@ int modmgr::procln(int ln,cchar *lp,int ll) noex {
 	    if (int il ; (il = s.fieldwht(&ip)) > 0) {
 		if_constexpr (f_debug) {
 	    	    strnul st(ip,il) ;
-	    	    debprintf(__func__,"piece=>%s<\n",ccp(st)) ;
+	    	    CDEBPR("piece=>%s<\n",ccp(st)) ;
 		}
 		if (strwcmp(istr,ip,il) == 0) {
 		    cint	ch_s = CH_SEMI ;
 		    cchar	*mp ;
-		    if_constexpr (f_debug) debprintf(__func__,"cmp\n") ;
+		    CDEBPR("cmp\n") ;
 		    if (int ml ; (ml = s.fieldchr(ch_s,&mp)) > 0) {
 			if_constexpr (f_debug) {
 	    	    	    strnul st(mp,ml) ;
-	    	    	    debprintf(__func__,"m=>%s<\n",ccp(st)) ;
+	    	    	    CDEBPR("m=>%s<\n",ccp(st)) ;
 			}
 			if (hasmodname(mp,ml)) {
 			    rs = vop->adduniq(mp,ml) ;
@@ -367,7 +399,7 @@ int modmgr::procln(int ln,cchar *lp,int ll) noex {
 	    rs1 = s.finish ;
 	    if (rs >= 0) rs = rs1 ;
 	} /* end if (strop) */
-	if_constexpr (f_debug) debprintf(__func__,"ret rs=%d c=%d\n",rs,c) ;
+	CDEBPR("ret rs=%d c=%d\n",rs,c) ;
 	return (rs >= 0) ? c : rs ;
 } /* end method (modmgr::procln) */
 
@@ -376,12 +408,12 @@ int modmgr::lnbegin() noex {
 	if (lbuf == nullptr) {
 	    if ((rs = ulibval.pagesz) >= 0) {
 		cint	sz = (ALTLNMULT * rs) ;
-		if (char *bp ; (rs = umem.mall(sz,&bp)) >= 0) {
+		if (char *bp ; (rs = um.mall(sz,&bp)) >= 0) {
 		    lbuf = bp ;
 		    llen = sz ;
 		    rs = lnmgr.start(lbuf,llen) ;
 		    if (rs < 0) {
-			umem.free(lbuf) ;
+			um.free(lbuf) ;
 			lbuf = nullptr ;
 			llen = 0 ;
 		    } /* end if (error) */
@@ -400,7 +432,7 @@ int modmgr::lnend() noex {
 		if (rs >= 0) rs = rs1 ;
 	    }
 	    {
-	        rs1 = umem.free(lbuf) ;
+	        rs1 = um.free(lbuf) ;
 	        if (rs >= 0) rs = rs1 ;
 	        lbuf = nullptr ;
 	        llen = 0 ;
@@ -421,8 +453,8 @@ int modmgr::lnready() noex {
 int modmgr::lnadd(cchar *sp,int sl) noex {
     	int		rs ;
 	if ((rs = lnready()) >= 0) {
-	    if ((rs = lnmgr.str(sp,sl)) >= 0) {
-	        lnalt = true ;
+	    if ((rs = lnmgr.str(sp,sl)) > 0) {
+	        flnalt = true ;
 	    } /* end if (lnadd) */
 	} /* end if (lnready) */
 	return rs ;
@@ -430,15 +462,42 @@ int modmgr::lnadd(cchar *sp,int sl) noex {
 
 int modmgr::lncheck(int ln) noex {
     	int		rs ;
-	if (cchar *cp ; (rs = lnmgr.get(&cp)) >= 0) {
-	    cint cl = rs ;
-	    if ((rs = checkln(ln,cp,cl)) >= 0) {
+	if (cchar *cp ; (rs = lnmgr.get(&cp)) > 0) {
+	    if ((rs = checker(ln,cp,rs)) >= 0) {
 	        if ((rs = lnmgr.reset) >= 0) {
-	            lnalt = false ;
+	            flnalt = false ;
 	        }
-	    } /* endif (checkln) */
+	    } /* endif (checker) */
 	} /* end if (lnmgr.get) */
 	return rs ;
 } /* end method (modmgr::lncheck) */
+
+int modmgr::swbegin() noex {
+    	int		rs = SR_OK ;
+	if (sbuf == nullptr) {
+	    if ((rs = ulibval.pagesz) >= 0) {
+		cint	sz = (ALTLNMULT * rs) * szof(short) ;
+		if (void *bp ; (rs = um.mall(sz,&bp)) >= 0) {
+		    sbuf = shortp(bp) ;
+		    slen = (ALTLNMULT * rs) ;
+		} /* end if (memory-allocation) */
+	    } /* end if (ulibvals) */
+	} /* end if (required) */
+	return rs ;
+} /* end method (modmgr::swbegin) */
+
+int modmgr::swend() noex {
+    	int		rs = SR_OK ;
+	int		rs1 ;
+	if (sbuf) {
+	    {
+	        rs1 = um.free(sbuf) ;
+	        if (rs >= 0) rs = rs1 ;
+	        sbuf = nullptr ;
+	        slen = 0 ;
+	    }
+	} /* end if (was initialized) */
+	return rs ;
+} /* end method (modmgr::swend) */
 
 
