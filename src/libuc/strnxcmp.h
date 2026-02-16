765aefa027cf75b7a09202a1dbb5b8c4aa36d34a
David A.D. Morano <david.a.morano@gmail.com>
2026-02-16 00:11:52 -0500

    update

--- src/libuc/strnxcmp.h
+++ src/libuc/strnxcmp.h
@@ -8,7 +8,7 @@
 
 /* revision history:
 
-	= 1998-11-01, David A­D­ Morano
+	= 1998-09-10, David A­D­ Morano
 	This subroutine was written for Rightcore Network Services.
 
 */
@@ -22,17 +22,19 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<string.h>		/* |strncmp(3c)| for inline below */
 #include	<clanguage.h>
-#include	<usysbase.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
 
 
 EXTERNC_begin
 
-extern int strnfoldcmp(cchar *,cchar *,int) noex ;
-extern int strnkeycmp(cchar *,cchar *,int) noex ;
-extern int strnvalcmp(cchar *,cchar *,int) noex ;
-extern int strnleadcmp(cchar *,cchar *,int) noex ;
-extern int strnncmp(cchar *,int,cchar *,int) noex ;
-extern int strnndictcmp(cchar *,int,cchar *,int) noex ;
+extern int strnkeycmp	(cchar *,cchar *,int) noex ;
+extern int strnvalcmp	(cchar *,cchar *,int) noex ;
+extern int strnfoldcmp	(cchar *,cchar *,int) noex ;
+extern int strnleadcmp	(cchar *,cchar *,int) noex ;
+extern int strnncmp	(cchar *,int,cchar *,int) noex ;
+extern int strnndictcmp	(cchar *,int,cchar *,int) noex ;
 
 static inline int strnbasecmp(cchar *s1,cchar *s2,int slen) noex {
 	return strncmp(s1,s2,slen) ;
