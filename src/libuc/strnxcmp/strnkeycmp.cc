66afe4c9588d334af68ed6fad47ea1c438bae3c9
David A.D. Morano <david.a.morano@gmail.com>
2026-02-16 00:12:10 -0500

    update

--- src/libuc/strnxcmp/strnkeycmp.cc
+++ src/libuc/strnxcmp/strnkeycmp.cc
@@ -25,7 +25,8 @@
 	that looks like a SHELL variable assignment.  For example,
 	consider the following compound string:
 		A=the_dog_house
-	The 'A' would be the key, and the part 'the_dog_house' is the value.
+	The 'A' would be the key, and the part 'the_dog_house' is
+	the value.
 
 	Synopsis:
 	int strnkeycmp(cchar *e1p,cchar *e2p,int n) noex
@@ -45,16 +46,30 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
+#include	<algorithm>		/* |min(3c++)| + |max(3c++)| */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
 #include	<usysdefs.h>
+#include	<mkchar.h>
 #include	<localmisc.h>
 
 #include	"strnxcmp.h"
 
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |lenstr(3u)| */
 
 /* local defines */
+
+
+/* imported namespaces */
+
+using std::min ;			/* subroutine */
+using std::max ;			/* subroutine */
+
+
+/* local typedefs */
 
 
 /* external subroutines */
@@ -68,10 +83,6 @@
 
 /* forward references */
 
-static inline bool keyend(int ch) noex {
-	return ((ch == '\0') || (ch == '=')) ;
-}
-
 
 /* local variables */
 
@@ -81,51 +92,89 @@ static inline bool keyend(int ch) noex {
 
 /* exported subroutines */
 
+local inline bool isend(int ch) noex {
+    	return (ch == '=') || (ch == '\0') ;
+}
+
 int strnkeycmp(cchar *e1p,cchar *e2p,int n) noex {
 	int		rc = 0 ;
-	if (e1p || e2p) {
-	    rc = 1 ;
-	    if (e1p) {
-		rc = -1 ;
-		if (e2p) {
-		    rc = 0 ;
-                    if (n >= 0) {
-                        while (*e1p && *e2p && (n > 0)) {
-                            if ((*e1p == '=') || (*e2p == '=')) break ;
-                            rc = *e1p - *e2p ;
-                            if (rc != 0) break ;
-                            e1p += 1 ;
-                            e2p += 1 ;
-                            n -= 1 ;
-                        } /* end while */
-                    } else {
-                        while (*e1p && *e2p) {
-                            if ((*e1p == '=') || (*e2p == '=')) break ;
-                            rc = *e1p - *e2p ;
-                            if (rc != 0) break ;
-                            e1p += 1 ;
-                            e2p += 1 ;
-                        } /* end while */
-                    } /* end if */
-                    if (n != 0) {
-                        rc = 0 ;
-                        if (*e1p != *e2p) {
-                            if ((rc == 0) && (*e1p == '=')) {
-                                rc = (*e2p == '\0') ? 0 : -1 ;
-                            }
-                            if ((rc == 0) && (*e2p == '=')) {
-                                rc = (*e1p == '\0') ? 0 : 1 ;
-                            }
-                            if (rc == 0) {
-				rc = (*e1p - *e2p) ;
-			    }
-                        }
-                    } /* end if (still non-zero) */
-		}
-	    }
+	if (e1p && e2p) {
+	    if (n < 0) {
+	        cint n1 = lenstr(e1p) ;
+	        cint n2 = lenstr(e2p) ;
+		n = min(n1,n2) ;
+	    } /* end if */
+	    {
+	        for (int i = 0 ; (i < n) && *e1p && *e2p ; i += 1) {
+	            if ((*e1p == '=') || (*e2p == '=')) break ;
+	            if (*e1p != *e2p) break ;
+	            e1p += 1 ;
+	            e2p += 1 ;
+	        } /* end for */
+	        if ((*e1p != *e2p) && n) {
+		    cint ch1 = mkchar(*e1p) ;
+		    cint ch2 = mkchar(*e2p) ;
+	            if (isend(ch1)) {
+		        rc = isend(ch2) ? 0 : (- ch2) ;
+	            } else if (isend(ch2)) {
+		        rc = (+ ch1) ;
+	            } else {
+		        rc = ch1 - ch2 ;
+		    }
+	        } /* end if (resolution) */
+	    } /* end block */
+	} /* end if (non-null) */
+	return rc ;
+}
+/* end subroutine (strnkeycmp) */
+
+#ifdef	OBSOLETE
+
+local inline int chdiff(cchar *c1p,cchar *c2p) noex {
+    	cint ch1 = mkchar(*c1p) ;
+    	cint ch2 = mkchar(*c2p) ;
+	return (ch1 - ch2) ;
+} /* end subroutine (chdiff) */
+
+int strnkeycmp(cchar *e1p,cchar *e2p,int n) noex {
+	int		rc = 0 ;
+	if (e1p && e2p) {
+            if (n >= 0) {
+                while (*e1p && *e2p && (n > 0)) {
+                    if ((*e1p == '=') || (*e2p == '=')) break ;
+                    rc = chdiff(e1p,e2p) ;
+                    if (rc != 0) break ;
+                    e1p += 1 ;
+                    e2p += 1 ;
+                    n -= 1 ;
+                } /* end while */
+            } else {
+                while (*e1p && *e2p) {
+                    if ((*e1p == '=') || (*e2p == '=')) break ;
+                    rc = chdiff(e1p,e2p) ;
+                    if (rc != 0) break ;
+                    e1p += 1 ;
+                    e2p += 1 ;
+                } /* end while */
+            } /* end if */
+            if ((rc == 0) && (n != 0)) {
+                if (*e1p != *e2p) {
+                    if ((rc == 0) && (*e1p == '=')) {
+                        rc = (*e2p == '\0') ? 0 : -1 ;
+                    }
+                    if ((rc == 0) && (*e2p == '=')) {
+                        rc = (*e1p == '\0') ? 0 : 1 ;
+                    }
+                    if (rc == 0) {
+                        rc = chdiff(e1p,e2p) ;
+                    }
+                } /* end if */
+            } /* end if (still non-zero) */
  	} /* end if (at least one non-null) */
 	return rc ;
 }
 /* end subroutine (strnkeycmp) */
+
+#endif /* OBSOLETE */
 
 
