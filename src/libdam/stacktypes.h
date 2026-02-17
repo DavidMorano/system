03f28cb1849a59de9f9c3ce44cfe4e413ec8580e
David A.D. Morano <david.a.morano@gmail.com>
2026-02-17 00:45:14 -0500

    restore

---
+++ src/libdam/stacktypes.h
@@ -0,0 +1,36 @@
+/* stacktypes HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get a stack size (of some type) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STACKTYPES_INCLUDE
+#define	STACKTYPES_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+enum stacktypes {
+	stacktype_thread,
+	stacktype_default,
+	stacktype_overlast
+} ; /* end enum */
+
+
+#endif /* STACKTYPES_INCLUDE */
+
+
