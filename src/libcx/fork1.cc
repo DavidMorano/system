a852c96a52f555ea72a297d9ef49a7e5c5d63a09
David A.D. Morano <david.a.morano@gmail.com>
2026-02-17 00:58:28 -0500

    update

--- src/libcx/fork1.cc
+++ src/libcx/fork1.cc
@@ -1,5 +1,6 @@
 /* fork1 SUPPORT */
-/* lang=C++20 */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* a |fork1(2)|if you do not already have it! */
 /* version %I% last-modified %G% */
@@ -14,10 +15,18 @@
 
 /* Copyright © 1989 David A-D- Morano.  All rights reserved. */
 
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<unistd.h>
-#include	<utypedefs.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
 #include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+#include	<localmisc.h>
 
 
 /* exported subroutines */
