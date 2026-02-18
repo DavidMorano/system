b6eab5f5f61498278fffc3c0de22f5dea57c3796
David A.D. Morano <david.a.morano@gmail.com>
2026-02-18 02:38:56 -0500

    restore

---
+++ src/libdam/getngroups.h
@@ -0,0 +1,44 @@
+/* getngroups HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get the maximum number of supplemetary groups allowed per process */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	GETNGROUPS_INCLUDE
+#define	GETNGROUPS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#ifdef	__cplusplus
+
+extern "C" {
+    extern int getngroups(int) noex ;
+    extern int getngroupsx(int = 0) noex ;
+}
+
+#else /* __cplusplus */
+
+extern int getngroups() noex ;
+extern int getngroupsx(int) noex ;
+
+#endif /* __cplusplus */
+
+
+#endif /* GETNGROUPS_INCLUDE */
+
+
