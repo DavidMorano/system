88c3c9739e58cceb9e8762e25160720680a6ee1f
David A.D. Morano <david.a.morano@gmail.com>
2026-02-18 02:47:54 -0500

    update

--- src/libpcs/mheader.cc
+++ src/libpcs/mheader.cc
@@ -40,23 +40,38 @@
 	==0		no match
 	<0		error (system-return)
 
+	Notes:
+	1. Reminder that header-key names MUST start in column
+	zero ('0') with no leading white-space characters (at all).
+	Historically, white-space WAS allowed AFTER the header-key
+	name and before the colon (':') character.
+
+	See-also:
+	hmatch(3pcs)
+	mheader(3pcs)
+	headkeymat(3mailmsg)
+	mailmsgmathdr(3mailmsg)
+
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
+#include	<strings.h>		/* |strncasecmp(3c)| */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>		/* |getenv(3c)| */
 #include	<cstring>		/* |strncasecmp(3c)| */
 #include	<clanguage.h>
 #include	<usysbase.h>
 #include	<six.h>			/* |sichr(3uc)| */
-#include	<sfx.h>			/* |sfnext(3uc)| */
+#include	<rmx.h>			/* |rmblk(3uc)| */
 #include	<localmisc.h>
 
 #include	"mheader.h"
 
 #pragma		GCC dependency		"mod/libutil.ccm"
+#pragma		GCC dependency		"mod/pcsutil.ccm"
 
 import libutil ;			/* |lenstr(3u)| */
+import pcsutil ;			/* |hdrmatbasic(3pcs)| */
 
 /* local defines */
 
@@ -87,21 +102,24 @@ import libutil ;			/* |lenstr(3u)| */
 
 /* exported subroutines */
 
-int mheader(cchar *hkeyp,cchar *ts) noex {
+int mheader(cchar *hkeyp,cchar *hbuf) noex {
     	int		rs = SR_FAULT ;
-	int		f = false ;
-	if (int hkeyl ; ts && (hkeyl = getlenstr(hkeyp)) >= 0) {
-	    rs = SR_OK ;
-	    if (int si ; (si = sichr(ts,':')) >= hkeyl) {
-		cchar *cp ;
-		if (int cl ; (cl = sfnext(ts,si,&cp)) == hkeyl) {
-		    if (strncasecmp(hkeyp,cp,cl) == 0) {
-			f = (si + 1) ;
-		    }
-		} /* end if (sfnext) */
-	    } /* end if (sichr) */
+	int		idx = 0 ; /* return-value */
+	if (cint hkeyl = getlenstr(hkeyp) ; hkeyl >= 0) {
+	    if (cint hlen = getlenstr(hbuf) ; hlen >= 0) {
+	        rs = SR_OK ;
+	        if (hdrmatbasic(hkeyp,hbuf)) {
+	            if (cint si = sichr(hbuf,hlen,':') ; si >= hkeyl) {
+		        if (cint kl = rmblk(hbuf,si) ; kl == hkeyl) {
+		            if (strncasecmp(hbuf,hkeyp,hkeyl) == 0) {
+			        idx = (si + 1) ;
+		            }
+		        } /* end if (sfnext) */
+	            } /* end if (sichr) */
+	        } /* end if (hdrmatbasic) */
+	    } /* end if (getlenstr) */
 	} /* end if (getlenstr) */
-	return (rs >= 0) ? f : rs ;
+	return (rs >= 0) ? idx : rs ;
 }
 /* end subroutine (mheader) */
 
