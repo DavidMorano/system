37839d5236f73ab5c96cf76d5fbfebce4214361c
David A.D. Morano <david.a.morano@gmail.com>
2026-02-19 01:01:16 -0500

    update

--- src/libu/timewatch.cc
+++ src/libu/timewatch.cc
@@ -27,10 +27,7 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<usupport.h>		/* |msleep(3u)| */
 #include	<localmisc.h>
 
