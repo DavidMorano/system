ab5fe97a828986f81be07b729e6486e84621a963
David A.D. Morano <david.a.morano@gmail.com>
2026-02-21 01:05:34 -0500

    restore

---
+++ src/libuc/strxcmp/strkeydictcmp.h
@@ -0,0 +1,38 @@
+/* strkeydictcmp HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* make a character out of an integer */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-09-10, David A­D­ Morano
+	This was written for Rightcore Network Services (RNS).
+ 
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STRKEYDICTCMP_INCLUDE
+#define	STRKEYDICTCMP_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern int strkeydictcmp(cchar *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* STRKEYDICTCMP_INCLUDE */
+
+
