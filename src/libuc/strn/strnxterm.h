feac7a8a700752f77caae6b8eee53c91ec2c4aae
David A.D. Morano <david.a.morano@gmail.com>
2026-02-21 01:10:32 -0500

    update

--- src/libuc/strn/strnxterm.h
+++ src/libuc/strn/strnxterm.h
@@ -8,12 +8,12 @@
 
 /* revision history:
 
-	= 1998-11-01, David A­D­ Morano
+	= 1999-06-08, David AÂ­DÂ­ Morano
 	This subroutine was written for Rightcore Network Services.
 
 */
 
-/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	STRNXTERM_INCLUDE
 #define	STRNXTERM_INCLUDE
