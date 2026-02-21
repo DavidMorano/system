87f47f5993fca4a88e95c81565305173d296b45b
David A.D. Morano <david.a.morano@gmail.com>
2026-02-21 01:07:40 -0500

    update

--- src/libuc/strxcmp/strkeycmp.h
+++ src/libuc/strxcmp/strkeycmp.h
@@ -25,8 +25,18 @@
 #include	<utypealiases.h>
 #include	<usysdefs.h>
 
+#include	<strkeydictcmp.h>
 
 EXTERNC_begin
+
+extern int strkeybasecmpo(cchar *,cchar *) noex ;
+extern int strkeybasecmpr(cchar *,cchar *) noex ;
+
+extern int strkeycasecmpo(cchar *,cchar *) noex ;
+extern int strkeycasecmpr(cchar *,cchar *) noex ;
+
+extern int strkeyfoldcmpo(cchar *,cchar *) noex ;
+extern int strkeyfoldcmpr(cchar *,cchar *) noex ;
 
 extern int strkeybasecmp(cchar *,cchar *) noex ;
 extern int strkeycasecmp(cchar *,cchar *) noex ;
@@ -36,7 +46,24 @@ local inline int strkeycmp(cchar *s1,cchar *s2) noex {
     	return strkeybasecmp(s1,s2) ;
 }
 
-extern int strkeydictcmp(cchar *,cchar *) noex ;
+extern int strkeyxbasecmpo(cchar *,cchar *) noex ;
+extern int strkeyxbasecmpr(cchar *,cchar *) noex ;
+
+extern int strkeyxcasecmpo(cchar *,cchar *) noex ;
+extern int strkeyxcasecmpr(cchar *,cchar *) noex ;
+
+extern int strkeyxfoldcmpo(cchar *,cchar *) noex ;
+extern int strkeyxfoldcmpr(cchar *,cchar *) noex ;
+
+local inline int strkeyxbasecmp(cchar *s1,cchar *s2) noex {
+    	return strkeyxbasecmpo(s1,s2) ;
+}
+local inline int strkeyxcasecmp(cchar *s1,cchar *s2) noex {
+    	return strkeyxcasecmpo(s1,s2) ;
+}
+local inline int strkeyxfoldcmp(cchar *s1,cchar *s2) noex {
+    	return strkeyxfoldcmpo(s1,s2) ;
+}
 
 EXTERNC_end
 
