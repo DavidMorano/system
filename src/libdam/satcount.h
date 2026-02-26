4db2bc6716ac2739cc2fa7de86f92fd43f3ba1c9
David A.D. Morano <david.a.morano@gmail.com>
2026-02-26 02:21:58 -0500

    restore

---
+++ src/libdam/satcount.h
@@ -0,0 +1,38 @@
+/* satcount HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* saturating counter */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-03-15, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SATCOUNT_INCLUDE
+#define	SATCOUNT_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern uint	satcount(uint,uint,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* SATCOUNT_INCLUDE */
+
+
