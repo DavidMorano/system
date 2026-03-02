71e887eddd92f144c48058163b0a89649dc4c6a1
David A.D. Morano <david.a.morano@gmail.com>
2026-03-02 00:39:49 -0500

    restore

---
+++ src/libdebug/debugging.h
@@ -0,0 +1,32 @@
+/* debugging HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* debug utilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	DEBUGGING_INCLUDE
+#define	DEBUGGING_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* DEBUGGING_INCLUDE */
+
+
