2ebbe6df6822780340d5c65371bb9174bed43a9e
David A.D. Morano <david.a.morano@gmail.com>
2026-03-02 00:40:40 -0500

    update

--- src/libdebug/debugfork.cc
+++ src/libdebug/debugfork.cc
@@ -1,11 +1,12 @@
-/* debugfork */
+/* debugfork SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
 
 /* debug-fork stubs */
-
+/* version %I% last-modified %G% */
 
 #define	CF_DEBUGS	1		/* compile-time debugforkging */
 #define	CF_UCOPEN	0		/* use 'uc_openprog(3uc)' */
-
 
 /* revision history:
 
@@ -18,55 +19,41 @@
 
 /*******************************************************************************
 
-	This modeule provides debugging support for the 'uc_fork(3uc)'.
-
+  	Description:
+	This modeule provides debugging support for the |uc_fork(3uc)|.
 
 *******************************************************************************/
 
-
-#include	<envstandards.h>
-
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<sys/param.h>
 #include	<unistd.h>
 #include	<fcntl.h>
 #include	<csignal>
 #include	<cstdlib>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
 #include	<cstring>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<strlinelen.h>
+#include	<localmisc.h>		/* |COLUMNS| + |TIMEBUFLEN| */
 
-#include	<usystem.h>
-#include	<storebuf.h>
-#include	<localmisc.h>
+#define		<debugprint.h>
+#define		<debugline.h>
 
 
 /* local defines */
 
-#ifndef	COLUMNS
-#define	COLUMNS		80
-#endif
-
 #define	RBUFLEN		100
-
-#ifndef	TIMEBUFLEN
-#define	TIMEBUFLEN	80
-#endif
 
 
 /* external subroutines */
 
-extern int	bufprintf(char *,int,const char *,...) ;
-extern int	nprintf(const char *,const char *,...) ;
-extern int	debugforkprintf(const char *,...) ;
-extern int	debugforkprint(const char *,int) ;
-
-#if	CF_DEBUGS
-extern int	debugprintf(const char *,...) ;
-extern int	strlinelen(const char *,int,int) ;
-#endif
-
-extern cchar	*getourenv(const char **,const char *) ;
-
-extern char	*strwcpy(char *,const char *,int) ;
+extern "C" {
+    extern int	debugforkprintf(cchar *,...) noex ;
+    extern int	debugforkprint(cchar *,int) noex ;
+}
 
 
 /* local structures */
@@ -81,24 +68,25 @@ extern char	*strwcpy(char *,const char *,int) ;
 /* local variables */
 
 
+/* exported variables */
+
+
 /* exported subroutines */
 
-
-int debugfork(const char *s)
-{
-	const int	of = O_RDWR ;
+int debugfork(cchar *s) noex {
+	cint		of = O_RDWR ;
 	int		rs ;
-	const char	*pf = "/home/dam/rje/proghello" ;
+	cchar		*pf = "/home/dam/rje/proghello" ;
 
 #if	CF_UCOPEN
 	debugprintf("debugfork: uc_openprog() %s\n",s) ;
 	rs = uc_openprog(pf,of,NULL,NULL) ;
 	debugprintf("debugfork: uc_openprog() rs=%d\n",rs) ;
 	if (rs >= 0) {
-	    const int	rlen = RBUFLEN ;
-	    const int	opts = 0 ;
-	    const int	to = 4 ;
-	    const int	fd = rs ;
+	    cint	rlen = RBUFLEN ;
+	    cint	opts = 0 ;
+	    cint	to = 4 ;
+	    cint	fd = rs ;
 	    char	rbuf[RBUFLEN+1] ;
 	    while ((rs = uc_reade(fd,rbuf,rlen,to,opts)) > 0) {
 	        int	len = rs ;
