ed9c3e63c1095cf241568f937866ff809341cfdf
David A.D. Morano <david.a.morano@gmail.com>
2026-03-02 00:39:54 -0500

    restore

---
+++ src/libdebug/debugline.h
@@ -0,0 +1,40 @@
+/* debugline HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* debug utilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	DEBUGLINE_INCLUDE
+#define	DEBUGLINE_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+EXTERNC_begin
+
+extern int strlinelen(cchar *,int,int) noex ;
+extern int strnnlen(cchar *,int,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* DEBUGLINE_INCLUDE */
+
+
