6556259bd0b3541bafb9050029feee5aeb0d1da2
David A.D. Morano <david.a.morano@gmail.com>
2026-03-04 01:05:56 -0500

    update

--- src/libdam/bfliner.cc
+++ src/libdam/bfliner.cc
@@ -46,7 +46,6 @@
 
 /* imported namespaces */
 
-using std::nullptr_t ;			/* type */
 using std::min ;			/* subroutine-template */
 using std::max ;			/* subroutine-template */
 using std::nothrow ;			/* constant */
