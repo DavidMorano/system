e2fcc6044cdb458a690dbae7d1698f1d2863de90
David A.D. Morano <david.a.morano@gmail.com>
2026-03-04 01:10:20 -0500

    restore

---
+++ src/libdam/deprecated.h
@@ -0,0 +1,31 @@
+/* deprecated HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* deprecated subroutines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-08-16, David A­D­ Morano
+	This module was originally written for hardware CAD support.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	DEPRECATED_INCLUDE
+#define	DEPRECATED_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+#endif /* DEPRECATED_INCLUDE */
+
+
