f367909f72472915e3d06b4fb204b35a3e58cdd5
David A.D. Morano <david.a.morano@gmail.com>
2026-03-05 00:25:14 -0500

    update

--- src/libuc/ucwrite.cc
+++ src/libuc/ucwrite.cc
@@ -59,7 +59,9 @@
 #include	<climits>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
 #include	<localmisc.h>
 
 #pragma		GCC dependency	"mod/libutil.ccm"
