f463f114bc5a8e1396927ccc0ebdeb5dfd07603f
David A.D. Morano <david.a.morano@gmail.com>
2026-03-06 00:45:15 -0500

    update

--- src/libmailmsg/mailalias/dbmake.hh
+++ src/libmailmsg/mailalias/dbmake.hh
@@ -55,7 +55,8 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<vecobj.h>
 #include	<strtab.h>
 
