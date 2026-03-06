891328b9c12a429c5a54bf6992c5cec1cbcc2f6a
David A.D. Morano <david.a.morano@gmail.com>
2026-03-06 00:39:16 -0500

    update

--- src/libdam/netfile.cc
+++ src/libdam/netfile.cc
@@ -84,10 +84,8 @@ import libutil ;
 
 /* imported namespaces */
 
-using std::nullptr_t ;			/* type */
 using std::min ;			/* subroutine-template */
 using std::max ;			/* subroutine-template */
-using std::nothrow ;			/* constant */
 
 
 /* local typedefs */
