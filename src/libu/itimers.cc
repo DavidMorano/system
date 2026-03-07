7c8969c9fd20677e2e86f5f420693fa4879d13d9
David A.D. Morano <david.a.morano@gmail.com>
2026-03-07 00:17:08 -0500

    restore

---
+++ src/libu/itimers.cc
@@ -0,0 +1,68 @@
+/* timers SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* constants used in identifying internal-timers */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-13, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Object:
+	itimer
+
+	Description:
+	This object simply provides the identifying values used in
+	selecting a process interval-timer.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	"itimers.hh"
+
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
+/* local subroutines */
+
+
