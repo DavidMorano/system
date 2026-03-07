83a6b439cf9a8c8973c57a08441c1680515f12d3
David A.D. Morano <david.a.morano@gmail.com>
2026-03-07 00:20:05 -0500

    restore

---
+++ src/libuc/sview.h
@@ -0,0 +1,34 @@
+/* sview HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* string-view */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-12, David A­D­ Morano
+	This code was originally written.
+
+	= 2020-05-07, David A-D- Morano
+	I converted this (formerly a header-only file) to a module.
+
+*/
+
+/* Copyright © 1998,2020 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SVIEW_INCLUDE
+#define	SVIEW_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+#endif /* SVIEW_INCLUDE */
+
+
