8d75be0847a439a433ec979ab0839b639fad1b2c
David A.D. Morano <david.a.morano@gmail.com>
2026-03-07 00:17:00 -0500

    restore

---
+++ src/libu/filetypes.cc
@@ -0,0 +1,53 @@
+/* filetypes SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* define file types according to the S_IFMT field of |mode_t| */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/types.h>
+#include	<sys/stat.h>		/* |S_IFMT| + |mode_t| */
+#include	<dirent.h>		/* possielbe preprocesor 'DT_{x}' */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
+/* local subroutines */
+
+
