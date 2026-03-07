e71e85ec5a593968fa3fade145d7def406a7d8f7
David A.D. Morano <david.a.morano@gmail.com>
2026-03-07 00:18:48 -0500

    restore

---
+++ src/libuc/fmtstr/fmtopts.cc
@@ -0,0 +1,41 @@
+/* fmtopts SUÞPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* options for |fmtstr(3uc)| */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-01, David A­D­ Morano
+	Of course, this subroutine was inspired by the UNIX®
+	equivalent, but this is my own version for a) when I do not
+	have the UNIX® libraries around, and b) to customize it to
+	what I want!
+
+*/
+
+/* Copyright © 1998 David A-D- Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Object:
+	fmtopt
+
+	Description:
+	This is the support portion of the FMTOPT object.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	"fmtopts.h"
+
+
+cint	fmtoptms::clean		= (1 << fmtopt_clean) ;
+cint	fmtoptms::nooverr	= (1 << fmtopt_nooverr) ;
+
+
