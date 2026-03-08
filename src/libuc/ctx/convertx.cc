b4498d3c43c5ad36132a34f836d608a569d61b5d
David A.D. Morano <david.a.morano@gmail.com>
2026-03-08 00:34:20 -0500

    update

--- src/libuc/ctx/convertx.cc
+++ src/libuc/ctx/convertx.cc
@@ -52,10 +52,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<localmisc.h>
 
 #include	"convertx.h"
