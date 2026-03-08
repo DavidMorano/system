5d654caa7864a00b58eb3f03ecc5a21fe035532c
David A.D. Morano <david.a.morano@gmail.com>
2026-03-08 00:32:42 -0500

    update

--- src/libu/usysutility.hh
+++ src/libu/usysutility.hh
@@ -25,10 +25,7 @@
 #include	<cstdlib>
 #include	<cstdarg>		/* |va_list(3c)| */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 namespace libu {
