3c95d541687b6525f9399d7a308805ee00fc833b
David A.D. Morano <david.a.morano@gmail.com>
2026-03-08 00:35:41 -0500

    restore

---
+++ src/libuc/ctx/cvtdig.h
@@ -0,0 +1,32 @@
+/* cvtdig HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* convert an integer value into a c-string of digits, given a value and base */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-01, David A­D­ Morano
+	This subroutine was written having been adapated (from
+	memory) from something I wrote back in the early 1980s (for
+	embedded work).  I had to write every ... last  ... thing
+	myself back in the old days.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CTDEC_INCLUDE
+#define	CTDEC_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#endif /* CTDEC_INCLUDE */
+
+
