0909412cc36d147290f33bf32e9e768b28cbab28
David A.D. Morano <david.a.morano@gmail.com>
2026-03-09 00:22:02 -0400

    update

--- src/libu/itimerval.cc
+++ src/libu/itimerval.cc
@@ -8,12 +8,12 @@
 
 /* revision history:
 
-	= 2014-04-04, David A­D­ Morano
+	= 1998-11-01, David A­D­ Morano
 	Originally written for Rightcore Network Services.
 
 */
 
-/* Copyright © 2014 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
