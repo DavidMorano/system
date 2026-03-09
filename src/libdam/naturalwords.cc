027a1c6f3a1b4730684302e8ad538100c7a167cd
David A.D. Morano <david.a.morano@gmail.com>
2026-03-09 00:14:18 -0400

    restore

---
+++ src/libdam/naturalwords.cc
@@ -0,0 +1,85 @@
+/* naturalwords SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* create some "naturalwords" constants */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was modeled from an original assembly
+	language version.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	naturalwords
+
+	Description:
+	This subroutine contains constants for managing facilities
+	that relate to natural-word processing.
+
+	Synopsis:
+	in_addr_t mknaturalwords() noex {
+
+	Arguments:
+	-	*none*
+
+	Returns:
+	-	the "bad" Internet-V4 address
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<localmisc.h>
+
+#include	"naturalwords.h"
+
+#pragma		GCC dependency	"mod/libutil.ccm"
+
+import libutil ;
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+const naturalwords	naturalword ;
+
+
+/* exported subroutines */
+
+
