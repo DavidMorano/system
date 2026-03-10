e757e13b92d6764ea82e254359de3088d653b9e2
David A.D. Morano <david.a.morano@gmail.com>
2026-03-10 00:30:28 -0400

    restore

---
+++ src/libdam/funmode.cc
@@ -0,0 +1,78 @@
+/* naturalwords SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* Function-Mode */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	funmode
+
+	Description:
+	Function-Mode
+
+	Synopsis:
+
+	Arguments:
+
+	Returns:
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<localmisc.h>
+
+#include	"funmode.hh"
+
+#pragma		GCC dependency	"mod/libutil.ccm"
+
+import libutil ;
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
