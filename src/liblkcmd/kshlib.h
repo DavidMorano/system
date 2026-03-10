caca31cc5f0abbf64d48c599ece8da6b2c0df16d
David A.D. Morano <david.a.morano@gmail.com>
2026-03-10 00:32:43 -0400

    update

--- src/liblkcmd/kshlib.h
+++ src/liblkcmd/kshlib.h
@@ -1,4 +1,5 @@
-/* kshlib */
+/* kshlib HEADER */
+/* charset=ISO8859-1 */
 /* lang=C89 */
 
 /* library initialization for KSH built-in command libraries */
@@ -15,24 +16,20 @@
 /* Copyright © 2001 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	KSHLIB_INCLUDE
-#define	KSHLIB_INCLUDE	1
+#define	KSHLIB_INCLUDE
 
 
 #include	<envstandards.h>
 #include	<sys/types.h>
-#include	<sesmsg.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<sesmsg.hh>
 
 
-#ifndef	TYPEDEF_CCHAR
-#define	TYPEDEF_CCHAR	1
-typedef const char	cchar ;
-#endif
-
-
+#define	KSHLIB_NOTE		struct kshlib_note
 #define	KSHLIB_NENTS		30
 #define	KSHLIB_INTPOLL		5
 #define	KSHLIB_INTSESCHECK	(1*3600)
-#define	KSHLIB_NOTE		struct kshlib_note
 #define	KSHLIB_SESHOUR		18 /* 6:00pm */
 #define	KSHLIB_SESDNAME		"/var/tmp/sessions"
 
@@ -45,7 +42,7 @@ enum kshlibcmds {
 	kshlibcmd_notecount,
 	kshlibcmd_notestate,
 	kshlibcmd_overlast
-} ;
+} ; /* end enum */
 
 struct kshlib_note {
 	time_t		stime ;
@@ -53,50 +50,42 @@ struct kshlib_note {
 	int		nlen ;
 	char		nbuf[SESMSG_NBUFLEN+1] ;
 	char		user[SESMSG_USERLEN+1] ;
-} ;
+} ; /* end struct */
 
+EXTERNC_begin
 
-#if	(! defined(KSHLIB_MASTER)) || (KSHLIB_MASTER == 0)
+extern void	lib_init(int,void *) noex ;
+extern void	lib_fini(void) noex ;
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
+extern int	lib_initenviron(void *) noex ;
+extern int	lib_initmemalloc(int) noex ;
+extern int	lib_mainbegin(cchar **,cint *) noex ;
+extern int	lib_mainend(void) noex ;
+extern int	lib_kshbegin(void *,cint *) noex ;
+extern int	lib_kshend(void) noex ;
+extern int	lib_runmode(void) noex ;
+extern int	lib_serial(void) noex ;
 
-extern void	lib_init(int,void *) ;
-extern void	lib_fini(void) ;
+extern int	lib_sigreset(int) noex ;
+extern int	lib_sigquit(void) noex ;
+extern int	lib_sigintr(void) noex ;
+extern int	lib_sigterm(void) noex ;
+extern int	lib_issig(int) noex ;
 
-extern int	lib_initenviron(void *) ;
-extern int	lib_initmemalloc(int) ;
-extern int	lib_mainbegin(cchar **,const int *) ;
-extern int	lib_mainend(void) ;
-extern int	lib_kshbegin(void *,const int *) ;
-extern int	lib_kshend(void) ;
-extern int	lib_runmode(void) ;
-extern int	lib_serial(void) ;
+extern int	lib_proghave(cchar *) noex ;
+extern int	lib_progaddr(cchar *,void *) noex ;
+extern int	lib_progcall(cchar *,int,cchar **,cchar **,void *) noex ;
+extern int	lib_progcalla(const void *,int,cchar **,cchar **,void *) noex ;
 
-extern int	lib_sigreset(int) ;
-extern int	lib_sigquit(void) ;
-extern int	lib_sigintr(void) ;
-extern int	lib_sigterm(void) ;
-extern int	lib_issig(int) ;
+extern int	lib_callcmd(cchar *,int,cchar **,cchar **,void *) noex ;
+extern int	lib_callfunc(const void *,int,cchar **,cchar **,void *) noex ;
 
-extern int	lib_proghave(cchar *) ;
-extern int	lib_progaddr(cchar *,void *) ;
-extern int	lib_progcall(cchar *,int,cchar **,cchar **,void *) ;
-extern int	lib_progcalla(const void *,int,cchar **,cchar **,void *) ;
+extern int	lib_noteadm(int,...) noex ;
+extern int	lib_noteread(KSHLIB_NOTE *,int) noex ;
+extern int	lib_notedel(int) noex ;
 
-extern int	lib_callcmd(cchar *,int,cchar **,cchar **,void *) ;
-extern int	lib_callfunc(const void *,int,cchar **,cchar **,void *) ;
+EXTERNC_end
 
-extern int	lib_noteadm(int,...) ;
-extern int	lib_noteread(KSHLIB_NOTE *,int) ;
-extern int	lib_notedel(int) ;
-
-#ifdef	__cplusplus
-}
-#endif
-
-#endif /* (! defined(KSHLIB_MASTER)) || (KSHLIB_MASTER == 0) */
 
 #endif /* KSHLIB_INCLUDE */
 
