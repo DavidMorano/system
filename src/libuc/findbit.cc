b1b0905169d3f1ec7d7611f0147cae46a321c054
David A.D. Morano <david.a.morano@gmail.com>
2026-03-11 00:22:09 -0400

    update

--- src/libuc/findbit.cc
+++ src/libuc/findbit.cc
@@ -23,9 +23,15 @@
 	due to use in shared objects or something else that indirectly
 	calls one or more of the subroutines in here.
 
+	= 2026-03-09, David A­D­ Morano
+	I changed this code to use the C++20 bit-manipulation
+	subroutines.  I have used them (C++20 bit-manipulation
+	subroutines) elsewhere but I have only gotten to change
+	these here now.
+
 */
 
-/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998,2001,2026 David A­D­ Morano.  All rights reserved. */
 
 /******************************************************************************
 
@@ -95,20 +101,33 @@
 ******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<climits>		/* |UCHAR_MAX| + |CHAR_BIT| */
+#include	<climits>		/* |CHAR_BIT| */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
+#include	<cstdintx>
+#include	<concepts>		/* |integral(3c++)| */
+#include	<bit>			/* |countl_zero(3c++)| */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
 #include	<usysdefs.h>
 #include	<stdintx.h>		/* for type |longlong| */
+#include	<localmisc.h>
+
+#include	"findbit.h"
 
 
 /* local defines */
 
 
 /* imported namespaces */
+
+using std::integral ;			/* concept */
+using std::countl_zero ;		/* subroutine-template */
+using std::countr_zero ;		/* subroutine-template */
+using std::countl_one ;			/* subroutine-template */
+using std::countr_one ;			/* subroutine-template */
+using std::popcount ;			/* subroutine-template */
 
 
 /* local typedefs */
@@ -125,46 +144,46 @@
 
 /* forward references */
 
+template<integral T> int ffbsx(T uv) noex {
+	int		bn = -1 ;
+	if (uv) {
+	    bn = countr_zero(uv) ;
+	}
+	return bn ;
+} /* end subroutine-template (ffbsx) */
+
+template<integral T> int ffbcx(T uv) noex {
+    	T		allones = (~ T(0)) ;
+	int		bn = -1 ;
+	if (uv != allones) {
+	    bn = countr_one(uv) ;
+	}
+	return bn ;
+} /* end subroutine-template (ffbcx) */
+
+template<integral T> int flbsx(T uv) noex {
+	int		bn = -1 ; /* return-value */
+	if (uv) {
+	    cint nbit = (szof(T) * CHAR_BIT) ;
+	    cint nc = countl_zero(uv) ;
+	    bn = (nbit - nc - 1) ;
+	} /* end if (non-zero positive) */
+	return bn ;
+} /* end subroutine-template (flbsx) */
+
+template<integral T> int flbcx(T uv) noex {
+    	T		allones = (~ T(0)) ;
+	int		bn = -1 ; /* return-value */
+	if (uv != allones) {
+	    cint nbit = (szof(T) * CHAR_BIT) ;
+	    cint nc = countl_one(uv) ;
+	    bn = (nbit - nc - 1) ;
+	} /* end if (non-zero positive) */
+	return bn ;
+} /* end subroutine-template (flbcx) */
+
 
 /* local variables */
-
-const unsigned char	ffbstab[] = {
-	0x0f, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x04, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x05, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x04, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x06, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x04, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x05, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x04, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x07, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x04, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x05, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x04, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x06, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x04, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x05, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01,
-	0x04, 0x01, 0x02, 0x01, 0x03, 0x01, 0x02, 0x01
-} ;
-
-const unsigned char	flbstab[] = {
-	0x0f, 0x11, 0x22, 0x22, 0x33, 0x33, 0x33, 0x33,
-	0x44, 0x44, 0x44, 0x44, 0x44, 0x44, 0x44, 0x44,
-	0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55,
-	0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55,
-	0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66,
-	0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66,
-	0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66,
-	0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66, 0x66,
-	0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77,
-	0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77,
-	0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77,
-	0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77,
-	0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77,
-	0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77,
-	0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77,
-	0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77, 0x77
-} ;
 
 
 /* exported variables */
@@ -172,219 +191,72 @@ const unsigned char	flbstab[] = {
 
 /* exported subroutines */
 
-int ffbsi(uint v) noex {
-	uint		ti, tr ;
-	cint		nb = szof(uint) ;
-	int		i = -1 ;
-	int		s ;
-	int		n ;
-	for (int b = 0 ; b < nb ; b += 1) {
-	    s = (b<<3) ;
-	    ti = ((v>>s) & UCHAR_MAX) ;
-	    if (ti) {
-		tr = ffbstab[ti>>1] ;
-		n = (ti&1) ? (tr>>4) : (tr&15) ;
-		i = n + s ;
-		break ;
-	    }
-	} /* end for */
-	return i ;
+int ffbsi(uint uv) noex {
+	return ffbsx(uv) ;
 }
-/* end subroutine (ffbsi) */
 
-int ffbsl(ulong v) noex {
-	ulong		ti, tr ;
-	cint		nb = szof(ulong) ;
-	int		i = -1 ;
-	int		s ;
-	int		n ;
-	for (int b = 0 ; b < nb ; b += 1) {
-	    s = (b<<3) ;
-	    ti = ((v>>s) & UCHAR_MAX) ;
-	    if (ti) {
-		tr = ffbstab[ti>>1] ;
-		n = (ti&1) ? (tr>>4) : (tr&15) ;
-		i = n + s ;
-		break ;
-	    }
-	} /* end for */
-	return i ;
+int ffbsl(ulong uv) noex {
+	return ffbsx(uv) ;
 }
-/* end subroutine (ffbsl) */
 
-int ffbsll(ulonglong v) noex {
-	ulonglong	ti, tr ;
-	cint		nb = szof(ulonglong) ;
-	int		i = -1 ;
-	int		s ;
-	int		n ;
-	for (int b = 0 ; b < nb ; b += 1) {
-	    s = (b<<3) ;
-	    ti = ((v>>s) & UCHAR_MAX) ;
-	    if (ti) {
-		tr = ffbstab[ti>>1] ;
-		n = (ti&1) ? (tr>>4) : (tr&15) ;
-		i = n + s ;
-		break ;
-	    }
-	} /* end for */
-	return i ;
+int ffbsll(ulonglong uv) noex {
+	return ffbsx(uv) ;
 }
-/* end subroutine (ffbsll) */
 
-int ffbci(uint v) noex {
-	cint		n = (CHAR_BIT * szof(uint)) ;
-	int		i ; /* U-A */
-	for (i = 0 ; i < n ; i += 1) {
-	    if (! (v & 1)) break ;
-	    v = v >> 1 ;
-	} /* end for */
-	return (i < n) ? i : -1 ;
+int ffbci(uint uv) noex {
+	return ffbcx(uv) ;
 }
 /* end subroutine (ffbci) */
 
-int ffbcl(ulong v) noex {
-	cint		n = (CHAR_BIT * szof(ulong)) ;
-	int		i ; /* U-A */
-	for (i = 0 ; i < n ; i += 1) {
-	    if (! (v & 1)) break ;
-	    v = v >> 1 ;
-	} /* end for */
-	return (i < n) ? i : -1 ;
+int ffbcl(ulong uv) noex {
+	return ffbcx(uv) ;
 }
 /* end subroutine (ffbcl) */
 
-int ffbcll(ulonglong v) noex {
-	cint		n = (CHAR_BIT * szof(ulonglong)) ;
-	int		i ; /* U-A */
-	for (i = 0 ; i < n ; i += 1) {
-	    if (! (v & 1)) break ;
-	    v = v >> 1 ;
-	} /* end for */
-	return (i < n) ? i : -1 ;
+int ffbcll(ulonglong uv) noex {
+	return ffbcx(uv) ;
 }
 /* end subroutine (ffbcll) */
 
-int flbsi(uint v) noex {
-	uint		ti, tr ;
-	cint		nb = szof(uint) ;
-	int		i = -1 ;
-	int		s ;
-	int		n ;
-	for (int b = (nb-1) ; b >= 0 ; b -= 1) {
-	    s = (b<<3) ;
-	    ti = ((v>>s) & UCHAR_MAX) ;
-	    if (ti) {
-		tr = flbstab[ti>>1] ;
-		n = (ti&1) ? (tr>>4) : (tr&15) ;
-		i = n + s ;
-		break ;
-	    }
-	} /* end for */
-	return i ;
+int flbsi(uint uv) noex {
+	return flbs(uv) ;
 }
-/* end subroutine (flbsi) */
 
-int flbsl(ulong v) noex {
-	ulong		ti, tr ;
-	cint		nb = szof(ulong) ;
-	int		i = -1 ;
-	int		s ;
-	int		n ;
-	for (int b = (nb-1) ; b >= 0 ; b -= 1) {
-	    s = (b<<3) ;
-	    ti = ((v>>s) & UCHAR_MAX) ;
-	    if (ti) {
-		tr = flbstab[ti>>1] ;
-		n = (ti&1) ? (tr>>4) : (tr&15) ;
-		i = n + s ;
-		break ;
-	    }
-	} /* end for */
-	return i ;
+int flbsl(ulong uv) noex {
+	return flbs(uv) ;
 }
-/* end subroutine (flbsl) */
 
-int flbsll(ulonglong v) noex {
-	ulonglong	ti, tr ;
-	cint		nb = szof(ulonglong) ;
-	int		i = -1 ;
-	int		s ;
-	int		n ;
-	for (int b = (nb-1) ; b >= 0 ; b -= 1) {
-	    s = (b<<3) ;
-	    ti = ((v>>s) & UCHAR_MAX) ;
-	    if (ti) {
-		tr = flbstab[ti>>1] ;
-		n = (ti&1) ? (tr>>4) : (tr&15) ;
-		i = n + s ;
-		break ;
-	    }
-	} /* end for */
-	return i ;
+int flbsll(ulonglong uv) noex {
+	return flbs(uv) ;
 }
-/* end subroutine (flbsll) */
 
-int flbci(uint v) noex {
-	cint		n = (CHAR_BIT * szof(uint)) ;
-	int		i ; /* U-A */
-	for (i = (n - 1) ; i >= 0 ; i -= 1) {
-	    if (! ((v >> i) & 1)) break ;
-	} /* end for */
-	return i ;
+int flbci(uint uv) noex {
+	return flbcx(uv) ;
 }
 /* end subroutine (flbci) */
 
-int flbcl(ulong v) noex {
-	cint		n = (CHAR_BIT * szof(ulong)) ;
-	int		i ; /* U-A */
-	for (i = (n - 1) ; i >= 0 ; i -= 1) {
-	    if (! ((v >> i) & 1)) break ;
-	} /* end for */
-	return i ;
+int flbcl(ulong uv) noex {
+	return flbcx(uv) ;
 }
 /* end subroutine (flbcl) */
 
-int flbcll(ulonglong v) noex {
-	cint		n = (CHAR_BIT * szof(ulonglong)) ;
-	int		i ; /* U-A */
-	for (i = (n - 1) ; i >= 0 ; i -= 1) {
-	    if (! ((v >> i) & 1)) break ;
-	} /* end for */
-	return i ;
+int flbcll(ulonglong uv) noex {
+	return flbcx(uv) ;
 }
 /* end subroutine (flbcll) */
 
-int fbscounti(uint v) noex {
-	cint		n = (CHAR_BIT * szof(uint)) ;
-	int		c = 0 ;
-	for (int i = 0 ; i < n ; i += 1) {
-	    if (v & 1) c += 1 ;
-	    v = v >> 1 ;
-	} /* end for */
-	return c ;
+int fbscounti(uint uv) noex {
+    	return popcount(uv) ;
 }
 /* end subroutine (fbscounti) */
 
-int fbscountl(ulong v) noex {
-	cint		n = (CHAR_BIT * szof(ulong)) ;
-	int		c = 0 ;
-	for (int i = 0 ; i < n ; i += 1) {
-	    if (v & 1) c += 1 ;
-	    v = v >> 1 ;
-	} /* end for */
-	return c ;
+int fbscountl(ulong uv) noex {
+    	return popcount(uv) ;
 }
 /* end subroutine (fbscountl) */
 
-int fbscountll(ulonglong v) noex {
-	cint		n = (CHAR_BIT * szof(ulonglong)) ;
-	int		c = 0 ;
-	for (int i = 0 ; i < n ; i += 1) {
-	    if (v & 1) c += 1 ;
-	    v = v >> 1 ;
-	} /* end for */
-	return c ;
+int fbscountll(ulonglong uv) noex {
+    	return popcount(uv) ;
 }
 /* end subroutine (fbscountll) */
 
