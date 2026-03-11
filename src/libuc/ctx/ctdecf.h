3555548dc12e09ae809467a558febb58d533953c
David A.D. Morano <david.a.morano@gmail.com>
2026-03-11 00:24:08 -0400

    update

--- src/libuc/ctx/ctdecf.h
+++ src/libuc/ctx/ctdecf.h
@@ -21,11 +21,7 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<stdintx.h>
+#include	<usysbase.h>
 
 
 EXTERNC_begin
