3ab0d5d49049b9c6f36c3cbc70dcd00ad1b69476
David A.D. Morano <david.a.morano@gmail.com>
2026-03-11 00:24:04 -0400

    update

--- src/libuc/ctx/ctdec.h
+++ src/libuc/ctx/ctdec.h
@@ -24,11 +24,10 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<stdintx.h>
+
+#include	<ctdecf.h>		/* <- extra convenience */
 #include	<ctdecp.h>		/* <- extra convenience */
 
 
