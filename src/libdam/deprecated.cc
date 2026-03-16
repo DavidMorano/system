319b6c42ce0498d0be44cd6d229e6f93cb716dba
David A.D. Morano <david.a.morano@gmail.com>
2026-03-16 00:37:20 -0400

    update

--- src/libdam/deprecated.cc
+++ src/libdam/deprecated.cc
@@ -2,13 +2,13 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 (conformance reviewed) */
 
-/* check for the ONC private key stuff */
+/* these are deprecated subroutines */
 /* version %I% last-modified %G% */
 
 
 /* revision history:
 
-	= 1999-08-17, David A­D­ Morano
+	= 1998-08-16, David A­D­ Morano
 	This subroutine was taken from the LOGDIR/LOGNAME program
 	(fist written for the SunOS 4.xx environment in 1989).  This
 	is severly hacked to work in the strange environment of
@@ -16,7 +16,7 @@
 
 */
 
-/* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
@@ -31,14 +31,13 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>		/* |system(3c)| */
-#include	<cstdio>		/* |gets(3stdio) */
+#include	<cstdlib>		/* |system(3c)| + |abort(3c)| */
+#include	<cstdio>		/* |gets(3stdio)| */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<localmisc.h>
+
+#include	"deprecated.h"
 
 
 /* local defines */
