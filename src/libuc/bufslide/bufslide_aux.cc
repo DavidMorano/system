5e4c3456a7d3792d41f6709cafb142d18a6e28d7
David A.D. Morano <david.a.morano@gmail.com>
2026-03-17 00:45:42 -0400

    update

--- src/libuc/bufslide/bufslide_aux.cc
+++ src/libuc/bufslide/bufslide_aux.cc
@@ -32,7 +32,8 @@
 #include	<string_view>
 #include	<clanguage.h>
 #include	<usysbase.h>
-#include	<mkchar.h>
+#include	<ulogerror.h>
+#include	<binchunk.hh>
 #include	<localmisc.h>
 
 #include	"bufslide.hh"
@@ -74,98 +75,81 @@ typedef string_view	strview ;
 
 /* exported subroutines */
 
-int bufslide::ilen() const noex {
-	csize	bsize = b.size() ;
-	return (intconv(bsize) - oi) ;
-} /* end method (bufslide::ilen) */
-
 local void bufslide_mems(bufslide *op) noex {
         op->start	(op,bufslidemem_start) ;
         op->finish	(op,bufslidemem_finish) ;
+        op->adv		(op,bufslidemem_adv) ;
         op->count	(op,bufslidemem_count) ;
         op->len		(op,bufslidemem_len) ;
+	op->extent	(op,bufslidemem_extent) ;
 } /* end subroutine (bufslide_mems) */
 
-bufslide::bufslide(cchar *sp,int sl) noex : oi(0) {
+bufslide::bufslide() noex : oi(0) {
     	bufslide_mems(this) ;
-        if (sp) {
-            if (sl < 0) {
-                sl = lenstr(sp) ;
-            }
-            try {
-                strview sv(sp,sl) ;
-                b += sv ;
-            } catch (...) {
-                (void) sp ;
-            }
-        } /* end if (non-null) */
+	chunkp = nullptr ;
 } /* end ctor (bufslide) */
 
+int bufslide::ilen() const noex {
+    	int		rs = SR_NOTOPEN ;
+	if (fl.open) ylikely {
+    	    rs = SR_BUGCHECK ;
+	    if (binchunk *bp = resumelife<binchunk>(chunkp) ; bp) ylikely {
+	        if ((rs = bp->len) >= 0) {
+		    if (cint bsz = rs ; bsz >= oi) ylikely {
+	                rs = (bsz - oi) ;
+		    } else {
+		        rs = SR_BUGCHECK ;
+		    }
+	        } /* end if (chunk_len) */
+	    } /* end if (non-null) */
+	} /* end if (open) */
+	return rs ;
+} /* end method (bufslide::ilen) */
+
+int bufslide::iextent() const noex {
+    	int		rs = SR_NOTOPEN ;
+	if (fl.open) {
+    	    rs = SR_BUGCHECK ;
+	    if (binchunk *bp = resumelife<binchunk>(chunkp) ; bp) ylikely {
+	        rs = bp->len ;
+	    } /* end if (non-null) */
+	} /* end if (open) */
+	return rs ;
+} /* end method (bufslide::iextent) */
+
 int bufslide::operator [] (int i) const noex {
-	cint		n = intconv(b.size()) ;
-	int		rch = 0 ; /* return-value */
-	if ((oi + i) < n) {
-	    rch = mkchar(b[oi + i]) ;
-	}
-	return rch ;
+    	return at(i) ;
 } /* end method (bufslide::operator) */
 
-int bufslide::add(cchar *sp,int µsl) noex {
-	int		rs = SR_FAULT ;
-	if (int sl ; (sl = getlenstr(sp,µsl)) >= 0) {
-	    rs = SR_OK ;
-	    try {
-	        strview sv(sp,sl) ;
-	        b += sv ;
-	        {
-	            csize	bsize = b.size() ;
-	            rs = (intconv(bsize) - oi) ;
-	        }
-	    } catch (...) {
-	        rs = SR_NOMEM ;
-	    }
-	} /* end if (getlenstr) */
-	return rs ;
-} /* end method (bufslide::add) */
+void bufslide::dtor() noex {
+	if (cint rs = finish ; rs < 0) {
+	    ulogerror("bufslide",rs,"fini-finish") ;
+	}
+} /* end method (bufslide::dtor) */
 
-int bufslide::add(int ch) noex {
-    	return push(ch) ;
-} /* end method (bufslide::add) */
-
-int bufslide::at(int i) const noex {
-	csize	bsize = b.size() ;
-	int	rs = SR_INVALID ;
-	int	rch = 0 ; /* return-value */
-	{
-	    cint n = intconv(bsize) ;
-	    if ((oi + i) < n) {
-		rs = SR_OK ;
-		rch = mkchar(b[oi + i]) ;
-	    } /* end if */
-	} /* end block */
-	return (rs >= 0) ? rch : rs ;
-} /* end method (bufslide::at) */
-
-int bufslide::readat(int ei,char *rbuf,int rlen) const noex {
-    	int		rs = SR_FAULT ;
-	int		rl = 0 ; /* return-value */
-	if (rbuf) {
-	    if ((rs = ilen()) > 0) {
-		if (cint cnt = rs ; (ei >= 0) && (ei < cnt)) {
-		    while (rlen-- && (rl < (cnt - ei))) {
-			rbuf[rl++] = b[ei++] ;
-		    } /* end while */
-		} else {
-		    rs = SR_INVALID ;
-		} /* end if */
-	    } /* end if (ilen) */
-	    rbuf[rl] = '\0' ;
+int bufslide_co::operator () (int a) noex {
+	int		rs = SR_BUGCHECK ;
+	if (op) ylikely {
+	    switch (w) {
+	    case bufslidemem_start:
+		rs = op->istart() ;
+	        break ;
+	    case bufslidemem_finish:
+	        rs = op->ifinish() ;
+	        break ;
+	    case bufslidemem_adv:
+	        rs = op->iadv(a) ;
+	        break ;
+	    case bufslidemem_count:
+	    case bufslidemem_len:
+	        rs = op->ilen() ;
+	        break ;
+	    case bufslidemem_extent:
+	        rs = op->iextent() ;
+	        break ;
+	    } /* end switch */
 	} /* end if (non-null) */
-	return (rs >= 0) ? rl : rs ;
-} /* end method (bufslide::readat) */
-
-int bufslide::read(char *rbuf,int rlen) const noex {
-    	return readat(0,rbuf,rlen) ;
-} /* end method (bufslide::read) */
+	return rs ;
+} /* end method (bufslide_co::operator) */
 
 
