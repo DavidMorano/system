0b8fbadef85176a2f07c2f189720cedd9b796b46
David A.D. Morano <david.a.morano@gmail.com>
2026-03-17 00:45:36 -0400

    update

--- src/libuc/bufslide/bufslide.hh
+++ src/libuc/bufslide/bufslide.hh
@@ -38,11 +38,14 @@
 #include	<clanguage.h>
 #include	<usysbase.h>
 
+
 enum bufslidemems {
 	bufslidemem_start,
+	bufslidemem_finish,
+	bufslidemem_adv,
 	bufslidemem_count,
 	bufslidemem_len,
-	bufslidemem_finish,
+	bufslidemem_extent,
 	bufslidemem_overlast
 } ; /* end enum (bufslidemems) */
 
@@ -53,48 +56,51 @@ struct bufslide_fl {
 struct bufslide ;
 
 struct bufslide_co {
-	bufslide		*op = nullptr ;
+	bufslide	*op = nullptr ;
 	int		w = -1 ;
 	void operator () (bufslide *p,int m) noex {
 	    op = p ;
 	    w = m ;
 	} ;
-	operator int () noex ;
-	int operator () () noex { 
-	    return operator int () ;
-	} ;
+	int operator () (int = -1) noex ;
+	operator int () noex {
+	    return operator () (-1) ;
+	}
 } ; /* end struct (bufslide_co) */
 
 class bufslide {
     	friend		bufslide_co ;
-	std::string	b ;
+	void		*chunkp ;	/* chunk-object pointer */
 	int		oi ;		/* output index */
 	int push(int) noex ;
 	int istart() noex ;
 	int ifinish() noex ;
+	int iadv(int) noex ;
 	int ilen() const noex ;
+	int iextent() const noex ;
 public:
-	bufslide_co		start ;
-	bufslide_co		finish ;
-	bufslide_co		count ;
-	bufslide_co		len ;
-	bufslide_fl		fl{} ;
+	bufslide_co	start ;
+	bufslide_co	finish ;
+	bufslide_co	adv ;
+	bufslide_co	count ;
+	bufslide_co	len ;
+	bufslide_co	extent ;
+	bufslide_fl	fl{} ;
 	bufslide(const bufslide &) = delete ;
 	bufslide &operator = (const bufslide &) = delete ;
-	bufslide(cchar * = nullptr,int = -1) noex ;
+	bufslide() noex ;
 	int add		(cchar *,int = -1) noex ;
 	int add		(int) noex ;
 	int at		(int) const noex ;
 	int readat	(int,char *,int) const noex ;
 	int read	(char *,int) const noex ;
-	int adv		(int) noex ;
 	void dtor() noex ;
 	int operator [] (int) const noex ;
 	operator int () const noex {
 	    return ilen() ;
 	} ;
 	destruct bufslide() {
-	    if (fl.open) dtor() ;
+	    if (chunkp) dtor() ;
 	} ; /* end dtor */
 } ; /* end class (bufslide) */
 
