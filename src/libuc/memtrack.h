615019bea4a889fbd42543b3a03be8509f27647d
David A.D. Morano <david.a.morano@gmail.com>
2026-03-18 00:07:15 -0400

    update

--- src/libuc/memtrack.h
+++ src/libuc/memtrack.h
@@ -21,13 +21,8 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 #endif /* __cplusplus */
