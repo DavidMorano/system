03e6f7b937e5fbd59abbd2cb7255b48eb139a162
David A.D. Morano <david.a.morano@gmail.com>
2026-03-18 00:10:03 -0400

    update

--- src/libuc/uclock/uclibsubs.h
+++ src/libuc/uclock/uclibsubs.h
@@ -75,12 +75,12 @@
 #include	<estrings.h>
 #include	<exitcodes.h>
 #include	<expcook.h>
-#include	<ffbs.h>
 #include	<fibonacci.h>
 #include	<field.h>
 #include	<filegrp.h>
 #include	<filemap.h>
 #include	<filer.h>
+#include	<findbit.h>
 #include	<findfilepath.h>
 #include	<findinline.h>
 #include	<findxfile.h>
@@ -148,6 +148,7 @@
 #include	<mkx.h>
 #include	<modload.h>
 #include	<namecache.h>
+#include	<nchr.h>
 #include	<ncol.h>
 #include	<ndig.h>
 #include	<ndigit.h>
@@ -298,7 +299,6 @@
 #include	<ucsysmisc.h>
 #include	<uctc.h>
 #include	<uctc.h>		/* terminal interface */
-#include	<uctim.h>
 #include	<uctimeconv.h>
 #include	<uctimer.h>
 #include	<ucuserattr.h>
@@ -330,11 +330,6 @@ EXTERNC_begin
 
 /* system group */
 extern int	uc_nprocs(int) noex ;
-
-/* time group */
-extern int	uc_clockset(clockid_t,const TIMESPEC *) noex ;
-extern int	uc_clockget(clockid_t,TIMESPEC *) noex ;
-extern int	uc_clockres(clockid_t,TIMESPEC *) noex ;
 
 /* process group */
 extern int	uc_atfork(void_f,void_f,void_f) noex ;
