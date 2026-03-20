6470f6e5a024c83398f2eca81b541ec3c55f6417
David A.D. Morano <david.a.morano@gmail.com>
2026-03-20 00:59:28 -0400

    update

--- src/libmailmsg/mailmsghdrx/mailmsghdrval.h
+++ src/libmailmsg/mailmsghdrx/mailmsghdrval.h
@@ -14,10 +14,7 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 #define	MAILMSGHDRVAL		struct mailmsghdrval_head
@@ -28,7 +25,7 @@ struct mailmsghdrval_head {
 	int		vlen ;
 	int		vl ;
 	int		idx ;
-} ;
+} ; /* end struct */
 
 typedef MAILMSGHDRVAL		mailmsghdrval ;
 
