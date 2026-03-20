bfb5f608902f4b67fca4b2e3d56a4856f0a8d231
David A.D. Morano <david.a.morano@gmail.com>
2026-03-20 00:59:50 -0400

    update

--- src/libmailmsg/mailmsgmatx/mailmsgmathdr.cc
+++ src/libmailmsg/mailmsgmatx/mailmsgmathdr.cc
@@ -5,7 +5,6 @@
 /* match on a message header (returns the key-name) */
 /* version %I% last-modified %G% */
 
-#define	CF_ALT1		0		/* use alternative-1 */
 
 /* revision history:
 
@@ -25,23 +24,29 @@
 	This subroutine tests whether a MSG-header is in the supplied
 	test string.  If a MSG-header is present, then the subroutine
 	returns the length of the MSG-header key, otherwise it
-	returns zero (0).
+	returns zero (0).  the string-index to the beginning of
+	the hdr-key is returned in the variable pointed to be 'vip'
+	argument.
 
 	Synopsis:
-	int mailmsgmathdr(cchar *sp,int sl,int *ip) noex
+	int mailmsgmathdr(cchar *sp,int sl,int *vip) noex
 
 	Arguments:
 	sp		test c-string pointer
 	sl		test c-string length
-	ip		pointer to string-index of header-value
+	vip		pointer to result string-index of hdr-key (optional)
 
 	Returns:
 	>0		yes-matched: length of found header-key
 	==0		no-match: did not get a match
 	<0		error code (system-return)
 
-	Design notes:
-	The 'ALT1' compile-time switch (above) was an experiment
+	Noes:
+	1. Reminder that header-key names MUST start in column
+	zero ('0') with no leading white-space characters (at all).
+	Historically, white-space WAS allowed AFTER the header-key
+	name and before the colon (':') character.
+	2.  The 'ALT1' compile-time switch (above) was an experiment
 	to try to find a faster way to execute this subroutine.
 	Profiling shows that this subroutine (as we might expect
 	already) is significant in the total performance of reading
@@ -50,24 +55,32 @@
 	performance within this subroutine has been found (so we
 	leave the compile-switch OFF).
 
+	See-also:
+	hmatch(3pcs)
+	mheader(3pcs)
+	headkeymat(3mailmsg)
+	mailmsgmathdr(3mailmsg)
+
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<cstring>		/* |strlen(3c)| */
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<char.h>
 #include	<localmisc.h>
 
 #include	"mailmsgmathdr.h"
 
-import libutil ;
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |lenstr(3u)| */
 
 /* local defines */
 
-#ifndef	CF_ALT1
-#define	CF_ALT1		0		/* use alternative-1 */
-#endif
+#define	ISBLK(ch)	isblk(ch)
+#define	ISALP(ch)	CHAR_ISALPHA(ch)
 
 
 /* external subroutines */
@@ -81,14 +94,16 @@ import libutil ;
 
 /* forward references */
 
-static inline bool ishdrwht(int ch) noex {
+local inline bool isblk(int ch) noex {
         return ((ch == ' ') || (ch == '\t')) ;
+}
+
+local inline bool iskey(int ch) noex {
+        return ISALP(ch) || (ch == '-') || (ch == '_') ;
 }
 
 
 /* local variables */
-
-constexpr bool		f_alt = CF_ALT1 ;
 
 
 /* exported variables */
@@ -96,45 +111,31 @@ constexpr bool		f_alt = CF_ALT1 ;
 
 /* exported subroutines */
 
-int mailmsgmathdr(cchar *sp,int sl,int *ip) noex {
+int mailmsgmathdr(cchar *sp,int µsl,int *vip) noex {
 	int		rs = SR_FAULT ;
-	if (sp) {
-	    int		tl ;
-	    int		kl = 0 ;
-	    cchar	*tp = sp ;
-	    if (sl < 0) sl = lenstr(sp) ;
-	    if (ip) {
-	        *ip = 0 ;
-	    }
-	    tl = sl ;
-	    if_constexpr (f_alt) {
-	        bool	f = false ;
-	        while (tl) {
-	            cint	ch = *tp ;
-		    f = (ch == 0) ;
-	            f = f || ishdrwht(ch) ;
-	            f = f || (ch == ':') ;
-		    if (f) break ;
+	int		kl = 0 ; /* return-value */
+	if (int sl ; (sl = getlenstr(sp,µsl)) >= 0) ylikely {
+	    rs = SR_OK ;
+	    if ((sl > 0) && ISALP(sp[0])) {
+	        int	tl = sl ;
+	        cchar	*tp = sp ;
+	        while (tl && *tp && iskey(*tp) && (*tp != ':')) {
 	            tp += 1 ;
 	            tl -= 1 ;
 	        } /* end while */
-	    } else {
-	        while (tl && *tp && (! ishdrwht(*tp)) && (*tp != ':')) {
+	        kl = intconv(tp - sp) ;
+	        while (tl && ISBLK(*tp)) {
 	            tp += 1 ;
 	            tl -= 1 ;
 	        } /* end while */
-	    } /* end if_constexpr (f_alt) */
-	    kl = intconv(tp - sp) ;
-	    while (tl && ishdrwht(*tp)) {
-	        tp += 1 ;
-	        tl -= 1 ;
-	    } /* end while */
-	    if (ip) {
-	        *ip = intconv((tp + 1) - sp) ;
-	    }
-	    rs = (tl && (*tp == ':')) ? kl : 0 ;
-	} /* end if (non-null) */
-	return rs ;
+	        if (tl && (*tp == ':')) {
+	            if (vip) *vip = intconv((tp + 1) - sp) ;
+		} else {
+		    kl = 0 ; /* <- signal no-match */
+		}
+	    } /* end if (required) */
+	} /* end if (gelenstr) */
+	return (rs >= 0) ? kl : rs ;
 }
 /* end subroutine (mailmsgmathdr) */
 
