d1c60afa65446af39aa0e9f3c2bc952d938fea8b
David A.D. Morano <david.a.morano@gmail.com>
2026-03-20 00:58:01 -0400

    update

--- src/libmailmsg/mailmsg/mailmsg_envtimes.cc
+++ src/libmailmsg/mailmsg/mailmsg_envtimes.cc
@@ -43,7 +43,8 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<dater.h>
 #include	<localmisc.h>
 
