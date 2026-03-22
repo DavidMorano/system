e922911dea4df46520d74e0638b5f48ade519903
David A.D. Morano <david.a.morano@gmail.com>
2026-03-22 00:47:52 -0400

    restore

--- src/libu/ufiledesc/ufiledesc.h
+++ src/libu/ufiledesc/ufiledesc.h
@@ -45,11 +45,25 @@
 #include	<uselect.h>
 #include	<usocket.h>
 #include	<usockaddr.h>
+#include	<uterminal.h>
 
 
 namespace libu {
-    int upoll(POLLFD *,int,int) noex ;
-    int uselect(int,fdset *,fdset *,fdset *,TIMEVAL *) noex ;
+    extern int ufstype(int fd,char *rbuf,int rlen) noex ;
+    extern int ulockf(int fd,int cmd,off_t sz) noex ;
+    extern int ureadp(int fd,void *rbuf,int rlen,off_t off) noex ;
+    extern int uwritep(int fd,cvoid *wbuf,int wlen,off_t off) noex ;
+    extern int uread(int fd,void *rbuf,int rlen) noex ;
+    extern int ureadv(int fd,IOVEC *iop,int n) noex ;
+    extern int ureade(int,void *,int,int,int) noex ;
+    extern int useeko(int fd,off_t wo,int w,off_t *offp) noex ;
+    extern int uwrite(int fd,cvoid *wbuf,int wlen) noex ;
+    extern int uwritev(int fd,CIOVEC *iop,int n) noex ;
+    extern int upoll(POLLFD *,int,int) noex ;
+    extern int uselect(int,fdset *,fdset *,fdset *,TIMEVAL *) noex ;
+    extern int uterminal(int) noex ;
+    extern int utermattrget(int,TERMIOS *) noex ;
+    extern int utermattrset(int,int,const TERMIOS *) noex ;
 } /* end namespace (libu) */
 
 
