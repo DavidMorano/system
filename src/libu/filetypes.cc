4718ebe138d4b9be394d27338820a26d1fdb30b5
David A.D. Morano <david.a.morano@gmail.com>
2026-03-22 00:51:33 -0400

    update

--- src/libu/filetypes.cc
+++ src/libu/filetypes.cc
@@ -9,13 +9,13 @@
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<sys/stat.h>		/* |S_IFMT| + |mode_t| */
-#include	<dirent.h>		/* possielbe preprocesor 'DT_{x}' */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
 #include	<usysdefs.h>
+#include	<localmisc.h>
+
+#include	"filetypes.h"
 
 
 /* local defines */
@@ -43,6 +43,26 @@
 
 
 /* exported variables */
+
+constexpr cpcchar	filetypes_names[] =  {
+	[filetype_unassigned0]	= "un0",
+	[filetype_fifo]		= "fifo",
+	[filetype_chr]		= "chr",
+	[filetype_unassigned3]	= "un3",
+	[filetype_dir]		= "dir",
+	[filetype_name]		= "mame",
+	[filetype_blk]		= "blk",
+	[filetype_unassigned7]	= "un7",
+	[filetype_reg]		= "reg",
+	[filetype_unassigned9]	= "un9",
+	[filetype_lnk]		= "lnk",
+	[filetype_unassigned11]	= "un11",
+	[filetype_sock]		= "sock",
+	[filetype_door]		= "door",
+	[filetype_wht]		= "wht",
+	[filetype_unassigned15]	= "un15",
+	[filetype_overlast]	= nullptr
+} ; /* end enum (filetypes_names) */
 
 
 /* exported subroutines */
