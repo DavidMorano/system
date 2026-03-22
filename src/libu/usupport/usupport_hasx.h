223fd1dfbae5c02bbaba5c3bab6cfc90b6656066
David A.D. Morano <david.a.morano@gmail.com>
2026-03-22 00:44:22 -0400

    update

--- src/libu/usupport/usupport_hasx.h
+++ src/libu/usupport/usupport_hasx.h
@@ -1,6 +1,6 @@
 /* usupport_hasx HEADER */
 /* charset=ISO8859-1 */
-/* lang=C20 */
+/* lang=C++20 */
 
 /* UNIX® kernel support subroutines */
 /* version %I% last-modified %G% */
@@ -10,22 +10,22 @@
 
 #ifndef	USUPPORTHASX_INCLUDE
 #define	USUPPORTHASX_INCLUDE
+#ifdef	__cplusplus /* (C++ only) */
 
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<usysbase.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
 
-
-#ifdef	__cplusplus
 
 namespace libu {
     extern bool hasnotdots(cchar *,int) noex ;
 }
 
-#endif /* __cplusplus */
 
-
+#endif /* __cplusplus (C++ only) */
 #endif /* USUPPORTHASX_INCLUDE */
 
 
