30e57832dfbde36639feaca183645af46636350f
David A.D. Morano <david.a.morano@gmail.com>
2026-03-23 00:25:03 -0400

    restore

---
+++ src/libu/funcodes.cc
@@ -0,0 +1,67 @@
+/* funcodes SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* function-codes (constants) fpr various services */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-13, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Object:
+	functioncodes
+
+	Description:
+	Function codes for extended Input-Output operations.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	"funcodes.h"
+
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
+/* local subroutines */
+
+
