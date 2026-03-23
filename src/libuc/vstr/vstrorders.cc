34d2206eb20dfd0d0fdc4259bd232f91afaa361b
David A.D. Morano <david.a.morano@gmail.com>
2026-03-23 00:17:39 -0400

    restore

---
+++ src/libuc/vstr/vstrorders.cc
@@ -0,0 +1,72 @@
+/* vstrorders SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* these are values (constants) for specifying comparison order */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-09-10, David A­D­ Morano
+	This program was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Group:
+	vstrorders
+
+	Description:
+
+	Arguments:
+
+	Returns:
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<localmisc.h>
+
+#include	"vstrorders.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
