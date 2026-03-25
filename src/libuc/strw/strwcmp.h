1b0add987eb8aecb23a40da87cd5bd7d2943f4f1
David A.D. Morano <david.a.morano@gmail.com>
2026-03-25 01:22:28 -0400

    update

--- src/libuc/strw/strwcmp.h
+++ src/libuc/strw/strwcmp.h
@@ -32,52 +32,11 @@ extern int strwbasecmp(cchar *,cchar *,int) noex ;
 extern int strwcasecmp(cchar *,cchar *,int) noex ;
 extern int strwfoldcmp(cchar *,cchar *,int) noex ;
 
-static inline int strwcmp(cchar *bs,cchar *sp,int sl) noex {
+local inline int strwcmp(cchar *bs,cchar *sp,int sl) noex {
 	return strwbasecmp(bs,sp,sl) ;
 }
 
-static inline int strwbasecmpo(cchar *s1,cchar *s2,int len) noex {
-	return strwbasecmp(s1,s2,len) ;
-}
-static inline int strwbasecmpr(cchar *s1,cchar *s2,int len) noex {
-	return strwbasecmp(s2,s1,len) ;
-}
-
-static inline int strwcasecmpo(cchar *s1,cchar *s2,int len) noex {
-	return strwcasecmp(s1,s2,len) ;
-}
-static inline int strwcasecmpr(cchar *s1,cchar *s2,int len) noex {
-	return strwcasecmp(s2,s1,len) ;
-}
-
-static inline int strwfoldcmpo(cchar *s1,cchar *s2,int len) noex {
-	return strwfoldcmp(s1,s2,len) ;
-}
-static inline int strwfoldcmpr(cchar *s1,cchar *s2,int len) noex {
-	return strwfoldcmp(s2,s1,len) ;
-}
-
 EXTERNC_end
-
-#ifdef	__cplusplus
-
-inline int strwcmpo(cchar *s1,cchar *s2,int len) noex {
-	return strwbasecmp(s1,s2,len) ;
-}
-inline int strwcmpr(cchar *s1,cchar *s2,int len) noex {
-	return strwbasecmp(s2,s1,len) ;
-}
-
-#else /* __cplusplus */
-
-static inline int strwcmpo(cchar *s1,cchar *s2,int len) noex {
-	return strwbasecmp(s1,s2,len) ;
-}
-static inline int strwcmpr(cchar *s1,cchar *s2,int len) noex {
-	return strwbasecmp(s2,s1,len) ;
-}
-
-#endif /* __cplusplus */
 
 
 #endif /* STRWCMP_INCLUDE */
