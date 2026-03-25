4a174c4d99df471f18b1a6126019c70c8ab1ff22
David A.D. Morano <david.a.morano@gmail.com>
2026-03-25 01:15:03 -0400

    update

--- src/libuc/uctc.h
+++ src/libuc/uctc.h
@@ -1,8 +1,8 @@
-/* uctc HEADER */
+/* uctc HEADER (LIBUC Terminal-Control) */
 /* charset=ISO8859-1 */
 /* lang=C20 */
 
-/* LIBUC Teermina-Control functions */
+/* LIBUC Teerminal-Control functions */
 /* version %I% last-modified %G% */
 
 
@@ -15,6 +15,37 @@
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
+/*******************************************************************************
+
+  	Group:
+	uctc
+
+	Names:
+	uc_tcdrain
+	uc_tcflow
+	uc_tcflush
+	uc_tcattrget
+	uc_tcattrset
+	uc_tcgetpgrp
+	uc_tcgetsid
+	uc_tcsetpgrp
+
+	Description:
+	These subroutines manage (manipulate) terminal-control
+	related settings.
+
+  	Synopsis:
+	extern int uc_tcdrain(int) noex
+	extern int uc_tcflow(int,int) noex
+	extern int uc_tcflush(int,int) noex
+	extern int uc_tcattrget(int,TERMIOS *) noex
+	extern int uc_tcattrset(int,int,const TERMIOS *) noex
+	extern int uc_tcgetpgrp(int) noex
+	extern int uc_tcgetsid(int) noex
+	extern int uc_tcsetpgrp(int,pid_t) noex
+
+*******************************************************************************/
+
 #ifndef	UCTC_INCLUDE
 #define	UCTC_INCLUDE
 
@@ -22,10 +53,7 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<termios.h>		/* |TERMIOS| */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>	/* |TERMIOS| */
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 EXTERNC_begin
