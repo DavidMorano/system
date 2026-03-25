ba8df5d86ed173b192295a5e27ae7b87731c6606
David A.D. Morano <david.a.morano@gmail.com>
2026-03-25 01:24:33 -0400

    update

--- src/libuc/snwcpy/snwcpyxc.cc
+++ src/libuc/snwcpy/snwcpyxc.cc
@@ -31,10 +31,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<toxc.h>		/* including |chtoxc(3uc)| */
 #include	<localmisc.h>
 
