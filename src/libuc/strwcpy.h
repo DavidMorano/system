7a3a0e64b60574c0e62269eda75e00c76eef30bc
David A.D. Morano <david.a.morano@gmail.com>
2026-03-25 01:21:19 -0400

    update

--- src/libuc/strwcpy.h
+++ src/libuc/strwcpy.h
@@ -20,38 +20,12 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<string.h>		/* |stpcpy(3c)| */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
 #include	<usysdefs.h>
 
 #include	<strwcpyx.h>
-
-
-#ifdef	__cplusplus
-
-constexpr inline char *strwcpy(char *dp,cchar *sp,int sl = -1) noex {
-    	if (dp && sp) {
-	    if (sl >= 0) {
-	        while (sl-- && *sp) *dp++ = *sp++ ;
-	        *dp = '\0' ;
-	    } else {
-	        dp = stpcpy(dp,sp) ;
-	    } /* end if */
-	} else {
-	    dp = nullptr ;
-	} /* end if (non-null) */
-	return dp ;
-} /* end subroutine (strwcpy) */
-
-#else /* __cplusplus */
-
-static inline char *strwcpy(char *dp,cchar *sp,int sl) noex {
-	return strwcpybc(dp,sp,sl) ;
-}
-
-#endif /* __cplusplus */
 
 
 #endif /* STRWCPY_INCLUDE */
