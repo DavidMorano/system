842b2dc3e75e7adbd92d416981935cade7ef7494
David A.D. Morano <david.a.morano@gmail.com>
2026-03-26 00:36:02 -0400

    update

--- src/libuc/hasx/hasvarprefix.cc
+++ src/libuc/hasx/hasvarprefix.cc
@@ -2,7 +2,7 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
-/* does a counted c-string have a varoable-prefix? */
+/* does the given counted c-string have a variable-prefix? */
 /* version %I% last-modified %G% */
 
 
