470157e5caa157f295b9554680f7e98ece4d9802
David A.D. Morano <david.a.morano@gmail.com>
2026-03-26 00:35:52 -0400

    update

--- src/libuc/hasx/hasprint.cc
+++ src/libuc/hasx/hasprint.cc
@@ -2,7 +2,7 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
-/* does a counted c-string has some characteristic? */
+/* does the given counted c-string have some characteristic? */
 /* version %I% last-modified %G% */
 
 
