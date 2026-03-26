aa534cf07da5da169013f59df89c9b49caa64dbc
David A.D. Morano <david.a.morano@gmail.com>
2026-03-26 00:38:53 -0400

    update

--- src/libuc/libuc.h
+++ src/libuc/libuc.h
@@ -1,73 +1,34 @@
-/* libuc HEADER */
+/* uclibsubs HEADER */
 /* charset=ISO8859-1 */
 /* lang=C20 */
 
+/* version %I% last-modified %G% */
+/* UNIX® C-language library (standard) subroutines */
+
+
 /* revision history:
 
-	= 1998-04-13, David A-D- Morano
-	Originally written for Rightcore Network Services.
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+	= 2017-08-01, David A­D­ Morano
+	Updated for lack of interfaces in MacOS Darwin
 
 */
 
-/* Copyright © 2017 David A­D­ Morano.  All rights reserved. */
-/* Use is subject to license terms. */
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
 
-#ifndef	LIBUC_INCLUDE
-#define	LIBUC_INCLUDE
+#ifndef	UCLIBSUBS_INCLUDE
+#define	UCLIBSUBS_INCLUDE
 
 
-#include	<envstandards.h>	/* ordered first to configure */
+#include	<envstandards.h>	/* MUST be first to configure */
+
+#include	<usysnative.h>		/* native system headers */
+
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-
-#include	<ucclock.h>
-#include	<ucclustername.h>
-#include	<ucent.h>
-#include	<ucenum.h>
-#include	<ucfdmanip.h>
-#include	<ucfiledesc.h>
-#include	<ucfunctioncodes.h>
-#include	<ucget.h>
-#include	<ucgethost.h>
-#include	<ucgetloadavg.h>
-#include	<ucgetpid.h>
-#include	<ucgetrandom.h>
-#include	<ucgetx.h>
-#include	<ucinetconv.h>
-#include	<uclibmemalloc.h>
-#include	<uclibsubs.h>
-#include	<uclock.h>
-#include	<ucmallocx.h>
-#include	<ucmallreg.h>
-#include	<ucmem.h>
-#include	<ucmemla.h>
-#include	<ucopen.h>
-#include	<ucopeninfo.h>
-#include	<ucpeek.h>
-#include	<ucprochave.h>
-#include	<ucproctypes.h>
-#include	<ucprogdata.h>
-#include	<ucproguser.h>
-#include	<ucpts.h>
-#include	<ucpwcache.h>
-#include	<ucrand.h>
-#include	<ucsem.h>
-#include	<ucsig.h>
-#include	<ucsthr.h>
-#include	<ucstrtod.h>
-#include	<ucstrtox.h>
-#include	<ucsys.h>
-#include	<ucsysauxinfo.h>
-#include	<ucsysconf.h>
-#include	<ucsysmisc.h>
-#include	<uctc.h>
-#include	<uctim.h>
-#include	<uctimeconv.h>
-#include	<uctimer.h>
-#include	<ucutility.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
 
 #include	<absfn.h>
 #include	<acceptpass.h>
@@ -83,7 +44,6 @@
 #include	<bufstr.h>
 #include	<bwops.h>
 #include	<cachetime.h>
-#include	<callback.h>
 #include	<calstrs.h>
 #include	<cfx.h>
 #include	<char.h>
@@ -96,25 +56,25 @@
 #include	<conmsghdr.h>
 #include	<csem.h>
 #include	<ctx.h>
-#include	<darwin_fsdir.h>
-#include	<database.h>
 #include	<dater.h>
 #include	<defproc.h>
 #include	<dictdiff.h>
 #include	<digval.h>
 #include	<dirempty.h>
+#include	<dirent.h>
 #include	<dstr.h>
 #include	<dupstr.h>
-#include	<ebuf.h>
 #include	<envhelp.h>
 #include	<envlist.h>
 #include	<envmgr.h>
 #include	<envs.h>
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<epsem.h>
+#include	<errno.h>
 #include	<estrings.h>
 #include	<exitcodes.h>
 #include	<expcook.h>
-#include	<ffbs.h>
 #include	<fibonacci.h>
 #include	<field.h>
 #include	<filegrp.h>
@@ -129,7 +89,9 @@
 #include	<fsdirtree.h>
 #include	<gecos.h>
 #include	<getbufsize.h>
+#include	<getexecname.h>
 #include	<getmjd.h>
+#include	<getngroups.h>
 #include	<getutmp.h>
 #include	<getx.h>
 #include	<getxname.h>
@@ -138,7 +100,6 @@
 #include	<grcache.h>
 #include	<grmems.h>
 #include	<hash.h>
-#include	<nonpath.h>
 #include	<hasx.h>
 #include	<hdb.h>
 #include	<hdbstr.h>
@@ -159,14 +120,12 @@
 #include	<isx.h>
 #include	<keyopt.h>
 #include	<kvsfile.h>
-#include	<libmallocxx.h>
-#include	<libuc.h>
+#include	<limits.h>
 #include	<liner.h>
 #include	<localmisc.h>
 #include	<lockfile.h>
 #include	<lookaside.h>
 #include	<mallocstuff.h>
-#include	<mallocxx.h>
 #include	<mapex.h>
 #include	<mapshmtmp.h>
 #include	<mapstrint.h>
@@ -180,19 +139,26 @@
 #include	<mkfile.h>
 #include	<mklogid.h>
 #include	<mkprogenv.h>
+#include	<mkdirs.h>
+#include	<mkfile.h>
+#include	<mklogid.h>
+#include	<mkpathxx.h>
+#include	<mkprogenv.h>
 #include	<mktmp.h>
-#include	<mkuix.h>
 #include	<mkx.h>
 #include	<modload.h>
 #include	<namecache.h>
+#include	<nchr.h>
 #include	<ncol.h>
 #include	<ndig.h>
 #include	<ndigit.h>
+#include	<netdb.h>
 #include	<netgroupcheck.h>
 #include	<netorder.h>
 #include	<nleadkeystr.h>
 #include	<nleadstr.h>
 #include	<nodedb.h>
+#include	<nonpath.h>
 #include	<nulstr.h>
 #include	<numsign.h>
 #include	<osetint.h>
@@ -202,9 +168,9 @@
 #include	<paramfile.h>
 #include	<pathadd.h>
 #include	<pathclean.h>
+#include	<permx.h>
 #include	<pow.h>
 #include	<prmktmpdir.h>
-#include	<protoent.h>
 #include	<psem.h>
 #include	<pwcache.h>
 #include	<pwentry.h>
@@ -212,13 +178,14 @@
 #include	<quoteshellarg.h>
 #include	<randlc.h>
 #include	<randomvar.h>
-#include	<rmdirs.h>
+#include	<rmdirfiles.h>
 #include	<rmx.h>
 #include	<rpsem.h>
 #include	<rsfree.h>
 #include	<sbuf.h>
+#include	<secdb.h>
 #include	<serialbuf.h>
-#include	<sesmsg.h>
+#include	<sesmsg.hh>
 #include	<setint.h>
 #include	<setstr.h>
 #include	<sfx.h>
@@ -253,7 +220,6 @@
 #include	<strnxcmp.h>
 #include	<strop.h>
 #include	<strpack.h>
-#include	<strsigabbr.h>
 #include	<strstdfname.h>
 #include	<strstore.h>
 #include	<strtab.h>
@@ -263,8 +229,9 @@
 #include	<strwcpy.h>
 #include	<strwcpyxc.h>
 #include	<strx.h>
-#include	<sysdbfn.h>
+
 #include	<sysdbfiles.h>
+#include	<sysdbfn.h>
 #include	<syshas.h>
 #include	<syshasutmpx.h>
 #include	<sysmemutil.h>
@@ -284,29 +251,179 @@
 #include	<timestr.h>
 #include	<tmpx.h>
 #include	<toxc.h>
-#include	<typenonpath.h>
+#include	<ucaddrinfo.h>
+#include	<ucclock.h>
+#include	<ucclustername.h>
+#include	<ucent.h>
+#include	<ucenum.h>
+#include	<ucfdmanip.h>
+#include	<ucfiledesc.h>
+#include	<ucfileop.h>
+#include	<ucfork.h>
+#include	<funcodes.h>
+#include	<ucget.h>
+#include	<ucgethost.h>
+#include	<ucgetloadavg.h>
+#include	<ucgetpid.h>
+#include	<ucgetrandom.h>
+#include	<ucgetx.h>
+#include	<ucids.h>
+#include	<ucinetconv.h>
+#include	<uckvamatch.h>
+#include	<uclibmem.h>
+#include	<uclock.h>
+#include	<ucmem.h>
+#include	<ucmemla.h>
+#include	<ucontext.h>
+#include	<ucopen.h>
+#include	<ucopeninfo.h>
+#include	<ucpeek.h>
+#include	<ucprochave.h>
+#include	<ucproctypes.h>
+#include	<ucprogdata.h>
+#include	<ucproguser.h>
+#include	<ucpts.h>
+#include	<ucpts.h>		/* pseudo-terminal management */
+#include	<ucpwcache.h>
+#include	<ucrand.h>
+#include	<ucread.h>
+#include	<ucsig.h>
+#include	<ucsock.h>
+#include	<ucsthr.h>
+#include	<ucstrtof.h>
+#include	<ucstrtof.h>		/* |float| + |double| + |longdouble| */
+#include	<ucstrtox.h>
+#include	<ucsys.h>
+#include	<ucsysauxinfo.h>
+#include	<ucsysconf.h>
+#include	<ucsysmisc.h>
+#include	<uctc.h>
+#include	<uctc.h>		/* terminal interface */
+#include	<uctimeconv.h>
+#include	<uctimer.h>
+#include	<ucuserattr.h>
+#include	<ucutility.h>
+#include	<ucwrite.h>
 #include	<udomain.h>
 #include	<uinfo.h>
 #include	<umask.h>
 #include	<unameo.h>
 #include	<upt.h>
-#include	<urotate.h>
 #include	<userattrdb.h>
-#include	<userattrent.h>
 #include	<userports.h>
+#include	<usyscalls.h>
 #include	<usysdefs.h>
+#include	<usysrets.h>
 #include	<uterm.h>
-#include	<utmpxx.h>
+#include	<utypealiases.h>
+#include	<utypedefs.h>
 #include	<varray.h>
 #include	<vars.h>
 #include	<vec.h>
-#include	<vstrcmpx.h>
-#include	<vstrkeycmpx.h>
+#include	<vstrcmp.h>
+#include	<vstrkeycmp.h>
 #include	<willaddover.h>
-#include	<xclustername.h>
 #include	<xfile.h>
-#include	<permx.h>
 
-#endif /* LIBUC_INCLUDE */
+
+EXTERNC_begin
+
+/* system group */
+extern int	uc_nprocs(int) noex ;
+
+/* process group */
+extern int	uc_atfork(void_f,void_f,void_f) noex ;
+
+extern int	uc_execve(cchar *,mainv,mainv) noex ;
+extern int	uc_isaexecve(cchar *,mainv,mainv) noex ;
+extern int	uc_initgroups(cchar *,gid_t) noex ;
+extern int	uc_exit(int) noex ;
+extern int	uc_atexit(void_f) noex ;
+
+extern int	uc_swapcontext(ucontext_t *,const ucontext_t *) noex ;
+extern int	uc_msync(caddr_t,size_t,int) noex ;
+
+extern int	uc_setpriority(int,id_t,int) noex ;
+extern int	uc_procpid(cchar *,uid_t) noex ;
+
+/* double-special open group */
+extern int	uc_pipe(int *) noex ;
+extern int	uc_pipes(int *,int) noex ;
+extern int	uc_piper(int *,int,int) noex ;
+extern int	uc_sockpair(int,int,int,int *) noex ;
+
+/* operate group */
+extern int	uc_mknod(cchar *,mode_t,dev_t) noex ;
+extern int	uc_statvfs(cchar *,USTATVFS *) noex ;
+extern int	uc_stat(cchar *,ustat *) noex ;
+extern int	uc_lstat(cchar *,ustat *) noex ;
+extern int	uc_readlink(cchar *,char *,int) noex ;
+extern int	uc_chmod(cchar *,mode_t) noex ;
+extern int	uc_chown(cchar *,uid_t,gid_t) noex ;
+extern int	uc_rename(cchar *,cchar *) noex ;
+extern int	uc_realpath(cchar *,char *) noex ;
+extern int	uc_resolvepath(cchar *,char *,int) noex ;
+extern int	uc_truncate(cchar *,off_t) noex ;
+
+/* use group */
+extern int	uc_fcntl(int,int,...) noex ;
+extern int	uc_ioctl(int,int,...) noex ;
+extern int	uc_fpassfd(int,int) noex ;
+extern int	uc_ftruncate(int,off_t) noex ;
+extern int	uc_fstat(int,ustat *) noex ;
+extern int	uc_fchown(int,uid_t,gid_t) noex ;
+extern int	uc_isatty(int) noex ;
+extern int	uc_fsync(int) noex ;
+extern int	uc_fsyncdata(int) noex ;
+extern int	uc_fattach(int,cchar *) noex ;
+extern int	uc_fdetach(cchar *) noex ;
+extern int	uc_minmod(cchar *,mode_t) noex ;
+extern int	uc_fminmod(int,mode_t) noex ;
+extern int	uc_fsize(int) noex ;
+extern int	uc_fuid(int) noex ;
+extern int	uc_fgid(int) noex ;
+extern int	uc_seeko(int,off_t,int,off_t *) noex ;
+extern int	uc_seek(int,off_t,int) noex ;
+extern int	uc_tell(int,off_t *) noex ;
+extern int	uc_rewind(int) noex ;
+
+extern int	uc_copy(int,int,int) noex ;
+extern int	uc_peek(int,void *,int) noex ;
+extern int	uc_keepalive(int,int) noex ;
+extern int	uc_moveup(int,int) noex ;
+extern int	uc_ndelay(int,int) noex ;
+extern int	uc_nonblock(int,int) noex ;
+extern int	uc_msgdiscard(int) noex ;
+extern int	uc_setappend(int,int) noex ;
+extern int	uc_closeonexec(int,int) noex ;
+extern int	uc_close(int) noex ;
+
+extern int	uc_remove(cchar *) noex ;
+extern int	uc_link(cchar *,cchar *) noex ;
+extern int	uc_symlink(cchar *,cchar *) noex ;
+extern int	uc_unlink(cchar *) noex ;
+extern int	uc_mkdir(cchar *,mode_t) noex ;
+extern int	uc_rmdir(cchar *) noex ;
+extern int	uc_access(cchar *,int) noex ;
+extern int	uc_pathconf(cchar *,int,long *) noex ;
+
+/* special */
+extern int	uc_safesleep(int) noex ;
+extern int	uc_ttyname(int,char *,int) noex ;
+extern int	uc_mkfifo(cchar *,mode_t) noex ;
+
+/* project related */
+extern int	uc_inproj(cchar *,cchar *,char *,int) noex ;
+
+/* POSIX® shared memory operations */
+extern int	uc_unlinkshm(cchar *) noex ;
+
+/* POSIX® semaphore */
+extern int	uc_unlinksem(cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCLIBSUBS_INCLUDE */
 
 
