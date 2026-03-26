d1510e28cb3ccdab418ecab6c3aa969241758ba5
David A.D. Morano <david.a.morano@gmail.com>
2026-03-26 00:44:07 -0400

    update

--- src/libdam/ba.h
+++ src/libdam/ba.h
@@ -21,41 +21,32 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<usysbase.h>
 
 
 #define	BA		struct ba_head
-#define	BA_NUM		struct ba_numarr
-#define	BANUM		BA_NUM
+#define	BA_DIGIT	ulong
 
-
-struct ba_numarr {
-	int		*num ;
-} ;
 
 struct ba_head {
-	BA_NUM		*cnp ;
-	ulong		*a ;
+	BA_DIGIT	*a ;
 	int		nbits ;
 	int		nwords ;
-} ;
+} ; /* end struct */
 
 typedef	BA		ba ;
-typedef	BA_NUM		ba_num ;
 
 EXTERNC_begin
 
-extern int	ba_start(ba *,ba_num *,int) noex ;
-extern int	ba_setones(ba *) noex ;
-extern int	ba_zero(ba *) noex ;
-extern int	ba_countdown(ba *) noex ;
-extern int	ba_and(ba *,ba *) noex ;
-extern int	ba_finish(ba *) noex ;
-/* helpers functions */
-extern int	banum_prepare(ba_num *) noex ;
-extern int	banum_forsake(ba_num *) noex ;
+extern int	ba_start	(ba *,int) noex ;
+extern int	ba_setones	(ba *) noex ;
+extern int	ba_zero		(ba *) noex ;
+extern int	ba_countdown	(ba *) noex ;
+extern int	ba_and		(ba *,ba *) noex ;
+extern int	ba_numones	(ba *) noex ;
+extern int	ba_shr		(ba *,int) noex ;
+extern int	ba_shl		(ba *,int) noex ;
+extern int	ba_finish	(ba *) noex ;
 
 EXTERNC_end
 
