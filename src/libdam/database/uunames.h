1aae4dbfa17e0a05383cb0972356912e9fb592c4
David A.D. Morano <david.a.morano@gmail.com>
2026-03-26 00:43:25 -0400

    update

--- src/libdam/database/uunames.h
+++ src/libdam/database/uunames.h
@@ -22,10 +22,7 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<time.h>		/* |time_t| */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<vecobj.h>
 
 
