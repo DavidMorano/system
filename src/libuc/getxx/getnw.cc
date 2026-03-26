d68a9f73cd628cac32e2a90fec565158b37a8a2f
David A.D. Morano <david.a.morano@gmail.com>
2026-03-26 00:38:11 -0400

    update

--- src/libuc/getxx/getnw.cc
+++ src/libuc/getxx/getnw.cc
@@ -37,7 +37,10 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<ucgetnw.h>
+#include	<localmisc.h>
 
 #include	"getnw.h"
 
