02ff1545614f6403098facefe89c5f798d2b21fe
David A.D. Morano <david.a.morano@gmail.com>
2026-03-27 01:17:36 -0400

    update

--- src/libuc/queue/chariq.cc
+++ src/libuc/queue/chariq.cc
@@ -26,13 +26,14 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<usyscalls.h>
+#include	<usysbase.h>
+#include	<ulogerror.h>
 #include	<uclibmem.h>
+#include	<charq.h>
+#include	<ptm.h>
 #include	<localmisc.h>
 
 #include	"chariq.h"
@@ -62,6 +63,42 @@ using libuc::libmem ;			/* variable */
 
 /* forward references */
 
+template<typename ... Args>
+local int chariq_ctor(chariq *op,Args ... args) noex {
+	cnullptr	np{} ;
+	cnothrow	nt{} ;
+	int		rs = SR_FAULT ;
+	if (op && (args && ...)) ylikely {
+	    rs = SR_NOMEM ;
+	    if ((op->cqp = new(nt) charq) != np) ylikely {
+	        if ((op->mxp = new(nt) ptm) != np) ylikely {
+		    rs = SR_OK ;
+		} /* end if (new-ptm) */
+		if (rs < 0) {
+		    delete op->cqp ;
+		    op->cqp = nullptr ;
+		}
+	    } /* end if (new-charq) */
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (chariq_ctor) */
+
+local int chariq_dtor(chariq *op) noex {
+	int		rs = SR_FAULT ;
+	if (op) ylikely {
+	    rs = SR_OK ;
+	    if (op->mxp) {
+		delete op->mxp ;
+		op->mxp = nullptr ;
+	    }
+	    if (op->cqp) {
+		delete op->cqp ;
+		op->cqp = nullptr ;
+	    }
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (chariq_dtor) */
+
 
 /* local variables */
 
@@ -72,16 +109,17 @@ using libuc::libmem ;			/* variable */
 /* exported subroutines */
 
 int chariq_start(chariq *op,int sz) noex {
-	int		rs = SR_FAULT ;
-	if (op) {
+	int		rs ;
+	if ((rs = chariq_ctor(op)) >= 0) ylikely {
+	    ptm *mxp = op->mxp ;
 	    if (sz <= 0) sz = DEFLEN ;
-	    if ((rs = ptm_create(&op->m,nullptr)) >= 0) {
-	        rs = charq_start(&op->q,sz) ;
+	    if ((rs = mxp->create) >= 0) ylikely {
+	        rs = charq_start(op->cqp,sz) ;
 	        if (rs < 0) {
-		    ptm_destroy(&op->m) ;
+		    mxp->destroy() ;
 	        }
-	    }
-	} /* end if (non-null) */
+	    } /* end if (ptm_create) */
+	} /* end if (chariq_ctor) */
 	return rs ;
 }
 /* end subroutine (chariq_start) */
@@ -89,14 +127,19 @@ int chariq_start(chariq *op,int sz) noex {
 int chariq_finish(chariq *op) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
-	if (op) {
+	if (op) ylikely {
 	    rs = SR_OK ;
-	    {
-	        rs1 = charq_finish(&op->q) ;
+	    if (op->cqp) ylikely {
+	        rs1 = charq_finish(op->cqp) ;
+	        if (rs >= 0) rs = rs1 ;
+	    }
+	    if (op->mxp) ylikely {
+	        ptm *mxp = op->mxp ;
+	        rs1 = mxp->destroy ;
 	        if (rs >= 0) rs = rs1 ;
 	    }
 	    {
-	        rs1 = ptm_destroy(&op->m) ;
+		rs1 = chariq_dtor(op) ;
 	        if (rs >= 0) rs = rs1 ;
 	    }
 	} /* end if (non-null) */
@@ -108,13 +151,14 @@ int chariq_ins(chariq *op,int ch) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
 	int		c = 0 ;
-	if (op) {
-	    if ((rs = ptm_lock(&op->m)) >= 0) {
+	if (op) ylikely {
+	    ptm *mxp = op->mxp ;
+	    if ((rs = mxp->lockbegin) >= 0) ylikely {
 	        {
-	            rs = charq_ins(&op->q,ch) ;
+	            rs = charq_ins(op->cqp,ch) ;
 		    c = rs ;
 	        }
-	        rs1 = ptm_unlock(&op->m) ;
+	        rs1 = mxp->lockend ;
 	        if (rs >= 0) rs = rs1 ;
 	    } /* end if (ptm) */
 	} /* end if (non-null) */
@@ -126,13 +170,14 @@ int chariq_rem(chariq *op,char *chp) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
 	int		c = 0 ;
-	if (op) {
-	    if ((rs = ptm_lock(&op->m)) >= 0) {
+	if (op) ylikely {
+	    ptm *mxp = op->mxp ;
+	    if ((rs = mxp->lockbegin) >= 0) ylikely {
 	        {
-	            rs = charq_rem(&op->q,chp) ;
+	            rs = charq_rem(op->cqp,chp) ;
 		    c = rs ;
 	        }
-	        rs1 = ptm_unlock(&op->m) ;
+	        rs1 = mxp->lockend ;
 	        if (rs >= 0) rs = rs1 ;
 	    } /* end if (ptm) */
 	} /* end if (non-null) */
@@ -140,17 +185,37 @@ int chariq_rem(chariq *op,char *chp) noex {
 }
 /* end subroutine (chariq_rem) */
 
+int chariq_remall(chariq *op) noex {
+	int		rs = SR_FAULT ;
+	int		rs1 ;
+	int		rv = 0 ; /* return-value */
+	if (op) ylikely {
+	    ptm *mxp = op->mxp ;
+	    if ((rs = mxp->lockbegin) >= 0) ylikely {
+		{
+		    rs = charq_remall(op->cqp) ;
+		    rv = rs ;
+		}
+	        rs1 = mxp->lockend ;
+	        if (rs >= 0) rs = rs1 ;
+	    } /* end if (ptm) */
+	} /* end if (non-null) */
+	return (rs >= 0) ? rv : rs ;
+}
+/* end subroutine (chariq_remall) */
+
 int chariq_size(chariq *op) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
-	int		rv = 0 ;
-	if (op) {
-	    if ((rs = ptm_lock(&op->m)) >= 0) {
+	int		rv = 0 ; /* return-value */
+	if (op) ylikely {
+	    ptm *mxp = op->mxp ;
+	    if ((rs = mxp->lockbegin) >= 0) ylikely {
 	        {
-	            rs = charq_size(&op->q) ;
+	            rs = charq_size(op->cqp) ;
 		    rv = rs ;
 	        }
-	        rs1 = ptm_unlock(&op->m) ;
+	        rs1 = mxp->lockend ;
 	        if (rs >= 0) rs = rs1 ;
 	    } /* end if (ptm) */
 	} /* end if (non-null) */
@@ -162,18 +227,66 @@ int chariq_count(chariq *op) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
 	int		c = 0 ;
-	if (op) {
-	    if ((rs = ptm_lock(&op->m)) >= 0) {
+	if (op) ylikely {
+	    ptm *mxp = op->mxp ;
+	    if ((rs = mxp->lockbegin) >= 0) ylikely {
 	        {
-	            rs = charq_count(&op->q) ;
+	            rs = charq_count(op->cqp) ;
 		    c = rs ;
 	        }
-	        rs1 = ptm_unlock(&op->m) ;
+	        rs1 = mxp->lockend ;
 	        if (rs >= 0) rs = rs1 ;
 	    } /* end if (ptm) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? c : rs ;
 }
 /* end subroutine (chariq_count) */
+
+int chariq::start(int µsz) noex {
+	return chariq_start(this,µsz) ;
+}
+
+int chariq::ins(int ch) noex {
+	return chariq_ins(this,ch) ;
+}
+
+int chariq::rem(char *rp) noex {
+	return chariq_rem(this,rp) ;
+}
+
+void chariq::dtor() noex {
+	if (cint rs = finish ; rs < 0) {
+	    ulogerror("chariq",rs,"fini-finish") ;
+	}
+} /* end method (chariq::dtor) */
+
+chariq::operator int () noex {
+    	return chariq_count(this) ;
+} /* end method (chariq::operator) */
+
+chariq_co::operator int () noex {
+	int		rs = SR_BUGCHECK ;
+	if (op) ylikely {
+	    switch (w) {
+	    case chariqmem_remall:
+	        rs = chariq_remall(op) ;
+	        break ;
+	    case chariqmem_size:
+	        rs = chariq_size(op) ;
+	        break ;
+	    case chariqmem_count:
+	        rs = chariq_count(op) ;
+	        break ;
+	    case chariqmem_len:
+	        rs = chariq_count(op) ;
+	        break ;
+	    case chariqmem_finish:
+	        rs = chariq_finish(op) ;
+	        break ;
+	    } /* end switch */
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end method (chariq_co::operator) */
 
 
