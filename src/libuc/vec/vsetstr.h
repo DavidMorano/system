61f89f5e85198589d20b6d599009e48481f296de
David A.D. Morano <david.a.morano@gmail.com>
2026-03-29 01:36:27 -0400

    update

--- src/libuc/vec/vsetstr.h
+++ src/libuc/vec/vsetstr.h
@@ -18,15 +18,15 @@
 #include	<vecpstr.h>
 
 
-#define	VSETSTR_MAGIC	0x09097641
-#define	VSETSTR_CSIZE	10
 #define	VSETSTR		struct vsetstr_head
 #define	VSETSTR_CUR	struct vsetstr_cursor
+#define	VSETSTR_MAGIC	0x09097641
+#define	VSETSTR_CSIZE	10
 
 
 struct vsetstr_head {
 	vecpstr		*elp ;
-	uint		magic ;
+	uint		magval ;
 } ; /* end struct (vsetstr_head) */
 
 struct vsetstr_cursor {
@@ -39,17 +39,17 @@ typedef	VSETSTR_CUR	vsetstr_cur ;
 
 EXTERNC_begin
 
-extern int vsetstr_start(vsetstr *,int) noex ;
-extern int vsetstr_already(vsetstr *,cchar *,int) noex ;
-extern int vsetstr_del(vsetstr *,cchar *,int) noex ;
-extern int vsetstr_count(vsetstr *) noex ;
-extern int vsetstr_extent(vsetstr *) noex ;
-extern int vsetstr_curbegin(vsetstr *,vsetstr_cur *) noex ;
-extern int vsetstr_curend(vsetstr *,vsetstr_cur *) noex ;
-extern int vsetstr_curdel(vsetstr *,vsetstr_cur *) noex ;
-extern int vsetstr_curenum(vsetstr *,vsetstr_cur *,cchar **) noex ;
-extern int vsetstr_curnext(vsetstr *,vsetstr_cur *) noex ;
-extern int vsetstr_finish(vsetstr *) noex ;
+extern int vsetstr_start	(vsetstr *,int) noex ;
+extern int vsetstr_already	(vsetstr *,cchar *,int) noex ;
+extern int vsetstr_del		(vsetstr *,cchar *,int) noex ;
+extern int vsetstr_count	(vsetstr *) noex ;
+extern int vsetstr_extent	(vsetstr *) noex ;
+extern int vsetstr_curbegin	(vsetstr *,vsetstr_cur *) noex ;
+extern int vsetstr_curend	(vsetstr *,vsetstr_cur *) noex ;
+extern int vsetstr_curdel	(vsetstr *,vsetstr_cur *) noex ;
+extern int vsetstr_curenum	(vsetstr *,vsetstr_cur *,cchar **) noex ;
+extern int vsetstr_curnext	(vsetstr *,vsetstr_cur *) noex ;
+extern int vsetstr_finish	(vsetstr *) noex ;
 
 EXTERNC_end
 
