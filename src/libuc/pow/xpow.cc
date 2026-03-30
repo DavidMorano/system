130d163f3a7ad19bfa6a6fc5f2e5bf6e43dc61e0
David A.D. Morano <david.a.morano@gmail.com>
2026-03-30 01:04:57 -0400

    update

--- src/libuc/pow/xpow.cc
+++ src/libuc/pow/xpow.cc
@@ -5,7 +5,6 @@
 /* calculate integer-power */
 /* version %I% last-modified %G% */
 
-#define	CF_DYNAMIC	1		/* dynamic programming */
 
 /* revision history:
 
@@ -41,8 +40,10 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
+#include	<climits>		/* |CHAR_BIT| */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
+#include	<bit>
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
@@ -54,10 +55,6 @@
 
 
 /* local defines */
-
-#ifndef	CF_DYNAMIC
-#define	CF_DYNAMIC	1		/* dynamic programming */
-#endif
 
 
 /* imported namespaces */
@@ -74,20 +71,60 @@
 
 /* local structures */
 
-
-/* forward references */
-
-local int	ipow10(int) noex ;
-
-
-/* local variables */
-
 constexpr int	b10tab[] = {
 	1, 10, 100, 1000, 10000, 100000, 1000000,
 	10000000, 100000000, 1000000000
-} ;
+} ; /* end array */
 
-constexpr bool	f_dynamic = CF_DYNAMIC ;
+
+/* forward references */
+
+template<typename T> local T xpowraw(T b,int n) noex {
+    	T	r = 1 ;
+	if (n == 1) {
+	    r = b ;
+	} else if (n == 2) { /* common case */
+	    r = b * b ;
+	} else if (n > 2) {
+	    cint t = xpowraw(b,(n / 2)) ;
+	    if ((n & 1) == 0) {
+		r = (t * t) ;
+	    } else {
+		r = b * (t * t) ;
+	    }
+	} /* end if */
+	return r ;
+} /* end subroutine (xpowraw) */
+
+template<typename T> local T xpowten(T b,int n) noex {
+	int		r = 0 ;
+	if (n < 9) {
+	    r = b10tab[n] ;
+	} else if (szof(T) > szof(int)) {
+	    r = xpowraw(b,n) ;
+	} /* end if */
+	return r ;
+} /* end subroutine (ipowten) */
+
+template<typename T> local T xpow(T b,int n) noex {
+	T	r = 1 ;
+	if (b == 10) {
+	    r = ipow10(b,n) ;
+	} else if (b == 2) {
+	    cint nb = (szof(T) * CHAR_BIT) ;
+	    if (b < 32) {
+	        r = (1 << n) ;
+	    } else {
+		r = 0 ;
+	    }
+	} else {
+	    r = xpowraw(b,n) ;
+	} /* end if (base-specialization) */
+	return r ;
+} /* end subroutine (zpow) */
+
+
+/* local variables */
 
 
 /* exported variables */
@@ -95,68 +132,19 @@ constexpr bool	f_dynamic = CF_DYNAMIC ;
 
 /* exported subroutines */
 
-int ipow(int b,int n) noex {
-	int		r = 1 ;
-	if (b == 10) {
-	    r = ipow10(n) ;
-	} else if (b == 2) {
-	    if (b < 32) {
-	        r = (1 << n) ;
-	    } else {
-		r = 0 ;
-	    }
-	} else {
-	    if_constexpr (f_dynamic) {
-	        if (n == 1) {
-	            r = b ;
-	        } else if (n == 2) { /* common case */
-	            r = b * b ;
-	        } else if (n > 2) {
-	            cint	t = ipow(b,(n/2)) ;
-	            if ((n & 1) == 0) {
-		        r = (t * t) ;
-	            } else {
-		        r = b * (t * t) ;
-	            }
-	        } /* end if */
-	    } else {
-	        for (int i = 0 ; i < n ; i += 1) {
-	            r *= b ;
-	        } /* end for */
-	    } /* end if_constexpr (f_dynamic) */
-	} /* end if (base-specialization) */
-	return r ;
-}
-/* end subroutine (ipow) */
+int		ipow(long b,int n) noex {
+    	return xpow(b,n) ;
+} /* end subroutine (ipow) */
 
-long lpow(long b,int n) noex {
-	long		v = 1 ;
-	for (int i = 1 ; i < n ; i += 1) {
- 	    v *= b ;
-	}
-	return v ;
-}
-/* end subroutine (lpow) */
+long 		lpow(long b,int n) noex {
+    	return xpow(b,n) ;
+} /* end subroutine (lpow) */
 
-longlong llpow(longlong b,int n) noex {
-	longlong	v = 1 ;
-	for (int i = 1 ; i < n ; i += 1) {
- 	    v *= b ;
-	}
-	return v ;
-}
-/* end subroutine (llpow) */
+longlong	llpow(longlong b,int n) noex {
+    	return xpow(b,n) ;
+} /* end subroutine (llpow) */
 
 
 /* local subroutines */
-
-local int ipow10(int n) noex {
-	int		r = 0 ;
-	if (n < 9) {
-	    r = b10tab[n] ;
-	} /* end if */
-	return r ;
-}
-/* end subroutine (ipow10) */
 
 
