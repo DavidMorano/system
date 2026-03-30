aa1e53238b8da1559ad25a374937b8327b2de4d5
David A.D. Morano <david.a.morano@gmail.com>
2026-03-30 01:04:13 -0400

    update

--- src/libuc/ourmjd.h
+++ src/libuc/ourmjd.h
@@ -2,18 +2,46 @@
 /* charset=ISO8859-1 */
 /* lang=C20 */
 
-/* test if the given counted c-string has "our" Modified-Julian-Date (MJD) */
+/* convert a counted c-string to Modified-Juluan-Day (MJD) */
 /* version %I% last-modified %G% */
 
 
 /* revision history:
 
-	= 1998-04-10, David A­D­ Morano
+	= 1998-11-01, David A­D­ Morano
 	This subroutine was written for Rightcore Network Services.
 
 */
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	ourmjd
+
+	Description:
+	Does the given string contain a Modified-Julian-Day (MJD)
+	specification?
+
+	Synopsis:
+	int ourmjd(cchar *sp,int sl) noex
+
+	Arguments:
+	sp		string to test
+	sl		length of strin to test
+
+	Returns:
+	>0		MJD
+	0		no MJD found
+	<0		error (system-return)
+
+	Notes:
+	1. This subrouine is meant to replace the older:
+		hasourmjd
+	subroutine.
+
+*******************************************************************************/
 
 #ifndef	OURMJD_INCLUDE
 #define	OURMJD_INCLUDE
@@ -21,10 +49,7 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 EXTERNC_begin
