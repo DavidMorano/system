5d1208e2aa2b2c9c75ff8c17f601c4d58459f0d3
David A.D. Morano <david.a.morano@gmail.com>
2026-03-31 01:46:55 -0400

    cleanup

--- src/libu/uatfork.h
+++ src/libu/uatfork.h
@@ -6,7 +6,7 @@
 /* version %I% last-modified %G% */
 
 
-/* Copyright © 1998,2014 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	UATFORK_INCLUDE
 #define	UATFORK_INCLUDE
