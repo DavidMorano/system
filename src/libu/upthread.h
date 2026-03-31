c42f6eb7ef0f2f93a12af2b97a39c366827477ad
David A.D. Morano <david.a.morano@gmail.com>
2026-03-31 01:47:00 -0400

    cleanup

--- src/libu/upthread.h
+++ src/libu/upthread.h
@@ -29,11 +29,11 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<signal.h>
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
 
 
 EXTERNC_begin
