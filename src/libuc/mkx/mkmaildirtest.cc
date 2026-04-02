add082350ccb91b1f884444cc332bd3563d2b043
David A.D. Morano <david.a.morano@gmail.com>
2026-04-02 00:32:41 -0400

    update

--- src/libuc/mkx/mkmaildirtest.cc
+++ src/libuc/mkx/mkmaildirtest.cc
@@ -44,10 +44,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<bufsizevar.hh>
 #include	<storebuf.h>
 #include	<localmisc.h>
@@ -74,7 +71,7 @@
 
 /* local variables */
 
-static bufsizevar	maxpathlen(getbufsize_mp) ;
+static bufsizevar	maxpathlen(bufsize_mp) ;
 
 constexpr char		saved[] = MAILDIR_SAVED ;
 
