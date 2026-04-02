adff260e10e6e43daadd69e5efd96c084f92fe1c
David A.D. Morano <david.a.morano@gmail.com>
2026-04-02 00:25:27 -0400

    restore

---
+++ src/libu/usupport/usupport_getsign.h
@@ -0,0 +1,31 @@
+/* usupport_getsign HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* UNIX® kernel support subroutines */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USUPPORTGETSIGN_INCLUDE
+#define	USUPPORTGETSIGN_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<stddef.h>		/* |nullptr_t| */
+#include	<stdlib.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+namespace libu {
+    extern int getsign(cchar *,int,bool *) noex ;
+} /* end namespace (libu) */
+
+
+#endif /* __cplusplus */
+#endif /* USUPPORTGETSIGN_INCLUDE */
+
+
