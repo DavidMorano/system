8d55316c66ca339f04161b560679dd6a38b20976
David A.D. Morano <david.a.morano@gmail.com>
2026-04-02 00:31:16 -0400

    update

--- src/libuc/mkx/mkbasename.cc
+++ src/libuc/mkx/mkbasename.cc
@@ -44,10 +44,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<sfx.h>			/* |sfbasename(3uc)| */
 #include	<snwcpy.h>		/* |snwcpy(3uc)| */
 #include	<localmisc.h>
