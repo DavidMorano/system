a21ba0ea342205d5519a799540659b507a1b3805
David A.D. Morano <david.a.morano@gmail.com>
2026-04-02 00:31:49 -0400

    update

--- src/libuc/mkx/mkfingerquery.cc
+++ src/libuc/mkx/mkfingerquery.cc
@@ -47,10 +47,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<sbuf.h>
 #include	<localmisc.h>
 
