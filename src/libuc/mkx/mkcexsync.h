f21c273cba4d90b6dfeffa872745cea9afa9b18e
David A.D. Morano <david.a.morano@gmail.com>
2026-04-02 00:31:29 -0400

    update

--- src/libuc/mkx/mkcexsync.h
+++ src/libuc/mkx/mkcexsync.h
@@ -14,10 +14,7 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 #define	MKCEXSYNC_MKLEN		10	/* length to create (in bytes) */
