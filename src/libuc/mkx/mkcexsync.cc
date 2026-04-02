0507eb5dcb6af5454205ec70b53620ddb60fc087
David A.D. Morano <david.a.morano@gmail.com>
2026-04-02 00:31:26 -0400

    update

--- src/libuc/mkx/mkcexsync.cc
+++ src/libuc/mkx/mkcexsync.cc
@@ -41,10 +41,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<ascii.h>
 #include	<localmisc.h>
 
