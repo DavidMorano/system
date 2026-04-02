9e6906ba5e6fc2c4e2a92a485ab0ef8ede116fa5
David A.D. Morano <david.a.morano@gmail.com>
2026-04-02 00:31:34 -0400

    update

--- src/libuc/mkx/mkdisplayable.cc
+++ src/libuc/mkx/mkdisplayable.cc
@@ -46,10 +46,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<sbuf.h>
 #include	<ascii.h>		/* |CH_{xx}| */
 #include	<mkchar.h>
