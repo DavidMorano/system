6a64e4455dadcbb051a4784da59e38f4b66c5a7a
David A.D. Morano <david.a.morano@gmail.com>
2026-04-02 00:32:28 -0400

    update

--- src/libuc/mkx/mklineclean.h
+++ src/libuc/mkx/mklineclean.h
@@ -8,12 +8,47 @@
 
 /* revision history:
 
-	= 2000-05-14, David A-D- Morano
-	This was written for Rightcore Network Services (RNS).
- 
+	= 1998-02-01, David A­D­ Morano
+	This code was originally written.
+
 */
 
-/* Copyright © 2000 David A-D- Morano.  All rights reserved. */
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	mklineclean
+
+	Description:
+	This subroutine cleans up a line of text.  A mode paramter
+	specifies how clean the resulting line gets.  To wit:
+
+	mode	delete­or­sub	terminate­on­nul
+	----------------------------------------
+	0	delete		YES
+	1	sub		YES
+	2	delete		NO
+	3	sub		NO
+
+	Synopsis:
+	int mklineclean(char *rbuf,int rlen,int m,cchar *lp,int ll) noex
+
+	Arguments:
+	rbuf		result buffer pointer
+	rlen		result buffer length
+	m		mode parameter (see above on use of mode)
+	lp		source counted c-string pointer
+	ll		source counted c-string length
+
+	Returns:
+	-		length of resulting line (not NL terminated)
+
+	Important note:
+	Resulting lines are never NL (EOL) terminated - even if the
+	input line was!
+
+*******************************************************************************/
 
 #ifndef	MKLINECLEAN_INCLUDE
 #define	MKLINECLEAN_INCLUDE
@@ -21,10 +56,7 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<linecleanopt.h>	/* line-cleaning options */
 
 
