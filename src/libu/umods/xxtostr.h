2c56b8279d97687f5a36c6b70ab2c5f35811d565
David A.D. Morano <david.a.morano@gmail.com>
2026-04-03 01:24:58 -0400

    restore

---
+++ src/libu/umods/xxtostr.h
@@ -0,0 +1,32 @@
+/* xxtostr HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* subroutines to convert an integer to a c-string */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	XXTOSTR_INCLUDE
+#define	XXTOSTR_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* XXTOSTR_INCLUDE */
+
+
