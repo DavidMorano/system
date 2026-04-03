b31c672b86d972d1261365a3733338105e4443f8
David A.D. Morano <david.a.morano@gmail.com>
2026-04-03 01:29:43 -0400

    update

--- src/libu/usupport/usupport_ctdec.cc
+++ src/libu/usupport/usupport_ctdec.cc
@@ -31,10 +31,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<new>			/* |nothrow(3c++)| */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<stdintx.h>
 #include	<xxtostr.h>		/* |{x}tostr(3u)| */
 #include	<localmisc.h>		/* |DECBUFLEN| */
