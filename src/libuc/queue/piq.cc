b721c86d9420cefd32e4354e3a21040dc49ca649
David A.D. Morano <david.a.morano@gmail.com>
2026-04-04 00:57:04 -0400

    update

--- src/libuc/queue/piq.cc
+++ src/libuc/queue/piq.cc
@@ -31,11 +31,8 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<usyscalls.h>
+#include	<usysbase.h>
+#include	<ulogerror.h>
 #include	<uclibmem.h>
 #include	<ptm.h>
 #include	<pq.h>
@@ -68,13 +65,13 @@ using std::nothrow ;			/* constant */
 /* forward references */
 
 template<typename ... Args>
-static inline int piq_ctor(piq *op,Args ... args) noex {
+local inline int piq_ctor(piq *op,Args ... args) noex {
 	cnullptr	np{} ;
 	int		rs = SR_FAULT ;
-	if (op && (args && ...)) {
+	if (op && (args && ...)) ylikely {
 	    rs = SR_NOMEM ;
-	    if ((op->mxp = new(nothrow) ptm) != np) {
-	        if ((op->qlp = new(nothrow) pq) != np) {
+	    if ((op->mxp = new(nothrow) ptm) != np) ylikely {
+	        if ((op->qlp = new(nothrow) pq) != np) ylikely {
 		    rs = SR_OK ;
 	        } /* end if (new-pq) */
 		if (rs < 0) {
@@ -86,15 +83,15 @@ static inline int piq_ctor(piq *op,Args ... args) noex {
 	return rs ;
 } /* end subroutine (piq_ctor) */
 
-static inline int piq_dtor(piq *op) noex {
+local inline int piq_dtor(piq *op) noex {
 	int		rs = SR_FAULT ;
-	if (op) {
+	if (op) ylikely {
 	    rs = SR_OK ;
-	    if (op->qlp) {
+	    if (op->qlp) ylikely {
 		delete op->qlp ;
 		op->qlp = nullptr ;
 	    }
-	    if (op->mxp) {
+	    if (op->mxp) ylikely {
 		delete op->mxp ;
 		op->mxp = nullptr ;
 	    }
@@ -103,10 +100,10 @@ static inline int piq_dtor(piq *op) noex {
 } /* end subroutine (piq_dtor) */
 
 template<typename ... Args>
-static inline int piq_magic(piq *op,Args ... args) noex {
+local inline int piq_magic(piq *op,Args ... args) noex {
 	int		rs = SR_FAULT ;
-	if (op && (args && ...)) {
-	    rs = (op->magic == PIQ_MAGIC) ? SR_OK : SR_NOTOPEN ;
+	if (op && (args && ...)) ylikely {
+	    rs = (op->magval == PIQ_MAGIC) ? SR_OK : SR_NOTOPEN ;
 	}
 	return rs ;
 } /* end subroutine (piq_magic) */
@@ -121,14 +118,15 @@ static inline int piq_magic(piq *op,Args ... args) noex {
 /* exported subroutines */
 
 int piq_start(piq *op) noex {
-	int		rs = SR_FAULT ;
-	if ((rs = piq_ctor(op)) >= 0) {
-	    if ((rs = ptm_create(op->mxp,nullptr)) >= 0) {
-	        if ((rs = pq_start(op->qlp)) >= 0) {
-		    op->magic = PIQ_MAGIC ;
+	int		rs ;
+	if ((rs = piq_ctor(op)) >= 0) ylikely {
+	    ptm *mxp = op->mxp ;
+	    if ((rs = mxp->create) >= 0) ylikely {
+	        if ((rs = pq_start(op->qlp)) >= 0) ylikely {
+		    op->magval = PIQ_MAGIC ;
 	        }
 	        if (rs < 0)
-		    ptm_destroy(op->mxp) ;
+		    mxp->destroy() ;
 	    }
 	    if (rs < 0) {
 		piq_dtor(op) ;
@@ -141,20 +139,21 @@ int piq_start(piq *op) noex {
 int piq_finish(piq *op) noex {
 	int		rs ;
 	int		rs1 ;
-	if ((rs = piq_magic(op)) >= 0) {
-	    {
+	if ((rs = piq_magic(op)) >= 0) ylikely {
+	    if (op->qlp) ylikely {
 	        rs1 = pq_finish(op->qlp) ;
 	        if (rs >= 0) rs = rs1 ;
 	    }
-	    {
-	        rs1 = ptm_destroy(op->mxp) ;
+	    if (op->mxp) ylikely {
+	        ptm *mxp = op->mxp ;
+	        rs1 = mxp->destroy ;
 	        if (rs >= 0) rs = rs1 ;
 	    }
 	    {
 	        rs1 = piq_dtor(op) ;
 	        if (rs >= 0) rs = rs1 ;
 	    }
-	    op->magic = 0 ;
+	    op->magval = 0 ;
 	} /* end if (magic) */
 	return rs ;
 }
@@ -164,16 +163,17 @@ int piq_ins(piq *op,void *vp) noex {
 	int		rs ;
 	int		rs1 ;
 	int		c = 0 ;
-	if ((rs = piq_magic(op,vp)) >= 0) {
-	        if ((rs = ptm_lock(op->mxp)) >= 0) {
-	            {
-		        pq_ent	*pep = (pq_ent *) vp ;
-	                rs = pq_ins(op->qlp,pep) ;
-	                c = rs ;
-	            }
-	            rs1 = ptm_unlock(op->mxp) ;
-		    if (rs >= 0) rs = rs1 ;
-	        } /* end if (mutex) */
+	if ((rs = piq_magic(op,vp)) >= 0) ylikely {
+	    ptm *mxp = op->mxp ;
+	    if ((rs = mxp->lockbegin) >= 0) ylikely {
+	        {
+		    pq_ent	*pep = (pq_ent *) vp ;
+	            rs = pq_ins(op->qlp,pep) ;
+	            c = rs ;
+	        }
+	        rs1 = mxp->lockend ;
+		if (rs >= 0) rs = rs1 ;
+	    } /* end if (mutex) */
 	} /* end if (magic) */
 	return (rs >= 0) ? c : rs ;
 }
@@ -183,14 +183,15 @@ int piq_rem(piq *op,void *vrp) noex {
 	int		rs ;
 	int		rs1 ;
 	int		c = 0 ;
-	if ((rs = piq_magic(op)) >= 0) {
+	if ((rs = piq_magic(op)) >= 0) ylikely {
 	    void	**vpp = (void **) vrp ;
-	    if ((rs = ptm_lock(op->mxp)) >= 0) {
+	    ptm *mxp = op->mxp ;
+	    if ((rs = mxp->lockbegin) >= 0) ylikely {
 	        if (pq_ent *pep{} ; (rs = pq_remtail(op->qlp,&pep)) >= 0) {
 	            c = rs ;
 	            if (vpp) *vpp = pep ;
 	        }
-	        rs1 = ptm_unlock(op->mxp) ;
+	        rs1 = mxp->lockend ;
 		if (rs >= 0) rs = rs1 ;
 	    } /* end if (mutex) */
 	} /* end if (magic) */
@@ -202,15 +203,16 @@ int piq_count(piq *op) noex {
 	int		rs ;
 	int		rs1 ;
 	int		c = 0 ;
-	if ((rs = piq_magic(op)) >= 0) {
-	        if ((rs = ptm_lock(op->mxp)) >= 0) {
-	            {
-	                rs = pq_count(op->qlp) ;
-	                c = rs ;
-	            }
-	            rs1 = ptm_unlock(op->mxp) ;
-		    if (rs >= 0) rs = rs1 ;
-	        } /* end if (mutex) */
+	if ((rs = piq_magic(op)) >= 0) ylikely {
+	    ptm *mxp = op->mxp ;
+	    if ((rs = mxp->lockbegin) >= 0) ylikely {
+	        {
+	            rs = pq_count(op->qlp) ;
+	            c = rs ;
+	        }
+	        rs1 = mxp->lockend ;
+		if (rs >= 0) rs = rs1 ;
+	    } /* end if (mutex) */
 	} /* end if (magic) */
 	return (rs >= 0) ? c : rs ;
 }
@@ -220,18 +222,60 @@ int piq_audit(piq *op) noex {
 	int		rs ;
 	int		rs1 ;
 	int		c = 0 ;
-	if ((rs = piq_magic(op)) >= 0) {
-	        if ((rs = ptm_lock(op->mxp)) >= 0) {
-	            {
-	                rs = pq_audit(op->qlp) ;
-		        c = rs ;
-    	            }
-	            rs1 = ptm_unlock(op->mxp) ;
-		    if (rs >= 0) rs = rs1 ;
-	        } /* end if (mutex) */
+	if ((rs = piq_magic(op)) >= 0) ylikely {
+	    ptm *mxp = op->mxp ;
+	    if ((rs = mxp->lockbegin) >= 0) ylikely {
+	        {
+	            rs = pq_audit(op->qlp) ;
+		    c = rs ;
+	        }
+	        rs1 = mxp->lockend ;
+		if (rs >= 0) rs = rs1 ;
+	    } /* end if (mutex) */
 	} /* end if (magic) */
 	return (rs >= 0) ? c : rs ;
 }
 /* end subroutine (piq_audit) */
+
+int piq::ins(void *ep) noex {
+	return piq_ins(this,ep) ;
+}
+
+int piq::rem(void *rpp) noex {
+	return piq_rem(this,rpp) ;
+}
+
+void piq::dtor() noex {
+	if (cint rs = finish ; rs < 0) {
+	    ulogerror("piq",rs,"fini-finish") ;
+	}
+} /* end method (piq::dtor) */
+
+piq::operator int () noex {
+	return piq_count(this) ;
+} /* end method (piq::operator) */
+
+piq_co::operator int () noex {
+	int		rs = SR_BUGCHECK ;
+	if (op) ylikely {
+	    switch (w) {
+	    case piqmem_start:
+	        rs = piq_start(op) ;
+	        break ;
+	    case piqmem_count:
+	        rs = piq_count(op) ;
+	        break ;
+	    case piqmem_audit:
+	        rs = piq_audit(op) ;
+	        break ;
+	    case piqmem_finish:
+	        rs = piq_finish(op) ;
+	        break ;
+	    } /* end switch */
+	} /* end if (non-null) */
+	return rs ;
+}
+/* end method (piq_co::operator) */
+
 
 
