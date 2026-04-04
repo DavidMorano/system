5cb2f0f2b0f33444c72a89c26ec87b190926fc97
David A.D. Morano <david.a.morano@gmail.com>
2026-04-04 00:55:57 -0400

    update

--- src/libuc/queue.h
+++ src/libuc/queue.h
@@ -21,10 +21,7 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 #include	<aiq.h>
 #include	<chariq.h>
