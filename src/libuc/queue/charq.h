abbd95f537aec506da5813b9c760312a5d62d92c
David A.D. Morano <david.a.morano@gmail.com>
2026-04-04 00:56:23 -0400

    update

--- src/libuc/queue/charq.h
+++ src/libuc/queue/charq.h
@@ -15,12 +15,23 @@
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
+/*******************************************************************************
+
+  	Object:
+	charq
+
+	Description:
+	This module implements a character queue object.
+
+*******************************************************************************/
+
 #ifndef	CHARQ_INCLUDE
 #define	CHARQ_INCLUDE
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 
 
 #define	CHARQ		struct charq_head
@@ -31,10 +42,61 @@ struct charq_head {
 	int		qlen ;
 	int		ri ;
 	int		wi ;
-	int		count ;
-} ;
+	int		cnt ;
+} ; /* end enum (charq_head) */
 
+#ifdef	__cplusplus
+enum charqmems {
+	charqmem_start,
+	charqmem_remall,
+	charqmem_size,
+	charqmem_count,
+	charqmem_len,
+	charqmem_finish,
+	charqmem_overlast
+} ; /* end enum (charqmems) */
+struct charq ;
+struct charq_co {
+        charq		*op = nullptr ;
+        int             w = -1 ;
+        constexpr void operator () (charq *p,int m) noex {
+            op = p ;
+            w = m ;
+        } ;
+	int operator () (int = 0) noex ;
+        operator int () noex {
+	    return operator () (0) ;
+	} ;
+} ; /* end struct (charq_co) */
+struct charq : charq_head {
+	charq_co	start ;
+	charq_co	remall ;
+	charq_co	size ;
+	charq_co	count ;
+	charq_co	len ;
+	charq_co	finish ;
+	constexpr charq() noex {
+	    start	(this,charqmem_start) ;
+	    remall	(this,charqmem_remall) ;
+	    size	(this,charqmem_size) ;
+	    count	(this,charqmem_count) ;
+	    len		(this,charqmem_len) ;
+	    finish	(this,charqmem_finish) ;
+	    qbuf = nullptr ;
+	} ; /* end ctor */
+	charq(const charq &) = delete ;
+	charq &operator = (const charq &) = delete ;
+	int	ins(int) noex ;
+	int	rem(char *) noex ;
+	void	dtor() noex ;
+	operator int () noex ;
+	destruct charq() {
+	    if (qbuf) dtor() ;
+	} ; /* end dtor (charq) */
+} ; /* end class (charq) */
+#else
 typedef CHARQ		charq ;
+#endif /* __cplusplus */
 
 EXTERNC_begin
 
