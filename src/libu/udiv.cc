4161d338d9fffe82c064c7b4b54549368126c7ca
David A.D. Morano <david.a.morano@gmail.com>
2026-04-04 00:53:26 -0400

    restore

---
+++ src/libu/udiv.cc
@@ -0,0 +1,68 @@
+/* udiv SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* concepts related to extended integer types (and operations on them) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2001-03-24, David A­D­ Morano
+	This code is newly written but modeled after some code that
+	was originally written to get some standardized variable
+	serialization for the Ethernet-Controller (ETHCON) development
+	work.
+
+*/
+
+/* Copyright © 2001 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Concept:
+
+	Description:
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<localmisc.h>
+
+#include	"udiv.hh"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
