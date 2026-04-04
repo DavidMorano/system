111c21072d9c9ea459c7c29ae695978592295a79
David A.D. Morano <david.a.morano@gmail.com>
2026-04-04 00:56:36 -0400

    update

--- src/libuc/queue/fifoelem.h
+++ src/libuc/queue/fifoelem.h
@@ -13,17 +13,16 @@
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<stddef.h>
+#include	<stdlib.h>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
-#define	FIFOELEM_MAGIC	0x12345678
 #define	FIFOELEM	struct fifoelem_head
 #define	FIFOELEM_ENT	struct fifoelem_entry
 #define	FIFOELEM_CUR	struct fifoelem_cursor
+#define	FIFOELEM_MAGIC	0x12345678
 
 
 struct fifoelem_entry {
@@ -31,47 +30,98 @@ struct fifoelem_entry {
 	FIFOELEM_ENT	*prev ;
 	void		*dp ;		/* data-pointer */
 	int		dl ;		/* data-length */
-} ;
-
+} ; /* end struct (fifoelem_entry) */
 
 struct fifoelem_head {
 	FIFOELEM_ENT	*head ;
 	FIFOELEM_ENT	*tail ;
-	uint		magic ;
+	uint		magval ;
 	int		n ;
-} ;
-
+	int		esz ;
+} ; /* end struct (fifoelem_head) */
 
 struct fifoelem_cursor {
 	FIFOELEM_ENT	*current ;
-} ;
+} ; /* end struct (fifoelem_cursor) */
 
-
-typedef FIFOELEM	fifoelem ;
 typedef FIFOELEM_ENT	fifoelem_ent ;
 typedef FIFOELEM_CUR	fifoelem_cur ;
 
 EXTERNC_begin
+typedef int (*fifoelem_cmp)(cvoid *,cvoid *) noex ;
+EXTERNC_end
 
-typedef int	(*fifoelem_cmp)(cvoid *,cvoid *) noex ;
 
-extern int fifoelem_start(fifoelem *) noex ;
-extern int fifoelem_finish(fifoelem *) noex ;
-extern int fifoelem_add(fifoelem *,void *,int) noex ;
-extern int fifoelem_rem(fifoelem *,void *,int) noex ;
-extern int fifoelem_get(fifoelem *,fifoelem_ent **) noex ;
-extern int fifoelem_del(fifoelem *) noex ;
-extern int fifoelem_count(fifoelem *) noex ;
-extern int fifoelem_curbegin(fifoelem *,fifoelem_cur *) noex ;
-extern int fifoelem_curend(fifoelem *,fifoelem_cur *) noex ;
-extern int fifoelem_curdel(fifoelem *,fifoelem_cur *) noex ;
-extern int fifoelem_curfetch(fifoelem *,fifoelem_cur *,fifoelem_ent **) noex ;
-extern int fifoelem_present(fifoelem *,cvoid *,int,fifoelem_cmp) noex ;
+#ifdef	__cplusplus
+enum fifoelemmems {
+	fifoelemmem_del,
+	fifoelemmem_count,
+	fifoelemmem_finish,
+	fifoelemmem_overlast
+} ; /* end enum (fifoelemmems) */
+struct fifoelem ;
+struct fifoelem_co {
+	fifoelem	*op = nullptr ;
+	int		w = -1 ;
+	void operator () (fifoelem *p,int m) noex {
+	    op = p ;
+	    w = m ;
+	} ;
+	operator int () noex ;
+	int operator () () noex {
+	    return operator int () ;
+	} ;
+} ; /* end struct (fifoelem_co) */
+struct fifoelem : fifoelem_head {
+	fifoelem_co	del ;
+	fifoelem_co	count ;
+	fifoelem_co	finish ;
+	fifoelem() noex {
+	    del		(this,fifoelemmem_del) ;
+	    count	(this,fifoelemmem_count) ;
+	    finish	(this,fifoelemmem_finish) ;
+	    magval = 0 ;
+	} ; /* end ctor */
+	fifoelem(const fifoelem &) = delete ;
+	fifoelem &operator = (const fifoelem &) = delete ;
+	int start	(int) noex ;
+	int ins		(void *) noex ;
+	int get		(fifoelem_ent **) noex ;
+	int rem		(void *,int) noex ;
+	int present	(cvoid *,int,fifoelem_cmp) noex ;
+	int curbegin	(fifoelem_cur *) noex ;
+	int curend	(fifoelem_cur *) noex ;
+	int curdel	(fifoelem_cur *) noex ;
+	int curenum	(fifoelem_cur *,fifoelem_ent **) noex ;
+	void dtor() noex ;
+	operator int () noex ;
+	destruct fifoelem() {
+	    if (magval) dtor() ;
+	} ;
+} ; /* end struct (fifoelem) */
+#else	/* __cplusplus */
+typedef FIFOSTR		fifoelem ;
+#endif /* __cplusplus */
+
+EXTERNC_begin
+
+extern int fifoelem_start    (fifoelem *,int) noex ;
+extern int fifoelem_finish   (fifoelem *) noex ;
+extern int fifoelem_ins	     (fifoelem *,void *) noex ;
+extern int fifoelem_get	     (fifoelem *,fifoelem_ent **) noex ;
+extern int fifoelem_rem	     (fifoelem *,void *,int) noex ;
+extern int fifoelem_del	     (fifoelem *) noex ;
+extern int fifoelem_count    (fifoelem *) noex ;
+extern int fifoelem_present  (fifoelem *,cvoid *,int,fifoelem_cmp) noex ;
+extern int fifoelem_curbegin (fifoelem *,fifoelem_cur *) noex ;
+extern int fifoelem_curend   (fifoelem *,fifoelem_cur *) noex ;
+extern int fifoelem_curdel   (fifoelem *,fifoelem_cur *) noex ;
+extern int fifoelem_curenum  (fifoelem *,fifoelem_cur *,fifoelem_ent **) noex ;
 
 /*
 extern int fifoelem_curenum(fifoelem *,fifoelem_cur *,void *) noex ;
-extern int fifoelem_search(fifoelem *,char *,int (*)(),char **) ;
-extern int fifoelem_find(fifoelem *,void *) ;
+extern int fifoelem_search(fifoelem *,char *,int (*)(),char **) noex ;
+extern int fifoelem_find(fifoelem *,void *) noex ;
 */
 
 EXTERNC_end
