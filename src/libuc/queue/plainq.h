36648d7ff33eac3ddf287b65e9836c6c291990b1
David A.D. Morano <david.a.morano@gmail.com>
2026-04-04 00:57:17 -0400

    update

--- src/libuc/queue/plainq.h
+++ src/libuc/queue/plainq.h
@@ -20,28 +20,90 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 
 
 #define	PLAINQ		struct plainq_head
+#define	PLAINQ_CUR	struct plainq_cursor
 #define	PLAINQ_ENT	struct plainq_entry
 #define	PLAINQ_MAGIC	0x76925634
 
 
+struct plainq_cursor {
+	PLAINQ_ENT	*entp ;
+} ; /* end struct (plainq_cursor) */
+
 struct plainq_entry {
 	ptrdiff_t	next ;
 	ptrdiff_t	prev ;
-} ;
+} ; /* end struct (plainq_entry) */
 
 struct plainq_head {
 	ptrdiff_t	head ;
 	ptrdiff_t	tail ;
-	uint		magic ;
-	int		count ;
-} ;
+	uint		magval ;
+	int		cnt ;
+} ; /* end struct (plainq_head) */
 
-typedef PLAINQ		plainq ;
+typedef PLAINQ_CUR	plainq_cur ;
 typedef PLAINQ_ENT	plainq_ent ;
+
+#ifdef	__cplusplus
+enum plainqmems {
+	plainqmem_start,
+	plainqmem_count,
+	plainqmem_audit,
+	plainqmem_finish,
+	plainqmem_overlast
+} ; /* end enum (plainqmems) */
+struct plainq ;
+struct plainq_co {
+        plainq		*op = nullptr ;
+        int             w = -1 ;
+        constexpr void operator () (plainq *p,int m) noex {
+            op = p ;
+            w = m ;
+        } ;
+        operator int () noex ;
+	int operator () () noex {
+	    return operator int () ;
+	} ;
+} ; /* end struct (plainq_co) */
+struct plainq : plainq_head {
+	plainq_co	start ;
+	plainq_co	count ;
+	plainq_co	audit ;
+	plainq_co	finish ;
+	constexpr plainq() noex {
+	    start	(this,plainqmem_start) ;
+	    count	(this,plainqmem_count) ;
+	    audit	(this,plainqmem_audit) ;
+	    finish	(this,plainqmem_finish) ;
+	    magval = 0 ;
+	} ; /* end ctor */
+	plainq(const plainq &) = delete ;
+	plainq &operator = (const plainq &) = delete ;
+	int ins		(plainq_ent *) noex ;
+	int insgroup	(plainq_ent *,int,int) noex ;
+	int inshead	(plainq_ent *) noex ;
+	int gethead	(plainq_ent **) noex ;
+	int gettail	(plainq_ent **) noex ;
+	int rem		(plainq_ent **) noex ;
+	int remtail	(plainq_ent **) noex ;
+	int unlink	(plainq_ent *) noex ;
+	int curbegin	(plainq_cur *) noex ;
+	int curend	(plainq_cur *) noex ;
+	int curenum	(plainq_cur *,plainq_ent **) noex ;
+	void	dtor() noex ;
+	operator int () noex ;
+	destruct plainq() {
+	    if (magval) dtor() ;
+	} ; /* end dtor (plainq) */
+} ; /* end class (plainq) */
+#else
+typedef PLAINQ		plainq ;
+#endif /* __cplusplus */
 
 EXTERNC_begin
 
@@ -50,11 +112,11 @@ extern int plainq_finish	(plainq *) noex ;
 extern int plainq_ins		(plainq *,plainq_ent *) noex ;
 extern int plainq_insgroup	(plainq *,plainq_ent *,int,int) noex ;
 extern int plainq_inshead	(plainq *,plainq_ent *) noex ;
-extern int plainq_unlink	(plainq *,plainq_ent *) noex ;
-extern int plainq_rem		(plainq *,plainq_ent **) noex ;
 extern int plainq_gethead	(plainq *,plainq_ent **) noex ;
-extern int plainq_remtail	(plainq *,plainq_ent **) noex ;
 extern int plainq_gettail	(plainq *,plainq_ent **) noex ;
+extern int plainq_rem		(plainq *,plainq_ent **) noex ;
+extern int plainq_remtail	(plainq *,plainq_ent **) noex ;
+extern int plainq_unlink	(plainq *,plainq_ent *) noex ;
 extern int plainq_count		(plainq *) noex ;
 extern int plainq_audit		(plainq *) noex ;
 
