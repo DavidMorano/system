f766a59555b6055220e6811dd357c0b2a3be1724
David A.D. Morano <david.a.morano@gmail.com>
2026-04-05 00:22:06 -0400

    update

--- src/libuc/sfx/sfcookkey.cc
+++ src/libuc/sfx/sfcookkey.cc
@@ -47,16 +47,15 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<ascii.h>
+#include	<usysbase.h>
 #include	<strn.h>		/* |strnchr(3uc)| */
 #include	<localmisc.h>
 
 #include	"sfx.h"
 
-import libutil ;
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |lenstr(3u)| */
 
 /* local defines */
 
