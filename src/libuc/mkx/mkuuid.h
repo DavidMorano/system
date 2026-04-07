97f1f81eca47772646f947ed8244e6a4dae6d7c2
David A.D. Morano <david.a.morano@gmail.com>
2026-04-07 00:40:46 -0400

    update

--- src/libuc/mkx/mkuuid.h
+++ src/libuc/mkx/mkuuid.h
@@ -15,10 +15,7 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<stdint.h>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 #define	UUID_VERSION	4		/* default UUID version */
