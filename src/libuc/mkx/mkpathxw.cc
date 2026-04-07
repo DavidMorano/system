40b5a492158b3d231f24910f62fae8f936e1065a
David A.D. Morano <david.a.morano@gmail.com>
2026-04-07 00:39:32 -0400

    update

--- src/libuc/mkx/mkpathxw.cc
+++ src/libuc/mkx/mkpathxw.cc
@@ -37,7 +37,8 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<localmisc.h>
 
 #include	"mkpathxw.h"
