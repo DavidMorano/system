ea03558ddb385063bc5029b26d94f1279a7f152a
David A.D. Morano <david.a.morano@gmail.com>
2026-04-07 00:40:10 -0400

    update

--- src/libuc/mkx/mktagfname.cc
+++ src/libuc/mkx/mktagfname.cc
@@ -43,10 +43,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<mkpathxw.h>
 #include	<localmisc.h>
 
