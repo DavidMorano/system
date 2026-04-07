ec0f912017b9f8251ed573de0cbb0f2a626e0e9a
David A.D. Morano <david.a.morano@gmail.com>
2026-04-07 00:39:27 -0400

    update

--- src/libuc/mkx/mkpathx.h
+++ src/libuc/mkx/mkpathx.h
@@ -20,7 +20,8 @@
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<mkpathxw.h>
 #include	<mknpathx.h>
 #include	<mknpathxw.h>
