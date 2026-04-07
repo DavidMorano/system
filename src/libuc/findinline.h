8ed618dccfef9c68949579aa700e81feeaec94b3
David A.D. Morano <david.a.morano@gmail.com>
2026-04-07 00:37:14 -0400

    update

--- src/libuc/findinline.h
+++ src/libuc/findinline.h
@@ -12,20 +12,20 @@
 	This module was changed to serve in the REFERM program.
 
 	= 2005-10-01, David A­D­ Morano
-	This was changed to work in the MMCITE program. The old
-	REFERM program is really obsolete. It used a database lookup
-	strategy to remote databases. The high-level problem is:
-	what to do if the cited BIB entry isn't found? How does a
+	This was changed to work in the MMCITE program.  The old
+	REFERM program is really obsolete.  It used a database lookup
+	strategy to remote databases.  The high-level problem is:
+	what to do if the cited BIB entry is not found?  How does a
 	maintainer of the present (local) document know what that
-	BIB entry was? The new strategy (implemented by the MMCITE
+	BIB entry was?  The new strategy (implemented by the MMCITE
 	program) is more like what is done with BibTeX in the TeX
-	(or LaTeX) world. All BIB databases are really expected to
+	(or LaTeX) world.  All BIB databases are really expected to
 	be maintained by the document creator -- not some centralized
-	entiry. The older centralized model reflected more the use
+	entiry.  The older centralized model reflected more the use
 	in the corporate world (where different people create BIB
 	entries) than in the more "modern" personal-responsibility
-	type of world! :-) Anyway, this is the way the gods seem
-	to now want to do things. Deal with it!
+	type of world! :-)  Anyway, this is the way the gods seem
+	to now want to do things.  Deal with it!
 
 */
 
@@ -62,6 +62,5 @@ EXTERNC_end
 
 
 #endif /* FINDINLINE_INCLUDE */
-
 
 
