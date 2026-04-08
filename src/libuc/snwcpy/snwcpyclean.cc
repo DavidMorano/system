2f409c181850db15f672f5246daf367ed81d93ed
David A.D. Morano <david.a.morano@gmail.com>
2026-04-08 00:50:19 -0400

    update

--- src/libuc/snwcpy/snwcpyclean.cc
+++ src/libuc/snwcpy/snwcpyclean.cc
@@ -62,10 +62,7 @@
 #include	<cstdlib>
 #include	<bitset>		/* |bitset(3c++)| */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<ascii.h>
 #include	<mkchar.h>
 #include	<localmisc.h>
