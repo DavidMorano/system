48126e23cd9fdf790e9d217245319acf7d1beb53
David A.D. Morano <david.a.morano@gmail.com>
2026-04-09 00:17:19 -0400

    update

--- src/libuc/sigx/sigaction.cc
+++ src/libuc/sigx/sigaction.cc
@@ -29,7 +29,8 @@
 #include	<csignal>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<localmisc.h>
 
 #include	"sigaction.h"
