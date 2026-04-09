1f1bead6251332be43c13239f7928affc2ec8ea4
David A.D. Morano <david.a.morano@gmail.com>
2026-04-09 00:16:45 -0400

    update

--- src/libuc/sigobj/sigblocker.h
+++ src/libuc/sigobj/sigblocker.h
@@ -21,11 +21,7 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<usyscalls.h>
+#include	<usysbase.h>
 
 
 #define	SIGBLOCKER	struct sigblocker_head
