6b292b41b6da99d21c47172ba8c87fcc348273b6
David A.D. Morano <david.a.morano@gmail.com>
2026-04-11 01:28:10 -0400

    restore

---
+++ doc/obsolete/boolvals.h
@@ -0,0 +1,23 @@
+/* boolvals HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+#if	(! defined(false)) && (! defined(true))
+
+enum truthvals {
+	false,		/* <- = intx(0) */
+	true,		/* <- = intx(1) */
+	overlast	/* <- = intx(2) */
+} ;
+
+#endif /* */
+
+#ifndef	false
+#define	false		0
+#endif
+
+#ifndef	true
+#define	true		1
+#endif
+
+
