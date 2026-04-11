899c5b85279f10fa0af2eb08f5cf09a2eaf0a38d
David A.D. Morano <david.a.morano@gmail.com>
2026-04-11 01:46:46 -0400

    cleanup

--- src/libuc/open/openport.h
+++ src/libuc/open/openport.h
@@ -13,7 +13,8 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<sockaddress.h>
 #include	<openportmsg.h>
 
