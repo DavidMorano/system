ceedbcc3e0d1c6ce0727c945a840a770438aac53
David A.D. Morano <david.a.morano@gmail.com>
2026-04-11 01:46:23 -0400

    cleanup

--- src/libuc/open/opendefstds.cc
+++ src/libuc/open/opendefstds.cc
@@ -25,6 +25,16 @@
 	This subroutine opens the default standard files up to a
 	certain file-descriptor number (generally should be '3').
 
+	Synopsis:
+	int opendefstds(int n) noex
+
+	Arguments:
+	n		number of low file-descriptos to check
+
+	Returns:
+	>=0		OK, number of newly opened file-descriptors
+	<0		error (system-return)
+
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
@@ -34,12 +44,16 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<cstring>
-#include	<usystem.h>
-#include	<syswords.hh>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
 #include	<localmisc.h>
 
 #include	"open.h"
 
+#pragma		GCC dependency		"mod/uconstants.ccm"
+
+import uconstants ;			/* |sysword(3u)| */
 
 /* local defines */
 
@@ -72,15 +86,17 @@
 
 int opendefstds(int n) noex {
 	int		rs = SR_OK ;
+	int		c = 0 ;
 	cmode		om = 0666 ;
 	cchar		*nfn = sysword.w_devnull ;
 	for (int i = 0 ; (rs >= 0) && (i < n) ; i += 1) {
 	    if ((rs = u_fcntl(i,F_GETFD,0)) == SR_BADF) {
 	        cint	of = (i == 0) ? O_RDONLY : O_WRONLY ;
 	        rs = u_open(nfn,of,om) ;
+		c += 1 ;
 	    }
 	} /* end if */
-	return rs ;
+	return (rs >= 0) ? c : rs ;
 }
 /* end subroutine (opendefstds) */
 
