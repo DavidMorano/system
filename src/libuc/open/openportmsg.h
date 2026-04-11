dd61a7b8048aa84659c92d074888ccd10c8b7518
David A.D. Morano <david.a.morano@gmail.com>
2026-04-11 01:46:55 -0400

    cleanup

--- src/libuc/open/openportmsg.h
+++ src/libuc/open/openportmsg.h
@@ -14,10 +14,7 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<sockaddress.h>
 #include	<localmisc.h>		/* |USERNAMELEN| */
 
