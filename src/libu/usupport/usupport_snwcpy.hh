0e94f34a1ff65a384c4d2f2ddc79c51c929417bb
David A.D. Morano <david.a.morano@gmail.com>
2026-04-12 00:23:22 -0400

    restore

---
+++ src/libu/usupport/usupport_snwcpy.hh
@@ -0,0 +1,39 @@
+/* usupport_snwcpy HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* UNIX® kernel support subroutines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-26, David A­D­ Morano
+	This was first written to give a little bit to UNIX® 
+	of what we have in our own circuit-pack OSes!
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USUPPORTSNWCPY_INCLUDE
+#define	USUPPORTSNWCPY_INCLUDE
+#ifdef	__cplusplus /* (C++ only) */
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+namespace libu {
+    extern int snwcpy(char *,int,cchar *,int = -1) noex ;
+} /* end namespace (libu) */
+
+
+#endif /* __cplusplus (C++ only) */
+#endif /* USUPPORTSNWCPY_INCLUDE */
+
+
