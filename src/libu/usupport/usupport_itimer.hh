cc7732fad4c0d060a6ef93141f33e1dcc36c3a3b
David A.D. Morano <david.a.morano@gmail.com>
2026-04-12 00:24:12 -0400

    restore

---
+++ src/libu/usupport/usupport_itimer.hh
@@ -0,0 +1,30 @@
+/* usupport_itimer HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* UNIX® kernel support subroutines */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USUPPORTITIMER_INCLUDE
+#define	USUPPORTITIMER_INCLUDE
+#ifdef	__cplusplus /* (C++ only) */
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+namespace libu {
+    extern int uitimer_get(int,ITIMERVAL *) noex ;
+    extern int uitimer_set(int,CITIMERVAL *,ITIMERVAL *) noex ;
+}
+
+
+#endif /* __cplusplus */
+#endif /* USUPPORTITIMER_INCLUDE */
+
+
