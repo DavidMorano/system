1d57ed6444187784f7a90d0691ea9651b5c0379c
David A.D. Morano <david.a.morano@gmail.com>
2026-04-13 01:23:09 -0400

    update

--- src/libuc/caches/namecache.cc
+++ src/libuc/caches/namecache.cc
@@ -32,11 +32,13 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<cstring>		/* |strncmp(3c)| */
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>		/* |getustime(3u)| */
+#include	<uclibmem.h>
 #include	<getbufsize.h>
 #include	<getax.h>
 #include	<getpwx.h>
-#include	<mallocxx.h>
 #include	<realname.h>
 #include	<mkgecosname.h>
 #include	<strwcpy.h>
@@ -66,7 +68,6 @@ import libutil ;			/* |memclear(3u)| + |lenstr(3u)| */
 
 /* imported namespaces */
 
-using libuc::libmem ;			/* variable */
 using std::nothrow ;			/* constant */
 
 
@@ -94,54 +95,51 @@ struct namecache_ent {
 /* forward references */
 
 template<typename ... Args>
-static int namecache_ctor(namecache *op,Args ... args) noex {
+local int namecache_ctor(namecache *op,Args ... args) noex {
     	NAMECACHE	*hop = op ;
 	cnullptr	np{} ;
 	int		rs = SR_FAULT ;
-	if (op && (args && ...)) {
+	if (op && (args && ...)) ylikely {
 	    rs = SR_NOMEM ;
 	    memclear(hop) ;
-	    if ((op->dbp = new(nothrow) hdb) != np) {
+	    if ((op->dbp = new(nothrow) hdb) != np) ylikely {
 		rs = SR_OK ;
 	    }
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (namecache_ctor) */
+} /* end subroutine (namecache_ctor) */
 
-static int namecache_dtor(namecache *op) noex {
+local int namecache_dtor(namecache *op) noex {
 	int		rs = SR_FAULT ;
-	if (op) {
+	if (op) ylikely {
 	    rs = SR_OK ;
-	    if (op->dbp) {
+	    if (op->dbp) ylikely {
 		delete op->dbp ;
 		op->dbp = nullptr ;
 	    }
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (namecache_dtor) */
+} /* end subroutine (namecache_dtor) */
 
 template<typename ... Args>
-static int namecache_magic(namecache *op,Args ... args) noex {
+local int namecache_magic(namecache *op,Args ... args) noex {
 	int		rs = SR_FAULT ;
-	if (op && (args && ...)) {
+	if (op && (args && ...)) ylikely {
 	    rs = (op->magic == NAMECACHE_MAGIC) ? SR_OK : SR_NOTOPEN ;
 	}
 	return rs ;
-}
-/* end subroutine (namecache_magic) */
+} /* end subroutine (namecache_magic) */
 
-static int	namecache_newent(NC *,NC_ENT **,cchar *,cchar *,int) noex ;
-static int	namecache_repent(NC *,NC_ENT **,cchar *,cchar *,int) noex ;
-static int	namecache_entfins(NC *) noex ;
+local int	namecache_newent(NC *,NC_ENT **,cchar *,cchar *,int) noex ;
+local int	namecache_repent(NC *,NC_ENT **,cchar *,cchar *,int) noex ;
+local int	namecache_entfins(NC *) noex ;
 
-static int	entry_start(NC_ENT *,cchar *,cchar *,int) noex ;
-static int	entry_update(NC_ENT *,cchar *,int) noex ;
-static int	entry_finish(NC_ENT *) noex ;
-static int	entry_loadnames(NC_ENT *,cchar *,cchar *,int) noex ;
+local int	entry_start(NC_ENT *,cchar *,cchar *,int) noex ;
+local int	entry_update(NC_ENT *,cchar *,int) noex ;
+local int	entry_finish(NC_ENT *) noex ;
+local int	entry_loadnames(NC_ENT *,cchar *,cchar *,int) noex ;
 
-static int	mkaname(char *,int,cchar *) noex ;
+local int	mkaname(char *,int,cchar *) noex ;
 
 
 /* local variables */
@@ -157,12 +155,12 @@ constexpr bool		f_fullname = CF_FULLNAME ;
 int namecache_start(NC *op,cchar *vname,int nmax,int ttl) noex {
 	cnullptr	np{} ;
 	int		rs ;
-	if ((rs = namecache_ctor(op,vname)) >= 0) {
+	if ((rs = namecache_ctor(op,vname)) >= 0) ylikely {
 	    rs = SR_INVALID ;
-	    if (vname[0]) {
+	    if (vname[0]) ylikely {
 	        if (nmax < 3) nmax = NAMECACHE_DEFMAX ;
 	        if (ttl < 1) ttl = NAMECACHE_DEFTO ;
-	        if (cchar *cp ; (rs = libmem.strw(vname,-1,&cp)) >= 0) {
+	        if (cchar *cp ; (rs = lm_strw(vname,-1,&cp)) >= 0) ylikely {
 	            cint	ne = NAMECACHE_DEFENTS ;
 	            op->vname = cp ;
 	            if ((rs = hdb_start(op->dbp,ne,1,np,np)) >= 0) {
@@ -172,7 +170,7 @@ int namecache_start(NC *op,cchar *vname,int nmax,int ttl) noex {
 	            } /* end if (hdb-start) */
 	            if (rs < 0) {
 			void *vp = voidp(op->vname) ;
-		        libmem.free(vp) ;
+		        lm_free(vp) ;
 		        op->vname = nullptr ;
 	            }
 	        } /* end if (memory-allocation) */
@@ -188,18 +186,18 @@ int namecache_start(NC *op,cchar *vname,int nmax,int ttl) noex {
 int namecache_finish(NC *op) noex {
 	int		rs ;
 	int		rs1 ;
-	if ((rs = namecache_magic(op)) >= 0) {
+	if ((rs = namecache_magic(op)) >= 0) ylikely {
 	    {
 	        rs1 = namecache_entfins(op) ;
 	        if (rs >= 0) rs = rs1 ;
 	    }
-	    if (op->dbp) {
+	    if (op->dbp) ylikely {
 	        rs1 = hdb_finish(op->dbp) ;
 	        if (rs >= 0) rs = rs1 ;
 	    }
-	    if (op->vname) {
+	    if (op->vname) ylikely {
 		void *vp = voidp(op->vname) ;
-	        rs1 = libmem.free(vp) ;
+	        rs1 = lm_free(vp) ;
 	        if (rs >= 0) rs = rs1 ;
 	        op->vname = nullptr ;
 	    }
@@ -215,9 +213,9 @@ int namecache_finish(NC *op) noex {
 
 int namecache_add(NC *op,cchar *un,cchar *rnp,int rnl) noex {
 	int		rs ;
-	if ((rs = namecache_magic(op,un,rnp)) >= 0) {
+	if ((rs = namecache_magic(op,un,rnp)) >= 0) ylikely {
 	    rs = SR_INVALID ;
-	    if (un[0]) {
+	    if (un[0]) ylikely {
 	        hdb_dat		key ;
 	        hdb_dat		val{} ;
 	        key.buf = un ;
@@ -243,13 +241,13 @@ int namecache_lookup(NC *op,cchar *un,cchar **rpp) noex {
 	int		rs1 ;
 	int		rl = 0 ;
 	if (rpp) *rpp = nullptr ;
-	if ((rs = namecache_magic(op,un)) >= 0) {
+	if ((rs = namecache_magic(op,un)) >= 0) ylikely {
 	    custime	dt = getustime ;
 	    rs = SR_INVALID ;
-	    if (un[0]) {
+	    if (un[0]) ylikely {
 	        cchar		*rp = nullptr ;
 	        op->s.total += 1 ;
-	        if (char *pwbuf{} ; (rs = malloc_pw(&pwbuf)) >= 0) {
+	        if (char *pwbuf ; (rs = lm_pw(&pwbuf)) >= 0) ylikely {
 	            ucentpw	pw{} ;
 	            NC_ENT	*ep ;
 	            hdb_dat	key ;
@@ -298,7 +296,7 @@ int namecache_lookup(NC *op,cchar *un,cchar **rpp) noex {
 		            } /* end if (real-name) */
 	                } /* end if (getpw_name) */
 	            } /* end if (hdb_fetch) */
-	            rs1 = libmem.free(pwbuf) ;
+	            rs1 = lm_free(pwbuf) ;
 		    if (rs >= 0) rs = rs1 ;
 	        } /* end if (memory-allocation) */
 	        if (rpp) {
@@ -312,8 +310,8 @@ int namecache_lookup(NC *op,cchar *un,cchar **rpp) noex {
 
 int namecache_stats(NC *op,NC_ST *sp) noex {
 	int		rs ;
-	if ((rs = namecache_magic(op,sp)) >= 0) {
-	    if ((rs = hdb_count(op->dbp)) >= 0) {
+	if ((rs = namecache_magic(op,sp)) >= 0) ylikely {
+	    if ((rs = hdb_count(op->dbp)) >= 0) ylikely {
 	        *sp = op->s ;
 	        sp->nentries = rs ;
 	    }
@@ -325,12 +323,12 @@ int namecache_stats(NC *op,NC_ST *sp) noex {
 
 /* private subroutines */
 
-static int namecache_newent(NC *op,NC_ENT **epp,cc *un,cc *sp,int sl) noex {
+local int namecache_newent(NC *op,NC_ENT **epp,cc *un,cc *sp,int sl) noex {
 	cint		msz = szof(NC_ENT) ;
 	int		rs ;
 	if (epp) *epp = nullptr ;
-	if (NC_ENT *ep{} ; (rs = libmem.mall(msz,&ep)) >= 0) {
-	    if ((rs = entry_start(ep,un,sp,sl)) >= 0) {
+	if (NC_ENT *ep{} ; (rs = lm_mall(msz,&ep)) >= 0) ylikely {
+	    if ((rs = entry_start(ep,un,sp,sl)) >= 0) ylikely {
 	        hdb_dat		key ;
 	        hdb_dat		val{} ;
 	        key.buf = ep->username ;
@@ -345,20 +343,20 @@ static int namecache_newent(NC *op,NC_ENT **epp,cc *un,cc *sp,int sl) noex {
 		}
 	    } /* end if (entry-start) */
 	    if (rs < 0) {
-		libmem.free(ep) ;
+		lm_free(ep) ;
 	    }
 	} /* end if (memory-allocation) */
 	return rs ;
 }
 /* end subroutine (namecache_newent) */
 
-static int namecache_repent(NC *op,NC_ENT **epp,cc *un,cc *sp,int sl) noex {
+local int namecache_repent(NC *op,NC_ENT **epp,cc *un,cc *sp,int sl) noex {
 	NC_ENT		*ep = nullptr ; /* used-afterwards */
 	hdb		*dbp = op->dbp ;
 	int		rs ;
 	int		rs1 ;
 	if (epp) *epp = nullptr ;
-	if (hdb_cur cur{} ; (rs = hdb_curbegin(dbp,&cur)) >= 0) {
+	if (hdb_cur cur{} ; (rs = hdb_curbegin(dbp,&cur)) >= 0) ylikely {
 	    hdb_dat	key ;
 	    hdb_dat	val{} ;
 	    while ((rs = hdb_curenum(dbp,&cur,&key,&val)) >= 0) {
@@ -381,11 +379,11 @@ static int namecache_repent(NC *op,NC_ENT **epp,cc *un,cc *sp,int sl) noex {
 }
 /* end subroutine (namecache_repent) */
 
-static int namecache_entfins(NC *op) noex {
+local int namecache_entfins(NC *op) noex {
 	hdb		*elp = op->dbp ;
 	int		rs = SR_OK ;
 	int		rs1 ;
-	if (hdb_cur cur{} ; (rs1 = hdb_curbegin(elp,&cur)) >= 0) {
+	if (hdb_cur cur{} ; (rs1 = hdb_curbegin(elp,&cur)) >= 0) ylikely {
 	    hdb_dat	key ;
 	    hdb_dat	val{} ;
 	    while (hdb_curenum(elp,&cur,&key,&val) >= 0) {
@@ -395,7 +393,7 @@ static int namecache_entfins(NC *op) noex {
 	            if (rs >= 0) rs = rs1 ;
 		}
 		{
-	            rs1 = libmem.free(ep) ;
+	            rs1 = lm_free(ep) ;
 	            if (rs >= 0) rs = rs1 ;
 		}
 	    } /* end while */
@@ -407,13 +405,13 @@ static int namecache_entfins(NC *op) noex {
 }
 /* end subroutine (namecache_entfins) */
 
-static int entry_start(NC_ENT *ep,cchar *up,cchar *rp,int rl) noex {
+local int entry_start(NC_ENT *ep,cchar *up,cchar *rp,int rl) noex {
 	custime		dt = getustime ;
 	int		rs = SR_FAULT ;
-	if (ep && up && rp) {
+	if (ep && up && rp) ylikely {
 	    rs = SR_INVALID ;
 	    memclear(ep) ;
-	    if (up[0]) {
+	    if (up[0]) ylikely {
 	        if (rl < 0) rl = lenstr(rp) ;
 	        ep->ti_init = dt ;
 	        ep->ti_access = dt ;
@@ -425,13 +423,13 @@ static int entry_start(NC_ENT *ep,cchar *up,cchar *rp,int rl) noex {
 }
 /* end subroutine (entry_start) */
 
-static int entry_finish(NC_ENT *ep) noex {
+local int entry_finish(NC_ENT *ep) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
-	if (ep) {
+	if (ep) ylikely {
 	    rs = SR_OK ;
-	    if (ep->a) {
-	        rs1 = libmem.free(ep->a) ;
+	    if (ep->a) ylikely {
+	        rs1 = lm_free(ep->a) ;
 	        if (rs >= 0) rs = rs1 ;
 	        ep->a = nullptr ;
 	    }
@@ -442,11 +440,11 @@ static int entry_finish(NC_ENT *ep) noex {
 }
 /* end subroutine (entry_finish) */
 
-static int entry_update(NC_ENT *ep,cchar *rp,int rl) noex {
+local int entry_update(NC_ENT *ep,cchar *rp,int rl) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
 	int		f_changed = false ;
-	if (ep && rp) {
+	if (ep && rp) ylikely {
 	    rs = SR_OK ;
 	    if (rl < 0) rl = lenstr(rp) ;
 	    f_changed = true ;
@@ -455,13 +453,13 @@ static int entry_update(NC_ENT *ep,cchar *rp,int rl) noex {
 	    if (f_changed) {
 	        ep->ti_init = getustime ;
 	        ep->realnamelen = rl ;
-		if (char *ubuf{} ; (rs = malloc_un(&ubuf)) >= 0) {
+		if (char *ubuf ; (rs = lm_un(&ubuf)) >= 0) ylikely {
 		    cint	ulen = rs ;
 		    {
 		        strdcpy1(ubuf,ulen,ep->username) ;
 	                rs = entry_loadnames(ep,ubuf,rp,rl) ;
 		    }
-		    rs1 = libmem.free(ubuf) ;
+		    rs1 = lm_free(ubuf) ;
 		    if (rs >= 0) rs = rs1 ;
 	        } /* end if (m-a-f) */
 	    } /* end if (changed) */
@@ -470,16 +468,16 @@ static int entry_update(NC_ENT *ep,cchar *rp,int rl) noex {
 }
 /* end subroutine (entry_update) */
 
-static int entry_loadnames(NC_ENT *ep,cchar *up,cchar *rp,int rl) noex {
+local int entry_loadnames(NC_ENT *ep,cchar *up,cchar *rp,int rl) noex {
 	int		rs ;
 	int		sz = 0 ;
 	if (ep->a != nullptr) {
-	    libmem.free(ep->a) ;
+	    lm_free(ep->a) ;
 	    ep->a = nullptr ;
 	}
 	sz += (lenstr(up)+1) ;
 	sz += (lenstr(rp,rl)+1) ;
-	if (char *bp{} ; (rs = libmem.mall(sz,&bp)) >= 0) {
+	if (char *bp ; (rs = lm_mall(sz,&bp)) >= 0) ylikely {
 	    ep->a = bp ;
 	    ep->username = bp ;
 	    bp = (strwcpy(bp,up,-1)+1) ;
@@ -491,16 +489,16 @@ static int entry_loadnames(NC_ENT *ep,cchar *up,cchar *rp,int rl) noex {
 /* end subroutine (entry_loadnames) */
 
 /* make a real name from a GECOS name */
-static int mkaname(char *nbuf,int nlen,cchar *gecos) noex {
+local int mkaname(char *nbuf,int nlen,cchar *gecos) noex {
 	cint		glen = GNAMELEN ;
 	int		rs ;
 	int		rs1 ;
 	int		rl = 0 ;
 	char		gbuf[GNAMELEN+1] ;
 	nbuf[0] = '\0' ;
-	if ((rs = mkgecosname(gbuf,glen,gecos)) >= 0) {
+	if ((rs = mkgecosname(gbuf,glen,gecos)) >= 0) ylikely {
 	    cint	gl = rs ;
-	    if (realname rn ; (rs = rn.start(gbuf,gl)) >= 0) {
+	    if (realname rn ; (rs = rn.start(gbuf,gl)) >= 0) ylikely {
 		if_constexpr (f_fullname) {
 	            rl = rn.fullname(nbuf,nlen) ;
 		} else {
