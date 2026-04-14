6ac24cdcc637b51d75b7f3e993c4c67e66bd4b61
David A.D. Morano <david.a.morano@gmail.com>
2026-04-14 00:57:33 -0400

    update

--- src/libuc/uccloseonexec.cc
+++ src/libuc/uccloseonexec.cc
@@ -20,7 +20,11 @@
 #include	<sys/types.h>
 #include	<unistd.h>
 #include	<fcntl.h>
-#include	<usystem.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
 #include	<localmisc.h>		/* |LEQUIV(3dam)| */
 
 
