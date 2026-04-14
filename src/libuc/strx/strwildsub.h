181e7cb2cb0e39aa6c441a2bb2ca60b786260628
David A.D. Morano <david.a.morano@gmail.com>
2026-04-14 00:43:19 -0400

    restore

---
+++ src/libuc/strx/strwildsub.h
@@ -0,0 +1,38 @@
+/* strwildsub HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* find a substring within a larger string, but w/ wild-card characters */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STRWILDSUB_INCLUDE
+#define	STRWILDSUB_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+EXTERNC_begin
+
+extern char	*strwildsub(cchar *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* STRWILDSUB_INCLUDE */
+
+
