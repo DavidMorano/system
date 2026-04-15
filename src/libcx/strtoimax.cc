f009c6d2b8e0982d4eb042e6c2a88d1cd5674900
David A.D. Morano <david.a.morano@gmail.com>
2026-04-15 00:14:06 -0400

    update

--- src/libcx/strtoimax.cc
+++ src/libcx/strtoimax.cc
@@ -1,4 +1,5 @@
 /* strtoimax SUPPORT */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* UNIX® c-string support */
@@ -14,30 +15,38 @@
 
 /* Copyright © 2017 David A­D­ Morano.  All rights reserved. */
 
-
-#include	<envstandards.h>
-#include	<sys/types.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<inttypes.h>
+#include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+#include	<usyscalls.h>
+#include	<localmisc.h>
+
+#include	<strtoxmax.h>
 
 
 #if defined(_LP64)
 
-intmax_t strtoimax(const char *str,char **endptr,int base) {
+intmax_t strtoimax(cchar *str,char **endptr,int base)  {
 	return strtol(str,endptr,base) ;
 }
 
-intmax_t strtointmax(const char *str,char **endptr,int base) {
+intmax_t strtointmax(cchar *str,char **endptr,int base)  {
 	return strtol(str,endptr,base) ;
 }
 
 #else /* defined(_LP64) */
 
-intmax_t strtoimax(const char *str,char **endptr,int base) {
+intmax_t strtoimax(cchar *str,char **endptr,int base)  {
 	return strtoll(str,endptr,base) ;
 }
 
-intmax_t strtointmax(const char *str,char **endptr,int base) {
+intmax_t strtointmax(cchar *str,char **endptr,int base)  {
 	return strtoll(str,endptr,base) ;
 }
 
