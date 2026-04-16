c7e35b19f17f18174791c75e9cbed229f9fba120
David A.D. Morano <david.a.morano@gmail.com>
2026-04-16 00:33:14 -0400

    update

--- src/base64dec/base64dec_dec.cc
+++ src/base64dec/base64dec_dec.cc
@@ -1,11 +1,10 @@
-/* base64dec_dec SUPPORT base64) */
+/* base64dec_dec SUPPORT (base64dec) */
 /* charset=ISO8859-1 */
 /* lang=C++20 (conformance reviewed) */
 
 /* decode a file (encoded in BASE64) */
 /* version %I% last-modified %G% */
 
-#define	CF_DEBUGS	0		/* compile-time */
 #define	CF_DEBUG	0		/* run-time debugging */
 
 /* revision history:
@@ -25,15 +24,14 @@
 	This module does the work of decoding the BASE64 input.
 
 	Synopsis:
-	int base64dec_dec(PI *pip,FILE *ofp,cchar *name) noex
+	int base64dec_dec(PI *pip,cchar *name) noex
 
 	Arguments:
 	pip		program information pointer
-	ofp		FILE output file pointer
 	name		filename
 
 	Returns:
-	>=		OK
+	>=0		OK
 	<0		error (system-return)
 
 ******************************************************************************/
@@ -44,16 +42,24 @@
 #include	<cstdio>
 #include	<clanguage.h>
 #include	<usysbase.h>
+#include	<umem.hh>
 #include	<ascii.h>
 #include	<rmx.h>			/* |rmoeol(3uc)| */
-#include	<strn.h>
+#include	<strn.h>		/* |strnbrk(3uc)| */
 #include	<b64decoder.h>
+#include	<ccfile.hh>
 #include	<localmisc.h>
-#include	<libdebug.h>		/* |LIBDEBUG| */
+#include	<libf.h>		/* LIBF */
+#include	<libdebug.h>		/* LIBDEBUG */
+#include	<dprintf.hh>		/* debugging */
 
-#include	"config.h"
-#include	"defs.h"
+#include	"base64dec.h"		/* program-private */
 
+#pragma		GCC dependency		"mod/libutil.ccm"
+#pragma		GCC dependency		"mod/ulibvals.ccm"
+
+import libutil ;			/* |lenstr(3u)| */
+import ulibvals ;			/* |ulibval(3u)| */
 
 /* local defines */
 
@@ -61,7 +67,17 @@
 #define	PI	proginfo
 #endif
 
-#define	STATE	state
+#ifndef	CF_DEBUG
+#define	CF_DEBUG	0		/* run-time debugging */
+#endif
+
+
+/* imported namespaces */
+
+using libu::umem ;			/* variable */
+
+
+/* local typedefs */
 
 
 /* external subroutines */
@@ -72,21 +88,49 @@
 
 /* local structures */
 
-struct state {
+namespace {
+    struct decoder {
+	PI		*pip ;
+	cchar		*name ;
+	char		*a ;
+	char		*lbuf ;
 	char		*obuf ;
+	int		llen ;
 	int		olen ;
-	int		cr ;
-} ; /* end struct (state) */
+	decoder(PI *p,cchar *n) noex : pip(p), name(n) { 
+	    a = nullptr ;
+	} ;
+	int start() noex ;
+	int finish() noex ;
+	int decobj() noex ;
+	int decfile(b64decoder *) noex ;
+	int procln(b64decoder *,cchar *,int) noex ;
+	int proctext(cchar *,int) noex ;
+	int proctextln(cchar *,int) noex ;
+	void dtor() noex ;
+	operator int () noex ;
+	destruct decoder() {
+	    if (a) dtor() ;
+	} ;
+    } ; /* end struct (decoder) */
+    void decoder::dtor() noex {
+	finish() ;
+    } ;
+} /* end namespace */
 
 
 /* forward references */
 
-local int	procln(PI *,FILE *,b64decoder *,STATE *,cchar *,int) noex ;
-
-local int	bwritetext(FILE *,int *,cchar *,int) noex ;
+local char *strnend(cchar *sp,int sl) noex {
+    	return strnsub(sp,sl,"\r\n") ;
+} /* end method (strnend) */
 
 
 /* local variables */
+
+static int	maxlinelen = ulibval.maxline ;
+
+cbool		f_debug = CF_DEBUG ;
 
 
 /* exported variables */
@@ -94,141 +138,155 @@ local int	bwritetext(FILE *,int *,cchar *,int) noex ;
 
 /* exported subroutines */
 
-int base64dec_dec(PI *pip,FILE *ofp,cchar *name) noex {
+int base64dec_dec(PI *pip,cchar *name) noex {
 	int		rs = SR_FAULT ;
-	int		rs1 ;
 	int		wlen = 0 ; /* return-value */
-	if (pip && ofp && name) {
+	DPRINTF("ent name=%s\n",name) ;
+	if (pip && name) {
 	    rs = SR_INVALID ;
 	    if (name[0]) {
-	size += (llen+1) ;
-	size += (olen+1) ;
-	if ((rs = uc_malloc(size,&abuf)) >= 0) {
-	    char	*lbuf = (abuf + 0) ;
-	    char	*obuf = (abuf + (llen+1)) ;
-	    if (b64decoder d ; (rs = b64decoder_start(&d)) >= 0) {
-	        if ((rs = bopen(ifp,name,"r",0666)) >= 0) {
-	    	    STATE	cb ;
-	    	    cb.obuf = obuf ;
-	    	    cb.olen = olen ;
-	    	    cb.cr = 0 ;
-	            while ((rs = breadln(ifp,lbuf,llen)) > 0) {
-	                if (cint len = rmeol(lbuf,rs) ; len > 0) {
-			    rs = procln(pip,ofp,&d,&cb,lbuf,len) ;
-			    wlen += rs ;
-			} /* end if (positive) */
-	                if (rs < 0) break ;
-	            } /* end while (reading) */
-	            rs1 = bclose(ifp) ;
-	            if (rs >= 0) rs = rs1 ;
-	        } /* end if (ifile) */
-	        rs1 = b64decoder_finish(&d) ;
-	        if (rs >= 0) rs = rs1 ;
-	    } /* end if (b64decoder) */
-	    rs1 = uc_free(abuf) ;
-	    if (rs >= 0) rs = rs1 ;
-	} /* end if (m-a) */
-
-#if	CF_DEBUG
-	if (DEBUGLEVEL(4))
-	    debugprintf("progdecode: ret rs=%d wlen=%u\n",rs,wlen) ;
-#endif
-
+		decoder od(pip,name) ;
+		rs = od ;
+		wlen = rs ;
+	    } /* end if (valid) */
+	} /* end if (non-null) */
+	DPRINTF("ret rs=%d wlen=%d\n",rs,wlen) ;
 	return (rs >= 0) ? wlen : rs ;
-}
-/* end subroutine (progdecode) */
+} /* end subroutine (base64dec_dec) */
 
 
 /* local subroutines */
 
-local int procln(PI *pip,FILE *ofp,b64decoder *dp,STATE *statep,
-		cchar *lp,int ll) noex {
+decoder::operator int () noex {
+    	int		rs ;
+	int		rs1 ;
+	int		wlen = 0 ; /* return-value */
+	DPRINTF("ent\n") ;
+	if ((rs = start()) >= 0) {
+	    int		sz = 0 ;
+	    int		ai = 2 ;
+	    sz += (ai * (maxlinelen + 1)) ;
+	    llen = maxlinelen ;
+	    olen = maxlinelen ;
+	    if ((rs = umem.mall(sz,&a)) >= 0) {
+	        lbuf = (a + (--ai * (maxlinelen + 1))) ;
+	        obuf = (a + (--ai * (maxlinelen + 1))) ;
+	        {
+	            rs = decobj() ;
+	            wlen = rs ;
+	        }
+	        rs1 = umem.free(a) ;
+	        if (rs >= 0) rs = rs1 ;
+	        a = nullptr ;
+	    } /* end if (m-a-f) */
+	    rs1 = finish() ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if */
+	DPRINTF("ret rs=%d wlen=%d\n",rs,wlen) ;
+	return (rs >= 0) ? wlen : rs ;
+} /* end method (decoder::operator) */
+
+int decoder::start() noex {
+    	return SR_OK ;
+} /* end method (decoder::start) */
+
+int decoder::finish() noex {
+    	return SR_OK ;
+} /* end method (decoder::finish) */
+
+int decoder::decobj() noex {
+    	int		rs ;
+	int		rs1 ;
+	int		wlen = 0 ; /* return-value */
+	DPRINTF("ent\n") ;
+	if (b64decoder d ; (rs = d.start) >= 0) {
+	    {
+	        rs = decfile(&d) ;
+		wlen += rs ;
+	    }
+	    rs1 = d.finish ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if (b64decoder) */
+	DPRINTF("ret rs=%d wlen=%d\n",rs,wlen) ;
+	return (rs >= 0) ? wlen : rs ;
+} /* end method (decoder::decobj) */
+
+int decoder::decfile(b64decoder *dop) noex {
+    	int		rs ;
+	int		rs1 ;
+	int		wlen = 0 ; /* return-value */
+	DPRINTF("ent\n") ;
+	if (ccfile inf ; (rs = inf.open(name,"r")) >= 0) {
+	    while ((rs = inf.readln(lbuf,llen)) > 0) {
+		if (cint len = rmeol(lbuf,rs) ; len > 0) {
+		    rs = procln(dop,lbuf,len) ;
+		    wlen += rs ;
+		} /* end if (positive) */
+		if (rs < 0) break ;
+	    } /* end while (reading) */
+	    rs1 = inf.close ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if (ccfile) */
+	DPRINTF("ret rs=%d wlen=%d\n",rs,wlen) ;
+	return (rs >= 0) ? wlen : rs ;
+} /* end method (decoder::decfile) */
+
+int decoder::procln(b64decoder *dop,cchar *lp,int ll) noex {
 	int		rs ;
-	int		wlen = 0 ;
-	if ((rs = b64decoder_load(dp,lp,ll)) > 0) {
-	    cint	olen = statep->olen ;
-	    int		*crp = &statep->cr ;
-	    char	*obuf = statep->obuf ;
-	    while ((rs = b64decoder_read(dp,obuf,olen)) > 0) {
+	int		wlen = 0 ; /* return-value */
+	DPRINTF("ent\n") ;
+	if ((rs = dop->load(lp,ll)) > 0) {
+	    while ((rs = dop->read(obuf,olen)) > 0) {
 		cint	ol = rs ;
 		if (pip->fl.text) {
-		    rs = bwritetext(ofp,crp,obuf,ol) ;
+		    rs = proctext(obuf,ol) ;
 	    	    wlen += rs ;
 		} else {
-		    rs = bwrite(ofp,obuf,ol) ;
+		    rs = fwriter(pip->ofp,obuf,ol) ;
 		    wlen += rs ;
 		}
 		if (rs < 0) break ;
 	    } /* end while (reading result) */
 	} /* end if (b64decoder_load) */
+	DPRINTF("ret rs=%d wlen=%d\n",rs,wlen) ;
 	return (rs >= 0) ? wlen : rs ;
-}
-/* end subroutine (procln) */
+} /* end method (decoder::procln) */
 
-/* write out adjusted text */
-local int bwritetext(FILE *ofp,int crp,cchar *sp,int sl) noex {
+int decoder::proctext(cchar *sp,int sl) noex {
 	int		rs = SR_OK ;
 	int		wlen = 0 ;
-	cchar		*cp ;
-
-	while ((cp = strnbrk(sp,sl,"\r\n")) != nullptr) {
-
-	    if (*crp && (*sp != '\n')) {
-	        *crp = false ;
-	        rs = bputc(ofp,'\r') ;
+	DPRINTF("ent\n") ;
+	for (char *tp ; (tp = strnend(sp,sl)) != nullptr ; ) {
+	    cint tl = intconv(tp - sp) ;
+	    {
+	        rs = proctextln(sp,tl) ;
 	        wlen += rs ;
-	    } /* end if */
-
-	    if ((rs >= 0) && ((cp - sp) > 0)) {
-	        rs = bwrite(ofp,sp,(cp - sp)) ;
-	        wlen += rs ;
-	    } /* end if */
-
-	    if ((rs >= 0) && (*cp == '\n')) {
-	        *crp = false ;
-	        rs = bputc(ofp,'\n') ;
-	        wlen += rs ;
-	    } else if (*cp == '\r') {
-	        *crp = TRUE ;
 	    }
-
-	    sl -= ((cp + 1) - sp) ;
-	    sp = (cp + 1) ;
-
+	    sl -= intconv((tp + 2) - sp) ;
+	    sp = (tp + 2) ;
 	    if (rs < 0) break ;
-	} /* end while */
-
+	} /* end for */
 	if ((rs >= 0) && (sl > 0)) {
-
-	    if (*crp) {
-	        *crp = false ;
-	        rs = bputc(ofp,'\r') ;
-	        wlen += rs ;
-	    } /* end if */
-
-	    if (rs >= 0) {
-	        rs = bwrite(ofp,sp,sl) ;
-	        wlen += rs ;
-	    }
-
-	} /* end if (end processing) */
-
+	    rs = fwriter(pip->ofp,sp,sl) ;
+	    wlen += rs ;
+	} /* end if (remaining) */
+	DPRINTF("ret rs=%d wlen=%d\n",rs,wlen) ;
 	return (rs >= 0) ? wlen : rs ;
-}
-/* end subroutine (bwritetext) */
+} /* end method (decoder::proctext) */
 
-local int coneol(cchar *lp,int ll) noex {
-	int		el = 0 ;
-	if ((ll > 0) && (lp[ll -1] == CH_NL)) {
-	    ll -= 1 ;
-	    el += 1 ;
-	}
-	if ((ll > 0) && (lp[ll -1] == CH_CR)) {
-	    ll -= 1 ;
-	    el += 1 ;
-	}
-	return el ;
-}
-/* end subroutine (coneol) */
+int decoder::proctextln(cchar *sp,int sl) noex {
+	int		rs = SR_OK ;
+	int		wlen = 0 ;
+	DPRINTF("ent\n") ;
+	if (sl > 0) {
+	    if ((rs = fwriter(pip->ofp,sp,sl)) >= 0) {
+		wlen += rs ;
+		rs = fputch(pip->ofp,CH_NL) ;
+		wlen += rs ;
+	    }
+	} /* end if (non-zero positive) */
+	DPRINTF("ret rs=%d wlen=%d\n",rs,wlen) ;
+	return (rs >= 0) ? wlen : rs ;
+} /* end method (decoder::proctextln) */
 
 
