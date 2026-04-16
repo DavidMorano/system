79af7518505b5980411307bf30a6d8be12f4840d
David A.D. Morano <david.a.morano@gmail.com>
2026-04-16 00:38:05 -0400

    update

--- src/libdam/termtrans.h
+++ src/libdam/termtrans.h
@@ -24,10 +24,7 @@
 #include	<stddef.h>		/* |wchar_t| */
 #include	<uiconv.h>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 
 
 #define	TERMTRANS	struct termtrans_head
