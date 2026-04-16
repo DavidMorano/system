dcdaece7c9dddd3c77b451b705b85b680f55509c
David A.D. Morano <david.a.morano@gmail.com>
2026-04-16 00:40:33 -0400

    restore

---
+++ src/libmailmsg/mailaddrtypes.h
@@ -0,0 +1,38 @@
+/* mailaddrtypes HEADER (Mail-Address-Types) */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* Mail_Address_Types */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-12, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MAILADDRTYPES_INCLUDE
+#define	MAILADDRTYPES_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+enum mailaddrtypes {
+    mailaddrtype_local,
+    mailaddrtype_uucp,
+    mailaddrtype_arpa,
+    mailaddrtype_arparoute,
+    mailaddrtype_overlast
+} ; /* end enum (mailaddrtypes) */
+
+
+#endif /* MAILADDRTYPES_INCLUDE */
+
+
