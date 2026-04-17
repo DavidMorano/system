16783689a53c5c6b00166182e8bb7aef1b9ab3b9
David A.D. Morano <david.a.morano@gmail.com>
2026-04-17 00:25:43 -0400

    restore

---
+++ src/libu/usys/usys_xtistructs.h
@@ -0,0 +1,89 @@
+/* usys_xtistructs HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* UNIX® XTI support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	System:
+	usys_xti
+
+	Description:
+	This file contains some support for the UNIX® USTSXTI related
+	operations.
+
+*******************************************************************************/
+
+#ifndef	USTSXTISTRUCTSINCLUDE
+#define	USTSXTISTRUCTSINCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/types.h>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+#if	defined(SYSHAS_XTI) && (SYSHAS_XTI > 0)
+
+/* congratulations: you have it! */
+
+#else /* defined(SYSHAS_XTI) && (SYSHAS_XTI > 0) */
+
+struct netbuf {
+        unsigned int maxlen;
+        unsigned int len;
+        void *buf;
+} ; /* end struct */
+
+struct t_info {
+        int addr;        /* max size of protocol address         */
+        int options;     /* max size of protocol options         */
+        int tsdu;        /* max size of max transport service    */
+                                /* data unit    */
+        int etsdu;       /* max size of max expedited tsdu       */
+        int connect;     /* max data for connection primitives   */
+        int discon;      /* max data for disconnect primitives   */
+        int servtype;    /* provider service type                */
+        int flags;       /* other info about transport providers */
+} ; /* end struct */
+
+struct t_bind {
+        struct netbuf   addr;
+        unsigned int    qlen;
+} ; /* end struct */
+
+struct t_call {
+        struct netbuf   addr;           /*  address             */
+        struct netbuf   opt;            /* options              */
+        struct netbuf   udata;          /* user data            */
+        int             sequence;       /* sequence number      */
+} ; /* end struct */
+
+/* type-aliases for XTI structures */
+#define	NETBUF		struct netbuf
+#define	UTINFO		struct t_info
+#define	UTBIND		struct t_bind
+#define	UTCALL		struct t_call
+
+
+#endif /* defined(SYSHAS_XTI) && (SYSHAS_XTI > 0) */
+
+
+#endif /* USTSXTISTRUCTSINCLUDE */
+
+
