fa1ee3080c61cb35333e2dc4258eaac54244516a
David A.D. Morano <david.a.morano@gmail.com>
2026-04-19 00:59:13 -0400

    update

--- src/utypedefs.h
+++ src/utypedefs.h
@@ -118,6 +118,13 @@ typedef decltype(std::nothrow)	nothrow_t ;
 #endif /* __cplusplus */
 #endif /* TYPEDEF_NOTHROWT */
 
+/* aliases for sysem types */
+
+#ifndef	TYPEDEF_FDSET
+#define	TYPEDEF_FDSET
+typedef fd_set			fdset ;
+#endif
+
 /* handle some really brain-damaged systems -- like MacOS-X Darwin®! */
 #if	defined(OSNAME_Darwin) && (OSNAME_Darwin > 0)
 #if	defined(OSNUM) && (OSNUM <= 7)
@@ -616,12 +623,25 @@ typedef const dev_t		cdev ;
 typedef const ino_t		cino ;
 #endif
 
+#ifndef	TYPEDEF_CFDSET
+#define	TYPEDEF_CFDSET
+typedef const fd_set		cfdset ;
+#endif
+
 #ifndef	TYPEDEF_VOIDF
 #define	TYPEDEF_VOIDF
 EXTERNC_begin
 typedef void (*void_f)() noex ;
 EXTERNC_end
 #endif /* TYPEDEF_VOIDF */
+
+
+#ifndef	TYPEDEF_QSORTF
+#define	TYPEDEF_QSORTF
+EXTERNC_begin
+typedef int (*qsort_f)(const void *,const void *) noex ;
+EXTERNC_end
+#endif	/* TYPEDEF_QSORTF */
 
 #ifndef	TYPEDEF_SORTVCMP
 #define	TYPEDEF_SORTVCMP
