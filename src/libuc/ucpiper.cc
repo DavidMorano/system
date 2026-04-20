8b64222d071c44718e4e6389586f2ea894c48c85
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:16:11 -0400

    update

--- src/libuc/ucpiper.cc
+++ src/libuc/ucpiper.cc
@@ -1,24 +1,41 @@
-/* uc_piper SUPPORT */
+/* ucpipeover SUPPORT */
 /* charset=ISO8859-1 */
-/* lang=C++20 (conformance reviewed) */
+/* lang=C20 */
 
-/* interface component for UNIX®Â® library-3c */
-/* create pipes but moved up to a minimum FD number */
+/* open pipes */
+/* version %I% last-modified %G% */
 
-#define	CF_DEBUGS	0		/* compile-time debugging */
 
 /* revision history:
 
-	= 1998-03-11, David A­D­ Morano
+	= 1999-04-01, David A­D­ Morano
 	This code was originally written.
 
 */
 
-/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
-  	Name:
+	Name:
+	uc_pipe2
+
+	Description:
+	Enhanced |pipe(2)|.
+
+	Synopsis:
+	int uc_pipe2(int pipes[2],int of) noex
+	int uc_piper(int pipes[2],int of,min) noex
+
+	Arguments:
+	pipes		array of two integers to receive created pipe FDs
+	om		open flags
+
+	Returns:
+	>=0		OK
+	<0		error (system-return)
+
+	Name:
 	uc_piper
 
 	Description:
@@ -26,15 +43,17 @@
 	resulting FDs that are at or above a minimum FD value.
 
 	Synopsis:
-	int uc_piper(int *pipes,int minfd) noex
+	int uc_piper(int *pipes,int of,ine minfd) noex
 
 	Arguments:
 	pipes		resulting array of two pipes
+	of		open-flags
 	minfd		minimum number where new FD will move to
 
 	Returns:
 	>=0		the new FD
 	<0		error (system-return)
+
 
 	Notes:
 	In the old days (or on many BSD type UNIX®Â® systems) the
@@ -54,20 +73,36 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>
-#include	<sys/param.h>
-#include	<sys/stat.h>
 #include	<unistd.h>
 #include	<fcntl.h>
 #include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<usystem.h>
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
 #include	<localmisc.h>
 
 
 /* local defines */
 
+#if	defined(SYSHAS_PIPES) && (SYSHAS_PIPES > 0)
+#define	F_PIPES		1
+#else
+#define	F_PIPES		0
+#endif
+
 
 /* external subroutines */
+
+extern "C" {
+    extern int uc_nonblock(int,int) noex ;
+    extern int uc_closeonexec(int,int) noex ;
+    extern int uc_moveup(int,int) noex ;
+}
+
+extern "C" {
+    extern int uc_pipe2(int *,int) noex ;
+}
 
 
 /* external variables */
@@ -81,16 +116,49 @@
 
 /* local variables */
 
+cbool		f_pipes = F_PIPES ;
+
 
 /* exported variables */
 
 
 /* exported subroutines */
 
-int uc_piper(int *pipes,int minfd) noex {
+int uc_pipes(int *pipes,int of) noex {
+	int		rs ;
+	if_constexpr (f_pipes) {
+	    rs = u_pipe2(pipes,of) ;
+	} else {
+	    if ((rs = u_pipe(pipes)) >= 0) {
+	        cint	f = true ;
+	        if ((rs >= 0) && (of & O_NONBLOCK)) {
+	            if ((rs = uc_nonblock(pipes[0],f)) >= 0) {
+	                rs = uc_nonblock(pipes[1],f) ;
+	            }
+	        }
+	        if ((rs >= 0) && (of & O_CLOEXEC)) {
+	            if ((rs = uc_closeonexec(pipes[0],f)) >= 0) {
+	                rs = uc_closeonexec(pipes[1],f) ;
+	            }
+	        }
+	        if (rs < 0) {
+		    u_close(pipes[0]) ;
+		    u_close(pipes[1]) ;
+	        }
+	    } /* end if (u_pipe) */
+	} /* end if_constexpr (f_pipes) */
+	return rs ;
+}
+/* end subroutine (uc_pipes) */
+
+int uc_pipe2(int *pipes,int of) noex {
+	return uc_pipes(pipes,of) ;
+}
+
+int uc_piper(int *pipes,int of,int minfd) noex {
 	int		rs = SR_FAULT ;
 	if (pipes) {
-	    if (((rs = u_pipe(pipes)) >= 0) && (minfd >= 1)) {
+	    if (((rs = uc_pipe2(pipes,of)) >= 0) && (minfd >= 1)) {
 	        for (int i = 0 ; (rs >= 0) && (i < 2) ; i += 1) {
 	            if (pipes[i] < minfd) {
 	                if ((rs = uc_moveup(pipes[i],minfd)) >= 0) {
