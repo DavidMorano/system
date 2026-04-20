3acf75f019f6536ab7f19c68875dbf884934699c
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:15:30 -0400

    update

--- src/libuc/ucftruncate.cc
+++ src/libuc/ucftruncate.cc
@@ -1,10 +1,9 @@
-/* uc_ftruncate */
+/* ucftruncate */
 /* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* interface component for UNIX® library-3c */
-
-
-#define	CF_DEBUGS	0
+/* version %I% last-modified %G% */
 
 
 /* revision history:
@@ -16,9 +15,7 @@
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
-
 #include	<envstandards.h>	/* MUST be first to configure */
-
 #include	<sys/types.h>
 #include	<sys/uio.h>
 #include	<sys/stat.h>
@@ -26,8 +23,13 @@
 #include	<fcntl.h>
 #include	<poll.h>
 #include	<cerrno>
-
-#include	<usystem.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<usupport.h>
+#include	<utimeout.h>
 #include	<localmisc.h>
 
 
@@ -38,26 +40,28 @@
 
 /* external subroutines */
 
-extern int	msleep(int) ;
-
 
 /* forward references */
 
-static int	uc_ftruncater(int,off_t) ;
+local int	uc_ftruncater(int,off_t) noex ;
+
+
+/* exported variables */
 
 
 /* exported subroutines */
 
-
-int uc_ftruncate(int fd,off_t len)
-{
-	int		rs = SR_OK ;
-	if (len < 0) {
-	    rs = u_seeko(fd,0L,SEEK_CUR,&len) ;
-	}
-	if (rs >= 0) {
-	    rs = uc_ftruncater(fd,len) ;
-	}
+int uc_ftruncate(int fd,off_t len) noex {
+	int		rs = SR_BADFD ;
+	if (fd >= 0) {
+	    rs = SR_OK ;
+	    if (len < 0) {
+	        rs = u_seeko(fd,0L,SEEK_CUR,&len) ;
+	    }
+	    if (rs >= 0) {
+	        rs = uc_ftruncater(fd,len) ;
+	    }
+	} /* end if (valid) */
 	return rs ;
 }
 /* end suboutine (uc_ftruncate) */
@@ -65,33 +69,29 @@ int uc_ftruncate(int fd,off_t len)
 
 /* local subroutines */
 
-
-static int uc_ftruncater(int fd,off_t len)
-{
+local int uc_ftruncater(int fd,off_t len) noex {
 	int		rs ;
-	int		to_again = TO_AGAIN ;
-	int		f_exit = FALSE ;
-
+	int		to_again = utimeout[uto_again] ;
+	bool		fexit = false ;
 	repeat {
-	    if ((rs = ftruncate(fd,len)) < 0) rs = (- errno) ;
-	    if (rs < 0) {
+	    if ((rs = ftruncate(fd,len)) < 0) {
+		rs = (- errno) ;
 	        switch (rs) {
 	        case SR_AGAIN:
 	            if (to_again-- > 0) {
 			msleep(10) ;
 		    } else {
-			f_exit = TRUE ;
+			fexit = true ;
 		    }
 	            break ;
 	        case SR_INTR:
 	            break ;
 	        default:
-		    f_exit = TRUE ;
+		    fexit = true ;
 	            break ;
 	        } /* end switch */
 	    } /* end if (error) */
-	} until ((rs >= 0) || f_exit) ;
-
+	} until ((rs >= 0) || fexit) ;
 	return rs ;
 }
 /* end subroutine (uc_ftruncater) */
