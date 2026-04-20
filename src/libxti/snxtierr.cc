3bd640e1505c45f1f0dd4af5f3336869935991f1
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:17:56 -0400

    update

--- src/libxti/snxtierr.cc
+++ src/libxti/snxtierr.cc
@@ -1,4 +1,4 @@
-/* snxtierr SUPPORT */
+/* snxtierr SUPPORT (X/Open Transport Interface) */
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
@@ -47,6 +47,8 @@
 #include	<sncpyx.h>
 #include	<localmisc.h>
 
+#include	"snxti.h"
+
 
 /* local defines */
 
@@ -63,52 +65,77 @@ struct val {
 	int		v ;
 	cchar		*n ;
 	cchar		*msg ;
-} ;
+} ; /* end struct */
 
 
 /* forward references */
 
-static int	findent(int) noex ;
+local int	findent(int) noex ;
 
 
 /* local variables */
 
-static const val	vals[] = {
-	{ TBADADDR, "BADADDR", "incorrect address format" },
- 	{ TBADOPT, "BADOPT", "incorrect options format" },
-	{ TACCES, "ACCESS", "illegal permissions" },
-	{ TBADF, "BADF", "illegal file descriptor" },
-	{ TNOADDR, "NOADDR", "could not allocate address" },
-	{ TOUTSTATE, "OUTSTATE", "routine will place interface out of state" },
-	{ TBADSEQ, "BADSEQ", "illegal called-calling sequence number" },
-	{ TSYSERR, "SYSERR", "system error" },
-	{ TLOOK, "LOOK", "an event requires attention" },
-	{ TBADDATA, "BADDATA", "illegal amount of data" },
-	{ TBUFOVFLW, "BUFOVFLW", "buffer not large enough" },
-	{ TFLOW, "FLOW" "cannot send message - (blocked)" },
-	{ TNODATA, "NODATA", "no message currently available" },
-	{ TNODIS, "NODIS", "disconnect message not found" },
-	{ TNOUDERR, "NOUDERR", "unitdata error message not found" },
-	{ TBADFLAG, "BADFLAG", "incorrect flags specified" },
-	{ TNOREL, "NOREL", "orderly release message not found" },
-	{ TNOTSUPPORT, "NOTSUPPORT", "primitive not supported by provider" },
-	{ TSTATECHNG, "STATECHNG", "state is in process of changing" },
-	{ TNOSTRUCTYPE, "TNOSTRUCTYPE", 
-	"unsupported structure type requested" },
-	{ TBADNAME, "BADNAME", "invalid transport provider name" },
-	{ TBADQLEN, "BADQLEN", "listener queue length limit is zero" },
-	{ TADDRBUSY, "ADDRBUSY", "transport address is in use" },
-	{ TINDOUT, "INDOUT", "outstanding connection indications" },
-	{ TPROVMISMATCH, "PROVMISMATCH", 
-	"listener-acceptor transport provider mismatch" },
-	{ TRESQLEN, "TRESQLEN", 
-	"connection acceptor has listen queue length limit greater than zero" },
-	{ TRESADDR, "RESADDR", 
-	"connection acceptor-listener addresses not "
-	"same but required by transport" },
-	{ TQFULL, "QFULL", "incoming connection queue is full" },
-	{ TPROTO, "PROTO", "protocol error on transport primitive" },
-	{ -1, nullptr }
+constexpr val		vals[] = {
+	{ TBADADDR,		"BADADDR",
+	    "incorrect address format" },
+ 	{ TBADOPT,		"BADOPT",
+	    "incorrect options format" },
+	{ TACCES,		"ACCESS",
+	    "illegal permissions" },
+	{ TBADF,		"BADF",
+	    "illegal file descriptor" },
+	{ TNOADDR,		"NOADDR",
+	    "could not allocate address" },
+	{ TOUTSTATE,		"OUTSTATE",
+	    "routine will place interface out of state" },
+	{ TBADSEQ,		"BADSEQ",
+	    "illegal called-calling sequence number" },
+	{ TSYSERR,		"SYSERR",
+	    "system error" },
+	{ TLOOK,		"LOOK",
+	    "an event requires attention" },
+	{ TBADDATA,		"BADDATA",
+	    "illegal amount of data" },
+	{ TBUFOVFLW,		"BUFOVFLW",
+	    "buffer not large enough" },
+	{ TFLOW,		"FLOW",
+	    "cannot send message - (blocked)" },
+	{ TNODATA,		"NODATA",
+	    "no message currently available" },
+	{ TNODIS,		"NODIS",
+	    "disconnect message not found" },
+	{ TNOUDERR,		"NOUDERR",
+	    "unitdata error message not found" },
+	{ TBADFLAG,		"BADFLAG",
+	    "incorrect flags specified" },
+	{ TNOREL,		"NOREL",
+	    "orderly release message not found" },
+	{ TNOTSUPPORT,		"NOTSUPPORT",
+	    "primitive not supported by provider" },
+	{ TSTATECHNG,		"STATECHNG",
+	    "state is in process of changing" },
+	{ TNOSTRUCTYPE,		"TNOSTRUCTYPE", 
+	    "unsupported structure type requested" },
+	{ TBADNAME,		"BADNAME",
+	    "invalid transport provider name" },
+	{ TBADQLEN,		"BADQLEN",
+	    "listener queue length limit is zero" },
+	{ TADDRBUSY,		"ADDRBUSY",
+	    "transport address is in use" },
+	{ TINDOUT,		"INDOUT",
+	    "outstanding connection indications" },
+	{ TPROVMISMATCH,	"PROVMISMATCH", 
+	    "listener-acceptor transport provider mismatch" },
+	{ TRESQLEN,		"TRESQLEN", 
+	    "connection acceptor has non-zero listen queue length limit" },
+	{ TRESADDR,		"RESADDR", 
+	    "connection acceptor-listener addresses not "
+		"same but required by transport" },
+	{ TQFULL,		"QFULL",
+	    "incoming connection queue is full" },
+	{ TPROTO,		"PROTO",
+	    	"protocol error on transport primitive" },
+	{ -1, nullptr, nullptr }
 } ; /* end struct (val) */
 
 
@@ -132,7 +159,7 @@ int snxtierr(char *dbuf,int dlen,int v) noex {
 
 /* local subroutines */
 
-static int findent(int v) noex {
+local int findent(int v) noex {
 	int		i ; /* used-afterwards */
 	bool		f = false ;
 	for (i = 0 ; vals[i].v >= 0 ; i += 1) {
@@ -140,7 +167,6 @@ static int findent(int v) noex {
 	    if (f) break ;
 	} /* end for */
 	return (f) ? i : -1 ;
-}
-/* end subroutine (findent) */
+} /* end subroutine (findent) */
 
 
