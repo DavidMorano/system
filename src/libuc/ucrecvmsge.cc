fff6c50f3cc2305fea0b579fe30da26ad839315a
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:16:22 -0400

    update

--- src/libuc/ucrecvmsge.cc
+++ src/libuc/ucrecvmsge.cc
@@ -1,4 +1,4 @@
-/* uc_recvmsge SUPPORT */
+/* ucrecvmsge SUPPORT */
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
@@ -23,12 +23,8 @@
 	abort if it times out.
 
 	Synopsis:
-	int uc_recvmsge(fd,msgp,flags,timeout,opts)
-	int		fd ;
-	struct msghdr	*msgp ;
-	int		flags ;
-	int		timeout ;
-	int		opts ;
+	int uc_recvmsge(int fd,MSGHDRD *msgp,int flags,
+		int timeout,int opts) noex
 
 	Arguments:
 	fd		file descriptor
@@ -50,10 +46,14 @@
 #include	<sys/stat.h>
 #include	<unistd.h>
 #include	<poll.h>
-#include	<climits>
 #include	<ctime>
-#include	<cstring>
-#include	<usystem.h>
+#include	<climits>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<usupport.h>
 #include	<bufprintf.h>
 #include	<localmisc.h>
 
@@ -69,8 +69,6 @@
 
 /* external subroutines */
 
-extern int	msleep(int) ;
-
 
 /* forward references */
 
@@ -84,36 +82,33 @@ static char	*d_reventstr() ;
 
 /* exported subroutines */
 
-int uc_recvmsge(fd,msgp,flags,timeout,opts)
-int		fd ;
-struct msghdr	*msgp ;
-int		flags ;
-int		timeout ;
-int		opts ;
-{
-	struct pollfd	fds[2] ;
-	time_t		previous = time(NULL) ;
+int uc_recvmsge(int fd,MSGHDR *msgp,int flags,int timeout,int opts) noex {
+	POLLFD		fds[2] = {} ;
+	time_t		previous = time(nullptr) ;
 	time_t		current ;
 	int		rs = SR_OK ;
-	int		events = POLLEVENTS ;
 	int		nfds ;
 	int		to = timeout ;
+	short		events = POLLEVENTS ;
 	int		pollint ;
 	int		len = 0 ;
-	int		f_first = TRUE ;
+	int		f_first = true ;
 
 #if	CF_DEBUGS
 	char		ebuf[EBUFLEN + 1] ;
 #endif
 
-	if (msgp == NULL) return SR_FAULT ;
+	(void) opts ;
+	if (msgp == nullptr) return SR_FAULT ;
 
-	if (timeout < 0)
+	if (timeout < 0) {
 	    timeout = INT_MAX ;
+	}
 
 	pollint = TO_POLL ;
-	if (timeout >= 0)
+	if (timeout >= 0) {
 	    pollint = MIN(timeout,TO_POLL) ;
+	}
 
 #if	defined(POLLRDNORM)
 	events |= POLLRDNORM ;
@@ -123,23 +118,21 @@ int		opts ;
 #endif
 
 	nfds = 0 ;
-	memset(fds,0,sizeof(fds)) ;
 	fds[nfds].fd = fd ;
 	fds[nfds].events = events ;
 	nfds += 1 ;
 	fds[nfds].fd = -1 ;
 
 	while ((rs >= 0) && (f_first || (to > 0))) {
-	    int	f_break = FALSE ;
-	    f_first = FALSE ;
+	    int	f_break = false ;
+	    f_first = false ;
 
 	    if ((rs = u_poll(fds,nfds,(pollint * POLL_INTMULT))) > 0) {
-		const int	re = fds[0].revents ;
-
+		cint	re = fds[0].revents ;
 		if ((re & POLLIN) || (re & POLLPRI)) {
 	            rs = u_recvmsg(fd,msgp,flags) ;
 	            len = rs ;
-		    f_break = TRUE ;
+		    f_break = true ;
 		} else if (re & POLLNVAL) {
 		    rs = SR_NOTOPEN ;
 		} else if (re & POLLERR) {
@@ -147,27 +140,25 @@ int		opts ;
 		} else if (re & POLLHUP) {
 		    msleep(1) ;
 		} /* end if */
-
 	    } else if (rs == 0) {
-
-	        current = time(NULL) ;
-
-	        to -= (current - previous) ;
+	        current = time(nullptr) ;
+	        to -= intconv(current - previous) ;
 	        previous = current ;
-	        if (to < TO_POLL)
+	        if (to < TO_POLL) {
 	            pollint = to ;
-
-	        if (to <= 0)
+		}
+	        if (to <= 0) {
 	            rs = SR_TIMEDOUT ;
-
-	    } else if (rs == SR_INTR)
+		}
+	    } else if (rs == SR_INTR) {
 		rs = SR_OK ;
-
+	    }
 	    if (f_break) break ;
 	} /* end while */
 
-	if ((rs >= 0) && (timeout >= 0) && (to <= 0))
+	if ((rs >= 0) && (timeout >= 0) && (to <= 0)) {
 	    rs = SR_TIMEDOUT ;
+	}
 
 #if	CF_DEBUGS
 	debugprintf("uc_recvmsge: ret rs=%d len=%d\n",
@@ -179,11 +170,7 @@ int		opts ;
 /* end subroutine (uc_recvmsge) */
 
 #if	CF_DEBUGS
-static char *d_reventstr(revents,buf,buflen)
-int	revents ;
-char	buf[] ;
-int	buflen ;
-{
+static char *d_reventstr(int revents,char *buf,int buflen) noex {
 	buf[0] = '\0' ;
 	bufprintf(buf,buflen,"%s %s %s %s %s %s %s %s %s",
 	    (revents & POLLIN) ? "I " : "  ",
