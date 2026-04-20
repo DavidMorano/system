c7a259c77fd1a0a6ba4136e7dec68c1ff90c963a
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:16:38 -0400

    update

--- src/libuc/ucsendfile.cc
+++ src/libuc/ucsendfile.cc
@@ -1,12 +1,11 @@
-/* uc_sendfile */
+/* ucsendfile */
 /* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* interface component for UNIX® library-3c */
 /* UNIX® write system call subroutine */
 
-
 #define	CF_DEBUGS	0		/* compile-time debugging */
-
 
 /* revision history:
 
@@ -17,14 +16,16 @@
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
-
-#include	<envstandards.h>
-
-#include	<sys/types.h>
-#include	<sys/sendfile.h>
-#include	<climits>
-
-#include	<usystem.h>
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/types.h>		/* for |sendfile(3net)| */
+#include	<sys/socket.h>		/* for |sendfile(3net)| */
+#include	<sys/uio.h>		/* for |sendfile(3net)| */
+#include	<climits>		/* |INT_MAX| */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
 #include	<localmisc.h>
 
 
@@ -46,44 +47,56 @@
 /* local variables */
 
 
+/* exported variables */
+
+
 /* exported subroutines */
 
-
-int uc_sendfile(int ofd,int ifd,off_t *offp,int wlen)
-{
-	off_t		fo ;
+int uc_sendfile(int ofd,int ifd,off_t *offp,int wlen) noex {
+	off_t		fo = 0z ;
 	int		rs = SR_OK ;
 	int		len = 0 ;
 
-	if (offp == NULL) return SR_FAULT ;
+	if (offp == nullptr) return SR_FAULT ;
 
-	repear {
+#ifdef	COMMENT
+	repeat {
 	    ssize_t	ss ;
 	    fo = (off_t) (*offp & INT_MAX) ;
 	    ss = sendfile(ofd,ifd,&fo,wlen) ;
 	    if (ss == -1) rs = (- errno) ;
 	    len = (ss & INT_MAX) ;
 	} until (rs != SR_INTR) ;
+#else /* COMMENT */
+	(void) ifd ;
+	(void) ofd ;
+	(void) wlen ;
+	rs = SR_NOSYS ;
+#endif /* COMENT */
 
 	*offp = (off_t) fo ;
 	return (rs >= 0) ? len : rs ;
 }
 /* end subroutine (uc_sendfile) */
 
-
-int uc_sendbuf(int ofd,void *wbuf,int wlen)
-{
+int uc_sendbuf(int ofd,void *wbuf,int wlen) noex {
 	int		rs = SR_OK ;
 	int		len = 0 ;
 
-	if (offp == NULL) return SR_FAULT ;
+	if (wbuf == nullptr) return SR_FAULT ;
 
-	repear {
+#ifdef	COMMENT
+	repeat {
 	    ssize_t	ss ;
 	    ss = sendfile(ofd,SFV_FD_SELF,wbuf,wlen) ;
 	    if (ss == -1) rs = (- errno) ;
 	    len = (ss & INT_MAX) ;
 	} until (rs != SR_INTR) ;
+#else /* COMMENT */
+	(void) ofd ;
+	(void) wlen ;
+	rs = SR_NOSYS ;
+#endif /* COMENT */
 
 	return (rs >= 0) ? len : rs ;
 }
