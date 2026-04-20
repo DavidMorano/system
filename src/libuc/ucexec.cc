2cbd2e0ff7db012e81acec89b63fbf7ac7960f36
David A.D. Morano <david.a.morano@gmail.com>
2026-04-19 21:56:36 -0400

    update

--- src/libuc/ucexec.cc
+++ src/libuc/ucexec.cc
@@ -19,6 +19,10 @@
 
 /*******************************************************************************
 
+  	Name:
+	uc_execve
+
+	Description:
 	For now, this just is a regular (fairly so at any rate)
 	|exec(2)|-like subroutine.
 
@@ -29,9 +33,12 @@
 #include	<unistd.h>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<uclibmem.h>
 #include	<mkx.h>
-#include	<libmallocxx.h>
+#include	<localmisc.h>
 
 
 /* local defines */
@@ -61,8 +68,8 @@ int uc_execve(cchar *fname,mainv av,mainv ev) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
 	int		rv = 0 ;
-	if (fname && av && ev) {
-	    if (char *ebuf ; (rs = libmalloc_mp(&ebuf)) >= 0) {
+	if (fname && av && ev) ylikely {
+	    if (char *ebuf ; (rs = lm_mp(&ebuf)) >= 0) ylikely {
 	        if ((rs = mkpathexp(ebuf,fname,-1)) > 0) {
 		    rs = u_execve(ebuf,av,ev) ;
 		    rv = rs ;
@@ -70,7 +77,7 @@ int uc_execve(cchar *fname,mainv av,mainv ev) noex {
 		    rs = u_execve(fname,av,ev) ;
 		    rv = rs ;
 	        }
-	        rs1 = libmalloc_free(ebuf) ;
+	        rs1 = lm_free(ebuf) ;
 	        if (rs >= 0) rs = rs1 ;
 	    } /* end if (m-a-f) */
 	} /* end if (non-null) */
