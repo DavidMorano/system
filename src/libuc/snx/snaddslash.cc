8845f80eb4eed2dec18aaea1365ba24f64239026
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:09:38 -0400

    update

--- src/libuc/snx/snaddslash.cc
+++ src/libuc/snx/snaddslash.cc
@@ -59,10 +59,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<localmisc.h>
 
 #include	"snaddslash.h"
