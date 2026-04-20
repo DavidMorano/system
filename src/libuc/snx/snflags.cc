4d28be8d29e9c10989da2e8090ac9c69916f41da
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:13:21 -0400

    update

--- src/libuc/snx/snflags.cc
+++ src/libuc/snx/snflags.cc
@@ -49,10 +49,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<ulogerror.h>
 #include	<storebuf.h>
 #include	<ascii.h>
