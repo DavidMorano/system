667a3397a887fb6369460493dfd131d04a7e9a1f
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:16:27 -0400

    update

--- src/libuc/ucreuseaddr.cc
+++ src/libuc/ucreuseaddr.cc
@@ -1,5 +1,6 @@
-/* uc_reuseaddr */
+/* ucreuseaddr */
 /* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* interface component for UNIX® library-3c */
 /* reuse an existing (perhaps) socket address */
@@ -14,25 +15,30 @@
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
-
 #include	<envstandards.h>
-
 #include	<sys/types.h>
 #include	<sys/socket.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<localmisc.h>
+
+
+/* exported variables */
 
 
 /* exported subroutines */
 
-
-int uc_reuseaddr(int s)
-{
-	const int	optlen = sizeof(int) ;
-	int		rs ;
+int uc_reuseaddr(int s) noex {
+	int		optlen = sizeof(int) ;
+	int		rs = SR_BADFD ;
 	int		one = 1 ;
-
-	rs = u_setsockopt(s,SOL_SOCKET,SO_REUSEADDR,&one,optlen) ;
-
-	return rs ;
+	if (s >= 0) {
+	    rs = u_setsockopt(s,SOL_SOCKET,SO_REUSEADDR,&one,optlen) ;
+	}
+	return (rs >= 0) ? one : rs ;
 }
 /* end subroutine (uc_reuseaddr) */
 
