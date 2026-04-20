c69f155d3181bd382fe7f85a4a4da04679c240fa
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:15:43 -0400

    update

--- src/libuc/uckeepalive.cc
+++ src/libuc/uckeepalive.cc
@@ -1,11 +1,9 @@
-/* uc_keepalive */
+/* uckeepalive */
 /* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* interface component for UNIX® library-3c */
 /* set the KeepAlive mode on a (socket) file descriptor */
-
-
-#define	CF_DEBUGS	0		/* compile-time debugging */
 
 
 /* revision history:
@@ -17,49 +15,43 @@
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
-
 #include	<envstandards.h>	/* MUST be first to configure */
-
 #include	<sys/types.h>
 #include	<sys/uio.h>
 #include	<sys/stat.h>
 #include	<unistd.h>
 #include	<fcntl.h>
 #include	<poll.h>
-
-#include	<usystem.h>
+#include	<usysbase.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
 #include	<localmisc.h>
 
 
 /* forward references */
 
 
+/* exported variables */
+
+
 /* exported subroutines */
 
-
-int uc_keepalive(fd,f)
-int		fd ;
-int		f ;
-{
-	const int	slev = SOL_SOCKET ;
-	const int	scmd = SO_KEEPALIVE ;
-
-	int	rs ;
-	int	keepalive ;
-	int	optlen ;
-	int	f_previous = FALSE ;
-
-
-	optlen = sizeof(int) ;
+int uc_keepalive(int fd,int f) noex {
+	cint		slev = SOL_SOCKET ;
+	cint		scmd = SO_KEEPALIVE ;
+	int		rs ;
+	int		keepalive ;
+	int		optlen = sizeof(int) ;
+	int		fprev = true ;
 	if ((rs = u_getsockopt(fd,slev,scmd,&keepalive,&optlen)) >= 0) {
-	    f_previous = (keepalive != 0) ? 1 : 0 ;
-	    if (! LEQUIV(f_previous,f)) {
+	    fprev = (keepalive != 0) ;
+	    if (! LEQUIV(fprev,f)) {
 	        keepalive = f ;
 	        rs = u_setsockopt(fd,slev,scmd,&keepalive,sizeof(int)) ;
 	    } /* end if (needed a change) */
 	} /* end if */
-
-	return (rs >= 0) ? f_previous : rs ;
+	return (rs >= 0) ? fprev : rs ;
 }
 /* end subroutine (uc_keepalive) */
 
