f9b32c93460ed2d2a0fb793bfcc07d4fdba320cb
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:12:57 -0400

    update

--- src/libuc/snx/sncpyx.h
+++ src/libuc/snx/sncpyx.h
@@ -21,10 +21,8 @@
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
+#include	<uconcepts.hh>
 
 #include	<sncpyxc.h>
 #include	<sncpyxw.h>
