b6e6a6228db0a3cc59c3ba9bbd7f99fc3abccb1d
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:09:30 -0400

    update

--- src/libuc/snx/snadd.cc
+++ src/libuc/snx/snadd.cc
@@ -66,10 +66,7 @@
 #include	<cstdlib>
 #include	<cstdarg>		/* |va_list(3c)| */
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<localmisc.h>
 
 #include	"snadd.h"		/* |snadd(3uc)| */
