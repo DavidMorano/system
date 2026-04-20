3cf0be192a095c1d1b46e7f316ac2b7e015eef21
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:13:48 -0400

    update

--- src/libuc/snx/snshellunder.cc
+++ src/libuc/snx/snshellunder.cc
@@ -64,10 +64,7 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<usysbase.h>
 #include	<storebuf.h>
 #include	<localmisc.h>
 
