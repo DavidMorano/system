25308c96762a8c4a9bea753c966c6e8c309fbbeb
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:08:36 -0400

    restore

---
+++ src/libu/uconcepts/uconcepts.hh
@@ -0,0 +1,46 @@
+/* uconcepts HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* these are various system-used concepts */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2020-01-14, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2020 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+/*******************************************************************************
+
+	Name:
+	uconcepts
+
+	Description:
+	These are various system-used concepts.
+
+*******************************************************************************/
+
+#ifndef	UCONCEPTS_INCLUDE
+#define	UCONCEPTS_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>
+#include	<cstdlib>
+#include	<clanguage.h>
+
+#include	<conccharp.hh>
+#include	<conintx.hh>
+
+
+#endif /* __cplusplus */
+#endif /* UCONCEPTS_INCLUDE */
+
+
