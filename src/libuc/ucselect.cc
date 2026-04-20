9d61319f5b4724815cd75aafd56435531314308d
David A.D. Morano <david.a.morano@gmail.com>
2026-04-20 00:16:35 -0400

    update

--- src/libuc/ucselect.cc
+++ src/libuc/ucselect.cc
@@ -1,12 +1,12 @@
-/* uc_select */
+/* ucselect */
 /* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* interface component for UNIX® library-3c */
-
+/* version %I% last-modified %G% */
 
 #define	CF_DEBUGS	0		/* compile-time debugging */
 #define	CF_INTR		0		/* do not return on an interrupt */
-
 
 /* revision history:
 
@@ -18,14 +18,16 @@
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 
-#include	<envstandards.h>
-
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<sys/time.h>
 #include	<unistd.h>
 #include	<cerrno>
-
-#include	<usystem.h>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
 #include	<localmisc.h>
 
 
@@ -41,23 +43,19 @@
 /* local variables */
 
 
+/* exported variables */
+
+
 /* exported subroutines */
 
-
-int uc_select(nfds,readfds,writefds,errorfds,tp)
-int		nfds ;
-fd_set		*readfds ;
-fd_set		*writefds ;
-fd_set		*errorfds ;
-struct timeval	*tp ;
-{
-	int	rs ;
-
-
-	rs = select(nfds,readfds,writefds,errorfds,tp) ;
-	if (rs < 0)
-	    rs = (- errno) ;
-
+int uc_select(int nfds,fd_set *readfds,fd_set *writefds,
+		fd_set *errorfds,TIMEVAL *tp) noex {
+	int		rs = SR_INVALID ;
+	if (readfds || writefds || errorfds) {
+	    if ((rs = select(nfds,readfds,writefds,errorfds,tp)) < 0) {
+	        rs = (- errno) ;
+	    }
+	} /* end if (valid) */
 	return rs ;
 }
 /* end subroutine (uc_select) */
