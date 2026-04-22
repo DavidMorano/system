5c03d11742c5c620097a31ed72de3956eae9d427
David A.D. Morano <david.a.morano@gmail.com>
2026-04-22 00:37:20 -0400

    restore

---
+++ src/libuc/ucdescwrite/ucwritefile.cc
@@ -0,0 +1,134 @@
+/* ucwritefile SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* interface component for UNIX® library-3c */
+/* copy from one file descriptor to another */
+
+
+/* revision history:
+
+	= 1998-01-10, David A­D­ Morano
+	The subroutine was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	uc_writefile
+
+	Description:
+	This subroutine copies data from the specified file to the
+	output file descriptor.
+
+	Synopsis:
+	int uc_writefile(int ofd,cchar *fname) noex
+
+	Arguments:
+	ofd		source file descriptor
+	fname		file to write to output file-descriptor
+
+	Returns:
+	>=0		length copied
+	<0		error (system-return)
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<localmisc.h>
+
+#include	"ucdescbase.hh"
+#include	"ucwritefile.h"
+
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+extern "C" {
+    extern int uc_open(cchar *,int,mode_t) noex ;
+    extern int uc_writedesc(int,int,int) noex ;
+    extern int uc_close(int) ;
+} /* end extern */
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int uc_writefile(int ofd,cchar *fname) noex {
+	int		rs = SR_FAULT ;
+	int		rs1 ;
+	int		len = 0 ; /* return-value */
+	if (fname) ylikely {
+	    rs = SR_BADF ;
+	    if (ofd >= 0) ylikely {
+	        rs = SR_INVALID ;
+		if (fname[0]) ylikely {
+		    cint	of = O_RDONLY ;
+		    cmode	om = 0 ;
+		    if ((rs = uc_open(fname,of,om)) >= 0) ylikely {
+	    	        cint	ifd = rs ;
+	    	        {
+			    rs = uc_writedesc(ofd,ifd,-1) ;
+			    len = rs ;
+ 	    	        }
+	    	        rs1 = uc_close(ifd) ;
+		        if (rs >= 0) rs = rs1 ;
+		    } /* end if (file) */
+	        } /* end if (valid) */
+	    } /* end if (valid-FD) */
+	} /* end if (non-null) */
+	return (rs >= 0) ? len : rs ;
+} /* end subroutine (uc_writefile) */
+
+int uc_filecopy(cchar *sfname,cchar *dfname) noex {
+    	int		rs = SR_FAULT ;
+	int		rs1 ;
+	int		len = 0 ; /* return-value */
+	if (dfname) ylikely {
+	    rs = SR_INVALID ;
+	    if (dfname[0]) ylikely {
+		cint	of = (O_WRONLY | O_CREAT) ;
+		cmode	om = 0777 ;
+		if ((rs = uc_open(dfname,of,om)) >= 0) {
+		    cint	dfd = rs ;
+		    {
+			rs = uc_writefile(dfd,sfname) ;
+			len = rs ;
+		    }
+		    rs1 = uc_close(dfd) ;
+		    if (rs >= 0) rs = rs1 ;
+		} /* end if (destination file) */
+	    } /* end if (valid) */
+	} /* end if (non-null) */
+	return (rs >= 0) ? len : rs ;
+} /* end subroutine (uc_filecopy) */
+
+
