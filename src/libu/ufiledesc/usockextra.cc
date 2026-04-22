3bb863e466546626f807d9b0e1debcc85c8be8d0
David A.D. Morano <david.a.morano@gmail.com>
2026-04-22 00:40:44 -0400

    restore

---
+++ src/libu/ufiledesc/usockextra.cc
@@ -0,0 +1,106 @@
+/* usockextra SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* translation layer interface for UNIX® equivalents */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services
+	(RNS).
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Group:
+	u_sockextra{x}
+
+	Description:
+	This provides special control for devices.
+
+	Synopsis:
+	int u_sockextra{x}(int fd,int cmd,...) noex
+
+	Arguments:
+	fd		file-descriptor
+	cmd		command (request)
+	...		any-argument
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<cstdint>		/* |uintptr_t| */
+#include	<cstdarg>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<usysflag.h>
+#include	<localmisc.h>
+
+#include	"ufiledescbase.hh"
+#include	"usockextra.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward refeferences */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int u_linger(int fd,int to) noex {
+	int		rs ;
+	if (ustat sb ; (rs = u_fstat(fd,&sb)) >= 0) {
+	    if (S_ISSOCK(sb.st_mode)) {
+	        LINGER	ls{} ;
+		cint	sol = SOL_SOCKET ;
+		cint	cmd = SO_LINGER ;
+		cint	llen = szof(LINGER) ;
+		if (to >= 0) {
+	            ls.l_onoff = true ;
+	            ls.l_linger = to ;
+		}
+		{
+		    cchar	*obuf = charp(&ls) ;
+	            rs = u_setsockopt(fd,sol,cmd,obuf,llen) ;
+		}
+	    } /* end if (socket) */
+	} /* end if (stat) */
+	return rs ;
+}
+/* end subroutine (u_linger) */
+
+
+/* local subroutines */
+
+
