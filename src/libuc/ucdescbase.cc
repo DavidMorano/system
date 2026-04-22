c857d16338c48df3fe40c58cd0985c277882e418
David A.D. Morano <david.a.morano@gmail.com>
2026-04-22 00:33:06 -0400

    restore

---
+++ src/libuc/ucdescbase.cc
@@ -0,0 +1,89 @@
+/* ucdescbase SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* interface component for UNIX® library-3c */
+/* extended read */
+
+#define	CF_DEBUG	0		/* debugging */
+#define	CF_NONBLOCK	1		/* use nonblocking mode */
+
+/* revision history:
+
+	= 1998-03-26, David A­D­ Morano
+	This was first written to give a little bit to UNIX® what
+	we have in our own circuit pack OSes!
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Names:
+	ucdescbase
+
+	Description:
+	Get some amount of data and time it also so that we can
+	abort if it times out.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<sys/types.h>
+#include	<sys/uio.h>
+#include	<unistd.h>
+#include	<fcntl.h>
+#include	<poll.h>
+#include	<climits>		/* |INT_MAX| */
+#include	<cstddef>
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<localmisc.h>		/* POLL_INTMULT */
+
+#include	"ucdescbase.hh"
+
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |memclear(3u)| */
+
+/* local defines */
+
+#ifndef	CF_DEBUG
+#define	CF_DEBUG	0
+#endif
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int ucdescbase::operator () (int fd) noex {
+    	return callstd(fd) ;
+}
+
+
