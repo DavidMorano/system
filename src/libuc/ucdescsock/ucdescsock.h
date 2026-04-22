44703331ee0259bbc90321728675a6921f5701fc
David A.D. Morano <david.a.morano@gmail.com>
2026-04-22 00:35:58 -0400

    restore

---
+++ src/libuc/ucdescsock/ucdescsock.h
@@ -0,0 +1,38 @@
+/* ucsock HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* string-index operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCSOCK_INCLUDE
+#define	UCSOCK_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	"ucsockprime.h"
+
+EXTERNC_begin
+
+extern int uc_sockjoin(int,SOCKADDR *,int,int,mode_t) noex ;
+extern int uc_sockreuse(int) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCSOCK_INCLUDE */
+
+
