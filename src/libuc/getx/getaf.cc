85f477068eafeff2a8e76312ab38d55c2cd8e5ef
David A.D. Morano <david.a.morano@gmail.com>
2026-04-23 00:28:40 -0400

    update

--- src/libuc/getx/getaf.cc
+++ src/libuc/getx/getaf.cc
@@ -1,10 +1,13 @@
-/* getaf SUPPORT */
+/* getaf SUPPORT (Get-Address-Family) */
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* get an address-family by name */
 /* version %I% last-modified %G% */
 
+#define	CF_DEBUG	0		/* debugging */
+#define	CF_TEST		0		/* test */
+#define	CF_GETAFLEN	0		/* |getaflen()| */
 
 /* revision history:
 
@@ -47,7 +50,6 @@
 	>=0		resulting address-family index
 	<0		error (system-return)
 
-
 	Name:
 	getaflen
 
@@ -72,31 +74,33 @@
 #include	<sys/socket.h>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
+#include	<algorithm>		/* |sort(3c++)| */
 #include	<clanguage.h>
 #include	<usysbase.h>
 #include	<usupport.h>		/* |libu::cfdec(3u)| */
 #include	<uinet.h>
 #include	<bufsizevar.hh>
 #include	<strdcpy.h>
-#include	<nleadstr.h>
-#include	<hasx.h>
+#include	<strwcmp.h>
+#include	<hasx.h>		/* |hasalldig(3uc)| */
 #include	<localmisc.h>
+#include	<dprintf.hh>		/* debugging */
 
 #include	"getaf.h"
 
+import libutil ;			/* |lenstr(3u)| */
 
 /* local defines */
 
-#ifndef	AFNAMELEN
-#define	AFNAMELEN	12		/* maximum of length in table below */
+#ifndef	CF_DEBUG
+#define	CF_DEBUG	0		/* debugging */
 #endif
-
-#define	ADDRFAM		struct addrfam
-#define	CADDRFAM	const ADDRFAM
 
 
 /* imported namespaces */
 
+using std::sort ;			/* subroutine */
+using std::partition_point ;		/* subroutine */
 using libu::cfdec ;			/* subroutine */
 
 
@@ -114,116 +118,136 @@ using libu::cfdec ;			/* subroutine */
 struct addrfam {
 	cchar		*name ;
 	int		af ;
-} ;
+} ; /* end struct (addrfam) */
+
+namespace {
+    struct getter {
+	con addrfam	*ns ;
+	con uchar	*tab ;
+	int 		ne ;
+	getter(con addrfam *ªns,con uchar *ªtab,int n) noex : ns(ªns) {
+	    tab = ªtab ;
+	    ne = n ;
+	} ; /* end ctor */
+	int operator () (cchar *sp,int sl) const noex {
+            int     rs = SR_AFNOSUPPORT ;
+            int     af = 0 ; /* return-value */
+            cauto predf = [this,sp,sl] (uchar c) noex -> bool {
+                cchar *an = ns[c].name ;
+                return (strwcmp(an,sp,sl) < 0) ;
+            } ; /* end lambda (predf) */
+            con uchar *itf = (tab + 0) ;
+            con uchar *itl = (tab + ne) ;
+            if (cauto it = partition_point(itf,itl,predf) ; it != itl) {
+                cauto mat = [this,sp,sl] (int c) noex -> bool {
+                    cchar *an = ns[c].name ;
+                    return (strwcmp(an,sp,sl) == 0) ;
+                } ; /* end lambda (mat) */
+                if (cint ii = *it ; mat(ii)) {
+                    if ((af = ns[ii].af) >= 0) rs = SR_OK ;
+                } /* end if (got a match) */
+            } /* end if */
+            return (rs >= 0) ? af : rs ;
+	} /* end method (operator) */
+    } ; /* end struct (getter) */
+} /* end namespace */
 
 
 /* forward references */
+
+consteval int cstrcmp(cchar *s1,cchar *s2) noex {
+	while (*s1 && *s2 && (*s1 == *s2)) {
+	    s1++ ;
+	    s2++ ;
+	}
+	return (int(*s1) - int(*s2)) ;
+} /* end subroutine (cstrcmp) */
 
 local int	getdb(cchar *,int) noex ;
 
 
 /* local variables */
 
-constexpr addrfam	addrfamilies[] = {
-	{ "unix", AF_UNIX },
-	{ "inet", AF_INET },
-	{ "inet4", AF_INET4 },
-	{ "inet6", AF_INET6 },
-#if	defined(AF_IMPLINK)
-	{ "implink", AF_IMPLINK },
-#endif
-#if	defined(AF_PUP)
-	{ "pup", AF_PUP },
-#endif
-#if	defined(AF_CHAOS)
-	{ "chaos", AF_CHAOS },
-#endif
-#if	defined(AF_NS)
-	{ "ns", AF_NS },
-#endif
-#if	defined(AF_NBS)
-	{ "nbs", AF_NBS },
-#endif
-#if	defined(AF_ECMA)
-	{ "ecma", AF_ECMA },
-#endif
-#if	defined(AF_DATAKIT)
-	{ "datakit", AF_DATAKIT },
-#endif
-#if	defined(AF_CCITT)
-	{ "ccitt", AF_CCITT },
-#endif
-#if	defined(AF_SNA)
-	{ "sna", AF_SNA },
-#endif
-#if	defined(AF_DECnet)
-	{ "decnet", AF_DECnet },
-#endif
-#if	defined(AF_DLI)
-	{ "dli", AF_DLI },
-#endif
-#if	defined(AF_LAT)
-	{ "lat", AF_LAT },
-#endif
-#if	defined(AF_HYLINK)
-	{ "hylink", AF_HYLINK },
-#endif
-#if	defined(AF_APPLETALK)
-	{ "appletalk", AF_APPLETALK },
-#endif
-#if	defined(AF_NIT)
-	{ "nit", AF_NIT },
-#endif
-#if	defined(AF_802)
-	{ "802", AF_802 },
-#endif
-#if	defined(AF_OSI)
-	{ "osi", AF_OSI },
-#endif
-#if	defined(AF_X25)
-	{ "x25", AF_X25 },
-#endif
-#if	defined(AF_OSINET)
-	{ "osinet", AF_OSINET },
-#endif
-#if	defined(AF_GOSIP)
-	{ "gosip", AF_GOSIP },
-#endif
-#if	defined(AF_IPX)
-	{ "ipx", AF_IPX },
-#endif
-#if	defined(AF_ROUTE)
-	{ "route", AF_ROUTE },
-#endif
-#if	defined(AF_LINK)
-	{ "link", AF_LINK },
-#endif
-#if	defined(AF_KEY)
-	{ "key", AF_KEY },
-#endif
-#if	defined(AF_NCA)
-	{ "nca", AF_NCA },
-#endif
-#if	defined(AF_LOCAL)
-	{ "local", AF_LOCAL },
-#endif
-#if	defined(AF_ISDN)
-	{ "isdn", AF_ISDN },
-#endif
-#if	defined(AF_SYSTEM)
-	{ "system", AF_SYSTEM },
-#endif
-#if	defined(AF_NETBIOS)
-	{ "netbios", AF_NETBIOS },
-#endif
-#if	defined(AF_NDRV)
-	{ "ndrv", AF_NDRV },
-#endif
-	{ "unspecified", AF_UNSPEC },
-	{ nullptr, 0 }
-} ; /* end array (addrfamilies) */
+constexpr addrfam	names_af[] = {
+	{ "unspec",	AF_UNSPEC },
+	{ "unix",	AF_UNIX },
+	{ "inet",	AF_INET },
+	{ "inet4",	AF_INET4 },
+	{ "inet6",	AF_INET6 },
+	{ "implink",	AF_IMPLINK },
+	{ "pup",	AF_PUP },
+	{ "chaos",	AF_CHAOS },
+	{ "ns",		AF_NS },
+	{ "nbs",	AF_NBS },
+	{ "ecma",	AF_ECMA },
+	{ "datakit",	AF_DATAKIT },
+	{ "ccitt",	AF_CCITT },
+	{ "sna",	AF_SNA },
+	{ "decnet",	AF_DECnet },
+	{ "dli",	AF_DLI },
+	{ "lat",	AF_LAT },
+	{ "hylink",	AF_HYLINK },
+	{ "appletalk",	AF_APPLETALK },
+	{ "nit",	AF_NIT },
+	{ "802",	AF_802 },
+	{ "osi",	AF_OSI },
+	{ "x25",	AF_X25 },
+	{ "osinet",	AF_OSINET },
+	{ "gosip",	AF_GOSIP },
+	{ "ipx",	AF_IPX },
+	{ "route",	AF_ROUTE },
+	{ "link",	AF_LINK },
+	{ "key",	AF_KEY },
+	{ "nca",	AF_NCA },
+	{ "local",	AF_LOCAL },
+	{ "isdn",	AF_ISDN },
+	{ "system",	AF_SYSTEM },
+	{ "netbios",	AF_NETBIOS },
+	{ "ndrv",	AF_NDRV },
+	{ "cnt",	AF_CNT },
+	{ "coip",	AF_COIP },
+	{ "e164",	AF_E164 },
+	{ "ieee80211",	AF_IEEE80211 },
+	{ "iso",	AF_ISO },
+	{ "max",	AF_MAX },
+	{ "natm",	AF_NATM },
+	{ "ppp",	AF_PPP },
+	{ "reserved_36", AF_RESERVED_36 },
+	{ "sip",	AF_SIP },
+	{ "utun",	AF_UTUN },
+	{ "vsock",	AF_VSOCK }
+} ; /* end array (names_af) */
+
+constexpr int	ne_af	= nelem(names_af) ;
+
+namespace {
+    struct codemgr {
+	uchar		tab_af	[ne_af] ;
+	consteval void tabload_x(mut uchar *tab,int n) noex {
+	    for (int i = 0 ; i < n ; i += 1) {
+		tab[i] = uchar(i) ;
+	    } ; /* end for */
+	} ; /* end method (tabload_x) */
+	consteval void tabinit(mut uchar *tab,con addrfam *pairs,int ne) noex {
+	    cauto predf = [pairs] (con uchar &ia,con uchar &ib) noex -> bool {
+		cchar *s1 = pairs[ia].name ;
+		cchar *s2 = pairs[ib].name ;
+    		return (cstrcmp(s1,s2) < 0) ;
+	    } ; /* end lambda */
+	    tabload_x(tab,ne) ;
+	    sort(tab,(tab+ne),predf) ;
+	} ; /* end method (tabinit) */
+	consteval codemgr() noex {
+	    tabinit(tab_af,names_af,ne_af) ;
+	} /* end ctor (codemgr) */
+    } ; /* end struct (codemgr) */
+} /* end namespace */
+
+constexpr codemgr	codetab ;
 
 static bufsizevar	maxpathlen(bufsize_mp) ;
+
+cbool			f_debug = CF_DEBUG ;
 
 
 /* exported variables */
@@ -231,9 +255,9 @@ static bufsizevar	maxpathlen(bufsize_mp) ;
 
 /* exported subroutines */
 
-int getaf(cchar *sp,int sl) noex {
+int getaf(cchar *sp,int ªsl) noex {
 	int		rs = SR_FAULT ;
-	if (sp) ylikely {
+	if (int sl = getlenstr(sp,ªsl) ; sl >= 0) ylikely {
 	    rs = SR_INVALID ;
 	    if ((sl > 0) && sp[0]) ylikely {
 	        if (hasalldig(sp,sl)) {
@@ -249,6 +273,7 @@ int getaf(cchar *sp,int sl) noex {
 }
 /* end subroutine (getaf) */
 
+#if	CF_GETAFLEN
 int getaflen(int af) noex {
 	int		rs = SR_AFNOSUPPORT ;
 	switch (af) {
@@ -265,10 +290,11 @@ int getaflen(int af) noex {
 	return rs ;
 }
 /* end subroutine (getaflen) */
+#endif /* CF_GETAFLEN */
 
 /* reads out as: str-af-name */
 cchar *strafname(int af) noex {
-	const ADDRFAM	*afs = addrfamilies ;
+	const addrfam	*afs = names_af ;
 	int		i{} ;
 	for (i = 0 ; afs[i].name ; i += 1) {
 	    if (afs[i].af == af) break ;
@@ -281,25 +307,38 @@ cchar *strafname(int af) noex {
 /* local subroutines */
 
 local int getdb(cchar *sp,int sl) noex {
-	cint		alen = AFNAMELEN ;
-    	int		rs = SR_INVALID ;
-	char		abuf[AFNAMELEN + 1] ;
-	if (int al = intconv(strdcpy1w(abuf,alen,sp,sl) - abuf) ; al > 0) {
-	    CADDRFAM	*afs = addrfamilies ;
-	    cint	n = 2 ;
-	    int		i{} ; /* used-afterwards */
-	    int		m ;
-	    for (i = 0 ; afs[i].name ; i += 1) {
-	        cchar	*anp = afs[i].name ;
-		bool	f = true ;
-	        m = nleadstr(anp,abuf,al) ;
-	        f = f && (m == al) ;
-		f = f && ((m >= n) || (anp[m] == '\0')) ;
-		if (f) break ;
-	    } /* end for */
-	    rs = (afs[i].name) ? afs[i].af : SR_AFNOSUPPORT ;
-	} /* end if */
-	return rs ;
+	getter go(names_af,codetab.tab_af,ne_af) ;
+	return go(sp,sl) ;
 } /* end subroutine (getdb) */
+
+#if	defined(CF_TEST) && (CF_TEST > 0)
+
+constexpr cpcchar	testafs[] = { 
+    "unspec", "unix", "local", "inet4", "inet6",
+    "decnet", "x25"
+} ; /* end array */
+
+int main() {
+    for (int i = 0 ; i < ne_af ; i += 1) {
+	cint idx = codetab.tab_af[i] ;
+	cint af = names_af[idx].af ;
+	cchar *name = names_af[idx].name ;
+	printf("%03d %03d %03d %s\n",i,idx,af,name) ;
+    } /* end for */
+    {
+	int rs = SR_OK ;
+	printf("test-get\n") ;
+	for (cauto &n : testafs) {
+	    printf("n=%s\n",n) ;
+	    if ((rs = getaf(n,-1)) >= 0) {
+		printf("req=%s af=%d\n",n,rs) ;
+	    }
+	    if (rs < 0) break ;
+	} /* end for */
+	printf("test-get rs=%d\n",rs) ;
+    } /* end block */
+} /* end subroutine (main) */
+
+#endif /* defined(CF_TEST) && (CF_TEST > 0) */
 
 
