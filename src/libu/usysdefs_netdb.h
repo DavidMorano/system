9ec02481fdc461c4da1b089f8fb4db874b229f57
David A.D. Morano <david.a.morano@gmail.com>
2026-04-23 00:24:12 -0400

    restore

---
+++ src/libu/usysdefs_netdb.h
@@ -0,0 +1,59 @@
+/* usysdefs HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* these are (a multitude) of various UNIX® system pre-processor defines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-09, David A-D- Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A-D- Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Description:
+	This file defines various preprocessor variables for use
+	in setting up the UNIX®-system default for various buffer
+	sizes.  The real run-time system buffer sizes are (indeed)
+	determined at runtime where they may exist.
+
+	Usage:
+	Among the most important uses for the data contained herein
+	is the BUFSIZE management facility.  It uses the data below
+	to set default buffer sizes for those systems that do not
+	have dynamicly managed buffer size support in the system
+	kernel.
+
+*******************************************************************************/
+
+#ifndef	USYSDEFSNETDB_INCLUDE
+#define	USYSDEFSNETDB_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/types.h>
+#include	<sys/utsname.h>
+#include	<sys/param.h>
+#include	<sys/stat.h>		/* |S_IS{x}| + S_IF{x}| */
+#include	<limits.h>		/* |{xxx}_MIN| + |{xxx}_MAX| */
+#include	<signal.h>		/* |SIG{x}| */
+#include	<unistd.h>		/* |_SC_{x}| + |_PC_{x}| */
+#include	<fcntl.h>		/* |O_{x}| */
+#include	<netdb.h>		/* |NI_MAX{x}| */
+
+
+/* network-related name resolving service (some system do not have) */
+#ifndef	NETDB_INTERAL
+#define	NETDB_INTERAL	-1
+#endif
+
+
+#endif /* USYSDEFSNETDB_INCLUDE */
+
+
