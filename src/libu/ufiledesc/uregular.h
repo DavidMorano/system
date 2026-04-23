29d48e9af6294203ba290f84a75b3f5217ce30d1
David A.D. Morano <david.a.morano@gmail.com>
2026-04-23 00:25:43 -0400

    update

--- src/libu/ufiledesc/uregular.h
+++ src/libu/ufiledesc/uregular.h
@@ -39,52 +39,52 @@
 
 EXTERNC_begin
 
-extern int u_closeonexec(int,int) noex ;
-extern int u_nonblock(int,int) noex ;
-extern int u_fchdir(int) noex ;
-extern int u_fchmod(int,mode_t) noex ;
-extern int u_fchmodmin(int,mode_t) noex ;
-extern int u_fchown(int,uid_t,gid_t) noex ;
-extern int u_ftimes(int,CTIMEVAL *) noex ;
-extern int u_close(int) noex ;
-extern int u_fstat(int,ustat *) noex ;
-extern int u_fstatfs(int,ustatfs *) noex ;
-extern int u_fstatvfs(int,ustatvfs *) noex ;
-extern int u_fpathconf(int,int,long *) noex ;
-extern int u_fstype(int,char *,int) noex ;
-extern int u_fsync(int) noex ;
-extern int u_fsyncdata(int) noex ;
-extern int u_fsize(int) noex ;
-extern int u_ftruncate(int,off_t) noex ;
-extern int u_ioctl(int,int,...) noex ;
-extern int u_lockf(int,int,off_t) noex ;
-extern int u_pread(int,void *,int,off_t) noex ;
-extern int u_pwrite(int,cvoid *,int,off_t) noex ;
-extern int u_read(int,void *,int) noex ;
-extern int u_readv(int,IOVEC *,int) noex ;
-extern int u_readn(int,void *,int) noex ;
-extern int u_seeko(int,off_t,int,off_t *) noex ;
-extern int u_write(int,cvoid *,int) noex ;
-extern int u_writev(int,CIOVEC *,int) noex ;
-extern int u_writen(int,cvoid *,int) noex ;
+extern int u_closeonexec	(int,int)		noex ;
+extern int u_nonblock		(int,int)		noex ;
+extern int u_fchdir		(int)			noex ;
+extern int u_fchmod		(int,mode_t)		noex ;
+extern int u_fchmodmin		(int,mode_t)		noex ;
+extern int u_fchown		(int,uid_t,gid_t)	noex ;
+extern int u_ftimes		(int,CTIMEVAL *)	noex ;
+extern int u_close		(int)			noex ;
+extern int u_fstat		(int,ustat *)		noex ;
+extern int u_fstatfs		(int,ustatfs *)		noex ;
+extern int u_fstatvfs		(int,ustatvfs *)	noex ;
+extern int u_fpathconf		(int,int,long *)	noex ;
+extern int u_fsize		(int)			noex ;
+extern int u_fstype		(int,char *,int)	noex ;
+extern int u_fsync		(int)			noex ;
+extern int u_fsyncdata		(int)			noex ;
+extern int u_ftruncate		(int,off_t)		noex ;
+extern int u_ioctl		(int,int,...)		noex ;
+extern int u_lockf		(int,int,off_t)		noex ;
+extern int u_read		(int,void *,int)	noex ;
+extern int u_readp		(int,void *,int,off_t)	noex ;
+extern int u_readn		(int,void *,int)	noex ;
+extern int u_readv		(int,IOVEC *,int)	noex ;
+extern int u_seeko		(int,off_t,int,off_t *)	noex ;
+extern int u_write		(int,cvoid *,int)	noex ;
+extern int u_writen		(int,cvoid *,int)	noex ;
+extern int u_writep		(int,cvoid *,int,off_t)	noex ;
+extern int u_writev		(int,CIOVEC *,int)	noex ;
 
-local inline int u_seek(int fd,off_t wo,int w) noex {
+local inline int u_seek		(int fd,off_t wo,int w) noex {
 	return u_seeko(fd,wo,w,nullptr) ;
 }
-local inline int u_seekoff(int fd,off_t wo,int w,off_t *offp) noex {
+local inline int u_seekoff	(int fd,off_t wo,int w,off_t *offp) noex {
 	return u_seeko(fd,wo,w,offp) ;
 }
-local inline int u_tell(int fd,off_t *rp) noex {
+local inline int u_tell		(int fd,off_t *rp) noex {
 	return u_seeko(fd,0z,SEEK_CUR,rp) ;
 }
-local inline int u_seekable(int fd) noex {
+local inline int u_seekable	(int fd) noex {
 	return u_seek(fd,0z,SEEK_CUR) ;
 }
-local inline int u_rewind(int fd) noex {
+local inline int u_rewind	(int fd) noex {
 	return u_seek(fd,0z,SEEK_SET) ;
 }
 
-extern int u_poll(POLLFD *,int,int) noex ;	/* <- special case */
+extern int u_poll		(POLLFD *,int,int) noex ; /* <- special case */
 
 EXTERNC_end
 
