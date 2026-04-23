5f2557a6e24c98bc55dbc82cf32c47c1c5718a52
David A.D. Morano <david.a.morano@gmail.com>
2026-04-23 00:29:09 -0400

    update

--- src/libuc/getx/getprotofamily.h
+++ src/libuc/getx/getprotofamily.h
@@ -11,17 +11,18 @@
 	= 1998-03-21, David A­D­ Morano
 	This module was originally written.
 
-	= 2017-08-01, David A­D­ Morano
-	Updated for lack of interfaces in Apple-Darwin
-
 */
 
-/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
-	This file contains some support for the UNIX® Address-Familt
-	(AF) related operations.
+  	Name:
+	getprotofamily
+
+	Description:
+	This subroutine will find a protocol family (if one exists)
+	that corresponds with a specified address family.
 
 *******************************************************************************/
 
