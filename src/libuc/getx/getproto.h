d55c8597b09899840e301f03a1c6f6b8aec980ea
David A.D. Morano <david.a.morano@gmail.com>
2026-04-23 00:29:00 -0400

    update

--- src/libuc/getx/getproto.h
+++ src/libuc/getx/getproto.h
@@ -11,12 +11,9 @@
 	= 1998-03-21, David A­D­ Morano
 	This module was originally written.
 
-	= 2017-08-01, David A­D­ Morano
-	Updated for lack of interfaces in Apple-Darwin
-
 */
 
-/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
@@ -24,8 +21,7 @@
 	getproto_name
 
 	Description:
-	This file contains some support for the UNIX® Address-Familt
-	(AF) related operations.
+	Retrieve a protocol number given a protocol name.
 
 *******************************************************************************/
 
