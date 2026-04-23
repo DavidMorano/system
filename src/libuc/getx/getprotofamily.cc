b31a1f7118242e7453de5e878b8cf563ec6f05b6
David A.D. Morano <david.a.morano@gmail.com>
2026-04-23 00:29:04 -0400

    update

--- src/libuc/getx/getprotofamily.cc
+++ src/libuc/getx/getprotofamily.cc
@@ -8,7 +8,7 @@
 
 /* revision history:
 
-	= 1998-04-17, David A­D­ Morano
+	= 1998-03-21, David A­D­ Morano
 	This code was originally written.
 
 */
@@ -38,7 +38,6 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>
-#include	<sys/param.h>
 #include	<sys/socket.h>
 #include	<netinet/in.h>
 #include	<arpa/inet.h>
@@ -68,7 +67,7 @@
 
 struct typematch {
 	cint		af, pf ;
-} ;
+} ; /* end struct (typematch) */
 
 
 /* forward references */
@@ -76,113 +75,48 @@ struct typematch {
 
 /* local variables */
 
-static const struct typematch	prototab[] = {
-	{ PF_UNSPEC, AF_UNSPEC },
-	{ PF_INET, AF_INET },
-#ifdef	PF_INET6
-	{ PF_INET6, AF_INET6 },
-#endif
-#ifdef	PF_LOCAL
-	{ PF_LOCAL, AF_LOCAL },
-#endif
-#ifdef	PF_UNIX
-	{ PF_UNIX, AF_UNIX },
-#endif
-#ifdef	PF_APPLETALK
-	{ PF_APPLETALK, AF_APPLETALK },
-#endif
-#if	defined(AF_IMPLINK)
-	{ PF_IMPLINK, AF_IMPLINK },
-#endif
-#if	defined(AF_PUP)
-	{ PF_PUP, AF_PUP },
-#endif
-#if	defined(AF_CHAOS)
-	{ PF_CHAOS, AF_CHAOS },
-#endif
-#if	defined(AF_NS)
-	{ PF_NS, AF_NS },
-#endif
-#if	defined(AF_NBS)
-	{ PF_NBS, AF_NBS },
-#endif
-#if	defined(AF_ECMA)
-	{ PF_ECMA, AF_ECMA },
-#endif
-#if	defined(AF_DATAKIT)
-	{ PF_DATAKIT, AF_DATAKIT },
-#endif
-#if	defined(AF_CCITT)
-	{ PF_CCITT, AF_CCITT },
-#endif
-#if	defined(AF_SNA)
-	{ PF_SNA, AF_SNA },
-#endif
-#if	defined(AF_DECnet)
-	{ PF_DECnet, AF_DECnet },
-#endif
-#if	defined(AF_DLI)
-	{ PF_DLI, AF_DLI },
-#endif
-#if	defined(AF_LAT)
-	{ PF_LAT, AF_LAT },
-#endif
-#if	defined(AF_HYLINK)
-	{ PF_HYLINK, AF_HYLINK },
-#endif
-#if	defined(AF_APPLETALK)
-	{ PF_APPLETALK, AF_APPLETALK },
-#endif
-#if	defined(AF_NIT)
-	{ PF_NIT, AF_NIT },
-#endif
-#if	defined(AF_802)
-	{ PF_802, AF_802 },
-#endif
-#if	defined(AF_OSI)
-	{ PF_OSI, AF_OSI },
-#endif
-#if	defined(AF_X25)
-	{ PF_X25, AF_X25 },
-#endif
-#if	defined(AF_OSINET)
-	{ PF_OSINET, AF_OSINET },
-#endif
-#if	defined(AF_GOSIP)
-	{ PF_GOSIP, AF_GOSIP },
-#endif
-#if	defined(AF_IPX)
-	{ PF_IPX, AF_IPX },
-#endif
-#if	defined(AF_ROUTE)
-	{ PF_ROUTE, AF_ROUTE },
-#endif
-#if	defined(AF_LINK)
-	{ PF_LINK, AF_LINK },
-#endif
-#if	defined(AF_KEY)
-	{ PF_KEY, AF_KEY },
-#endif
-#if	defined(AF_NCA)
-	{ PF_NCA, AF_NCA },
-#endif
-#if	defined(AF_LOCAL)
-	{ PF_LOCAL, AF_LOCAL },
-#endif
-#if	defined(AF_ISDN)
-	{ PF_ISDN, AF_ISDN },
-#endif
-#if	defined(AF_SYSTEM)
-	{ PF_SYSTEM, AF_SYSTEM },
-#endif
-#if	defined(AF_NETBIOS)
-	{ PF_NETBIOS, AF_NETBIOS },
-#endif
-#if	defined(AF_NDRV)
-	{ PF_NDRV, AF_NDRV },
-#endif
+constexpr typematch	prototab[] = {
+	{ PF_UNSPEC,	AF_UNSPEC },
+	{ PF_INET,	AF_INET },
+	{ PF_INET6,	AF_INET6 },
+	{ PF_LOCAL,	AF_LOCAL },
+	{ PF_UNIX,	AF_UNIX },
+	{ PF_APPLETALK,	AF_APPLETALK },
+	{ PF_IMPLINK,	AF_IMPLINK },
+	{ PF_PUP,	AF_PUP },
+	{ PF_CHAOS,	AF_CHAOS },
+	{ PF_NS,	AF_NS },
+	{ PF_NBS,	AF_NBS },
+	{ PF_ECMA,	AF_ECMA },
+	{ PF_DATAKIT,	AF_DATAKIT },
+	{ PF_CCITT,	AF_CCITT },
+	{ PF_SNA,	AF_SNA },
+	{ PF_DECnet,	AF_DECnet },
+	{ PF_DLI,	AF_DLI },
+	{ PF_LAT,	AF_LAT },
+	{ PF_HYLINK,	AF_HYLINK },
+	{ PF_APPLETALK,	AF_APPLETALK },
+	{ PF_NIT,	AF_NIT },
+	{ PF_802,	AF_802 },
+	{ PF_OSI,	AF_OSI },
+	{ PF_X25,	AF_X25 },
+	{ PF_OSINET,	AF_OSINET },
+	{ PF_GOSIP,	AF_GOSIP },
+	{ PF_IPX,	AF_IPX },
+	{ PF_ROUTE,	AF_ROUTE },
+	{ PF_LINK,	AF_LINK },
+	{ PF_KEY,	AF_KEY },
+	{ PF_NCA,	AF_NCA },
+	{ PF_LOCAL,	AF_LOCAL },
+	{ PF_ISDN,	AF_ISDN },
+	{ PF_SYSTEM,	AF_SYSTEM },
+	{ PF_NETBIOS,	AF_NETBIOS },
+	{ PF_NDRV,	AF_NDRV },
 	{ -1, -1 }
-} ;
+} ; /* end array */
+
+
+/* exported variables */
 
 
 /* exported subroutines */
