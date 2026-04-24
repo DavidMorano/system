d3f6519b586ed99ed155b906c467f7b209d0163f
David A.D. Morano <david.a.morano@gmail.com>
2026-04-24 00:18:19 -0400

    update

--- src/libuc/open/openport.cc
+++ src/libuc/open/openport.cc
@@ -104,13 +104,12 @@ extern "C" {
 
 /* external variables */
 
-extern cchar	**environ ;
-
 
 /* local structures */
 
 namespace {
     struct openporter {
+	mainv		envv ;
 	char		*a{} ;
 	char		*ubuf ;
 	char		*pbuf ;
@@ -137,14 +136,14 @@ namespace {
 
 /* forward references */
 
-static int procspawn(cchar *,cchar *,mainv,mainv,int,int,int,SA *) noex ;
-static int procspawn_begin(SP_CON *,cchar *,mainv,mainv) noex ;
-static int procspawn_end(pid_t,int *) noex ;
+local int procspawn(cchar *,cchar *,mainv,mainv,int,int,int,SA *) noex ;
+local int procspawn_begin(SP_CON *,cchar *,mainv,mainv) noex ;
+local int procspawn_end(pid_t,int *) noex ;
 
-static int procexchange(cchar *,int,int,int,int,SA *) noex ;
+local int procexchange(cchar *,int,int,int,int,SA *) noex ;
 
-static int	loadenvs(vecstr *,cchar *,cchar *,cchar *,int) noex ;
-static int	progfind(char *) noex ;
+local int	loadenvs(vecstr *,cchar *,cchar *,cchar *,int) noex ;
+local int	progfind(char *) noex ;
 
 
 /* local variables */
@@ -153,7 +152,7 @@ constexpr cpcchar	prs[] = {
 	"/usr/extra",
 	"/usr/preroot",
 	nullptr
-} ;
+} ; /* end arry (prs) */
 
 constexpr int		unlen = OPENPORTMSG_UNLEN ;
 
@@ -195,35 +194,35 @@ openporter::operator int () noex {
 	} /* end if */
 	if ((rs < 0) && (fd >= 0)) uc_close(fd) ;
 	return (rs >= 0) ? fd : rs ;
-}
-/* end method (openporter::operator) */
+} /* end method (openporter::operator) */
 
 int openporter::start() noex {
 	int		rs ;
-	if ((rs = getbufsize(bufsize_un)) >= 0) {
-	    ulen = rs ;
-	    if ((rs = getbufsize(bufsize_mp)) >= 0) {
-		cint	sz = (ulen+1) + (rs+1) ;
-		plen = rs ;
-		if ((rs = lm_mall(sz,&a)) >= 0) {
-		    ubuf = a ;
-		    pbuf = (ubuf + (ulen+1)) ;
-		    rs = getusername(ubuf,ulen,-1) ;
-		    if (rs < 0) {
-			lm_free(a) ;
-			ubuf = nullptr ;
-			pbuf = nullptr ;
-		    } /* end if (error) */
-		} /* end if (memory-allocation) */
+	if ((rs = u_getenviron(&envv)) >= 0) {
+	    if ((rs = getbufsize(bufsize_un)) >= 0) {
+	        ulen = rs ;
+	        if ((rs = getbufsize(bufsize_mp)) >= 0) {
+		    cint	sz = (ulen+1) + (rs+1) ;
+		    plen = rs ;
+		    if ((rs = lm_mall(sz,&a)) >= 0) {
+		        ubuf = a ;
+		        pbuf = (ubuf + (ulen+1)) ;
+		        rs = getusername(ubuf,ulen,-1) ;
+		        if (rs < 0) {
+			    lm_free(a) ;
+			    ubuf = nullptr ;
+			    pbuf = nullptr ;
+		        } /* end if (error) */
+		    } /* end if (memory-allocation) */
+	        } /* end if (getbufsize) */
+	        if (rs < 0) {
+		    plen = 0 ;
+		    ulen = 0 ;
+	        } /* end if (error) */
 	    } /* end if (getbufsize) */
-	    if (rs < 0) {
-		plen = 0 ;
-		ulen = 0 ;
-	    } /* end if (error) */
-	} /* end if (getbufsize) */
+	} /* end if (u_getenviron) */
 	return rs ;
-}
-/* end method (openporter::start) */
+} /* end method (openporter::start) */
 
 int openporter::finish() noex {
 	int		rs = SR_OK ;
@@ -236,8 +235,7 @@ int openporter::finish() noex {
 	    ubuf = nullptr ;
 	}
 	return rs ;
-}
-/* end method (openporter::finish) */
+} /* end method (openporter::finish) */
 
 int openporter::opener() noex {
 	int		rs ;
@@ -274,8 +272,8 @@ int openporter::progproc(cchar *bn,int bl) noex {
 	                sargv[ai] = nullptr ;
 		    }
 	            int i = 0 ; /* used-multiple for env-var array */
-	            if (int j ; (j = matkeystr(environ,vnhome,-1)) >= 0) {
-	                senvv[i++] = environ[j] ;
+	            if (int j ; (j = matkeystr(envv,vnhome,-1)) >= 0) {
+	                senvv[i++] = envv[j] ;
 	            }
 	            for (int j = 0 ; envs.get(j,&ep) >= 0 ; j += 1) {
 	                if (ep) {
@@ -302,10 +300,9 @@ int openporter::progproc(cchar *bn,int bl) noex {
 	    if ((rs < 0) && (fd >= 0)) uc_close(fd) ;
 	} /* end if (vecstr) */
 	return (rs >= 0) ? fd : rs ;
-}
-/* end method (openporter::progproc) */
+} /* end method (openporter::progproc) */
 
-static int procspawn(cc *un,cc *prog,mainv sargv,mainv senvv,
+local int procspawn(cc *un,cc *prog,mainv sargv,mainv senvv,
 		int pf,int pt,int proto,SA *sap) noex {
 	int		rs ;
 	int		rs1 ;
@@ -325,10 +322,9 @@ static int procspawn(cc *un,cc *prog,mainv sargv,mainv senvv,
 	    if ((rs < 0) && (fd >= 0)) uc_close(fd) ;
 	} /* end if (spawned program) */
 	return (rs >= 0) ? fd : rs ;
-}
-/* end subroutine (procspawn) */
+} /* end subroutine (procspawn) */
 
-static int procspawn_begin(SP_CON *psp,cc *prog,mainv sargv,mainv senvv) noex {
+local int procspawn_begin(SP_CON *psp,cc *prog,mainv sargv,mainv senvv) noex {
 	int		rs = SR_FAULT ;
 	if (psp && prog && sargv && senvv) {
 	    int i = 0 ;
@@ -341,15 +337,13 @@ static int procspawn_begin(SP_CON *psp,cc *prog,mainv sargv,mainv senvv) noex {
 	    rs = spawnproc(psp,prog,sargv,senvv) ;
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (procspawn_begin) */
+} /* end subroutine (procspawn_begin) */
 
-static int procspawn_end(pid_t pid,int *csp) noex {
+local int procspawn_end(pid_t pid,int *csp) noex {
 	return u_waitpid(pid,csp,0) ;
-}
-/* end subroutine (procspawn_end) */
+} /* end subroutine (procspawn_end) */
 
-static int procexchange(cc *un,int cfd,int pf,int pt,int proto,SA *sap) noex {
+local int procexchange(cc *un,int cfd,int pf,int pt,int proto,SA *sap) noex {
 	int		rs ;
 	int		rs1 ;
 	int		fd = -1 ;
@@ -393,10 +387,9 @@ static int procexchange(cc *un,int cfd,int pf,int pt,int proto,SA *sap) noex {
 	    if (rs >= 0) rs = rs1 ;
 	} /* end if (m-a-f) */
 	return (rs >= 0) ? fd : rs ;
-}
-/* end subroutine (procexchange) */
+} /* end subroutine (procexchange) */
 
-static int loadenvs(vecstr *elp,cchar *un,cchar *prog,cchar *bn,int bl) noex {
+local int loadenvs(vecstr *elp,cchar *un,cchar *prog,cchar *bn,int bl) noex {
 	constexpr cpcchar penvnames[] = { "_", "_EF" } ;
 	int		rs = SR_OK ;
 	int		c = 0 ;
@@ -415,10 +408,9 @@ static int loadenvs(vecstr *elp,cchar *un,cchar *prog,cchar *bn,int bl) noex {
 	    if (rs < INT_MAX) c += 1 ;
 	}
 	return (rs >= 0) ? c : rs ;
-}
-/* end subroutine (loadenvs) */
+} /* end subroutine (loadenvs) */
 
-static int progfind(char *progfname) noex {
+local int progfind(char *progfname) noex {
 	cnullptr	np{} ;
 	int		rs = SR_LIBACC ;
 	int		len = 0 ; /* return-value */
@@ -447,7 +439,6 @@ static int progfind(char *progfname) noex {
 	    rs = SR_LIBACC ;
 	}
 	return (rs >= 0) ? len : rs ;
-}
-/* end subroutine (progfind) */
+} /* end subroutine (progfind) */
 
 
