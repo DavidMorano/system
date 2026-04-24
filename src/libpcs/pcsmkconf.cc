639d6ec8d3146286c287f865e1899b42878f5bc3
David A.D. Morano <david.a.morano@gmail.com>
2026-04-24 00:14:43 -0400

    update

--- src/libpcs/pcsmkconf.cc
+++ src/libpcs/pcsmkconf.cc
@@ -1,4 +1,5 @@
 /* pcsmkconf SUPPORT */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* make the PCS CONF index file */
@@ -16,6 +17,10 @@
 
 /*******************************************************************************
 
+  	Name:
+	pcsmkconf
+
+	Description:
 	This subroutine makes the PCS CONF index file.
 
 *******************************************************************************/
@@ -28,7 +33,9 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<cstring>
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
 #include	<mktmp.h>
 #include	<ids.h>
 #include	<vecstr.h>
@@ -42,8 +49,8 @@ import libutil ;			/* |memclear(3u)| */
 
 /* local defines */
 
-#define	SUBINFO		struct subinfo
-#define	SUBINFO_FL	struct subinfo_flags
+#define	SI		subinfo
+#define	SI_FL	subinfo_flags
 
 #define	PRCONF		"conf"
 
@@ -54,60 +61,42 @@ import libutil ;			/* |memclear(3u)| */
 
 /* external subroutines */
 
-extern int	sncpy3(char *,int,cchar *,cchar *,cchar *) ;
-extern int	snwcpy(char *,int,cchar *,int) ;
-extern int	mkpath1(char *,cchar *) ;
-extern int	mkpath1w(char *,cchar *,int) ;
-extern int	mkpath2(char *,cchar *,cchar *) ;
-extern int	mkpath3(char *,cchar *,cchar *,cchar *) ;
-extern int	mkfnamesuf1(char *,cchar *,cchar *) ;
-extern int	nleadstr(cchar *,cchar *,int) ;
-extern int	sfbasename(cchar *,int,cchar **) ;
-extern int	pathclean(char *,cchar *,int) ;
-extern int	perm(cchar *,uid_t,gid_t,gid_t *,int) ;
-extern int	permf(int,uid_t,gid_t,gid_t *,int) ;
-extern int	permid(IDS *,USTAT *,int) ;
-extern int	permsched(cchar **,vecstr *,char *,int,cchar *,int) ;
-extern int	vecstr_adduniq(vecstr *,cchar *,int) ;
-extern int	vecstr_envadd(vecstr *,cchar *,cchar *,int) ;
-extern int	vecstr_envset(vecstr *,cchar *,cchar *,int) ;
-extern int	vecstr_loadfile(vecstr *,int,cchar *) ;
-
-extern int	prmktmpdir(cchar *,char *,cchar *,mode_t) ;
-
-extern char	*strwcpy(char *,cchar *,int) ;
-
 
 /* external variables */
-
-extern char	**environ ;
 
 
 /* local structures */
 
+namespace {
+    struct vars {
+	mainv	envv ;
+	operator int () noex ;
+    } ; /* end struct (vars) */
+} /* end namespace */
+
 struct subinfo_flags {
 	uint		localconf:1 ;
-} ;
+} ; /* end struct */
 
 struct subinfo {
-	SUBINFO_FL	f ;
+	SI_FL		fl ;
 	paramfile	pf ;
 	varmk		v ;
-	mainb		envv ;
+	mainv		envv ;
 	cchar		*prconf ;
 	cchar		*pr ;
 	cchar		*cfname ;
-} ;
+} ; /* end struct */
 
 
 /* forward references */
 
-static int	subinfo_start(SUBINFO *,cchar *,cchar **,cchar *) noex ;
-static int	subinfo_finish(SUBINFO *) noex ;
-static int	subinfo_startsubs(SUBINFO *,vecstr *) noex ;
-static int	subinfo_confglobal(SUBINFO *,char *) noex ;
-static int	subinfo_conflocal(SUBINFO *,char *) noex ;
-static int	subinfo_proc(SUBINFO *) noex ;
+local int	subinfo_start(SI *,cchar *,cchar **,cchar *) noex ;
+local int	subinfo_finish(SI *) noex ;
+local int	subinfo_startsubs(SI *,vecstr *) noex ;
+local int	subinfo_confglobal(SI *,char *) noex ;
+local int	subinfo_conflocal(SI *,char *) noex ;
+local int	subinfo_proc(SI *) noex ;
 
 
 /* external variables */
@@ -115,12 +104,14 @@ static int	subinfo_proc(SUBINFO *) noex ;
 
 /* local variables */
 
-static cchar	*schedconf[] = {
+constexpr cpcchar	schedconf[] = {
 	"%p/etc/%n.%f",
 	"%p/etc/%f",
 	"%p/%n.%f",
 	nullptr
-} ;
+} ; /* end array (scedconf) */
+
+static vars		var ;
 
 
 /* exported variables */
@@ -129,27 +120,27 @@ static cchar	*schedconf[] = {
 /* exported subroutines */
 
 int pcsmkconf(cchar *pr,mainv envv,cc *cfname) noex {
-	SUBINFO		si ;
-	int		rs ;
+	int		rs = SR_FAULT ;
 	int		rs1 ;
-
-	if (pr == nullptr) return SR_FAULT ;
-
-	if (pr[0] == '\0')
-	    return SR_INVALID ;
-
-	if ((cfname != nullptr) && (cfname[0] == '\0')) return SR_INVALID ;
-
-	if ((rs = subinfo_start(&si,pr,envv,cfname)) >= 0) {
-
-	    rs = subinfo_proc(&si) ;
-
-	    rs1 = subinfo_finish(&si) ;
-	    if (rs >= 0) rs = rs1 ;
-	} /* end if (subinfo) */
-
-	if (rs == SR_EXIST) rs = SR_OK ;
-
+	int		rv = 0 ; /* return-value */
+	if (pr && cfname) {{
+	    rs = SR_INVALID ;
+	    if (pr[0] && cfname[0]) {
+		static cint rsv = var ;
+		if ((rs = rsv) >= 0) {
+	            SI	si ;
+	            if ((rs = subinfo_start(&si,pr,envv,cfname)) >= 0) {
+	                {
+	                    rs = subinfo_proc(&si) ;
+		            rv = rs ;
+	                }
+	                rs1 = subinfo_finish(&si) ;
+	                if (rs >= 0) rs = rs1 ;
+	            } /* end if (subinfo) */
+	            if (rs == SR_EXIST) rs = SR_OK ;
+		} /* end if (vars) */
+	    /* end if (valid) */
+	} /* end if (non-null) */
 	return rs ;
 }
 /* end subroutine (pcsmkconf) */
@@ -157,18 +148,15 @@ int pcsmkconf(cchar *pr,mainv envv,cc *cfname) noex {
 
 /* local subroutines */
 
-static int subinfo_start(SUBINFO *sip,cc *pr,mainv envv,cc *cfname) noex {
-	vecstr		subs ;
+local int subinfo_start(SI *sip,cc *pr,mainv envv,cc *cfname) noex {
 	cint		of = O_CREAT ;
 	cint		n = 20 ;
 	int		rs = SR_OK ;
 	int		rs1 ;
 	cmode		vm = 0444 ;
-	vbool		f_global = (cfname == nullptr) ;
+	cbool		f_global = (cfname == nullptr) ;
 	char		dbname[MAXPATHLEN+1] ;
-	if (envv == nullptr) {
-	    envv = (cchar **) environ ;
-	}
+	if (envv == nullptr) envv = var.envv ;
 	memclear(sip) ;
 	sip->envv = envv ;
 	sip->prconf = PRCONF ;
@@ -177,6 +165,7 @@ static int subinfo_start(SUBINFO *sip,cc *pr,mainv envv,cc *cfname) noex {
 
 /* open the PCS-configuration file (if it exists) */
 
+	vecstr		subs ;
 	if ((rs = vecstr_start(&subs,4,0)) >= 0) {
 	    char	tmpfname[MAXPATHLEN+1] ;
 	    rs = subinfo_startsubs(sip,&subs) ;
@@ -193,7 +182,7 @@ static int subinfo_start(SUBINFO *sip,cc *pr,mainv envv,cc *cfname) noex {
 	        } else {
 	            rs = subinfo_conflocal(sip,dbname) ;
 	        }
-/* see if we can create a new VAR DB */
+		/* see if we can create a new VAR DB */
 	        if (rs >= 0) {
 		    rs = varmk_open(&sip->v,dbname,of,vm,n) ;
 	        } /* end if (ok) */
@@ -205,7 +194,7 @@ static int subinfo_start(SUBINFO *sip,cc *pr,mainv envv,cc *cfname) noex {
 }
 /* end subroutine (subinfo_start) */
 
-static int subinfo_finish(SUBINFO *sip) noex )
+local int subinfo_finish(SI *sip) noex )
 	int		rs = SR_OK ;
 	int		rs1 ;
 	{
@@ -216,7 +205,7 @@ static int subinfo_finish(SUBINFO *sip) noex )
 }
 /* end subroutine (subinfo_finish) */
 
-static int subinfo_startsubs(SUBINFO *sip,vecstr *slp) noex {
+local int subinfo_startsubs(SI *sip,vecstr *slp) noex {
 	int		rs = SR_OK ;
 	int		cl ;
 	cchar		*cp ;
@@ -234,21 +223,19 @@ static int subinfo_startsubs(SUBINFO *sip,vecstr *slp) noex {
 }
 /* end subroutine (subinfo_startsubs) */
 
-static int subinfo_confglobal(SUBINFO *sip,char *dbname) noex {
+local int subinfo_confglobal(SI *sip,char *dbname) noex {
 	int		rs ;
 	cmode		dm = 0777 ;
 	cchar		*cdname = "pcsconf" ;
 	char		tmpdname[MAXPATHLEN+1] ;
-
 	if ((rs = prmktmpdir(sip->pr,tmpdname,cdname,dm)) >= 0) {
 	    rs = mkpath2(dbname,tmpdname,sip->prconf) ;
 	}
-
 	return rs ;
 }
 /* end subroutine (subinfo_confglobal) */
 
-static int subinfo_conflocal(SUBINFO *sip,char *dbname) noex {
+local int subinfo_conflocal(SI *sip,char *dbname) noex {
 	int		rs ;
 	cmode		dm = 0775 ;
 	cchar		*cdname = "pcsconf" ;
@@ -262,15 +249,13 @@ static int subinfo_conflocal(SUBINFO *sip,char *dbname) noex {
 }
 /* end subroutine (subinfo_conflocal) */
 
-
-static int subinfo_proc(SUBINFO *sip)
-{
+local int subinfo_proc(SI *sip) noex {
 	paramfile	*pfp = &sip->pf ;
-	paramfile_cur	cur ;
-	paramfile_ent	pe ;
 	int		rs ;
 	int		rs1 ;
 	if ((rs = paramfile_open(pfp,sip->envv,sip->cfname)) >= 0) {
+	    paramfile_cur	cur ;
+	    paramfile_ent	pe ;
 	    if ((rs = paramfile_curbegin(pfp,&cur)) >= 0) {
 	        cint	plen = PARAMBUFLEN ;
 	        int	kl ;
@@ -283,7 +268,8 @@ static int subinfo_proc(SUBINFO *sip)
 	                rs = varmk_addvar(&sip->v,pe.key,pe.value,pe.vlen) ;
 		    }
 	        } /* end while (reading parameters) */
-	        paramfile_curend(pfp,&cur) ;
+	        rs1 = paramfile_curend(pfp,&cur) ;
+	        if (rs >= 0) rs = rs1 ;
 	    } /* end if (cursor) */
 	    rs1 = paramfile_close(pfp) ;
 	    if (rs >= 0) rs = rs1 ;
@@ -291,5 +277,9 @@ static int subinfo_proc(SUBINFO *sip)
 	return rs ;
 }
 /* end subroutine (subinfo_proc) */
+
+vars::operator int () noex {
+	return u_getenviron(&envv) ;
+} /* end method (vars::operator) */
 
 
