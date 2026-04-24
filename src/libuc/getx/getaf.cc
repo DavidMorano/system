2426fb92b16b4c0f373554bc3e1c9e773945fcb5
David A.D. Morano <david.a.morano@gmail.com>
2026-04-24 00:17:19 -0400

    update

--- src/libuc/getx/getaf.cc
+++ src/libuc/getx/getaf.cc
@@ -148,7 +148,7 @@ namespace {
                 } /* end if (got a match) */
             } /* end if */
             return (rs >= 0) ? af : rs ;
-	} /* end method (operator) */
+	} ; /* end method (operator) */
     } ; /* end struct (getter) */
 } /* end namespace */
 
