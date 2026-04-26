974cf6a9b968eaabd4047cf7024301b2527273c4
David A.D. Morano <david.a.morano@gmail.com>
2026-04-26 01:14:33 -0400

    update

--- src/libuc/getx/geteaddrinfo.cc
+++ src/libuc/getx/geteaddrinfo.cc
@@ -2,7 +2,7 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
-/* subroutine to get a canonical hostname */
+/* get effective host inforamation given a name */
 /* version %I% last-modified %G% */
 
 
