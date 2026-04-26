f7b1eb6b347b61546135d449f397465f75220e1d
David A.D. Morano <david.a.morano@gmail.com>
2026-04-26 01:17:28 -0400

    update

--- src/libuc/getx/getsystypenum.cc
+++ src/libuc/getx/getsystypenum.cc
@@ -2,7 +2,7 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
-/* get the 'osts' and 'osnum' for the current system and release */
+/* get the system 'type-number' and 'osnum' for a given system and release */
 /* version %I% last-modified %G% */
 
 
@@ -21,22 +21,27 @@
 	getsystypenum
 
 	Description:
-	We find (or make) a system type-name from the combination
-	of the system 'sysname" and 'release'.  We also somehow
-	divine a system OS number.
+	Given a system-name c-string and a release c-string, this
+	subroutine returns (if found) the corresponding system-type-name
+	and OS-number c-string.
 
 	Synopsis:
-	int getsystypenum(char *rbuf,char *nbuf,cc *sysname,cc *release) noex
+	int getsystypenum(char *rbuf,char *nbuf,cc *stn,cc *release) noex
 
 	Arguments:
 	tbuf		result buffer for 'osts' result
 	nbuf		result buffer for 'osnum' result
-	sysname		specified SYSNAME c-string
+	stn		specified SYSNAME c-string
 	release		specified RELEASE c-string
 
 	Returns:
-	>=0		OK
+	>=0		OK length of returned type-name string
 	<0		error (system-return)
+
+	Notes:
+	1. The two output buffer ('rbuf' and 'tbuf') must be at
+	least the length (plus-1) of the username-len on the current
+	system.
 
 *******************************************************************************/
 
@@ -72,24 +77,17 @@ import libutil ;			/* |lenstr(3u)| */
 
 /* forward references */
 
-static int	getfield(cchar *,int,cchar **) noex ;
+local int	getfield(cchar *,int,cchar **) noex ;
 
 
 /* local variables */
 
-enum sysnames {
-	sysname_sunos,
-	sysname_darwin,
-	sysname_linux,
-	sysname_overlast
-} ;
-
-constexpr cpcchar	sysnames[] = {
-	"SunOS",
+constexpr cpcchar	names[] = {
+	"SysV",
 	"Darwin",
 	"Linux",
 	nullptr
-} ;
+} ; /* end array (names) */
 
 
 /* exported variables */
@@ -97,34 +95,37 @@ constexpr cpcchar	sysnames[] = {
 
 /* exported subroutines */
 
-int getsystypenum(char *tbuf,char *nbuf,cchar *sysname,cchar *release) noex {
+int getsystypenum(char *tbuf,char *nbuf,cchar *stn,cchar *release) noex {
 	int		rs = SR_FAULT ;
-	if (tbuf && nbuf && sysname && release) {
+	if (tbuf && nbuf && stn && release) {
 	    rs = SR_INVALID ;
 	    tbuf[0] = '\0' ;
 	    nbuf[0] = '\0' ;
-	    if (sysname[0] && release[0]) {
+	    if (stn[0] && release[0]) {
 		if ((rs = getbufsize(bufsize_un)) >= 0) {
 		    cint	olen = rs ;
-	            if (int si ; (si = matstr(sysnames,sysname,-1)) >= 0) {
+	            if (int si ; (si = matstr(names,stn,-1)) >= 0) {
 	                int	cl ;
 	                cchar	*osts = nullptr ;
 	                cchar	*cp ;
 	                rs = SR_NOTFOUND ;
 	                switch (si) {
-	                case sysname_sunos:
-	                case sysname_linux:
+	                case systypename_sysv:
+	                case systypename_linux:
 		            osts = "SYSV" ;
 		            if ((cl = getfield(release,1,&cp)) > 0) {
 		                rs = snwcpy(nbuf,olen,cp,cl) ;
 		            }
 		            break ;
-	                case sysname_darwin:
+	                case systypename_darwin:
 		            osts = "BSD" ;
 		            if ((cl = getfield(release,0,&cp)) > 0) {
 		                rs = snwcpy(nbuf,olen,cp,cl) ;
 		            }
 		            break ;
+			default:
+			    rs = SR_BUGCHECK ;
+			    break ;
 	                } /* end switch */
 	                if ((rs >= 0) && osts) {
 		            rs = snwcpy(tbuf,olen,osts) ;
@@ -142,12 +143,11 @@ int getsystypenum(char *tbuf,char *nbuf,cchar *sysname,cchar *release) noex {
 
 /* local subroutines */
 
-static int getfield(cchar *sp,int n,cchar **rpp) noex {
+local int getfield(cchar *sp,int n,cchar **rpp) noex {
 	int		i = 0 ; /* used-afterwards */
 	int		cl = -1 ; /* return-value */
-	cchar		*tp ;
-	cchar		*cp = nullptr ;
-	while ((tp = strchr(sp,'.')) != nullptr) {
+	cchar		*cp = nullptr ; /* used-multiple */
+	for (cchar *tp ; (tp = strchr(sp,'.')) != nullptr ; ) {
 	    if (i == n) {
 		cp = sp ;
 		cl = intconv(tp - sp) ;
@@ -155,7 +155,7 @@ static int getfield(cchar *sp,int n,cchar **rpp) noex {
 	    }
 	    sp = (tp + 1) ;
 	    i += 1 ;
-	} /* end if */
+	} /* end for */
 	if ((cp == nullptr) && sp[0] && (i == n)) {
 	    cp = sp ;
 	    cl = lenstr(sp) ;
@@ -164,7 +164,6 @@ static int getfield(cchar *sp,int n,cchar **rpp) noex {
 	    *rpp = cp ;
 	}
 	return cl ;
-}
-/* end subroutine (getfield) */
+} /* end subroutine (getfield) */
 
 
