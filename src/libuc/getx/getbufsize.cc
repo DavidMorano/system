392bf78566be26160761122c10d6590b7802823a
David A.D. Morano <david.a.morano@gmail.com>
2026-04-26 01:13:40 -0400

    update

--- src/libuc/getx/getbufsize.cc
+++ src/libuc/getx/getbufsize.cc
@@ -183,7 +183,7 @@ int ubufsize::init() noex {
 	    cauto lamb = [this] () -> int {
 	        int	rsl = SR_OK ;
 	        if (!finit) {
-		    rsl = SR_LOCKLOST ;		/* <- failure */
+		    rsl = SR_LOCKFAIL ;		/* <- failure */
 	        } else if (finitdone) {
 		    rsl = 1 ;			/* <- OK ready */
 	        }
