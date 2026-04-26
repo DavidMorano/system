de0d67d8236d554776998b093dacc26b4aab4ce3
David A.D. Morano <david.a.morano@gmail.com>
2026-04-26 01:17:50 -0400

    update

--- src/libuc/getxname/getgroupname.h
+++ src/libuc/getxname/getgroupname.h
@@ -2,7 +2,7 @@
 /* charset=ISO8859-1 */
 /* lang=C20 */
 
-/* get a group name (from the system account database) for current process */
+/* get a group name from the system */
 /* version %I% last-modified %G% */
 
 
