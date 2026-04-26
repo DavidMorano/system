1e37e5e483e40396da56342b05984ee7cac5b254
David A.D. Morano <david.a.morano@gmail.com>
2026-04-26 01:20:02 -0400

    update

--- src/libuc/ucmem/ucmem_prime.cc
+++ src/libuc/ucmem/ucmem_prime.cc
@@ -503,7 +503,7 @@ int submgr::iinit() noex {
 	        auto lamb = [this] () -> int {
 	            int		rsl = SR_OK ;
 	            if (!finit) {
-		        rsl = SR_LOCKLOST ;		/* <- failure */
+		        rsl = SR_LOCKFAIL ;		/* <- failure */
 	            } else if (finitdone) {
 		        rsl = 1 ;			/* <- OK ready */
 	            }
