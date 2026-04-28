12247a71c7c20c29ec9d8e96bd4e4f3c424bb407
David A.D. Morano <david.a.morano@gmail.com>
2026-04-28 01:18:19 -0400

    restore

---
+++ src/libuc/ucgetx/ucgetnetname.h
@@ -0,0 +1,36 @@
+/* ucgetnetname HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* subroutines to retrieve various system information items */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-23, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCGETNETNAME_INCLUDE
+#define	UCGETNETNAME_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	uc_getnetname(char *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCGETNETNAME_INCLUDE */
+
+
