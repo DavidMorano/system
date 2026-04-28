625b28a1c660a774cf00e09350449ad7995f3901
David A.D. Morano <david.a.morano@gmail.com>
2026-04-28 01:13:13 -0400

    restore

---
+++ src/libu/usys/usys_onc.hh
@@ -0,0 +1,47 @@
+/* usys_onc HEADER (Open-Network-Compuring) */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* miscelllaneous (XXX) operating system support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	This file contains the UNIX® system types that the brain-damaged
+	MacOS operating system does NOT have.  We are trying in a very
+	small way to make up for some of the immense brain-damage within
+	the Apple Darwin operating system.
+
+*******************************************************************************/
+
+#ifndef	USYSONC_INCLUDE
+#define	USYSONC_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+namespace usys {
+    extern sysret_t getnetnamelen() noex ;
+}
+
+
+#endif /* __cplusplus */
+#endif /* USYSONC_INCLUDE */
+
+
