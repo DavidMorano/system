f0380a214bf3be83878782991edf27c35632b6a9
David A.D. Morano <david.a.morano@gmail.com>
2026-04-29 03:17:45 -0400

    restore

---
+++ src/libuc/getx/getpf.h
@@ -0,0 +1,47 @@
+/* getpf HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get a protocol family from an address family */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	getprotofamily
+
+	Description:
+	This subroutine will find a protocol family (if one exists)
+	that corresponds with a specified address family.
+
+*******************************************************************************/
+
+#ifndef	GETPF_INCLUDE
+#define	GETPF_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	getpf(int) noex ;
+
+EXTERNC_end
+
+
+#endif /* GETPF_INCLUDE */
+
+
