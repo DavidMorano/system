4bf2fff1a9886d011a37ed1b6ae233c8d769d830
David A.D. Morano <david.a.morano@gmail.com>
2026-04-29 03:32:08 -0400

    update

--- src/libuc/ucaddrinfo.cc
+++ src/libuc/ucaddrinfo.cc
@@ -69,12 +69,21 @@
 #include	<clanguage.h>
 #include	<usysbase.h>
 #include	<usyscalls.h>
+#include        <errtimer.hh>
 #include	<localmisc.h>
 
 #include	"ucaddrinfo.h"
 
 
 /* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+using ai = ADDRINFO ;
 
 
 /* external subroutines */
@@ -88,88 +97,81 @@
 
 /* forward references */
 
+local int std_geaddinfo(cc *,cc *,con ai *,ai **) noex ;
+
 
 /* local variables */
 
 
 /* exported subroutines */
 
-int uc_addrinfoget(cchar *hn,cchar *svc,ADDRINFO *hintp,ADDRINFO **rpp) noex {
-	int		to_again = utimeout[uto_again] ;
-	int		rs ;
-	int		rc = 0 ;
-	int		f_exit = false ;
-	repeat {
-	    rs = SR_OK ;
-	    errno = 0 ;
-	    if ((rc = getaddrinfo(hn,svc,hintp,rpp)) != 0) {
-	        switch (rc) {
-	        case EAI_ADDRFAMILY:
-	            rs = SR_AFNOSUPPORT ;
-		    f_exit = true ;
-	            break ;
-	        case EAI_AGAIN:
-	            if (to_again-- > 0) {
-		        msleep(1000) ;
-		    } else {
-	                rs = SR_AGAIN ;
-			f_exit = true ;
-		    }
-	            break ;
-	        case EAI_BADFLAGS:
-	            rs = SR_INVALID ;
-		    f_exit = true ;
-	            break ;
-	        case EAI_FAIL:
-	            rs = SR_FAIL;
-		    f_exit = true ;
-	            break ;
-	        case EAI_FAMILY:
-	            rs = SR_PFNOSUPPORT ;
-		    f_exit = true ;
-	            break ;
-	        case EAI_MEMORY:
-	            rs = SR_NOMEM ;
-		    f_exit = true ;
-	            break ;
-	        case EAI_NODATA:
-	            rs = SR_NODATA ;
-		    f_exit = true ;
-	            break ;
-	        case EAI_NONAME:
-	            rs = SR_NOTFOUND ;
-		    f_exit = true ;
-	            break ;
-	        case EAI_SERVICE:
-	            rs = SR_ADDRNOTAVAIL ;
-		    f_exit = true ;
-	            break ;
-	        case EAI_SOCKTYPE:
-	            rs = SR_SOCKTNOSUPPORT ;
-		    f_exit = true ;
-	            break ;
-	        case EAI_SYSTEM:
-	            rs = (- errno) ;
-	            switch (rs) {
-	            case SR_AGAIN:
-	                if (to_again-- > 0) {
-			    msleep(1000) ;
-			} else {
-			    f_exit = true ;
-			}
-	                break ;
-		    default:
-		        f_exit = true ;
-			break ;
-	            } /* end switch */
-	            break ;
-	        default:
-	            rs = SR_NOANODE ;
-		    f_exit = true ;
-		    break ;
-	        } /* end switch */
-	    } /* end if (some sort of error condition) */
-	} until ((rs >= 0) || f_exit) ;
+int uc_addrinfoget(cc *hn,cc *svc,con ai *hintp,ai **rpp) noex {
+    	int		rs = SR_FAULT ;
+	if (hn && svc) {
+	    rs = SR_INVALID ;
+	    if (hn[0] && svc[0]) {
+                errtimer    to_again        = utimeout[uto_again] ;
+                errtimer    to_busy         = utimeout[uto_busy] ;
+                errtimer    to_nomem        = utimeout[uto_nomem] ;
+                errtimer    to_nosr         = utimeout[uto_nosr] ;
+                errtimer    to_nobufs       = utimeout[uto_nobufs] ;
+                errtimer    to_mfile        = utimeout[uto_mfile] ;
+                errtimer    to_nfile        = utimeout[uto_nfile] ;
+                errtimer    to_nolck        = utimeout[uto_nolck] ;
+                errtimer    to_nospc        = utimeout[uto_nospc] ;
+                errtimer    to_dquot        = utimeout[uto_dquot] ;
+                errtimer    to_io           = utimeout[uto_io] ;
+		errtimer    to_inprogress   = utimeout[uto_inprogress] ;
+                reterr      r ;
+	        repeat {
+	            if ((rs = std_geaddinfo(hn,svc,hintp,rpp)) < 0) {
+                        r(rs) ;		/* <- default causes exit */
+                        switch (rs) {
+                        case SR_AGAIN:
+                            r = to_again(rs) ;
+                            break ;
+                        case SR_BUSY:
+                            r = to_busy(rs) ;
+                            break ;
+                        case SR_NOMEM:
+                            r = to_nomem(rs) ;
+                            break ;
+                        case SR_NOSR:
+                            r = to_nosr(rs) ;
+                            break ;
+                        case SR_NOBUFS:
+                            r = to_nobufs(rs) ;
+                            break ;
+                        case SR_MFILE:
+                            r = to_mfile(rs) ;
+                            break ;
+                        case SR_NFILE:
+                            r = to_nfile(rs) ;
+                            break ;
+                        case SR_NOLCK:
+                            r = to_nolck(rs) ;
+                            break ;
+                        case SR_NOSPC:
+                            r = to_nospc(rs) ;
+                            break ;
+                        case SR_DQUOT:
+                            r = to_dquot(rs) ;
+                            break ;
+                        case SR_IO:
+                            r = to_io(rs) ;
+                            break ;
+                        case SR_INPROGRESS: /* who thought up this? */
+                            r = to_inprogress(rs) ;
+                            break ;
+                        case SR_INTR:
+                            r(false) ;
+                            break ;
+                        } /* end switch */
+                        rs = r ;
+                    } /* end if (callstd) */
+                } until ((rs >= 0) || r.fexit) ;
+	    } /* end if (valid) */
+	} /* end if (non-null) */
 	return rs ;
 }
 /* end subroutine (uc_addrinfoget) */
@@ -183,5 +185,54 @@ int uc_addrinfofree(ADDRINFO *aip) noex {
 	return rs ;
 }
 /* end subroutine (uc_addrinfofree) */
+
+
+/* local subroutines */
+
+local int std_geaddinfo(cc *hn,cc *svc,con ai *hintp,ai **rpp) noex {
+    	int		rs = SR_OK ;
+	errno = 0 ;
+	if (int rc ; (rc = getaddrinfo(hn,svc,hintp,rpp)) != 0) {
+            switch (rc) {
+            case EAI_ADDRFAMILY:
+                rs = SR_AFNOSUPPORT ;
+                break ;
+            case EAI_AGAIN:
+                rs = SR_AGAIN ;
+                break ;
+            case EAI_BADFLAGS:
+                rs = SR_INVALID ;
+                break ;
+            case EAI_FAIL:
+                rs = SR_FAIL;
+                break ;
+            case EAI_FAMILY:
+                rs = SR_PFNOSUPPORT ;
+                break ;
+            case EAI_MEMORY:
+                rs = SR_NOMEM ;
+                break ;
+            case EAI_NODATA:
+                rs = SR_NODATA ;
+                break ;
+            case EAI_NONAME:
+                rs = SR_NOTFOUND ;
+                break ;
+            case EAI_SERVICE:
+                rs = SR_ADDRNOTAVAIL ;
+                break ;
+            case EAI_SOCKTYPE:
+                rs = SR_SOCKTNOSUPPORT ;
+                break ;
+            case EAI_SYSTEM:
+                rs = (- errno) ;
+                break ;
+            default:
+                rs = SR_NOANODE ;
+                break ;
+            } /* end switch */
+        } /* end if (some sort of error condition) */
+	return rs ;
+} /* end subroutine (std_getaddrinfo) */
 
 
