b7ef970dd9c3cac8802409d3cdf2c7451f464e68
David A.D. Morano <david.a.morano@gmail.com>
2026-05-02 00:20:06 -0400

    restore

---
+++ src/libuc/ucdata/ucinproj.h
@@ -0,0 +1,36 @@
+/* ucinproj HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* LIBUC identification management operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCINPROJ_INCLUDE
+#define	UCINPROJ_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	uc_inproj(cchar *,cchar *,char *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCINPROJ_INCLUDE */
+
+
