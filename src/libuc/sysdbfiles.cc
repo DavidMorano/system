d36354c3e7d47eb11cb9d935e8830dd459a17123
David A.D. Morano <david.a.morano@gmail.com>
2026-05-03 00:18:33 -0400

    update

--- src/libuc/sysdbfiles.cc
+++ src/libuc/sysdbfiles.cc
@@ -25,6 +25,8 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>
+#include	<cstdlib>
 #include	<clanguage.h>
 #include	<usysbase.h>
 #include	<localmisc.h>
