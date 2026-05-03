4c2bb112a826ad4c30d446ebb6f445f1b1a97a89
David A.D. Morano <david.a.morano@gmail.com>
2026-05-03 00:17:27 -0400

    update

--- src/libuc/matxstr/matlocalfs.h
+++ src/libuc/matxstr/matlocalfs.h
@@ -26,6 +26,37 @@
 #include	<usysdefs.h>
 
 
+enum localfses {
+	localfs_apfs,
+	localfs_ext1,
+	localfs_ext2,
+	localfs_ext3,
+	localfs_ext4,
+	localfs_xfs,
+	localfs_zfs,
+	localfs_btrfs,
+	localfs_tmpfs,
+	localfs_autofs,
+	localfs_nfs,
+	localfs_devfs,
+	localfs_lofs,
+	localfs_fat,
+	localfs_exfat,			/* ExFAT */
+	localfs_vfat,			/* FAT32 */
+	localfs_msdos,
+	localfs_ntfs,
+	localfs_cd9660,			/* ISO 9660 */
+	localfs_udf,			/* Universal Disk Format */
+	localfs_ufs,
+	localfs_vxfs,
+	localfs_pcfs,
+	localfs_smbfs,
+	localfs_hfs,
+	localfs_sysv,
+	localfs_overlast
+} ; /* end enum (localfses) */
+
+
 EXTERNC_begin
 
 extern int	matlocalfs(cchar *sp,int sl) noex ;
