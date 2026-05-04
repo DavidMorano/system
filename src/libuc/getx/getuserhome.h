4d85b812920f9b7f1b4decb469b8c4d9ac81aa0f
David A.D. Morano <david.a.morano@gmail.com>
2026-05-04 01:23:28 -0400

    update

--- src/libuc/getx/getuserhome.h
+++ src/libuc/getx/getuserhome.h
@@ -2,9 +2,16 @@
 /* charset=ISO8859-1 */
 /* lang=C20 */
 
-/* get the best approximation of the username of the current user */
+/* get the best home directory for a given username */
 /* version %I% last-modified %G% */
 
+
+/* revision history:
+
+	= 1998-06-01, David A­D­ Morano
+	This code was originally written.
+
+*/
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
