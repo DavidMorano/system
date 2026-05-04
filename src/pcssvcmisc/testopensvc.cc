50e9dcdd36c984039d01f66c4963ebc06861c380
David A.D. Morano <david.a.morano@gmail.com>
2026-05-04 01:14:49 -0400

    update

--- src/pcssvcmisc/testopensvc.cc
+++ src/pcssvcmisc/testopensvc.cc
@@ -36,8 +36,6 @@
 
 #define	VARDEBUGFNAME	"TESTOPENSVC_DEBUGFILE"
 
-extern int	bufprintf(char *,int,const char *,...) ;
-extern int	fbwrite(FILE *,const void *,int) ;
 
 #if	CF_DEBUGS
 extern int	debugopen(const char *) ;
@@ -47,9 +45,6 @@ extern int	debugprinthexblock(const char *,int,const void *,int) ;
 extern int	strlinelen(const char *,int,int) ;
 #endif
 
-extern cchar 	*getourenv(cchar **,cchar *) ;
-
-extern char	*timestr_logz(time_t,char *) ;
 
 /* forward references */
 
