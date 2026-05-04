4ce2399d9923c2c176aa22ad8c599a1530e006d0
David A.D. Morano <david.a.morano@gmail.com>
2026-05-04 01:26:35 -0400

    update

--- src/libu/usys/usysargz.hh
+++ src/libu/usys/usysargz.hh
@@ -23,13 +23,14 @@
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<atomic>		/* |once_flag(3c++)| */
-#include	<mutex>
+#include	<atomic>
+#include	<mutex>			/* |once_flag(3c++)| */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
 #include	<usysdefs.h>
 #include	<usysrets.h>
+
 
 using std::once_flag ;
 
