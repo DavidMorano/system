077d2534eae10c9f1938e0d86e16ae720f32e586
David A.D. Morano <david.a.morano@gmail.com>
2026-05-04 01:20:39 -0400

    update

--- src/libuc/getx/getdefzdata.cc
+++ src/libuc/getx/getdefzdata.cc
@@ -8,12 +8,12 @@
 
 /* revision history:
 
-	- 2008-10-01, David A­D­ Morano
+	- 1998-10-01, David A­D­ Morano
 	This object module was originally written.
 
 */
 
-/* Copyright © 2008 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
