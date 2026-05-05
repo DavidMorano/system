531f76888e306b9eeb6d05272aa3f98d0bf0f252
David A.D. Morano <david.a.morano@gmail.com>
2026-05-05 08:26:08 -0400

    restore

---
+++ src/libuc/ucdescmisc/ucftruncate.h
@@ -0,0 +1,51 @@
+/* ucftruncate HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* additional UNIX® support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+	= 2017-08-01, David A­D­ Morano
+	Updated for lack of interfaces in Apple-Darwin.
+
+*/
+
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Names:
+	uc_ftruncate
+
+  	Description:
+	Additional (or supplemental) support for UNIX® limits.
+
+*******************************************************************************/
+
+#ifndef	UCFTRUNCATE_INCLUDE
+#define	UCFTRUNCATE_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<stddef.h>		/* |nullptr_t| */
+#include	<stdlib.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	uc_ftruncate(int,off_t) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCFTRUNCATE_INCLUDE */
+
+
