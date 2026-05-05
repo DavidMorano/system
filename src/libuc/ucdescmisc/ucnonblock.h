cdae8c04846d13c34ddcc2628245f289336e5424
David A.D. Morano <david.a.morano@gmail.com>
2026-05-05 08:26:12 -0400

    restore

---
+++ src/libuc/ucdescmisc/ucnonblock.h
@@ -0,0 +1,51 @@
+/* ucnonblock HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* additional UNIX® support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+	= 2017-08-01, David A­D­ Morano
+	Updated for lack of interfaces in Apple-Darwin.
+
+*/
+
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Names:
+	uc_nonblock
+
+  	Description:
+	Additional (or supplemental) support for UNIX® limits.
+
+*******************************************************************************/
+
+#ifndef	UCNONBLOCK_INCLUDE
+#define	UCNONBLOCK_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<stddef.h>		/* |nullptr_t| */
+#include	<stdlib.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	uc_nonblock(int,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCNONBLOCK_INCLUDE */
+
+
