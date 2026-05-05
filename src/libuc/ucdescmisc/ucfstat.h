246af1976d9f14d15391390c64ca4b9f131462cd
David A.D. Morano <david.a.morano@gmail.com>
2026-05-05 08:26:27 -0400

    restore

---
+++ src/libuc/ucdescmisc/ucfstat.h
@@ -0,0 +1,54 @@
+/* ucfstat HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* additional UNIX® support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+	= 2017-08-01, David A­D­ Morano
+	Updated for lack of interfaces in Apple-Darwin.
+
+*/
+
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Names:
+	uc_fstat
+
+  	Description:
+	Additional (or supplemental) support for UNIX® limits.
+
+*******************************************************************************/
+
+#ifndef	UCFSTAT_INCLUDE
+#define	UCFSTAT_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<stddef.h>		/* |nullptr_t| */
+#include	<stdlib.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	uc_fstat(int,ustat *)	noex ;
+extern int	uc_fsize(int)		noex ;
+extern int	uc_fuid(int)		noex ;
+extern int	uc_fgid(int)		noex ;
+
+EXTERNC_end
+
+
+#endif /* UCFSTAT_INCLUDE */
+
+
