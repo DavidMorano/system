9a7bc2b3fc75ffc2034e3478a6ddc8b1c45d6593
David A.D. Morano <david.a.morano@gmail.com>
2026-05-05 08:21:50 -0400

    update

--- src/libuc/getx/getuserorg.h
+++ src/libuc/getx/getuserorg.h
@@ -2,9 +2,16 @@
 /* charset=ISO8859-1 */
 /* lang=C20 */
 
-/* get the best approximation of the username of the current user */
+/* get the best organization string */
 /* version %I% last-modified %G% */
 
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This code was originally written.
+
+*/
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
