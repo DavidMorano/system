46dc04dc1ec67956224d716b89386b1539c32f88
David A.D. Morano <david.a.morano@gmail.com>
2026-05-05 08:20:14 -0400

    update

--- src/libpcs/pcsnsmgr.cc
+++ src/libpcs/pcsnsmgr.cc
@@ -169,7 +169,7 @@ int pcsnsmgr_init() noex {
 		rs = msleep(1) ;
 		if (rs == SR_INTR) rs = SR_OK ;
 	    }
-	    if ((rs >= 0) && (! uip->f_init)) rs = SR_LOCKLOST ;
+	    if ((rs >= 0) && (! uip->f_init)) rs = SR_LOCKFAIL ;
 	}
 	return (rs >= 0) ? f : rs ;
 }
