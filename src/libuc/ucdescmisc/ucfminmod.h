78f18d1e5d7efab4a9bd63f427070f7cd2098703
David A.D. Morano <david.a.morano@gmail.com>
2026-05-05 08:26:04 -0400

    restore

---
+++ src/libuc/ucdescmisc/ucfminmod.h
@@ -0,0 +1,51 @@
+/* ucfminmod HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* additional UNIX® support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+	= 2017-08-01, David A­D­ Morano
+	Updated for lack of interfaces in Apple-Darwin.
+
+*/
+
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Names:
+	uc_fminmod
+
+  	Description:
+	Additional (or supplemental) support for UNIX® limits.
+
+*******************************************************************************/
+
+#ifndef	UCFMINMODE_INCLUDE
+#define	UCFMINMODE_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<stddef.h>		/* |nullptr_t| */
+#include	<stdlib.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	uc_fminmod(int,mode_t) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCFMINMODE_INCLUDE */
+
+
