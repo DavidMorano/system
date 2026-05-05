cb50ed2094e2fa31657b8f6c34d2e2f2bb0c1508
David A.D. Morano <david.a.morano@gmail.com>
2026-05-05 08:15:51 -0400

    restore

---
+++ src/libuc/ucopen/openxsvc.h
@@ -0,0 +1,29 @@
+/* openxsvc HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* LIBUC open facility */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	OPENXSVC_INCLUDE
+#define	OPENXSVC_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#endif /* OPENXSVC_INCLUDE */
+
+
