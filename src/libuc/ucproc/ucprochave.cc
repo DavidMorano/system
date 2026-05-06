1e406206a1b7aede3eea4442ae6005cfbf04497d
David A.D. Morano <david.a.morano@gmail.com>
2026-05-06 00:32:55 -0400

    restore

---
+++ src/libuc/ucproc/ucprochave.cc
@@ -0,0 +1,94 @@
+/* ucprochave SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* interface components for UNIX® library-3c */
+/* is a process (specified by PID) currently in the system? */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-10, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	ucprochave
+
+	Description:
+	This subroutine checks to see if the specific process
+	identified by its PID, is still on the system.  Note that
+	even zombie processes can satisfy the search requirement
+	on some systems!
+
+	Synopsis:
+	int ucprochave(pid_t pid) noex
+
+	Arguments:
+	pid			PID of process to search for
+
+	Returns:
+	true			process was found on system
+	false			process was not found on system
+	<0			error (system-return)
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<usyscalls.h>
+#include	<localmisc.h>
+
+#include	"ucprochave.h"
+
+
+/* local defines */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int ucprochave(pid_t pid) noex {
+	int		rs = SR_INVALID ;
+	int		f = false ;
+	if (pid >= 0) {
+	    if ((rs = u_kill(pid,0)) >= 0) {
+	        f = true ;
+	    } else if (rs == SR_PERM) {
+	        f = true ;
+	        rs = SR_OK ;
+	    } else if (rs == SR_SRCH) {
+	        rs = SR_OK ;
+	    }
+	} /* end if (valid) */
+	return (rs >= 0) ? f : rs ;
+}
+/* end subroutine (ucprochave) */
+
+
