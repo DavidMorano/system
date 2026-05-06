d89386d5389773b18da51c7f021889c48e98fe38
David A.D. Morano <david.a.morano@gmail.com>
2026-05-06 00:30:16 -0400

    update

--- src/libdam/statmsg.cc
+++ src/libdam/statmsg.cc
@@ -52,8 +52,8 @@
 #include	<usysbase.h>
 #include	<usyscalls.h>
 #include	<uclibmem.h>
-#include	<ucread.h>
-#include	<ucwrite.h>
+#include	<ucdescread.h>
+#include	<ucdescwrite.h>
 #include	<getax.h>
 #include	<getusername.h>
 #include	<getuserhome.h>
