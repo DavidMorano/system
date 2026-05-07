8183493fcc587693b6d4d51b577d1e7d1957e516
David A.D. Morano <david.a.morano@gmail.com>
2026-05-07 08:36:09 -0400

    restore

---
+++ src/libu/usysdefs.cc
@@ -0,0 +1,75 @@
+/* usysdefs SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* standard (relatively) UNIX® preprocessor defiitions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2001-03-24, David A­D­ Morano
+	This code is newly written but modeled after some code that
+	was originally written to get some standardized variable
+	serialization for the Ethernet-Controller (ETHCON) development
+	work.
+
+*/
+
+/* Copyright © 2001 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Variable:
+	netdberr
+
+	Description:
+	This is a variable that has member integers that are the
+	return error code for the various NETDB functions.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<localmisc.h>
+
+#include	"stdfiles.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+constexpr netdberrs	netdberr ;
+
+
+/* exported subroutines */
+
+
