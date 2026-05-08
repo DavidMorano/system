23417731724abeaea03f27a21ed143af61a874a6
David A.D. Morano <david.a.morano@gmail.com>
2026-05-08 00:16:09 -0400

    restore

---
+++ src/libuc/gethe.h
@@ -0,0 +1,33 @@
+/* gethe HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get a Host-Entry (HOSTENT) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This program was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	GETHE_INCLUDE
+#define	GETHE_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	<getheaddr.h>
+#include	<gethename.h>
+#include	<getheour.h>
+
+
+#endif /* GETHE_INCLUDE */
+
+
