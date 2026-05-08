204af3765de15707449e4a93fb3abe243ca54d66
David A.D. Morano <david.a.morano@gmail.com>
2026-05-08 00:17:13 -0400

    restore

---
+++ src/libuc/getx/getarch.h
@@ -0,0 +1,36 @@
+/* getarch HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get the machine arch string of the current system */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	GETARCH_INCLUDE
+#define	GETARCH_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	getarch(char *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* GETARCH_INCLUDE */
+
+
