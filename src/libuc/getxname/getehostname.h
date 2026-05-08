a8b8a943a9f2f6ba07619e9eeacd3cc8d4cac409
David A.D. Morano <david.a.morano@gmail.com>
2026-05-08 00:17:57 -0400

    restore

---
+++ src/libuc/getxname/getehostname.h
@@ -0,0 +1,36 @@
+/* gethehostname HEADER (Get-Effective-Hostname) */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get a HOSTENT for a given INET name */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This program was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	GETEHOSTNAME_INCLUDE
+#define	GETEHOSTNAME_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	getehostname(char *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* GETEHOSTNAME_INCLUDE */
+
+
