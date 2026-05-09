745990c361894c502fc2f922278a70fb2ab1b0e6
David A.D. Morano <david.a.morano@gmail.com>
2026-05-09 00:35:19 -0400

    restore

---
+++ src/libu/ugetx/ugetx.h
@@ -0,0 +1,33 @@
+/* ugetx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* miscellaneous LIBU library support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-23, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UGETX_INCLUDE
+#define	UGETX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	<getfdfile.h>
+#include	<getev.h>
+#include	<getenver.h>
+
+
+#endif /* UGETX_INCLUDE */
+
+
