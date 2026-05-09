f00178f2f8e119ad79dc2de769686c229954d35a
David A.D. Morano <david.a.morano@gmail.com>
2026-05-09 00:34:01 -0400

    restore

---
+++ src/libu/localmisc.cc
@@ -0,0 +1,69 @@
+/* localmisc SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* standard (relatively) UNIX® preprocessor defiitions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-15, David A­D­ Morano
+	This code is newly written but modeled after some code that
+	was originally written to get some standardized variable
+	serialization for the Ethernet-Controller (ETHCON) development
+	work.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Miscellaneous:
+
+	Description:
+	This contains miscellaneious support subroutines.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<localmisc.h>
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
