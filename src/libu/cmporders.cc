15693b139a714f0ef584a074a59e49e6de867f5a
David A.D. Morano <david.a.morano@gmail.com>
2026-05-09 00:34:11 -0400

    restore

---
+++ src/libu/cmporders.cc
@@ -0,0 +1,76 @@
+/* cmporders SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* these are values (constants) for specifying comparison order */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	cmporders
+
+	Description:
+	These are values (constants) for specifying comparison
+	order.
+
+	Synopsis:
+
+	Arguments:
+
+	Returns:
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<localmisc.h>
+
+#include	"cmporders.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
