5636bd954a12fdfc19544b2bad0d2354e9d23fe1
David A.D. Morano <david.a.morano@gmail.com>
2026-05-09 00:44:57 -0400

    update

--- src/libu/usupport/usupport_strnchar.cc
+++ src/libu/usupport/usupport_strnchar.cc
@@ -83,12 +83,9 @@ local char *strnxx(xchr_f xchr,cchar *sp,int sl) noex {
 	char		*rsp = nullptr ;
 	if (sp) ylikely {
 	    bool	f = false ;
-	    while (sl-- && *sp) {
-		cint	ch = mkchar(*sp) ;
-		f = xchr(ch) ;
-		if (f) break ;
-		sp += 1 ;
-	    } /* end while */
+	    for (int ch ; sl-- && ((ch = mkchar(*sp))) ; sp += 1) {
+		if ((f = xchr(ch))) break ;
+	    } /* end for */
 	    if (f) rsp = charp(sp) ;
 	} /* end if (non-null) */
 	return rsp ;
