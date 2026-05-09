223c924b31cfe9ab1f4d59528ce636e5f7d35252
David A.D. Morano <david.a.morano@gmail.com>
2026-05-09 00:36:27 -0400

    restore

---
+++ src/libu/usupport/usupport_nleadkeystr.hh
@@ -0,0 +1,36 @@
+/* usupport_nleadkeystr HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* match on the leading part of a string */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USUPPORTNLEADKEYSTR_INCLUDE
+#define	USUPPORTNLEADKEYSTR_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+namespace libu {
+    extern int nleadkeystr(cchar *,cchar *,int) noex ;
+}
+
+
+#endif /* USUPPORTNLEADKEYSTR_INCLUDE */
+
+
