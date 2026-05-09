24e88b790da2a319cfbad221a43a5861c0fd9fe1
David A.D. Morano <david.a.morano@gmail.com>
2026-05-09 00:36:05 -0400

    restore

---
+++ src/libu/usupport/usupport_hasnot.cc
@@ -0,0 +1,137 @@
+/* usupport_hasnot SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* does the given counted c-string have some characteristic? */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-10-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Group:
+	hasnot
+
+	Names:
+	hasnotdots
+	hasnotempty
+	
+	Aliases:
+	hasNotDots
+	hasNotEmpty
+
+	Description:
+	These subroutines determine if a given counted c-string
+	does not have something.
+
+	Synopsis:
+	bool hasnotdots(cchar *sp,int sl) noex
+	bool hasnotempty(cchar *sp,int sl) noex
+
+	Arguments:
+	sp		given counted c-string pointer
+	sl		given counted c-string length
+
+	Returns:
+	true		determination is true
+	false		determination is false
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<ascii.h>
+#include	<mkchar.h>
+#include	<localmisc.h>		/* |UC(3dam)| */
+
+#include	"usupport_hasnot.hh"
+
+#pragma		GCC dependency		"mod/libutil.ccm"
+#pragma		GCC dependency		"mod/ureserve.ccm"
+
+import libutil ;			/* |getlenstr(3u)| */
+import ureserve ;			/* |char_{x}(3u)| */
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+local bool	isnotempry(int) noex ;
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+namespace libu {
+    bool hasnotdots(cchar *sp,int µsl) noex {
+	bool		f = true ;
+	if (int sl ; (sl = getlenstr(sp,µsl)) > 0) {
+	    if (sp[0] == '.') {
+		switch (sl) {
+		case 1:
+		    f = false ;
+		    break ;
+		case 2:
+		    f = (sp[1] != '.') ;
+		    break ;
+		} /* end switch */
+	    } /* end if (had a dot) */
+	} /* end if (getlenstr) */
+	return f ;
+    } /* end subroutine (hasnotdots) */
+    bool hasnotempty(cchar *sp,int sl) noex {
+	bool		f = false ;
+	if (sp) ylikely {
+	    for (int ch ; sl-- && ((ch = mkchar(*sp))) ; sp += 1) {
+	        if ((f = isnotempry(ch))) break ;
+	    } /* end for */
+	} /* end if (non-null) */
+	return f ;
+    } /* end subroutine (hasnotempty) */
+} /* end namespace (libu) */
+
+
+/* local subroutines */
+
+local bool isnotempry(int ch) noex {
+    	bool f = true ;
+	f = f && (! char_iswhite(ch)) ;
+	f = f && (ch != CH_NL) ;
+	return f ;
+} /* end subroutine (isnotempry) */
+
+
