b5e0bac545ed9abfe7e3cd18fe058db688439d2c
David A.D. Morano <david.a.morano@gmail.com>
2026-05-10 00:10:12 -0400

    update

--- src/libuc/getxname/getnodename.cc
+++ src/libuc/getxname/getnodename.cc
@@ -50,8 +50,8 @@
 #include	<cstdlib>		/* <- |getenv(3c)| */
 #include	<clanguage.h>
 #include	<usysbase.h>
+#include	<usyscalls.h>
 #include	<uinfo.h>
-#include	<getx.h>		/* |getenver(3uc)| */
 #include	<sncpyx.h>
 #include	<snwcpy.h>
 #include	<rmx.h>			/* <- for |rmchr(3uc)| */
