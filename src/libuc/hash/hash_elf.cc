7488673354e91d9a7c71e152f4443031aea25ed4
David A.D. Morano <david.a.morano@gmail.com>
2026-05-10 00:10:46 -0400

    update

--- src/libuc/hash/hash_elf.cc
+++ src/libuc/hash/hash_elf.cc
@@ -53,7 +53,7 @@
 
 *******************************************************************************/
 
-#include	<envstandards.h>	/* first to configure */
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<cstdint>
