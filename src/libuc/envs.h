d629159ac603f1c9e4577ebbed7e0696815cb7b9
David A.D. Morano <david.a.morano@gmail.com>
2026-05-10 00:07:08 -0400

    update

--- src/libuc/envs.h
+++ src/libuc/envs.h
@@ -30,33 +30,33 @@
 struct envs_cursor {
 	hdb_cur		*curp ;
 	int		i ;
-} ;
+} ; /* end struct */
 
 struct envs_head {
 	hdb		*varp ;
 	uint		magic ;
-} ;
+} ; /* end struct */
 
 typedef ENVS		envs ;
 typedef ENVS_CUR	envs_cur ;
 
 EXTERNC_begin
 
-extern int envs_start(envs *,int) noex ;
-extern int envs_store(envs *,cchar *,int,cchar *,int) noex ;
-extern int envs_present(envs *,cchar *,int) noex ;
-extern int envs_substr(envs *,cchar *,int,cchar *,int) noex ;
-extern int envs_curbegin(envs *,envs_cur *) noex ;
-extern int envs_curend(envs *,envs_cur *) noex ;
-extern int envs_curenumkey(envs *,envs_cur *,cchar **) noex ;
-extern int envs_curenum(envs *,envs_cur *,cchar **,cchar **) noex ;
-extern int envs_fetch(envs *,cchar *,int,envs_cur *,cchar **) noex ;
-extern int envs_delname(envs *,cchar *,int) noex ;
-extern int envs_count(envs *) noex ;
-extern int envs_finish(envs *) noex ;
+extern int envs_start		(envs *,int) noex ;
+extern int envs_store		(envs *,cchar *,int,cchar *,int) noex ;
+extern int envs_present		(envs *,cchar *,int) noex ;
+extern int envs_substr		(envs *,cchar *,int,cchar *,int) noex ;
+extern int envs_curbegin	(envs *,envs_cur *) noex ;
+extern int envs_curend		(envs *,envs_cur *) noex ;
+extern int envs_curenumkey	(envs *,envs_cur *,cchar **) noex ;
+extern int envs_curenum		(envs *,envs_cur *,cchar **,cchar **) noex ;
+extern int envs_fetch		(envs *,cchar *,int,envs_cur *,cchar **) noex ;
+extern int envs_delname		(envs *,cchar *,int) noex ;
+extern int envs_count		(envs *) noex ;
+extern int envs_finish		(envs *) noex ;
 
-extern int envs_procxe(envs *,expcook *,mainv,vecstr *,cchar *) noex ;
-extern int envs_subs(envs *,expcook *,vecstr *,vecstr *) noex ;
+extern int envs_procxe		(envs *,expcook *,mainv,vecstr *,cchar *) noex ;
+extern int envs_subs		(envs *,expcook *,vecstr *,vecstr *) noex ;
 
 EXTERNC_end
 
