36778f9559ea2519e0967d85ea4aea316ef177ac
David A.D. Morano <david.a.morano@gmail.com>
2026-05-10 00:06:34 -0400

    update

--- src/libu/usupport/usupport_prime.h
+++ src/libu/usupport/usupport_prime.h
@@ -21,9 +21,16 @@
 #include	<usysbase.h>
 
 
+#ifndef	SUBROUTINE_MSLEEP
+#define	SUBROUTINE_MSLEEP
+EXTERNC_begin
+extern int	msleep(int) noex ;
+EXTERNC_end
+#endif
+
 #ifndef	TYPEDEF_MTIME
 #define	TYPEDEF_MTIME
-typedef int64_t		mtime_t ;
+typedef int64_t	mtime_t ;
 #endif
 
 #ifndef	SUBROUTINE_UMTIME
@@ -32,52 +39,6 @@ EXTERNC_begin
 extern mtime_t	umtime(void) noex ;
 EXTERNC_end
 #endif
-
-#ifndef	SUBROUTINE_MSLEEP
-#define	SUBROUTINE_MSLEEP
-EXTERNC_begin
-extern int	msleep(int) noex ;
-EXTERNC_end
-#endif
-
-#ifndef	OBJECT_GETMTIME
-#define	OBJECT_GETMTIME
-#ifdef	__cplusplus
-
-struct usys_mtime {
-	operator mtime_t () noex {
-	    return umtime() ;
-	} ;
-	static mtime_t operator () () noex {
-	    return umtime() ;
-	} ;
-} ; /* end struct (usys_mtime) */
-
-extern usys_mtime	getusmtime ;
-
-
-#endif /* __cplusplus */
-#endif /* OBJECT_GETMTIME */
-
-#ifndef	OBJECT_GETUSTIME
-#define	OBJECT_GETUSTIME
-#ifdef	__cplusplus
-
-namespace libu {
-    struct ugetustime {
-	operator time_t () noex {
-	    return time(nullptr) ;
-	} ;
-	static time_t operator () () noex {
-	    return time(nullptr) ;
-	} ;
-    } ; /* end struct (getustime) */
-} /* end namespace (libu) */
-
-extern libu::ugetustime		getustime ;
-
-#endif /* __cplusplus */
-#endif /* OBJECT_GETUSTIME */
 
 
 #endif /* USUPPORTPRIME_INCLUDE */
