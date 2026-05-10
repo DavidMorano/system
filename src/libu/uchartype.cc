0edaf8ecc72a5fa3d4f164c60641a7d4c3aa9f12
David A.D. Morano <david.a.morano@gmail.com>
2026-05-10 00:04:51 -0400

    update

--- src/libu/uchartype.cc
+++ src/libu/uchartype.cc
@@ -15,7 +15,7 @@
 
 /* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
 
-#include	<envstandards.h>	/* order first to configure */
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
