cdc72120bc1af81f9c03d54225c248a3b164bf0d
David A.D. Morano <david.a.morano@gmail.com>
2026-05-12 01:29:20 -0400

    restore

---
+++ src/libu/exitcodes.cc
@@ -0,0 +1,67 @@
+/* exitcodes SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* standard (relatively) UNIX® preprocessor defiitions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+
+/*******************************************************************************
+
+  	Miscellaneious:
+
+	Description:
+	This contains miscellaneious support subroutines.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<localmisc.h>
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
