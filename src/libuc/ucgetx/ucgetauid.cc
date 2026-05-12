912ca2a7d6c87ae4496373052bdb3f4ea877d3a0
David A.D. Morano <david.a.morano@gmail.com>
2026-05-12 01:34:16 -0400

    update

--- src/libuc/ucgetx/ucgetauid.cc
+++ src/libuc/ucgetx/ucgetauid.cc
@@ -1,11 +1,10 @@
-/* ucgetauid */
+/* ucgetauid SUPPORT */
 /* charset=ISO8859-1 */
 /* lang=C++20 (conformance reviewed) */
 
 /* interface component for UNIX® library-3c */
 /* version %I% last-modified %G% */
 
-#define	CF_DEBUGS	0		/* compile-time debugging */
 
 /* revision history:
 
@@ -22,30 +21,28 @@
 	uc_getauid
 
 	Description:
-	This subroutine retrieves from the system the Audit-UID of
+	This subroutine retrieves from the system the Audit-ID of
 	the current process.
 
 	Synopsis:
-	int uc_getauid()
+	int uc_getauid() noex
 
 	Arguments:
-	*node*
+	*none*
 
 	Returns:
-	>=0		UID of process (as an integer)
+	>=0		Audit-ID of process (as an integer)
 	<0		error (system-return)
 
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>
-#include	<sys/param.h>
-#include	<climits>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>		/* |getenv(3c)| */
 #include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
+#include	<usysbase.h>		/* |au_id_t| */
+#include	<intsat.h>
 #include	<localmisc.h>
 
 #include	"ucgetx.h"
@@ -57,10 +54,16 @@
 /* external subroutines */
 
 
-/* local variables */
+/* external variables */
+
+
+/* local structures */
 
 
 /* forward references */
+
+
+/* local variables */
 
 
 /* exported variables */
@@ -70,18 +73,12 @@
 
 int uc_getauid() noex {
 	int		rs ;
-	uid_t		auid = 0 ;
-
-#if	defined(SYSHAS_AUDIT) && (SYSHAS_AUDIT > 0) 
-	rs = SR_OK ;
-	if (getauid(&auid) < 0) rs = (- errno) ;
-	else rs = (int) (auid & INT_MAX) ;
-#else
-	rs = SR_NOSYS ;
-#endif /* SYSHAS_AUDIT */
-
-	return (rs >= 0) ? int(auid) : rs ;
-}
-/* end subroutine (uc_getauid) */
+	if (au_id_t auid{} ; getauid(&auid) >= 0) {
+	    rs = intsat(auid) ;
+	} else {
+	    rs = (- errno) ;
+	}
+	return rs ;
+} /* end subroutine (uc_getauid) */
 
 
