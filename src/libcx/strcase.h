d9cb0c71a32cc863e682a6e60316ecc9d685c057
David A.D. Morano <david.a.morano@gmail.com>
2026-05-12 01:28:01 -0400

    update

--- src/libcx/strcase.h
+++ src/libcx/strcase.h
@@ -18,7 +18,7 @@
 #ifndef STRCASE_INCLUDE
 #define	STRCASE_INCLUDE
 
-#include	<envstandards.h>	/* order first to configure */
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
