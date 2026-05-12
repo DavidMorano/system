8d6e3bed1edc3bc6adc784b35f8515223e060faa
David A.D. Morano <david.a.morano@gmail.com>
2026-05-12 01:28:06 -0400

    update

--- src/libcx/strlen.cc
+++ src/libcx/strlen.cc
@@ -27,7 +27,7 @@
 
 *******************************************************************************/
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
