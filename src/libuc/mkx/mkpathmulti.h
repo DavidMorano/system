d8b2de024209c285b9cf02979f3208fbeb19c8db
David A.D. Morano <david.a.morano@gmail.com>
2026-05-12 01:30:58 -0400

    restore

---
+++ src/libuc/mkx/mkpathmulti.h
@@ -0,0 +1,36 @@
+/* mkpathmulti HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* make a file rooted from the present-working-directory (PWD) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MKPATHMULTI_INCLUDE
+#define	MKPATHMULTI_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	mkpathmulti(char *,cchar *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* MKPATHMULTI_INCLUDE */
+
+
