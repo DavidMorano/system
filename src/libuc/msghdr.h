eba9b92b8fbaa337265bf20e7959c919fdd97472
David A.D. Morano <david.a.morano@gmail.com>
2026-05-13 02:14:46 -0400

    restore

---
+++ src/libuc/msghdr.h
@@ -0,0 +1,37 @@
+/* msghdr HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* control-message-header */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1999-08-01, Dave morano
+	File was originally written.
+
+*/
+
+/* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MSGHDR_INCLUDE
+#define	MSGHDR_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<sys/socket.h>		/* |CONMSGHDR| */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	msghdr_sz(MSGHDR *) noex ;
+
+EXTERNC_end
+
+
+#endif /* MSGHDR_INCLUDE */
+
+
