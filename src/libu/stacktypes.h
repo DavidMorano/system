86e1597f9def31a85f13380ab080f3da62138431
David A.D. Morano <david.a.morano@gmail.com>
2026-05-13 02:09:11 -0400

    restore

---
+++ src/libu/stacktypes.h
@@ -0,0 +1,38 @@
+/* stacktypes HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* enumerate the various stack types */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STACKTYPES_INCLUDE
+#define	STACKTYPES_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+enum stacktypes {
+	stacktype_thread,
+	stacktype_default,
+	stacktype_overlast
+} ; /* end enum */
+
+
+#endif /* STACKTYPES_INCLUDE */
+
+
