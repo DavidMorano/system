3074cd4fa1bf08c3ca617ed09d7c04af8c61765a
David A.D. Morano <david.a.morano@gmail.com>
2026-05-13 02:09:07 -0400

    restore

---
+++ src/libu/stacktypes.cc
@@ -0,0 +1,68 @@
+/* stacktypes SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* enumerate the various stack types */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Miscellaneous:
+
+	Description:
+	This contains miscellaneious support subroutines.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<localmisc.h>
+
+#include	"stacktypes.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
