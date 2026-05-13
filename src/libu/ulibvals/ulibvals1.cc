40e13a19bcea896e606dc75e9d1a73ee19ce9d0d
David A.D. Morano <david.a.morano@gmail.com>
2026-05-13 02:24:51 -0400

    update

--- src/libu/ulibvals/ulibvals1.cc
+++ src/libu/ulibvals/ulibvals1.cc
@@ -88,6 +88,7 @@ const int	ulibvals::maxbase	= MAXBASE ; /* the standard value */
 const int	ulibvals::maxpid	= getval(_SC_PID_MAX) ;
 const int	ulibvals::maxarg	= getval(_SC_ARG_MAX) ;
 const int	ulibvals::maxline	= getval(_SC_LINE_MAX) ;
+const int	ulibvals::maxlink	= getval(_SC_LINK_MAX) ;
 const int	ulibvals::maxlogin	= getval(_SC_LOGIN_NAME_MAX) ;
 const int	ulibvals::maxsymbol	= getval(_SC_SYMBOL_MAX) ;
 const int	ulibvals::maxgroups	= getval(_SC_NGROUPS_MAX) ;
