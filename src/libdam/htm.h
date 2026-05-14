2e40225d704144b5ad98d32b061fcce6b586553f
David A.D. Morano <david.a.morano@gmail.com>
2026-05-14 00:35:07 -0400

    update

--- src/libdam/htm.h
+++ src/libdam/htm.h
@@ -14,12 +14,9 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/types.h>		/* |off_t| */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
 #include	<stdarg.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<shio.h>
 
 
