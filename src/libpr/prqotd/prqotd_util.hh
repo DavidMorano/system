6bffd333e1b4b09ebc27c35199092352a408dbae
David A.D. Morano <david.a.morano@gmail.com>
2026-05-14 00:47:07 -0400

    restore

---
+++ src/libpr/prqotd/prqotd_util.hh
@@ -0,0 +1,73 @@
+/* prqotd_util SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* open a channel (file-descriptor) to the quote-of-the-day (QOTD) */
+/* version %I% last-modified %G% */
+
+#define	CF_DEBUGS	0		/* compile-time debugging */
+#define	CF_OPENDEF	0		/* ? */
+#define	CF_SOURCES	1		/* use sources */
+#define	CF_CONFIGCHECK	0		/* |config_check()| */
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	prqotd_util
+
+	Description:
+	This subroutine sort of forms the back-end maintenance end
+	of the QOTD mechansim.
+
+	Synopsis:
+
+	Arguments:
+
+	Returns:
+	>=0		FD of QOTD
+	<0		error (system-return)
+
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+#include	"prqotd.h"
+
+
+namespace prqotd {
+    struct vars {
+	mainv	envv ;
+	int	pid ;
+	int	maxnamelen ;
+	int	maxpathlen ;
+	int	ebuflen ;
+	int	vbuflen ;
+	operator int () noex ;
+    } ; /* end struct (vars) */
+} /* end namespace (prqotd) */
+
+namespace prqotd {
+    extern int	getdefmjd(time_t) noex ;
+    extern int	mkqfname(char *,cchar *,int) noex ;
+    extern int	mkourname(char *,cchar *,cchar *,cchar *,int) noex ;
+} /* end namespace (prqotd) */
+
+namespace prqotd {
+    extern vars		var ;
+} /* end namespace (prqotd) */
+
+
