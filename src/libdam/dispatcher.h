7487f61edcc3a7cca8e27acdac234cbf83ee238b
David A.D. Morano <david.a.morano@gmail.com>
2026-05-14 00:36:01 -0400

    update

--- src/libdam/dispatcher.h
+++ src/libdam/dispatcher.h
@@ -30,7 +30,7 @@
 #define	DISPATCHER_INCLUDE
 
 
-#include	<envstandards.h>	/* must be before others */
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
 #include	<usysbase.h>
 #include	<ciq.h>
