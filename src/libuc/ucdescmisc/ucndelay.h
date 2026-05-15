4630ef4cef4b3870a2aef2da9beae95636b1dfcd
David A.D. Morano <david.a.morano@gmail.com>
2026-05-15 01:53:04 -0400

    restore

---
+++ src/libuc/ucdescmisc/ucndelay.h
@@ -0,0 +1,36 @@
+/* ucndelay HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get random data from the system */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCNDELAY_INCLUDE
+#define	UCNDELAY_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	uc_ndelay(int,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCNDELAY_INCLUDE */
+
+
