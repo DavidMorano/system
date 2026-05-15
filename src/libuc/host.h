a6586baa3f8472b6038ca9fe46acc373a654462b
David A.D. Morano <david.a.morano@gmail.com>
2026-05-15 01:51:40 -0400

    restore

---
+++ src/libuc/host.h
@@ -0,0 +1,36 @@
+/* host HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* miscellaneous stuff which essentially every program wants! */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-15, David A­D­ Morano
+	This code was started to make life easier on the outside
+	(outside of Lucent Technologies).  This file largely contains
+	those things (defines) that I have found to be either useful
+	or problematic in the past.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	HOST_INCLUDE
+#define	HOST_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysdefs.h>
+
+#include	<hostaddr.h>
+#include	<hostent.h>
+#include	<hostinfo.h>
+
+
+#endif /* HOST_INCLUDE */
+
+
