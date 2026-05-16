66bdd93f603f60a0883a87998e325df888c281f9
David A.D. Morano <david.a.morano@gmail.com>
2026-05-16 00:25:27 -0400

    update

--- src/libuc/expcook.h
+++ src/libuc/expcook.h
@@ -37,24 +37,67 @@ struct expcook_cursor {
 
 struct expcook_head {
 	hdbstr		*hlp ;
-	uint		magic ;
+	uint		magval ;
 } ; /* end struct (expcook_head) */
 
-typedef EXPCOOK		expcook ;
 typedef EXPCOOK_CUR	expcook_cur ;
+
+#ifdef	__cplusplus
+enum expcookmems {
+    	expcookmem_start,
+    	expcookmem_finish,
+	expcookmem_overlast
+} ; /* end enum (expcookmems) */
+struct expcook ;
+struct expcook_co {
+	expcook		*op = nullptr ;
+	int		w = -1 ;
+	void operator () (expcook *p,int m) noex {
+	    op = p ;
+	    w = m ;
+	} ;
+	operator int () noex ;
+	int operator () () noex {
+	    return operator int () ;
+	} ;
+} ; /* end struct (expcook_co) */
+struct expcook : expcook_head {
+    	expcook_co	start ;
+    	expcook_co	finish ;
+	expcook() noex {
+	    start	(this,expcookmem_start) ;
+	    finish	(this,expcookmem_finish) ;
+	    magval = 0 ;
+	} ; /* end ctor */
+	int add		(cchar *,cchar *,int) noex ;
+	int curbegin	(expcook_cur *) noex ;
+	int curend	(expcook_cur *) noex ;
+	int curenum	(expcook_cur *,char *,int) noex ;
+	int findkey	(cchar *,int,cchar **) noex ;
+	int delkey	(cchar *) noex ;
+	int exp		(int,char *,int,cchar *,int) noex ;
+	int expbuf	(int,buffer *,cchar *,int) noex ;
+	void dtor() noex ;
+	destruct expcook() {
+	    if (magval) dtor() ;
+	} ; /* end destruct */
+} ; /* end struct (expcook) */
+#else
+typedef EXPCOOK		expcook ;
+#endif /* __cplusplus */
 
 EXTERNC_begin
 
-extern int expcook_start(expcook *) noex ;
-extern int expcook_add(expcook *,cchar *,cchar *,int) noex ;
-extern int expcook_curbegin(expcook *,expcook_cur *) noex ;
-extern int expcook_curend(expcook *,expcook_cur *) noex ;
-extern int expcook_curenum(expcook *,expcook_cur *,char *,int) noex ;
-extern int expcook_findkey(expcook *,cchar *,int,cchar **) noex ;
-extern int expcook_delkey(expcook *,cchar *) noex ;
-extern int expcook_exp(expcook *,int,char *,int,cchar *,int) noex ;
-extern int expcook_expbuf(expcook *,int,buffer *,cchar *,int) noex ;
-extern int expcook_finish(expcook *) noex ;
+extern int expcook_start	(expcook *) noex ;
+extern int expcook_add		(expcook *,cchar *,cchar *,int) noex ;
+extern int expcook_curbegin	(expcook *,expcook_cur *) noex ;
+extern int expcook_curend	(expcook *,expcook_cur *) noex ;
+extern int expcook_curenum	(expcook *,expcook_cur *,char *,int) noex ;
+extern int expcook_findkey	(expcook *,cchar *,int,cchar **) noex ;
+extern int expcook_delkey	(expcook *,cchar *) noex ;
+extern int expcook_exp		(expcook *,int,char *,int,cchar *,int) noex ;
+extern int expcook_expbuf	(expcook *,int,buffer *,cchar *,int) noex ;
+extern int expcook_finish	(expcook *) noex ;
 
 EXTERNC_end
 
