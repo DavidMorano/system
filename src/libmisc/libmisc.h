c33f2e0ca5e6862415c5c9073f47cf043817329e
David A.D. Morano <david.a.morano@gmail.com>
2026-05-16 00:21:12 -0400

    restore

---
+++ src/libmisc/libmisc.h
@@ -0,0 +1,39 @@
+/* libmisc HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* LIBMISC utilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This file was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	LIBMISC_INCLUDE
+#define	LIBMISC_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysnase.h>
+
+#include	<groupent.h>
+#include	<projectent.h>
+#include	<sysgroup.h>
+#include	<syspasswd.h>
+#include	<sysproject.h>
+#include	<sysusernames.h>
+#include	<sysusers.h>
+#include	<<sysusershells.h>
+#include	<userattrent.h>
+
+
+#endif /* LIBMISC_INCLUDE */
+
+
