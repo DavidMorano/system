7b761a5cb00049aba2d0fea7dd18ff4fc139c6b2
David A.D. Morano <david.a.morano@gmail.com>
2026-05-16 00:29:36 -0400

    update

--- src/libuc/getx/getx.h
+++ src/libuc/getx/getx.h
@@ -30,6 +30,7 @@
 #include	<getdefzdata.h>
 #include	<geteaddrinfo.h>
 #include	<gethz.h>
+#include	<getlogx.h>
 #include	<getmailgid.h>
 #include	<getmjd.h>
 #include	<getnfile.h>
@@ -39,26 +40,30 @@
 #include	<getnodeinfo.h>
 #include	<getnprocessors.h>
 #include	<getoflags.h>
+#include	<getopendial.h>
 #include	<getpassword.h>
 #include	<getportnum.h>
+#include	<getprogexec.h>
 #include	<getprogpath.h>
 #include	<getproto.h>
 #include	<getpf.h>
 #include	<getpwd.h>
 #include	<getpwentry.h>
+#include	<getpwlogname.h>
+#include	<getpwx.h>
 #include	<getrand.h>
+#include	<getseed.h>
 #include	<getserial.h>
 #include	<getserv.h>
 #include	<getsocktype.h>
 #include	<getsyslogx.h>
 #include	<getsystypenum.h>
+#include	<gettmpdir.h>
 #include	<getuserhome.h>
 #include	<getuserorg.h>
+#include	<getuserterm.h>
 #include	<getuserterms.h>
 #include	<getxid.h>
-#include	<getopendial.h>
-#include	<getseed.h>
-#include	<gettmpdir.h>
 
 
 #endif /* GETX_INCLUDE */
