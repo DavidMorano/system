e05d0a0ccf666ca4143f2923fd0cc126a66f2d90
David A.D. Morano <david.a.morano@gmail.com>
2026-05-16 00:22:17 -0400

    update

--- src/libmisc/sysproject.cc
+++ src/libmisc/sysproject.cc
@@ -2,40 +2,46 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
-/* enumerate system SYSDB-XX entries */
+/* system project-entry enumeration */
 /* version %I% last-modified %G% */
 
 
 /* revision history:
 
-	= 1998-03-24, David A­D­ Morano
+	= 1998-03-24, David A-D- Morano
 	This object module was morphed from some previous one. I
 	do not remember what the previous one was.
 
 */
 
-/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1998 David A-D- Morano.  All rights reserved. */
 
 /*******************************************************************************
 
-	We enumerate (reentrantly and thread safely) SYSDB entries
-	from the system SYSDB-XX database.
+  	Object:
+	sysproject
+
+	Description:
+	We enumerate (reentrantly and thread safely) PROJECT entries
+	from the system PROJECT database.
 
 *******************************************************************************/
 
-#include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<unistd.h>
-#include	<climits>
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<climits>		/* |INT_MAX| */
+#include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
-#include	<cstring>
-#include	<usystem.h>
-#include	<sysdbfn.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<ucentpj.h>
+#include	<filemap.h>
 #include	<localmisc.h>
 
-#include	"ucenumxx.h"
 #include	"sysproject.h"
 
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |memclear(3u)| */
 
 /* local defines */
 
@@ -49,19 +55,55 @@
 /* external subroutines */
 
 
+/* external variables */
+
+
 /* local structures */
 
 
 /* forward references */
 
+template<typename ... Args>
+local int sysproject_ctor(sysproject *op,Args ... args) noex {
+    	SYSPROJECT	*hop = op ;
+	cnullptr	np{} ;
+	cnothrow	nt{} ;
+	int		rs = SR_FAULT ;
+	if (op && (args && ...)) ylikely {
+	    memclear(hop) ;
+	    rs = SR_NOMEM ;
+	    if ((op->fmp = new(nt) filemap) != np) ylikely {
+		rs = SR_OK ;
+	    } /* end if (new-filemap) */
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (sysproject_ctor) */
+
+local int sysproject_dtor(sysproject *op) noex {
+	int		rs = SR_FAULT ;
+	if (op) ylikely {
+	    rs = SR_OK ;
+	    if (op->fmp) ylikely {
+		delete op->fmp ;
+		op->fmp = nullptr ;
+	    } /* end if (momory-release) */
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (sysproject_dtor) */
+
+template<typename ... Args>
+local inline int sysproject_magic(sysproject *op,Args ... args) noex {
+	int		rs = SR_FAULT ;
+	if (op && (args && ...)) ylikely {
+	    rs = (op->magval == SYSPROJECT_MAGIC) ? SR_OK : SR_NOTOPEN ;
+	}
+	return rs ;
+} /* end subroutine (sysproject_magic) */
+
 
 /* local variables */
 
-constexpr uint		mxx = SYSPROJECT_MAGIC ;
-constexpr sysdbfiles	w = sysdbfile_pj ;
-
-
-/* local subroutines */
+constexpr cchar		defufname[] = SYSPROJECT_FNAME ;
 
 
 /* exported variables */
@@ -69,35 +111,60 @@ constexpr sysdbfiles	w = sysdbfile_pj ;
 
 /* exported subroutines */
 
-int sysproject_open(sysproject *op,cchar *efname) noex {
-	ucenumxxco<sysproject_ent>	ueo(op,mxx) ;
+int sysproject_open(sysproject *op,cchar *sufname) noex {
+	csize		nmax = INT_MAX ;
 	int		rs ;
-	if (cchar *rp{} ; (rs = sysdbfn_get(w,efname,&rp)) >= 0) {
-	    rs = ueo.open(rp) ;
-	} /* end if (sysdbfn_get) */
+	if (sufname == nullptr) sufname = defufname ; /* default */
+	if ((rs = sysproject_ctor(op)) >= 0) ylikely {
+	    filemap	*fmp = op->fmp ;
+	    if ((rs = fmp->open(sufname,nmax)) >= 0) ylikely {
+	        op->magval = SYSPROJECT_MAGIC ;
+	    }
+	} /* end if (magic) */
 	return rs ;
 }
-/* end if (sysproject_open) */
+/* end if (sysproject_start) */
 
 int sysproject_close(sysproject *op) noex {
-	ucenumxxco<sysproject_ent>	ueo(op,mxx) ;
-	return ueo.close() ;
+	int		rs ;
+	int		rs1 ;
+	if ((rs = sysproject_magic(op)) >= 0) ylikely {
+	    if (op->fmp) ylikely {
+		filemap		*fmp = op->fmp ;
+		rs1 = fmp->close ;
+		if (rs >= 0) rs = rs1 ;
+	    }
+	    {
+		rs1 = sysproject_dtor(op) ;
+		if (rs >= 0) rs = rs1 ;
+	    }
+	    op->magval = 0 ;
+	} /* end if (magic) */
+	return rs ;
 } 
 /* end subroutine (sysproject_close) */
 
-int sysproject_readent(sysproject *op,sysproject_ent *ep,char *bp,int bl) noex {
-	ucenumxxco<sysproject_ent>	ueo(op,mxx) ;
-	return ueo.readent(ep,bp,bl) ;
+int sysproject_readent(sysproject *op,ucentpj *pwp,char *pwbuf,int pwlen) noex {
+	int		rs ;
+	if ((rs = sysproject_magic(op,pwp,pwbuf)) >= 0) ylikely {
+	    filemap	*fmp = op->fmp ;
+	    for (cchar *lp ; (rs = fmp->getln(&lp)) > 0 ; ) {
+	        rs = pwp->parse(pwbuf,pwlen,lp,rs) ;
+	        if (rs) break ;
+	    } /* end for */
+	} /* end if (magic) */
+	return rs ;
 }
 /* end subroutine (sysproject_readent) */
 
 int sysproject_reset(sysproject *op) noex {
-	ucenumxxco<sysproject_ent>	ueo(op,mxx) ;
-	return ueo.reset() ;
+	int		rs ;
+	if ((rs = sysproject_magic(op)) >= 0) ylikely {
+	    filemap	*fmp = op->fmp ;
+	    rs = fmp->rewind ;
+	} /* end if (magic) */
+	return rs ;
 }
 /* end subroutine (sysproject_reset) */
-
-
-/* local subroutines */
 
 
