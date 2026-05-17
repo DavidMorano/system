e0b8627c752c251a0999946167b975086aa721e0
David A.D. Morano <david.a.morano@gmail.com>
2026-05-17 00:38:30 -0400

    restore

---
+++ src/libdam/argutils/argutils.h
@@ -0,0 +1,29 @@
+/* argutils HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* argument-vector utilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-06-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	ARGUTILS_INCLUDE
+#define	ARGUTILS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#endif /* ARGUTILS_INCLUDE */
+
+
