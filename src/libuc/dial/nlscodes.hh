033a6f5ad30b23d6878dd9c7dc18c98b873820e8
David A.D. Morano <david.a.morano@gmail.com>
2026-05-17 00:46:04 -0400

    restore

---
+++ src/libuc/dial/nlscodes.hh
@@ -0,0 +1,60 @@
+/* nlscodes HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* information to create and read an NLS-connect message */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+/*******************************************************************************
+
+  	Name:
+	nlscodes
+
+  	Description:
+	This header file contains some pre-processor defines used to
+	create an NLS connection message and to parse the result.
+
+	Definitions:
+	NLPS		Network-Listener-Process-Server
+	NLS		Network-Listener-Service
+
+*******************************************************************************/
+
+#ifndef	NLSCODES_INCLUDE
+#define	NLSCODES_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<nlsmsg.h>		/* <- standard defines */
+
+
+struct nlscodes {
+    	static constexpr int	start		= NLSSTART ;
+    	static constexpr int	format		= NLSFORMAT ;
+    	static constexpr int	sunknown	= NLSUNKNOWN ;
+    	static constexpr int	disabled	= NLSDISABLED ;
+} ; /* end struct (nlscodes) */
+
+extern const nlscodes	nlscode ;
+
+
+#endif /* __cplusplus */
+#endif /* NLSCODES_INCLUDE */
+
+
