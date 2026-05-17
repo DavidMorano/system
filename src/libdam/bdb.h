e49e9d40e9ad34212144394401d092081bb04e02
David A.D. Morano <david.a.morano@gmail.com>
2026-05-17 00:39:32 -0400

    update

--- src/libdam/bdb.h
+++ src/libdam/bdb.h
@@ -2,6 +2,9 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 (conformance reviewed) */
 
+/* Bibliographical DataBase */
+/* version %I% last-modified %G% */
+
 
 /* revision history:
 
@@ -9,20 +12,20 @@
 	This module was changed to serve in the REFERM program.
 
 	= 2005-10-01, David A­D­ Morano
-	This was changed to work in the MMCITE program. The old
-	REFERM program is really obsolete. It used a database lookup
-	strategy to remote databases. The high-level problem is:
-	what to do if the cited BIB entry isn't found? How does a
+	This was changed to work in the MMCITE program.  The old
+	REFERM program is really obsolete.  It used a database lookup
+	strategy to remote databases.  The high-level problem is:
+	what to do if the cited BIB entry is not found?  How does a
 	maintainer of the present (local) document know what that
-	BIB entry was? The new strategy (implemented by the MMCITE
+	BIB entry was?  The new strategy (implemented by the MMCITE
 	program) is more like what is done with BibTeX in the TeX
-	(or LaTeX) world. All BIB databases are really expected to
+	(or LaTeX) world.  All BIB databases are really expected to
 	be maintained by the document creator -- not some centralized
-	entiry. The older centralized model reflected more the use
+	entiry.  The older centralized model reflected more the use
 	in the corporate world (where different people create BIB
 	entries) than in the more "modern" personal-responsibility
-	type of world! :-) Anyway, this is the way the gods seem
-	to now want to do things. Deal with it!
+	type of world! :-)  Anyway, this is the way the gods seem
+	to now want to do things.  Deal with it!
 
 */
 
@@ -55,7 +58,7 @@
 
 struct bdb_cursor {
 	HDB_CUR		cur ;
-} ;
+} ; /* end struct */
 
 struct bdb_entry {
 	cchar		*(*keyvals)[2] ;
@@ -63,7 +66,7 @@ struct bdb_entry {
 	int		nkeys ;
 	int		size ;
 	int		fi ;		/* file index */
-} ;
+} ; /* end struct */
 
 struct bdb_head {
 	cchar		*qkbuf ;	/* query key name (usually 'Q') */
@@ -73,7 +76,7 @@ struct bdb_head {
 	uint		opts ;
 	int		unindexed ;	/* number of unindexed files */
 	int		qklen ;
-} ;
+} ; /* end struct */
 
 typedef	BDB		bdb ;
 typedef	BDB_CUR		bdb_cur ;
