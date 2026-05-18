189e82a18674384909592d7acaa2880cec3d29c3
David A.D. Morano <david.a.morano@gmail.com>
2026-05-18 00:23:43 -0400

    update

--- src/libuc/sigobj/sigman.h
+++ src/libuc/sigobj/sigman.h
@@ -26,9 +26,9 @@
 #include	<usysbase.h>
 
 
-#define	SIGMAN_MAGIC	0x66938271
 #define	SIGMAN		struct sigman_head
 #define	SIGMAN_HA	struct sigman_hale
+#define	SIGMAN_MAGIC	0x66938271
 
 
 struct sigman_hale {
@@ -39,17 +39,50 @@ struct sigman_hale {
 struct sigman_head {
 	SIGMAN_HA	*handles ;
 	sigset_t	osm ;
-	uint		magic ;
+	uint		magval ;
 	int		nhs ;
 	int		nblks ;
 } ; /* end struct */
 
-typedef	SIGMAN		sigman ;
 typedef	SIGMAN_HA	sigman_ha ;
 
 EXTERNC_begin
-
 typedef void (*sigmanhand_f)(int) noex ;
+EXTERNC_end
+
+#ifdef	__cplusplus
+enum sigmanmems {
+	sigmanmem_finish,
+	sigmanmem_overlast
+} ; /* end enum */
+struct sigman ;
+struct sigman_co {
+	sigman		*op = nullptr ;
+	int		w = -1 ;
+	void operator () (sigman *p,int m) noex {
+	    op = p ;
+	    w = m ;
+	} ;
+	operator int () noex ;
+} ; /* end struct (sigman_co) */
+struct sigman : sigman_head {
+	sigman_co	finish ;
+	sigman() noex {
+	    finish	(this,sigmanmem_finish) ;
+	    magval = 0 ;
+	} ; /* end ctor */
+	int start(cint *,cint *,cint *,sigmanhand_f) noex ;
+	void dtor() noex ;
+	destruct sigman() {
+	    if (magval) dtor() ;
+	} ; /* end destruct */
+} ; /* end class (sigman) */
+
+#else
+typedef	SIGMAN		sigman ;
+#endif /* __cplusplus */
+
+EXTERNC_begin
 
 extern int	sigman_start(sigman *,cint *,cint *,cint *,sigmanhand_f) noex ;
 extern int	sigman_finish(sigman *) noex ;
