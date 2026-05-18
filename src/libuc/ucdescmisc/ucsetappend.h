877136e75fb9b49277c103db8ebff37d41ffeced
David A.D. Morano <david.a.morano@gmail.com>
2026-05-18 00:25:46 -0400

    restore

---
+++ src/libuc/ucdescmisc/ucsetappend.h
@@ -0,0 +1,53 @@
+/* ucsetappend HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* additional UNIX® support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+	= 2017-08-01, David A­D­ Morano
+	Updated for lack of interfaces in Apple-Darwin.
+
+*/
+
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Names:
+	uc_setappend
+	uc_append
+
+  	Description:
+	Additional (or supplemental) support for UNIX® limits.
+
+*******************************************************************************/
+
+#ifndef	UCSETAPPEND_INCLUDE
+#define	UCSETAPPEND_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<stddef.h>		/* |nullptr_t| */
+#include	<stdlib.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	uc_setappend	(int,int) noex ;
+extern int	uc_append	(int,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCSETAPPEND_INCLUDE */
+
+
