0a4168346ae3e2f5d3c2208ce8223f16e6feec31
David A.D. Morano <david.a.morano@gmail.com>
2026-05-18 00:26:30 -0400

    update

--- src/libuc/ucdescsock/ucsockprime.h
+++ src/libuc/ucdescsock/ucsockprime.h
@@ -26,16 +26,16 @@
 EXTERNC_begin
 
 extern int	uc_bind(int,cvoid *,int) noex ;
-extern int	uc_connect(int,cvoid *,int) noex ;
-extern int	uc_connecte(int,cvoid *,int,int) noex ;
 extern int	uc_getpeername(int,void *,int *) noex ;
 extern int	uc_getsockname(int,void *,int *) noex ;
-extern int	uc_getsockopt(int,int,int,void *,int *) noex ;
+extern int	uc_sockoptget(int,int,int,void *,int *) noex ;
+extern int	uc_sockoptget(int,int,int,void *,int *) noex ;
 extern int	uc_getsocktype(int) noex ;
 extern int	uc_linger(int,int) noex ;
 extern int	uc_listen(int,int) noex ;
 extern int	uc_recv(int,void *,int,int) noex ;
 extern int	uc_recve(int,void *,int,int,int,int) noex ;
+extern int	uc_recvfrom(int,void *,int,int,void *,int *) noex ;
 extern int	uc_recvfrome(int,void *,int,int,void *,int *,int,int) noex ;
 extern int	uc_recvmsg(int,MSGHDR *,int) noex ;
 extern int	uc_recvmsge(int,MSGHDR *,int,int,int) noex ;
@@ -43,7 +43,7 @@ extern int	uc_send(int,cvoid *,int,int) noex ;
 extern int	uc_sendfiler(int,int,off_t,size_t) noex ;
 extern int	uc_sendmsg(int,CMSGHDR *,int) noex ;
 extern int	uc_sendto(int,cvoid *,int,int,cvoid *,int) noex ;
-extern int	uc_setsockopt(int,int,int,int *,int) noex ;
+extern int	uc_sockoptset(int,int,int,cvoid *,int) noex ;
 extern int	uc_shutdown(int,int) noex ;
 extern int	uc_sockatmark(int) noex ;
 
