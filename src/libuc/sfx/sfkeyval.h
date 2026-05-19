dd3a77db5913631388838e531f933ac5d4cddc48
David A.D. Morano <david.a.morano@gmail.com>
2026-05-19 00:48:05 -0400

    restore

---
+++ src/libuc/sfx/sfkeyval.h
@@ -0,0 +1,37 @@
+/* sfkeyval HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* various string-find operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-09-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SFKEYVAL_INCLUDE
+#define	SFKEYVAL_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	sfkeyval	(cchar *,int,cchar **) noex ;
+extern int	sfcontent	(cchar *,int,cchar **) noex ;
+
+EXTERNC_end
+
+
+#endif /* SFKEYVAL_INCLUDE */
+
+
