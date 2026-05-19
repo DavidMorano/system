b30730847f28f90a971ad1f203239bf8f3184f73
David A.D. Morano <david.a.morano@gmail.com>
2026-05-19 00:47:42 -0400

    restore

---
+++ src/libu/usupport/usupport_strwcmp.cc
@@ -0,0 +1,186 @@
+/* usupport_strwcmp SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* compare a (NUL-terminated) base c-string with a counted c-string */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	Originally written for a convenience.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	strwcmp
+	strw{x}cmp
+
+	Description: 
+	This subroutine compares two strings, but the second string
+	is allowed to be counted instead of NUL terminated.
+
+	Synopsis:
+	int strwcmp(cchar *s1,cchar *s2,int s2len) noex
+	int strw{x}cmp(cchar *s1,cchar *s2,int s2len) noex
+
+	Arguments:
+	{×}	'base' 'case' 'fold'
+	s1	one string
+	s2	second string
+	s2len	length of second string
+
+	Returns:
+	>0	first string is greater than the second
+	0	both strings are equal (as compared)
+	<0	first string is less than the second
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<climits>		/* |UCHAR_MAX| */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usupport.h>
+#include	<stdclib.hh>		/* |std_strncasecmp(3u)| */
+#include	<localmisc.h>
+
+#include	"usupport_strncmp.hh"
+#include	"usupport_strwcmp.hh"
+
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |lenstr(3u)| */
+
+/* local defines */
+
+
+/* imported namespaces */
+
+using libu::strnbasecmp ;		/* subroutine */
+using libu::strncasecmp ;		/* subroutine */
+using libu::strnfoldcmp ;		/* subroutine */
+using libu::nleadbasestr ;		/* subroutine */
+using libu::nleadcasestr ;		/* subroutine */
+using libu::nleadfoldstr ;		/* subroutine */
+using libu::tobc ;			/* subroutine */
+using libu::touc ;			/* subroutine */
+using libu::tofc ;			/* subroutine */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+namespace {
+   struct cmpx {
+	int x(cc *,cc *,int)			const noex ;
+	virtual int tox(int ch) 		const noex = 0 ;
+	virtual int strnxcmp(cc *,cc *,int) 	const noex = 0 ;
+	virtual int nleadxstr(cc *,cc *,int)	const noex = 0 ;
+   } ; /* end struct (cmpx) */
+   struct basecmpx : cmpx {
+	int tox(int ch)				const noex override final {
+	    return tobc(ch) ;
+	} ;
+	int strnxcmp(cc *bs,cc *sp,int sl)	const noex override final {
+	    return strnbasecmp(bs,sp,sl) ;
+	} ;
+	int nleadxstr(cc *s1,cc *s2,int sl)	const noex override final {
+	    return nleadbasestr(s1,s2,sl) ;
+	} ;
+   } ; /* end struct */
+   struct casecmpx : cmpx {
+	int tox(int ch)				const noex override final {
+	    return touc(ch & UCHAR_MAX) ;
+	} ;
+	int strnxcmp(cc *bs,cc *sp,int sl)	const noex override final {
+	    return strncasecmp(bs,sp,sl) ;
+	} ;
+	int nleadxstr(cc *s1,cc *s2,int sl)	const noex override final {
+	    return nleadcasestr(s1,s2,sl) ;
+	} ;
+   } ; /* end struct */
+   struct foldcmpx : cmpx {
+	int tox(int ch)				const noex override final {
+	    return tofc(ch & UCHAR_MAX) ;
+	} ;
+	int strnxcmp(cc *bs,cc *sp,int sl)	const noex override final {
+	    return strnfoldcmp(bs,sp,sl) ;
+	} ;
+	int nleadxstr(cc *s1,cc *s2,int sl)	const noex override final {
+	    return nleadfoldstr(s1,s2,sl) ;
+	} ;
+   } ; /* end struct */
+} /* end namespace */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* external subroutines */
+
+
+/* exported subroutines */
+
+namespace libu {
+    int strwbasecmp(cchar *s1,cchar *s2,int s2len) noex {
+	basecmpx co ;
+	return co.x(s1,s2,s2len) ;
+    } /* end subroutine (strwbasecmpx) */
+    int strwcasecmp(cchar *s1,cchar *s2,int s2len) noex {
+	casecmpx co ;
+	return co.x(s1,s2,s2len) ;
+    } /* end subroutine (strwcasecmpx) */
+    int strwfoldcmp(cchar *s1,cchar *s2,int s2len) noex {
+	foldcmpx co ;
+	return co.x(s1,s2,s2len) ;
+    } /* end subroutine (strwfoldcmpx) */
+} /* end namespace (libu) */
+
+
+/* local subroutines */
+
+int cmpx::x(cchar *s1,cchar *s2,int µs2len) const noex {
+	int		rc = 0 ;
+	if (s1 && s2) {
+	    rc = tox(s1[0]) ;
+	    if (cint s2len = getlenstr(s2,µs2len) ; s2len > 0) {
+	        cint	ch1 = tox(*s1) ;
+	        cint	ch2 = tox(*s2) ;
+	        if ((rc = (ch1 - ch2)) == 0) {
+	            if ((rc = strnxcmp(s1,s2,s2len)) == 0) {
+	                if (cint m = nleadxstr(s1,s2,s2len) ; m < s2len) {
+	    	            cint	m1 = tox(s1[m]) ;
+	    	            cint	m2 = tox(s2[m]) ;
+		            rc = (m1 - m2) ;
+		        } else {
+		            rc = tox(s1[m]) ;
+		        }
+		    } /* end if (strnxcmp) */
+	        } /* end if (first characters were equal) */
+	    } /* end if (getlenstr) */
+	} /* end if (non-null) */
+	return rc ;
+} /* end method (cmpx::x) */
+
+
