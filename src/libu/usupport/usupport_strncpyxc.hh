6ab45536b886fa702a4bb4a1ecff9038eda89d20
David A.D. Morano <david.a.morano@gmail.com>
2026-05-19 00:47:36 -0400

    restore

---
+++ src/libu/usupport/usupport_strncpyxc.hh
@@ -0,0 +1,39 @@
+/* usupport_strncpyxc HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* string-copy variant */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USUPPORTSTRNCPYXC_INCLUDE
+#define	USUPPORTSTRNCPYXC_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+namespace libu {
+    extern char	* strncpybc (char *,cchar *,int) noex ;
+    extern char	* strncpylc (char *,cchar *,int) noex ;
+    extern char	* strncpyuc (char *,cchar *,int) noex ;
+    extern char	* strncpyfc (char *,cchar *,int) noex ;
+} /* end namespace (libu) */
+
+
+#endif /* __cplusplus */
+#endif /* USUPPORTSTRNCPYXC_INCLUDE */
+
+
