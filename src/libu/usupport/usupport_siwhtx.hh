3bbca886de26119de8dcbad7a074be4ea55da9e1
David A.D. Morano <david.a.morano@gmail.com>
2026-05-19 00:47:19 -0400

    restore

---
+++ src/libu/usupport/usupport_siwhtx.hh
@@ -0,0 +1,35 @@
+/* usupport_siwhtx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* find a character or white-space */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USUPPORTSIWHTX_INCLUDE
+#define	USUPPORTSIWHTX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+namespace libu {
+    extern int	siwhtchr	(cchar *,int,int) noex ;
+    extern int	siwhtbrk	(cchar *,int,cchar *) noex ;
+} /* end namespace (libu) */
+
+
+#endif /* USUPPORTSIWHTX_INCLUDE */
+
+
