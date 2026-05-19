67744e449585b34072ca30f26cb739c5e93d0da6
David A.D. Morano <david.a.morano@gmail.com>
2026-05-19 00:48:01 -0400

    restore

---
+++ src/libuc/sfx/sfkey.h
@@ -0,0 +1,36 @@
+/* sfkey HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* various string-find operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-09-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SFKEY_INCLUDE
+#define	SFKEY_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+EXTERNC_begin
+
+extern int	sfkey	(cchar *,int,cchar **) noex ;
+
+EXTERNC_end
+
+
+#endif /* SFKEY_INCLUDE */
+
+
