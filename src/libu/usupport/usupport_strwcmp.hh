722dde2bcd0125b5a86b794bed5c1637e395013f
David A.D. Morano <david.a.morano@gmail.com>
2026-05-19 00:47:47 -0400

    restore

---
+++ src/libu/usupport/usupport_strwcmp.hh
@@ -0,0 +1,43 @@
+/* usupport_strwcmp HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* string-comparison variant */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USUPPORTSTRWCMP_INCLUDE
+#define	USUPPORTSTRWCMP_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+namespace libu {
+    extern int strwbasecmp(cchar *,cchar *,int) noex ;
+    extern int strwcasecmp(cchar *,cchar *,int) noex ;
+    extern int strwfoldcmp(cchar *,cchar *,int) noex ;
+    inline int strwcmp(cchar *bs,cchar *sp,int sl) noex {
+	return strwbasecmp(bs,sp,sl) ;
+    } /* end subroutine (strwcmp) */
+} /* end namespace (libu) */
+
+
+#endif /* __cplusplus */
+#endif /* USUPPORTSTRWCMP_INCLUDE */
+
+
