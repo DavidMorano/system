e13e76dd3337dcb8e6fc32722e363bb9fb2c2847
David A.D. Morano <david.a.morano@gmail.com>
2026-05-19 01:00:44 -0400

    update

--- src/libuc/bufsize/bufsizes.cc
+++ src/libuc/bufsize/bufsizes.cc
@@ -1,4 +1,4 @@
-/* bufsizenames SUPPORT */
+/* bufsizes SUPPORT */
 /* charset=ISO8859-1 */
 /* lang=C++23 (MUST-BE C-language; conformance reviewed to C23) */
 
@@ -18,7 +18,7 @@
 /*******************************************************************************
 
 	Name:
-	bufsizenames
+	bufsizs
 
 	Description:
 	This file contains the enumerations for the various system
@@ -33,37 +33,72 @@
 #include	<clanguage.h>
 #include	<usysbase.h>
 
-#include	"bufsizenames.h"
+#include	"bufsizes.h"
 
 
-cchar *bufsizes[] = {
-	[bufsize_ps]		= "PS",		/* page-size */
-	[bufsize_mi]		= "MI",		/* max-pid */
-	[bufsize_ma]		= "MA",		/* max-args */
-	[bufsize_mn]		= "MN",		/* max-name */
-	[bufsize_mp]		= "MP",		/* max-path */
-	[bufsize_ml]		= "ML",		/* max-line */
-	[bufsize_mm]		= "MM",		/* max-message */
-	[bufsize_nn]		= "NN",		/* node-name */
-	[bufsize_hn]		= "HN",		/* host-name */
-	[bufsize_un]		= "UN",		/* user-name */
-	[bufsize_gn]		= "GN",		/* group-name */
-	[bufsize_pn]		= "PN",		/* project-name */
-	[bufsize_pw]		= "PW",		/* entry-passwd */
-	[bufsize_sp]		= "SP",		/* entry-shadow */
-	[bufsize_ua]		= "UA",		/* entry-userattr */
-	[bufsize_gr]		= "GR",		/* entry-group */
-	[bufsize_pj]		= "PJ",		/* entry-project */
-	[bufsize_pr]		= "PR",		/* entry-protocol */
-	[bufsize_nw]		= "NW",		/* entry-network */
-	[bufsize_ho]		= "HO",		/* entry-host */
-	[bufsize_sv]		= "SV",		/* entry-servicet */
-	[bufsize_fs]		= "FS",		/* filesystem-type */
-	[bufsize_sn]		= "SN",		/* symbol-name */
-	[bufsize_zn]		= "ZN",		/* time-zone-name abbr */
-	[bufsize_zi]		= "ZI",		/* zone-info-name */
-	[bufsize_mailaddr]	= "MAILADDR",	/* mail-address */
-	[bufsize_overlast]	= nullptr
-} ; /* end array (bufsizes) */
+/* local structures */
+
+namespace {
+    struct bufsizer {
+	cchar	*names[bufsize_overlast + 1] ;
+	consteval void mknames() noex ;
+	consteval bufsizer() noex {
+	    mknames() ;
+	} ;
+    } ; /* end struct (bufsizer) */
+    struct initer {
+	initer() noex ;
+    } ; /* end struct (initer) */
+} /* end namespace */
+
+consteval void bufsizer::mknames() noex {
+    	cchar **n = names ;
+	n[bufsize_ps]		= "PS" ;	/* page-size */
+	n[bufsize_mi]		= "MI" ;	/* max-pid */
+	n[bufsize_ma]		= "MA" ;	/* max-args */
+	n[bufsize_mn]		= "MN" ;	/* max-name */
+	n[bufsize_mp]		= "MP" ;	/* max-path */
+	n[bufsize_ml]		= "ML" ;	/* max-line */
+	n[bufsize_mm]		= "MM" ;	/* max-message */
+	n[bufsize_nn]		= "NN" ;	/* node-name */
+	n[bufsize_hn]		= "HN" ;	/* host-name */
+	n[bufsize_un]		= "UN" ;	/* user-name */
+	n[bufsize_gn]		= "GN" ;	/* group-name */
+	n[bufsize_pn]		= "PN" ;	/* project-name */
+	n[bufsize_pw]		= "PW" ;	/* entry-passwd */
+	n[bufsize_sp]		= "SP" ;	/* entry-shadow */
+	n[bufsize_ua]		= "UA" ;	/* entry-userattr */
+	n[bufsize_gr]		= "GR" ;	/* entry-group */
+	n[bufsize_pj]		= "PJ" ;	/* entry-project */
+	n[bufsize_pr]		= "PR" ;	/* entry-protocol */
+	n[bufsize_nw]		= "NW" ;	/* entry-network */
+	n[bufsize_ho]		= "HO" ;	/* entry-host */
+	n[bufsize_sv]		= "SV" ;	/* entry-servicet */
+	n[bufsize_fs]		= "FS" ;	/* filesystem-type */
+	n[bufsize_sn]		= "SN" ;	/* symbol-name */
+	n[bufsize_zn]		= "ZN" ;	/* time-zone-name abbr */
+	n[bufsize_zi]		= "ZI" ;	/* zone-info-name */
+	n[bufsize_mailaddr]	= "MAILADDR" ;	/* mail-address */
+	n[bufsize_overlast]	= nullptr ;
+} /* end method (bufsizer::operator) */
+
+
+/* exported variables */
+
+cpcchar			*bufsizenames ;
+
+constexpr bufsizer	bufsize_data ;
+
+static initer		initer_data ;
+
+
+/* exported subroutines */
+
+
+/* local subroutines */
+
+initer::initer() noex {
+	bufsizenames = bufsize_data.names ;
+} ; /* end ctor */
 
 
