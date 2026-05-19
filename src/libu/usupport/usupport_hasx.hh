0d947d890157964f41daf08206518fc9d7518a89
David A.D. Morano <david.a.morano@gmail.com>
2026-05-19 00:59:20 -0400

    update

--- src/libu/usupport/usupport_hasx.hh
+++ src/libu/usupport/usupport_hasx.hh
@@ -6,6 +6,13 @@
 /* version %I% last-modified %G% */
 
 
+/* revision history:
+
+	= 1998-10-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	USUPPORTHASX_INCLUDE
