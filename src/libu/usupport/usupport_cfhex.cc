b9db61563d3966a9bcdf0d0d4c1c108fa2b49b00
David A.D. Morano <david.a.morano@gmail.com>
2026-05-20 01:54:02 -0400

    restore

---
+++ src/libu/usupport/usupport_cfhex.cc
@@ -0,0 +1,117 @@
+/* usupport_cfhex SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* UNIX® kernel support subroutines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-26, David A­D­ Morano
+	This was first written to give a little bit to UNIX® what
+	we have in our own circuit-pack OSes!
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	cfhex
+
+	Description:
+	Convert from a decimal representation to its binary integer.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<intsat.h>
+#include	<stdintx.h>
+#include	<strtox.h>
+#include	<strnul.hh>
+#include	<localmisc.h>		/* |DECBUFLEN| */
+
+#include	"usupport_cfhex.hh"
+
+
+/* local defines */
+
+#define	CFBASE		16		/* Convert-From Base */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+namespace libu {
+    template<typename T>
+    local int cfhexx(T (*cfx)(cc *,char **,int),cc *sp,int sl,T *rp) noex {
+	[[maybe_unused]] char	*endp{} ; /* <- unused */
+	cint		b = CFBASE ;	/* conversion number base */
+	int		rs = SR_FAULT ;
+	if (sp) {
+	    T		v{} ;
+	    strnul	str(sp,sl) ;
+	    errno = 0 ;
+	    v = cfx(str,&endp,b) ;
+	    if (rp) *rp = v ;
+	    if (errno) {
+		rs = (- errno) ;
+	    } else {
+		rs = intsat(v) ;
+	    }
+	} /* end if (non-null) */
+	return rs ;
+    } /* end subroutine-template (cfhexx) */
+    int cfhex(cchar *sp,int sl,int *rp)		noex {
+	return cfhexx(strtoxi,sp,sl,rp) ;
+    }
+    int cfhex(cchar *sp,int sl,long *rp)	noex {
+	return cfhexx(strtoxl,sp,sl,rp) ;
+    }
+    int cfhex(cchar *sp,int sl,longlong *rp)	noex {
+	return cfhexx(strtoxll,sp,sl,rp) ;
+    }
+    int cfhex(cchar *sp,int sl,uint *rp)	noex {
+	return cfhexx(strtoxui,sp,sl,rp) ;
+    }
+    int cfhex(cchar *sp,int sl,ulong *rp)	noex {
+	return cfhexx(strtoxul,sp,sl,rp) ;
+    }
+    int cfhex(cchar *sp,int sl,ulonglong *rp)	noex {
+	return cfhexx(strtoxull,sp,sl,rp) ;
+    }
+} /* end namespace (libu) */
+
+
+/* local subroutines */
+
+
