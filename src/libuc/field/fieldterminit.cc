80f3e46d76b5093614c0aea2ecbb1c8b78b43b2d
David A.D. Morano <david.a.morano@gmail.com>
2026-05-20 01:43:44 -0400

    restore

---
+++ src/libuc/field/fieldterminit.cc
@@ -0,0 +1,80 @@
+/* fieldterminit SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* initialize a field-terminator block */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-05-01, David A­D­ Morano
+	This code module was originally written in VAX assembly
+	(back around 1980 or so). It was then written into C-language
+	perhaps around 1983 (at AT&T Bell Laboratories).  This
+	(below) is a new incarnation based on that older C-language
+	code.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	fieldterminit
+
+	Description:
+	This object facilitates initializeing a field-terminator-block
+	given a c-string of characters.
+
+	Synopsis:
+	constexpr fieldterminit	qterms("...xxx...") ;
+
+	Returns:
+	There are no returns as this object substitutes as a C++ type.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"fieldterminit.hh"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
