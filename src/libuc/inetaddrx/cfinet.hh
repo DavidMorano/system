9537b59e01bbf810c7e214fbb9307f614e1616ea
David A.D. Morano <david.a.morano@gmail.com>
2026-05-21 01:07:28 -0400

    restore

---
+++ src/libuc/inetaddrx/cfinet.hh
@@ -0,0 +1,34 @@
+/* cfinet HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* Convert-From-Internet-Address */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CFINET_INCLUDE
+#define	CFINET_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+extern int cfinet(cchar *,int,uint *) noex ;
+
+
+#endif /* __cplusplus */
+#endif /* CFINET_INCLUDE */
+
+
