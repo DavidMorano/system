28e494ac8806fe8b13c0441ed9303b8872733c25
David A.D. Morano <david.a.morano@gmail.com>
2026-05-21 01:17:20 -0400

    restore

---
+++ src/libu/usupport/usupport_cfinet.hh
@@ -0,0 +1,36 @@
+/* usupport_cfinet HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* Convert-From-Decimal-Time */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USUPPORTCFINET_INCLUDE
+#define	USUPPORTCFINET_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+namespace libu {
+    extern int cfinet(cchar *,int,uint *) noex ;
+} /* end namespace (libu) */
+
+
+#endif /* __cplusplus */
+#endif /* USUPPORTCFINET_INCLUDE */
+
+
