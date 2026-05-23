67c67d960c61ecce446c6bb5801049efddb3ac7b
David A.D. Morano <david.a.morano@gmail.com>
2026-05-22 23:17:49 -0400

    restore

---
+++ src/libu/usupport/vstrorders.cc
@@ -0,0 +1,72 @@
+/* vstrorders SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* these are values (constants) for specifying comparison order */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-09-10, David A­D­ Morano
+	This program was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Group:
+	vstrorders
+
+	Description:
+
+	Arguments:
+
+	Returns:
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD |nullptr_t| */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"vstrorders.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
