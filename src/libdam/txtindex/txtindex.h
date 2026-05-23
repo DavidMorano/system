5d7b26bc2f32fd36cbc624503f023b0eae3ae46e
David A.D. Morano <david.a.morano@gmail.com>
2026-05-23 00:13:36 -0400

    update

--- src/libdam/txtindex/txtindex.h
+++ src/libdam/txtindex/txtindex.h
@@ -19,7 +19,7 @@
 #define	TXTINDEX_INCLUDE
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
