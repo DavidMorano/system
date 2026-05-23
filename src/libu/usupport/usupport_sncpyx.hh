482ae1d32976322c8ee1814ee42e1929dee27864
David A.D. Morano <david.a.morano@gmail.com>
2026-05-22 23:22:47 -0400

    update

--- src/libu/usupport/usupport_sncpyx.hh
+++ src/libu/usupport/usupport_sncpyx.hh
@@ -29,29 +29,29 @@
 namespace libu {
     extern int sncpyx(char *,int,int,...) noex ;
     inline int sncpy1(char *dp,int dl,cc *s1) noex {
-	return sncpyx(dp,dl,1,s1) ;
+	return libu::sncpyx(dp,dl,1,s1) ;
     }
     inline int sncpy2(char *dp,int dl,cc *s1,cc *s2) noex {
-	return sncpyx(dp,dl,2,s1,s2) ;
+	return libu::sncpyx(dp,dl,2,s1,s2) ;
     }
     inline int sncpy3(char *dp,int dl,cc *s1,cc *s2,cc *s3) noex {
-	return sncpyx(dp,dl,3,s1,s2,s3) ;
+	return libu::sncpyx(dp,dl,3,s1,s2,s3) ;
     }
     inline int sncpy4(char *dp,int dl,cc *s1,cc *s2,cc *s3,cc *s4) noex {
-	return sncpyx(dp,dl,4,s1,s2,s3,s4) ;
+	return libu::sncpyx(dp,dl,4,s1,s2,s3,s4) ;
     }
     inline int sncpy5(char *dp,int dl,cc *s1,cc *s2,cc *s3,cc *s4,
 	    cc *s5) noex {
-	return sncpyx(dp,dl,5,s1,s2,s3,s4,s5) ;
+	return libu::sncpyx(dp,dl,5,s1,s2,s3,s4,s5) ;
     }
     inline int sncpy6(char *dp,int dl,cc *s1,cc *s2,cc *s3,cc *s4,cc *s5,
 	    cc *s6) noex {
-	return sncpyx(dp,dl,6,s1,s2,s3,s4,s5,s6) ;
+	return libu::sncpyx(dp,dl,6,s1,s2,s3,s4,s5,s6) ;
     }
     template<typename ... Args>
     inline int sncpy(char *dp,int dl,Args ... args) noex {
         cint	na = npack(Args) ;
-        return sncpyx(dp,dl,na,args ...) ;
+        return libu::sncpyx(dp,dl,na,args ...) ;
     }
 } /* end namespace (libu) */
 
