f18b43077c3081d4a4d075f8bb615be18ed10564
David A.D. Morano <david.a.morano@gmail.com>
2026-05-22 23:18:14 -0400

    restore

---
+++ src/libu/usupport/usupport_strdcpyxw.hh
@@ -0,0 +1,71 @@
+/* usupport_strdcpyxw HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* USUPPORT string-copy variant */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USUPPORTSTRDCPYXW_INCLUDE
+#define	USUPPORTSTRDCPYXW_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+namespace libu {
+    extern char *strdcpyxw(char *,int,int,...) noex ;
+} /* end namespace */
+
+namespace libu {
+    extern char *strdcpy1w(char *,int,cc *,int) noex ;
+    extern char *strdcpy2w(char *,int,cc *,cc *,int) noex ;
+    extern char *strdcpy3w(char *,int,cc *,cc *,cc *,int) noex ;
+    extern char *strdcpy4w(char *,int,cc *,cc *,cc *,cc *,int) noex ;
+    extern char *strdcpy5w(char *,int,cc *,cc *,cc *,cc *,cc *,int) noex ;
+    extern char *strdcpy6w(char *,int,cc *,cc *,cc *,cc *,cc *,cc *,int) noex ;
+} /* end namespace */
+
+namespace libu {
+    inline char *strdcpyw(char *dp,int dl,cc *s1,int sl) noex {
+	return libu::strdcpyxw(dp,dl,1,s1,sl) ;
+    }
+    inline char *strdcpyw(char *dp,int dl,cc *s1,cc *s2,int sl) noex {
+	return libu::strdcpyxw(dp,dl,2,s1,s2,sl) ;
+    }
+    inline char *strdcpyw(char *dp,int dl,cc *s1,cc *s2,cc *s3,int sl) noex {
+	return libu::strdcpyxw(dp,dl,3,s1,s2,s3,sl) ;
+    }
+    inline char *strdcpyw(char *dp,int dl,cc *s1,cc *s2,cc *s3,cc *s4,
+	    	int sl) noex {
+	return libu::strdcpyxw(dp,dl,4,s1,s2,s3,s4,sl) ;
+    }
+    inline char *strdcpyw(char *dp,int dl,cc *s1,cc *s2,cc *s3,cc *s4,
+		cc *s5,int sl) noex {
+	return libu::strdcpyxw(dp,dl,5,s1,s2,s3,s4,s5,sl) ;
+    }
+    inline char *strdcpyw(char *dp,int dl,cc *s1,cc *s2,cc *s3,cc *s4,
+		cc *s5,cc *s6,int sl) noex {
+	return libu::strdcpyxw(dp,dl,6,s1,s2,s3,s4,s5,s6,sl) ;
+    }
+} /* end namespace */
+
+
+#endif /* __cplusplus */
+#endif /* USUPPORTSTRDCPYXW_INCLUDE */
+
+
