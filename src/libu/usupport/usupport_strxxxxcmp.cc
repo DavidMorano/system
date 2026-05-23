cd0fa08d7913be394db5f98e325c3458fc518f49
David A.D. Morano <david.a.morano@gmail.com>
2026-05-22 23:17:19 -0400

    restore

---
+++ src/libu/usupport/usupport_strxxxxcmp.cc
@@ -0,0 +1,209 @@
+/* usuport_strxxxxcmp SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* USUPPORT compare c-strings w/ various case insensitivities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-09-10, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	strbasecmp
+	strcasecmp
+	strfoldcmp
+
+	Description:
+	This subroutine performs a c-string comparisonn using folded
+	case.  This is almost identical to |strncasecmp(3c)| except
+	that the comparison is done using folded case.
+
+	Synopsis:
+	int strbasecmp(cchar *e1p,cchar *e2p) noex
+	int strcasecmp(cchar *e1p,cchar *e2p) noex
+	int strfoldcmp(cchar *e1p,cchar *e2p) noex
+
+	Arguments:
+	e1p		first string
+	e2p		second string
+
+	Returns:
+	>0		first c-string is greater than the second
+	==0		both c-strings are equal
+	<0		first c-string is less than the second
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<strings.h>		/* CSTD |strcasecmp(3c)| */
+#include	<cstddef>		/* CSTD |nullptr_t| */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD |strcmp(3c)| */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<mkchar.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+#include	<cmporders.h>		/* LIBU */
+
+#include	"usupport_strxxxxcmp.hh"
+
+import ureserve ;			/* |ischarx(3u)| */
+
+/* local defines */
+
+#define	TOBC(ch)	char_tobc(ch)
+#define	TOLC(ch)	char_tolc(ch)
+#define	TOUC(ch)	char_touc(ch)
+#define	TOFC(ch)	char_tofc(ch)
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+typedef cmporders	co ;
+
+extern "C" {
+    typedef int (*strcmp_f)(cchar *,cchar *) noex ;
+}
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+local int strxcmp(strcmp_f fun,cchar *s1,cchar *s2,co fo) noex {
+    	int		rc = 0 ;
+	if (s1 || s2) {
+	    rc = +1 ;
+	    if (s1) {
+		rc = -1 ;
+		if (s2) {
+		    if (fo) {
+		        rc = (- fun(s1,s2)) ;
+		    } else {
+		        rc = (+ fun(s1,s2)) ;
+		    }
+		}
+	    }
+	}
+	return rc ;
+} /* end subroutine (strxcmp) */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+namespace libu {
+    int strbasecmp(cchar *s1,cchar *s2) noex {
+    	int		rc = 0 ;
+	if (s1 && s2) {
+	    rc = strcmp(s1,s2) ;
+	}
+	return rc ;
+    } /* end subroutine (strbasecmp) */
+    int strcasecmp(cchar *s1,cchar *s2) noex {
+	int		rc = 0 ;
+	if (s1 && s2) {
+	    while (rc == 0) {
+	        cint ch1 = TOUC(*s1++) ;
+	        cint ch2 = TOUC(*s2++) ;
+	        rc = ch1 - ch2 ;
+	        if (!ch1 || !ch2) break ;
+	    } /* end while */
+	} /* end if (non-null) */
+	return rc ;
+    } /* end subroutine (strcasecmp) */
+    int strfoldcmp(cchar *s1,cchar *s2) noex {
+	int		rc = 0 ;
+	if (s1 && s2) {
+	    while (rc == 0) {
+	        cint ch1 = TOFC(*s1++) ;
+	        cint ch2 = TOFC(*s2++) ;
+	        rc = ch1 - ch2 ;
+	        if (!ch1 || !ch2) break ;
+	    } /* end while */
+	} /* end if (non-null) */
+	return rc ;
+    } /* end subroutine (strfoldcmp) */
+} /* end namespace (libu) */
+
+namespace libu {
+    int strbasecmpo(cchar *s1,cchar *s2) noex {
+	return (+ strbasecmp(s1,s2)) ;
+    }
+    int strbasecmpr(cchar *s1,cchar *s2) noex {
+	return (- strbasecmp(s1,s2)) ;
+}
+} /* end namespace (libu) */
+
+namespace libu {
+    int strcasecmpo(cchar *s1,cchar *s2) noex {
+	return (+ strcasecmp(s1,s2)) ;
+    }
+    int strcasecmpr(cchar *s1,cchar *s2) noex {
+	return (- strcasecmp(s1,s2)) ;
+    }
+} /* end namespace (libu) */
+
+namespace libu {
+    int strfoldcmpo(cchar *s1,cchar *s2) noex {
+	return (+ strfoldcmp(s1,s2)) ;
+    }
+    int strfoldcmpr(cchar *s1,cchar *s2) noex {
+	return (- strfoldcmp(s1,s2)) ;
+    }
+} /* end namespace (libu) */
+
+namespace libu {
+    int strxbasecmpo(cchar *s1,cchar *s2) noex {
+    	return strxcmp(strbasecmp,s1,s2,cmporder_obverse) ;
+    }
+    int strxbasecmpr(cchar *s1,cchar *s2) noex {
+    	return strxcmp(strbasecmp,s1,s2,cmporder_reverse) ;
+    }
+} /* end namespace (libu) */
+
+namespace libu {
+    int strxcasecmpo(cchar *s1,cchar *s2) noex {
+    	return strxcmp(strcasecmp,s1,s2,cmporder_obverse) ;
+    }
+    int strxcasecmpr(cchar *s1,cchar *s2) noex {
+    	return strxcmp(strcasecmp,s1,s2,cmporder_reverse) ;
+    }
+} /* end namespace (libu) */
+
+namespace libu {
+    int strxfoldcmpo(cchar *s1,cchar *s2) noex {
+    	return strxcmp(strfoldcmp,s1,s2,cmporder_obverse) ;
+    }
+    int strxfoldcmpr(cchar *s1,cchar *s2) noex {
+    	return strxcmp(strfoldcmp,s1,s2,cmporder_reverse) ;
+    }
+} /* end namespace (libu) */
+
+
