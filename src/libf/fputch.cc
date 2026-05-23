cbc3877264794abf557a12e3b8b00881c5ce36b0
David A.D. Morano <david.a.morano@gmail.com>
2026-05-23 00:14:20 -0400

    update

--- src/libf/fputch.cc
+++ src/libf/fputch.cc
@@ -27,16 +27,16 @@
 ******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<climits>		/* |UCHAR_MAX| */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<cstdio>
-#include	<cstring>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
-#include	<intsat.h>
-#include	<localmisc.h>
+#include	<climits>		/* STD-C |UCHAR_MAX| */
+#include	<cstddef>		/* STD-C |nullptr_t| */
+#include	<cstdlib>		/* STD-C */
+#include	<cstdio>		/* STD-C */
+#include	<cstring>		/* STD-C */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<intsat.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"libf.h"
 
