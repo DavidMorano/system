50ffc5be13d32c99cd653d4febfaa3c9f3220d09
David A.D. Morano <david.a.morano@gmail.com>
2026-05-25 01:12:20 -0400

    update

--- src/libu/usysconf.h
+++ src/libu/usysconf.h
@@ -20,8 +20,9 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<sysconfcmds.h>		/* LIBU */
 
 
 EXTERNC_begin
