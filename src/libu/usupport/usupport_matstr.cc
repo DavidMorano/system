5ef1f8d2b05801ff077ebcc5c3b67c2062287625
David A.D. Morano <david.a.morano@gmail.com>
2026-05-25 01:13:38 -0400

    update

--- src/libu/usupport/usupport_matstr.cc
+++ src/libu/usupport/usupport_matstr.cc
@@ -77,9 +77,8 @@
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
 #include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<usysbase.h>
+#include	<usupport.h>
 #include	<localmisc.h>
 
 #include	"usupport_toxc.hh"
