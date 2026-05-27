05f1cc2fc0a0906ba3d9eaa2c5b0f8025648990a
David A.D. Morano <david.a.morano@gmail.com>
2026-05-27 00:56:51 -0400

    restore

---
+++ src/libu/utypedefs_mints.h
@@ -0,0 +1,37 @@
+/* utypedefs_mints HEADER (Integers-Mutable) */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* virtual-system definitions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+	= 2017-08-01, David A­D­ Morano
+	Updated for lack of interfaces in MacOS Darwin
+
+*/
+
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UTYPEDEFSMINTS_INCLUDE
+#define	UTYPEDEFSMINTS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<usysnative.h>		/* useful native system headers */
+#include	<clanguage.h>		/* <- necessary inclusion */
+#include	<stdintx.h>		/* extended integer types */
+#include	<syspredefined.h>	/* system pre-defined types */
+
+#include	<utypedefs_msints.h>
+#include	<utypedefs_muints.h>
+
+
+#endif /* UTYPEDEFSMINTS_INCLUDE */
+
+
