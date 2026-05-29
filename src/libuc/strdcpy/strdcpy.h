ae5a491e10727ef8e6d40a3fe3bbfc87e3caaf09
David A.D. Morano <david.a.morano@gmail.com>
2026-05-29 00:46:58 -0400

    update

--- src/libuc/strdcpy/strdcpy.h
+++ src/libuc/strdcpy/strdcpy.h
@@ -39,24 +39,24 @@ EXTERNC_end
 
 #ifdef	__cplusplus
 
-static inline char *strdcpy(char *dp,int dl,cc *s1,int sl) noex {
+local inline char *strdcpy(char *dp,int dl,cc *s1,int sl) noex {
 	return strdcpyxw(dp,dl,1,s1,sl) ;
 }
-static inline char *strdcpy(char *dp,int dl,cc *s1,cc *s2,int sl) noex {
+local inline char *strdcpy(char *dp,int dl,cc *s1,cc *s2,int sl) noex {
 	return strdcpyxw(dp,dl,2,s1,s2,sl) ;
 }
-static inline char *strdcpy(char *dp,int dl,cc *s1,cc *s2,cc *s3,int sl) noex {
+local inline char *strdcpy(char *dp,int dl,cc *s1,cc *s2,cc *s3,int sl) noex {
 	return strdcpyxw(dp,dl,3,s1,s2,s3,sl) ;
 }
-static inline char *strdcpy(char *dp,int dl,cc *s1,cc *s2,cc *s3,
+local inline char *strdcpy(char *dp,int dl,cc *s1,cc *s2,cc *s3,
 		cc *s4,int sl) noex {
 	return strdcpyxw(dp,dl,4,s1,s2,s3,s4,sl) ;
 }
-static inline char *strdcpy(char *dp,int dl,cc *s1,cc *s2,cc *s3,cc *s4,
+local inline char *strdcpy(char *dp,int dl,cc *s1,cc *s2,cc *s3,cc *s4,
 		cc *s5,int sl) noex {
 	return strdcpyxw(dp,dl,5,s1,s2,s3,s4,s5,sl) ;
 }
-static inline char *strdcpy(char *dp,int dl,cc *s1,cc *s2,cc *s3,cc *s4,
+local inline char *strdcpy(char *dp,int dl,cc *s1,cc *s2,cc *s3,cc *s4,
 		cc *s5,cc *s6,int sl) noex {
 	return strdcpyxw(dp,dl,6,s1,s2,s3,s4,s5,s6,sl) ;
 }
