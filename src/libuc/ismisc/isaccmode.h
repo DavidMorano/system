ae8d41ae7ed6f063419e528e708d56872411230c
David A.D. Morano <david.a.morano@gmail.com>
2026-05-30 00:12:53 -0400

    restore

---
+++ src/libuc/ismisc/isaccmode.h
@@ -0,0 +1,39 @@
+/* isaccmode HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* does a file access-mode have a property? */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	ISACCMODE_INCLUDE
+#define	ISACCMODE_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern bool	isaccmode_readable(int) noex ;
+extern bool	isaccmode_writable(int) noex ;
+
+EXTERNC_end
+
+
+#endif /* ISACCMODE_INCLUDE */
+
+
