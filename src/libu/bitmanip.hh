ce219c8177d3f6750786048b9ccf11db4e8e4c7a
David A.D. Morano <david.a.morano@gmail.com>
2026-05-30 00:10:55 -0400

    restore

---
+++ src/libu/bitmanip.hh
@@ -0,0 +1,34 @@
+/* bitmanip HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* bit manipulations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-15, David A­D­ Morano
+	This module was originally written.
+
+	= 2017-08-01, David A­D­ Morano
+	Updated for lack of interfaces in MacOS Darwin
+
+*/
+
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	BITMANIP_INCLUDE
+#define	BITMANIP_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+#endif /* BITMANIP_INCLUDE */
+
+
