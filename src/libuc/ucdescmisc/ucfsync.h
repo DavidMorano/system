68d6beedbda97df7d22c51f9ae91c5b0feede332
David A.D. Morano <david.a.morano@gmail.com>
2026-05-30 00:13:34 -0400

    restore

---
+++ src/libuc/ucdescmisc/ucfsync.h
@@ -0,0 +1,37 @@
+/* ucfsync HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get random data from the system */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCFSYNC_INCLUDE
+#define	UCFSYNC_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int	uc_fsync	(int) noex ;
+extern int	uc_fsyncdata	(int) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCFSYNC_INCLUDE */
+
+
