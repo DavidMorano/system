d8f9ceb2b80e0f08ad1dd01c93bfd1aa9ae9c4eb
David A.D. Morano <david.a.morano@gmail.com>
2026-05-30 00:10:28 -0400

    restore

---
+++ src/libb/linex.h
@@ -0,0 +1,39 @@
+/* linex HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* text line management */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	LINEX_INCLUDE
+#define	LINEX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+#include	<linebuffer.h>
+#include	<linecenter.h>
+#include	<lineclean.h>
+#include	<linefilter.h>
+#include	<linefold.h>
+#include	<linehist.h>
+
+
+#endif /* LINEX_INCLUDE */
+
+
