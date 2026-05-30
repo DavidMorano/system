f1f55e347719ba3ff59df8d6b701df2ee5b34cd5
David A.D. Morano <david.a.morano@gmail.com>
2026-05-30 00:11:12 -0400

    restore

---
+++ src/libu/clanguage_concepts.h
@@ -0,0 +1,35 @@
+/* clanguage_concepts HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* C-language defines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-15, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CLANGUAGECONCEPTS_INCLUDE
+#define	CLANGUAGECONCEPTS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+
+
+#ifdef	__cplusplus
+#ifndef	integral_signed
+#define	integral_signed		signed_integral
+#define	integral_unsigned	unsigned_integral
+#endif
+#endif /* __cplusplus */
+
+
+#endif /* CLANGUAGECONCEPTS_INCLUDE */
+
+
