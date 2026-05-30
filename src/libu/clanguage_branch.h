af01f59d51a923b26171c604620308416d63878b
David A.D. Morano <david.a.morano@gmail.com>
2026-05-30 00:11:43 -0400

    restore

---
+++ src/libu/clanguage_branch.h
@@ -0,0 +1,36 @@
+/* clanguage_branch HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* C-language defines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-15, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CLANGUAGEBRANCH_INCLUDE
+#define	CLANGUAGEBRANCH_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+
+
+#ifndef	ylikely
+#define	ylikely			[[likely]]
+#endif /* ylikely */
+
+#ifndef	nlikely
+#define	nlikely			[[unlikely]]
+#endif /* nlikely */
+
+
+#endif /* CLANGUAGEBRANCH_INCLUDE */
+
+
