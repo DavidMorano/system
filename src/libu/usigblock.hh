6bda0d55e8f5f4ff85204612d1e428d264c73e3a
David A.D. Morano <david.a.morano@gmail.com>
2026-05-31 00:16:58 -0400

    restore

---
+++ src/libu/usigblock.hh
@@ -0,0 +1,34 @@
+/* usigblock HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* perform some signal operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USIGBLOCK_INCLUDE
+#define	USIGBLOCK_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+#include	<usysrets.h>
+
+
+#endif /* __cplusplus */
+#endif /* USIGBLOCK_INCLUDE */
+
+
