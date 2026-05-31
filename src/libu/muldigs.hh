2408c521087a4561a19856f6815af9b9553075dc
David A.D. Morano <david.a.morano@gmail.com>
2026-05-31 00:18:32 -0400

    restore

---
+++ src/libu/muldigs.hh
@@ -0,0 +1,33 @@
+/* muldigs HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* subroutine interfaces to compiler built-in instrinsics */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MULDIGS_INCLUDE
+#define	MULDIGS_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+#endif /* __cplusplus */
+#endif /* MULDIGS_INCLUDE */
+
+
