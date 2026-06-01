8a076387b9b84abedb090658606e0618e370dd62
David A.D. Morano <david.a.morano@gmail.com>
2026-06-01 01:30:55 -0400

    update

--- src/libuc/conallof.cc
+++ src/libuc/conallof.cc
@@ -41,16 +41,16 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<climits>		/* |UCHAR_MAX| */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<bitset>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<dupstr.h>
-#include	<six.h>			/* |sichr(3uc)| */
-#include	<mkchar.h>
-#include	<localmisc.h>
+#include	<climits>		/* CSTD |UCHAR_MAX| */
+#include	<cstddef>		/* CSTD |nullptr_t| */
+#include	<cstdlib>		/* CSTD */
+#include	<bitset>		/* C++STD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<dupstr.h>		/* LIBUC */
+#include	<six.h>			/* LIBUC |sichr(3uc)| */
+#include	<mkchar.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"conallof.h"
 
@@ -81,10 +81,14 @@ using std::bitset ;			/* type */
 
 /* forward references */
 
+local int conallof_bitset(cchar *,int,cchar *) noex ;
+local int conallof_dupstr(cchar *,int,cchar *) noex ;
+
 
 /* local variables */
 
-cint		nchars = (UCHAR_MAX + 1) ;
+cint		nchars		= (UCHAR_MAX + 1) ;
+cbool		f_bitset	= CF_BITSET ;
 
 
 /* exported variables */
@@ -92,7 +96,27 @@ cint		nchars = (UCHAR_MAX + 1) ;
 
 /* exported subroutines */
 
-#if	CF_BITSET
+int conallof(cchar *sp,int sl,cchar *tstr) noex {
+	int		rs = SR_FAULT ;
+	int		f = true ;
+	if (sp && tstr) {
+	    rs = SR_INVALID ;
+	    if (tstr[0]) {
+		if_constexpr (f_bitset) {
+		    rs = conallof_bitset(sp,sl,tstr) ;
+		    f = rs ;
+		} else {
+		    rs = conallof_dupstr(sp,sl,tstr) ;
+		    f = rs ;
+		} /* end if */
+	    } /* end if (valid) */
+	} /* end if (non-null) */
+	return (rs >= 0) ? f : rs ;
+}
+/* end subroutine (conallof) */
+
+
+/* local subroutines */
 
 namespace {
     struct bitsetx : bitset<nchars> {
@@ -100,34 +124,46 @@ namespace {
     } ; /* end struct (bitsetx) */
 } /* end namespace */
 
-int conallof(cchar *sp,int sl,cchar *tstr) noex {
-	int		rs = SR_FAULT ;
-	int		f = true ;
-	if (sp && tstr) {
-	    rs = SR_INVALID ;
-	    if (tstr[0]) {
-	        f = false ;
-	        if (bitsetx bs ; (rs = bs.load(tstr)) >= 0) {
-	            while (sl && *sp) {
-		        if (cint ch = mkchar(*sp) ; bs[ch]) {
-			    bs.reset(ch) ;
-			    f = bs.none() ;
-	                    if (f) break ;
-		        } /* end if */
-	                sp += 1 ;
-	                sl -= 1 ;
-	            } /* end while */
-	        } /* end if (bitsetx) */
-	    } /* end if (valid) */
-	} /* end if (non-null) */
+local int conallof_bitset(cchar *sp,int sl,cchar *tstr) noex {
+	int		rs ;
+	int		f = false ;
+	if (bitsetx bs ; (rs = bs.load(tstr)) >= 0) {
+	    for (int ch ; sl-- && ((ch = mkchar(*sp))) ; sp += 1) {
+	 	if (bs[ch]) {
+		    bs.reset(ch) ;
+		    f = bs.none() ;
+	            if (f) break ;
+		} /* end if */
+	    } /* end for */
+	} /* end if (bitsetx) */
 	return (rs >= 0) ? f : rs ;
-}
-/* end subroutine (conallof) */
+} /* end subroutine (conallof) */
+
+local int conallof_dupstr(cchar *sp,int sl,cchar *tstr) noex {
+	int		rs ;
+	int		rs1 ;
+	int		f = false ;
+	char		*bp{} ;
+	if (dupstr sd ; (rs = sd.start(tstr,-1,&bp)) >= 0) {
+	    for (int bl = rs ; sl-- && *sp ; sp += 1) {
+		if (int si ; (si = sichr(bp,bl,sp[0])) >= 0) {
+		    if (bl-- > 1) {
+			if (si < bl) bp[si] = bp[bl] ;
+		    }
+		    f = (bl == 0) ;
+	            if (f) break ;
+		} /* end if (sichr) */
+	    } /* end for */
+	    rs1 = sd.finish ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if (dupstr) */
+	return (rs >= 0) ? f : rs ;
+} /* end subroutine (conallof_dupstr) */
 
 int bitsetx::load(cchar *sp) noex {
 	int		rs = SR_OK ;
 	try {
-	    for (int ch ; (ch = mkchar(*sp++)) > 0 ; ) {
+	    for (int ch ; (ch = mkchar(*sp)) > 0 ; sp += 1) {
 		set(ch) ;
 	    } /* end for */
 	} catch (...) {
@@ -135,40 +171,5 @@ int bitsetx::load(cchar *sp) noex {
 	}
 	return rs ;
 } /* end method (bitsetx::load) */
-
-#else /* CF_BITSET */
-
-int conallof(cchar *sp,int sl,cchar *tstr) noex {
-	int		rs = SR_FAULT ;
-	int		rs1 ;
-	int		f = true ;
-	if (sp && tstr) {
-	    rs = SR_INVALID ;
-	    if (tstr[0]) {
-		char	*bp{} ;
-	        f = false ;
-	        if (dupstr sd ; (rs = sd.start(tstr,-1,&bp)) >= 0) {
-	            int	bl = rs ;
-	            while (sl && *sp) {
-		        if (int si ; (si = sichr(bp,bl,sp[0])) >= 0) {
-			    if (bl-- > 1) {
-			        if (si < bl) bp[si] = bp[bl] ;
-			    }
-		            f = (bl == 0) ;
-	                    if (f) break ;
-		        } /* end if (sichr) */
-	                sp += 1 ;
-	                sl -= 1 ;
-	            } /* end while */
-	            rs1 = sd.finish ;
-	            if (rs >= 0) rs = rs1 ;
-	        } /* end if (dupstr) */
-	    } /* end if (valid) */
-	} /* end if (non-null) */
-	return (rs >= 0) ? f : rs ;
-}
-/* end subroutine (conallof) */
-
-#endif /* CF_BITSET */
 
 
