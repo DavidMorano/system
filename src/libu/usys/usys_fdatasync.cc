851624ea3e20cc4f4dffad16ed096423c5611206
David A.D. Morano <david.a.morano@gmail.com>
2026-06-01 01:28:47 -0400

    update

--- src/libu/usys/usys_fdatasync.cc
+++ src/libu/usys/usys_fdatasync.cc
@@ -42,9 +42,9 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<unistd.h>
-#include	<cerrno>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
+#include	<cerrno>		/* CSTD */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
