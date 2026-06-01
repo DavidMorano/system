20667e9f8e4b1041ccec28a68db5f65400ec50fb
David A.D. Morano <david.a.morano@gmail.com>
2026-06-01 01:31:45 -0400

    update

--- src/libuc/filex/fileprintable.cc
+++ src/libuc/filex/fileprintable.cc
@@ -37,18 +37,20 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<uclibmem.h>
-#include	<ucdesc.h>
-#include	<filer.h>
-#include	<rmx.h>
-#include	<char.h>
-#include	<hasx.h>		/* |hasprintbad(3uc)| */
-#include	<localmisc.h>
+#include	<unistd.h>		/* POSIX */
+#include	<fcntl.h>		/* POSIX */
+#include	<cstddef>		/* CSTD |nullptr_t| */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<uclibmem.h>		/* LIBUC */
+#include	<ucopen.h>		/* LIBUC */
+#include	<ucdesc.h>		/* LIBUC */
+#include	<filer.h>		/* LIBUC */
+#include	<rmx.h>			/* LIBUC */
+#include	<char.h>		/* LIBUC */
+#include	<hasx.h>		/* LIBUC |hasprintbad(3uc)| */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"fileprintable.h"
 
@@ -57,12 +59,6 @@
 
 
 /* external subroutines */
-
-extern "C" {
-    extern int uc_open(cchar *,int,mode_t) noex ;
-    extern int uc_fstat(int,ustat *) noex ;
-    extern int uc_read(int,void *,int) noex ;
-}
 
 
 /* external variables */
