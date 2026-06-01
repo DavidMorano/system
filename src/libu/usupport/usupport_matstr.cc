438a352beded2a93234e33ea75c93edf7c28f438
David A.D. Morano <david.a.morano@gmail.com>
2026-06-01 01:26:18 -0400

    update

--- src/libu/usupport/usupport_matstr.cc
+++ src/libu/usupport/usupport_matstr.cc
@@ -74,8 +74,8 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<clanguage.h>
 #include	<usysbase.h>
 #include	<usupport.h>
