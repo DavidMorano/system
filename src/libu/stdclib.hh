673932ddbbaf40b231cc09369f34a0d9daee505a
David A.D. Morano <david.a.morano@gmail.com>
2026-06-01 01:24:35 -0400

    update

--- src/libu/stdclib.hh
+++ src/libu/stdclib.hh
@@ -21,12 +21,12 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
 
 
 namespace stdclib {
