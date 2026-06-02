6178996a57bbf3758475d9cb490bdd9164efe1e7
David A.D. Morano <david.a.morano@gmail.com>
2026-06-02 00:13:09 -0400

    update

--- src/libb/bwasteline.cc
+++ src/libb/bwasteline.cc
@@ -28,8 +28,8 @@
 ******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<clanguage.h>
 #include	<usysbase.h>
 #include	<localmisc.h>
