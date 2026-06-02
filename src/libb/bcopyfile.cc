58b70bfd540b7ba648620660a2e9a1420f56ba0b
David A.D. Morano <david.a.morano@gmail.com>
2026-06-02 00:09:34 -0400

    update

--- src/libb/bcopyfile.cc
+++ src/libb/bcopyfile.cc
@@ -42,7 +42,7 @@
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<unistd.h>		/* POSIX */
 #include	<fcntl.h>		/* POSIX */
-#include	<cstddef>		/* CSTD |nullptr_t| */
+#include	<cstddef>		/* CSTD */
 #include	<cstdlib>		/* CSTD */
 #include	<clanguage.h>		/* LIBU */
 #include	<usysbase.h>		/* LIBU */
