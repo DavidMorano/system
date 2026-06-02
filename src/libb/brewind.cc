10f0af918d89f8127c052b6b54c112fd6544cfd9
David A.D. Morano <david.a.morano@gmail.com>
2026-06-02 00:13:29 -0400

    update

--- src/libb/brewind.cc
+++ src/libb/brewind.cc
@@ -22,8 +22,8 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<clanguage.h>
 #include	<usysbase.h>
 #include	<localmisc.h>
