f319f0b7e22282cd6fb9cef91ef84a725368d26b
David A.D. Morano <david.a.morano@gmail.com>
2026-06-03 00:12:51 -0400

    update

--- src/libuc/mkx/mkaddr.h
+++ src/libuc/mkx/mkaddr.h
@@ -27,8 +27,8 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 EXTERNC_begin
