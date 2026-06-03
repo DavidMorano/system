a6957e081407ec4f32422746c5a1f191a1f49437
David A.D. Morano <david.a.morano@gmail.com>
2026-06-03 00:10:00 -0400

    update

--- src/libuc/hasx/haslead.cc
+++ src/libuc/hasx/haslead.cc
@@ -80,15 +80,15 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<six.h>			/* |sispanwht(3uc)| */
-#include	<mkchar.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<six.h>			/* LIBUC |sispanwht(3uc)| */
+#include	<mkchar.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"haslead.h"
 
