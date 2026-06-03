e5f030546b2083c96b9ce2e8df41719d9df76490
David A.D. Morano <david.a.morano@gmail.com>
2026-06-03 00:10:22 -0400

    update

--- src/libuc/hasx/hasnon.cc
+++ src/libuc/hasx/hasnon.cc
@@ -46,15 +46,15 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
 #include	<usysdefs.h>
 #include	<mkchar.h>
 #include	<char.h>
-#include	<localmisc.h>		/* |UC(3dam)| */
+#include	<localmisc.h>		/* LIBU |UC(3u)| */
 
 #include	"hasnon.h"
 
