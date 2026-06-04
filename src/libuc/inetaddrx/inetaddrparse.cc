f5f82ec374f079a9d51b33f24dbd696fa5707d45
David A.D. Morano <david.a.morano@gmail.com>
2026-06-04 00:45:29 -0400

    update

--- src/libuc/inetaddrx/inetaddrparse.cc
+++ src/libuc/inetaddrx/inetaddrparse.cc
@@ -43,12 +43,12 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<strn.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<strn.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"inetaddrparse.h"
 
