205f27525c11edf74e6635053884190741cbd5d2
David A.D. Morano <david.a.morano@gmail.com>
2026-06-05 01:31:50 -0400

    restore

---
+++ src/libuc/ctx/cvtfloat.hh
@@ -0,0 +1,34 @@
+/* cvtfloat HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* convert a float (|double| + |longdouble|) into a figit-string */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-01, David A­D­ Morano
+	This subroutine was written having been adapted (from
+	memory) from something I wrote back in the early 1980s (for
+	embedded work).  I had to write every ... last  ... thing
+	myself back in the old days.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CVTFLOAT_INCLUDE
+#define	CVTFLOAT_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* CVTFLOAT_INCLUDE */
+
+
