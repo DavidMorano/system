45503cdf93b9ac07d2ac49397351469657d1be0b
David A.D. Morano <david.a.morano@gmail.com>
2026-06-05 01:32:28 -0400

    restore

---
+++ src/libuc/fmtstr/fmtspec.hh
@@ -0,0 +1,31 @@
+/* fmtspec HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* header file for the FMTSPEC subroutine */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This file was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FMTSPEC_INCLUDE
+#define	FMTSPEC_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* FMTSPEC_INCLUDE */
+
+
