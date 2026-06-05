5e7b4826cdd0a99f805fb301429983da380f06b1
David A.D. Morano <david.a.morano@gmail.com>
2026-06-05 01:30:47 -0400

    restore

---
+++ src/libu/umods/constdiv.hh
@@ -0,0 +1,33 @@
+/* constdiv HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* constant division */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2002-10-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2002 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CONSTDIV_INCLUDE
+#define	CONSTDIV_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* CONSTDIV_INCLUDE */
+
+
