66addcc5fffeabdc57e21a4a1f506c0b70498047
David A.D. Morano <david.a.morano@gmail.com>
2026-06-05 01:31:11 -0400

    restore

---
+++ src/libu/umods/numbasedigs.hh
@@ -0,0 +1,33 @@
+/* numbasedigs HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* constant division */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2002-10-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2002 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	NUMBASEDIGS_INCLUDE
+#define	NUMBASEDIGS_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* NUMBASEDIGS_INCLUDE */
+
+
