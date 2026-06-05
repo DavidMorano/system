6b2cf67235c4754ba28580570c2ed942601d8cad
David A.D. Morano <david.a.morano@gmail.com>
2026-06-05 01:32:41 -0400

    restore

---
+++ src/libuc/fmtstr/fmtutil.hh
@@ -0,0 +1,31 @@
+/* fmtutil HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* header file for the FMTOPT subroutine */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This file was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FMTUTIL_INCLUDE
+#define	FMTUTIL_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* FMTUTIL_INCLUDE */
+
+
