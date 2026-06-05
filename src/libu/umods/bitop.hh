d99a17a8f81b72946aaba92f8202e21c5a133aa6
David A.D. Morano <david.a.morano@gmail.com>
2026-06-05 01:30:37 -0400

    restore

---
+++ src/libu/umods/bitop.hh
@@ -0,0 +1,33 @@
+/* bitop HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* convert a single character to a given case */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	BITOP_INCLUDE
+#define	BITOP_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* BITOP_INCLUDE */
+
+
