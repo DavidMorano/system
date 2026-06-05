e13e7b1c6f1c69c76826cda0fea7dc900c44ea00
David A.D. Morano <david.a.morano@gmail.com>
2026-06-05 01:28:11 -0400

    update

--- src/libu/conintx.cc
+++ src/libu/conintx.cc
@@ -27,11 +27,11 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<localmisc.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"conintx.hh"
 
