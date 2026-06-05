fe90ff14d0ae6064a0e7f19527343cfdad4ab4e7
David A.D. Morano <david.a.morano@gmail.com>
2026-06-05 01:32:36 -0400

    restore

---
+++ src/libuc/fmtstr/fmtstrdata.hh
@@ -0,0 +1,31 @@
+/* fmtstrdata HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* header file for the FMTOPT subroutine */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This file was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FMTSTRDATA_INCLUDE
+#define	FMTSTRDATA_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* FMTSTRDATA_INCLUDE */
+
+
