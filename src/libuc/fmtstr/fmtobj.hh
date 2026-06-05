5265b34cf89fed801cf69a7c6b72e3fa8c36d79e
David A.D. Morano <david.a.morano@gmail.com>
2026-06-05 01:32:23 -0400

    restore

---
+++ src/libuc/fmtstr/fmtobj.hh
@@ -0,0 +1,31 @@
+/* fmtobj HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* header file for the FMTOBJ subroutine */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-01, David A­D­ Morano
+	This file was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FMTOBJ_INCLUDE
+#define	FMTOBJ_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* CSTD */
+#include	<usysbase.h>		/* CSTD */
+
+
+#endif /* __cplusplus */
+#endif /* FMTOBJ_INCLUDE */
+
+
