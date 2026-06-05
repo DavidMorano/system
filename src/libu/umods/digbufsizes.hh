eb039f45d83a0b467c95a1794b1359456c9d9837
David A.D. Morano <david.a.morano@gmail.com>
2026-06-05 01:30:56 -0400

    restore

---
+++ src/libu/umods/digbufsizes.hh
@@ -0,0 +1,33 @@
+/* digbufsizes HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* constant division */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2002-10-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2002 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	DIGBUFSIZES_INCLUDE
+#define	DIGBUFSIZES_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* DIGBUFSIZES_INCLUDE */
+
+
