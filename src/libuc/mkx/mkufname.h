d0388d98b237f93af962c0b878a9c19ad33ac641
David A.D. Morano <david.a.morano@gmail.com>
2026-06-06 00:42:45 -0400

    update

--- src/libuc/mkx/mkufname.h
+++ src/libuc/mkx/mkufname.h
@@ -2,26 +2,26 @@
 /* charset=ISO8859-1 */
 /* lang=C20 */
 
-/* make a file rooted from the present-working-directory (PWD) */
+/* make a "user" file-name (the name is quite misleading) */
 /* version %I% last-modified %G% */
 
 
 /* revision history:
 
-	= 1998-11-01, David A­D­ Morano
+	= 1995-04-01, David A­D­ Morano
 	This subroutine was written for Rightcore Network Services.
 
 */
 
-/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1995 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	MKUFNAME_INCLUDE
 #define	MKUFNAME_INCLUDE
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 EXTERNC_begin
