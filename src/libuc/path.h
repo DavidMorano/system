dc24faf484a8aed20450587834b087fab45d547c
David A.D. Morano <david.a.morano@gmail.com>
2026-06-06 00:44:27 -0400

    update

--- src/libuc/path.h
+++ src/libuc/path.h
@@ -20,8 +20,8 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 #include	<pathadd.h>
 #include	<pathclean.h>
