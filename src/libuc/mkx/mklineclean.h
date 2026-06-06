ee1f0e8b6b8b9fece7dbaf68bbf475de0d48b48a
David A.D. Morano <david.a.morano@gmail.com>
2026-06-06 00:40:11 -0400

    update

--- src/libuc/mkx/mklineclean.h
+++ src/libuc/mkx/mklineclean.h
@@ -55,8 +55,8 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 #include	<linecleanopt.h>	/* line-cleaning options */
 
 
