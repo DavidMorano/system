bee7cca9935909df4e44b7f0c60fd22aa401bb46
David A.D. Morano <david.a.morano@gmail.com>
2026-06-07 01:04:16 -0400

    update

--- src/libuc/strn/strnxchr.h
+++ src/libuc/strn/strnxchr.h
@@ -20,8 +20,8 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* CSTD */
+#include	<usysbase.h>		/* CSTD */
 
 
 EXTERNC_begin
