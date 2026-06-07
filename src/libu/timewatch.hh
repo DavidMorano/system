76b8a35b8c8fdd5e78d3428c8087e41b7c4da8d1
David A.D. Morano <david.a.morano@gmail.com>
2026-06-07 01:02:39 -0400

    update

--- src/libu/timewatch.hh
+++ src/libu/timewatch.hh
@@ -35,12 +35,12 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<usupport.h>		/* |msleep(3u)| */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<usupport.h>		/* LIBU |msleep(3u)| */
 
 
 #ifndef	POLL_INTMULT
