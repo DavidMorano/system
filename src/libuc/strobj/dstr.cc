7baa501757fca13c5e2e7d97190e8b77d7d6ca5a
David A.D. Morano <david.a.morano@gmail.com>
2026-06-07 01:05:03 -0400

    update

--- src/libuc/strobj/dstr.cc
+++ src/libuc/strobj/dstr.cc
@@ -26,13 +26,13 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<uclibmem.h>
-#include	<strwcpy.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD |nullptr_t| */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<uclibmem.h>		/* LIBUC */
+#include	<strwcpy.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"dstr.h"
 
