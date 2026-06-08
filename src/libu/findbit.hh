08fe68c8e08b6a882a07de18c69fda871754709c
David A.D. Morano <david.a.morano@gmail.com>
2026-06-08 00:09:48 -0400

    restore

---
+++ src/libu/findbit.hh
@@ -0,0 +1,41 @@
+/* findbit HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* constants used in identifying internal-timers */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-09, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Group:
+	findbit
+
+	Description:
+	These subroutines find a specified bit in an integer.
+
+*******************************************************************************/
+
+#ifndef	FINDBIT_INCLUDE
+#define	FINDBIT_INCLUDE
+#ifdef	__cplusplus	/* C++ only! */
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus (C++ only) */
+#endif /* FINDBIT_INCLUDE */
+
+
