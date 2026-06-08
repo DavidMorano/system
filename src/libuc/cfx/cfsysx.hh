92a6af179afd31daddc6b272a9d26dac5c277f9d
David A.D. Morano <david.a.morano@gmail.com>
2026-06-08 00:03:42 -0400

    restore

---
+++ src/libuc/cfx/cfsysx.hh
@@ -0,0 +1,31 @@
+/* cfsysx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* convert a digit c-string to its integer value */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CFSYSX_INCLUDE
+#define	CFSYSX_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#endif /* __cplusplus */
+#endif /* CFSYSX_INCLUDE */
+
+
