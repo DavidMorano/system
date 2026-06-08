9d64a543a96b2c9eae2d7c307ca03cac69b1b0a1
David A.D. Morano <david.a.morano@gmail.com>
2026-06-08 00:14:38 -0400

    update

--- src/libuc/ctx/cta26.h
+++ src/libuc/ctx/cta26.h
@@ -20,42 +20,42 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<stdintx.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<stdintx.h>		/* LIBU */
 
 
 EXTERNC_begin
 
-extern int cta26i(char *,int,int,int,int) noex ;
-extern int cta26l(char *,int,int,int,long) noex ;
-extern int cta26ll(char *,int,int,int,longlong) noex ;
+extern int cta26i	(char *,int,int,int,int)		noex ;
+extern int cta26l	(char *,int,int,int,long)		noex ;
+extern int cta26ll	(char *,int,int,int,longlong)		noex ;
 
-extern int cta26ui(char *,int,int,int,uint) noex ;
-extern int cta26ul(char *,int,int,int,ulong) noex ;
-extern int cta26ull(char *,int,int,int,ulonglong) noex ;
+extern int cta26ui	(char *,int,int,int,uint)		noex ;
+extern int cta26ul	(char *,int,int,int,ulong)		noex ;
+extern int cta26ull	(char *,int,int,int,ulonglong)		noex ;
 
 EXTERNC_end
 
 #ifdef	__cplusplus
 
-inline int cta26(char *bp,int bl,int t,int p,int v) noex {
+inline int cta26(char *bp,int bl,int t,int p,int v)		noex {
 	return cta26i(bp,bl,t,p,v) ;
 }
-inline int cta26(char *bp,int bl,int t,int p,long v) noex {
+inline int cta26(char *bp,int bl,int t,int p,long v)		noex {
 	return cta26l(bp,bl,t,p,v) ;
 }
-inline int cta26(char *bp,int bl,int t,int p,longlong v) noex {
+inline int cta26(char *bp,int bl,int t,int p,longlong v)	noex {
 	return cta26ll(bp,bl,t,p,v) ;
 }
 
-inline int cta26(char *bp,int bl,int t,int p,uint uv) noex {
+inline int cta26(char *bp,int bl,int t,int p,uint uv)		noex {
 	return cta26ui(bp,bl,t,p,uv) ;
 }
-inline int cta26(char *bp,int bl,int t,int p,ulong uv) noex {
+inline int cta26(char *bp,int bl,int t,int p,ulong uv)		noex {
 	return cta26ul(bp,bl,t,p,uv) ;
 }
-inline int cta26(char *bp,int bl,int t,int p,ulonglong uv) noex {
+inline int cta26(char *bp,int bl,int t,int p,ulonglong uv)	noex {
 	return cta26ull(bp,bl,t,p,uv) ;
 }
 
