935aae1f5986523912bd17f37c5c64c5c21f8c29
David A.D. Morano <david.a.morano@gmail.com>
2026-06-08 00:05:45 -0400

    restore

---
+++ src/libuc/ctx/uconstants.hh
@@ -0,0 +1,31 @@
+/* uconstants HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* UNIX® global constants */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCONSTABTS_INCLUDE
+#define	UCONSTABTS_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* UCONSTABTS_INCLUDE */
+
+
