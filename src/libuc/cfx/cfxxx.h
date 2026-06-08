3d64455a58a4c8857bd9446ae1b2eaed99cc6323
David A.D. Morano <david.a.morano@gmail.com>
2026-06-08 00:14:21 -0400

    update

--- src/libuc/cfx/cfxxx.h
+++ src/libuc/cfx/cfxxx.h
@@ -20,42 +20,42 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<stdintx.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<stdintx.h>		/* LIBU */
 
 
 EXTERNC_begin
 
-extern int cfxxxi(cchar *,int,int *) noex ;
-extern int cfxxxl(cchar *,int,long *) noex ;
-extern int cfxxxll(cchar *,int,longlong *) noex ;
+extern int cfxxxi	(cchar *,int,int *)		noex ;
+extern int cfxxxl	(cchar *,int,long *)		noex ;
+extern int cfxxxll	(cchar *,int,longlong *)	noex ;
 
-extern int cfxxxui(cchar *,int,uint *) noex ;
-extern int cfxxxul(cchar *,int,ulong *) noex ;
-extern int cfxxxull(cchar *,int,ulonglong *) noex ;
+extern int cfxxxui	(cchar *,int,uint *)		noex ;
+extern int cfxxxul	(cchar *,int,ulong *)		noex ;
+extern int cfxxxull	(cchar *,int,ulonglong *)	noex ;
 
 EXTERNC_end
 
 #if	__cplusplus
 
-inline int cfxxx(cchar *sp,int sl,int *rp) noex {
+inline int cfxxx(cchar *sp,int sl,int *rp)		noex {
 	return cfxxxi(sp,sl,rp) ;
 }
-inline int cfxxx(cchar *sp,int sl,long *rp) noex {
+inline int cfxxx(cchar *sp,int sl,long *rp)		noex {
 	return cfxxxl(sp,sl,rp) ;
 }
-inline int cfxxx(cchar *sp,int sl,longlong *rp) noex {
+inline int cfxxx(cchar *sp,int sl,longlong *rp)		noex {
 	return cfxxxll(sp,sl,rp) ;
 }
 
-inline int cfxxx(cchar *sp,int sl,uint *rp) noex {
+inline int cfxxx(cchar *sp,int sl,uint *rp)		noex {
 	return cfxxxui(sp,sl,rp) ;
 }
-inline int cfxxx(cchar *sp,int sl,ulong *rp) noex {
+inline int cfxxx(cchar *sp,int sl,ulong *rp)		noex {
 	return cfxxxul(sp,sl,rp) ;
 }
-inline int cfxxx(cchar *sp,int sl,ulonglong *rp) noex {
+inline int cfxxx(cchar *sp,int sl,ulonglong *rp)	noex {
 	return cfxxxull(sp,sl,rp) ;
 }
 
