4b97f38d4f49f85b389744b01580b00414af2b8b
David A.D. Morano <david.a.morano@gmail.com>
2026-06-08 00:16:00 -0400

    update

--- src/libuc/ctx/ctwords.hh
+++ src/libuc/ctx/ctwords.hh
@@ -21,12 +21,9 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<string>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<string>		/* C++STD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 extern int ctwords(std::string *,int) noex ;
