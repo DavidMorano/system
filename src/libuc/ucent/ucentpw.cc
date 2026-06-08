4aa898f27b4aea5edf85a0c0e913272902b24267
David A.D. Morano <david.a.morano@gmail.com>
2026-06-08 00:18:08 -0400

    update

--- src/libuc/ucent/ucentpw.cc
+++ src/libuc/ucent/ucentpw.cc
@@ -21,8 +21,8 @@
 	uc_entpw{x}
 
 	Description:
-	These subroutines perform some PASSWD-structure management
-	functions.
+	These subroutines facilitate read-nnly access to the the
+	system PASSWD database.
 
 *******************************************************************************/
 
@@ -75,8 +75,8 @@ using ucent::si_copystr ;		/* local group support subroutine */
 
 /* forward references */
 
-static int ucentpw_parseone(ucentpw *,SI *,int,cchar *,int) noex ;
-static int ucentpw_parsedefs(ucentpw *,SI *,int) noex ;
+local int ucentpw_parseone(ucentpw *,SI *,int,cchar *,int) noex ;
+local int ucentpw_parsedefs(ucentpw *,SI *,int) noex ;
 
 
 /* local variables */
@@ -90,12 +90,12 @@ static int ucentpw_parsedefs(ucentpw *,SI *,int) noex ;
 int ucentpw::parse(char *pwbuf,int pwlen,cc *sp,int sl) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
-	if (this && pwbuf && sp) ylikely {
+	if ( pwbuf && sp) ylikely {
 	    PASSWD *pep = this ;
 	    memclear(pep) ;
 	    if (sl < 0) sl = lenstr(sp) ;
 	    if (storeitem si ; (rs = si.start(pwbuf,pwlen)) >= 0) ylikely {
-	        int		fi = 0 ;
+	        int	fi = 0 ;
 	        for (int idx ; (idx = sichr(sp,sl,':')) >= 0 ; ) ylikely {
 	            rs = ucentpw_parseone(this,&si,fi++,sp,idx) ;
 	            sl -= (idx +1) ;
@@ -123,7 +123,7 @@ int ucentpw::parse(char *pwbuf,int pwlen,cc *sp,int sl) noex {
 int ucentpw::load(char *pwbuf,int pwlen,const ucentpw *spwp) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
-	if (this && pwbuf && spwp) ylikely {
+	if (pwbuf && spwp) ylikely {
 	    PASSWD *pep = this ;
 	    *pep = *spwp ;	/* <- copy over opaque values */
 	    if (storeitem si ; (rs = si.start(pwbuf,pwlen)) >= 0) ylikely {
@@ -133,7 +133,7 @@ int ucentpw::load(char *pwbuf,int pwlen,const ucentpw *spwp) noex {
 	            si_copystr(&si,&pw_gecos,spwp->pw_gecos) ;
 	            si_copystr(&si,&pw_dir,spwp->pw_dir) ;
 	            si_copystr(&si,&pw_shell,spwp->pw_shell) ;
-		}
+		} /* end block */
 	        rs1 = si.finish ;
 	        if (rs >= 0) rs = rs1 ;
 	    } /* end if (storeitem) */
@@ -149,7 +149,7 @@ int ucentpw::format(char *rbuf,int rlen) noex {
 	    if (sbuf b ; (rs = b.start(rbuf,rlen)) >= 0) ylikely {
 	        for (int i = 0 ; i < 7 ; i += 1) {
 	            if (i > 0) rs = b.chr(':') ;
-	            if (rs >= 0) {
+	            if (rs >= 0) ylikely {
 	                switch (int v ; i) {
 	                case 0:
 	                    rs = b.str(pw_name) ;
@@ -187,26 +187,24 @@ int ucentpw::format(char *rbuf,int rlen) noex {
 /* end subroutine (ucentpw::format) */
 
 int ucentpw::size() noex {
-	int		rs = SR_FAULT ;
-	if (this) ylikely {
+	int		rs = SR_OK ;
 	    int		sz = 1 ;
 	    if (pw_name) {
-	        sz += (lenstr(pw_name)+1) ;
+	        sz += (lenstr(pw_name) + 1) ;
 	    }
 	    if (pw_passwd) {
-	        sz += (lenstr(pw_passwd)+1) ;
+	        sz += (lenstr(pw_passwd) + 1) ;
 	    }
 	    if (pw_gecos) {
-	        sz += (lenstr(pw_gecos)+1) ;
+	        sz += (lenstr(pw_gecos) + 1) ;
 	    }
 	    if (pw_dir) {
-	        sz += (lenstr(pw_dir)+1) ;
+	        sz += (lenstr(pw_dir) + 1) ;
 	    }
 	    if (pw_shell) {
-	        sz += (lenstr(pw_shell)+1) ;
+	        sz += (lenstr(pw_shell) + 1) ;
 	    }
 	    rs = sz ;
-	} /* end if (non-null) */
 	return rs ;
 }
 /* end subroutine (ucentpw::size) */
@@ -226,7 +224,7 @@ int ucentpw::getuid(char *pwbuf,int pwlen,uid_t uid) noex {
 
 /* local subroutines */
 
-static int ucentpw_parseone(ucentpw *pwp,SI *sip,int fi,cc *vp,int vl) noex {
+local int ucentpw_parseone(ucentpw *pwp,SI *sip,int fi,cc *vp,int vl) noex {
 	int		rs = SR_OK ;
 	cchar		**vpp = nullptr ;
 	switch (int v = -1 ; fi) {
@@ -259,12 +257,11 @@ static int ucentpw_parseone(ucentpw *pwp,SI *sip,int fi,cc *vp,int vl) noex {
 	    if (int cl ; (cl = sfshrink(vp,vl,&cp)) >= 0) {
 	        rs = sip->strw(cp,cl,vpp) ;
 	    }
-	}
+	} /* end if */
 	return rs ;
-}
-/* end subroutine (ucentpw_parseone) */
+} /* end subroutine (ucentpw_parseone) */
 
-static int ucentpw_parsedefs(ucentpw *pwp,storeitem *sip,int sfi) noex {
+local int ucentpw_parsedefs(ucentpw *pwp,storeitem *sip,int sfi) noex {
 	int		rs = SR_OK ;
 	if (sfi == 6) {
 	    cchar	**vpp = ccharpp(&pwp->pw_shell) ;
@@ -273,9 +270,8 @@ static int ucentpw_parsedefs(ucentpw *pwp,storeitem *sip,int sfi) noex {
 	    vp = (sp + lenstr(sp)) ;
 	    sfi += 1 ;
 	    rs = sip->strw(vp,0,vpp) ;
-	}
+	} /* end if */
 	return (rs >= 0) ? sfi : rs ;
-}
-/* end subroutine (ucentpw_parsedefs) */
+} /* end subroutine (ucentpw_parsedefs) */
 
 
