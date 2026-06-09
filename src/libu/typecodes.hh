82e5b8487f230e66001764158cf6d4d7e4cda47f
David A.D. Morano <david.a.morano@gmail.com>
2026-06-09 00:28:38 -0400

    restore

---
+++ src/libu/typecodes.hh
@@ -0,0 +1,31 @@
+/* typecodes HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* bit manipulations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2020-03-09, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 2020 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	TYPECODES_INCLUDE
+#define	TYPECODES_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<utypedefs.h>
+#include	<utypealiases.h>
+#include	<usysdefs.h>
+
+
+#endif /* TYPECODES_INCLUDE */
+
+
