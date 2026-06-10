b98c24e38e136031ea6393d464711117d639632b
David A.D. Morano <david.a.morano@gmail.com>
2026-06-10 00:08:35 -0400

    restore

---
+++ src/libdam/varsub/varsub_util.hh
@@ -0,0 +1,31 @@
+/* varsub_util HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* utilities for the VARSUB object implementation */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	VARSUBUTIL_INCLUDE
+#define	VARSUBUTIL_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* VARSUBUTIL_INCLUDE */
+
+
