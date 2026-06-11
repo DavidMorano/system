0d4c95adcd4de9693f8ec5837de9b54a3b2add51
David A.D. Morano <david.a.morano@gmail.com>
2026-06-11 08:24:14 -0400

    update

--- src/libuc/ndigit.h
+++ src/libuc/ndigit.h
@@ -20,44 +20,44 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<stdintx.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<stdintx.h>		/* LIBU */
 
 
 EXTERNC_begin
 
-extern int ndigiti(int,int) noex ;
-extern int ndigitl(long,int) noex ;
-extern int ndigitll(longlong,int) noex ;
+extern int ndigiti	(int,int)		noex ;
+extern int ndigitl	(long,int)		noex ;
+extern int ndigitll	(longlong,int)		noex ;
 
-extern int ndigitui(uint,int) noex ;
-extern int ndigitul(ulong,int) noex ;
-extern int ndigitull(ulonglong,int) noex ;
+extern int ndigitui	(uint,int)		noex ;
+extern int ndigitul	(ulong,int)		noex ;
+extern int ndigitull	(ulonglong,int)		noex ;
 
 EXTERNC_end
 
 #ifdef	__cplusplus
 
-inline int ndigit(int v,int b) noex {
+inline int ndigit(int v,int b)			noex {
 	return ndigiti(v,b) ;
 }
-inline int ndigit(long v,int b) noex {
+inline int ndigit(long v,int b)			noex {
 	return ndigitl(v,b) ;
 }
-inline int ndigit(longlong v,int b) noex {
+inline int ndigit(longlong v,int b)		noex {
 	return ndigitll(v,b) ;
 }
 
-inline int ndigit(uint v,int b) noex {
+inline int ndigit(uint v,int b)			noex {
 	return ndigitui(v,b) ;
 }
-inline int ndigit(ulong v,int b) noex {
+inline int ndigit(ulong v,int b)		noex {
 	return ndigitul(v,b) ;
 }
-inline int ndigit(ulonglong v,int b) noex {
+inline int ndigit(ulonglong v,int b)		noex {
 	return ndigitull(v,b) ;
 }
 
