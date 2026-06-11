203945542f799a3ead833bed55f10f4ed5c785cf
David A.D. Morano <david.a.morano@gmail.com>
2026-06-11 08:26:27 -0400

    update

--- src/libu/umods/bitop.hh
+++ src/libu/umods/bitop.hh
@@ -2,7 +2,7 @@
 /* charset=ISO8859-1 */
 /* lang=C++20 */
 
-/* convert a single character to a given case */
+/* perform the most basic bit operations on integral words */
 /* version %I% last-modified %G% */
 
 
