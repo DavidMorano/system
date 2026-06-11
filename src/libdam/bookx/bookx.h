3f13d11a2da211b3b78fd3b32f1c824cbe50da91
David A.D. Morano <david.a.morano@gmail.com>
2026-06-11 08:35:31 -0400

    restore

---
+++ src/libdam/bookx/bookx.h
@@ -0,0 +1,33 @@
+/* bookx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* manage a BVS "book" entry */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2008-10-01, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 2008 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	BOOKX_INCLUDE
+#define	BOOKX_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<bvsbook.h>
+#include	<bvshdr.h>
+#include	<bvsmk.h>
+
+
+#endif /* BOOKX_INCLUDE */
+
+
