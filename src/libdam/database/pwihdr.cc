2c2bbedbefc42cfc10012294d590fcebbe305151
David A.D. Morano <david.a.morano@gmail.com>
2026-06-11 08:39:16 -0400

    update

--- src/libdam/database/pwihdr.cc
+++ src/libdam/database/pwihdr.cc
@@ -44,22 +44,25 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<ctime>
-#include	<climits>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<cstring>		/* |memcpy(3c)| */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
-#include	<uclibmem.h>
-#include	<endian.h>
-#include	<mkmagic.h>
-#include	<hasx.h>
-#include	<localmisc.h>
+#include	<ctime>			/* CSTD */
+#include	<climits>		/* CSTD */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<endian.h>		/* LIBU */
+#include	<uclibmem.h>		/* LIBUC */
+#include	<mkmagic.h>		/* LIBUC */
+#include	<hasx.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"pwihdr.h"
 
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |lenstr(3u)| */
 
 /* local defines */
 
@@ -78,9 +81,10 @@
 
 /* local variables */
 
-constexpr int		tabsize = (pwihdr_overlast * szof(uint)) ;
-constexpr int		magicsize = PWIHDR_MAGICSIZE ;
-constexpr char		magicstr[] = PWIHDR_MAGICSTR ;
+constexpr int		tabsize		= (pwihdr_overlast * szof(uint)) ;
+constexpr int		magicsize	= PWIHDR_MAGICSIZE ;
+constexpr int		vsz		= szof(uint) ;	/* VETU */
+constexpr char		magicstr[]	= PWIHDR_MAGICSTR ;
 
 
 /* exported variables */
@@ -94,36 +98,36 @@ int pwihdr_rd(pwihdr *op,char *hbuf,int hlen) noex {
 	if (op && hbuf) {
 	    int		bl = hlen ;
 	    char	*bp = hbuf ;
-	    if (bl >= (magicsize + 4)) {
+	    if (bl >= (magicsize + vsz)) {
 	        if ((rs = mkmagic(bp,magicsize,magicstr)) >= 0) {
 	            bp += magicsize ;
 	            bl -= magicsize ;
-	    	    memcpy(bp,op->vetu,4) ;
-	    	    bp[0] = PWIHDR_VERSION ;
-	    	    bp[1] = ENDIAN ;
-	    	    bp += 4 ;
-	    	    bl -= 4 ;
+	    	    memcopy(bp,op->vetu,vsz) ;
+	    	    bp[0] = uchar(PWIHDR_VERSION) ;
+	    	    bp[1] = uchar(ENDIAN) ;
+	    	    bp += vsz ;
+	    	    bl -= vsz ;
 	    	    if (bl >= tabsize) {
-	        	uint	*header = uintp(bp) ;
-			header[pwihdr_fsize] = op->fsize ;
-			header[pwihdr_wrtime] = op->wrtime ;
-			header[pwihdr_wrcount] = op->wrcount ;
-			header[pwihdr_rectab] = op->rectab ;
-			header[pwihdr_reclen] = op->reclen ;
-			header[pwihdr_recsize] = op->recsize ;
-			header[pwihdr_strtab] = op->strtab ;
-			header[pwihdr_lenstr] = op->lenstr ;
-			header[pwihdr_strsize] = op->strsize ;
-			header[pwihdr_idxlen] = op->idxlen ;
-			header[pwihdr_idxsize] = op->idxsize ;
-			header[pwihdr_idxl1] = op->idxl1 ;
-			header[pwihdr_idxl3] = op->idxl3 ;
-			header[pwihdr_idxf] = op->idxf ;
-			header[pwihdr_idxfl3] = op->idxfl3 ;
-			header[pwihdr_idxun] = op->idxun ;
+	        	uint			*header = uintp(bp) ;
+			header[pwihdr_fsz]	= op->fsz ;
+			header[pwihdr_wrtime]	= op->wrtime ;
+			header[pwihdr_wrcount]	= op->wrcount ;
+			header[pwihdr_rectab]	= op->rectab ;
+			header[pwihdr_reclen]	= op->reclen ;
+			header[pwihdr_recsize]	= op->recsize ;
+			header[pwihdr_strtab]	= op->strtab ;
+			header[pwihdr_lenstr]	= op->lenstr ;
+			header[pwihdr_strsize]	= op->strsize ;
+			header[pwihdr_idxlen]	= op->idxlen ;
+			header[pwihdr_idxsize]	= op->idxsize ;
+			header[pwihdr_idxl1]	= op->idxl1 ;
+			header[pwihdr_idxl3]	= op->idxl3 ;
+			header[pwihdr_idxf]	= op->idxf ;
+			header[pwihdr_idxfl3]	= op->idxfl3 ;
+			header[pwihdr_idxun]	= op->idxun ;
 	        	bp += tabsize ;
 	        	bl -= tabsize ;
-			len = (bp - hbuf) ;
+			len = intconv(bp - hbuf) ;
 	            } else {
 	                rs = SR_OVERFLOW ;
 	            }
@@ -147,37 +151,37 @@ int pwihdr_wr(pwihdr *op,cchar *hbuf,int hlen) noex {
 	        bp += magicsize ;
 	        bl -= magicsize ;
 	        /* read out the VETU information */
-	        if (bl >= 4) {
-	            memcpy(op->vetu,bp,4) ;
+	        if (bl >= vsz) {
+	            memcopy(op->vetu,bp,vsz) ;
 	            if (op->vetu[0] != PWIHDR_VERSION) {
 	                rs = SR_PROTONOSUPPORT ;
 		    }
 	            if ((rs >= 0) && (op->vetu[1] != ENDIAN)) {
 	                rs = SR_PROTOTYPE ;
 		    }
-	            bp += 4 ;
-	            bl -= 4 ;
+	            bp += vsz ;
+	            bl -= vsz ;
 	            if ((rs >= 0) && (bl >= tabsize)) {
 			const uint	*header = uintp(bp) ;
-	                op->fsize = header[pwihdr_fsize] ;
-	                op->wrtime = header[pwihdr_wrtime] ;
-	                op->wrcount = header[pwihdr_wrcount] ;
-	                op->rectab = header[pwihdr_rectab] ;
-	                op->reclen = header[pwihdr_reclen] ;
-	                op->recsize = header[pwihdr_recsize] ;
-	                op->strtab = header[pwihdr_strtab] ;
-	                op->lenstr = header[pwihdr_lenstr] ;
-	                op->strsize = header[pwihdr_strsize] ;
-	                op->idxlen = header[pwihdr_idxlen] ;
-	                op->idxsize = header[pwihdr_idxsize] ;
-	                op->idxl1 = header[pwihdr_idxl1] ;
-	                op->idxl3 = header[pwihdr_idxl3] ;
-	                op->idxf = header[pwihdr_idxf] ;
-	                op->idxfl3 = header[pwihdr_idxfl3] ;
-	                op->idxun = header[pwihdr_idxun] ;
+	                op->fsz		= header[pwihdr_fsz] ;
+	                op->wrtime	= header[pwihdr_wrtime] ;
+	                op->wrcount	= header[pwihdr_wrcount] ;
+	                op->rectab	= header[pwihdr_rectab] ;
+	                op->reclen	= header[pwihdr_reclen] ;
+	                op->recsize	= header[pwihdr_recsize] ;
+	                op->strtab	= header[pwihdr_strtab] ;
+	                op->lenstr	= header[pwihdr_lenstr] ;
+	                op->strsize	= header[pwihdr_strsize] ;
+	                op->idxlen	= header[pwihdr_idxlen] ;
+	                op->idxsize	= header[pwihdr_idxsize] ;
+	                op->idxl1	= header[pwihdr_idxl1] ;
+	                op->idxl3	= header[pwihdr_idxl3] ;
+	                op->idxf	= header[pwihdr_idxf] ;
+	                op->idxfl3	= header[pwihdr_idxfl3] ;
+	                op->idxun	= header[pwihdr_idxun] ;
 	                bp += tabsize ;
 	                bl -= tabsize ;
-			len = (bp - hbuf) ;
+			len = intconv(bp - hbuf) ;
 	            } else {
 	                rs = SR_ILSEQ ;
 		    }
@@ -191,5 +195,16 @@ int pwihdr_wr(pwihdr *op,cchar *hbuf,int hlen) noex {
 	return (rs >= 0) ? len : rs ;
 }
 /* end subroutine (pwihdr_wr) */
+
+
+/* local subroutines */
+
+int pwihdr::rd(char *rbuf,int rlen) noex {
+    	return pwihdr_rd(this,rbuf,rlen) ;
+} /* end method (pwihdr::rd) */
+
+int pwihdr::wr(cchar *wbuf,int wlen) noex {
+    	return pwihdr_wr(this,wbuf,wlen) ;
+} /* end method (pwihdr::wr) */
 
 
