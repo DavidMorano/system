3f59b0b36cff5501e6bf93cdcb70085b050755ff
David A.D. Morano <david.a.morano@gmail.com>
2026-06-11 08:36:20 -0400

    update

--- src/libdam/bookx/bvcitekey.cc
+++ src/libdam/bookx/bvcitekey.cc
@@ -21,7 +21,7 @@
 
 *******************************************************************************/
 
-#include	<envstandards.h>	/* must be before others */
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<climits>
 #include	<cstddef>		/* |nullptr_t| */
 #include	<cstdlib>
