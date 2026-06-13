941004b8ae537784c15aaf8ed8906269d7d38a5f
David A.D. Morano <david.a.morano@gmail.com>
2026-06-13 01:33:40 -0400

    update

--- src/libdam/bookx/bpimk.h
+++ src/libdam/bookx/bpimk.h
@@ -18,7 +18,7 @@
 #define	BPIMK		struct bpimk_head
 #define	BPIMK_OBJ	struct bpimk_object
 #define	BPIMK_FL	struct bpimk_flags
-#define	BPIMK_VER	struct bpimk_verser
+#define	BPIMK_V		struct bpimk_verse
 #define	BPIMK_INFO	struct bpimk_information
 #define	BPIMK_MAGIC	0x88773423
 #define	BPIMK_INTOPEN	(10*60)
@@ -39,7 +39,7 @@ struct bpimk_information {
 	uint		nzverses ;
 } ; /* end struct */
 
-struct bpimk_verser {
+struct bpimk_verse {
 	uchar		nlines, b, c, v ;
 } ; /* end struct */
 
@@ -56,7 +56,7 @@ struct bpimk_head {
 	cchar 		*dbname ;
 	cchar		*idname ;
 	char		*nidxfname ;
-	vecobj		verses ;
+	vecobj		*vlp ;		/* verse-list-pointer */
 	BPIMK_FL	fl ;
 	uint		magval ;
 	uint		pcitation ;
@@ -67,23 +67,25 @@ struct bpimk_head {
 	uint		nzverses ;
 	int		nfd ;
 	mode_t		om ;
-} ; /* end struct */
+} ; /* end struct (bpimk_head) */
 
 typedef BPIMK		bpimk ;
 typedef	BPIMK_FL	bpimk_fl ;
 typedef	BPIMK_OBJ	bpimk_obj ;
-typedef	BPIMK_VER	bpimk_ver ;
+typedef	BPIMK_V		bpimk_v ;
 typedef	BPIMK_INFO	bpimk_info ;
 
 EXTERNC_begin
 
-extern int	bpimk_open(bpimk *,cchar *,int,mode_t) noex ;
-extern int	bpimk_add(bpimk *,bpimk_ver *) noex ;
-extern int	bpimk_abort(bpimk *,int) noex ;
-extern int	bpimk_info(bpimk *,bpimk_info *) noex ;
-extern int	bpimk_close(bpimk *) noex ;
+extern int	bpimk_open	(bpimk *,cchar *,int,mode_t) noex ;
+extern int	bpimk_add	(bpimk *,bpimk_v *) noex ;
+extern int	bpimk_abort	(bpimk *,int) noex ;
+extern int	bpimk_getinfo	(bpimk *,bpimk_info *) noex ;
+extern int	bpimk_close	(bpimk *) noex ;
 
 EXTERNC_end
+
+extern const bpimk_obj		bpimk_modinfo ;
 
 
 #endif /* BPIMK_INCLUDE */
