7361d9bc2016902d3f76a01527e94576eba307f1
David A.D. Morano <david.a.morano@gmail.com>
2026-06-13 01:41:49 -0400

    update

--- src/mktagprint/proctagprint.cc
+++ src/mktagprint/proctagprint.cc
@@ -49,7 +49,7 @@
 ***********************************************************************/
 
 
-#include	<envstandards.h>	/* must be before others */
+#include	<envstandards.h>	/* ordered first to configure */
 
 #include	<sys/types.h>
 #include	<sys/param.h>
