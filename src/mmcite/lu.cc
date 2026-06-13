2fe7b3da4e1ab643acc9ec48f72193b1be920d99
David A.D. Morano <david.a.morano@gmail.com>
2026-06-13 01:41:14 -0400

    update

--- src/mmcite/lu.cc
+++ src/mmcite/lu.cc
@@ -31,7 +31,7 @@
 *******************************************************************************/
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 
 #include	<sys/types.h>
 #include	<sys/param.h>
