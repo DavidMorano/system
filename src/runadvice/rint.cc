5d1d82fe32fe2621cafd26495b9b9ec4b1aca776
David A.D. Morano <david.a.morano@gmail.com>
2026-06-13 01:54:31 -0400

    update

--- src/runadvice/rint.cc
+++ src/runadvice/rint.cc
@@ -1,13 +1,70 @@
+/* rint SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* revision history:
+
+	= 1995-01-06, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1995 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+/*******************************************************************************
+
+	Name:
+	rint
+
+	Description:
+	Convert a double type to an integer type.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 
-int rint(d)
-double	d ;
-{
+/* local defines */
 
 
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int rint(double d) noex {
 	return (int) d ;
-}
-
-
+} /* end subroutine */
 
 
