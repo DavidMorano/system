b76a29af15a6cf063982ca639682fd0acb082816
David A.D. Morano <david.a.morano@gmail.com>
2026-06-13 01:34:08 -0400

    update

--- src/libdam/bookx/bvs.cc
+++ src/libdam/bookx/bvs.cc
@@ -6,7 +6,7 @@
 /* load management and interface for the BVSES object */
 /* version %I% last-modified %G% */
 
-#define	CF_DEBUGS	0		/* non-switchable debug print-outs */
+#define	CF_DEBUG	0		/* non-switchable debug print-outs */
 
 /* revision history:
 
@@ -30,20 +30,22 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/types.h>
-#include	<sys/param.h>
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<cstdlib>
-#include	<cstring>
-#include	<usystem.h>
-#include	<vecstr.h>
-#include	<localmisc.h>
+#include	<sys/types.h>		/* POSIX */
+#include	<sys/param.h>		/* POSIX */
+#include	<unistd.h>		/* POSIX */
+#include	<fcntl.h>		/* POSIX */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<ucmem.h>		/* LIBUC */
+#include	<vecstr.h>		/* LIBUC */
+#include	<sncpyx.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
-#include	"bvs.h"
 #include	"bvses.h"
+#include	"bvs.h"
 
 #pragma		GCC dependency		"mod/libutil.ccm"
 
@@ -54,14 +56,26 @@ import libutil ;			/* |memclear(3u)| */
 #define	BVS_MODBNAME	"bvses"
 #define	BVS_OBJNAME	"bvses"
 
-#undef	SMM_INFO
-#define	SMM_INFO	BVSES_INFO
-#undef	SMM_VERSE
-#define	SMM_VERSE	BVSES_VERSE
-
 #ifndef	SYMNAMELEN
 #define	SYMNAMELEN	60
 #endif
+
+
+/* imported namespaces */
+
+using libuc::mem ;			/* variable */
+
+
+/* local typedefs */
+
+extern "C" {
+    typedef int	(*soopen_f)	(void *,cchar *,cchar *) noex ;
+    typedef int	(*socount_f)	(void *) noex ;
+    typedef int	(*sogetinfo_f)	(void *,bvses_info *) noex ;
+    typedef int	(*somkmodq_f)	(void *,bvses_v *,int) noex ;
+    typedef int	(*soaudit_f)	(void *) noex ;
+    typedef int	(*soclose_f)	(void *) noex ;
+} /* end extern (C) */
 
 
 /* external subroutines */
@@ -72,14 +86,75 @@ import libutil ;			/* |memclear(3u)| */
 
 /* local structures */
 
+struct bvs_calls {
+    soopen_f		open ;
+    socount_f		count ;
+    sogetinfo_f		getinfo ;
+    somkmodq_f		mkmodq ;
+    soaudit_f		audit ;
+    soclose_f		close ;
+} ; /* end struct (bvs) */
+
+typedef bvs_calls *	callsp ;
+
 
 /* forward references */
 
-static int	bvs_objloadbegin(BVS *,cchar *,cchar *) ;
-static int	bvs_objloadend(BVS *) ;
-static int	bvs_loadcalls(BVS *,cchar *) ;
+template<typename ... Args>
+local int bvs_ctor(bvs *op,Args ... args) noex {
+	BVS		*hop = op ;
+	cnullptr	np{} ;
+	cnothrow	nt{} ;
+	int		rs = SR_FAULT ;
+	if (op && (args && ...)) ylikely {
+	    memclear(hop) ;
+	    rs = SR_NOMEM ;
+	    if ((op->mlp = new(nt) modload) != np) ylikely {
+		bvs_calls    *callp ;
+                if ((callp = new(nt) bvs_calls) != np) ylikely {
+                    op->callp = callp ;
+                    rs = SR_OK ;
+                } /* end if (new-bvs_calls) */
+                if (rs < 0) {
+                    delete op->mlp ;
+                    op->mlp = nullptr ;
+                } /* end if (error) */
+	    } /* end if (new-modload) */
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (bvs_ctor) */
 
-static int	isrequired(int) ;
+local int bvs_dtor(bvs *op) noex {
+	int		rs = SR_FAULT ;
+	if (op) ylikely {
+	    rs = SR_OK ;
+            if (op->callp) ylikely {
+                bvs_calls    *callp = callsp(op->callp) ;
+                delete callp ;
+                op->callp = nullptr ;
+            }
+	    if (op->mlp) ylikely {
+		delete op->mlp ;
+		op->mlp = nullptr ;
+	    }
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (bvs_dtor) */
+
+template<typename ... Args>
+local inline int bvs_magic(bvs *op,Args ... args) noex {
+	int		rs = SR_FAULT ;
+	if (op && (args && ...)) ylikely {
+	    rs = (op->magval == BVS_MAGIC) ? SR_OK : SR_NOTOPEN ;
+	} /* end if */
+	return rs ;
+} /* end subroutine (bvs_magic) */
+
+local int	bvs_objloadbegin	(bvs *,cchar *,cchar *) noex ;
+local int	bvs_objloadend		(bvs *) noex ;
+local int	bvs_loadcalls		(bvs *,vecstr *) noex ;
+
+local bool	isrequired(int) noex ;
 
 
 /* local variables */
@@ -87,357 +162,278 @@ static int	isrequired(int) ;
 enum subs {
 	sub_open,
 	sub_count,
-	sub_info,
-	sub_mkmodquery,
+	sub_getinfo,
+	sub_mkmodq,
 	sub_audit,
 	sub_close,
 	sub_overlast
-} ;
+} ; /* end enum */
 
-constexpr cpcchar	subs[] = {
+constexpr cpcchar	subnames[] = {
 	"open",
 	"count",
-	"info",
-	"mkmodquery",
+	"getinfo",
+	"mkmodq",
 	"audit",
 	"close",
-	NULL
-} ;
+	nullptr
+} ; /* end array (subnames) */
 
 
 /* exported variables */
 
-extern const bvs_obj		bvs_modinfo = {
-    	"bvs",
-	szof(bvs)
-} ;
-
 
 /* exported subroutines */
 
-int bvs_open(BVS *op,cchar *pr,cchar *dbname) noex {
+int bvs_open(bvs *op,cchar *pr,cchar *dbn) noex {
 	int		rs ;
-	cchar		*objname = BVS_OBJNAME ;
-
-	if (op == NULL) return SR_FAULT ;
-	if (pr == NULL) return SR_FAULT ;
-	if (dbname == NULL) return SR_FAULT ;
-
-	if (pr[0] == '\0') return SR_INVALID ;
-	if (dbname[0] == '\0') return SR_INVALID ;
-
-	memclear(op) ;
-
-	if ((rs = bvs_objloadbegin(op,pr,objname)) >= 0) {
-	    if ((rs = (*op->call.open)(op->obj,pr,dbname)) >= 0) {
-		op->magic = BVS_MAGIC ;
-	    }
-	    if (rs < 0)
-		bvs_objloadend(op) ;
-	} /* end if (bvs-objloadbegin) */
-
-#if	CF_DEBUGS
-	debugprintf("bvs_open: ret rs=%d\n",rs) ;
-#endif
-
+	if ((rs = bvs_ctor(op,pr,dbn)) >= 0) {
+	    rs = SR_INVALID ;
+	    if (pr[0] && dbn[0]) {
+	        cchar	*objn = BVS_OBJNAME ;
+                if ((rs = bvs_objloadbegin(op,pr,objn)) >= 0) {
+                    bvs_calls   *callp = callsp(op->callp) ;
+                    rs = SR_NOSYS ;
+                    if (callp->open) {
+                        cauto   co = callp->open ;
+                        if ((rs = co(op->obj,pr,dbn)) >= 0) {
+                            op->magval = BVS_MAGIC ;
+                        }
+                    } /* end if (open) */
+	            if (rs < 0) {
+		        bvs_objloadend(op) ;
+	            } /* end if (error) */
+	        } /* end if (bvs-objloadbegin) */
+	    } /* end if (valid) */
+	    if (rs < 0) {
+		bvs_dtor(op) ;
+	    } /* end if (error) */
+	} /* end if (bvs_ctor) */
 	return rs ;
-}
-/* end subroutine (bvs_open) */
-
+} /* end subroutine (bvs_open) */
 
 /* free up the entire vector string data structure object */
-int bvs_close(BVS *op)
-{
-	int		rs = SR_OK ;
+int bvs_close(bvs *op) noex {
+	int		rs ;
 	int		rs1 ;
-
-	if (op == NULL) return SR_FAULT ;
-
-	if (op->magic != BVS_MAGIC) return SR_NOTOPEN ;
-
-	rs1 = (*op->call.close)(op->obj) ;
-	if (rs >= 0) rs = rs1 ;
-
-	rs1 = bvs_objloadend(op) ;
-	if (rs >= 0) rs = rs1 ;
-
-	op->magic = 0 ;
-	return rs ;
-}
-/* end subroutine (bvs_close) */
-
-
-int bvs_count(BVS *op)
-{
-	int		rs = SR_NOSYS ;
-
-	if (op == NULL) return SR_FAULT ;
-
-	if (op->magic != BVS_MAGIC) return SR_NOTOPEN ;
-
-	if (op->call.count != NULL) {
-	    rs = (*op->call.count)(op->obj) ;
-	}
-
-	return rs ;
-}
-/* end subroutine (bvs_count) */
-
-
-int bvs_info(BVS *op,BVS_INFO *ip)
-{
-	BVSES_INFO	bvsi ;
-	int		rs = SR_NOSYS ;
-	int		n = 0 ;
-
-	if (op == NULL) return SR_FAULT ;
-
-	if (op->magic != BVS_MAGIC) return SR_NOTOPEN ;
-
-	if (op->call.info != NULL) {
-	    rs = (*op->call.info)(op->obj,&bvsi) ;
-	    n = rs ;
-	}
-
-	if (ip != NULL) {
-	    memclear(ip) ;
-	    if (rs >= 0) {
-		ip->ctime = bvsi.ctime ;
-		ip->mtime = bvsi.mtime ;
-		ip->nzbooks = bvsi.nzbooks ;
-		ip->nbooks = bvsi.nbooks ;
-		ip->nchapters = bvsi.nchapters ;
-		ip->nverses = bvsi.nverses ;
-		ip->nzverses = bvsi.nzverses ;
+	if ((rs = bvs_magic(op)) >= 0) {
+	    bvs_calls	*callp = callsp(op->callp) ;
+	    if (callp->close) {
+		cauto 	co = callp->close ;
+	        rs1 = co(op->obj) ;
+	        if (rs >= 0) rs = rs1 ;
+	    } else {
+		rs = SR_NOSYS ;
 	    }
-	}
-
-	return (rs >= 0) ? n : rs ;
-}
-/* end subroutine (bvs_info) */
-
-int bvs_mkmodquery(BVS *op,BVS_VERSE *bvep,int mjd) noex {
-	BVSES_VERSE	bv ;
-	int		rs = SR_NOSYS ;
-	int		n = 0 ;
-
-	if (op == NULL) return SR_FAULT ;
-
-	if (op->magic != BVS_MAGIC) return SR_NOTOPEN ;
-
-	if (op->call.mkmodquery != NULL) {
-	    rs = (*op->call.mkmodquery)(op->obj,&bv,mjd) ;
-	    n = rs ;
-	}
-
-	if (bvep != NULL) {
-	    memclear(bvep) ;
-	    if (rs >= 0) {
-		bvep->b = bv.b ;
-		bvep->c = bv.c ;
-		bvep->v = bv.v ;
+	    {
+	        rs1 = bvs_objloadend(op) ;
+	        if (rs >= 0) rs = rs1 ;
 	    }
-	}
-
-	return (rs >= 0) ? n : rs ;
-}
-/* end subroutine (bvs_mkmodquery) */
-
-
-int bvs_audit(BVS *op)
-{
-	int		rs = SR_NOSYS ;
-
-	if (op == NULL) return SR_FAULT ;
-
-	if (op->magic != BVS_MAGIC) return SR_NOTOPEN ;
-
-	if (op->call.audit != NULL) {
-	    rs = (*op->call.audit)(op->obj) ;
-	}
-
+	    {
+	        rs1 = bvs_dtor(op) ;
+	        if (rs >= 0) rs = rs1 ;
+	    }
+	    op->magval = 0 ;
+	} /* end if (bvs_magic) */
 	return rs ;
-}
-/* end subroutine (bvs_audit) */
+} /* end subroutine (bvs_close) */
+
+int bvs_count(bvs *op) noex {
+	int		rs ;
+	if ((rs = bvs_magic(op)) >= 0) {
+	    bvs_calls	*callp = callsp(op->callp) ;
+	    rs = SR_NOSYS ;
+	    if (callp->count) {
+		cauto 	co = callp->count ;
+    		rs = co(op->obj) ;
+	    }
+	} /* end if (bvs_magic) */
+	return rs ;
+} /* end subroutine (bvs_count) */
+
+int bvs_getinfo(bvs *op,BVS_INFO *ip) noex {
+	int		rs ;
+	int		n = 0 ; /* return-value */
+	if ((rs = bvs_magic(op)) >= 0) {
+	    bvses_info	bvsi{} ;
+	    bvs_calls	*callp = callsp(op->callp) ;
+	    rs = SR_NOSYS ;
+	    if (callp->getinfo) {
+		cauto 	co = callp->getinfo ;
+    		rs = co(op->obj,&bvsi) ;
+	    }
+	    if (ip) {
+	        memclear(ip) ;
+	        if (rs >= 0) {
+		    ip->ctime		= bvsi.ctime ;
+		    ip->mtime		= bvsi.mtime ;
+		    ip->nzbooks		= bvsi.nzbooks ;
+		    ip->nbooks		= bvsi.nbooks ;
+		    ip->nchapters	= bvsi.nchapters ;
+		    ip->nverses		= bvsi.nverses ;
+		    ip->nzverses	= bvsi.nzverses ;
+	        } /* end if */
+	    } /* end if (non-null) */
+	} /* end if (bvs_magic) */
+	return (rs >= 0) ? n : rs ;
+} /* end subroutine (bvs_getinfo) */
+
+int bvs_mkmodq(bvs *op,bvs_v *bvep,int mjd) noex {
+	int		rs ;
+	int		n = 0 ; /* return-value */
+	if ((rs = bvs_magic(op)) >= 0) {
+	    bvses_v	bv{} ;
+	    bvs_calls	*callp = callsp(op->callp) ;
+	    rs = SR_NOSYS ;
+	    if (callp->mkmodq) {
+		cauto 	co = callp->mkmodq ;
+    		rs = co(op->obj,&bv,mjd) ;
+	    }
+	    if (bvep) {
+	        memclear(bvep) ;
+	        if (rs >= 0) {
+		    bvep->b = bv.b ;
+		    bvep->c = bv.c ;
+		    bvep->v = bv.v ;
+	        } /* end if (ok) */
+	    } /* end if (non-null) */
+	} /* end if (bvs_magic) */
+	return (rs >= 0) ? n : rs ;
+} /* end subroutine (bvs_mkmodq) */
+
+int bvs_audit(bvs *op) noex {
+	int		rs ;
+	if ((rs = bvs_magic(op)) >= 0) {
+	    bvs_calls	*callp = callsp(op->callp) ;
+	    rs = SR_NOSYS ;
+	    if (callp->audit) {
+		cauto 	co = callp->audit ;
+	        rs = co(op->obj) ;
+	    }
+	} /* end if (bvs_magic) */
+	return rs ;
+} /* end subroutine (bvs_audit) */
 
 
 /* private subroutines */
 
-
-/* find and load the DB-access object */
-static int bvs_objloadbegin(BVS *op,cchar *pr,cchar *objname)
-{
-	MODLOAD		*lp = &op->loader ;
-	VECSTR		syms ;
-	const int	n = nelem(subs) ;
+local int bvs_objloadbegin(bvs *op,cchar *pr,cchar *objn) noex {
+	modload		*lp = op->mlp ;
+	cint		vn = sub_overlast ;
+	cint		vo = vecstrm.compact ;
 	int		rs ;
 	int		rs1 ;
-	int		opts ;
-
-#if	CF_DEBUGS
-	debugprintf("bvs_objloadbegin: pr=%s\n",pr) ;
-	debugprintf("bvs_objloadbegin: objname=%s\n",objname) ;
-#endif
-
-	opts = VECSTR_OCOMPACT ;
-	if ((rs = vecstr_start(&syms,n,opts)) >= 0) {
-	    const int	nlen = SYMNAMELEN ;
-	    int		i ;
-	    int		f_modload = FALSE ;
-	    char	nbuf[SYMNAMELEN + 1] ;
-
-	    for (i = 0 ; (i < n) && (subs[i] != NULL) ; i += 1) {
-	        if (isrequired(i)) {
-	            if ((rs = sncpy3(nbuf,nlen,objname,"_",subs[i])) >= 0) {
-			rs = vecstr_add(&syms,nbuf,rs) ;
-		    }
-		}
-		if (rs < 0) break ;
-	    } /* end for */
-
-	    if (rs >= 0) {
-		cchar	**sv ;
-	        if ((rs = vecstr_getvec(&syms,&sv)) >= 0) {
-	            cchar	*modbname = BVS_MODBNAME ;
-	            opts = (MODLOAD_OLIBVAR | MODLOAD_OSDIRS) ;
-	            rs = modload_open(lp,pr,modbname,objname,opts,sv) ;
-		    f_modload = (rs >= 0) ;
-		}
-	    }
-
-	    rs1 = vecstr_finish(&syms) ;
+	if (vecstr syms ; (rs = syms.start(vn,vo)) >= 0) {
+	    if ((rs = syms.addsyms(objn,subnames)) >= 0) {
+	        if (mainv sv ; (rs = syms.getvec(&sv)) >= 0) {
+	            cchar	*mn = BVS_MODBNAME ;
+	            cchar	*on = objn ;
+	            int		mo = 0 ;
+	            mo |= modloadm.libvar ;
+	            mo |= modloadm.libprs ;
+	            mo |= modloadm.libsdirs ;
+	            mo |= modloadm.avail ;
+	            if ((rs = modload_open(lp,pr,mn,on,mo,sv)) >= 0) {
+		        op->fl.modload = true ;
+	                if (int mv[2] ; (rs = modload_getmva(lp,mv,2)) >= 0) {
+			    cint	osz = mv[0] ;
+	                    op->objsz = mv[0] ;
+	                    op->cursz = mv[1] ;
+			    if (void *vp ; (rs = mem.mall(osz,&vp)) >= 0) {
+	                        op->obj = vp ;
+	                        rs = bvs_loadcalls(op,&syms) ;
+	                        if (rs < 0) {
+	                            mem.free(op->obj) ;
+	                            op->obj = nullptr ;
+	                        } /* end if (error) */
+	                    } /* end if (memory-allocation) */
+	                } /* end if (modload_getmva) */
+	                if (rs < 0) {
+		            op->fl.modload = false ;
+	                    modload_close(lp) ;
+	                } /* end if (error) */
+	            } /* end if (modload_open) */
+		} /* end if (vecstr_getvec) */
+	    } /* end if (vecstr_addsyms) */
+	    rs1 = syms.finish ;
 	    if (rs >= 0) rs = rs1 ;
-	    if ((rs < 0) && f_modload) {
+	    if ((rs < 0) && op->fl.modload) {
+		op->fl.modload = false ;
 		modload_close(lp) ;
-	    }
-	} /* end if (allocation) */
-
-#if	CF_DEBUGS
-	debugprintf("bvs_objloadbegin: modload_open() rs=%d\n",rs) ;
-#endif
-
-	if (rs >= 0) {
-	    if ((rs = modload_getmv(lp,0)) >= 0) {
-		int	objsize = rs ;
-		void	*p ;
-		if ((rs = uc_malloc(objsize,&p)) >= 0) {
-		    op->obj = p ;
-		    rs = bvs_loadcalls(op,objname) ;
-		    if (rs < 0) {
-			uc_free(op->obj) ;
-			op->obj = NULL ;
-		    }
-		} /* end if (memory-allocation) */
-	    } /* end if (getmv) */
-	    if (rs < 0)
-		modload_close(lp) ;
-	} /* end if (ok) */
-
+	    } /* end if (error) */
+	} /* end if (vecstr-syms) */
 	return rs ;
-}
-/* end subroutine (bvs_objloadbegin) */
+} /* end subroutine (bvs_objloadbegin) */
 
-
-static int bvs_objloadend(BVS *op)
-{
+local int bvs_objloadend(bvs *op) noex {
 	int		rs = SR_OK ;
 	int		rs1 ;
-
-	if (op->obj != NULL) {
-	    rs1 = uc_free(op->obj) ;
+	if (op->obj) {
+	    rs1 = mem.free(op->obj) ;
 	    if (rs >= 0) rs = rs1 ;
-	    op->obj = NULL ;
-	}
-
-	rs1 = modload_close(&op->loader) ;
-	if (rs >= 0) rs = rs1 ;
-
+	    op->obj = nullptr ;
+	} /* end if (memory-release) */
+	if (op->mlp && op->fl.modload) {
+	    op->fl.modload = false ;
+	    rs1 = modload_close(op->mlp) ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if */
 	return rs ;
-}
-/* end subroutine (bvs_objloadend) */
+} /* end subroutine (bvs_objloadend) */
 
-
-static int bvs_loadcalls(BVS *op,cchar objname[])
-{
-	MODLOAD		*lp = &op->loader ;
-	const int	nlen = SYMNAMELEN ;
+local int bvs_loadcalls(bvs *op,vecstr *slp) noex {
+	modload		*mlp = op->mlp ;
+	cint		rsn = SR_NOTFOUND ;
 	int		rs = SR_OK ;
-	int		i ;
+	int		rs1 ;
 	int		c = 0 ;
-	char		nbuf[SYMNAMELEN + 1] ;
-	const void	*snp = NULL ;
-
-	for (i = 0 ; subs[i] != NULL ; i += 1) {
-
-	    if ((rs = sncpy3(nbuf,nlen,objname,"_",subs[i])) >= 0) {
-	         if ((rs = modload_getsym(lp,nbuf,&snp)) == SR_NOTFOUND) {
-		     snp = NULL ;
-		     if (! isrequired(i)) rs = SR_OK ;
-		}
-	    }
-
+	cchar		*sname{} ;
+	for (int i = 0 ; (rs1 = slp->get(i,&sname)) >= 0 ; i += 1) {
+	    if (cvoid *snp{} ; (rs = modload_getsym(mlp,sname,&snp)) >= 0) {
+                bvs_calls   *callp = callsp(op->callp) ;
+                c += 1 ;
+                switch (i) {
+                case sub_open:
+                    callp->open		= soopen_f(snp) ;
+                    break ;
+                case sub_count:
+                    callp->count	= socount_f(snp) ;
+                    break ;
+                case sub_getinfo:
+                    callp->getinfo	= sogetinfo_f(snp) ;
+                    break ;
+		case sub_mkmodq:
+                    callp->mkmodq	= somkmodq_f(snp) ;
+                    break ;
+                case sub_audit:
+                    callp->audit	= soaudit_f(snp) ;
+                    break ;
+                case sub_close:
+                    callp->close	= soclose_f(snp) ;
+                    break ;
+                } /* end switch */
+            } else if (rs == rsn) {
+                if (! isrequired(i)) rs = SR_OK ;
+            } /* end if (it had the call) */
 	    if (rs < 0) break ;
-
-#if	CF_DEBUGS
-	    debugprintf("bvs_loadcalls: call=%s %c\n",
-		subs[i],
-		((snp != NULL) ? 'Y' : 'N')) ;
-#endif
-
-	    if (snp != NULL) {
-	        c += 1 ;
-		switch (i) {
-		case sub_open:
-		    op->call.open = 
-			(int (*)(void *,cchar *,cchar *)) snp ;
-		    break ;
-		case sub_count:
-		    op->call.count = (int (*)(void *)) snp ;
-		    break ;
-		case sub_info:
-		    op->call.info = (int (*)(void *,BVSES_INFO *)) snp ;
-		    break ;
-		case sub_mkmodquery:
-		    op->call.mkmodquery = 
-			(int (*)(void *,BVSES_VERSE *,int)) snp ;
-		    break ;
-		case sub_audit:
-		    op->call.audit = (int (*)(void *)) snp ;
-		    break ;
-		case sub_close:
-		    op->call.close = (int (*)(void *)) snp ;
-		    break ;
-		} /* end switch */
-	    } /* end if (it had the call) */
-
-	} /* end for (subs) */
-
-#if	CF_DEBUGS
-	debugprintf("bvs_loadcalls: ret rs=%d c=%u\n",rs,c) ;
-#endif
-
+	} /* end for (vecstr_get) */
+	if ((rs >= 0) && (rs1 != rsn)) rs = rs1 ;
 	return (rs >= 0) ? c : rs ;
-}
-/* end subroutine (bvs_loadcalls) */
+} /* end subroutine (bvs_loadcalls) */
 
-
-static int isrequired(int i)
-{
-	int		f = FALSE ;
-
+local bool isrequired(int i) noex {
+	bool		f = false ;
 	switch (i) {
 	case sub_open:
 	case sub_count:
-	case sub_info:
-	case sub_mkmodquery:
+	case sub_getinfo:
+	case sub_mkmodq:
 	case sub_audit:
 	case sub_close:
-	    f = TRUE ;
+	    f = true ;
 	    break ;
 	} /* end switch */
-
 	return f ;
-}
-/* end subroutine (isrequired) */
+} /* end subroutine (isrequired) */
 
 
