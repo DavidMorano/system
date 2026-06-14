cf0cab3c1b5ea17a5b490751c19b5acd5561dae9
David A.D. Morano <david.a.morano@gmail.com>
2026-06-14 00:29:09 -0400

    update

--- src/mkindexing/progtagprinter.cc
+++ src/mkindexing/progtagprinter.cc
@@ -47,7 +47,7 @@
 *******************************************************************************/
 
 
-#include	<envstandards.h>	/* must be before others */
+#include	<envstandards.h>	/* ordered first to configure */
 
 #include	<sys/types.h>
 #include	<sys/param.h>
