8938d823c68aea00b89e3226d802cae6b1c8b89d
David A.D. Morano <david.a.morano@gmail.com>
2026-06-14 00:28:30 -0400

    update

--- src/mkindexing/progeigen.h
+++ src/mkindexing/progeigen.h
@@ -24,7 +24,7 @@
 #define	PROGEIGEN_INCLUDE	1
 
 
-#include	<envstandards.h>	/* must be before others */
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<eigendb.h>
 #include	<localmisc.h>
