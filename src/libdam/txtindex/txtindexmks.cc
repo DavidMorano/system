ae4d610bc0c66c9a014ff4bb1cdc11bc59d59760
David A.D. Morano <david.a.morano@gmail.com>
2026-06-14 00:19:32 -0400

    update

--- src/libdam/txtindex/txtindexmks.cc
+++ src/libdam/txtindex/txtindexmks.cc
@@ -819,7 +819,7 @@ local int txtindexmks_mkhash(TIM *op) noex {
 	        hdr.hfsize = rs ;
 	        if ((rs = txtindexhdr_rd(&hdr,hbuf,hlen)) >= 0) {
 	            cint	bl = rs ;
-	            if ((rs = u_pwrite(op->nfd,hbuf,bl,0L)) >= 0) {
+	            if ((rs = u_writep(op->nfd,hbuf,bl,0L)) >= 0) {
 	                cmode	om = op->om ;
 	                rs = uc_fminmod(op->nfd,om) ;
 	            }
