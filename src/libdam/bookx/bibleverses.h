42ddd55dccad717167889198e1d92e7fc33228ca
David A.D. Morano <david.a.morano@gmail.com>
2026-06-15 01:15:40 -0400

    update

--- src/libdam/bookx/bibleverses.h
+++ src/libdam/bookx/bibleverses.h
@@ -12,7 +12,6 @@
 
 /* Copyright © 2008 David A­D­ Morano.  All rights reserved. */
 
-
 #ifndef	BIBLEVERSES_INCLUDE
 #define	BIBLEVERSES_INCLUDE
 
@@ -27,7 +26,7 @@
 #define	BIBLEVERSES_FL		struct bibleverses_flags
 #define	BIBLEVERSES_OBJ		struct bibleverses_object
 #define	BIBLEVERSES_CITE	struct bibleverses_query
-#define	bibleverses_q		struct bibleverses_query
+#define	BIBLEVERSES_Q		struct bibleverses_query
 #define	BIBLEVERSES_CUR		struct bibleverses_cursor
 #define	BIBLEVERSES_INFO	struct bibleverses_information
 #define	BIBLEVERSES_MAGIC	0x99447245
@@ -54,7 +53,7 @@ struct bibleverses_query {
 } ; /* end struct */
 
 struct bibleverses_cursor {
-	BVI_CUR		vicur ;
+	BVI_CUR		*vicurp ;
 } ; /* end struct */
 
 struct bibleverses_flags {
@@ -66,18 +65,18 @@ struct bibleverses_head {
 	cchar 		*dbname ;		/* DB-name */
 	cchar 		*dbfname ;		/* DB file-name */
 	char		*mapdata ;		/* memory-map address */
-	BIBLEVERSES_FL	fl ;
-	bvi		vind ;			/* verse-index */
+	bvi		*bvip ;			/* verse-index pointer */
 	time_t		ti_db ;			/* DB file modification */
 	time_t		ti_map ;		/* DB map */
 	time_t		ti_lastcheck ;		/* last check of file */
 	time_t		ti_vind ;		/* verse-index */
 	size_t		mapsize ;		/* map size */
 	size_t		filesize ;		/* file size */
-	uint		magic ;
+	BIBLEVERSES_FL	fl ;
+	uint		magval ;
 	int		nverses ;
 	int		ncursors ;
-} ; /* end struct */
+} ; /* end struct (bibleverses_head) */
 
 typedef	BIBLEVERSES		bibleverses ;
 typedef	BIBLEVERSES_FL		bibleverses_fl ;
@@ -94,11 +93,11 @@ extern int bibleverses_count(bibleverses *) noex ;
 extern int bibleverses_read(bibleverses *,char *,int,bibleverses_q *) noex ;
 extern int bibleverses_get(bibleverses *,bibleverses_q *,char *,int) noex ;
 extern int bibleverses_curbegin(bibleverses *,bibleverses_cur *) noex ;
-extern int bibleverses_enum(bibleverses *,bibleverses_cur *,
+extern int bibleverses_curenum(bibleverses *,bibleverses_cur *,
 			bibleverses_q *,char *,int) noex ;
 extern int bibleverses_curend(bibleverses *,bibleverses_cur *) noex ;
 extern int bibleverses_audit(bibleverses *) noex ;
-extern int bibleverses_info(bibleverses *,bibleverses_info *) noex ;
+extern int bibleverses_getinfo(bibleverses *,bibleverses_info *) noex ;
 extern int bibleverses_chapters(bibleverses *,int,uchar *,int) noex ;
 extern int bibleverses_close(bibleverses *) noex ;
 
