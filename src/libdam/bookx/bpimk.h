f78d04c694eb5824f103d97c24f7d85ff6589cf7
David A.D. Morano <david.a.morano@gmail.com>
2026-06-15 01:16:02 -0400

    update

--- src/libdam/bookx/bpimk.h
+++ src/libdam/bookx/bpimk.h
@@ -2,6 +2,16 @@
 /* charset=ISO8859-1 */
 /* lang=C20 (conformance reviewed) */
 
+/* make a BPI database */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-01, David A­D­ Morano
+	This code was originally written.
+
+*/
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
