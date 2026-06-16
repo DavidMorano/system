29e7dc8155b0f103ae6f7ed9719f4a00050f2d7b
David A.D. Morano <david.a.morano@gmail.com>
2026-06-16 00:18:24 -0400

    update

--- src/bbnews/proghdr.h
+++ src/bbnews/proghdr.h
@@ -7,7 +7,7 @@
 #define	PROGHDR_INCLUDE		1
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<localmisc.h>		/* extra types */
 
