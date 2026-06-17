6a2208180fbc9568708854a4f729411b04dd1c20
David A.D. Morano <david.a.morano@gmail.com>
2026-06-17 00:18:04 -0400

    update

--- src/libprog/progexpand.cc
+++ src/libprog/progexpand.cc
@@ -55,7 +55,7 @@
 
 *******************************************************************************/
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<sys/param.h>
 #include	<sys/stat.h>
