81f5c26292866efc59c353cf12e72d3847b2adbd
David A.D. Morano <david.a.morano@gmail.com>
2026-06-18 00:22:46 -0400

    update

--- src/libdam/bookx/bookx.h
+++ src/libdam/bookx/bookx.h
@@ -23,9 +23,25 @@
 #include	<clanguage.h>		/* LIBU */
 #include	<usysbase.h>		/* LIBU */
 
-#include	<bvsbook.h>
+#include	<bcspec.h>
+#include	<biblecite.h>
+#include	<biblemta.h>
+#include	<biblecur.h>
+#include	<bibleverses.h>
+#include	<bibleverse.h>
+#include	<bpihdr.h>
+#include	<bpimk.h>
+#include	<bpi.h>
+#include	<bvcitekey.h>
+#include	<bvchapters.h>
+#include	<bvihdr.h>
+#include	<bvimk.h>
+#include	<bvi.h>
 #include	<bvshdr.h>
+#include	<bvsbook.h>
 #include	<bvsmk.h>
+#include	<bvs.h>
+#include	<bvses.h>
 
 
 #endif /* BOOKX_INCLUDE */
