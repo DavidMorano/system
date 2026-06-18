d346f4332b3519ab29938f016e8fd4db657bb183
David A.D. Morano <david.a.morano@gmail.com>
2026-06-18 00:21:58 -0400

    update

--- src/libdam/bookx/biblepara.h
+++ src/libdam/bookx/biblepara.h
@@ -27,71 +27,62 @@
 
 
 #define	BIBLEPARA		struct biblepara_head
+#define	BIBLEPARA_FL		struct biblepara_flags
 #define	BIBLEPARA_CITE		struct biblepara_query
 #define	BIBLEPARA_Q		struct biblepara_query
 #define	BIBLEPARA_CUR		struct biblepara_cursor
 #define	BIBLEPARA_INFO		struct biblepara_information
-#define	BIBLEPARA_CA		struct biblepara_calls
 #define	BIBLEPARA_MAGIC		0x99447246
 
 
+struct biblepara_flags {
+        uint		modload:1 ;
+} ; /* end struct */
+
 struct biblepara_information {
-	time_t	dbtime ;		/* db-time */
-	time_t	vitime ;		/* vi-time */
-	uint	maxbook ;
-	uint	maxchapter ;
-	uint	nverses ;
-	uint	nzverses ;
+	time_t		dbtime ;	/* db-time */
+	time_t		vitime ;	/* vi-time */
+	uint		maxbook ;
+	uint		maxchap ;
+	uint		nverses ;
+	uint		nzverses ;
 } ; /* end struct */
 
 struct biblepara_query {
-	uchar	b, c, v ;
+	uchar		b, c, v ;
 } ; /* end struct */
 
 struct biblepara_cursor {
-	void	*scp ;
-	uint	magval ;
+	void		*scp ;
+	uint		magval ;
 } ; /* end struct */
 
-EXTERNC_begin
-struct biblepara_calls {
-	int	(*open)(void *,cchar *,cchar *) ;
-	int	(*count)(void *) ;
-	int	(*ispara)(void *,BIBLEPARAS_Q *) ;
-	int	(*curbegin)(void *,BIBLEPARAS_CUR *) ;
-	int	(*enumerate)(void *,BIBLEPARAS_CUR *,BIBLEPARAS_Q *) ;
-	int	(*curend)(void *,BIBLEPARAS_CUR *) ;
-	int	(*audit)(void *) ;
-	int	(*info)(void *,BIBLEPARAS_INFO *) ;
-	int	(*close)(void *) ;
-} ;
-EXTERNC_end
-
 struct biblepara_head {
-	MODLOAD		loader ;
-	BIBLEPARA_CALLS	call ;
+	modload		*mlp ;
+	void		*callp ;
 	void		*obj ;		/* object pointer */
+	BIBLEPARA_FL	fl ;
 	uint		magval ;
 	int		objsz ;		/* object size */
 	int		cursz ;		/* cursor size */
 } ; /* end struct */
 
 typedef	BIBLEPARA		biblepara ;
+typedef	BIBLEPARA_FL		biblepara_fl ;
 typedef	BIBLEPARA_CITE		biblepara_cute ;
 typedef	BIBLEPARA_Q		biblepara_q ;
 typedef	BIBLEPARA_CUR		biblepara_cur ;
 typedef	BIBLEPARA_INFO		biblepara_info ;
-typedef	BIBLEPARA_CA		biblepara_ca ;
 
 EXTERNC_begin
 
 extern int	biblepara_open(biblepara *,cchar *,cchar *) noex ;
 extern int	biblepara_count(biblepara *) noex ;
-extern int	biblepara_ispara(biblepara *,biblepara_q *) noex ;
+extern int	biblepara_ispara(biblepara *,con biblepara_q *) noex ;
 extern int	biblepara_curbegin(biblepara *,biblepara_cur *) noex ;
+extern int	biblepara_curend(biblepara *,biblepara_cur *) noex ;
 extern int	biblepara_curenum(biblepara *,biblepara_cur *,
 			biblepara_q *) noex ;
-extern int	biblepara_curend(biblepara *,biblepara_cur *) noex ;
 extern int	biblepara_audit(biblepara *) noex ;
 extern int	biblepara_getinfo(biblepara *,biblepara_info *) noex ;
 extern int	biblepara_close(biblepara *) noex ;
