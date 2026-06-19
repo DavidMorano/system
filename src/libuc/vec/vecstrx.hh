9becdc122e8b8fef746891ec16417fdfb314358b
David A.D. Morano <david.a.morano@gmail.com>
2026-06-19 00:17:26 -0400

    update

--- src/libuc/vec/vecstrx.hh
+++ src/libuc/vec/vecstrx.hh
@@ -32,11 +32,8 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 EXTERNC_begin
@@ -47,33 +44,33 @@ EXTERNC_end
 
 struct vecstrx {
 	vecstrx &operator = (const vecstrx &) = delete ;
-	virtual int add(cchar *,int = -1) noex = 0 ;
-	virtual int adduniq(cchar *,int = -1) noex = 0 ;
-	virtual int addkeyval(cchar *,int,cchar *,int = -1) noex = 0 ;
-	virtual int insert(int,cchar *,int = -1) noex = 0 ;
-	virtual int get(int,cchar **) noex = 0 ;
-	virtual int getvec(mainv *) noex = 0 ;
-	virtual int finder(cchar *,vecstrx_f,cchar **) noex = 0 ;
-	virtual int del(int = -1) noex = 0 ;
-	virtual int sort(vecstrx_f = nullptr) noex = 0 ;
-	int addpathclean(cchar *,int) noex ;
-	int addpath(cchar *,int) noex ;
-	int addcspath() noex ;
-	int adds(cchar *,int) noex ;
-	int addsyms(cchar *,mainv) noex ;
-	int addsubdirs(cchar *) noex ;
-	int adduniqs(cchar *,int) noex ;
-	int envadd(cchar *,cchar *,int) noex ;
-	int envadds(cchar *,int) noex ;
-	int envset(cchar *,cchar *,int) noex ;
-	int envget(cchar *,cchar **) noex ;
-	int envfile(cchar *) noex ;
-	int foilcmp(vecstrx *) noex ;
-	int loadfile(int,cchar *) noex ;
-	int loadgrusers(gid_t) noex ;
-	int loadpjusers(cchar *) noex ;
-	int srvargs(cchar *) noex ;
-	int svcargs(int *,cchar *) noex ;
+	virtual int add		(cchar *,int = -1) noex = 0 ;
+	virtual int adduniq	(cchar *,int = -1) noex = 0 ;
+	virtual int addkeyval	(cchar *,int,cchar *,int = -1) noex = 0 ;
+	virtual int insert	(int,cchar *,int = -1) noex = 0 ;
+	virtual int get		(int,cchar **) noex = 0 ;
+	virtual int getvec	(mainv *) noex = 0 ;
+	virtual int finder	(cchar *,vecstrx_f,cchar **) noex = 0 ;
+	virtual int del		(int = -1) noex = 0 ;
+	virtual int sort	(vecstrx_f = nullptr) noex = 0 ;
+	int addpathclean	(cchar *,int) noex ;
+	int addpath		(cchar *,int) noex ;
+	int addcspath		() noex ;
+	int adds		(cchar *,int) noex ;
+	int addsyms		(cchar *,mainv) noex ;
+	int addsubdirs		(cchar *) noex ;
+	int adduniqs		(cchar *,int) noex ;
+	int envadd		(cchar *,cchar *,int) noex ;
+	int envadds		(cchar *,int) noex ;
+	int envset		(cchar *,cchar *,int) noex ;
+	int envget		(cchar *,cchar **) noex ;
+	int envfile		(cchar *) noex ;
+	int foilcmp		(vecstrx *) noex ;
+	int loadfile		(int,cchar *) noex ;
+	int loadgrusers		(gid_t) noex ;
+	int loadpjusers		(cchar *) noex ;
+	int srvargs		(cchar *) noex ;
+	int svcargs		(int *,cchar *) noex ;
 } ; /* end struct (vecstrx) */
 
 
