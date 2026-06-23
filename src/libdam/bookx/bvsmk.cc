7d8cafff01d90393bc9f15af496fd315f390be5a
David A.D. Morano <david.a.morano@gmail.com>
2026-06-23 00:06:31 -0400

    update

--- src/libdam/bookx/bvsmk.cc
+++ src/libdam/bookx/bvsmk.cc
@@ -76,7 +76,6 @@
 #include	<usysbase.h>		/* LIBU */
 #include	<usyscalls.h>		/* LIBU */
 #include	<endian.h>		/* LIBU */
-#include	<estrings.h>		/* LIBU */
 #include	<uclibmem.h>		/* LIBUC */
 #include	<ucmem.h>		/* LIBUC */
 #include	<ucsysmisc.h>		/* LIBUC */
@@ -86,6 +85,7 @@
 #include	<ucdesc.h>		/* LIBUC */
 #include	<ucfileop.h>		/* LIBUC */
 #include	<bufsizeget.h>		/* LIBUC */
+#include	<estrings.h>		/* LIBUC */
 #include	<vecobj.h>		/* LIBUC */
 #include	<filer.h>		/* LIBUC */
 #include	<nulstr.h>		/* LIBUC */
