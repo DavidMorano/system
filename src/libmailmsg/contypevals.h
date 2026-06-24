f656f43a02acdb11a1bb23cdfc3e09754c36477a
David A.D. Morano <david.a.morano@gmail.com>
2026-06-24 00:47:54 -0400

    update

--- src/libmailmsg/contypevals.h
+++ src/libmailmsg/contypevals.h
@@ -20,11 +20,11 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
 
 
 enum contypevals {
@@ -32,10 +32,10 @@ enum contypevals {
 	contypeval_plain,
 	contypeval_binary,
 	contypeval_overlast
-} ;
+} ; /* end enum (contypevals) */
 
 
-extern cpcchar	contypevals[] ;
+extern cpcchar	contypenames[] ;
 
 
 #endif /* CONTYPEVALS */
