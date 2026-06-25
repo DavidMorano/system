b91a27eb8bc155af53678bcdd635b4560e6f5c66
David A.D. Morano <david.a.morano@gmail.com>
2026-06-25 00:23:46 -0400

    restore

---
+++ src/libdam/bookx/strongeigens.h
@@ -0,0 +1,32 @@
+/* strongeigens HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* Strong's Eigen-Words */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2008-02-07, David A­D­ Morano
+	This code was originally written.  
+
+*/
+
+/* Copyright © 2008 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	STRONHEIGENS_INCLUDE
+#define	STRONHEIGENS_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+extern const ccharp	strongeigens[] ;
+
+
+#endif /* STRONHEIGENS_INCLUDE */
+
+
