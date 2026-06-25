340f5d8e9f50f1c041fcc8becc7824c348273c7d
David A.D. Morano <david.a.morano@gmail.com>
2026-06-25 00:11:43 -0400

    update

--- src/comsat/progerr.cc
+++ src/comsat/progerr.cc
@@ -1,18 +1,18 @@
-/* progerr */
+/* progerr SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
 /* Program-Error */
 /* version %I% last-modified %G% */
 
-
 #define	CF_DEBUGS	0		/* non-switchable print-outs */
 #define	CF_DEBUG	0		/* switchable print-outs */
-
 
 /* revision history:
 
 	= 2008-09-01, David A­D­ Morano
-        This subroutine was borrowed and modified from previous generic
-        front-end 'main' subroutines!
+	This subroutine was borrowed and modified from previous
+	generic front-end 'main' subroutines!
 
 */
 
@@ -20,19 +20,21 @@
 
 /*******************************************************************************
 
-	Here we do some multi-threaded error printing.
+  	Name:
+	progerr
 
+	Description:
+	Here we do some multi-threaded error printing.
 
 *******************************************************************************/
 
-
 #include	<envstandards.h>	/* MUST be first to configure */
-
 #include	<sys/types.h>
-#include	<cstdlib>
+#include	<cstddef>		/* |nullptr_t| */
+#include	<cstdlib>		/* |getenv(3c)| */
 #include	<cstdarg>
-
-#include	<usystem.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<bfile.h>
 #include	<fmtstr.h>
 #include	<localmisc.h>
@@ -45,23 +47,9 @@
 
 /* external subroutines */
 
-extern int	snsd(char *,int,const char *,uint) ;
-extern int	snsds(char *,int,const char *,const char *) ;
-extern int	sncpy1(char *,int,const char *) ;
-extern int	sncpy2(char *,int,const char *,const char *) ;
-extern int	mkpath1(char *,const char *) ;
-extern int	mkpath2(char *,const char *,const char *) ;
-extern int	mkpath3(char *,const char *,const char *,const char *) ;
-extern int	mkpath4(char *,cchar *,cchar *,cchar *,cchar *) ;
-extern int	mkpath1w(char *,const char *,int) ;
-extern int	matstr(const char **,const char *,int) ;
-extern int	matpstr(const char **,int,const char *,int) ;
-extern int	sfshrink(const char *,int,const char **) ;
-extern int	isNotPresent(int) ;
-
 #if	CF_DEBUGS || CF_DEBUG
-extern int	debugprintf(const char *,...) ;
-extern int	strlinelen(const char *,int,int) ;
+extern int	debugprintf(cchar *,...) noex ;
+extern int	strlinelen(cchar *,int,int) noex ;
 #endif
 
 
@@ -73,46 +61,47 @@ extern int	strlinelen(const char *,int,int) ;
 
 /* forward references */
 
-int	progerr_vprintf(PROGINFO *,cchar *,va_list) ;
-int	progerr_print(PROGINFO *,cchar *,int) ;
+int	progerr_vprintf(PROGINFO *,cchar *,va_list) noex ;
+int	progerr_print(PROGINFO *,cchar *,int) noex ;
 
 
 /* local variables */
 
 
+/* exported variables */
+
+
 /* exported subroutines */
 
-
-int progerr_begin(PROGINFO *pip)
-{
+int progerr_begin(PROGINFO *pip) noex {
 	int		rs ;
-	rs = ptm_create(&pip->efm,NULL) ;
+	{
+	    ptm *emp = &pip->efm ;
+	    rs = emp->create ;
+	}
 	return rs ;
 }
 /* end subroutine (progerr_begin) */
 
-
-int progerr_end(PROGINFO *pip)
-{
+int progerr_end(PROGINFO *pip) noex {
 	int		rs = SR_OK ;
 	int		rs1 ;
-
-	rs1 = ptm_destroy(&pip->efm) ;
-	if (rs >= 0) rs = rs1 ;
-
+	{
+	    ptm *emp = &pip->efm ;
+	    rs1 = rmp->destroy ;
+	    if (rs >= 0) rs = rs1 ;
+	}
 	return rs ;
 }
 /* end subroutine (progerr_end) */
 
-
-int progerr_printf(PROGINFO *pip,cchar *fmt,...)
-{
+int progerr_printf(PROGINFO *pip,cchar *fmt,...) noex {
+	va_list		ap ;
 	int		rs ;
 
 	if (fmt == NULL) return SR_FAULT ;
 
 	{
-	    va_list	ap ;
 	    va_begin(ap,fmt) ;
 	    rs = progerr_vprintf(pip,fmt,ap) ;
 	    va_end(ap) ;
@@ -122,11 +111,9 @@ int progerr_printf(PROGINFO *pip,cchar *fmt,...)
 }
 /* end subroutine (progerr_printf) */
 
-
 /* vprintf-like thing */
-int progerr_vprintf(PROGINFO *pip,cchar *fmt,va_list ap)
-{
-	const int	flen = LINEBUFLEN ;
+int progerr_vprintf(PROGINFO *pip,cchar *fmt,va_list ap) noex {
+	cint		flen = LINEBUFLEN ;
 	int		rs ;
 	int		wlen = 0 ;
 	char		fbuf[LINEBUFLEN+1] ;
@@ -142,18 +129,19 @@ int progerr_vprintf(PROGINFO *pip,cchar *fmt,va_list ap)
 }
 /* end subroutine (progerr_vprintf) */
 
-
-int progerr_print(PROGINFO *pip,cchar *sp,int sl)
-{
+int progerr_print(PROGINFO *pip,cchar *sp,int sl) noex {
 	int		rs ;
 	int		rs1 ;
 	int		len = 0 ;
 	if (pip == NULL) return SR_FAULT ;
 	if (sp == NULL) return SR_FAULT ;
-	if ((rs = ptm_lock(&pip->efm)) >= 0) {
-	    rs = bprintln(pip->efp,sp,sl) ;
-	    len = rs ;
-	    rs1 = ptm_unlock(&pip->efm) ;
+	ptm *emp = &pip->efm ;
+	if ((rs = emp->lockbegin) >= 0) {
+	    {
+	        rs = bprintln(pip->efp,sp,sl) ;
+	        len = rs ;
+	    }
+	    rs1 = emp->lockend ;
 	    if (rs >= 0) rs = rs1 ;
 	} /* end if (ptm) */
 	return (rs >= 0) ? len : rs ;
