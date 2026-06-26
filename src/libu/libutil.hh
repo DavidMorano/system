80701708d58b1568f951912dfee88046674189d7
David A.D. Morano <david.a.morano@gmail.com>
2026-06-26 00:20:53 -0400

    update

---
+++ src/libu/libutil.hh
@@ -0,0 +1,33 @@
+/* libutil HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* UNIX® kernel support subroutines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2020-10-13, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2020 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	LIBUTIL_INCLUDE
+#define	LIBUTIL_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* LIBUTIL_INCLUDE */
+
+
