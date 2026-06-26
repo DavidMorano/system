0808d9dcbf0325d0b13a15e6df22657e54dafbab
David A.D. Morano <david.a.morano@gmail.com>
2026-06-26 00:16:38 -0400

    restore

---
+++ src/libprog/pimkrand.hh
@@ -0,0 +1,33 @@
+/* pimkrand HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* handle the keys while processing a file */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	PIMKRAND_INCLUDE
+#define	PIMKRAND_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	"proginfo.hh"
+
+
+EXTERNC_begin
+
+extern int	pimkrand(proginfo *) noex ;
+
+EXTERNC_end
+
+
+#endif /* __cplusplus */
+#endif /* PIMKRAND_INCLUDE */
+
+
