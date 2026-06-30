e689ab54196f55f40ae9e935e4019b1f7c80c32b
David A.D. Morano <david.a.morano@gmail.com>
2026-06-30 00:09:04 -0400

    restore

---
+++ src/libuc/six/substring.h
@@ -0,0 +1,38 @@
+/* substring HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* routine to match a substring within a larger string */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SUBSTRING_INCLUDE
+#define	SUBSTRING_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int	substring(cchar *,int,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* SUBSTRING_INCLUDE */
+
+
