a118716c21e08d51c87c199717f1ecb98e12ae71
David A.D. Morano <david.a.morano@gmail.com>
2026-07-04 00:17:36 -0400

    restore

---
+++ src/libdam/bibdb/bibdb_mag.hh
@@ -0,0 +1,72 @@
+/* bibdb_mag HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* Bibliographical DataBase */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-09-10, David A­D­ Morano
+	This module was changed to serve in the REFERM program.
+
+	= 2005-10-01, David A­D­ Morano
+	This was changed to work in the MMCITE program.  The old
+	REFERM program is really obsolete.  It used a database lookup
+	strategy to remote databases.  The high-level problem is:
+	what to do if the cited BIB entry is not found?  How does a
+	maintainer of the present (local) document know what that
+	BIB entry was?  The new strategy (implemented by the MMCITE
+	program) is more like what is done with BibTeX in the TeX
+	(or LaTeX) world.  All BIB databases are really expected to
+	be maintained by the document creator -- not some centralized
+	entity.  The older centralized model reflected more the use
+	in the corporate world (where different people create BIB
+	entries) than in the more "modern" personal-responsibility
+	type of world! :-)  Anyway, this is the way the gods seem
+	to now want to do things.  Deal with it!
+
+*/
+
+/* Copyright © 1998,2005 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Object:
+	bibdb
+
+	Description:
+	This code object module implements a little DB query facility.
+	The database is a set of files that contain bibliographical
+	entries in the "REFER" format.  Queries to the database
+	that succeed return a structure with the elements of the
+	bibliographic entry.  All queries are to database files
+	that were referenced last (most previously) in the processing
+	of the document.  The idea is that a more recent reference
+	to a database file means that it is the preferred one to
+	use since it is closest to the user (or the most preferred
+	by the user).  A small nice thing about this database is
+	that database files (files containing "REFER"-formatted
+	entries) are not indexed unless they are needed (due to a
+	failure to find the query in existing indexed files).  Only
+	after all database files have been indexed and scanned to
+	try to satisfy the query (and the request os not found),
+	does the query fail.
+
+*******************************************************************************/
+
+#ifndef	BIBDBMAG_INCLUDE
+#define	BIBDBMAG_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* BIBDBMAG_INCLUDE */
+
+
