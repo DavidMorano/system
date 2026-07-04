c8f3ee13ab09d05e72adb7eba5c05fdbb2791b3f
David A.D. Morano <david.a.morano@gmail.com>
2026-07-04 00:15:48 -0400

    restore

---
+++ src/libdam/combinations.h
@@ -0,0 +1,38 @@
+/* combinations HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
+
+/* n-choose-k function WITHOUT repitition */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1994-03-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	COMBINATIONS_INCLUDE
+#define	COMBINATIONS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int	long combinations(int,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* COMBINATIONS_INCLUDE */
+
+
