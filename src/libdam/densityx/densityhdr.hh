1c146ce45947297a92d9fe2281dfa9636407367a
David A.D. Morano <david.a.morano@gmail.com>
2026-07-04 00:15:16 -0400

    restore

---
+++ src/libdam/densityx/densityhdr.hh
@@ -0,0 +1,31 @@
+/* densityhdr HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* Density-Database-Header */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	DENSTYHDR_INCLUDE
+#define	DENSTYHDR_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* DENSTYHDR_INCLUDE */
+
+
