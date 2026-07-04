af7cb0f25d81ff79ffabc4fbb239864176b812f6
David A.D. Morano <david.a.morano@gmail.com>
2026-07-04 00:12:48 -0400

    restore

---
+++ src/libdam/msgsub.hh
@@ -0,0 +1,40 @@
+/* msgsub HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* Function-Mode */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FUNMODE_INCLUDE
+#define	FUNMODE_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+enum struct msgsub {
+    	wr,
+	rd,
+	overlast
+} ; /* end enum-struct */
+
+
+#endif /* __cplusplus */
+#endif /* FUNMODE_INCLUDE */
+
+
