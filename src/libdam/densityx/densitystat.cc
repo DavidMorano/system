a810b72a1ef693d26b133ce8fc9975a6f3de603a
David A.D. Morano <david.a.morano@gmail.com>
2026-07-04 00:21:55 -0400

    update

--- src/libdam/densityx/densitystat.cc
+++ src/libdam/densityx/densitystat.cc
@@ -43,11 +43,12 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<concepts>		/* C++CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"densitystat.h"
 
