e29351337f2c7169b3f828900b71b3bfe12c7a43
David A.D. Morano <david.a.morano@gmail.com>
2026-07-04 00:19:27 -0400

    update

--- src/libdam/cgi.h
+++ src/libdam/cgi.h
@@ -20,9 +20,9 @@
 #include	<shio.h>
 
 
-#define	CGI_MAGIC	0x43628195
 #define	CGI		struct cgi_head
 #define	CGI_FL		struct cgi_flags
+#define	CGI_MAGIC	0x43628195
 /* options */
 #define	CGI_FBUFWHOLE		(1<<0)
 #define	CGI_FBUFLINE		(1<<1)
@@ -32,33 +32,33 @@
 
 struct cgi_flags {
 	uint		dummy:1 ;
-} ;
+} ; /* end struct */
 
 struct cgi_head {
 	shio		*ofp ;
 	CGI_FL		fl ;		/* currently unused! */
-	uint		magic ;
+	uint		magval ;
 	int		wlen ;
-} ;
+} ; /* end struct */
 
 typedef	CGI		cgi ;
 typedef	CGI_FL		cgi_fl ;
 
 EXTERNC_begin
 
-extern int	cgi_start(cgi *,shio *) noex ;
-extern int	cgi_finish(cgi *) noex ;
-extern int	cgi_eoh(cgi *) noex ;
-extern int	cgi_hdrdate(cgi *,time_t) noex ;
-extern int	cgi_hdr(cgi *,cchar *,cchar *,int) noex ;
-extern int	cgi_write(cgi *,const void *,int) noex ;
-extern int	cgi_println(cgi *,cchar *,int) noex ;
-extern int	cgi_vprintf(cgi *,cchar *,va_list) noex ;
-extern int	cgi_printf(cgi *,cchar *,...) noex ;
-extern int	cgi_putc(cgi *,int) noex ;
-extern int	cgi_seek(cgi *,off_t,int) noex ;
-extern int	cgi_flush(cgi *) noex ;
-extern int	cgi_reserve(cgi *,int) noex ;
+extern int	cgi_start	(cgi *,shio *) noex ;
+extern int	cgi_finish	(cgi *) noex ;
+extern int	cgi_eoh		(cgi *) noex ;
+extern int	cgi_hdrdate	(cgi *,time_t) noex ;
+extern int	cgi_hdr		(cgi *,cchar *,cchar *,int) noex ;
+extern int	cgi_write	(cgi *,const void *,int) noex ;
+extern int	cgi_println	(cgi *,cchar *,int) noex ;
+extern int	cgi_vprintf	(cgi *,cchar *,va_list) noex ;
+extern int	cgi_printf	(cgi *,cchar *,...) noex ;
+extern int	cgi_putc	(cgi *,int) noex ;
+extern int	cgi_seek	(cgi *,off_t,int) noex ;
+extern int	cgi_flush	(cgi *) noex ;
+extern int	cgi_reserve	(cgi *,int) noex ;
 
 EXTERNC_end
 
