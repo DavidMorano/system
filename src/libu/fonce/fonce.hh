a8acfafc19ca8044f967b72af44a3f393213895d
David A.D. Morano <david.a.morano@gmail.com>
2026-07-05 01:39:45 -0400

    restore

---
+++ src/libu/fonce/fonce.hh
@@ -0,0 +1,29 @@
+/* fonce HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* File-Once management */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FONCE_INCLUDE
+#define	FONCE_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* FONCE_INCLUDE */
+
+
