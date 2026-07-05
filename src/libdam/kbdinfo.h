2907e3d50165323ee9d8f7ea452c6e1e04a97b75
David A.D. Morano <david.a.morano@gmail.com>
2026-07-05 01:14:20 -0400

    update

--- src/libdam/kbdinfo.h
+++ src/libdam/kbdinfo.h
@@ -13,17 +13,18 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<usystem.h>
-#include	<keysymer.h>
-#include	<vecobj.h>
-#include	<termcmd.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<keysymer.h>		/* LIBUC */
+#include	<vecobj.h>		/* LIBUC */
+#include	<termcmd.h>		/* LIBUC */
 
 
-#define	KBDINFO_MAGIC		0x24182138
 #define	KBDINFO			struct kbdinfo_head
 #define	KBDINFO_FL		struct kbdinfo_flags
 #define	KBDINFO_ENT		struct kbdinfo_entry
 #define	KBDINFO_CUR		struct kbdinfo_cursor
+#define	KBDINFO_MAGIC		0x24182138
 /* types (enums) */
 enum kbdinfo_types {
 	kbdinfot_reg,
@@ -46,10 +47,10 @@ enum kbdinfo_types {
 
 struct kbdinfo_flags {
 	uint		dummy:1 ;	/* new mail arrived */
-} ;
+} ; /* end struct */
 
 struct kbdinfo_entry {
-	cchar		*a ;		/* the memory allocation */
+	char		*a ;		/* the memory allocation */
 	cchar		*keyname ;	/* keysym */
 	cchar		*istr ;
 	cchar		*dstr ;
@@ -58,18 +59,18 @@ struct kbdinfo_entry {
 	int		name ;		/* key name */
 	int		keynum ;	/* key number */
 	int		nparams ;	/* number of paramters */
-} ;
+} ; /* end struct */
 
 struct kbdinfo_cursor {
 	int		i, j ;
-} ;
+} ; /* end struct */
 
 struct kbdinfo_head {
 	keysymer	*ksp ;
 	vecobj		types[KBDINFO_TOVERLAST] ;
 	KBDINFO_FL	fl ;
-	uint		magic ;
-} ;
+	uint		magval ;
+} ; /* end struct */
 
 typedef	KBDINFO			kbdinfo ;
 typedef	KBDINFO_ENT		kbdinfo_ent ;
@@ -77,13 +78,13 @@ typedef	KBDINFO_CUR		kbdinfo_cur ;
 
 EXTERNC_begin
 
-extern int kbdinfo_open(kbdinfo *,keysymer *,cchar *) noex ;
-extern int kbdinfo_count(kbdinfo *) noex ;
-extern int kbdinfo_lookup(kbdinfo *,char *,int,termcmd *) noex ;
-extern int kbdinfo_curbegin(kbdinfo *,kbdinfo_cur *) noex ;
-extern int kbdinfo_curend(kbdinfo *,kbdinfo_cur *) noex ;
-extern int kbdinfo_curenum(kbdinfo *,kbdinfo_cur *,kbdinfo_ent **) noex ;
-extern int kbdinfo_close(kbdinfo *) noex ;
+extern int kbdinfo_open		(kbdinfo *,keysymer *,cchar *) noex ;
+extern int kbdinfo_count	(kbdinfo *) noex ;
+extern int kbdinfo_lookup	(kbdinfo *,char *,int,termcmd *) noex ;
+extern int kbdinfo_curbegin	(kbdinfo *,kbdinfo_cur *) noex ;
+extern int kbdinfo_curend	(kbdinfo *,kbdinfo_cur *) noex ;
+extern int kbdinfo_curenum	(kbdinfo *,kbdinfo_cur *,kbdinfo_ent **) noex ;
+extern int kbdinfo_close	(kbdinfo *) noex ;
 
 EXTERNC_end
 
