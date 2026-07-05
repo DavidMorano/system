d09a5cfa7f45d594b043f0295a085bcaf7db252f
David A.D. Morano <david.a.morano@gmail.com>
2026-07-05 01:12:50 -0400

    update

--- src/libdam/ftpsession.h
+++ src/libdam/ftpsession.h
@@ -1,53 +1,45 @@
-/* ftpsession */
+/* ftpsession HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
 
 /* FTPSESSION operations */
+/* version %I% last-modified %G% */
 
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	FTPSESSION_INCLUDE
-#define	FTPSESSION_INCLUDE	1
+#define	FTPSESSION_INCLUDE
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-
-#include	<usystem.h>
-
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 #include	<FtpLibrary.h>
 
 
 /* object defines */
-
 #define	FTPSESSION		struct ftpsession_head
 
 
 struct ftpsession_head {
-	uint		magic ;
 	FTP		*sp ;
-} ;
+	uint		magval ;
+} ; /* end struct */
 
+typedef FTPSESSION	ftpsession ;
 
-typedef struct ftpsession_head	ftpsession ;
+EXTERNC_begin
 
+extern int ftpsession_open(FTPSESSION *,char *,char *,char *,char *) noex ;
+extern int ftpsession_fileread(FTPSESSION *,char *) noex ;
+extern int ftpsession_setmode(FTPSESSION *,int) noex ;
+extern int ftpsession_read(FTPSESSION *,char *,int) noex ;
+extern int ftpsession_write(FTPSESSION *,char *,int) noex ;
+extern int ftpsession_close(FTPSESSION *) noex ;
 
-#ifndef	FTPSESSION_MASTER
+EXTERNC_end
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
-
-extern int ftpsession_open(FTPSESSION *,char *,char *,char *,char *) ;
-extern int ftpsession_fileread(FTPSESSION *,char *) ;
-extern int ftpsession_setmode(FTPSESSION *,int) ;
-extern int ftpsession_read(FTPSESSION *,char *,int) ;
-extern int ftpsession_write(FTPSESSION *,char *,int) ;
-extern int ftpsession_close(FTPSESSION *) ;
-
-#ifdef	__cplusplus
-}
-#endif
-
-#endif /* FTPSESSION_MASTER */
 
 #endif /* FTPSESSION_INCLUDE */
 
