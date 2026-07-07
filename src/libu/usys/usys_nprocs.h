a62d677c6180ec055f4f04f6ea2f4628bd468c39
David A.D. Morano <david.a.morano@gmail.com>
2026-07-07 00:37:09 -0400

    restore

---
+++ src/libu/usys/usys_nprocs.h
@@ -0,0 +1,52 @@
+/* usys_nprocs HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* miscelllaneous (XXX) operating system support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	System:
+	usys_waitid
+
+	Description:
+	This file contains the UNIX® system types that the brain-damaged
+	MacOS operating system does NOT have.  We are trying in a
+	very small way to make up for some of the immense brain-damage
+	within the Apple Darwin operating system.
+
+*******************************************************************************/
+
+#ifndef	USYSNPROCS_INCLUDE
+#define	USYSNPROCS_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+
+
+namespace usys {
+    extern sysret_t	usys_nprocs() noex ;
+}
+
+
+#endif /* __cplusplus */
+#endif /* USYSNPROCS_INCLUDE */
+
+
