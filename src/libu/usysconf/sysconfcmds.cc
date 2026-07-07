a186e48e36d415e7984ba115777993cc0612c672
David A.D. Morano <david.a.morano@gmail.com>
2026-07-07 00:36:40 -0400

    restore

---
+++ src/libu/usysconf/sysconfcmds.cc
@@ -0,0 +1,73 @@
+/* sysconfcmds SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* standard (relatively) UNIX® preprocessor defiitions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-15, David A­D­ Morano
+	This code is newly written but modeled after some code that
+	was originally written to get some standardized variable
+	serialization for the Ethernet-Controller (ETHCON) development
+	work.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	sysconfcmds
+
+	Description:
+	These are some of the commands to the |sysconf(2)| system
+	call that may not actually exist on the current system.
+	For those commands that do not exit on the current system
+	(this system), they are emulated.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"sysconfcmds.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
