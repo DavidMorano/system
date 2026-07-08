4254b6ac71de10b708ee40e494717c8a4af3e48c
David A.D. Morano <david.a.morano@gmail.com>
2026-07-08 00:22:13 -0400

    update

--- src/libdam/linex.h
+++ src/libdam/linex.h
@@ -20,11 +20,8 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 #include	<linebuffer.h>
 #include	<linecenter.h>
