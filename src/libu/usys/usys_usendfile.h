4ac0ba56e6745809558c0d86dd09fcf3b6e505e1
David A.D. Morano <david.a.morano@gmail.com>
2026-07-09 00:28:29 -0400

    update

--- src/libu/usys/usys_usendfile.h
+++ src/libu/usys/usys_usendfile.h
@@ -45,12 +45,12 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<dirent.h>		/* |dirent| */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<dirent.h>		/* POSIX® |dirent| */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
 
 
 namespace usys {
