6b4ec7e20bdbd52c939874ebc6f63a8c83fe07cf
David A.D. Morano <david.a.morano@gmail.com>
2026-07-12 00:24:43 -0400

    restore

---
+++ src/libu/usysconf/usysconfcheck.hh
@@ -0,0 +1,31 @@
+/* usysconfcheck HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* system configuration cached check */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USYSCONFCHECK_INCLUDE
+#define	USYSCONFCHECK_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* USYSCONFCHECK_INCLUDE */
+
+
