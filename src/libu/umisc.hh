0c8bfcfc6000974f6b66dc04b16ad97db0264940
David A.D. Morano <david.a.morano@gmail.com>
2026-07-12 00:24:57 -0400

    restore

---
+++ src/libu/umisc.hh
@@ -0,0 +1,31 @@
+/* umisc HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* LIBU miscellaneous interfaces */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UMISC_INCLUDE
+#define	UMISC_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#endif /* __cplusplus */
+#endif /* UMISC_INCLUDE */
+
+
