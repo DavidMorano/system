369f34efa936aa321fc1674845db50ede94e5766
David A.D. Morano <david.a.morano@gmail.com>
2026-07-12 00:25:27 -0400

    restore

---
+++ src/libu/charnames.h
@@ -0,0 +1,34 @@
+/* charnames HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* names (c-strings) associated with ISO-Latin-1 character codes */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	Originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CHARNAMES_INCLUDE
+#define	CHARNAMES_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+extern cchar	(*charname)[4] ;
+
+
+#endif /* CHARNAMES_INCLUDE */
+
+
