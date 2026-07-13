f570bbb6a1be6389c35ad548b11fd8a7f89284ec
David A.D. Morano <david.a.morano@gmail.com>
2026-07-13 01:02:03 -0400

    restore

---
+++ src/libdam/timer.hh
@@ -0,0 +1,24 @@
+/* timer HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* TIMER object */
+/* version %I% last-modified %G% */
+
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	TIMER_INCLUDE
+#define	TIMER_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* TIMER_INCLUDE */
+
+
