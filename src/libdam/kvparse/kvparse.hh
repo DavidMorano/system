e08629293e4c0d1ca0637d21d9521b2374e8a4c3
David A.D. Morano <david.a.morano@gmail.com>
2026-07-13 00:58:45 -0400

    restore

---
+++ src/libdam/kvparse/kvparse.hh
@@ -0,0 +1,31 @@
+/* kvparse HEADER (Key-Value Parse) */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* key-value parsing */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-06-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	KVPARSE_INCLUDE
+#define	KVPARSE_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* KVPARSE_INCLUDE */
+
+
