2b6523c9613ed5de0d83270f3e0c9a6d0f08d062
David A.D. Morano <david.a.morano@gmail.com>
2026-07-13 00:55:20 -0400

    update

--- src/libuc/ctx/uconstants.hh
+++ src/libuc/ctx/uconstants.hh
@@ -1,6 +1,6 @@
 /* uconstants HEADER */
 /* charset=ISO8859-1 */
-/* lang=C20 */
+/* lang=C++20 */
 
 /* UNIX® global constants */
 /* version %I% last-modified %G% */
@@ -17,6 +17,7 @@
 
 #ifndef	UCONSTABTS_INCLUDE
 #define	UCONSTABTS_INCLUDE
+#ifdef	__cplusplus	/* C++ only! */
 
 
 #include	<envstandards.h>	/* ordered first to configure */
@@ -26,6 +27,7 @@
 #include	<usysdefs.h>		/* LIBU */
 
 
+#endif /* __cplusplus (C++ only) */
 #endif /* UCONSTABTS_INCLUDE */
 
 
