1dfa40a0a490bffb9ef17dc3e1a0a12678c2ae50
David A.D. Morano <david.a.morano@gmail.com>
2026-07-13 00:51:32 -0400

    restore

---
+++ src/libuc/intenum.hh
@@ -0,0 +1,31 @@
+/* intenum HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* integer enumeration */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	INTENUM_INCLUDE
+#define	INTENUM_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* INTENUM_INCLUDE */
+
+
