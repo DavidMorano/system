9f679951995889037110af45584838c4459e4499
David A.D. Morano <david.a.morano@gmail.com>
2026-07-13 01:07:50 -0400

    update

--- src/libpcs/pcsdialuucp.cc
+++ src/libpcs/pcsdialuucp.cc
@@ -59,7 +59,7 @@
 #include	<bfile.h>
 #include	<sbuf.h>
 #include	<vecstr.h>
-#include	<vstrxcmp.h>		/* |vstrkeycmp(3uc)| */
+#include	<vstrcmp.h>		/* |vstrkeycmp(3uc)| */
 #include	<exitcodes.h>
 #include	<localmisc.h>
 
