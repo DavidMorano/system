ee1b0c6730dfa9e5432140ea672b981c6dad2f9d
David A.D. Morano <david.a.morano@gmail.com>
2026-07-14 00:22:50 -0400

    update

--- src/libu/ufiledesc/uselect.cc
+++ src/libu/ufiledesc/uselect.cc
@@ -1,6 +1,6 @@
 /* uselect SUPPORT (UNIX®-At-Xntthing) */
 /* charset=ISO8859-1 */
-/* lang=C20 */
+/* lang=C++20 */
 
 /* translation layer interface for UNIX® equivalents */
 /* version %I% last-modified %G% */
