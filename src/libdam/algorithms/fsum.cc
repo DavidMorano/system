75c4ebb078727191cca15eb1fd9646d0e87eb6dd
David A.D. Morano <david.a.morano@gmail.com>
2026-07-14 00:14:33 -0400

    update

--- src/libdam/algorithms/fsum.cc
+++ src/libdam/algorithms/fsum.cc
@@ -37,11 +37,13 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"fsum.h"
 
@@ -81,7 +83,6 @@ double fsum(double *a,int n) noex {
 	    fsum += a[i] ;
 	} /* end for */
 	return fsum ;
-}
-/* end subroutine (fsum) */
+} /* end subroutine (fsum) */
 
 
