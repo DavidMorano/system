961fda765ea8e27a6643d429171ab5e9ac6402a8
David A.D. Morano <david.a.morano@gmail.com>
2026-07-14 00:17:41 -0400

    update

--- src/libdam/randmwc.h
+++ src/libdam/randmwc.h
@@ -1,17 +1,20 @@
-/* randmwc */
+/* randmwc HEADER (RNG Multiply-With-Carry) */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* random number generator (Multiply-With-Carry) */
+/* version %I% last-modified %G% */
 
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	RANDMWC_INCLUDE
-#define	RANDMWC_INCLUDE	1
+#define	RANDMWC_INCLUDE
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-
-#include	<sys/types.h>
-
-#include	<localmisc.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 #define	RANDMWC		struct randmwc_head
@@ -19,25 +22,19 @@
 
 struct randmwc_head {
 	uint		a ;
-	ULONG		x, c ;
-} ;
+	ulong		x, c ;
+} ; /* end struct */
 
+typedef	RANDMWC		randmwc ;
 
-#if	(! defined(RANDMWC_MASTER)) || (RANDMWC_MASTER == 0)
+EXTERNC_begin
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
+extern int randmwc_start	(randmwc *,int,uint) noex ;
+extern int randmwc_finish	(randmwc *) noex ;
+extern int randmwc_getulong	(randmwc *,ulong *) noex ;
 
-extern int	randmwc_start(RANDMWC *,int,uint) ;
-extern int	randmwc_finish(RANDMWC *) ;
-extern int	randmwc_getulong(RANDMWC *,ULONG *) ;
+EXTERNC_end
 
-#ifdef	__cplusplus
-}
-#endif
-
-#endif
 
 #endif /* RANDMWC_INCLUDE */
 
