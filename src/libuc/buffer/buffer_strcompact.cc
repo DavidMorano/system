e3a0ddac7a29e477b269491b12fe1509317169c2
David A.D. Morano <david.a.morano@gmail.com>
2026-07-16 01:40:36 -0400

    update

--- src/libuc/buffer/buffer_strcompact.cc
+++ src/libuc/buffer/buffer_strcompact.cc
@@ -40,13 +40,13 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<ascii.h>
-#include	<sfx.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<ascii.h>		/* LIBU */
+#include	<sfx.h>			/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"buffer.h"
 
@@ -103,7 +103,6 @@ int buffer_strcompact(buffer *op,cchar *sp,int µsl) noex {
 	    } /* end for */
 	} /* end if (getlenstr) */
 	return (rs >= 0) ? len : rs ;
-}
-/* end subroutine (buffer_strcompact) */
+} /* end subroutine (buffer_strcompact) */
 
 
