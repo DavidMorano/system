19d92bf70d4981af2693cf8b7ddb37f6d10697ae
David A.D. Morano <david.a.morano@gmail.com>
2026-07-16 01:30:16 -0400

    update

--- src/libu/usupport/usupport_hasnot.cc
+++ src/libu/usupport/usupport_hasnot.cc
@@ -49,12 +49,12 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<cstddef>		/* CSTD */
 #include	<cstdlib>		/* CSTD */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<ascii.h>
-#include	<mkchar.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<ascii.h>		/* LIBU */
+#include	<mkchar.h>		/* LIBU */
 #include	<localmisc.h>		/* LIBU |UC(3u)| */
 
 #include	"usupport_hasnot.hh"
