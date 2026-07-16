3e3a8a04d43cd220b5ad35d022ed2140b4e91c2e
David A.D. Morano <david.a.morano@gmail.com>
2026-07-16 01:39:15 -0400

    restore

---
+++ src/libu/utypedefs_mintpp.h
@@ -0,0 +1,37 @@
+/* utypedefs_mintpp HEADER (Integers-Mutable) */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* virtual-system definitions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+	= 2017-08-01, David A­D­ Morano
+	Updated for lack of interfaces in MacOS Darwin
+
+*/
+
+/* Copyright © 1998,2017 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UTYPEDEFSMINTPP_INCLUDE
+#define	UTYPEDEFSMINTPP_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<usysnative.h>		/* useful native system headers */
+#include	<clanguage.h>		/* <- necessary inclusion */
+#include	<stdintx.h>		/* extended integer types */
+#include	<syspredefined.h>	/* system pre-defined types */
+
+#include	<utypedefs_msintpp.h>
+#include	<utypedefs_muintpp.h>
+
+
+#endif /* UTYPEDEFSMINTPP_INCLUDE */
+
+
