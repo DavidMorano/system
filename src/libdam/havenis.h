53310b2e236250a3bb79d6e36e19605d2f191eb4
David A.D. Morano <david.a.morano@gmail.com>
2026-07-17 01:48:08 -0400

    restore

---
+++ src/libdam/havenis.h
@@ -0,0 +1,36 @@
+/* havenis HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* format a number for a three-column-wide field for the VMAIL program */
+/* version %I% last-modified %G% */
+
+
+/* revistion history:
+
+	= 1998-09-01, Dave morano
+	This was originally made for the PCSPOLL program.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	HAVENIS_INCLUDE
+#define	HAVENIS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int	havenis(char *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* HAVENIS_INCLUDE */
+
+
