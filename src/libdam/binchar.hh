4b78c226d517ac789ff8d8c2c59667af4ed060e8
David A.D. Morano <david.a.morano@gmail.com>
2026-07-17 01:48:58 -0400

    restore

---
+++ src/libdam/binchar.hh
@@ -0,0 +1,33 @@
+/* binchar HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* return the charater-digit corresponding to a specified bit in an integer */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2020-05-02, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2020 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	BINCHAR_INCLUDE
+#define	BINCHAR_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* BINCHAR_INCLUDE */
+
+
