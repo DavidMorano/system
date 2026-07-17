8f6ff7b09caaab9792b55892aca453f568fd8ad7
David A.D. Morano <david.a.morano@gmail.com>
2026-07-17 01:50:18 -0400

    update

--- src/libdam/chowns.h
+++ src/libdam/chowns.h
@@ -20,12 +20,9 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<fcntl.h>		/* |mode_t| */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<fcntl.h>		/* POSIX® |mode_t| */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 EXTERNC_begin
