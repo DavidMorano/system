c2e448da7c6ceaa3390f8b10d65ff7a0cb487932
David A.D. Morano <david.a.morano@gmail.com>
2026-07-17 01:50:27 -0400

    update

--- src/libdam/cvttemperature.cc
+++ src/libdam/cvttemperature.cc
@@ -17,15 +17,36 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
 
 #include	"cvttemperature.h"
 
 
 /* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
 
 
 /* exported variables */
@@ -36,13 +57,11 @@
 /* convert degrees Fahrenheit to Centigrade */
 double cvtfahtocen(double fah) noex {
 	return ((fah - 32.0) * 5.0 / 9.0) ;
-}
-/* end subroutine (cvtfahtocen) */
+} /* end subroutine (cvtfahtocen) */
 
 /* convert degrees Centigrade to Fahrenheit */
 double cvtcentofah(double cen) noex {
 	return ((cen * 9.0 / 5.0) + 32.0) ;
-}
-/* end subroutine (cvtcentofah) */
+} /* end subroutine (cvtcentofah) */
 
 
