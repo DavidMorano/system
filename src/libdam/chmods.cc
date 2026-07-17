154490712198ffbc2b9aad757327146d8841d77a
David A.D. Morano <david.a.morano@gmail.com>
2026-07-17 01:50:04 -0400

    update

--- src/libdam/chmods.cc
+++ src/libdam/chmods.cc
@@ -40,16 +40,20 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/types.h>		/* |uid_t| */
-#include	<sys/stat.h>
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<cstring>		/* |strchr(3c)| + |strcmp(3c)| */
-#include	<usystem.h>
-#include	<mallocxx.h>
-#include	<mkpathx.h>
-#include	<strwcpy.h>
-#include	<localmisc.h>
+#include	<sys/types.h>		/* POSIX® |uid_t| */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD |strchr(3c)| + |strcmp(3c)| */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<uclibmem.h>		/* LIBUC */
+#include	<mkpathx.h>		/* LIBUC */
+#include	<strwcpy.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"chmods.h"
 
@@ -57,6 +61,14 @@
 /* local defines */
 
 #define	CI		xcheckinfo
+
+
+/* imported namespaces */
+
+using libuc::libmem ;			/* variable */
+
+
+/* local typedefs */
 
 
 /* external subroutines */
@@ -70,14 +82,14 @@
 struct xcheckinfo {
 	uid_t		uid ;
 	mode_t		dm ;
-} ;
+} ; /* end struct */
 
 
 /* forward references */
 
-static int	checkinfo_start(CI *,mode_t) noex ;
-static int	checkinfo_dir(CI *,cchar *) noex ;
-static int	checkinfo_finish(CI *) noex ;
+local int	checkinfo_start		(CI *,mode_t) noex ;
+local int	checkinfo_dir		(CI *,cchar *) noex ;
+local int	checkinfo_finish	(CI *) noex ;
 
 
 /* local variables */
@@ -92,9 +104,9 @@ int chmods(cchar *dname,mode_t dm) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
 	int		c = 0 ; /* return-value */
-	if (dname) {
+	if (dname) ylikely {
 	    if (CI ci ; (rs = checkinfo_start(&ci,dm)) >= 0) {
-	        if (char *dbuf ; (rs = malloc_mp(&dbuf)) >= 0) {
+	        if (char *dbuf ; (rs = libmem.mp(&dbuf)) >= 0) {
 	            if ((rs = mkpath1(dbuf,dname)) >= 0) {
 		        char	*cp, *cp2 ;
 	                cp = dbuf ;
@@ -113,34 +125,33 @@ int chmods(cchar *dname,mode_t dm) noex {
 			    c += rs ;
 	                }
 	            } /* end if (mkpath) */
-	            rs = rsfree(rs,dbuf) ;
+	            rs1 = libmem.free(dbuf) ;
+	            if (rs >= 0) rs = rs1 ;
 	        } /* end if (m-a-f) */
 	        rs1 = checkinfo_finish(&ci) ;
 	        if (rs >= 0) rs = rs1 ;
 	    } /* end if (checkinfo) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? c : rs ;
-}
-/* end subroutine (chmods) */
+} /* end subroutine (chmods) */
 
 
 /* local subroutines */
 
-static int checkinfo_start(CI *cip,mode_t dm) noex {
+local int checkinfo_start(CI *cip,mode_t dm) noex {
     	int		rs = SR_FAULT ;
-	if (cip) {
+	if (cip) ylikely {
 	    cip->dm = dm ;
 	    cip->uid = getuid() ;
 	    rs = SR_OK ;
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (checkinfo_start) */
+} /* end subroutine (checkinfo_start) */
 
-static int checkinfo_dir(CI *cip,cchar *dirbuf) noex {
+local int checkinfo_dir(CI *cip,cchar *dirbuf) noex {
 	int		rs ;
-	int		c = 0 ;
-	if (ustat sb ; (rs = u_stat(dirbuf,&sb)) >= 0) {
+	int		c = 0 ; /* return-value */
+	if (ustat sb ; (rs = u_stat(dirbuf,&sb)) >= 0) ylikely {
 	    if (sb.st_uid == cip->uid) {
 	        if ((sb.st_mode & cip->dm) != cip->dm) {
 		    cmode	nm = (sb.st_mode | cip->dm) ;
@@ -148,14 +159,12 @@ static int checkinfo_dir(CI *cip,cchar *dirbuf) noex {
 		    c = 1 ;
 	        }
 	    }
-	} /* end if */
+	} /* end if (u_stat) */
 	return (rs >= 0) ? c : rs ;
-}
-/* end subroutine (checkinfo_dir) */
+} /* end subroutine (checkinfo_dir) */
 
-static int checkinfo_finish(CI *) noex {
+local int checkinfo_finish(CI *) noex {
 	return SR_OK ;
-}
-/* end subroutine (checkinfo_finish) */
+} /* end subroutine (checkinfo_finish) */
 
 
