ae1504dd9cdfed7fc7d1756178e075c7eb4052b0
David A.D. Morano <david.a.morano@gmail.com>
2026-07-18 02:34:04 -0400

    restore

---
+++ src/libdam/minmaxelem.hh
@@ -0,0 +1,40 @@
+/* minmaxelem HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* calculate the minimum and maximum at the same time */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+	= 2018-09-15, David A­D­ Morano
+        This was separated out from where it was to make a stand-alone
+        (exported) subroutine (subroutine-template as the case is).
+
+	= 2020-05-07, David A-D- Morano
+	I converted this (formerly a header-only file) to a module.
+
+*/
+
+/* Copyright © 2000,2018,2020 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MINMAXELEM_INCLUDE
+#define	MINMAXELEM_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* MINMAXELEM_INCLUDE */
+
+
