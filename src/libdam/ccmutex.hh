5692930fce3cba9a5fea56126fc7448fd5ab2763
David A.D. Morano <david.a.morano@gmail.com>
2026-07-18 02:35:10 -0400

    restore

---
+++ src/libdam/ccmutex.hh
@@ -0,0 +1,36 @@
+/* ccmutex HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* CCMUTEX object */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-12-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+	= 2020-05-07, David A-D- Morano
+	I converted this (formerly a header-only file) to a module.
+
+*/
+
+/* Copyright © 1998,2020 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CCMUTES_INCLUDE
+#define	CCMUTES_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* CCMUTES_INCLUDE */
+
+
