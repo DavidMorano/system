9f65a633c215c871d7068968a2f6ed98a3757ddf
David A.D. Morano <david.a.morano@gmail.com>
2026-07-18 02:35:03 -0400

    restore

---
+++ src/libdam/cmdutils.hh
@@ -0,0 +1,31 @@
+/* cmdutils HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* command utilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-06-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CMDUTILS_INCLUDE
+#define	CMDUTILS_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* CMDUTILS_INCLUDE */
+
+
