04e96e234ab8492f089bb105c1fb24e76c37b0d8
David A.D. Morano <david.a.morano@gmail.com>
2026-07-18 02:37:08 -0400

    restore

---
+++ src/libdam/systypes.h
@@ -0,0 +1,26 @@
+/* systypes HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* system types (w/ a different name) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+   	= 1002-04-18, David A-D- Morano
+	This is just for (some sort of) convenience.
+*/
+
+/* Copyright © 1992 David A­D­ Morano.  All rights reserved. */
+/* Use is subject to license terms. */
+
+#ifndef	SSTYPES_INCLUDE
+#define	SSTYPES_INCLUDE
+
+
+#include	<sys/types.h>
+
+
+#endif /* SSTYPES_INCLUDE */
+
+
