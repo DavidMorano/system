930c42147db3d710f8bfec46744c99c9196ce592
David A.D. Morano <david.a.morano@gmail.com>
2026-07-18 02:38:51 -0400

    restore

---
+++ src/librsa/librsa.h
@@ -0,0 +1,39 @@
+/* librsa HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* library of RSA functions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-13, David A-D- Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/*******************************************************************************
+
+  	Description:
+	This is the header for a library containing various RSA 
+	(encryption) functions.
+
+*******************************************************************************/
+
+#ifndef	LIBRSA_INCLUDE
+#define	LIBRSA_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+#include	<md5c.h>
+
+
+#endif /* LIBRSA_INCLUDE */
+
+
