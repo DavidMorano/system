716882aa1abae004bf62acf166c5a2c5c2a7f374
David A.D. Morano <david.a.morano@gmail.com>
2026-07-18 02:27:05 -0400

    restore

---
+++ src/libdam/rshiftx.hh
@@ -0,0 +1,33 @@
+/* rshiftx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* right-shift an integer and return the shifted result */
+/* lang=C++20 (conformance reviewed) */
+
+
+/* revision history:
+
+	= 2020-05-02, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2020 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	RSHIFTX_INCLUDE
+#define	RSHIFTX_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* RSHIFTX_INCLUDE */
+
+
