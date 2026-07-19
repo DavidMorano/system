88ce9297165457aae0902a8f8e16d681acfae543
David A.D. Morano <david.a.morano@gmail.com>
2026-07-19 00:37:49 -0400

    update

--- src/libdam/rmsesfiles.cc
+++ src/libdam/rmsesfiles.cc
@@ -54,35 +54,35 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<sys/stat.h>
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<cstring>		/* |lenstr(3c)| */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
-#include	<uclibmem.h>
-#include	<ucdesc.h>		/* |uc_lockf(3uc)| */
-#include	<ucprochave.h>		/* |uc_prochave(3uc)| */
-#include	<sigblocker.h>
-#include	<cfdec.h>
-#include	<ids.h>
-#include	<fsdir.h>
-#include	<vecpstr.h>
-#include	<permx.h>
-#include	<removes.h>
-#include	<pathadd.h>
-#include	<mkpathx.h>
-#include	<sfx.h>
-#include	<strnul.hh>
-#include	<mkchar.h>
-#include	<hasx.h>
-#include	<isoneof.h>
-#include	<isnot.h>
-#include	<localmisc.h>
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<uclibmem.h>		/* LIBUC */
+#include	<ucdesc.h>		/* LIBUC |uc_lockf(3uc)| */
+#include	<ucprochave.h>		/* LIBUC |uc_prochave(3uc)| */
+#include	<sigblocker.h>		/* LIBUC */
+#include	<cfdec.h>		/* LIBUC */
+#include	<ids.h>			/* LIBUC */
+#include	<fsdir.h>		/* LIBUC */
+#include	<vecpstr.h>		/* LIBUC */
+#include	<permx.h>		/* LIBUC */
+#include	<removes.h>		/* LIBUC */
+#include	<pathadd.h>		/* LIBUC */
+#include	<mkpathx.h>		/* LIBUC */
+#include	<sfx.h>			/* LIBUC */
+#include	<strnul.hh>		/* LIBUC */
+#include	<mkchar.h>		/* LIBUC */
+#include	<hasx.h>		/* LIBUC */
+#include	<isoneof.h>		/* LIBUC */
+#include	<isnot.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"rmsesfiles.h"
 
@@ -114,22 +114,22 @@ struct openstate {
 
 /* forward references */
 
-static int	rmsesfiler(ids *,char *,cchar *) noex ;
+local int	rmsesfiler(ids *,char *,cchar *) noex ;
 
-static int	lockbegin(char *,int) noex ;
-static int	lockend(char *,int,int) noex ;
+local int	lockbegin(char *,int) noex ;
+local int	lockend(char *,int,int) noex ;
 
-static int	rmsesdir(ids *,char *,int) noex ;
-static int	rmdirfiles(char *,int) noex ;
+local int	rmsesdir(ids *,char *,int) noex ;
+local int	rmdirfiles(char *,int) noex ;
 
-static int	vecpstr_dirload(vecpstr *,char *,int) noex ;
-static int	vecpstr_dirdel(vecpstr *,char *,int) noex ;
+local int	vecpstr_dirload(vecpstr *,char *,int) noex ;
+local int	vecpstr_dirdel(vecpstr *,char *,int) noex ;
 
-static int	openstate_open(openstate *,cchar *,int,mode_t) noex ;
-static int	rmfile(cchar *) noex ;
+local int	openstate_open(openstate *,cchar *,int,mode_t) noex ;
+local int	rmfile(cchar *) noex ;
+local int	NotRunning(cchar *,int) noex ;
 
-static bool	isLocked(int) noex ;
-static bool	isNotRunning(cchar *,int) noex ;
+local bool	isLocked(int) noex ;
 
 
 /* local variables */
@@ -169,13 +169,12 @@ int rmsesfiles(cchar *dname) noex {
 	    } /* end if (valid) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? c : rs ;
-}
-/* end subroutine (rmsesfiles) */
+} /* end subroutine (rmsesfiles) */
 
 
 /* local subroutines */
 
-static int rmsesfiler(ids *idp,char *pbuf,cchar *dname) noex {
+local int rmsesfiler(ids *idp,char *pbuf,cchar *dname) noex {
 	int		rs ;
 	int		rs1 ;
 	int		c = 0 ;
@@ -217,10 +216,9 @@ static int rmsesfiler(ids *idp,char *pbuf,cchar *dname) noex {
 	    } /* end if (m-a-f) */
 	} /* end if (mkpath) */
 	return (rs >= 0) ? c : rs ;
-}
-/* end subroutine (rmsesfiler) */
+} /* end subroutine (rmsesfiler) */
 
-static int lockbegin(char *pbuf,int plen) noex {
+local int lockbegin(char *pbuf,int plen) noex {
 	int		rs ;
 	int		lfd = -1 ;
 	cchar		*lfn = RMSESFILES_LOCKFILE ;
@@ -245,10 +243,9 @@ static int lockbegin(char *pbuf,int plen) noex {
 	    pbuf[plen] = '\0' ; /* <- restore */
 	} /* end if (pathadd) */
 	return (rs >= 0) ? lfd : rs ;
-}
-/* end subroutine (lockbegin) */
+} /* end subroutine (lockbegin) */
 
-static int lockend(char *pbuf,int plen,int lfd) noex {
+local int lockend(char *pbuf,int plen,int lfd) noex {
 	int		rs ;
 	cchar		*lfn = RMSESFILES_LOCKFILE ;
 	if ((rs = pathadd(pbuf,plen,lfn)) >= 0) {
@@ -256,10 +253,9 @@ static int lockend(char *pbuf,int plen,int lfd) noex {
 	    u_close(lfd) ;
 	}
 	return rs ;
-}
-/* end subroutine (lockend) */
+} /* end subroutine (lockend) */
 
-static int rmsesdir(ids *idp,char *pbuf,int plen) noex {
+local int rmsesdir(ids *idp,char *pbuf,int plen) noex {
 	int		rs ;
 	int		c = 0 ;
 	if (ustat sb ; (rs = u_stat(pbuf,&sb)) >= 0) {
@@ -270,7 +266,7 @@ static int rmsesdir(ids *idp,char *pbuf,int plen) noex {
 	                cchar	*cp{} ;
 	                c += rs ;
 	                if (int cl ; (cl = sfbasename(pbuf,plen,&cp)) > 0) {
-	                    if ((rs = isNotRunning(cp,cl)) > 0) {
+	                    if ((rs = NotRunning(cp,cl)) > 0) {
 	                        rs = removes(pbuf) ;
 	                    }
 	                }
@@ -283,10 +279,9 @@ static int rmsesdir(ids *idp,char *pbuf,int plen) noex {
 	    rs = SR_OK ;
 	} /* end if (stat) */
 	return (rs >= 0) ? c : rs ;
-}
-/* end subroutine (rmsesdir) */
+} /* end subroutine (rmsesdir) */
 
-static int rmdirfiles(char *pbuf,int plen) noex {
+local int rmdirfiles(char *pbuf,int plen) noex {
 	int		rs ;
 	int		rs1 ;
 	int		rc = 0 ;
@@ -311,10 +306,9 @@ static int rmdirfiles(char *pbuf,int plen) noex {
 	    } /* end if (valid) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? rc : rs ;
-}
-/* end subroutine (rmdirfiles) */
+} /* end subroutine (rmdirfiles) */
 
-static int vecpstr_dirload(vecpstr *flp,char *pbuf,int plen) noex {
+local int vecpstr_dirload(vecpstr *flp,char *pbuf,int plen) noex {
 	int		rs = SR_NOMEM ;
 	int		rs1 ;
 	int		c = 0 ;
@@ -341,10 +335,9 @@ static int vecpstr_dirload(vecpstr *flp,char *pbuf,int plen) noex {
 	    } /* end if (m-a-f) */
 	} /* end if (strnul) */
 	return (rs >= 0) ? c : rs ;
-}
-/* end subroutine (vecpstr_dirload) */
+} /* end subroutine (vecpstr_dirload) */
 
-static int vecpstr_dirdel(vecpstr *flp,char *pbuf,int plen) noex {
+local int vecpstr_dirdel(vecpstr *flp,char *pbuf,int plen) noex {
 	int		rs = SR_OK ;
 	int		c = 0 ;
 	cchar		*sp ;
@@ -352,10 +345,10 @@ static int vecpstr_dirdel(vecpstr *flp,char *pbuf,int plen) noex {
 	    if ((sp != nullptr) && (sp[0] != '\0')) {
 	        if ((rs = pathadd(pbuf,plen,sp)) >= 0) {
 	            if (sp[0] == 'p') {
-	                if ((rs = isNotRunning(sp,-1)) > 0) {
+	                if ((rs = NotRunning(sp,-1)) > 0) {
 	                    rs = rmfile(pbuf) ;
 	                    c += rs ;
-	                } /* end if (isNotRunning) */
+	                } /* end if (NotRunning) */
 	            } else {
 	                rs = rmfile(pbuf) ;
 	                c += rs ;
@@ -366,10 +359,9 @@ static int vecpstr_dirdel(vecpstr *flp,char *pbuf,int plen) noex {
 	} /* end for */
 	pbuf[plen] = '\0' ; /* restore */
 	return (rs >= 0) ? c : rs ;
-}
-/* end subroutine (vecpstr_dirdel) */
+} /* end subroutine (vecpstr_dirdel) */
 
-static int openstate_open(openstate *lsp,cchar *fn,int of,mode_t om) noex {
+local int openstate_open(openstate *lsp,cchar *fn,int of,mode_t om) noex {
 	int		rs = SR_FAULT ;
 	if (lsp && fn) {
 	    rs = SR_INVALID ;
@@ -383,10 +375,9 @@ static int openstate_open(openstate *lsp,cchar *fn,int of,mode_t om) noex {
 	    } /* end if (valid) */
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (openstate_open) */
+} /* end subroutine (openstate_open) */
 
-static int rmfile(cchar *fn) noex {
+local int rmfile(cchar *fn) noex {
 	int		rs ;
 	if ((rs = u_unlink(fn)) >= 0) {
 	    rs = 1 ;
@@ -394,15 +385,9 @@ static int rmfile(cchar *fn) noex {
 	    rs = SR_OK ;
 	}
 	return rs ;
-}
-/* end subroutine (rmfile) */
+} /* end subroutine (rmfile) */
 
-static bool isLocked(int rs) noex {
-	return isOneOf(rslocked,rs) ;
-}
-/* end subroutine (isLocked) */
-
-static bool isNotRunning(cchar *sp,int µsl) noex {
+local int NotRunning(cchar *sp,int µsl) noex {
 	int		rs = SR_FAULT ;
 	int		f = false ;
 	if (int sl ; (sl = getlenstr(sp,µsl)) > 0) {
@@ -423,7 +408,10 @@ static bool isNotRunning(cchar *sp,int µsl) noex {
 	    } /* end if */
 	} /* end if (getlenstr) */
 	return (rs >= 0) ? f : rs ;
-}
-/* end subroutine (isNotRunning) */
+} /* end subroutine (NotRunning) */
+
+local bool isLocked(int rs) noex {
+	return isOneOf(rslocked,rs) ;
+} /* end subroutine (isLocked) */
 
 
