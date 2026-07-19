dea72d0ea34e37ba80ed8019d1d5bde58b79372f
David A.D. Morano <david.a.morano@gmail.com>
2026-07-19 00:37:41 -0400

    update

--- src/libdam/rijndael.cc
+++ src/libdam/rijndael.cc
@@ -26,23 +26,25 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/types.h>		/* POSIX® */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD |memcpy93c)| */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
-#include	<sys/types.h>
-#include <cstdlib>
-#include <cstring>
-
-#include	<usystem.h>
-
-#include "rijndael.h"
-
+#include	"rijndael.h"
 
 
 /* local defines */
 
 #define FULL_UNROLL	1
-
-
-
 
 /*
 Te0[x] = S [x].[02, 01, 01, 03];
@@ -58,7 +60,7 @@ Td3[x] = Si[x].[09, 0d, 0b, 0e];
 Td4[x] = Si[x].[01, 01, 01, 01];
 */
 
-static const u32 Te0[256] = {
+constexpr u32 Te0[256] = {
     0xc66363a5U, 0xf87c7c84U, 0xee777799U, 0xf67b7b8dU,
     0xfff2f20dU, 0xd66b6bbdU, 0xde6f6fb1U, 0x91c5c554U,
     0x60303050U, 0x02010103U, 0xce6767a9U, 0x562b2b7dU,
@@ -123,8 +125,9 @@ static const u32 Te0[256] = {
     0x65bfbfdaU, 0xd7e6e631U, 0x844242c6U, 0xd06868b8U,
     0x824141c3U, 0x299999b0U, 0x5a2d2d77U, 0x1e0f0f11U,
     0x7bb0b0cbU, 0xa85454fcU, 0x6dbbbbd6U, 0x2c16163aU,
-};
-static const u32 Te1[256] = {
+} ; /* end array */
+
+constexpr const u32 Te1[256] = {
     0xa5c66363U, 0x84f87c7cU, 0x99ee7777U, 0x8df67b7bU,
     0x0dfff2f2U, 0xbdd66b6bU, 0xb1de6f6fU, 0x5491c5c5U,
     0x50603030U, 0x03020101U, 0xa9ce6767U, 0x7d562b2bU,
@@ -189,8 +192,9 @@ static const u32 Te1[256] = {
     0xda65bfbfU, 0x31d7e6e6U, 0xc6844242U, 0xb8d06868U,
     0xc3824141U, 0xb0299999U, 0x775a2d2dU, 0x111e0f0fU,
     0xcb7bb0b0U, 0xfca85454U, 0xd66dbbbbU, 0x3a2c1616U,
-};
-static const u32 Te2[256] = {
+} ; /* end array */
+
+constexpr u32 Te2[256] = {
     0x63a5c663U, 0x7c84f87cU, 0x7799ee77U, 0x7b8df67bU,
     0xf20dfff2U, 0x6bbdd66bU, 0x6fb1de6fU, 0xc55491c5U,
     0x30506030U, 0x01030201U, 0x67a9ce67U, 0x2b7d562bU,
@@ -255,9 +259,9 @@ static const u32 Te2[256] = {
     0xbfda65bfU, 0xe631d7e6U, 0x42c68442U, 0x68b8d068U,
     0x41c38241U, 0x99b02999U, 0x2d775a2dU, 0x0f111e0fU,
     0xb0cb7bb0U, 0x54fca854U, 0xbbd66dbbU, 0x163a2c16U,
-};
-static const u32 Te3[256] = {
+} ; /* end array */
 
+constexpr u32 Te3[256] = {
     0x6363a5c6U, 0x7c7c84f8U, 0x777799eeU, 0x7b7b8df6U,
     0xf2f20dffU, 0x6b6bbdd6U, 0x6f6fb1deU, 0xc5c55491U,
     0x30305060U, 0x01010302U, 0x6767a9ceU, 0x2b2b7d56U,
@@ -322,8 +326,9 @@ static const u32 Te3[256] = {
     0xbfbfda65U, 0xe6e631d7U, 0x4242c684U, 0x6868b8d0U,
     0x4141c382U, 0x9999b029U, 0x2d2d775aU, 0x0f0f111eU,
     0xb0b0cb7bU, 0x5454fca8U, 0xbbbbd66dU, 0x16163a2cU,
-};
-static const u32 Te4[256] = {
+} ; /* end array */
+
+constexpr u32 Te4[256] = {
     0x63636363U, 0x7c7c7c7cU, 0x77777777U, 0x7b7b7b7bU,
     0xf2f2f2f2U, 0x6b6b6b6bU, 0x6f6f6f6fU, 0xc5c5c5c5U,
     0x30303030U, 0x01010101U, 0x67676767U, 0x2b2b2b2bU,
@@ -388,8 +393,9 @@ static const u32 Te4[256] = {
     0xbfbfbfbfU, 0xe6e6e6e6U, 0x42424242U, 0x68686868U,
     0x41414141U, 0x99999999U, 0x2d2d2d2dU, 0x0f0f0f0fU,
     0xb0b0b0b0U, 0x54545454U, 0xbbbbbbbbU, 0x16161616U,
-};
-static const u32 Td0[256] = {
+} ; /* end aray */
+
+constexpr u32 Td0[256] = {
     0x51f4a750U, 0x7e416553U, 0x1a17a4c3U, 0x3a275e96U,
     0x3bab6bcbU, 0x1f9d45f1U, 0xacfa58abU, 0x4be30393U,
     0x2030fa55U, 0xad766df6U, 0x88cc7691U, 0xf5024c25U,
@@ -454,8 +460,9 @@ static const u32 Td0[256] = {
     0x161dc372U, 0xbce2250cU, 0x283c498bU, 0xff0d9541U,
     0x39a80171U, 0x080cb3deU, 0xd8b4e49cU, 0x6456c190U,
     0x7bcb8461U, 0xd532b670U, 0x486c5c74U, 0xd0b85742U,
-};
-static const u32 Td1[256] = {
+} ; /* end array */
+
+constexpr u32 Td1[256] = {
     0x5051f4a7U, 0x537e4165U, 0xc31a17a4U, 0x963a275eU,
     0xcb3bab6bU, 0xf11f9d45U, 0xabacfa58U, 0x934be303U,
     0x552030faU, 0xf6ad766dU, 0x9188cc76U, 0x25f5024cU,
@@ -520,8 +527,9 @@ static const u32 Td1[256] = {
     0x72161dc3U, 0x0cbce225U, 0x8b283c49U, 0x41ff0d95U,
     0x7139a801U, 0xde080cb3U, 0x9cd8b4e4U, 0x906456c1U,
     0x617bcb84U, 0x70d532b6U, 0x74486c5cU, 0x42d0b857U,
-};
-static const u32 Td2[256] = {
+} ; /* end array */
+
+constexpr u32 Td2[256] = {
     0xa75051f4U, 0x65537e41U, 0xa4c31a17U, 0x5e963a27U,
     0x6bcb3babU, 0x45f11f9dU, 0x58abacfaU, 0x03934be3U,
     0xfa552030U, 0x6df6ad76U, 0x769188ccU, 0x4c25f502U,
@@ -587,8 +595,9 @@ static const u32 Td2[256] = {
     0xc372161dU, 0x250cbce2U, 0x498b283cU, 0x9541ff0dU,
     0x017139a8U, 0xb3de080cU, 0xe49cd8b4U, 0xc1906456U,
     0x84617bcbU, 0xb670d532U, 0x5c74486cU, 0x5742d0b8U,
-};
-static const u32 Td3[256] = {
+} ; /* end array */
+
+constexpr u32 Td3[256] = {
     0xf4a75051U, 0x4165537eU, 0x17a4c31aU, 0x275e963aU,
     0xab6bcb3bU, 0x9d45f11fU, 0xfa58abacU, 0xe303934bU,
     0x30fa5520U, 0x766df6adU, 0xcc769188U, 0x024c25f5U,
@@ -653,8 +662,9 @@ static const u32 Td3[256] = {
     0x1dc37216U, 0xe2250cbcU, 0x3c498b28U, 0x0d9541ffU,
     0xa8017139U, 0x0cb3de08U, 0xb4e49cd8U, 0x56c19064U,
     0xcb84617bU, 0x32b670d5U, 0x6c5c7448U, 0xb85742d0U,
-};
-static const u32 Td4[256] = {
+} ; /* end array */
+
+constexpr u32 Td4[256] = {
     0x52525252U, 0x09090909U, 0x6a6a6a6aU, 0xd5d5d5d5U,
     0x30303030U, 0x36363636U, 0xa5a5a5a5U, 0x38383838U,
     0xbfbfbfbfU, 0x40404040U, 0xa3a3a3a3U, 0x9e9e9e9eU,
@@ -719,12 +729,14 @@ static const u32 Td4[256] = {
     0xbabababaU, 0x77777777U, 0xd6d6d6d6U, 0x26262626U,
     0xe1e1e1e1U, 0x69696969U, 0x14141414U, 0x63636363U,
     0x55555555U, 0x21212121U, 0x0c0c0c0cU, 0x7d7d7d7dU,
-};
-static const u32 rcon[] = {
+} ; /* end array */
+
+/* for 128-bit blocks, Rijndael never uses more than 10 rcon values */
+constexpr u32 rcon[] = {
 	0x01000000, 0x02000000, 0x04000000, 0x08000000,
 	0x10000000, 0x20000000, 0x40000000, 0x80000000,
-	0x1B000000, 0x36000000, /* for 128-bit blocks, Rijndael never uses more than 10 rcon values */
-};
+	0x1B000000, 0x36000000, 
+} ; /* end array */
 
 #define GETU32(pt) (((u32)(pt)[0] << 24) ^ ((u32)(pt)[1] << 16) ^ ((u32)(pt)[2] <<  8) ^ ((u32)(pt)[3]))
 #define PUTU32(ct, st) { (ct)[0] = (u8)((st) >> 24); (ct)[1] = (u8)((st) >> 16); (ct)[2] = (u8)((st) >>  8); (ct)[3] = (u8)(st); }
@@ -734,7 +746,9 @@ static const u32 rcon[] = {
  *
  * @return	the number of rounds for the given cipher key size.
  */
-static int rijndaelKeySetupEnc(u32 rk[/*4*(Nr + 1)*/], const u8 cipherKey[], int keyBits) {
+
+local int rijndaelKeySetupEnc(u32 rk[/*4*(Nr + 1)*/], 
+		const u8 *cipherKey, int keyBits) {
    	int i = 0;
 	u32 temp;
 
@@ -812,16 +826,17 @@ static int rijndaelKeySetupEnc(u32 rk[/*4*(Nr + 1)*/], const u8 cipherKey[], int
 		}
 	}
 	return 0;
-}
+} /* end subroutine */
 
 /**
  * Expand the cipher key into the decryption key schedule.
  *
  * @return	the number of rounds for the given cipher key size.
  */
-static int
-rijndaelKeySetupDec(u32 rk[/*4*(Nr + 1)*/], const u8 cipherKey[], int keyBits,
-    int have_encrypt) {
+
+local int
+rijndaelKeySetupDec(u32 rk[/*4*(Nr + 1)*/], const u8 *cipherKey, int keyBits,
+    		int have_encrypt) {
 	int Nr, i, j;
 	u32 temp;
 
@@ -863,9 +878,10 @@ rijndaelKeySetupDec(u32 rk[/*4*(Nr + 1)*/], const u8 cipherKey[], int keyBits,
 			Td3[Te4[(rk[3]      ) & 0xff] & 0xff];
 	}
 	return Nr;
-}
+} /* end subroutine */
 
-static void rijndaelEncrypt(const u32 rk[/*4*(Nr + 1)*/], int Nr, const u8 pt[16], u8 ct[16]) {
+local void rijndaelEncrypt(const u32 rk[/*4*(Nr + 1)*/], int Nr, 
+		const u8 pt[16], u8 ct[16]) {
 	u32 s0, s1, s2, s3, t0, t1, t2, t3;
 #ifndef FULL_UNROLL
     int r;
@@ -1044,9 +1060,10 @@ static void rijndaelEncrypt(const u32 rk[/*4*(Nr + 1)*/], int Nr, const u8 pt[16
 		(Te4[(t2      ) & 0xff] & 0x000000ff) ^
 		rk[3];
 	PUTU32(ct + 12, s3);
-}
+} /* end subroutine */
 
-static void rijndaelDecrypt(const u32 rk[/*4*(Nr + 1)*/], int Nr, const u8 ct[16], u8 pt[16]) {
+local void rijndaelDecrypt(const u32 rk[/*4*(Nr + 1)*/], int Nr, 
+		const u8 ct[16], u8 pt[16]) {
 	u32 s0, s1, s2, s3, t0, t1, t2, t3;
 #ifndef FULL_UNROLL
     int r;
@@ -1225,66 +1242,49 @@ static void rijndaelDecrypt(const u32 rk[/*4*(Nr + 1)*/], int Nr, const u8 ct[16
    		(Td4[(t0      ) & 0xff] & 0x000000ff) ^
    		rk[3];
 	PUTU32(pt + 12, s3);
-}
-
+} /* end subroutine */
 
 
 /* API */
 
+int rijndael_set_key(rijndael_ctx *ctx, u_char *key, 
+		int bits, int encrypt) noex {
+    	int		rs = SR_FAULT ;
+	if (ctx) {
+	    rs = SR_INVALID ;
+	    ctx->Nr = rijndaelKeySetupEnc(ctx->ek, key, bits);
+	    if (ctx->Nr != 0) {
+		rs = SR_OK ;
+	        if (encrypt) {
+		    ctx->decrypt = 0;
+		    memset(ctx->dk, 0, sizeof(ctx->dk));
+	        } else {
+		    ctx->decrypt = 1;
+		    memcpy(ctx->dk, ctx->ek, sizeof(ctx->dk));
+		    rijndaelKeySetupDec(ctx->dk, key, bits, ctx->Nr);
+	        }
+	    } /* end if (non-zero) */
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (rijndael_set_key) */
 
+int rijndael_decrypt(rijndael_ctx *ctx, u_char *src, u_char *dst) noex {
+    	int		rs = SR_FAULT ;
+	if (ctx) {
+	    rs = SR_OK ;
+	    rijndaelDecrypt(ctx->dk, ctx->Nr, src, dst);
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (rijndael_decrypt) */
 
-int rijndael_set_key(rijndael_ctx *ctx, u_char *key, int bits, int encrypt)
-{
-
-
-	if (ctx == NULL)
-		return SR_FAULT ;
-
-	ctx->Nr = rijndaelKeySetupEnc(ctx->ek, key, bits);
-
-	if (ctx->Nr == 0)
-		return SR_INVALID ;
-
-	if (encrypt) {
-		ctx->decrypt = 0;
-		memset(ctx->dk, 0, sizeof(ctx->dk));
-	} else {
-		ctx->decrypt = 1;
-		memcpy(ctx->dk, ctx->ek, sizeof(ctx->dk));
-		rijndaelKeySetupDec(ctx->dk, key, bits, ctx->Nr);
-	}
-
-	return SR_OK ;
-}
-/* end subroutine (rijndael_set_key) */
-
-
-int rijndael_decrypt(rijndael_ctx *ctx, u_char *src, u_char *dst)
-{
-
-
-	if (ctx == NULL)
-		return SR_FAULT ;
-
-	rijndaelDecrypt(ctx->dk, ctx->Nr, src, dst);
-
-	return SR_OK ;
-}
-/* end subroutine (rijndael_decrypt) */
-
-
-int rijndael_encrypt(rijndael_ctx *ctx, u_char *src, u_char *dst)
-{
-
-
-	if (ctx == NULL)
-		return SR_FAULT ;
-
-	rijndaelEncrypt(ctx->ek, ctx->Nr, src, dst);
-
-	return SR_OK ;
-}
-/* end subroutine (rijndael_encrypt) */
+int rijndael_encrypt(rijndael_ctx *ctx, u_char *src, u_char *dst) noex {
+    	int		rs = SR_FAULT ;
+	if (ctx) {
+	    rs = SR_OK ;
+	    rijndaelEncrypt(ctx->ek, ctx->Nr, src, dst);
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (rijndael_encrypt) */
 
 
 
