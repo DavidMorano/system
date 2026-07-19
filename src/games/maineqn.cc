f673963c34b2988c04ee5e104eff046493fad81d
David A.D. Morano <david.a.morano@gmail.com>
2026-07-19 00:20:52 -0400

    update

--- src/games/maineqn.cc
+++ src/games/maineqn.cc
@@ -11,7 +11,7 @@
 
 /* Copyright © 2010 David A­D­ Morano.  All rights reserved. */
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<cstdlib>
 #include	<cinttypes>
