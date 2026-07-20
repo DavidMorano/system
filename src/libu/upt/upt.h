706d21776da3423892912032ed25d31f771bf473
David A.D. Morano <david.a.morano@gmail.com>
2026-07-20 03:22:59 -0400

    restore

---
+++ src/libu/upt/upt.h
@@ -0,0 +1,33 @@
+/* upt HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* UNIX® POSIX Thread manipulation */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-08-20, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UPT_INCLUDE
+#define	UPT_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<pthread.h>		/* POSIX® */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<uptprime.h>
+#include	<uptspawn.h>
+
+
+#endif /* UPT_INCLUDE */
+
+
