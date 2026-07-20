fb5f216c36f54b98bae903140ec334e2603b6d7d
David A.D. Morano <david.a.morano@gmail.com>
2026-07-20 03:20:57 -0400

    restore

---
+++ src/libu/atox.h
@@ -0,0 +1,40 @@
+/* atox HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* convert a decimal c-string to an integer type */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-09-03, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	ATOX_INCLUDE
+#define	ATOX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<stdintx.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern slonglong	atosll(cchar *) noex ;
+extern ulonglong	atoull(cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* ATOX_INCLUDE */
+
+
