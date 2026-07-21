9f6021d32dd445bd14ab1f42095a1e38a7947653
David A.D. Morano <david.a.morano@gmail.com>
2026-07-21 00:23:54 -0400

    cleanup

--- src/libuc/hash/hash_elf.cc
+++ src/libuc/hash/hash_elf.cc
@@ -54,14 +54,13 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<cstdint>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"hash.h"
 
@@ -115,7 +114,6 @@ uint hash_elf(cvoid *vp,int sl) noex {
 	    }
 	} /* end while */
 	return h ;
-}
-/* end subroutine (hash_elf) */
+} /* end subroutine (hash_elf) */
 
 
