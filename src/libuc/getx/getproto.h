b3e4a5a569d0f32c20704e07b380fb0f5226da75
David A.D. Morano <david.a.morano@gmail.com>
2026-07-21 00:20:55 -0400

    update

--- src/libuc/getx/getproto.h
+++ src/libuc/getx/getproto.h
@@ -30,8 +30,8 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 EXTERNC_begin
