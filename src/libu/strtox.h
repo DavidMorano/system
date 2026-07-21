b2a010c25c4ae4cbde7cca59cfb28fd3a300314f
David A.D. Morano <david.a.morano@gmail.com>
2026-07-21 00:16:36 -0400

    update

--- src/libu/strtox.h
+++ src/libu/strtox.h
@@ -29,27 +29,37 @@
 
 EXTERNC_begin
 
-extern int		strtoxi(cchar *,char **,int) noex ;	/* 32-bits */
+extern sint		strtoxsi(cchar *,char **,int) noex ;	/* 32-bits */
 extern uint		strtoxui(cchar *,char **,int) noex ;	/* 32-bits */
 
-extern long		strtoxl(cchar *,char **,int) noex ;	/* 64-bits */
+extern slong		strtoxsl(cchar *,char **,int) noex ;	/* 64-bits */
 extern ulong		strtoxul(cchar *,char **,int) noex ;	/* 64-bits */
 
-extern longlong		strtoxll(cchar *,char **,int) noex ;	/* 128-bits */
+extern slonglong	strtoxsll(cchar *,char **,int) noex ;	/* 128-bits */
 extern ulonglong	strtoxull(cchar *,char **,int) noex ;	/* 128-bits */
+
+local inline int	strtoxi	(cchar *sp,char **endpp,int b) noex {
+    	return strtoxsi(sp,endpp,b) ;
+}
+local inline long	strtoxl	(cchar *sp,char **endpp,int b) noex {
+    	return strtoxsl(sp,endpp,b) ;
+}
+local inline longlong	strtoxll	(cchar *sp,char **endpp,int b) noex {
+    	return strtoxsll(sp,endpp,b) ;
+}
 
 EXTERNC_end
 
 #ifdef	COMMENT /* this is a 256-bit integer; but it does not yet exist */
 #ifdef	__cplusplus
-extern intmax		strtoxim(cchar *,char **,int) noex ;	/* 256-bits */
+extern sintmax		strtoxsim(cchar *,char **,int) noex ;	/* 256-bits */
 extern uintmax		strtoxuim(cchar *,char **,int) noex ;	/* 256-bits */
 #endif /* __cplusplus */
 #endif /* COMMENT */
 
 #ifdef	COMMENT /* this is a 512-bit integer; but it does not yet exist */
 #ifdef	__cplusplus
-extern int512_t		strtos256(cchar *,char **,int) noex ;	/* 512-bits */
+extern sint512_t	strtos256(cchar *,char **,int) noex ;	/* 512-bits */
 extern uint512_t	strtou256(cchar *,char **,int) noex ;	/* 512-bits */
 #endif /* __cplusplus */
 #endif /* COMMENT */
