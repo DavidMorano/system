21aa3de6e31c09714876807d40920ea7b8f2b495
David A.D. Morano <david.a.morano@gmail.com>
2026-07-22 00:30:23 -0400

    restore

---
+++ src/libdam/pingstatx/pingstatx.h
@@ -0,0 +1,31 @@
+/* pingstatx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* string-index operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	PINGSTATX_INCLUDE
+#define	PINGSTATX_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<pingstatdb.h>
+
+
+#endif /* PINGSTATX_INCLUDE */
+
+
