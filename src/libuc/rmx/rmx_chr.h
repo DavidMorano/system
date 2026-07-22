6a8a8deb9d2e0b64c26f599a3c5f665cce058985
David A.D. Morano <david.a.morano@gmail.com>
2026-07-22 00:40:52 -0400

    update

--- src/libuc/rmx/rmx_chr.h
+++ src/libuc/rmx/rmx_chr.h
@@ -34,19 +34,8 @@ extern int	rmrchr(cchar *,int,int) noex ;
 local inline int rmchr(cchar *sp,int sl,int sch) noex {
 	return rmochr(sp,sl,sch) ;
 }
-local inline int rmdot(cchar *sp,int sl) noex {
-	return rmrchr(sp,sl,'.') ;
-}
 
 EXTERNC_end
-
-#ifdef	__cplusplus
-
-local inline int rmdot(cchar *sp) noex {
-	return rmrchr(sp,-1,'.') ;
-}
-
-#endif /* __cplusplus */
 
 
 #endif /* RMXCHR_INCLUDE */
