4917216c8a21e0e3ed85af30c8b4eb79bd073853
David A.D. Morano <david.a.morano@gmail.com>
2026-07-22 00:30:11 -0400

    restore

---
+++ src/libdam/pingstatx/pingstatdb_rec.hh
@@ -0,0 +1,31 @@
+/* pingstatdb_rec HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* command utilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-06-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	PINGSTATDBREC_INCLUDE
+#define	PINGSTATDBREC_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* PINGSTATDBREC_INCLUDE */
+
+
