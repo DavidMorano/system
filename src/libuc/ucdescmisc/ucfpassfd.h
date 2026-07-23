1c41ee2391af3c1a4066b0ef13ab615710537461
David A.D. Morano <david.a.morano@gmail.com>
2026-07-23 00:25:20 -0400

    restore

---
+++ src/libuc/ucdescmisc/ucfpassfd.h
@@ -0,0 +1,36 @@
+/* ucfpassfd HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get random data from the system */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCFPASSFD_INCLUDE
+#define	UCFPASSFD_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int	uc_fpassfd(int,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCFPASSFD_INCLUDE */
+
+
