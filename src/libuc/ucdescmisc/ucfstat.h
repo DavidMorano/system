fd6a3d47e8918bf6805d795a99ab5760ecdfa8b0
David A.D. Morano <david.a.morano@gmail.com>
2026-07-23 00:24:31 -0400

    update

--- src/libuc/ucdescmisc/ucfstat.h
+++ src/libuc/ucdescmisc/ucfstat.h
@@ -33,10 +33,9 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<stddef.h>		/* |nullptr_t| */
-#include	<stdlib.h>
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<sys/stat.h>		/* POSIX® */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 EXTERNC_begin
