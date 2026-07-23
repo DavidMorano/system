c151ad89b861f9e06f644f8f985323758427bfa6
David A.D. Morano <david.a.morano@gmail.com>
2026-07-23 00:13:09 -0400

    update

--- src/libuc/snx/snabbr.h
+++ src/libuc/snx/snabbr.h
@@ -20,15 +20,15 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 EXTERNC_begin
 
-extern int snabbrerr(char *,int,int) noex ;
-extern int snabbrsig(char *,int,int) noex ;
-extern int snabbrname(char *,int,cchar *,int) noex ;
+extern int snabbrerr	(char *,int,int) noex ;
+extern int snabbrsig	(char *,int,int) noex ;
+extern int snabbrname	(char *,int,cchar *,int) noex ;
 
 EXTERNC_end
 
