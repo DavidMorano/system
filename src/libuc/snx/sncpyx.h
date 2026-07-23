4fc9432fa9e164de1eb73d2b593fba012f040211
David A.D. Morano <david.a.morano@gmail.com>
2026-07-23 00:14:27 -0400

    update

--- src/libuc/snx/sncpyx.h
+++ src/libuc/snx/sncpyx.h
@@ -20,9 +20,9 @@
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<uconcepts.hh>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<uconcepts.hh>		/* LIBU */
 
 #include	<sncpyxc.h>
 #include	<sncpyxw.h>
