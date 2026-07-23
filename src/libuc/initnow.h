3a667032c8548dc41ac9afe8173595937ad98800
David A.D. Morano <david.a.morano@gmail.com>
2026-07-23 00:11:53 -0400

    update

--- src/libuc/initnow.h
+++ src/libuc/initnow.h
@@ -21,8 +21,8 @@
 
 #include	<envstandards.h>	/* MUST be first to configure */
 #include	<sys/timeb.h>		/* |TIMEB| */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 EXTERNC_begin
