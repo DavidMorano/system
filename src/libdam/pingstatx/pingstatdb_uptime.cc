a7abd876f69bef40f335e5af34e7aa78b264e32c
David A.D. Morano <david.a.morano@gmail.com>
2026-07-24 00:19:37 -0400

    update

--- src/libdam/pingstatx/pingstatdb_uptime.cc
+++ src/libdam/pingstatx/pingstatdb_uptime.cc
@@ -5,7 +5,7 @@
 /* object to manipulate a PINGSTATDB file */
 /* version %I% last-modified %G% */
 
-#define	CF_DEBUG	0		/* non-switchable debug print-outs */
+#define	CF_DEBUG	1		/* non-switchable debug print-outs */
 
 /* revision history:
 
@@ -22,7 +22,7 @@
 	pingstatdb
 
   	Description:
-	This subroutine maintains a PINGSTATDB file. These files
+	This subroutine maintains a PINGSTATDB file.  These files
 	are used to maintain the names and status of a PING event.
 
 	Synopsis:
@@ -72,12 +72,15 @@
 #include	<cstddef>		/* CSTD */
 #include	<cstdlib>		/* CSTD */
 #include	<cstring>		/* CSTD */
+#include	<new>			/* C++TD */
+#include	<functional>		/* C++TD */
 #include	<clanguage.h>		/* LIBU */
 #include	<usysbase.h>		/* LIBU */
 #include	<ugetx.h>		/* LIBU |getustime(3u)| */
 #include	<ucmem.h>		/* LIBUC */
 #include	<vechand.h>		/* LIBUC */
 #include	<dater.h>		/* LIBUC */
+#include	<timestr.h>		/* LIBUC */
 #include	<ismisc.h>		/* LIBUC */
 #include	<localmisc.h>		/* LIBU |TIMEBUFLEN| */
 #include	<bfile.h>		/* LIBB */
@@ -119,9 +122,15 @@ import pingstatdb_rec ;
 #define	CF_DEBUG	0		/* non-switchable debug print-outs */
 #endif
 
+#define	DEBUGPRINTT(str,tval)		\
+    	if_constexpr (f_debug) {	\
+	    debugprintt(str,tval) ;	\
+	}
+    
 
 /* imported namespaces */
 
+using std::bind_front ;			/* subroutine */
 using libuc::mem ;			/* variable */
 
 
@@ -139,14 +148,12 @@ using libuc::mem ;			/* variable */
 
 /* forward references */
 
+local int debugprintt(cchar *,time_t) noex ;
+
 
 /* local variables */
 
 cbool		f_debug		= CF_DEBUG ;
-
-#if	CF_DEBUG
-static cchar	hostname[] = "EMPTY" ;
-#endif
 
 
 /* exported variables */
@@ -154,56 +161,83 @@ static cchar	hostname[] = "EMPTY" ;
 
 /* exported subroutines */
 
-
-
-#ifdef	COMMENT
-
 namespace {
     struct uptimer {
 	PSD		*op ;	/* caller supplied */
 	PSD_UP		*up ;	/* caller supplied */
 	cchar		*hn ;	/* caller supplied */
 	bfile		*pfp ;
+	dater		*cdp ;
+	dater		*udp ;
+	time_t		dt ;
+	uint		ts ;	/* time-stamp */
+	bool		f_up ;
 	uptimer(PSD *o,cchar *h,PSD_UP *u) noex : op(o), up(u), hn(h) { 
 	    pfp = op->pfp ;
+	    cdp = nullptr ;
+	    udp = nullptr ;
+	    dt = getustime ;
+	    ts = 0 ;
+	    f_up = false ;
 	} ; /* end ctor */
 	operator int () noex ;
 	int readoff	() noex ;
 	int lockon	() noex ;
+	int lockoff	() noex ;
+	int daterbeg	() noex ;
+	int daterend	() noex ;
+	int sync	() noex ;
+	int timestamp	() noex ;
+	int decide	() noex ;
+	int upold	(PSD_REC *) noex ;
+	int upnew	() noex ;
+	int enter	(uint) noex ;
+	int uplast	() noex ;
+	int done	() noex ;
+	int seekend	(off_t *) noex ;
     } ; /* end struct (uptimer) */
 } /* end namespace */
 
 int pingstatdb_uptime(PSD *op,cchar *hn,PSD_UP *up) noex {
     	int		rs ;
-	int		fchanged = false ;
+	int		fchanged = false ; /* return-value */
 	DEBUGPRINTF("ent hn=%s\n",hn) ;
-	if ((rs = pingstatdb_magic(op,hn,up)) >= 0) {
+	if ((rs = pingstatdb_magic(op,hn,up)) >= 0) ylikely {
 	    rs = SR_INVALID ;
-	    if (hn[0]) {
+	    if (hn[0]) ylikely {
 	        rs = SR_BADF ;	/* not open for writing */
-		if (op->fl.writable) {
+		if (op->fl.writable) ylikely {
 		    uptimer uo(op,hn,up) ;
 	            rs = uo ;
 		    fchanged = int(rs > 0) ;
 		} /* end if (writable) */
 	    } /* end if (valid) */
-	} /* end if (non-null) */
-	DEBUGPRINTF("ret rs=%d fchanged=%u\n", rs,f_changed) ;
+	} /* end if (pingstatdb_magic) */
+	DEBUGPRINTF("ret rs=%d fchanged=%u\n", rs,fchanged) ;
 	return (rs >= 0) ? fchanged : rs ;
 } /* end subroutine (pingstatdb_uptime) */
 
 uptimer::operator int () noex {
     	int		rs ;
-	if ((rs = readoff()) >= 0) {
-	    if ((rs = lockon()) >= 0) {
-		if ((rs = pingstatdb_checkcache(op)) >= 0) {
-		    rs = SR_OK ;
-
-
+	int		rs1 ;
+	int		fchanged = false ; /* return-value */
+	if ((rs = readoff()) >= 0) ylikely {
+	    if ((rs = lockon()) >= 0) ylikely {
+		if ((rs = pingstatdb_checkcache(op)) >= 0) ylikely {
+		    if ((rs = daterbeg()) >= 0) {
+			if ((rs = decide()) >= 0) {
+			    fchanged = int(rs > 0) ;
+			    rs = done() ;
+			} /* end if (decide) */
+			rs1 = daterend() ;
+			if (rs >= 0) rs = rs1 ;
+		    } /* end if */
 		} /* end if (pingstatdb_checkcache) */
+		rs1 = lockoff() ;
+		if (rs >= 0) rs = rs1 ;
 	    } /* end if (lockon) */
 	} /* end if (readoff) */
-	return rs ;
+	return (rs >= 0) ? fchanged : rs ;
 } /* end method (uptimer::operator) */
 
 int uptimer::readoff() noex {
@@ -226,223 +260,244 @@ int uptimer::lockon() noex {
 	return rs ;
 } /* end method (uptimer::lockon) */
 
-
-#endif /* COMMENT */
-
-#ifdef	COMMENT
-int pingstatdb_uptime(PSD *op,cchar *hostname,PSD_UP *up) noex {
-	PSD_REC		e, *rp ;
-	dater		cd, ud, *cdp ;
-	off_t		boff ;
-	custime		dt = getustime ;
-	time_t		ptime = 0 ;
-	uint		timestamp ;
-	uint		ctimeange ;
-	uint		roff ;
-	int		rs = SR_OK ;
-	int		sz ;
-	int		f_up = true ;
-	int		f_changed = false ;
-
-	if (op == nullptr) return SR_FAULT ;
-	if (hostname == nullptr) return SR_FAULT ;
-	if (up == nullptr) return SR_FAULT ;
-
-	if (op->magval != PSD_MAGIC) return SR_NOTOPEN ;
-
-	if (hostname[0] == '\0') return SR_INVALID ;
-
-	if (! op->fl.writable) {
-	    rs = SR_BADF ;	/* not open for writing */
-	    goto ret0 ;
-	}
-
-	DEBUGPRINTF("lock stuff\n") ;
-
-	if (op->fl.readlocked) {
-	    op->fl.readlocked = false ;
+int uptimer::lockoff() noex {
+    	int		rs = SR_OK ;
+	if (op->fl.writelocked) {
 	    op->fl.writelocked = false ;
-	    bcontrol(op->pfp,BC_UNLOCK,0) ;
+	    rs = bcontrol(op->pfp,BC_UNLOCK,0) ;
 	}
+	return rs ;
+} /* end method (uptimer::lockoff) */
 
-	if (! op->fl.writelocked) {
-	    cint bcmd = BC_LOCKWRITE ;
-	    rs = bcontrol(op->pfp,bcmd,TO_LOCK) ;
-	    op->fl.writelocked = (rs >= 0) ;
-	} /* end if (we did not already have a lock on the file) */
-
-	if (rs < 0)
-	    goto ret0 ;
-
-	rs = pingstatdb_checkcache(op) ;
-	if (rs < 0)
-	    goto ret1 ;
-
-	op->now.time = dt ;
-	rs = dater_start(&ud,op->nowp,op->zname,-1) ;
-	if (rs < 0)
-	    goto ret1 ;
-
-	rs = dater_start(&cd,op->nowp,op->zname,-1) ;
-	if (rs < 0)
-	    goto ret2 ;
-
-	timestamp = up->timestamp ;
-	if ((up->timestamp == 0) || (up->timestamp > dt)) {
-	    timestamp = dt ;
-	}
-
-	if_constexpr (f_debug) {
-	    char	timebuf[TIMEBUFLEN+1] ;
-	    DEBUGPRINTF("now=%s\n",
-	        timestr_logz(op->now.time,timebuf)) ;
-	    DEBUGPRINTF("dater_start() rs=%d cur_date=%s\n",
-	        rs,timestr_logz(ud.b.time,timebuf)) ;
-	}
-
-	if_constexpr (f_debug) {
-	    DEBUGPRINTF("about to match\n") ;
-	    rs = vechand_count(op->rlp) ;
-	    DEBUGPRINTF("entries in cache %d\n",rs) ;
-	}
-
-	if ((rs = pingstatdb_recget(op,hostname,&rp)) >= 0) {
-	    int	f_greater = (! LEQUIV(f_up,rp->f_up)) ;
-
-	    DEBUGPRINTF("found match rs=%d\n",rs) ;
-
-	    rs = dater_gettime(&rp->pdate,&ptime) ;
-	    f_greater = ((rs >= 0) && (timestamp > ptime)) ;
-	    /* the update (ping) time */
-	    if (ptime > timestamp) {
-	        timestamp = ptime ;
-	    }
-
-	    dater_settimezn(&ud,timestamp,op->zname,-1) ;
-
-/* the change time */
-
-	    if (f_changed || (up->ctimeange != 0)) {
-
-	        ctimeange = up->ctimeange ;
-	        if (up->ctimeange == 0) {
-	            ctimeange = dt ;
-		}
-
-	        cdp = &cd ;
-	        dater_settimezn(&cd,(time_t) ctimeange,op->zname,-1) ;
-
-	    } else {
-	        cdp = &rp->cdate ;
-	    }
-
-/* force a change */
-
-	    DEBUGPRINTF("count=%d\n",
-	        up->count) ;
-
-	    if ((! f_changed) && (up->count != rp->count))
-	        f_changed = true ;
-
-	    {
-	        char	timebuf[TIMEBUFLEN + 1] ;
-	        DEBUGPRINTF("dater_gettime() ptime=%s\n",
-	            timestr_logz(ptime,timebuf)) ;
-	    }
-
-	    if ((rs < 0) || f_changed ||
-	        (((dt - ptime) > TO_MINUPDATE) && f_greater)) {
-
-	        boff = rp->roff ;
-	        bseek(op->pfp,boff,SEEK_SET) ;
-
-	        rs = record_write(rp,op->pfp,cdp,&ud,up->count,f_up) ;
-
-	        DEBUGPRINTF("record_write() rs=%d\n",rs) ;
-
-	    } /* end if (did the update) */
-
-	} else {
-	    TIMEB	*nowp = op->nowp ;
-	    bool	f_rec = false ;
-	    cchar	*zn = op->zname ;
-	    cchar	*hn = hostname ;
-
-	    DEBUGPRINTF("no match found rs=%d\n",rs) ;
-
-	    dater_settimezn(&ud,timestamp,op->zname,-1) ;
-
-	    if ((ctimeange = up->ctimeange) == 0) {
-	        ctimeange = dt ;
-	    }
-	    dater_settimezn(&cd,(time_t) ctimeange,op->zname,-1) ;
-
-	    f_changed = true ;
-	    bseek(op->pfp,0L,SEEK_END) ;
-
-	    btell(op->pfp,&boff) ;
-	    roff = boff ;
-
-	    if ((rs = record_start(&e,nowp,zn,roff,hn,&ud)) >= 0) {
-	        f_rec = true ;
-	    }
-
-	    DEBUGPRINTF("record_start() rs=%d\n",rs) ;
-	    DEBUGPRINTF("hostname=%s\n", hostname) ;
-
-	    if (rs >= 0) {
-	        rs = record_write(&e,op->pfp,&ud,&cd,up->count,f_up) ;
-	    }
-
-	    DEBUGPRINTF("record_write() rs=%d\n", rs) ;
-
-	    if (rs >= 0) {
-	        sz = szof(PSD_REC) ;
-	        rs = vechand_add(op->rlp,&e,sz) ;
-	    }
-
-	    if ((rs < 0) && f_rec) {
-	        record_finish(&e) ;
+int uptimer::daterbeg() noex {
+    	cnothrow	nt{} ;
+	timeb		*nowp = op->nowp ;
+    	int		rs = SR_NOMEM ;
+	cchar		*zn = op->znbuf ;
+	if (cdp = new(nt) dater ; cdp) {
+	    if (cdp = new(nt) dater ; cdp) {
+		if ((rs = cdp->start(nowp,zn)) >= 0) {
+		    if ((rs = udp->start(nowp,zn)) >= 0) {
+			ts = conv<uint>(cdp->cb.time) ;
+		    } /* end if (dater_start) */
+		    if (rs < 0) {
+			cdp->finish() ;
+		    } /* end if (error) */
+		} /* end if (dater_start) */
+	        if (rs < 0) {
+		    delete udp ;
+		    udp = nullptr ;
+	        } /* end if (error) */
+	    } /* end if (new-dater) */
+	    if (rs < 0) {
+		delete cdp ;
+		cdp = nullptr ;
 	    } /* end if (error) */
-	} /* end if (target entry) */
+	} /* end if (new-dater) */
+	return rs ;
+} /* end method (uptimer::daterbeg) */
 
-/* update the LASTUPDATE entry */
+int uptimer::daterend() noex {
+    	int		rs = SR_OK ;
+	int		rs1 ;
+	if (udp) {
+	    {
+	        rs1 = udp->finish ;
+	        if (rs >= 0) rs = rs1 ;
+	    }
+	    {
+		delete udp ;
+		udp = nullptr ;
+	    }
+	} /* end if */
+	if (cdp) {
+	    {
+	        rs1 = cdp->finish ;
+	        if (rs >= 0) rs = rs1 ;
+	    }
+	    {
+		delete cdp ;
+		cdp = nullptr ;
+	    }
+	} /* end if */
+	return rs ;
+} /* end method (uptimer::daterend) */
 
-	if (rs >= 0) {
-	    rs = pingstatdb_recupd(op,dt,&cd,LASTUPDATE,f_up,timestamp) ;
-	}
-
-/* udpate our last modification time to keep our cache current */
-
-	op->mtime = dt ;
-	bcontrol(op->pfp,BC_SYNC,0) ;
-
-	dater_finish(&cd) ;
-
-ret2:
-	dater_finish(&ud) ;
-
-/* unlock it */
-ret1:
-	op->fl.writelocked = false ;
-	bcontrol(op->pfp,BC_UNLOCK,0) ;
-
-ret0:
-
-	DEBUGPRINTF("ret rs=%d f_changed=%u\n", rs,f_changed) ;
-	return (rs >= 0) ? f_changed : rs ;
-} /* end subroutine (pingstatdb_uptime) */
-#else /* COMMENT */
-int pingstatdb_uptime(PSD *op,cchar *hn,PSD_UP *up) noex {
-    	int		rs = SR_FAULT ;
-	if (op && hn && up) {
-	    rs = SR_OK ;
+int uptimer::sync() noex {
+    	int		rs = SR_BUGCHECK ;
+	if (pfp) {
+	    op->mtime = dt ;
+	    rs = pfp->control(BC_SYNC,0) ;
 	} /* end if (non-null) */
 	return rs ;
-} /* end subroutine (pingstatdb_uptime) */
-#endif /* COMMENT */
+} /* end method (uptimer::sync) */
 
+int uptimer::timestamp() noex {
+    	int		rs = SR_BUGCHECK ;
+	if ((up->timestamp == 0) || (up->timestamp > dt)) {
+	    ts = conv<uint>(dt) ;
+	} else {
+	    ts = conv<uint>(up->timestamp) ;
+	}
+	return rs ;
+} /* end method (uptimer::timestamp) */
+
+int uptimer::decide() noex {
+    	int		rsn = SR_NOTFOUND ;
+    	int		rs = SR_BUGCHECK ;
+	int		fchanged = false ;
+	if ((rs = timestamp()) >= 0) {
+	    if (PSD_REC *rep ; (rs = pingstatdb_recget(op,hn,&rep)) >= 0) {
+		rs = upold(rep) ;
+		fchanged = int(rs > 0) ;
+	    } else if (rs == rsn) {
+		rs = upnew() ;
+		fchanged = int(rs > 0) ;
+	    } /* end if (pingstatdb_recget) */
+	} /* end if (timestamp) */
+	return (rs >= 0) ? fchanged : rs ;
+} /* end method (uptimer::decide) */
+
+int uptimer::upold(PSD_REC *rep) noex {
+    	dater		*pdp = rep->pdp ;
+    	int		rs ;
+	int		fchanged = false ; /* return-value */
+	DEBUGPRINTF("ent\n") ;
+	f_up = !!rep->f_up ;
+	if (time_t ptime{} ; (rs = pdp->gettime(&ptime)) >= 0) {
+	    uint pts = conv<uint>(ptime) ;
+	    cchar *zn = op->znbuf ;
+	    bool f_greater = (ts > pts) ;
+	    DEBUGPRINTT("dater_gettime() ptime=",ptime) ;
+	    /* the update (ping) time */
+	    if (pts > ts) {
+	        ts = pts ;
+	    }
+	    if (time_t t = time_t(ts) ; (rs = udp->settimezn(t,zn,-1)) >= 0) {
+		dater *tdp = nullptr ;
+		uint ts_chg = 0 ;
+		int to_upd = TO_MINUPDATE ;
+	        /* the change time */
+	        if (fchanged || (up->timechange != 0)) {
+	            ts_chg = up->timechange ;
+	            if (up->timechange == 0) {
+	                ts_chg = conv<uint>(dt) ;
+		    }
+	            tdp = cdp ;
+		    custime time_chg = time_t(ts_chg) ;
+	            rs = cdp->settimezn(time_chg,op->znbuf,-1) ;
+	        } else {
+	            tdp = rep->cdp ;
+	        }
+	        /* force a change */
+	        DEBUGPRINTF("count=%d\n", up->count) ;
+	        if ((! fchanged) && (up->count != rep->cnt)) {
+	            fchanged = true ;
+	        }
+	        DEBUGPRINTT("dater_gettime() ptime=",ptime) ;
+		bool f = fchanged ;
+	        f = f || ((intconv(dt - ptime) > to_upd) && f_greater) ;
+		if ((rs >= 0) && f) {
+	            coff boff = rep->roff ;
+	            if ((rs = bseek(op->pfp,boff,SEEK_SET)) >= 0) {
+			cauto rec_wr = bind_front(record_write,rep) ;
+	                rs = rec_wr(op->pfp,tdp,udp,up->count,f_up) ;
+	                DEBUGPRINTF("record_write() rs=%d\n",rs) ;
+		    }
+	        } /* end if (did the update) */
+	    } /* end if (dater_settimexan) */
+	} /* end if (dater_gettime) */
+	DEBUGPRINTF("ret rs=%d fchanged=%u\n",rs,fchanged) ;
+    	return (rs >= 0) ? fchanged : rs ;
+} /* end method (uptimer::upold) */
+
+int uptimer::upnew() noex {
+    	int		rs ;
+	int		fchanged = false ; /* return-value */
+	bool		f_rec = false ;
+	cchar		*zn = op->znbuf ;
+	DEBUGPRINTF("ent\n") ;
+	(void) f_rec ;
+	if (time_t t = time_t(ts) ; (rs = udp->settimezn(t,op->znbuf)) >= 0) {
+	    uint ts_chg = 0 ;
+	    if ((ts_chg = up->timechange) == 0) {
+	        ts = conv<uint>(dt) ;
+	    }
+	    t = time_t(ts_chg) ;
+	    if ((rs = cdp->settimezn(t,zn)) >= 0) {
+	        fchanged = true ;
+		if (off_t off ; (rs = seekend(&off)) >= 0) {
+		    con uint roff = conv<uint>(off) ;
+		    rs = enter(roff) ;
+		} /* end if (seekend) */
+	    } /* end if (dater_settimezn) */
+	} /* end if (dater_gettime) */
+	DEBUGPRINTF("ret rs=%d fchanged=%u\n",rs,fchanged) ;
+    	return (rs >= 0) ? fchanged : rs ;
+} /* end method (uptimer::upnew) */
+
+int uptimer::enter(uint roff) noex {
+	timeb		*nowp = op->nowp ;
+	cnothrow	nt{} ;
+    	int		rs = SR_NOMEM ;
+	cchar		*zn = op->znbuf ;
+	DEBUGPRINTF("ent\n") ;
+	if (PSD_REC *rep = new(nt) PSD_REC ; rep) {
+	    cauto 	rec_st = bind_front(record_start,rep) ;
+	    cauto 	rec_wr = bind_front(record_write,rep) ;
+	    cauto 	rec_fi = bind_front(record_finish,rep) ;
+	    if ((rs = rec_st(nowp,zn,roff,hn)) >= 0) {
+		cint cnt = up->count ;
+	        bool f_rec = true ;
+	        if ((rs = rec_wr(op->pfp,udp,cdp,cnt,f_up)) >= 0) {
+		    vechand *rlp = op->rlp ;
+	            DEBUGPRINTF("record_write() rs=%d\n", rs) ;
+	            rs = rlp->add(rep) ;
+		} /* end if (record_write) */
+	        if ((rs < 0) && f_rec) {
+	            rec_fi() ;
+	        } /* end if (error) */
+	    } /* end if (record_start) */
+	    if (rs < 0) {
+		delete rep ;
+	    } /* end if (error) */
+	} /* end if (new-record) */
+	DEBUGPRINTF("ret rs=%d\n",rs) ;
+	return rs ;
+} /* end method (uptimer::enter) */
+
+int uptimer::uplast() noex {
+	return pingstatdb_recupd(op,dt,cdp,LASTUPDATE,f_up,ts) ;
+} /* end method (uptimer::uplast) */
+
+int uptimer::done() noex {
+    	int		rs ;
+	if ((rs = uplast()) >= 0) {
+	    rs = sync() ;
+	}
+	return rs ;
+} /* end method (uptimer::done) */
+
+int uptimer::seekend(off_t *offp) noex {
+    	int		rs ;
+	if ((rs = pfp->seek(0z,SEEK_END)) >= 0) {
+	    rs = pfp->tell(offp) ;
+	}
+	return rs ;
+} /* end method (uptimer::seekend) */
+			
 
 /* local subroutines */
+
+local int debugprintt(cchar *str,time_t tval) noex {
+    	int		rs = SR_OK ;
+	if_constexpr (f_debug) {
+    	    cchar	*fmt = "%s %s\n" ;
+	    char	tbuf[TIMEBUFLEN + 1] ;
+	    timestr_logz(tval,tbuf) ;
+	    rs = DEBUGPRINTF(fmt,str,tbuf) ;
+	} /* end if_constexpr (f_debug) */
+    	return rs ; 
+} /* end subroutine (debugprintt) */
 
 
