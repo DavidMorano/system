54d40692a219ae4b508f77b7c11b8f462df0ddf8
David A.D. Morano <david.a.morano@gmail.com>
2026-07-25 00:39:11 -0400

    restore

---
+++ src/libuc/ucproc/ucnprocs.h
@@ -0,0 +1,36 @@
+/* ucnprocs HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* manipulate real names */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1997-11-18, David A-D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1997 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCNPROCS_INCLUDE
+#define	UCNPROCS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int	uc_nprocs(int) noex ;
+
+EXTERNC_end
+
+
+#endif /* UCNPROCS_INCLUDE */
+
+
