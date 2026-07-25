0efa04679e9f1ba3effe2903c858ea82bd167a29
David A.D. Morano <david.a.morano@gmail.com>
2026-07-25 00:48:28 -0400

    update

--- src/libuc/vec/vecstrx_addpath.cc
+++ src/libuc/vec/vecstrx_addpath.cc
@@ -63,18 +63,18 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<climits>		/* |INT_MAX| */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
-#include	<usupport.h>
-#include	<usysconf.h>
-#include	<uclibmem.h>
-#include	<strn.h>		/* |strnbrk(3uc)| */
-#include	<pathclean.h>
-#include	<localmisc.h>
+#include	<climits>		/* CSTD |INT_MAX| */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<usupport.h>		/* LIBU */
+#include	<usysconf.h>		/* LIBU */
+#include	<uclibmem.h>		/* LIBUC */
+#include	<strn.h>		/* LIBUC |strnbrk(3uc)| */
+#include	<pathclean.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"vecstrx.hh"
 
