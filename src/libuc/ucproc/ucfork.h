2fabb4b421dc61b033e64f92e6b9d75a35b8eb58
David A.D. Morano <david.a.morano@gmail.com>
2026-07-25 00:38:33 -0400

    update

--- src/libuc/ucproc/ucfork.h
+++ src/libuc/ucproc/ucfork.h
@@ -53,8 +53,8 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 EXTERNC_begin
