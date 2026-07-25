ead312f77659f9bba3f8e3e587beef7ed026f593
David A.D. Morano <david.a.morano@gmail.com>
2026-07-25 00:43:17 -0400

    restore

---
+++ src/libuc/uctime/uctime.h
@@ -0,0 +1,34 @@
+/* uctime HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* time related facilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-05, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCMEM_INCLUDE
+#define	UCMEM_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<uctim.h>
+#include	<uctimeconv.h>
+#include	<uctimeout.h>
+#include	<uctimer.h>
+
+
+#endif /* UCMEM_INCLUDE */
+
+
