d9397ffe31b57f6aaaa96fed2542c970cffd0efa
David A.D. Morano <david.a.morano@gmail.com>
2026-07-25 00:47:27 -0400

    update

--- src/libuc/vec/vecitem.cc
+++ src/libuc/vec/vecitem.cc
@@ -32,13 +32,13 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
-#include	<uclibmem.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<uclibmem.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"vecitem.h"
 
