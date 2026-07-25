f27ed33714531a4a98f30c2ecc2151f10a35ab1c
David A.D. Morano <david.a.morano@gmail.com>
2026-07-25 00:35:49 -0400

    update

--- src/libuc/ucmemla.h
+++ src/libuc/ucmemla.h
@@ -33,8 +33,8 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 #define	UCMEMLA		struct ucmemla_head
