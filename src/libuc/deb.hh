df6f3cf8ae16bcce05f2ec160832320d23d69640
David A.D. Morano <david.a.morano@gmail.com>
2026-07-26 00:36:00 -0400

    update

--- src/libuc/deb.hh
+++ src/libuc/deb.hh
@@ -37,10 +37,10 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 #define DEBPRINTF(FMT, ...) \
