0293f9473f8a2f80b7e0fb6b6814df563d39380f
David A.D. Morano <david.a.morano@gmail.com>
2026-07-27 01:19:43 -0400

    restore

---
+++ src/libuc/calendar/calstrs.h
@@ -0,0 +1,36 @@
+/* calstrs HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* calendar strings */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CALSTRS_INCLUDE
+#define	CALSTRS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+extern cpcchar	calstrs_months[] ;
+extern cpcchar	calstrs_days[] ;
+extern cpcchar	calstrs_daytypes[] ;
+
+
+#endif /* CALSTRS_INCLUDE */
+
+
