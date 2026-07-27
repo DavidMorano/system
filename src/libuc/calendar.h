c903868e9cef015d19c7d21064bb9b1c5e2f353e
David A.D. Morano <david.a.morano@gmail.com>
2026-07-27 01:19:29 -0400

    restore

---
+++ src/libuc/calendar.h
@@ -0,0 +1,31 @@
+/* calendar HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* time-management facilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CALSTRS_INCLUDE
+#define	CALSTRS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<calstrs.h>
+
+
+#endif /* CALSTRS_INCLUDE */
+
+
