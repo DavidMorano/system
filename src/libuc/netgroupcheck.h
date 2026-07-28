0d369810a2ac869e939ea47e66afdd7b49b556db
David A.D. Morano <david.a.morano@gmail.com>
2026-07-28 00:46:31 -0400

    update

--- src/libuc/netgroupcheck.h
+++ src/libuc/netgroupcheck.h
@@ -20,12 +20,9 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<vecstr.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<vecstr.h>		/* LIBUC */
 
 
 EXTERNC_begin
