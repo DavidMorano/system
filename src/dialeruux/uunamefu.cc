0bdb7eb1fdce925721617d24441493435bd99107
David A.D. Morano <david.a.morano@gmail.com>
2026-07-28 00:37:49 -0400

    update

--- src/dialeruux/uunamefu.cc
+++ src/dialeruux/uunamefu.cc
@@ -42,7 +42,7 @@
 
 *******************************************************************************/
 
-#include	<envstandards.h>	/* must be before others */
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<sys/param.h>
 #include	<unistd.h>
