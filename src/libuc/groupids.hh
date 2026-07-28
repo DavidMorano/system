a1e0c8f2535cc2d1dcdbc237196e977fc242dbfb
David A.D. Morano <david.a.morano@gmail.com>
2026-07-28 00:44:21 -0400

    update

--- src/libuc/groupids.hh
+++ src/libuc/groupids.hh
@@ -21,9 +21,9 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>		/* system types */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<sys/types.h>		/* POSIX® system types */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 enum groupidsmems {
