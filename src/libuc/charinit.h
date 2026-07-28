4bbdd354e4583b9eb033262f6bb042cf0c2c9ce1
David A.D. Morano <david.a.morano@gmail.com>
2026-07-28 00:43:17 -0400

    update

--- src/libuc/charinit.h
+++ src/libuc/charinit.h
@@ -46,10 +46,10 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
 
 
 /* support tables; not accessed directly by callers  */
