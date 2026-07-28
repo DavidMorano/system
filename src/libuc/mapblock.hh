da5f8d32d02bad19e13c3f2ef9762e01867c92a6
David A.D. Morano <david.a.morano@gmail.com>
2026-07-28 00:45:57 -0400

    restore

---
+++ src/libuc/mapblock.hh
@@ -0,0 +1,29 @@
+/* mapblock HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* implement a map container of blocks (of a given structure) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MAPBLOCK_INCLUDE
+#define	MAPBLOCK_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* MAPBLOCK_INCLUDE */
+
+
