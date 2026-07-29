0d21d21b5c165f9e4365b8a15028191347f273a5
David A.D. Morano <david.a.morano@gmail.com>
2026-07-29 00:09:36 -0400

    restore

---
+++ src/libdam/term.h
@@ -0,0 +1,48 @@
+/* term HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* these are terminal related subroutines and objects */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Group:
+	term
+
+	Description:
+	These are terminal related subroutines and objects.
+
+*******************************************************************************/
+
+#ifndef	TERM_INCLUDE
+#define	TERM_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<termcharsets.h>
+#include	<termdisp.h>
+#include	<termenq.h>
+#include	<terment.h>
+#include	<termnote.h>
+#include	<termout.h>
+#include	<termtrans.h>
+#include	<termwritable.h>
+
+
+#endif /* TERM_INCLUDE */
+
+
