48cd3971b392104aaaf828ae2b527b40dc2bf1df
David A.D. Morano <david.a.morano@gmail.com>
2026-07-29 00:20:01 -0400

    update

--- src/libuc/xfile.h
+++ src/libuc/xfile.h
@@ -20,12 +20,9 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<ids.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<ids.h>			/* LIBUC */
 
 
 EXTERNC_begin
