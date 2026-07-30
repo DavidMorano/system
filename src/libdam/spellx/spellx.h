8d769d00f11c5a7c00cc0b5df1e6cc9b39c67769
David A.D. Morano <david.a.morano@gmail.com>
2026-07-30 02:09:40 -0400

    restore

---
+++ src/libdam/spellx/spellx.h
@@ -0,0 +1,31 @@
+/* spellx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* facilitate "string-list" operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+  = 2000-05-14, David A-D- Morano
+	This was written for Rightcore Network Services (RNS).
+ 
+*/
+
+/* Copyright © 1998 David A-D- Morano.  All rights reserved. */
+
+#ifndef	SPELLX_INCLUDE
+#define	SPELLX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<spellcheck.h>
+
+
+#endif /* SPELLX_INCLUDE */
+
+
