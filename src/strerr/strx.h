692b56b8c6dae16d09bc6bb2da1714310cfe9681
David A.D. Morano <david.a.morano@gmail.com>
2026-07-30 02:20:30 -0400

    update

--- src/strerr/strx.h
+++ src/strerr/strx.h
@@ -6,6 +6,13 @@
 /* version %I% last-modified %G% */
 
 
+/* revision history:
+
+	= 1998-03-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	STRX_INCLUDE
@@ -22,6 +29,7 @@
 #include	<usysdefs.h>
 
 #include	<strsub.h>
+#include	<strwildsub.h>
 #include	<strxbrk.h>
 #include	<strxcmp.h>
 #include	<strabbrerr.h>
@@ -35,11 +43,15 @@
 
 EXTERNC_begin
 
-extern int	strwildsub(cchar *,cchar *) noex ;
-
+/* this ( |strochr(3c)| ) is the companion subroutine of |strrchr(3c)| */
+#ifndef	SUBROUTINE_STROCHR
+#define	SUBROUTINE_STROCHR
+EXTERNC_begin
 local inline char *strochr(cchar *sp,int sch) noex {
     	return strchr(sp,sch) ;
 }
+EXTERNC_end
+#endif /* SUBROUTINE_STROCHR */
 
 /* deprecated (can give erroneous result) */
 [[deprecated("can give erroneous result")]] 
