8864e4069b7722aa852b53a088a6dbf86e483e65
David A.D. Morano <david.a.morano@gmail.com>
2026-07-30 02:16:14 -0400

    restore

---
+++ src/strerr/sfx.h
@@ -0,0 +1,43 @@
+/* sfx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* various string-find operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SFX_INCLUDE
+#define	SFX_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+#include	<sfcasesub.h>
+#include	<sfext.h>
+#include	<sfkey.h>
+#include	<sfkeyval.h>
+#include	<sfmisc.h>
+#include	<sfnext.h>
+#include	<sfxchr.h>
+#include	<sfxbrk.h>
+#include	<sfxname.h>
+#include	<sfxsub.h>
+#include	<sfweirdo.h>
+
+
+#endif /* SFX_INCLUDE */
+
+
