aeadc3eaa8cb318f370b3107423a5f47a9836994
David A.D. Morano <david.a.morano@gmail.com>
2026-07-30 02:10:19 -0400

    update

--- src/libdam/strfile/strfilemk.h
+++ src/libdam/strfile/strfilemk.h
@@ -13,49 +13,43 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>		/* system types */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<usyscalls.h>
+#include	<sys/types.h>		/* POSIX® system types */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 #include	"strfilemks.h"
 
 
 #define	STRFILEMK	struct strfilemk_head
-#define	STRFILEMK_CALLS	struct strfilemk_calls
+#define	STRFILEMK_FL	struct strfilemk_flags
 #define	STRFILEMK_MAGIC	0x99447256
 
 
-struct strfilemk_calls {
-	int	(*open)(void *,cchar *,int,mode_t,int) ;
-	int	(*chgrp)(void *,gid_t) ;
-	int	(*addfile)(void *,cchar *,int) ;
-	int	(*abort)(void *) ;
-	int	(*close)(void *) ;
-} ;
+struct strfilemk_flags {
+        uint            modload:1 ;
+} ; /* end struct */
 
 struct strfilemk_head {
-	uint		magic ;
-	MODLOAD		loader ;
-	STRFILEMK_CALLS call ;
-	void		*sop ;		/* shared-object (SO) pointer */
+	modload		*mlp ;
+	void		*callp ;
 	void		*obj ;		/* object pointer */
-	int		objsize ;	/* object size */
-} ;
+	STRFILEMK_FL	fl ;
+	uint		magval ;
+	int		objsz ;		/* object size */
+	int		cursz ;		/* cursor size */
+} ; /* end struct (strfilemk_head) */
 
 typedef	STRFILEMK	strfilemk ;
-typedef	STRFILEMK_CALLS	strfilemk_ca ;
+typedef	STRFILEMK_FL	strfilemk_fl ;
 
 EXTERNC_begin
 
-extern int	strfilemk_open(STRFILEMK *,cchar *,int,mode_t,int) noex ;
-extern int	strfilemk_chgrp(STRFILEMK *,gid_t) noex ;
-extern int	strfilemk_addfile(STRFILEMK *,cchar *,int) noex ;
-extern int	strfilemk_abort(STRFILEMK *) noex ;
-extern int	strfilemk_close(STRFILEMK *) noex ;
+extern int strfilemk_open	(strfilemk *,cchar *,int,mode_t,int) noex ;
+extern int strfilemk_chgrp	(strfilemk *,gid_t) noex ;
+extern int strfilemk_addfile	(strfilemk *,cchar *,int) noex ;
+extern int strfilemk_count	(strfilemk *) noex ;
+extern int strfilemk_abort	(strfilemk *) noex ;
+extern int strfilemk_close	(strfilemk *) noex ;
 
 EXTERNC_end
 
