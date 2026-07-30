ba6ea0a47e91cc7e6e866bdae983c55c7a286666
David A.D. Morano <david.a.morano@gmail.com>
2026-07-30 02:05:44 -0400

    restore

---
+++ src/libdam/holidayx/holidayx.h
@@ -0,0 +1,32 @@
+/* holidayx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* facilitate "string-list" operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+  = 2000-05-14, David A-D- Morano
+	This was written for Rightcore Network Services (RNS).
+ 
+*/
+
+/* Copyright © 1998 David A-D- Morano.  All rights reserved. */
+
+#ifndef	HOLIDAYX_INCLUDE
+#define	HOLIDAYX_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<holidays.h>
+#include	<holidayer.h>
+
+
+#endif /* HOLIDAYX_INCLUDE */
+
+
