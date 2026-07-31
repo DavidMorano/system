278c07de1fab3019b0fe0ab1e7a47e72ae55ba88
David A.D. Morano <david.a.morano@gmail.com>
2026-07-31 01:14:00 -0400

    update

--- src/libuc/sfx/sfxbrk.cc
+++ src/libuc/sfx/sfxbrk.cc
@@ -44,13 +44,13 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<strn.h>		/* |strnxbrk(3uc)| */
-#include	<char.h>		/* |CHAR_ISWHITE(3uc)| */
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<strn.h>		/* LIBUC |strnxbrk(3uc)| */
+#include	<char.h>		/* LIBUC |CHAR_ISWHITE(3uc)| */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"sfx.h"
 #include	"sfxbrk.h"
