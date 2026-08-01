99c7a4292807757740e8340146f073b745b006eb
David A.D. Morano <david.a.morano@gmail.com>
2026-08-01 00:34:58 -0400

    update

--- src/libuc/mnw/mnwcpy.cc
+++ src/libuc/mnw/mnwcpy.cc
@@ -50,16 +50,16 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<sncpyx.h>		/* |sncpy1(3uc)| */
-#include	<strwcpy.h>		/* |strwcpy(3uc)| */
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<sncpyx.h>		/* LIBUC |sncpy1(3uc)| */
+#include	<strwcpy.h>		/* LIBUC |strwcpy(3uc)| */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"mnwcpy.h"
 
@@ -120,7 +120,6 @@ int mnwcpy(char *dp,int dl,cchar *sp,int sl) noex {
 	    } /* end if */
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (mnwcpy) */
+} /* end subroutine (mnwcpy) */
 
 
