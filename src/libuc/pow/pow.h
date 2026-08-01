889be4f8d7be9df8cfe7c06da9d7fa397156389a
David A.D. Morano <david.a.morano@gmail.com>
2026-08-01 00:39:41 -0400

    update

--- src/libuc/pow/pow.h
+++ src/libuc/pow/pow.h
@@ -20,11 +20,11 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<stdintx.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<stdintx.h>		/* LIBU */
 
 #include	<powtwo.h>
 #include	<xpow.h>
