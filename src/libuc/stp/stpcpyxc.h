fedd43d660cdca6ad210fde11ce9f2154f496bf5
David A.D. Morano <david.a.morano@gmail.com>
2026-08-01 00:40:04 -0400

    update

--- src/libuc/stp/stpcpyxc.h
+++ src/libuc/stp/stpcpyxc.h
@@ -20,11 +20,11 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<strcpyxc.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<strcpyxc.h>		/* LIBUC */
 
 
 EXTERNC_begin
