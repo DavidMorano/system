e35076d71d11674c7352fe94de87715843959afe
David A.D. Morano <david.a.morano@gmail.com>
2026-08-01 00:34:23 -0400

    update

--- src/libuc/mapx/mapstrint.h
+++ src/libuc/mapx/mapstrint.h
@@ -20,12 +20,9 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<hdb.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<hdb.h>			/* LIBUC */
 
 
 #define	MAPSTRINT	hdb
