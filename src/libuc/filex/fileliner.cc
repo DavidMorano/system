c63a837830580fd5dc15d649fe586c71f56dc636
David A.D. Morano <david.a.morano@gmail.com>
2026-08-01 00:30:22 -0400

    update

--- src/libuc/filex/fileliner.cc
+++ src/libuc/filex/fileliner.cc
@@ -43,7 +43,7 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* CSTD |nullptr_t| */
+#include	<cstddef>		/* CSTD */
 #include	<cstdlib>		/* CSTD */
 #include	<clanguage.h>		/* LIBU */
 #include	<usysbase.h>		/* LIBU */
