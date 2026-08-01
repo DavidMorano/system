e4ab22c04efba649c532fe9cd2fb06269d752725
David A.D. Morano <david.a.morano@gmail.com>
2026-08-01 00:37:50 -0400

    update

--- src/libuc/open/opendial.h
+++ src/libuc/open/opendial.h
@@ -21,9 +21,9 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<opendials.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<opendials.h>		/* LIBUC */
 
 
 enum opendialopts {
