c630b5e01e3a74a89225f9f252bfd31b17ca60b6
David A.D. Morano <david.a.morano@gmail.com>
2026-08-02 00:52:15 -0400

    update

--- src/libuc/getxx/getpr.h
+++ src/libuc/getxx/getpr.h
@@ -13,9 +13,9 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<ucentpr.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<ucentpr.h>		/* LIBUC */
 
 
 EXTERNC_begin
