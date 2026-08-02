51af3dc71a3da5dc5da29e119a4b48c747a5c003
David A.D. Morano <david.a.morano@gmail.com>
2026-08-02 00:43:27 -0400

    update

--- src/libuc/gethe/getheour.h
+++ src/libuc/gethe/getheour.h
@@ -20,9 +20,9 @@
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<ucent.h>		/* |ucentho(3uc)| */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<ucent.h>		/* LIBUC |ucentho(3uc)| */
 
 
 EXTERNC_begin
