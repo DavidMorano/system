4e7e7bd915da9fb06caa9430e97b7da8e9aea794
David A.D. Morano <david.a.morano@gmail.com>
2026-08-02 00:48:32 -0400

    update

--- src/libuc/getx/getuserorg.cc
+++ src/libuc/getx/getuserorg.cc
@@ -46,28 +46,29 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<cstring>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
-#include	<uclibmem.h>
-#include	<ucpwcache.h>
-#include	<getax.h>
-#include	<getpwx.h>
-#include	<getusername.h>
-#include	<getuserhome.h>
-#include	<gecos.h>
-#include	<filereadln.h>
-#include	<sncpyx.h>
-#include	<mkpathx.h>
-#include	<char.h>
-#include	<isnot.h>
-#include	<localmisc.h>
+#include	<sys/types.h>		/* POSIX® */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<uclibmem.h>		/* LIBUC */
+#include	<ucpwcache.h>		/* LIBUC */
+#include	<ucpwcache.h>		/* LIBUC */
+#include	<getax.h>		/* LIBUC */
+#include	<getpwx.h>		/* LIBUC */
+#include	<getusername.h>		/* LIBUC */
+#include	<getuserhome.h>		/* LIBUC */
+#include	<gecos.h>		/* LIBU */
+#include	<filereadln.h>		/* LIBUC */
+#include	<sncpyx.h>		/* LIBUC */
+#include	<mkpathx.h>		/* LIBUC */
+#include	<char.h>		/* LIBUC */
+#include	<isnot.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"getuserorg.h"
 
@@ -132,13 +133,13 @@ int getuserorg(char *rbuf,int rlen,cchar *un) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
 	int		len = 0 ;
-	if (rbuf && un) {
+	if (rbuf && un) ylikely {
 	    rs = SR_INVALID ;
 	    rbuf[0] = '\0' ;
-	    if (un[0]) {
+	    if (un[0]) ylikely {
 	        subinfo		si, *sip = &si ;
 		cchar		*on = orgname ;
-	        if ((rs = subinfo_start(&si,on,rbuf,rlen,un)) >= 0) {
+	        if ((rs = subinfo_start(&si,on,rbuf,rlen,un)) >= 0) ylikely {
 	            for (int i = 0 ; i < 3 ; i += 1) {
 	                switch (i) {
 	                case 0:
@@ -160,20 +161,19 @@ int getuserorg(char *rbuf,int rlen,cchar *un) noex {
 	    } /* end if (valid) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? len : rs ;
-}
-/* end subroutine (getuserorg) */
+} /* end subroutine (getuserorg) */
 
 int gethomeorg(char *rbuf,int rlen,cchar *hd) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
 	int		len = 0 ;
-	if (rbuf && hd) {
+	if (rbuf && hd) ylikely {
 	    rs = SR_INVALID ;
 	    rbuf[0] = '\0' ;
-	    if (hd[0]) {
-	        if (char *cbuf ; (rs = lm_mn(&cbuf)) >= 0) {
+	    if (hd[0]) ylikely {
+	        if (char *cbuf ; (rs = lm_mn(&cbuf)) >= 0) ylikely {
 		    cint	clen = rs ;
-	            if ((rs = sncpy2(cbuf,clen,".",orgname)) >= 0) {
+	            if ((rs = sncpy2(cbuf,clen,".",orgname)) >= 0) ylikely {
 	                if (char *obuf ; (rs = lm_mp(&obuf)) >= 0) {
 	                    if ((rs = mkpath(obuf,hd,cbuf)) >= 0) {
 	                        if ((rs = filereadln(obuf,rbuf,rlen)) >= 0) {
@@ -192,15 +192,14 @@ int gethomeorg(char *rbuf,int rlen,cchar *hd) noex {
 	    } /* end if (valid) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? len : rs ;
-}
-/* end subroutine (gethomeorg) */
+} /* end subroutine (gethomeorg) */
 
 
 /* local subroutines */
 
 local int subinfo_start(SI *sip,cc *ofp,char *rbuf,int rlen,cc *un) noex {
 	int		rs = SR_FAULT ;
-	if (sip) {
+	if (sip) ylikely {
 	    rs = memclear(sip) ;
 	    sip->ofp = ofp ;
 	    sip->rbuf = rbuf ;
@@ -212,7 +211,7 @@ local int subinfo_start(SI *sip,cc *ofp,char *rbuf,int rlen,cc *un) noex {
 
 local int subinfo_finish(SI *sip) noex {
 	int		rs = SR_FAULT ;
-	if (sip) {
+	if (sip) ylikely {
 	    rs = SR_OK ;
 	}
 	return rs ;
@@ -245,9 +244,9 @@ local int getuserorg_home(SI *sip) noex {
 	int		rs ;
 	int		rs1 ;
 	int		len = 0 ;
-	if (char *hbuf ; (rs = lm_mp(&hbuf)) >= 0) {
+	if (char *hbuf ; (rs = lm_mp(&hbuf)) >= 0) ylikely { 
 	    cint	hlen = rs ;
-	    if ((rs = getuserhome(hbuf,hlen,sip->un)) >= 0) {
+	    if ((rs = getuserhome(hbuf,hlen,sip->un)) >= 0) ylikely {
 	        if ((rs = gethomeorg(sip->rbuf,sip->rlen,hbuf)) >= 0) {
 		    len = rs ;
 	        }
@@ -263,7 +262,7 @@ local int getuserorg_passwd(SI *sip) noex {
 	int		rs ;
 	int		rs1 ;
 	int		len = 0 ;
-	if (char *pwbuf ; (rs = lm_pw(&pwbuf)) >= 0) {
+	if (char *pwbuf ; (rs = lm_pw(&pwbuf)) >= 0) ylikely {
 	    ucentpw	pw ;
 	    cint	pwlen = rs ;
 	    if (sip->un[0] == '-') {
@@ -271,7 +270,7 @@ local int getuserorg_passwd(SI *sip) noex {
 	    } else {
 	        rs = getpwx_name(&pw,pwbuf,pwlen,sip->un) ;
 	    }
-	    if (rs >= 0) {
+	    if (rs >= 0) ylikely {
 	        if (gecos g ; (rs = gecos_start(&g,pw.pw_gecos,-1)) >= 0) {
 	            cint	gi = gecosval_organization ;
 	            cchar	*vp{} ;
@@ -295,8 +294,8 @@ local int getuserorg_sys(SI *sip) noex {
 	int		rs ;
 	int		rs1 ;
 	int		len = 0 ;
-	if (char *ofname ; (rs = lm_mp(&ofname)) >= 0) {
-	    if ((rs = mkpath(ofname,etcdir,sip->ofp)) >= 0) {
+	if (char *ofname ; (rs = lm_mp(&ofname)) >= 0) ylikely {
+	    if ((rs = mkpath(ofname,etcdir,sip->ofp)) >= 0) ylikely {
 	        rs = filereadln(ofname,sip->rbuf,sip->rlen) ;
 	        len = rs ;
 	    }
