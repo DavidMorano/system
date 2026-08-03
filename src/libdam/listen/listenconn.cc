320e6066058e1cc3a82b6341ad783e00b98173ef
David A.D. Morano <david.a.morano@gmail.com>
2026-08-03 00:40:12 -0400

    update

--- src/libdam/listen/listenconn.cc
+++ src/libdam/listen/listenconn.cc
@@ -1,4 +1,5 @@
 /* listenconn SUPPORT */
+/* charset=ISO8859-1 */
 /* lang=C++20 */
 
 /* subroutine to listen on a mounted FIFO for passed file-descriptors */
@@ -16,6 +17,10 @@
 
 /*******************************************************************************
 
+  	Name:
+	listenconn
+
+	Description:
 	This little subroutine checks for or establishes (if possible)
 	a mounted FIFO for listening for passed file descriptors.
 	This is a common method for standing servers to receive new
@@ -24,17 +29,23 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/types.h>
-#include	<sys/stat.h>
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<utime.h>		/* for |utime(2)| */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<netdb.h>
-#include	<usystem.h>
-#include	<isnot.h>
-#include	<localmisc.h>
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<utime.h>		/* POSIX® |utime(2)| */
+#include	<netdb.h>		/* POSIX® */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<ucmem.h>		/* LIBUC */
+#include	<ucopen.h>		/* LIBUC */
+#include	<ucdesc.h>		/* LIBUC */
+#include	<ucfileop.h>		/* LIBUC */
+#include	<isnot.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"listenconn.h"
 
@@ -45,6 +56,8 @@
 
 
 /* imported namespaces */
+
+using libuc::mem ;			/* variable */
 
 
 /* local typedefs */
@@ -61,7 +74,7 @@
 
 /* forward references */
 
-static int	mntcheck(cchar *,mode_t) noex ;
+local int	mntcheck(cchar *,mode_t) noex ;
 
 
 /* local variables */
@@ -76,12 +89,11 @@ int listenconn(cchar *mntfname,mode_t om,int opts) noex {
 	int		rs = SR_FAULT ;
 	int		fd = -1 ;
 	(void) opts ;
-	if (mntfname) {
+	if (mntfname) ylikely {
 	    rs = SR_INVALID ;
-	    if (mntfname[0]) {
-	        if ((rs = mntcheck(mntfname,om)) >= 0) {
-	            int		pipes[2] ;
-	            if ((rs = u_pipe(pipes)) >= 0) {
+	    if (mntfname[0]) ylikely {
+	        if ((rs = mntcheck(mntfname,om)) >= 0) ylikely {
+	            if (int pipes[2] ; (rs = u_pipe(pipes)) >= 0) ylikely {
 	                int	cfd = pipes[1] ;	/* client-side */
 	                fd = pipes[0] ;			/* server-side */
 	                if ((rs = u_ioctl(cfd,I_PUSH,"connld")) >= 0) {
@@ -90,8 +102,8 @@ int listenconn(cchar *mntfname,mode_t om,int opts) noex {
 	                        cfd = -1 ;
 	                        rs = uc_closeonexec(fd,true) ;
 			        if (rs < 0) {
-	            	            uc_fdetach(mntfname) ;
-			        }
+	            	            uc_detach(mntfname) ;
+			        } /* end if (error) */
 		            } /* end if (uc_fattach) */
 	                } /* end if (u_ioctl) */
 		        if (rs < 0) {
@@ -101,39 +113,38 @@ int listenconn(cchar *mntfname,mode_t om,int opts) noex {
 			        u_close(cfd) ;
 			        cfd = -1 ;
 		            }
-		        }
+		        } /* end if (error) */
 	            } /* end if (u_pipe) */
 	        } /* end if (mntcheck) */
 	    } /* end if (valid) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? fd : rs ;
-}
-/* end subroutine (listenconn) */
+} /* end subroutine (listenconn) */
 
 
 /* local subroutines */
 
-static int mntcheck(cchar *mntfname,mode_t om) noex {
+local int mntcheck(cchar *mntfname,mode_t om) noex {
 	cint		am = (R_OK|W_OK) ;
 	int		rs ;
-	int		f = FALSE ;
+	int		rs1 ;
+	int		f = false ; /* return-value */
 	if ((rs = uc_access(mntfname,am)) >= 0) {
 	    f = true ;
 	} else if (rs == SR_NOENT) {
 	    cint	of = (O_CREAT|O_RDWR) ;
 	    if ((rs = uc_open(mntfname,of,om)) >= 0) {
-	        USTAT	sb ;
 		cint	fd = rs ;
-	        if ((rs = u_fstat(fd,&sb)) >= 0) {
+	        if (ustat sb ; (rs = u_fstat(fd,&sb)) >= 0) {
 	            if ((sb.st_mode & S_IWOTH) == 0) {
 	                u_fchmod(fd,om) ;
 		    }
 	        }
-		u_close(fd) ;
+		rs1 = uc_close(fd) ;
+		if (rs >= 0) rs = rs1 ;
 	    } /* end if (uc_open) */
 	} /* end if */
 	return (rs >= 0) ? f : rs ;
-}
-/* end subroutine (mntcheck) */
+} /* end subroutine (mntcheck) */
 
 
