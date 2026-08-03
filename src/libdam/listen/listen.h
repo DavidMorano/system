8cbee8b26db8ecefaeb675a25e2b3baf1fcb7e5e
David A.D. Morano <david.a.morano@gmail.com>
2026-08-03 00:39:57 -0400

    restore

---
+++ src/libdam/listen/listen.h
@@ -0,0 +1,38 @@
+/* listen HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* network listerner related subroutines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2001-03-23, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2001 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	LISTEN_INCLUDE
+#define	LISTEN_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<sockaddress.h>		/* LIBU */
+
+#include	<listenconn.h>
+#include	<listenpass.h>
+#include	<listenspec.h>
+#include	<listentcp.h>
+#include	<listenudp.h>
+#include	<listenusd.h>
+#include	<listenuss.h>
+
+
+#endif /* LISTEN_INCLUDE */
+
+
