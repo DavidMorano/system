6a99a4b570a4575eb9e129179c0bd093b417db5e
David A.D. Morano <david.a.morano@gmail.com>
2026-08-03 01:15:34 -0400

    update

--- src/libuc/strnxcmp/strnxxxxcmp.h
+++ src/libuc/strnxcmp/strnxxxxcmp.h
@@ -20,11 +20,11 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<strings.h>		/* |strncasecmp(3c)| */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<strings.h>		/* BSD |strncasecmp(3c)| */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
 
 
 EXTERNC_begin
