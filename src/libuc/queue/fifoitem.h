ef1488c478d6c6211021ad8bc713aa06d1581031
David A.D. Morano <david.a.morano@gmail.com>
2026-08-03 00:51:35 -0400

    update

--- src/libuc/queue/fifoitem.h
+++ src/libuc/queue/fifoitem.h
@@ -13,10 +13,10 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<stddef.h>
-#include	<stdlib.h>
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<stddef.h>		/* CSTD */
+#include	<stdlib.h>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 #define	FIFOITEM	struct fifoitem_head
