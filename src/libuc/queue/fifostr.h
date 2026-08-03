e39b134ce7a06f2149a83cfaf83f434495005756
David A.D. Morano <david.a.morano@gmail.com>
2026-08-03 00:51:38 -0400

    update

--- src/libuc/queue/fifostr.h
+++ src/libuc/queue/fifostr.h
@@ -23,10 +23,10 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<stddef.h>
-#include	<stdlib.h>
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<stddef.h>		/* CSTD */
+#include	<stdlib.h>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 #define	FIFOSTR		struct fifostr_head
