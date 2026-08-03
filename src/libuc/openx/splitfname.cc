02d6fc2e2760d0b9c8ef956b4c05828fde99e140
David A.D. Morano <david.a.morano@gmail.com>
2026-08-03 00:49:38 -0400

    update

--- src/libuc/openx/splitfname.cc
+++ src/libuc/openx/splitfname.cc
@@ -40,7 +40,7 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* CSTD |nullptr_t| */
+#include	<cstddef>		/* CSTD */
 #include	<cstdlib>		/* CSTD */
 #include	<clanguage.h>		/* LIBU */
 #include	<usysbase.h>		/* LIBU */
@@ -87,7 +87,7 @@ cbool			f_debug		= CF_DEBUG ;
 int splitfname_split(splitfname *op,cchar *fp,int fl) noex {
 	int		rs = SR_FAULT ;
 	int		bl = 0 ; /* return-value */
-	if (op && fp) {
+	if (op && fp) ylikely {
 	    {
 		strnul	ps(fp,fl) ;
 	        DPRINTF("ent fl=%d f=>%s<\n",fl,ccp(ps)) ;
@@ -108,15 +108,14 @@ int splitfname_split(splitfname *op,cchar *fp,int fl) noex {
 	} /* end if (non-null) */
 	DPRINTF("ent rs=%d bl=%d\n",rs,bl) ;
 	return (rs >= 0) ? bl : rs ;
-}
-/* end subroutine (splitfname_split) */
+} /* end subroutine (splitfname_split) */
 
 
 /* local subroutines */
 
 int splitfname::operator () (cchar *fp,int fl) noex {
 	int		rs = SR_BUGCHECK ;
-	if (this) {
+	if (fp) ylikely {
 	    rs = splitfname_split(this,fp,fl) ;
 	}
 	return rs ;
