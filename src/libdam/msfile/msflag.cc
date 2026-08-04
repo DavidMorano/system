6331daec72a0da2109d5a497fc2e88b6bc4274d8
David A.D. Morano <david.a.morano@gmail.com>
2026-08-04 01:22:32 -0400

    restore

---
+++ src/libdam/msfile/msflag.cc
@@ -0,0 +1,68 @@
+/* msflag SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* standard (relatively) UNIX® preprocessor defiitions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1999-06-01, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	msflag
+
+	Description:
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"msflag.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
