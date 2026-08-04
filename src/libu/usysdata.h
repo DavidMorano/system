0ef31a4f41759e29fdcd8aef57c72e61097c84b2
David A.D. Morano <david.a.morano@gmail.com>
2026-08-04 01:27:26 -0400

    update

--- src/libu/usysdata.h
+++ src/libu/usysdata.h
@@ -19,10 +19,10 @@
 /*******************************************************************************
 
 	Names:
-	int u_uname(UTSNAME *) noex
-	int u_getnodename(char *,int) noex
-	int u_getauxinfo(char *,int,int) noex
-	int u_gethostid(ulong *) noex
+	int u_uname		(UTSNAME *) noex
+	int u_getnodename	(char *,int) noex
+	int u_getauxinfo	(char *,int,int) noex
+	int u_gethostid		(ulong *) noex
 
 	Description:
 	These subroutines mange the operating system itself.
@@ -34,31 +34,31 @@
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<sys/types.h>		/* system types */
-#include	<sys/utsname.h>		/* |UTSNAME| */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<sys/types.h>		/* POSIX® system types */
+#include	<sys/utsname.h>		/* POSIX® |UTSNAME| */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
 
 #include	<usysauxinfo.h>		/* the request codes */
 
 
 EXTERNC_begin
 
-extern int u_uname(UTSNAME *) noex ;
-extern int u_getnodename(char *,int) noex ;
-extern int u_getauxinfo(char *,int,int) noex ;
-extern int u_gethostid(ulong *) noex ;
+extern int u_uname		(UTSNAME *) noex ;
+extern int u_getnodename	(char *,int) noex ;
+extern int u_getauxinfo		(char *,int,int) noex ;
+extern int u_gethostid		(ulong *) noex ;
 
 EXTERNC_end
 
 #ifdef	__cplusplus
 
 namespace libu {
-    extern int loadhostid(char *,int) noex ;
-    extern int ugethostid(ulong *) noex ;
+    extern int locdhwserial	(char *,int) noex ;
+    extern int ugethostid	(ulong *) noex ;
 }
 
 #endif /* __cplusplus */
