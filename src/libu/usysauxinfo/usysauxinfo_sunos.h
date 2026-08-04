7432cca609286ff96e18970de14d0a255aab4634
David A.D. Morano <david.a.morano@gmail.com>
2026-08-04 01:27:18 -0400

    update

--- src/libu/usysauxinfo/usysauxinfo_sunos.h
+++ src/libu/usysauxinfo/usysauxinfo_sunos.h
@@ -35,8 +35,6 @@
 /* USYSAUXINFO_SUNOS start */
 #if	defined(OSNAME_SunOS) && (OSNAME_SunOS > 0)
 
-#include	<sys/types.h>
-#include	<sys/systeminfo.h>	/* <- where the stuff we want is */
 #include	<clanguage.h>
 #include	<utypedefs.h>
 #include	<utypealiases.h>
