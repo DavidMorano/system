2e1618b003b4f3ed057f172933d6d153af94ea59
David A.D. Morano <david.a.morano@gmail.com>
2026-08-04 01:30:45 -0400

    update

--- src/libuc/termx/termconseq.h
+++ src/libuc/termx/termconseq.h
@@ -20,12 +20,12 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<stdarg.h>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<stdarg.h>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
 
 
 #define	TERMCONSEQ_ARGS		struct termconseq_args
@@ -33,8 +33,8 @@
 
 EXTERNC_begin
 
-extern int termconseqva(char *,int,int,cchar *,int,va_list) noex ;
-extern int termconseqa(char *,int,int,cchar *,int,...) noex ;
+extern int termconseqva	(char *,int,int,cchar *,int,va_list) noex ;
+extern int termconseqa	(char *,int,int,cchar *,int,...) noex ;
 
 EXTERNC_end
 
@@ -42,37 +42,37 @@ EXTERNC_end
 
 #ifdef	COMMENT
 
-static inline int termconseq(char *dp,int dl,int n,
+local inline int termconseq(char *dp,int dl,int n,
 		int a1 = -1,int a2 = -1,int a3 = -1,int a4 = -1) noex {
 	return termconseqa(dp,dl,n,nullptr,4,a1,a2,a3,a4) ;
-}
+} /* end subroutine */
 
 #else /* COMMENT */
 
 template<typename ... Args>
-static inline int termconseq(char *dp,int dl,int nm,Args ... args) noex {
+local inline int termconseq(char *dp,int dl,int nm,Args ... args) noex {
 	std::nullptr_t	np{} ;
 	cint		na = npack(Args) ;
 	return termconseqa(dp,dl,nm,np,na,args ...) ;
-}
+} /* end subroutine */
 
 #endif /* COMMENT */
 
-static inline int termconseqi(char *dp,int dl,int n,cchar *is,
+local inline int termconseqi(char *dp,int dl,int n,cchar *is,
 		int a1 = -1,int a2 = -1,int a3 = -1,int a4 = -1) noex {
 	return termconseqa(dp,dl,n,is,4,a1,a2,a3,a4) ;
-}
+} /* end subroutine */
 
 #else /* __cplusplus */
 
-static inline int termconseq(char *dp,int dl,int n,
+local inline int termconseq(char *dp,int dl,int n,
 		int a1,int a2,int a3,int a4) noex {
 	return termconseqa(dp,dl,n,nullptr,4,a1,a2,a3,a4) ;
-}
-static inline int termconseqi(char *dp,int dl,int n,cchar *is,
+} /* end subroutine */
+local inline int termconseqi(char *dp,int dl,int n,cchar *is,
 		int a1,int a2,int a3,int a4) noex {
 	return termconseqa(dp,dl,n,is,4,a1,a2,a3,a4) ;
-}
+} /* end subroutine */
 
 #endif /* __cplusplus */
 
