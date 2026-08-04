5a4396e7577c5c09de2606af4232523e1a481bf0
David A.D. Morano <david.a.morano@gmail.com>
2026-08-04 00:59:01 -0400

    update

--- src/libdam/langx/langproc.h
+++ src/libdam/langx/langproc.h
@@ -20,12 +20,9 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<langstate.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<langstate.h>		/* LIBUC */
 
 
 #define	LANGPROC		struct langproc_head
