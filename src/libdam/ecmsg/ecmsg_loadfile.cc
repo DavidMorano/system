a61dc022e9d85ade909a44943ef1430699f4a804
David A.D. Morano <david.a.morano@gmail.com>
2026-08-05 01:08:59 -0400

    update

--- src/libdam/ecmsg/ecmsg_loadfile.cc
+++ src/libdam/ecmsg/ecmsg_loadfile.cc
@@ -31,20 +31,23 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/types.h>
-#include	<sys/stat.h>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<cstring>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
-#include	<uclibmem.h>
-#include	<ucdesc.h>
-#include	<bufsizevar.hh>
-#include	<intsat.h>
-#include	<isnot.h>
-#include	<localmisc.h>
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBUC */
+#include	<uclibmem.h>		/* LIBUC */
+#include	<ucopen.h>		/* LIBUC */
+#include	<ucdesc.h>		/* LIBUC */
+#include	<ucfileop.h>		/* LIBUC */
+#include	<bufsizevar.hh>		/* LIBUC */
+#include	<isnot.h>		/* LIBUC */
+#include	<intsat.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"ecmsg.h"
 
@@ -60,12 +63,6 @@
 
 /* external subroutines */
 
-extern "C" {
-    extern int uc_open(cchar *,int,mode_t) noex ;
-    extern int uc_stat(cchar *,ustat *) noex ;
-    extern int uc_readn(int,void *,int) noex ;
-}
-
 
 /* external variables */
 
@@ -75,7 +72,7 @@ extern "C" {
 
 /* forward references */
 
-static int	getlinelen(cchar *) noex ;
+local int	getlinelen(cchar *) noex ;
 
 
 /* local variables */
@@ -92,19 +89,19 @@ int ecmsg_loadfile(ecmsg *op,cchar *fn) noex {
 	int		rs = SR_FAULT ;
 	int		rs1 ;
 	int		len = 0 ;
-	if (op && fn) {
+	if (op && fn) ylikely {
 	    rs = SR_INVALID ;
-	    if (fn[0]) {
-	        if ((rs = getlinelen(fn)) >= 0) {
+	    if (fn[0]) ylikely {
+	        if ((rs = getlinelen(fn)) >= 0) ylikely {
 		    int		mlen = rs ;
 	            char	*mbuf ;
 	            if (mlen > ECMSG_MAXBUFLEN) mlen = ECMSG_MAXBUFLEN ;
-	            if ((rs = lm_mall((mlen+1),&mbuf)) >= 0) {
+	            if ((rs = lm_mall((mlen+1),&mbuf)) >= 0) ylikely {
 		        cint	of = O_RDONLY ;
 		        cmode	om = 0666 ;
-	                if ((rs= uc_open(fn,of,om)) >= 0) {
+	                if ((rs= uc_open(fn,of,om)) >= 0) ylikely {
 		            cint	mfd = rs ;
-		            if ((rs = uc_readn(mfd,mbuf,mlen)) >= 0) {
+		            if ((rs = uc_readn(mfd,mbuf,mlen)) >= 0) ylikely {
 	                        rs = ecmsg_loadbuf(op,mbuf,rs) ;
 		                len = rs ;
 		            }
@@ -118,24 +115,22 @@ int ecmsg_loadfile(ecmsg *op,cchar *fn) noex {
 	    } /* end if (valid) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? len : rs ;
-}
-/* end subroutine (ecmsg_loadfile) */
+} /* end subroutine (ecmsg_loadfile) */
 
 
 /* local subroutines */
 
-static int getlinelen(cchar *fn) noex {
-	USTAT		sb ;
+local int getlinelen(cchar *fn) noex {
 	int		rs ;
 	int		mlen = 0 ;
-	if ((rs = uc_stat(fn,&sb)) >= 0) {
-	    mlen = intsat(sb.st_size) ;
+	if (ustat sb ; (rs = uc_stat(fn,&sb)) >= 0) {
+	    csize fsize = size_t(sb.st_size) ;
+	    mlen = intsat(fsize) ;
 	} else if (isNotPresent(rs)) {
 	    rs = maxlinelen ;
 	    mlen = rs ;
-	}
+	} /* end if */
 	return (rs >= 0) ? mlen : rs ;
-}
-/* end subroutine (getlinelen) */
+} /* end subroutine (getlinelen) */
 
 
