ce508d2f9cca876d5da34cf9c69d60b829dedbc4
David A.D. Morano <david.a.morano@gmail.com>
2026-08-05 01:13:47 -0400

    restore

---
+++ src/libuc/spawn/spawn.h
@@ -0,0 +1,42 @@
+/* spawn HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* spawn a process (in various ways) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-10, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	spawn
+
+	Description:
+	Spawn a process (in different ways).
+
+*******************************************************************************/
+
+#ifndef	SPAWN_INCLUDE
+#define	SPAWN_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<spawner.h>
+#include	<spawnproc.h>
+
+
+#endif /* SPAWN_INCLUDE */
+
+
