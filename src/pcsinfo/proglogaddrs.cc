f7a45dff4a78dfd93508cf73090583581bf46493
David A.D. Morano <david.a.morano@gmail.com>
2026-08-06 01:08:50 -0400

    update

--- src/pcsinfo/proglogaddrs.cc
+++ src/pcsinfo/proglogaddrs.cc
@@ -35,7 +35,7 @@
 
 *******************************************************************************/
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 
 #include	<sys/types.h>
 #include	<sys/param.h>
