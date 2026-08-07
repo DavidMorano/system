1fa46b17ccb4700f0c895a6c459e4828ff2a061f
David A.D. Morano <david.a.morano@gmail.com>
2026-08-07 00:35:35 -0400

    update

--- src/libdam/database/commandments.cc
+++ src/libdam/database/commandments.cc
@@ -38,7 +38,8 @@
 #include	<cstddef>		/* CSTD */
 #include	<cstdlib>		/* CSTD */
 #include	<cstring>		/* CSTD */
-#include	<memory>                /* C++STD |destroy_at(3c++)| */
+#include	<new>			/* C++STD placement-new */
+#include	<memory>		/* C++STD |destroy_at(3c++)| */
 #include	<clanguage.h>		/* LIBU */
 #include	<usysbase.h>		/* LIBU */
 #include	<usyscalls.h>		/* LIBU */
