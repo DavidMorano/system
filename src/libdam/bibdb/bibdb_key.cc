4dc3fe00efa1b14e67178ab1aaeb47c6752a17c1
David A.D. Morano <david.a.morano@gmail.com>
2026-08-07 00:35:16 -0400

    update

--- src/libdam/bibdb/bibdb_key.cc
+++ src/libdam/bibdb/bibdb_key.cc
@@ -67,7 +67,8 @@
 #include	<cstddef>		/* CSTD */
 #include	<cstdlib>		/* CSTD */
 #include	<cstring>		/* CSTD */
-#include	<memory>                /* C++STD |destroy_at(3c++)| */
+#include	<new>			/* C++STD placement-new */
+#include	<memory>		/* C++STD |destroy_at(3c++)| */
 #include	<clanguage.h>		/* LIBU */
 #include	<usysbase.h>		/* LIBU */
 #include	<ucmem.h>		/* LIBUC */
