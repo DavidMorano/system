ef25c294dda19d2b107ad03a9716e36ddad09c9f
David A.D. Morano <david.a.morano@gmail.com>
2026-08-08 00:26:44 -0400

    restore

---
+++ src/libdam/mathx/fft.hh
@@ -0,0 +1,33 @@
+/* fft HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* calculate the Fast-Fourier-Transform (FFT) on a database */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FFT_INCLUDE
+#define	FFT_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* FFT_INCLUDE */
+
+
