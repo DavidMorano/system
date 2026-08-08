55df00ff9eabf010523081ff91241aaa9de7e038
David A.D. Morano <david.a.morano@gmail.com>
2026-08-08 00:27:14 -0400

    restore

---
+++ src/libdam/instr/instropts.cc
@@ -0,0 +1,72 @@
+/* instropts SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* standard (relatively) UNIX® preprocessor defiitions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1992-05-14, David A­D­ Morano
+	This was originally written for a UNIX-based hardware 
+	support tool.
+
+*/
+
+/* Copyright © 1992 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	instropts
+
+	Description:
+	This contains miscellaneious support subroutines.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"instropts.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+const instroptms		instroptm ;
+
+
+/* exported subroutines */
+
+
