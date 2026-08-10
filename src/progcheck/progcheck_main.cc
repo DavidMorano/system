61e7712609f9b63e6a230fae251da0638bac80a6
David A.D. Morano <david.a.morano@gmail.com>
2026-08-10 00:35:18 -0400

    update

--- src/progcheck/progcheck_main.cc
+++ src/progcheck/progcheck_main.cc
@@ -1041,7 +1041,7 @@ local int procopts(PI *pip,KO *kop) noex {
 	        int	kl, vl ;
 	        cchar	*kp, *vp ;
 
-	        while ((kl = keyopt_enumkeys(kop,&kcur,&kp)) >= 0) {
+	        while ((kl = keyopt_curenumkeys(kop,&kcur,&kp)) >= 0) {
 
 	            if ((oi = matostr(akonames,2,kp,kl)) >= 0) {
 
