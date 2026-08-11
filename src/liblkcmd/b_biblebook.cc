28073a5a72efd0199e4a06a499202a9560679630
David A.D. Morano <david.a.morano@gmail.com>
2026-08-11 00:37:55 -0400

    update

--- src/liblkcmd/b_biblebook.cc
+++ src/liblkcmd/b_biblebook.cc
@@ -886,7 +886,7 @@ local int procopts(PROGINFO *pip,keyopt *kop)
 	        int	kl, vl ;
 	        cchar	*kp, *vp ;
 
-	        while ((kl = keyopt_enumkeys(kop,&kcur,&kp)) >= 0) {
+	        while ((kl = keyopt_curenumkeys(kop,&kcur,&kp)) >= 0) {
 
 	            if ((oi = matostr(akonames,2,kp,kl)) >= 0) {
 
