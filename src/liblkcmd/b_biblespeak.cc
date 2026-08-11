0d55fbc36afafdee219a936175ebc923183937b1
David A.D. Morano <david.a.morano@gmail.com>
2026-08-11 00:38:02 -0400

    update

--- src/liblkcmd/b_biblespeak.cc
+++ src/liblkcmd/b_biblespeak.cc
@@ -1094,7 +1094,7 @@ local int procopts(PROGINFO *pip,keyopt *kop) noex {
 	        int	kl, vl ;
 	        cchar	*kp, *vp ;
 
-	        while ((kl = keyopt_enumkeys(kop,&kcur,&kp)) >= 0) {
+	        while ((kl = keyopt_curenumkeys(kop,&kcur,&kp)) >= 0) {
 
 	            if ((oi = matostr(akonames,2,kp,kl)) >= 0) {
 
