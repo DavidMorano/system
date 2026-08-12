09408119426245c577eb57c2ace9027f53e52891
David A.D. Morano <david.a.morano@gmail.com>
2026-08-12 01:41:36 -0400

    restore

---
+++ src/libdam/get.h
@@ -0,0 +1,36 @@
+/* get HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* string-index operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	GET_INCLUDE
+#define	GET_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<getclusters.h>
+#include	<getfiledirs.h>
+#include	<getprogroot.h>
+#include	<getprovider.h>
+#include	<getsrcname.h>
+#include	<gettid.h>
+
+
+#endif /* GET_INCLUDE */
+
+
