55829ce421d0c4aefa891e890711c461ce6494b2
David A.D. Morano <david.a.morano@gmail.com>
2026-08-12 01:58:13 -0400

    restore

---
+++ src/rmailerd/rmailerd_proc.h
@@ -0,0 +1,18 @@
+/* rmailerd_proc HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* mail this message off to its recipients */
+/* version %I% last-modified %G% */
+
+
+struct address {
+	int	f_single ;
+	char	*ema ;
+	char	*hostname ;
+	char	*localname ;
+} ;
+
+
+
+
