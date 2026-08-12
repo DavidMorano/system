998a875efc04983377262afc8ad183ab773a3a0d
David A.D. Morano <david.a.morano@gmail.com>
2026-08-12 01:42:08 -0400

    restore

---
+++ src/libdam/get/getprogroot.h
@@ -0,0 +1,36 @@
+/* getprogroot HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get program root directory */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	GETPROGROOT_INCLUDE
+#define	GETPROGROOT_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int	getprogroot(cchar *,con mainv,int *,char *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* GETPROGROOT_INCLUDE */
+
+
