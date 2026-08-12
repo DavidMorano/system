b11f3b940f9f4710cc4843a6d8c45927d66ba90d
David A.D. Morano <david.a.morano@gmail.com>
2026-08-12 01:48:13 -0400

    restore

---
+++ src/libu/vardefs.cc
@@ -0,0 +1,69 @@
+/* vardefs SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* standard (relatively) UNIX® preprocessor defiitions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2001-04-11, David A-D- Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2001 David A-D- Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	vardefs
+
+	Description:
+	This contains miscellaneious support subroutines.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"vardefs.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
