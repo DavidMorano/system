e546f1e1597419b7e25e887065033eb349b9c6e5
David A.D. Morano <david.a.morano@gmail.com>
2026-08-12 01:41:52 -0400

    restore

---
+++ src/libdam/get/getclusters.h
@@ -0,0 +1,37 @@
+/* getclusters HEADER (Check-ONC) */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* get all clusters for the specified nodename */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1995-07-01, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1995 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	GETCLUSTERS_INCLUDE
+#define	GETCLUSTERS_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<vecstr.h>		/* LIBUC */
+
+
+EXTERNC_begin
+
+extern int	getclusters(cchar *,vecstr *,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* GETCLUSTERS_INCLUDE */
+
+
