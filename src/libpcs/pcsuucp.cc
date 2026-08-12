a1be91e836c279d38ec8634c14edcbd7a5a115c0
David A.D. Morano <david.a.morano@gmail.com>
2026-08-12 01:47:44 -0400

    update

--- src/libpcs/pcsuucp.cc
+++ src/libpcs/pcsuucp.cc
@@ -5,7 +5,7 @@
 /* get a FD to a file from another host using UUCP */
 /* version %I% last-modified %G% */
 
-#define	CF_DEBUGS	0		/* compile-time debugging */
+#define	CF_DEBUG	0		/* compile-time debugging */
 #define	CF_BDUMP	1		/* dump extraneous output */
 
 /* revision history:
@@ -42,23 +42,24 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/types.h>
-#include	<sys/param.h>
-#include	<sys/stat.h>
-#include	<sys/socket.h>
-#include	<sys/wait.h>
-#include	<netinet/in.h>
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<netdb.h>
-#include	<strings.h>		/* |strncasecmp(3c)| */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>		/* |getenv(3c)| */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<bfile.h>
-#include	<exitcodes.h>
-#include	<localmisc.h>
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/param.h>		/* POSIX® */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<sys/socket.h>		/* POSIX® */
+#include	<sys/wait.h>		/* POSIX® */
+#include	<netinet/in.h>		/* POSIX® */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<netdb.h>		/* POSIX® */
+#include	<strings.h>		/* BSD |strncasecmp(3c)| */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<exitcodes.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+#include	<libdebug.h>		/* LIBDEBUG |DEBUGPRINTF(3debug)| */
+#include	<bfile.h>		/* LIBB */
 
 #include	"pcsuucp.h"
 
@@ -74,10 +75,6 @@
 
 
 /* external subroutines */
-
-#if	CF_DEBUGS
-extern int	debugprintf(cchar *,...) ;
-#endif
 
 
 /* external variables */
@@ -109,21 +106,16 @@ int pcsuucp(cchar *rhost,cchar *filename,int *fd2p) noex {
 	cchar	*cmd_uucp = PROG_UUCP ;
 	char		pfname[MAXPATHLEN + 1] ;
 	char		dst[DSTLEN + 1] ;
-
-
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("uucp: ent\n") ;
 #endif
-
-/* check for bad input */
-
-	if ((rhost == NULL) || (rhost[0] == '\0'))
-	    return SR_INVAL ;
+	/* check for bad input */
+	if ((rhost == NULL) || (rhost[0] == '\0')) return SR_INVAL ;
 
 	if (u_access(cmd_uucp,X_OK) < 0) 
 	    return SR_PROTO ;
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("uucp: got in\n") ;
 #endif
 
@@ -133,7 +125,7 @@ int pcsuucp(cchar *rhost,cchar *filename,int *fd2p) noex {
 	if (rs < 0)
 	    return SR_HOSTUNREACH ;
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("uucp: continuing\n") ;
 #endif
 
@@ -144,7 +136,7 @@ int pcsuucp(cchar *rhost,cchar *filename,int *fd2p) noex {
 	if ((fd2p != NULL) && ((rs = u_pipe(pipes[2])) < 0))
 	    goto badpipes ;
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("uucp: about to make pipe file\n") ;
 #endif
 
@@ -155,7 +147,7 @@ int pcsuucp(cchar *rhost,cchar *filename,int *fd2p) noex {
 	}
 
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("uucp: got pipes \n") ;
 #endif
 
@@ -166,14 +158,14 @@ int pcsuucp(cchar *rhost,cchar *filename,int *fd2p) noex {
 
 /* we fork the command */
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("uucp: about to fork\n") ;
 #endif
 
 	if ((rs = uc_fork()) == 0) { /* child */
 	    int		fd ;
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	    debugprintf("uucp: inside fork\n") ;
 #endif
 
@@ -207,7 +199,7 @@ int pcsuucp(cchar *rhost,cchar *filename,int *fd2p) noex {
 	} else if (rs < 0)
 		goto badfork ;
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("uucp: main line continue\n") ;
 #endif
 
@@ -220,7 +212,7 @@ int pcsuucp(cchar *rhost,cchar *filename,int *fd2p) noex {
 
 /* open the pipe file */
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("uucp: about to open the pipe\n") ;
 #endif
 
@@ -229,7 +221,7 @@ int pcsuucp(cchar *rhost,cchar *filename,int *fd2p) noex {
 	if (rs < 0)
 	    goto badopen ;
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("uucp: opened pipe, about to read answer\n") ;
 	sleep(10) ;
 	system("ps -f") ;
@@ -242,14 +234,14 @@ int pcsuucp(cchar *rhost,cchar *filename,int *fd2p) noex {
 	int f_bad = false ;
 	while ((rs = reade(pipes[2][0],pfname,1,0,10)) > 0) {
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	    debugprintf("uucp: 'reade' goto one\n") ;
 #endif
 
 	    f_bad = true ;
 	}
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("uucp: read answer, f_bad=%d\n",f_bad) ;
 #endif
 	}
@@ -265,7 +257,7 @@ int pcsuucp(cchar *rhost,cchar *filename,int *fd2p) noex {
 
 ret0:
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("uucp: ret rs=%d fd=%d\n", rs,pfd) ;
 #endif
 
@@ -287,8 +279,7 @@ badpipemk:
 
 badpipes:
 	goto ret0 ;
-}
-/* end subroutine (uucp) */
+} /* end subroutine (uucp) */
 
 
 /* local subroutines */
@@ -306,14 +297,14 @@ local int testuucp(cchar *queue_machine) {
 	char		buf[NODENAMELEN + 1] ;
 	char		*cp ;
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("testuucp: ent\n") ;
 #endif
 
 	if ((queue_machine == NULL) || (queue_machine[0] == '\0'))
 	    return BAD ;
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("testuucp: got in\n") ;
 #endif
 
@@ -324,14 +315,14 @@ local int testuucp(cchar *queue_machine) {
 	fpa[1] = pfp ;		/* capture the standard output ! */
 	fpa[2] = &file2 ;
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("testuucp: about to open command - FPA[1]=%08X\n",fpa[1]) ;
 	debugprintf("testuucp: FPA[0]=%08X\n",fpa[0]) ;
 #endif
 
 	if ((rs = bopencmd(fpa,cmd_uuname)) >= 0) {
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	    debugprintf("testuucp: opened command OK\n") ;
 #endif
 
@@ -349,7 +340,7 @@ local int testuucp(cchar *queue_machine) {
 	    rs = BAD ;
 	    while ((l = breadln(pfp,buf,NODENAMELEN)) > 0) {
 
-#if	CF_DEBUGS && 0
+#if	CF_DEBUG && 0
 	        debugprintf("testuucp: got a line\n") ;
 #endif
 
@@ -360,7 +351,7 @@ local int testuucp(cchar *queue_machine) {
 
 	        if ((i == j) && (strncasecmp(cp,queue_machine,i) == 0)) {
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	            debugprintf("testuucp: got a machine match\n") ;
 #endif
 
@@ -381,13 +372,12 @@ local int testuucp(cchar *queue_machine) {
 
 	} /* end if (program spawned) */
 
-#if	CF_DEBUGS
+#if	CF_DEBUG
 	debugprintf("testuucp: passed the command OK\n") ;
 #endif
 
 	return rs ;
-}
-/* end subroutine (testuucp) */
+} /* end subroutine (testuucp) */
 
 #if	CF_BDUMP
 
@@ -395,27 +385,19 @@ static void bdump(bfile *f1p,bfile *f2p) noex {
 	int	f_done1 = false ;
 	int	f_done2 = false ;
 	char	buf[DUMPLEN + 1] ;
-
 	while ((! f_done1) || (! f_done2)) {
-
 	    if (! f_done1) {
-
-	        if (bread(f1p,buf,DUMPLEN) <= 0)
+	        if (bread(f1p,buf,DUMPLEN) <= 0) {
 	            f_done1 = true ;
-
+		}
 	    }
-
 	    if (! f_done2) {
-
-	        if (bread(f2p,buf,DUMPLEN) <= 0)
+	        if (bread(f2p,buf,DUMPLEN) <= 0) {
 	            f_done2 = true ;
-
+		}
 	    }
-
 	} /* end while */
-
-}
-/* end subroutine (bdump) */
+} /* end subroutine (bdump) */
 
 #endif /* CF_BDUMP */
 
