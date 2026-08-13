62679f89ce83d659cd7b2d8e66b838c7fd7584d6
David A.D. Morano <david.a.morano@gmail.com>
2026-08-13 01:40:46 -0400

    upgrade

--- src/rmail/rmail_main.cc
+++ src/rmail/rmail_main.cc
@@ -1421,7 +1421,7 @@ local int procopts(PI *pip,keyopt *kop,paramopt *aop) noex {
 	        int	kl, vl ;
 	        cchar	*kp, *vp ;
 
-	        while ((kl = keyopt_enumkeys(kop,&kcur,&kp)) >= 0) {
+	        while ((kl = keyopt_curenumkeys(kop,&kcur,&kp)) >= 0) {
 
 	            if ((oi = matostr(locopts,3,kp,kl)) >= 0) {
 
