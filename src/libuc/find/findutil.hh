7bfb68e3b0d24052e802e18e8dcbbe5a8571d560
David A.D. Morano <david.a.morano@gmail.com>
2026-08-14 01:45:53 -0400

    update

---
+++ src/libuc/find/findutil.hh
@@ -0,0 +1,44 @@
+/* findutil dwdeb HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* find-file utilities */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	findutil
+
+	Description:
+	These code pieces support various means of finding files
+	in the filesystem.
+
+*******************************************************************************/
+
+#ifndef	FINDUTIL_INCLUDE
+#define	FINDUTIL_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* FINDUTIL_INCLUDE */
+
+
