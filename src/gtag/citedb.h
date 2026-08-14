c4a9aa51b98de3292fabb519703db969f2d62777
David A.D. Morano <david.a.morano@gmail.com>
2026-08-14 01:31:07 -0400

    update

--- src/gtag/citedb.h
+++ src/gtag/citedb.h
@@ -1,4 +1,9 @@
-/* citedb */
+/* citedb HEADER (Citætion Data-base) */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* maintain a DB of encountered citations */
+/* version %I% last-modified %G% */
 
 
 /* revision history:
@@ -24,89 +29,82 @@
 
 /* Copyright © 1998,2005 David A­D­ Morano.  All rights reserved. */
 
-
 #ifndef	CITEDB_INCLUDE
-#define	CITEDB_INCLUDE	1
+#define	CITEDB_INCLUDE
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-
-#include	<sys/types.h>
-
-#include	<vecobj.h>
-#include	<hdb.h>
-#include	<localmisc.h>		/* extra types */
+#include	<sys/types.h>		/* POSIX® */
+#include	<vecobj.h>		/* LIBUC */
+#include	<hdb.h>		/* LIBUC */
 
 
 #define	CITEDB			struct citedb_head
-#define	CITEDB_STORE		struct citedb_store
-#define	CITEDB_ENT		struct citedb_e
+#define	CITEDB_STORE		struct citedb_storer
+#define	CITEDB_ENT		struct citedb_entry
 #define	CITEDB_OFF		struct citedb_offset
-#define	CITEDB_CUR		struct citedb_cur
-
+#define	CITEDB_CUR		struct citedb_cursor
+#define	CITEDB_MAGIC		0x86544567
 #define	CITEDB_CITESTRLEN	3
 #define	CITEDB_CITEKEYLEN	80
 
 
-struct citedb_cur {
+struct citedb_cursor {
 	int		i ;
-} ;
+} ; /* end struct */
 
 /* user visible structure */
-struct citedb_e {
-	uint		coff ;
+struct citedb_entry {
+	uint		off ;
 	int		fi ;
 	int		ci ;		/* index of this citation */
 	int		n ;		/* total number of this citation */
 	char		citekey[CITEDB_CITEKEYLEN + 1] ;
 	char		citestr[CITEDB_CITESTRLEN + 1] ;
-} ;
+} ; /* end struct (citedb_entry) */
 
 /* store key-offset in list */
 struct citedb_offset {
 	CITEDB_STORE	*sp ;
-	uint		coff ;		/* file offset of citation */
+	uint		off ;		/* file offset of citation */
 	int		fi ;		/* file index */
 	int		ci ;		/* citation index of this citation */
-} ;
+} ; /* end struct */
 
 /* store keyname in hash table */
-struct citedb_store {
-	const char	*citekey ;
+struct citedb_storer {
+	cchar		*citekey ;
 	int		n ;		/* total number of this citation */
 	char		citestr[CITEDB_CITESTRLEN + 1] ;
-} ;
+} ; /* end struct */
 
 struct citedb_head {
-	uint		magic ;
-	HDB		store ;
-	VECOBJ		list ;
+	vecobj		*elp ;		/* entry-list-pointer */
+	hdb		*slp ;		/* store-list-pointer */
+	uint		magval ;
 	int		citestrindex ;	/* incremented as necessary */
-} ;
+} ; /* end struct */
 
+typedef	CITEDB		citedb ;
+typedef	CITEDB_STORE	citedb_store ;
+typedef	CITEDB_ENT	citedb_ent ;
+typedef	CITEDB_OFF	citedb_off ;
+typedef	CITEDB_CUR	citedb_cur ;
 
-#if	(! defined(CITEDB_MASTER)) || (CITEDB_MASTER == 0)
+EXTERNC_begin
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
+extern int citedb_start		(citedb *) noex ;
+extern int citedb_finish	(citedb *) noex ;
+extern int citedb_add		(citedb *op,int fi,uint fo,cc *kp,int kl) noex ;
+extern int citedb_adds		(citedb *,int,uint,cchar *,int) noex ;
+extern int citedb_curbegin	(citedb *,citedb_cur *) noex ;
+extern int citedb_curend	(citedb *,citedb_cur *) noex ;
+extern int citedb_curenum	(citedb *,citedb_cur *,citedb_ent *) noex ;
+extern int citedb_fetch		(citedb *,cchar *,
+			citedb_cur *,citedb_ent *) noex ;
+extern int	citedb_audit(citedb *) noex ;
 
-extern int	citedb_start(CITEDB *) ;
-extern int	citedb_finish(CITEDB *) ;
-extern int	citedb_adds(CITEDB *,int,uint,const char *,int) ;
-extern int	citedb_curbegin(CITEDB *,CITEDB_CUR *) ;
-extern int	citedb_curend(CITEDB *,CITEDB_CUR *) ;
-extern int	citedb_enum(CITEDB *,CITEDB_CUR *,CITEDB_ENT *) ;
-extern int	citedb_fetch(CITEDB *,const char *,
-			CITEDB_CUR *,CITEDB_ENT *) ;
-extern int	citedb_audit(CITEDB *) ;
-
-#ifdef	__cplusplus
-}
-#endif
-
-#endif /* CITEDB_MASTER */
-
+EXTERNC_end
 
 #endif /* CITEDB_INCLUDE */
 
