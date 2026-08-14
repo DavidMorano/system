6677bef43ebef683437d166edda1e69c4d899e19
David A.D. Morano <david.a.morano@gmail.com>
2026-08-14 01:45:39 -0400

    update

---
+++ src/libuc/find/findfilepath.h
@@ -0,0 +1,36 @@
+/* findfilepath HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* find a file path (given some information) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	FINDFILEPATH_INCLUDE
+#define	FINDFILEPATH_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int findfilepath(char *,cchar *,cchar *,int) noex ;
+
+EXTERNC_end
+
+
+#endif /* FINDFILEPATH_INCLUDE */
+
+
