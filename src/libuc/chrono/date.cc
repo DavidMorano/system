7f32e8c3d83daeb5f7ca0a425564ba293d2c2178
David A.D. Morano <david.a.morano@gmail.com>
2026-08-16 00:15:57 -0400

    update

--- src/libuc/chrono/date.cc
+++ src/libuc/chrono/date.cc
@@ -48,7 +48,6 @@
 
 #include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/timeb.h>		/* POSIX® |TIMEB| */
-#include	<tzfile.h>		/* POSIX® |TM_YEAR_BASE| */
 #include	<ctime>			/* CSTD */
 #include	<cstddef>		/* CSTD */
 #include	<cstdlib>		/* CSTD */
