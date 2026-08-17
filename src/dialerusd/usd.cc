4f50f850cd283eb337ef3fb11d4e95096990a758
David A.D. Morano <david.a.morano@gmail.com>
2026-08-17 00:45:23 -0400

    update

--- src/dialerusd/usd.cc
+++ src/dialerusd/usd.cc
@@ -1225,7 +1225,7 @@ keyopt		*kop ;
 
 	if ((rs = keyopt_curbegin(kop,&kcur)) >= 0) {
 
-	while ((kl = keyopt_enumkeys(kop,&kcur,&kp)) >= 0) {
+	while ((kl = keyopt_curenumkeys(kop,&kcur,&kp)) >= 0) {
 
 /* get the first value for this key */
 
@@ -1539,7 +1539,7 @@ int		dlen ;
 
 	if ((rs1 >= 0) && S_ISDIR(sb.st_mode)) {
 
-	    rs1 = permid(&sip->id,&sb,(R_OK | X_OK)) ;
+	    rs1 = permids(&sip->id,&sb,(R_OK | X_OK)) ;
 	    f = (rs1 >= 0) ;
 
 	} /* end if */
