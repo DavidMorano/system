ebdd4a4c68b9835f3f612f1dd907500d9d7b4359
David A.D. Morano <david.a.morano@gmail.com>
2026-08-17 00:51:50 -0400

    update

--- src/execname/progreport.h
+++ src/execname/progreport.h
@@ -25,7 +25,7 @@
 #define	PROGREPORT_INCLUDE	1
 
 
-#include	<envstandards.h>	/* must be before others */
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<sys/types.h>
 #include	<localmisc.h>
 
