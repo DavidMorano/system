3aa5b2a1fcc23096b65d73cdade0cda48a247857
David A.D. Morano <david.a.morano@gmail.com>
2026-08-17 00:45:59 -0400

    update

--- src/dialeruss/tcpnls.h
+++ src/dialeruss/tcpnls.h
@@ -1,52 +1,58 @@
-/* tcpnls */
+/* tcpnls HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
+/* SYSDIALER "tcpnls" dialer */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2003-11-04, David A­D­ Morano
+	This was created as one of the first dialer modules for the
+	SYSDIALER object.
+
+*/
+
+/* Copyright © 2003 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	TCPNLS_INCLUDE
-#define	TCPNLS_INCLUDE	1
+#define	TCPNLS_INCLUDE
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/types.h>
-#include	<localmisc.h>
+#include	<clanguage.h>
+#include	<usysbase.h>
+#include	<sysdialer.h>
 
-#include	"sysdialer.h"
 
-
-#define	TCPNLS_MAGIC	31415926
 #define	TCPNLS		struct tcpnls_head
+#define	TCPNLS_MAGIC	31415926
 
 
 struct tcpnls_head {
-	uint		magic ;
 	time_t		opentime ;
+	uint		magic ;
 	int		fd ;
-} ;
+} ; /* end struct */
 
-
-#if	(! defined(TCPNLS_MASTER)) || (TCPNLS_MASTER == 0)
-
-#ifdef	__cplusplus
-extern "C" {
-#endif
+EXTERNC_begin
 
 extern int tcpnls_open(TCPNLS *,SYSDIALER_ARGS *,
-		const char *,const char *,const char **) ;
-extern int tcpnls_reade(TCPNLS *,char *,int,int,int) ;
-extern int tcpnls_recve(TCPNLS *,char *,int,int,int,int) ;
-extern int tcpnls_recvfrome(TCPNLS *,char *,int,int,void *,int *,int,int) ;
-extern int tcpnls_recvmsge(TCPNLS *,struct msghdr *,int,int,int) ;
-extern int tcpnls_write(TCPNLS *,const char *,int) ;
-extern int tcpnls_send(TCPNLS *,const char *,int,int) ;
-extern int tcpnls_sendto(TCPNLS *,const char *,int,int,void *,int) ;
-extern int tcpnls_sendmsg(TCPNLS *,struct msghdr *,int) ;
-extern int tcpnls_shutdown(TCPNLS *,int) ;
-extern int tcpnls_close(TCPNLS *) ;
+		cchar *,cchar *,cchar **) noex ;
+extern int tcpnls_reade(TCPNLS *,char *,int,int,int) noex ;
+extern int tcpnls_recve(TCPNLS *,char *,int,int,int,int) noex ;
+extern int tcpnls_recvfrome(TCPNLS *,char *,int,int,void *,int *,int,int) noex ;
+extern int tcpnls_recvmsge(TCPNLS *,MSGHDR *,int,int,int) noex ;
+extern int tcpnls_write(TCPNLS *,cchar *,int) noex ;
+extern int tcpnls_send(TCPNLS *,cchar *,int,int) noex ;
+extern int tcpnls_sendto(TCPNLS *,cchar *,int,int,void *,int) noex ;
+extern int tcpnls_sendmsg(TCPNLS *,MSGHDR *,int) noex ;
+extern int tcpnls_shutdown(TCPNLS *,int) noex ;
+extern int tcpnls_close(TCPNLS *) noex ;
 
-#ifdef	__cplusplus
-}
-#endif
+EXTERNC_end
 
-#endif /* TCPNLS_MASTER */
 
 #endif /* TCPNLS_INCLUDE */
 
