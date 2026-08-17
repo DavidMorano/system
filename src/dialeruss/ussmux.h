323818002f9875c7f735803b8335909797d03c64
David A.D. Morano <david.a.morano@gmail.com>
2026-08-17 00:46:23 -0400

    update

--- src/dialeruss/ussmux.h
+++ src/dialeruss/ussmux.h
@@ -1,63 +1,66 @@
-/* ussmux */
+/* ussmux HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 (conformance reviewed) */
 
+/* SYSDIALER "ussmux" dialer */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-04, David A­D­ Morano
+	This was created as one of the first dialer modules for the
+	SYSDIALER object.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #ifndef	USSMUX_INCLUDE
-#define	USSMUX_INCLUDE	1
+#define	USSMUX_INCLUDE
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-
-#include	<sys/types.h>
-
+#include	<clanguage.h>
+#include	<usysbase.h>
 #include	<logfile.h>
-#include	<localmisc.h>
-
-#include	"sysdialer.h"
+#include	<sysdialer.h>
 
 
-#define	USSMUX_MAGIC		31415926
 #define	USSMUX			struct ussmux_head
 #define	USSMUX_FL		struct ussmux_flags
+#define	USSMUX_MAGIC		31415926
 
 
 struct ussmux_flags {
 	uint		log:1 ;
-
-} ;
+} ; /* end struct (ussmux_flags) */
 
 struct ussmux_head {
+	logfile		lh ;
+	USSMUX_FL	fl, open ;
 	uint		magic ;
-	LOGFILE		lh ;
-	USSMUX_FL	f, open ;
 	int		tlen ;
 	int		fd ;
-} ;
+} ; /* end struct (ussmux_head) */
 
-
-#if	(! defined(USSMUX_MASTER)) || (USSMUX_MASTER == 0)
-
-#ifdef	__cplusplus
-extern "C" {
-#endif
+EXTERNC_begin
 
 extern int ussmux_open(USSMUX *,SYSDIALER_ARGS *,
-		const char *,const char *,const char **) ;
-extern int ussmux_reade(USSMUX *,char *,int,int,int) ;
-extern int ussmux_recve(USSMUX *,char *,int,int,int,int) ;
-extern int ussmux_recvfrome(USSMUX *,char *,int,int,void *,int *,int,int) ;
-extern int ussmux_recvmsge(USSMUX *,struct msghdr *,int,int,int) ;
-extern int ussmux_write(USSMUX *,const char *,int) ;
-extern int ussmux_send(USSMUX *,const char *,int,int) ;
-extern int ussmux_sendto(USSMUX *,const char *,int,int,void *,int) ;
-extern int ussmux_sendmsg(USSMUX *,struct msghdr *,int) ;
-extern int ussmux_shutdown(USSMUX *,int) ;
-extern int ussmux_close(USSMUX *) ;
+		cchar *,cchar *,cchar **) noex ;
+extern int ussmux_reade(USSMUX *,char *,int,int,int) noex ;
+extern int ussmux_recve(USSMUX *,char *,int,int,int,int) noex ;
+extern int ussmux_recvfrome(USSMUX *,char *,int,int,void *,int *,int,int) noex ;
+extern int ussmux_recvmsge(USSMUX *,MSGHDR *,int,int,int) noex ;
+extern int ussmux_write(USSMUX *,cchar *,int) noex ;
+extern int ussmux_send(USSMUX *,cchar *,int,int) noex ;
+extern int ussmux_sendto(USSMUX *,cchar *,int,int,void *,int) noex ;
+extern int ussmux_sendmsg(USSMUX *,MSGHDR *,int) noex ;
+extern int ussmux_shutdown(USSMUX *,int) noex ;
+extern int ussmux_close(USSMUX *) noex ;
 
-#ifdef	__cplusplus
-}
-#endif
+EXTERNC_end
 
-#endif /* USSMUX_MASTER */
 
 #endif /* USSMUX_INCLUDE */
 
