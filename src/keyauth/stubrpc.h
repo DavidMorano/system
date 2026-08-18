bb5ed1b5174cfb024379648089e2f62fc57ceeac
David A.D. Morano <david.a.morano@gmail.com>
2026-08-18 02:05:01 -0400

    update

--- src/keyauth/stubrpc.h
+++ src/keyauth/stubrpc.h
@@ -1,7 +1,8 @@
-/* onc SUPPORT */
+/* stubrpc SUPPORT */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
-/* ONC operations */
+/* stub RPC functions */
 /* version %I% last-modified %G% */
 
 
@@ -14,19 +15,39 @@
 
 /* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
 
-#ifndef	ONC_INCLUDE
-#define	ONC_INCLUDE
+#ifndef	STUBRPC_INCLUDE
+#define	STUBRPC_INCLUDE
 
+
+#define	STUBRPC_KEYNETSTARG	struct key_netstarg
+
+#ifndef	MAXNETNAMELEN
+#define	MAXNETNAMELEN		255	/* maximum length network user name */
+#endif
+
+#ifndef	STUBRPC_HEXKEYBYTES
+#define	STUBRPC_HEXKEYBYTES	48
+#endif
+
+
+struct key_netstarg {
+	char		st_priv_key	[STUBRPC_HEXKEYBYTES+1] ;
+	char		st_pub_key	[STUBRPC_HEXKEYBYTES+1] ;
+	char		*st_netname ;
+} ; /* end struct (key_netstarg) */
+
+typedef STUBRPC_KEYNETSTARG	key_netstarg ;
 
 EXTERNC_begin
 
-extern int onckeyalready(cchar *) noex ;
-extern int onckeygetset(cchar *,cchar *) noex ;
-extern int onckeylogin(cchar *) noex ;
+extern int	key_secretkey_is_set(void) noex ;
+extern int	key_setnet(key_netstarg *) noex ;
+extern int	getnetname(char *) noex ;
+extern int	getsecretkey(cchar *,char *,cchar *) noex ;
 
 EXTERNC_end
 
 
-#endif /* ONC_INCLUDE */
+#endif /* STUBRPC_INCLUDE */
 
 
