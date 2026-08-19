3884fcda54b248661a47790b0cdd8abbceee3451
David A.D. Morano <david.a.morano@gmail.com>
2026-08-19 00:17:39 -0400

    update

--- src/liblkcmd/b_mailalias.cc
+++ src/liblkcmd/b_mailalias.cc
@@ -110,7 +110,6 @@ extern int	cfdeci(const char *,int,int *) ;
 extern int	cfdecui(const char *,int,uint *) ;
 extern int	optbool(const char *,int) ;
 extern int	optvalue(const char *,int) ;
-extern int	permid(IDS *,ustat *,int) ;
 extern int	haswhite(const char *,int) ;
 extern int	isdigitlatin(int) ;
 extern int	isalphalatin(int) ;
