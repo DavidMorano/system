0f27ca975e5b6f07cd01e97c09be6e9a0b252f47
David A.D. Morano <david.a.morano@gmail.com>
2026-08-19 00:19:06 -0400

    update

--- src/libu/ptx/ptx.h
+++ src/libu/ptx/ptx.h
@@ -23,14 +23,9 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<pthread.h>
-#include	<errno.h>
-
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
+#include	<pthread.h>		/* POSIX® */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 #include	<pta.h>
 #include	<ptc.h>
