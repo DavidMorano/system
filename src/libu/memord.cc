2d494242fa3674eb8faf1849c82a028b8e7410ff
David A.D. Morano <david.a.morano@gmail.com>
2026-08-20 00:36:09 -0400

    restore

---
+++ src/libu/memord.cc
@@ -0,0 +1,70 @@
+/* memord SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* memory order definitions */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2001-04-11, David A-D- Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2001 David A-D- Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	memorg
+
+	Description:
+	This file defines some preprocessor variables that
+	specify some meory orderings.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"memord.hh"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
