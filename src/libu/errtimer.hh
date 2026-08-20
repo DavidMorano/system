f1bdc94c14df10c6ba6d0ff5776733e05a5177b5
David A.D. Morano <david.a.morano@gmail.com>
2026-08-20 00:36:32 -0400

    update

--- src/libu/errtimer.hh
+++ src/libu/errtimer.hh
@@ -32,10 +32,10 @@
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>		
-#include	<usysbase.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 struct errtimer ;
