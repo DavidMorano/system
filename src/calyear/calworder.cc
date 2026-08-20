35c3c8ef8993221c4a7949be0d01a443721ba746
David A.D. Morano <david.a.morano@gmail.com>
2026-08-20 00:15:32 -0400

    update

--- src/calyear/calworder.cc
+++ src/calyear/calworder.cc
@@ -1,68 +1,69 @@
-/* calworder */
+/* calworder SUPPORT */
 /* charset=ISO8859-1 */
 /* lang=C++20 (conformance reviewed) */
 
-/* word management */
+/* word management - CALWORDER */
 /* version %I% last-modified %G% */
 
-#define	CF_DEBUGS	0		/* non-switchable debug print-outs */
+#define	CF_DEBUG	0		/* non-switchable debug print-outs */
 
 /* revision history:
 
-	- 2008-10-01, David A­D­ Morano
+	= 1999-05-19, David A-D- Morano
 	This object module was originally written.
 
 */
 
-/* Copyright © 2008 David A­D­ Morano.  All rights reserved. */
+/* Copyright © 1999 David A­D­ Morano.  All rights reserved. */
 
 /*******************************************************************************
 
+  	Object:
+	calworder
+
+	Description:
 	We manage words, for calendars.
 
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<sys/param.h>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<cstring>
-#include	<usystem.h>
-#include	<estrings.h>
-#include	<sbuf.h>
-#include	<sfx.h>
-#include	<localmisc.h>
+#include	<sys/param.h>		/* POSIX® */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<ucmem.h>		/* LIBUC */
+#include	<estrings.h>		/* LIBUC */
+#include	<sbuf.h>		/* LIBUC */
+#include	<sfx.h>			/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
+#include	<libdebug.h>		/* LIBDEBUG |DEBUGPRINTF(3debug)| */
+#include	<calent.h>		/* LIBDAM */
 
 #include	"calworder.h"
 
 
 /* local defines */
 
+#define	CW	calworder
+
+#define	CE	calent
+#define	CE_LN	calent_ln
+
+
+/* imported namespaces */
+
+using libuc::mem ;			/* variable */
+
+
+/* local typedefs */
+
 
 /* external subroutines */
 
-extern int	snsds(char *,int,cchar *,cchar *) ;
-extern int	snwcpy(char *,int,cchar *,int) ;
-extern int	sncpy1(char *,int,cchar *) ;
-extern int	mkpath2(char *,cchar *,cchar *) ;
-extern int	mkpath3(char *,cchar *,cchar *,cchar *) ;
-extern int	mkfnamesuf1(char *,cchar *,cchar *) ;
-extern int	mkfnamesuf2(char *,cchar *,cchar *,cchar *) ;
-extern int	sibrk(cchar *,int,cchar *) ;
-extern int	siskipwhite(cchar *,int) ;
-extern int	matstr(cchar **,cchar *,int) ;
-extern int	matcasestr(cchar **,cchar *,int) ;
-extern int	matocasestr(cchar **,int,cchar *,int) ;
-extern int	matpcasestr(cchar **,int,cchar *,int) ;
-extern int	nleadstr(cchar *,cchar *,int) ;
-extern int	cfdeci(cchar *,int,int *) ;
-extern int	cfdecui(cchar *,int,uint *) ;
-extern int	isdigitlatin(int) ;
 
-#if	CF_DEBUGS
-extern int	debugprintf(cchar *,...) ;
-extern int	strlinelen(cchar *,int,int) ;
-#endif
+/* external variables */
 
 
 /* local structures */
@@ -77,59 +78,60 @@ extern int	strlinelen(cchar *,int,int) ;
 /* local variables */
 
 
+/* exported variables */
+
+
 /* exported subroutines */
 
+int calworder_start(CW *wp,cchar *md,calent *ep) noex {
+    	int		rs = SR_FAULT ;
+	if (wp && md && ep) ylikely {
+	    calent_ln	*lines = ep->lines ;
+	    rs = SR_OK ;
+	    wp->i = 0 ;
+	    wp->nlines = ep->i ;
+	    wp->lines = ep->lines ;
+	    wp->md = md ;
+	    if (lines) {
+	        wp->sp = (md + lines[0].loff) ;
+	        wp->sl = (lines[0].llen) ;
+	    } /* end if (lines) */
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (calworder_start) */
 
-int calworder_start(CALWORDER *wp,cchar *md,CALENT *ep)
-{
-	CALENT_LINE	*lines = ep->lines ;
-	wp->i = 0 ;
-	wp->nlines = ep->i ;
-	wp->lines = ep->lines ;
-	wp->md = md ;
-	if (lines != NULL) {
-	    wp->sp = (md + lines[0].loff) ;
-	    wp->sl = (lines[0].llen) ;
-	}
+int calworder_finish(CW *wp) noex {
+    	int		rs = SR_FAULT ;
+	if (wp) {
+	    rs = SR_OK ;
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (calworder_finish) */
 
-	return SR_OK ;
-}
-/* end subroutine (calworder_start) */
-
-
-/* ARGSUSED */
-int calworder_finish(CALWORDER *wp)
-{
-	if (wp == NULL) return SR_FAULT ;
-	return SR_OK ;
-}
-/* end subroutine (calworder_finish) */
-
-
-int calworder_get(CALWORDER *wp,cchar **rpp)
-{
+int calworder_get(CW *wp,cchar **rpp) noex {
+    	int		rs = SR_FAULT ;
 	int		cl = 0 ;
-	cchar	*cp = NULL ; /* ¥ GCC is stupid! */
-
-	while (wp->i < wp->nlines) {
-	    if ((cl = nextfield(wp->sp,wp->sl,&cp)) > 0) {
-	        wp->sl -= ((cp + cl) - wp->sp) ;
-	        wp->sp = (cp + cl) ;
-	    } else {
-	        wp->i += 1 ;
-	        if (wp->i < wp->nlines) {
-	            wp->sp = (wp->md + wp->lines[wp->i].loff) ;
-	            wp->sl = (wp->lines[wp->i].llen) ;
+	if (wp) {
+	    cchar	*cp = nullptr ; /* ¥ GCC is stupid! */
+	    rs = SR_OK ;
+	    while (wp->i < wp->nlines) {
+	        if ((cl = sfnext(wp->sp,wp->sl,&cp)) > 0) {
+	            wp->sl -= conv<int>((cp + cl) - wp->sp) ;
+	            wp->sp = (cp + cl) ;
+	        } else {
+	            wp->i += 1 ;
+	            if (wp->i < wp->nlines) {
+	                wp->sp = (wp->md + wp->lines[wp->i].loff) ;
+	                wp->sl = (wp->lines[wp->i].llen) ;
+	            }
 	        }
+	        if (cl > 0) break ;
+	    } /* end while */
+	    if (rpp) {
+	        *rpp = cp ;
 	    }
-	    if (cl > 0) break ;
-	} /* end while */
-
-	if (rpp != NULL)
-	    *rpp = cp ;
-
-	return cl ;
-}
-/* end subroutine (calworder_get) */
+	} /* end if (non-null) */
+	return (rs >= 0) ? cl : rs ;
+} /* end subroutine (calworder_get) */
 
 
