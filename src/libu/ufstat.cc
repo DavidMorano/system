9379bcf0e25b2af193f872e63dcce4b009f6e89f
David A.D. Morano <david.a.morano@gmail.com>
2026-08-21 00:11:46 -0400

    update

--- src/libu/ufstat.cc
+++ src/libu/ufstat.cc
@@ -17,14 +17,17 @@
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<sys/types.h>
-#include	<sys/stat.h>
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<cerrno>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<usyscalls.h>
-#include	<localmisc.h>
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<cerrno>		/* CSTD */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 
 /* local defines */
@@ -38,10 +41,11 @@
 int u_fstat(int fd,USTAT *sbp) noex {
 	int		rs ;
 	repeat {
-	    if ((rs = fstat(fd,sbp)) < 0) rs = (- errno) ;
+	    if ((rs = fstat(fd,sbp)) < 0) {
+		rs = (neg errno) ;
+	    }
 	} until (rs != SR_INTR) ;
 	return rs ;
-}
-/* end subroutine (u_fstat) */
+} /* end subroutine (u_fstat) */
 
 
