315a28f694b211e68c2bd1a8dbfca73f06e12d04
David A.D. Morano <david.a.morano@gmail.com>
2026-08-22 03:03:14 -0400

    restore

---
+++ src/libuc/ema/ema_entry.hh
@@ -0,0 +1,31 @@
+/* ema_entry HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* E-Mail Address Entry (entry) -- used within EMA */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-01, David A­D­ Morano
+	This object module was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	EMAENTRY_INCLUDE
+#define	EMAENTRY_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* EMAENTRY_INCLUDE */
+
+
