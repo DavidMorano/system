61337db48eac051ac93edca34dad1d856de7b169
David A.D. Morano <david.a.morano@gmail.com>
2026-08-23 00:29:20 -0400

    restore

---
+++ src/libu/intx/intminmax.hh
@@ -0,0 +1,31 @@
+/* intminmax HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* define some integer minimum and maximum values */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2000 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	INTMINMAX_INCLUDE
+#define	INTMINMAX_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* INTMINMAX_INCLUDE */
+
+
