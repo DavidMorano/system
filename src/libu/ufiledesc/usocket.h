096b50dfadda4a4f107cc2cbb364998a3f3585cf
David A.D. Morano <david.a.morano@gmail.com>
2026-08-23 00:30:06 -0400

    update

--- src/libu/ufiledesc/usocket.h
+++ src/libu/ufiledesc/usocket.h
@@ -30,9 +30,9 @@
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<sys/types.h>		/* system types */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<sys/types.h>		/* POSIX® system types */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 EXTERNC_begin
 
@@ -41,7 +41,6 @@ extern int u_connect		(int,cvoid *,int) noex ;
 extern int u_connecte		(int,cvoid *,int,int) noex ;
 extern int u_getpeername	(int,void *,int *) noex ;
 extern int u_getsockname	(int,void *,int *) noex ;
-extern int u_getsockopt		(int,int,int,void *,int *) noex ;
 extern int u_getsocktype	(int) noex ;
 extern int u_linger		(int,int) noex ;
 extern int u_listen		(int,int) noex ;
@@ -54,9 +53,17 @@ extern int u_send		(int,cvoid *,int,int) noex ;
 extern int u_sendfile		(int,int,off_t,size_t) noex ;
 extern int u_sendmsg		(int,CMSGHDR *,int) noex ;
 extern int u_sendto		(int,cvoid *,int,int,cvoid *,int) noex ;
-extern int u_setsockopt		(int,int,int,cvoid *,int) noex ;
 extern int u_shutdown		(int,int) noex ;
 extern int u_sockatmark		(int) noex ;
+extern int u_sockoptget		(int,int,int,void *,int *) noex ;
+extern int u_sockoptset		(int,int,int,cvoid *,int) noex ;
+
+local inline int u_getsockopt	(int fd,int lv,int on,void *vap,int *lp) noex {
+	return u_sockoptget(fd,lv,on,vap,lp) ;
+}
+local inline int u_setsockopt	(int fd,int lv,int on,cvoid *vap,int l) noex {
+	return u_sockoptset(fd,lv,on,vap,l) ;
+}
 
 EXTERNC_end
 
