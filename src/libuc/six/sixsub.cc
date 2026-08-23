3810d669e2cd72ceea6dc07d67ee5c51f4b8b31a
David A.D. Morano <david.a.morano@gmail.com>
2026-08-23 00:35:09 -0400

    update

--- src/libuc/six/sixsub.cc
+++ src/libuc/six/sixsub.cc
@@ -82,7 +82,7 @@ import libutil ;			/* |lenstr(3u)| */
 extern "C" {
     typedef int (*toxc_f)(int) noex ;
     typedef int (*nleadxstr_f)(cchar *,cchar *,int) noex ;
-}
+} /* end extern (C) */
 
 
 /* external subroutines */
