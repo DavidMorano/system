1ac40e5c222ec0cfece11243d390c65409af2088
David A.D. Morano <david.a.morano@gmail.com>
2026-08-23 00:38:22 -0400

    update

--- src/libuc/uccfsetospeed.cc
+++ src/libuc/uccfsetospeed.cc
@@ -5,7 +5,7 @@
 /* interface component for UNIX® library-3c */
 /* version %I% last-modified %G% */
 
-#define	CF_DEBUGS	0		/* compile-time debugging */
+#define	CF_DEBUG	0		/* compile-time debugging */
 
 /* revision history:
 
@@ -16,21 +16,20 @@
 
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
-
 #include	<envstandards.h>	/* MUST be first to configure */
-
-#include	<sys/types.h>
-#include	<termios.h>
-#include	<unistd.h>
-#include	<fcntl.h>
-#include	<poll.h>
-#include	<cerrno>
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>		/* |getenv(3c)| */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
-#include	<localmisc.h>
+#include	<sys/types.h>		/* POSIX® */
+#include	<termios.h>		/* POSIX® */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<poll.h>		/* POSIX® */
+#include	<cerrno>		/* CSYD */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+#include	<libdebug.h>		/* LIBDEBUG |DEBUGPRINTF(3debug)| */
 
 
 /* exported variables */
@@ -40,13 +39,12 @@
 
 int uc_cfsetospeed(TERMIOS *tp,speed_t speed) noex {
 	int		rs = SR_FAULT ;
-	if (tp) {
+	if (tp) ylikely {
 	    if ((rs = cfsetospeed(tp,speed)) < 0) {
-	        rs = (- errno) ;
+	        rs = (neg errno) ;
 	    }
 	} /* end if (non-null) */
 	return rs ;
-}
-/* end subroutine (uc_cfsetospeed) */
+} /* end subroutine (uc_cfsetospeed) */
 
 
