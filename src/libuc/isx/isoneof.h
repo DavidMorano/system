797c319245d9ba879a188846282f378c571b8727
David A.D. Morano <david.a.morano@gmail.com>
2026-08-24 01:28:15 -0400

    update

--- src/libuc/isx/isoneof.h
+++ src/libuc/isx/isoneof.h
@@ -34,6 +34,9 @@ extern bool isonebad	(cint *,int) noex ;
 local inline bool isOneOf(cint *rsa,int rs) noex {
 	return isoneof(rsa,rs) ;
 }
+local inline bool isOneBad(cint *rsa,int rs) noex {
+	return isonebad(rsa,rs) ;
+}
 
 EXTERNC_end
 
