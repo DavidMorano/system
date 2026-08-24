11e8a8f659bb3802ddefbf3c21429c44169a89f5
David A.D. Morano <david.a.morano@gmail.com>
2026-08-24 01:31:30 -0400

    update

--- src/libuc/ucfileop/ucstatvfs.cc
+++ src/libuc/ucfileop/ucstatvfs.cc
@@ -17,13 +17,13 @@
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
-#include	<prefixfn.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<prefixfn.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"ucfileop.h"
 
@@ -67,8 +67,7 @@ int uc_statfs(cchar *fname,ustatfs *sbp) noex {
 	    } /* end if (prefixfn) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? rv : rs ;
-}
-/* end subroutine (uc_statfs) */
+} /* end subroutine (uc_statfs) */
 
 int uc_statvfs(cchar *fname,ustatvfs *sbp) noex {
 	int		rs = SR_FAULT ;
@@ -86,6 +85,6 @@ int uc_statvfs(cchar *fname,ustatvfs *sbp) noex {
 	    } /* end if (prefixfn) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? rv : rs ;
-}
-/* end subroutine (uc_statvfs) */
+} /* end subroutine (uc_statvfs) */
+
 
