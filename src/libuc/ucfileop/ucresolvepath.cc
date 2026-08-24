ccd8a7a4cd69bae7c069ffd2cb933f873eb42c77
David A.D. Morano <david.a.morano@gmail.com>
2026-08-24 01:31:20 -0400

    update

--- src/libuc/ucfileop/ucresolvepath.cc
+++ src/libuc/ucfileop/ucresolvepath.cc
@@ -28,13 +28,13 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
-#include	<prefixfn.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<prefixfn.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"ucfileop.h"
 
@@ -88,7 +88,5 @@ int uc_resolvepath(cchar *fname,char *rbuf,int rlen) noex {
 	    } /* end if (valid) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? rv : rs ;
-}
-/* end subroutine (uc_resolvepath) */
-
+} /* end subroutine (uc_resolvepath) */
 
