1c40993dfc626797327a69752d6575313b20678e
David A.D. Morano <david.a.morano@gmail.com>
2026-08-24 01:30:50 -0400

    update

--- src/libuc/ucfileop/uclink.cc
+++ src/libuc/ucfileop/uclink.cc
@@ -17,13 +17,13 @@
 /* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
-#include	<prefixfn.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<prefixfn.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"ucfileop.h"
 
@@ -76,5 +76,6 @@ int uc_link(cchar *fname,cchar *nfname) noex {
 	    } /* end if (valid) */
 	} /* end if (non-null) */
 	return (rs >= 0) ? rv : rs ;
-}
-/* end subroutine (uc_link) */
+} /* end subroutine (uc_link) */
+
+
