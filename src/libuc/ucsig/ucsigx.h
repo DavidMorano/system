b19b4914be0150f8b1686b2c7f3d3afb92b8de70
David A.D. Morano <david.a.morano@gmail.com>
2026-08-24 01:39:10 -0400

    update

--- src/libuc/ucsig/ucsigx.h
+++ src/libuc/ucsig/ucsigx.h
@@ -20,9 +20,9 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<usyscalls.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
 
 
 EXTERNC_begin
