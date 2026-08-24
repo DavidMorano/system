4993b61c935497afd713c00af985dfbf07145e1b
David A.D. Morano <david.a.morano@gmail.com>
2026-08-24 01:33:06 -0400

    update

--- src/libuc/ucget/ucgetsv.h
+++ src/libuc/ucget/ucgetsv.h
@@ -20,9 +20,9 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<ucentsv.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<ucentsv.h>		/* LIBUC */
 
 
 EXTERNC_begin
