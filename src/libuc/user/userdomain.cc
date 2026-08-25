ac82bd0b9b02ee5eb05ad9512c8ae3c9e12746ec
David A.D. Morano <david.a.morano@gmail.com>
2026-08-25 00:13:00 -0400

    restore

---
+++ src/libuc/user/userdomain.cc
@@ -0,0 +1,270 @@
+/* userdomain SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* get user domain */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	I made up this idea for supporting multiple domains on the
+	same machine so that each user could have a different domain
+	name. This idea of multiplexing a single machine to appear
+	to be multiple different machines is becoming very important
+	in the Internet age!
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	userdomain
+
+	Description:
+	This subroutine looks up the given username and returns the
+	domainname for that user.  Per-user domain names are optional
+	and are administered through the file 'etc/userdomain' located
+	relative to the programroot directory that is optionally
+	supplied.  If no programroot is supplied, then '/' is used.
+	This subroutine uses two different ways to read the UDOMAIN
+	DB based on the file it is in.  This is so that in addition
+	to the file being "regular" it can also be (secretly) a
+	network file or portal of some kind.
+
+	Synopsis:
+	int userdomain(cchar *pr,char *dbuf,int dlen,cchar *username) noex
+
+	Arguments:
+	pr		program-root
+	dbuf		buffer to receive results (must be MAXPATHLEN in size)
+	dlen		length of supplied buffer
+	username	specified username to lookup
+
+	Returns:
+	>=0		length of returned domain-name
+	<0		error (system-return)
+
+	Notes:
+	Format of file entries:
+		username	domainname
+	Note that this strategy of reading the "userdomain" file is obsoleted by
+	the use of the 'id' (Internet Domain) key-value pair in the 'user_attr'
+	DB of the system!
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD |strcmp(3c)| */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<uclibmem.h>		/* LIBUC */
+#include	<ucopen.h>		/* LIBUC */
+#include	<ucdesc.h>		/* LIBUC */
+#include	<ucfileop.h>		/* LIBUC */
+#include	<estrings.h>		/* LIBUC */
+#include	<snwcpy.h>		/* LIBUC */
+#include	<mkpathx.h>		/* LIBUC */
+#include	<filemap.h>		/* LIBUC */
+#include	<filer.h>		/* LIBUC */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"userdomain.h"
+
+#pragma		GCC dependency		"mod/libutil.ccm"
+
+import libutil ;			/* |lenstr(3u)| */
+
+/* local defines */
+
+#ifndef	UDOMASTDFNIN
+#define	UDOMASTDFNIN	"/etc/userdomain"
+#endif
+
+#define	MAXFILESIZE	(2 * 1024 * 1024)
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+extern "C" {
+    int userdomain(cchar *,char *,int,cchar *) noex ;
+} /* end extern (C) */
+
+
+/* external variables */
+
+
+/* local structures */
+
+namespace {
+    struct uargs {
+	cchar		*un = nullptr ;
+	char		*dbuf ;
+	int		dlen ;
+	int		ul = 0 ;
+	int		maxfilesz ;
+	uargs(char *dp,int dl,cchar *u,int mf) noex : dbuf(dp) , dlen(dl) {
+	    un = u ;
+	    if (un) {
+	        ul = lenstr(un) ;
+ 	    }
+	    maxfilesz = mf ;
+	} ; /* end ctor */
+	int userdomainer(cchar *) noex ;
+	int userdomainerm(cchar *) noex ;
+	int userdomainerf(cchar *) noex ;
+	int parseline(cchar *,int) noex ;
+    } ; /* end struct (uargs) */
+} /* end namespace */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int userdomain(cchar *pr,char *dbuf,int dlen,cchar *username) noex {
+	int		rs = SR_FAULT ;
+	int		rs1 ;
+	int		len = 0 ; /* return-value */
+	if (dbuf && username) ylikely {
+	    rs = SR_INVALID ;
+	    dbuf[0] = '\0' ;
+	    if (username[0]) ylikely {
+	        uargs	a(dbuf,dlen,username,MAXFILESIZE) ;
+	        cchar	*fname = UDOMASTDFNIN ;
+	        if (pr && (pr[0] != '\0') && (strcmp(pr,"/") != 0)) {
+		     if (char *fbuf ; (rs = lm_mp(&fbuf)) >= 0) {
+	    	         if ((rs = mkpath(fbuf,pr,fname)) >= 0) {
+	             	    rs = a.userdomainer(fbuf) ;
+		     	    len = rs ;
+		         }
+		         rs1 = lm_free(fbuf) ;
+		         if (rs >= 0) rs = rs1 ;
+		     } /* end if (m-a-f) */
+	         } else {
+	             rs = a.userdomainer(fname) ;
+		     len = rs ;
+	         }
+	    } /* end if (valid) */
+	} /* end if (non-null) */
+	return (rs >= 0) ? len : rs ;
+} /* end subroutine (userdomain) */
+
+
+/* local subroutines */
+
+int uargs::userdomainer(cchar *fname) noex {
+	int		rs ;
+	int		len = 0 ;
+	if (ustat sb ; (rs = u_stat(fname,&sb)) >= 0) {
+	    if (S_ISREG(sb.st_mode) && (sb.st_size < maxfilesz)) {
+		rs = userdomainerm(fname) ;
+		len = rs ;
+	    } else {
+		rs = userdomainerf(fname) ;
+		len = rs ;
+	    } /* end if (type of file) */
+	} /* end if (stat) */
+	return (rs >= 0) ? len : rs ;
+} /* end subroutine (uargs::userdomainer) */
+
+int uargs::userdomainerm(cchar *fname) noex {
+	csize		mfsize = size_t(maxfilesz) ;
+	int		rs ;
+	int		rs1 ;
+	int		len = 0 ;
+	if (filemap udf ; (rs = udf.open(fname,mfsize)) >= 0) {
+	    cchar	*lp ;
+            while ((rs = udf.getln(&lp)) > 0) {
+                cint	ll = rs ;
+                {
+                    rs = parseline(lp,ll) ;
+                    len = rs ;
+                }
+                if (len > 0) break ;
+                if (rs < 0) break ;
+            } /* end while (reading lines) */
+            rs1 = udf.close ;
+            if (rs >= 0) rs = rs1 ;
+        } /* end if (opened file) */
+	return (rs >= 0) ? len : rs ;
+} /* end method (uargs::userdomainerm) */
+
+int uargs::userdomainerf(cchar *fname) noex {
+	int		rs ;
+	int		rs1 ;
+	int		len = 0 ;
+	if (char *lbuf ; (rs = lm_ml(&lbuf)) >= 0) ylikely {
+	    cint	llen = rs ;
+	    if ((rs = uc_open(fname,O_RDONLY,0666)) >= 0) ylikely {
+	        cint	fd = rs ;
+	        if (filer b ; (rs = b.start(fd,0L,0,0)) >= 0) ylikely {
+	            while ((rs = b.readln(lbuf,llen,-1)) > 0) {
+			{
+	                    rs = parseline(lbuf,rs) ;
+	                    len = rs ;
+			}
+	                if (len > 0) break ;
+	                if (rs < 0) break ;
+	            } /* end while (reading lines) */
+	            rs1 = b.finish ;
+		    if (rs >= 0) rs = rs1 ;
+	        } /* end if (filer) */
+	        rs1 = uc_close(fd) ;
+		if (rs >= 0) rs = rs1 ;
+	    } /* end if (opened file) */
+	    rs1 = lm_free(lbuf) ;
+	    if (rs >= 0) rs = rs1 ;
+	} /* end if (m-a-f) */
+	return (rs >= 0) ? len : rs ;
+} /* end method (uargs::userdomainerf) */
+
+int uargs::parseline(cchar *lbuf,int llen) noex {
+	int		rs = SR_OK ;
+	int		sl = llen ;
+	int		cl ;
+	int		len = 0 ;
+	cchar		*tp ;
+	cchar		*sp = lbuf ;
+	cchar		*cp ;
+	if ((sl > 0) && (sp[sl-1] == '\n')) {
+	    sl -= 1 ;
+	}
+	if ((tp = strnchr(sp,sl,'#')) != nullptr) {
+	    sl = intconv(tp - sp) ;
+	}
+	if ((cl = sfnext(sp,sl,&cp)) > 0) {
+	    if ((cl == ul) && (strncmp(un,cp,cl) == 0)) {
+	        sl -= intconv((cp + cl) - sp) ;
+	        sp = (cp + cl) ;
+	        if ((cl = sfnext(sp,sl,&cp)) > 0) {
+	            rs = snwcpy(dbuf,dlen,cp,cl) ;
+		    len = rs ;
+	        }
+	    } /* end if (username match) */
+	} /* end if (sfnext) */
+	return (rs >= 0) ? len : rs ;
+} /* end subroutine (uargs::parseline) */
+
+
