9e2a4e42b1b57d2039ce6121710a607a80a7c871
David A.D. Morano <david.a.morano@gmail.com>
2026-08-25 00:12:46 -0400

    restore

---
+++ src/libuc/user/user.h
@@ -0,0 +1,33 @@
+/* user HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* user specific subroutines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USER_INCLUDE
+#define	USER_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<userattrdb.h>
+#include	<userdomain.h>
+#include	<userports.h>
+
+
+#endif /* USER_INCLUDE */
+
+
