d6fb1e0983eb72d2ce6be9840bbbb7b4f169e553
David A.D. Morano <david.a.morano@gmail.com>
2026-08-25 00:05:05 -0400

    restore

---
+++ src/libu/usyscalls.cc
@@ -0,0 +1,70 @@
+/* usyscalls SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* interface component for UNIX® library-3c */
+/* UNIX® system-calls */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-21, David A­D­ Morano
+	This module was originally written.
+
+*/
+
+/* Copyright © 1998 David A-D- Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	usyscalls
+
+	Description:
+	This is support for the UNIX®-POSIX® system calls.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"usyscalls.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
