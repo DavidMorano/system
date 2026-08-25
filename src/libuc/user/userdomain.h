c0175aca4dc793b82a53065df0260845a810cf76
David A.D. Morano <david.a.morano@gmail.com>
2026-08-25 00:13:04 -0400

    restore

---
+++ src/libuc/user/userdomain.h
@@ -0,0 +1,36 @@
+/* userdomain HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* retrieve a domain-name for a given username */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	USERDOMAIN_INCLUDE
+#define	USERDOMAIN_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int userdomain(cchar *,char *,int,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* USERDOMAIN_INCLUDE */
+
+
