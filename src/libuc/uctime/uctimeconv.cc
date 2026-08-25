d6bdc873edf73b8937e4a2cfc7e5ead80c4268da
David A.D. Morano <david.a.morano@gmail.com>
2026-08-25 00:11:17 -0400

    update

--- src/libuc/uctime/uctimeconv.cc
+++ src/libuc/uctime/uctimeconv.cc
@@ -46,6 +46,21 @@
 	>=0	OK
 	<0	error (system-return)
 
+	Notes:
+	1. POSIX® (or somebody) says that calls to |localtime_r(3c)|
+	or |gmtime_r(3c)| (like what I am doing below) are not
+	required to call the subroutine |tzset(3c)| before executing
+	their own particular function.  So I call |tzset(3c)| muself
+	before calling those other subroutines.  But this only has to be
+	done once in the lifetime of a program.
+	2. Of course, the |tzset(3c)| subroutine has its own mutual
+	exclusion lock to prevent it repeaing its own extensive
+	initialization (reading the zone-info file, et cetera) on
+	repeated calls.  But I restrict multiple calls to that
+	subroutine myself for ... what ... fun.
+	3. Blah, blah, blah, blah, blag.  Yes, I am not using a real
+	memory-barrier atomic lock below.  It is not a real problem.
+
 *******************************************************************************/
 
 #include	<envstandards.h>	/* MUST be first to configure */
@@ -57,7 +72,6 @@
 #include	<clanguage.h>		/* LIBU */
 #include	<usysbase.h>		/* LIBU */
 #include	<usyscalls.h>		/* LIBU */
-#include	<aflag.hh>		/* LIBU */
 #include	<localmisc.h>		/* LIBU */
 
 #include	"uctimeconv.h"
@@ -80,11 +94,20 @@
 
 /* local structures */
 
+namespace {
+    struct initer {
+	bool		finit ;
+	operator int () noex ;
+    } ; /* end struct (initer) */
+} /* end namespace */
+
 
 /* forward references */
 
 
 /* local variables */
+
+static initer		init ;
 
 
 /* exported variables */
@@ -93,19 +116,19 @@
 /* exported subroutines */
 
 int uc_timelocal(custime *tp,TM *tsp) noex {
-    	cnullptr	np{} ;
 	int		rs = SR_FAULT ;
 	assert(tp && tsp) ;
 	if (tp && tsp) ylikely {
-	    rs = SR_OK ;
-	    errno = 0 ;
-	    if (syshas.localtimer) ylikely {
-	        if (TM *rp ; (rp = localtime_r(tp,tsp)) == np) {
-	            rs = (neg errno) ;
-		}
-	    } else {
-		rs = SR_NOSYS ;
-	    } /* end if_constexpr (f_reentrant) */
+	    if ((rs = init) >= 0) ylikely {
+	        errno = 0 ;
+	        if (syshas.localtimer) ylikely {
+	            if (TM *rp = localtime_r(tp,tsp) ; (!rp)) {
+	                rs = (neg errno) ;
+		    }
+	        } else {
+		    rs = SR_NOSYS ;
+	        } /* end if_constexpr (f_reentrant) */
+	    } /* end if (init) */
 	} /* end if (non-null) */
 	return rs ;
 } /* end subroutine (uc_timelocal) */
@@ -114,15 +137,16 @@ int uc_timegm(custime *tp,TM *tsp) noex {
 	int		rs = SR_FAULT ;
 	assert(tp && tsp) ;
 	if (tp && tsp) ylikely {
-	    rs = SR_OK ;
-	    errno = 0 ;
-	    if (syshas.gmtimer) ylikely {
-	        if (TM *rp = gmtime_r(tp,tsp) ; rp) {
-	            rs = (neg errno) ;
-		}
-	    } else {
-		rs = SR_NOSYS ;
-	    } /* end if_constexpr (f_reentrant) */
+	    if ((rs = init) >= 0) ylikely {
+	        errno = 0 ;
+	        if (syshas.gmtimer) ylikely {
+	            if (TM *rp = gmtime_r(tp,tsp) ; (!rp)) {
+	                rs = (neg errno) ;
+		    }
+	        } else {
+		    rs = SR_NOSYS ;
+	        } /* end if_constexpr (f_reentrant) */
+	    } /* end if (init) */
 	} /* end if (non-null) */
 	return rs ;
 } /* end subroutine (uc_timegm) */
@@ -142,17 +166,30 @@ int uc_mktime(TM *tmp,time_t *rp) noex {
 	int		rs = SR_FAULT ;
 	assert(tmp && rp) ;
 	if (tmp && rp) ylikely {
-	    time_t	res = 0 ; /* used-multiple */
-	    rs = SR_OK ;
-	    errno = 0 ;
-	    if ((res = mktime(tmp)) < 0) {
-	        if (errno) rs = (neg errno) ;
-	    } /* end if (error) */
-	    if (rp) {
-	        *rp = (rs >= 0) ? res : 0 ;
-	    }
+	    if ((rs = init) >= 0) ylikely {
+	        time_t	res = 0 ; /* used-multiple */
+	        errno = 0 ;
+	        if ((res = mktime(tmp)) < 0) {
+	            if (errno) rs = (neg errno) ;
+	        } /* end if (error) */
+	        if (rp) {
+	            *rp = (rs >= 0) ? res : 0 ;
+	        }
+	    } /* end if (init) */
 	} /* end if (non-null) */
 	return rs ;
 } /* end subroutine (uc_mktime) */
+
+
+/* local subroutine */
+
+initer::operator int () noex {
+    	int		rs = SR_OK ;
+    	if (! finit) {
+	    tzset() ;
+	    finit = true ;
+	} /* end if (initialization needed) */
+	return (rs >= 0) ? int(finit) : rs ;
+} /* end method (initer::operator) */
 
 
