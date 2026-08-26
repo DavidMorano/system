0c51a202f11e45b82d37d58d52424b562fd6e7fe
David A.D. Morano <david.a.morano@gmail.com>
2026-08-26 00:47:47 -0400

    update

--- src/libfb/fbread.cc
+++ src/libfb/fbread.cc
@@ -45,7 +45,7 @@
 ******************************************************************************/
 
 
-#include	<envstandards.h>
+#include	<envstandards.h>	/* ordered first to configure */
 
 #include	<cstdio>
 
