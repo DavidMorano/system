92adc13bb9f97334bc2f7a5304c3a8f14f2f4a01
David A.D. Morano <david.a.morano@gmail.com>
2026-08-27 00:17:44 -0400

    restore

---
+++ src/libu/utimezone/utimezones.hh
@@ -0,0 +1,29 @@
+/* utimezones HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* time-zone retrieval */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	Originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UTIMEZONES_INCLUDE
+#define	UTIMEZONES_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* UTIMEZONES_INCLUDE */
+
+
