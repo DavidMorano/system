4dcfe9f142b23eb8ecebe2e82d1a0ff192c4800f
David A.D. Morano <david.a.morano@gmail.com>
2026-08-28 00:18:07 -0400

    update

--- src/libu/uexec.h
+++ src/libu/uexec.h
@@ -32,14 +32,11 @@
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<sys/types.h>		/* system types */
-#include	<unistd.h>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<usupport.h>
+#include	<sys/types.h>		/* POSIX® system types */
+#include	<unistd.h>		/* POSIX® */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usupport.h>		/* LIBU */
 
 
 EXTERNC_begin
