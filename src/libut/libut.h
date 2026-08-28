03533be0aade10d00b889ef524daec1fac194323
David A.D. Morano <david.a.morano@gmail.com>
2026-08-28 00:38:51 -0400

    update

--- src/libut/libut.h
+++ src/libut/libut.h
@@ -31,8 +31,8 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 #include	<snxti.h>
 
