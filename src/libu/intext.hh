51a15980dec294c7ecf95f543c46ec398f2c5d7b
David A.D. Morano <david.a.morano@gmail.com>
2026-08-28 00:16:34 -0400

    restore

---
+++ src/libu/intext.hh
@@ -0,0 +1,33 @@
+/* intext HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* subroutine interfaces to compiler built-in instrinsics */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	INTEXT_INCLUDE
+#define	INTEXT_INCLUDE
+#ifdef	__cplusplus /* C++ only */
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* INTEXT_INCLUDE */
+
+
