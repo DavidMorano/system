485f5d5d152f6f8c23182920ade80a2cadce0760
David A.D. Morano <david.a.morano@gmail.com>
2026-08-28 00:21:41 -0400

    update

--- src/libu/umsg.h
+++ src/libu/umsg.h
@@ -41,15 +41,11 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<sys/msg.h>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-
-#include	<usys.h>
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/msg.h>		/* POSIX® */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usys.h>		/* LIBU */
 
 
 #ifndef	TYPEDEF_SYSVMSGTYPE
