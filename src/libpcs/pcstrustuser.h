542ffd42f4f4916a77970a75e7c5d5fcc1e8e6b0
David A.D. Morano <david.a.morano@gmail.com>
2026-08-28 00:13:50 -0400

    update

--- src/libpcs/pcstrustuser.h
+++ src/libpcs/pcstrustuser.h
@@ -21,9 +21,8 @@
 
 
 #include	<envstandards.h>	/* MUST be first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<vecstr.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 
 EXTERNC_begin
