d13318b34842b113d2e2ba92e0667b51563a33e9
David A.D. Morano <david.a.morano@gmail.com>
2026-08-28 00:17:29 -0400

    update

--- src/libu/ucomposite/ucomposite.h
+++ src/libu/ucomposite/ucomposite.h
@@ -29,8 +29,8 @@
 
 
 #include	<envstandards.h>	/* MUST be ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
 
 #include	<ucopy.h>
 #include	<ulinkfile.h>
