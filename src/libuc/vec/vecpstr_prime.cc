bd8cec522b348655986b97cb4643675006bc6463
David A.D. Morano <david.a.morano@gmail.com>
2026-08-29 20:10:17 -0400

    update

--- src/libuc/vec/vecpstr_prime.cc
+++ src/libuc/vec/vecpstr_prime.cc
@@ -80,8 +80,8 @@
 #include	<clanguage.h>		/* LIBU */
 #include	<usysbase.h>		/* LIBU */
 #include	<usyscalls.h>		/* LIBU */
+#include	<nulstr.h>		/* LIBU */
 #include	<uclibmem.h>		/* LIBUC */
-#include	<nulstr.h>		/* LIBUC */
 #include	<vechand.h>		/* LIBUC */
 #include	<vstrcmp.h>		/* LIBUC */
 #include	<nleadstr.h>		/* LIBUC */
