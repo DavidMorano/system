04e9f4dafd1bc6eb5b098b059e61ac21401da00b
David A.D. Morano <david.a.morano@gmail.com>
2026-08-29 20:10:13 -0400

    update

--- src/libuc/usysnative.h
+++ src/libuc/usysnative.h
@@ -25,15 +25,15 @@
 #include	<envstandards.h>	/* MUST be first to configure */
 
 #include	<sys/mount.h>		/* for |USTATFS| */
-#include	<sys/resource.h>
-#include	<sys/socket.h>
-#include	<sys/stat.h>
-#include	<sys/statvfs.h>
+#include	<sys/resource.h>	/* POSIX® */
+#include	<sys/socket.h>		/* POSIX® */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<sys/statvfs.h>		/* POSIX® */
 #include	<sys/time.h>		/* for |u_adjtime(3u)| */
 #include	<sys/timeb.h>		/* for |uc_ftime(3uc)| */
-#include	<sys/types.h>
-#include	<sys/uio.h>
-#include	<sys/utsname.h>
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/uio.h>		/* POSIX® */
+#include	<sys/utsname.h>		/* POSIX® */
 #include	<sys/select.h>		/* |u_select(3u)| */
 
 #include	<sys/shm.h>		/* UNIX® Sys-V IPC - Shared-Memory */
@@ -42,25 +42,25 @@
 
 #include	<arpa/inet.h>		/* <- |in_addr_t| */
 
-#include	<dirent.h>
-#include	<errno.h>
-#include	<fcntl.h>
-#include	<grp.h>
-#include	<limits.h>
-#include	<netdb.h>
-#include	<poll.h>
-#include	<pthread.h>
-#include	<pwd.h>
-#include	<signal.h>
-#include	<stddef.h>
-#include	<stdlib.h>
-#include	<stdarg.h>
-#include	<stdint.h>
-#include	<termios.h>
-#include	<time.h>
-#include	<ucontext.h>
-#include	<unistd.h>
-#include	<utime.h>		/* |u_utime(2)| */
+#include	<ucontext.h>		/* POSIX® */
+#include	<dirent.h>		/* POSIX® */
+#include	<unistd.h>		/* POSIX® */
+#include	<fcntl.h>		/* POSIX® */
+#include	<netdb.h>		/* POSIX® */
+#include	<poll.h>		/* POSIX® */
+#include	<pthread.h>		/* POSIX® */
+#include	<termios.h>		/* POSIX® */
+#include	<utime.h>		/* POSIX® |u_utime(2)| */
+#include	<pwd.h>			/* POSIX® */
+#include	<grp.h>			/* POSIX® */
+#include	<time.h>		/* CSTD */
+#include	<errno.h>		/* CSTD */
+#include	<limits.h>		/* CSTD */
+#include	<signal.h>		/* CSTD */
+#include	<stddef.h>		/* CSTD */
+#include	<stdlib.h>		/* CSTD */
+#include	<stdarg.h>		/* CSTD */
+#include	<stdint.h>		/* CSTD */
 
 /* synthetics */
 #include	<usysacl.h>		/* need for stupid Apple-Darwin */
