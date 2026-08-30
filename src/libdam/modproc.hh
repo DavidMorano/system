32edb5a3ea8f66ac0002f8a9bd9e2e3ae3271ccc
David A.D. Morano <david.a.morano@gmail.com>
2026-08-29 20:02:03 -0400

    restore

---
+++ src/libdam/modproc.hh
@@ -0,0 +1,31 @@
+/* modproc HEADER (Module-Processing) */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* Module-Processing - C++ module name processing (from C++ source files) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MODPROC_INCLUDE
+#define	MODPROC_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>
+#include	<usysbase.h>
+
+
+#endif /* __cplusplus */
+#endif /* MODPROC_INCLUDE */
+
+
