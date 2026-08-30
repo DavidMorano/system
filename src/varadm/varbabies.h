7e2be4ca548cb61c8f1ae6006bf75f1446d033dc
David A.D. Morano <david.a.morano@gmail.com>
2026-08-29 20:22:16 -0400

    update

--- src/varadm/varbabies.h
+++ src/varadm/varbabies.h
@@ -1,4 +1,5 @@
 /* varbabies HEADER */
+/* charset=ISO8859-1 */
 /* lang=C20 */
 
 /* KSH variable BABIES */
@@ -11,17 +12,15 @@
 #define	VARBABIES_INCLUDE
 
 
-#include	<envstandards.h>	/* first to configure */
-#include	<sys/types.h>
-#include	<sys/param.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
+#include	<envstandards.h>	/* ordered first to configure */
 #include	<clanguage.h>
+#include	<usysbase.h>
 #include	<vecobj.h>
-#include	<localmisc.h>
 
 
 #define	VARBABIES	struct varbabies_head
+#define	VARBABIES_CUR	struct varbabies_cursor
+#define	VARBABIES_VAR	struct varbabies_variable
 
 
 struct varbabies_head {
@@ -29,29 +28,33 @@ struct varbabies_head {
 	uint		magic ;
 } ;
 
-struct varbabies_c {
+struct varbabies_cursor {
 	int		i ;
 } ;
 
-struct varbabies_var {
+struct varbabies_variable {
 	cchar		*varname ;
 	cchar		*soname ;
 	int		refcount ;
 } ;
 
+typedef VARBABIES	varbabies ;
+typedef VARBABIES_CUR	varbabies_cur ;
+typedef VARBABIES_VAR	varbabies_var ;
+
 EXTERNC_begin
 
-extern int varbabies_set(VARBABIES *) noex ;
-extern int varbabies_store(VARBABIES *,VARBABIES_VAR *) noex ;
-extern int varbabies_fetch(VARBABIES *,VARBABIES_VAR *) noex ;
-extern int varbabies_enum(VARBABIES *,VARBABIES_CUR *,VARBABIES_VAR *) noex ;
-extern int varbabies_finish(VARBABIES *) noex ;
+extern int varbabies_set(varbabies *) noex ;
+extern int varbabies_store(varbabies *,varbabies_var *) noex ;
+extern int varbabies_fetch(varbabies *,varbabies_var *) noex ;
+extern int varbabies_curbegin(varbabies *,varbabies_cur *) noex ;
+extern int varbabies_curenum(varbabies *,varbabies_cur *,varbabies_var *) noex ;
+extern int varbabies_curend(varbabies *,varbabies_cur *) noex ;
+extern int varbabies_finish(varbabies *) noex ;
 
 EXTERNC_end
 
 
 #endif /* VARBABIES_INCLUDE */
-
-
 
 
