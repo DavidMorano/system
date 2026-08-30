1ba7bb26e6536a83255ad5673e5cf6638da91005
David A.D. Morano <david.a.morano@gmail.com>
2026-08-29 20:22:01 -0400

    update

--- src/varadm/kshvarhead.h
+++ src/varadm/kshvarhead.h
@@ -12,39 +12,39 @@
 #define	KSHVAR_INCLUDE
 
 
-#include	<envstandards.h>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<vecobj.h>
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<vecobj.h>		/* LIBUC */
 
 
 #define	KSHVAR		struct kshvar_head
+#define	KSHVAR_MAGIC	0x49736218
 
 
 struct kshvar_head {
 	vecobj		vars ;
-	uint		magic ;
-} ;
+	uint		magval ;
+} ; /* end struct */
 
 struct kshvar_c {
 	int		i ;
-} ;
+} ; /* end struct */
 
 struct kshvar_var {
 	cchar		*varname ;
 	cchar		*soname ;
 	int		refcount ;
-} ;
+} ; /* end struct */
 
 EXTERNC_begin
 
 extern int	kshvar_start(KSHVAR *) noex ;
 extern int	kshvar_store(KSHVAR *,KSHVAR_VAR *) noex ;
 extern int	kshvar_fetch(KSHVAR *,KSHVAR_VAR *) noex ;
-extern int	kshvar_enum(KSHVAR *,KSHVAR_CUR *,KSHVAR_VAR *) noex ;
+extern int	kshvar_curbegin(KSHVAR *,KSHVAR_CUR *) noex ;
+extern int	kshvar_curenum(KSHVAR *,KSHVAR_CUR *,KSHVAR_VAR *) noex ;
+extern int	kshvar_curend(KSHVAR *,KSHVAR_CUR *) noex ;
 extern int	kshvar_finish(KSHVAR *) noex ;
 
 EXTERNC_end
