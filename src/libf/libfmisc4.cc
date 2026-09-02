44043933a52d2a41adf5b7cd52ea4b6d428fb19a
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 01:04:19 -0400

    restore

---
+++ src/libf/libfmisc4.cc
@@ -0,0 +1,119 @@
+/* libfmisc4 MODULE */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* UNIX® Miscellaneous support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-19, David A­D­ Morano
+	This code was originally written.
+
+	= 2020-05-07, David A-D- Morano
+	I converted this (formerly a header-only file) to a module.
+
+*/
+
+/* Copyright © 1998,2020 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Module:
+	libfmisc
+
+	Description:
+	This module provides miscellaneous UNIX® or other
+	common (library oriented) subroutines.
+
+*******************************************************************************/
+
+module ;
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+module libfmisc ;
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int snaddw(char *rbuf,int rlen,int i,cchar *sp,int sl) noex {
+	int		rs = SR_FAULT ;
+	int		len = 0 ;
+	if (rbuf && sp) {
+	    rs = SR_INVALID ;
+ 	    if (i >= 0) {	
+	        char	*bp = (rbuf + i) ;
+	        rs = SR_OK ;
+	        if (rlen < 0) {
+	            if (sl < 0) {
+	                while (*sp) {
+	                    *bp++ = *sp++ ;
+		        }
+	            } else {
+	                while (sl && *sp) {
+	                    *bp++ = *sp++ ;
+	                    sl -= 1 ;
+	                }
+	            } /* end if */
+	        } else {
+	            if (sl < 0) {
+	                while ((bp < (rbuf + rlen)) && *sp) {
+	                    *bp++ = *sp++ ;
+		        }
+		        if ((bp == (rbuf + rlen)) && (*sp != '\0')) {
+		            rs = SR_OVERFLOW ;
+		        }
+	            } else {
+	                while ((bp < (rbuf + rlen)) && sl && *sp) {
+	                    *bp++ = *sp++ ;
+	                    sl -= 1 ;
+	                }
+		        if ((bp == (rbuf + rlen)) && sl) {
+		            rs = SR_OVERFLOW ;
+		        }
+	            } /* end if */
+	        } /* end if */
+	        *bp = '\0' ;
+		len = intconv(bp - (rbuf + i)) ;
+	    } /* end if (valid) */
+	} /* end if (non-null) */
+	return (rs >= 0) ? len : rs ;
+} /* end subroutine (snaddw) */
+
+
