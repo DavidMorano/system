9fa221808ca4778e578ca2c8303d037ed1e7ec99
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 01:06:13 -0400

    restore

---
+++ src/mods/modsnwcpy.hh
@@ -0,0 +1,34 @@
+/* modsnwcpy HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* string-index operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MODSNWCPY_INCLUDE
+#define	MODSNWCPY_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* MODSNWCPY_INCLUDE */
+
+
