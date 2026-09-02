be446611c77a2e22eaf5199b00c1bf57f3bcb74e
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 01:02:00 -0400

    update

--- src/libu/intx/intsat.h
+++ src/libu/intx/intsat.h
@@ -31,40 +31,40 @@
 
 EXTERNC_begin
 
-static inline int intsati(cint v) noex {
+local inline int intsati(cint v) noex {
 	return v ;
 }
 
-extern int intsatl	(clong) noex ;
-extern int intsatll	(clonglong) noex ;
+extern int intsatl	(clong)		noex ;
+extern int intsatll	(clonglong)	noex ;
 
-extern int intsatui	(cuint) noex ;
-extern int intsatul	(culong) noex ;
-extern int intsatull	(culonglong) noex ;
+extern int intsatui	(cuint)		noex ;
+extern int intsatul	(culong)	noex ;
+extern int intsatull	(culonglong)	noex ;
 
 EXTERNC_end
 
 #ifdef	__cplusplus
 
-static inline int intsat(cint v) noex {
+local inline int intsat(cint v)		noex {
 	return intsati(v) ;
-}
-static inline int intsat(clong v) noex {
+} /* end */
+local inline int intsat(clong v)	noex {
 	return intsatl(v) ;
-}
-static inline int intsat(clonglong v) noex {
+} /* end */
+local inline int intsat(clonglong v)	noex {
 	return intsatll(v) ;
-}
+} /* end */
 
-static inline int intsat(cuint v) noex {
+local inline int intsat(cuint v)	noex {
 	return intsatui(v) ;
-}
-static inline int intsat(culong v) noex {
+} /* end */
+local inline int intsat(culong v)	 noex {
 	return intsatul(v) ;
-}
-static inline int intsat(culonglong v) noex {
+} /* end */
+local inline int intsat(culonglong v)	noex {
 	return intsatull(v) ;
-}
+} /* end */
 
 #endif /* __cplusplus */
 
