f7f99911b11466118f67fb510f46860f235d3211
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 01:02:54 -0400

    restore

---
+++ src/libf/libfsup.hh
@@ -0,0 +1,34 @@
+/* libfsup HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* UNIX® kernel support subroutines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2020-10-13, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 2020 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	LIBFUTIL_INCLUDE
+#define	LIBFUTIL_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* LIBFUTIL_INCLUDE */
+
+
