afc9f9fe65d1db848928ef90f2451d02e4c62848
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 00:51:25 -0400

    restore

---
+++ src/libu/ucmpx/cmptimeval.h
@@ -0,0 +1,36 @@
+/* cmptimeval HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* LIBU comparison operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CMPTIMEVAL_INCLUDE
+#define	CMPTIMEVAL_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int cmptimeval(CTIMEVAL *,CTIMEVAL *) noex ;
+
+EXTERNC_end
+
+
+#endif /* CMPTIMEVAL_INCLUDE */
+
+
