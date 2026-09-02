a0cea39187a8aac43af8cf326af398d4395440f4
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 00:51:08 -0400

    restore

---
+++ src/libu/ucmpx/cmpitimer.h
@@ -0,0 +1,36 @@
+/* cmpitimer HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* LIBU comparison operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CMPITIMER_INCLUDE
+#define	CMPITIMER_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int cmpitermerval(CITIMERVAL *,CITIMERVAL *) noex ;
+
+EXTERNC_end
+
+
+#endif /* CMPITIMER_INCLUDE */
+
+
