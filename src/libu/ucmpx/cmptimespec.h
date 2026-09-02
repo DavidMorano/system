f9a70d4ca6275418f027ca68ae8ab5bf669d3209
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 00:51:17 -0400

    restore

---
+++ src/libu/ucmpx/cmptimespec.h
@@ -0,0 +1,36 @@
+/* cmptimespec HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* LIBU comparison operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	CMPTIMESPEC_INCLUDE
+#define	CMPTIMESPEC_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern int cmptimespec(CTIMESPEC *,CTIMESPEC *) noex ;
+
+EXTERNC_end
+
+
+#endif /* CMPTIMESPEC_INCLUDE */
+
+
