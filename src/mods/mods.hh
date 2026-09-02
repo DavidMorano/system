4fbc80b66fdd765fe410444104f118c571f32be2
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 01:05:33 -0400

    restore

---
+++ src/mods/mods.hh
@@ -0,0 +1,38 @@
+/* mods HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* string-index operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MODS_INCLUDE
+#define	MODS_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+
+#include	<modsncpy.hh>
+#include	<modsnwcpy.hh>
+#include	<modstrw.hh>
+
+
+#endif /* __cplusplus */
+#endif /* MODS_INCLUDE */
+
+
