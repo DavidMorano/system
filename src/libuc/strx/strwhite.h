ee5038b936faadedb2b31a591d9b7adb33dd7b12
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 00:56:20 -0400

    update

--- src/libuc/strx/strwhite.h
+++ src/libuc/strx/strwhite.h
@@ -29,10 +29,10 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>		/* LINU */
-#include	<utypedefs.h>		/* LINU */
-#include	<utypealiases.h>	/* LINU */
-#include	<usysdefs.h>		/* LINU */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
 
 
 EXTERNC_begin
