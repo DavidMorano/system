bcaaa0980959f51da90b783c5cac5646d862fac6
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 00:54:12 -0400

    restore

---
+++ src/libu/umods/sysdbfnames.hh
@@ -0,0 +1,33 @@
+/* sysdbfnames HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* these are the standard (relatively) UNIX® database file names */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	SYSDBFNAMES_INCLUDE
+#define	SYSDBFNAMES_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* SYSDBFNAMES_INCLUDE */
+
+
