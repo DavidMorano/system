42cc67202433d113c197016dd6fd6e5e41517b2a
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 01:04:04 -0400

    restore

---
+++ src/libf/libfmisc.hh
@@ -0,0 +1,31 @@
+/* libfmisc HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* LIBU miscellaneous interfaces */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	LIBFMISC_INCLUDE
+#define	LIBFMISC_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* LIBFMISC_INCLUDE */
+
+
