94de8e3b65a3a65db3f90cafecfa000d2a0bfbd8
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 00:52:17 -0400

    restore

---
+++ src/libu/ucmpx/ucmpx.h
@@ -0,0 +1,33 @@
+/* ucmpx HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* LIBU comparison operations */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	UCMPX_INCLUDE
+#define	UCMPX_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+#include	<cmpitimer.h>
+#include	<cmptimeval.h>
+#include	<cmptimespec.h>
+
+
+#endif /* UCMPX_INCLUDE */
+
+
