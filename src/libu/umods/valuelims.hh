2cb4f5399854687ba20d2d77bc3e58b27f446d06
David A.D. Morano <david.a.morano@gmail.com>
2026-09-02 00:54:22 -0400

    restore

---
+++ src/libu/umods/valuelims.hh
@@ -0,0 +1,45 @@
+/* valuelims HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* numeric value limits (constants) */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Object:
+	itimer
+
+	Description:
+	This object simply provides the identifying values used in
+	selecting a process interval-timer.
+
+*******************************************************************************/
+
+#ifndef	VALUELIMS_INCLUDE
+#define	VALUELIMS_INCLUDE
+#ifdef	__cplusplus	/* C++ only! */
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+
+
+#endif /* __cplusplus (C++ only) */
+#endif /* VALUELIMS_INCLUDE */
+
+
