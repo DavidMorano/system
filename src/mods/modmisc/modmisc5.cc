17b1a1e953d7b1b1f5f3c37ddf34ee1a4fd10cf6
David A.D. Morano <david.a.morano@gmail.com>
2026-09-03 00:30:40 -0400

    restore

---
+++ src/mods/modmisc/modmisc5.cc
@@ -0,0 +1,94 @@
+/* modmisc5 MODULE */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* UNIX® Miscellaneous support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-19, David A­D­ Morano
+	This code was originally written.
+
+	= 2020-05-07, David A-D- Morano
+	I converted this (formerly a header-only file) to a module.
+
+*/
+
+/* Copyright © 1998,2020 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Module:
+	modmisc
+
+	Description:
+	This module provides miscellaneous UNIX® or other
+	common (library oriented) subroutines.
+
+*******************************************************************************/
+
+module ;
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstdarg>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+module modmisc ;
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int snaddx(char *rbuf,int rlen,int rl,int n,...) noex {
+	va_list		ap ;
+	int		rs = SR_FAULT ;
+	int		len = 0 ;
+	if (rbuf) {
+	    va_begin(ap,n) ;
+	    rs = SR_OK ;
+	    for (int i = 0 ; (rs >= SR_OK) && (i < n) ; i += 1) {
+		cchar	*sp = (char *) va_arg(ap,char *) ;
+		rs = snadd(rbuf,rlen,rl,sp) ;
+		len += rs ;
+		rl += rs ;
+	    } /* end for */
+	    va_end(ap) ;
+	} /* end if (non-null) */
+	return (rs >= 0) ? len : rs ;
+} /* end subroutine (snaddx) */
+
+
