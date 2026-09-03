5b6f3e18cd20e8d14940faee3ec14a02a0c3be81
David A.D. Morano <david.a.morano@gmail.com>
2026-09-03 00:30:43 -0400

    restore

---
+++ src/mods/modmisc/modmisc6.cc
@@ -0,0 +1,92 @@
+/* modmisc6 MODULE */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* UNIX® Miscellaneous support */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-07-19, David A­D­ Morano
+	This code was originally written.
+
+	= 2020-05-07, David A-D- Morano
+	I converted this (formerly a header-only file) to a module.
+
+*/
+
+/* Copyright © 1998,2020 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Module:
+	modmisc
+
+	Description:
+	This module provides miscellaneous UNIX® or other
+	common (library oriented) subroutines.
+
+*******************************************************************************/
+
+module ;
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstdarg>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+module modmisc ;
+
+/* local defines */
+
+
+/* local namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int snaddslash(char *rbuf,int rlen,int rl) noex {
+	int		rs = SR_FAULT ;
+	int		len = 0 ;
+	if (rbuf) ylikely {
+	    rs = SR_INVALID ;
+	    if ((rlen >= 0) && (rl >= 0)) ylikely {
+	        rs = SR_OK ;
+		if ((rl == 0) || (rbuf[rl - 1] != '/')) {
+		    rbuf[rl] = '/' ;
+		    len += 1 ;
+		}
+	    } /* end if (valid) */
+	} /* end if (non-null) */
+	return (rs >= 0) ? len : rs ;
+} /* end subroutine (snaddslash) */
+
+
