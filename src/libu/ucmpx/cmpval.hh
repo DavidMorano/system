c7e5db805755f46855f92cc4a15fa4ac4333ab32
David A.D. Morano <david.a.morano@gmail.com>
2026-09-03 00:41:02 -0400

    restore

---
+++ src/libu/ucmpx/cmpval.hh
@@ -0,0 +1,45 @@
+/* cmpval HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* Compare-Value */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 2000-05-14, David A­D­ Morano
+	This code was originally written.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Object:
+	itimer
+
+	Description:
+	This object simply provides the identifying values used in
+	selecting a process interval-timer.
+
+*******************************************************************************/
+
+#ifndef	CMPVAL_INCLUDE
+#define	CMPVAL_INCLUDE
+#ifdef	__cplusplus	/* C++ only! */
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<usysrets.h>		/* LIBU */
+
+
+#endif /* __cplusplus (C++ only) */
+#endif /* CMPVAL_INCLUDE */
+
+
