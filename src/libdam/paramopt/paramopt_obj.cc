7d98c7b4017b97cdddb7b52d50417999a879f21e
David A.D. Morano <david.a.morano@gmail.com>
2026-09-05 00:56:24 -0400

    update

--- src/libdam/paramopt/paramopt_obj.cc
+++ src/libdam/paramopt/paramopt_obj.cc
@@ -43,6 +43,7 @@
 #pragma		GCC dependency		"mod/libutil.ccm"
 
 import libutil ;			/* |lenstr(3u)| */
+import paramopt_mag ;
 
 /* local defines */
 
@@ -74,51 +75,51 @@ import libutil ;			/* |lenstr(3u)| */
 
 int paramopt::loadu(cchar *sp,int sl) noex {
 	return paramopt_loadu(this,sp,sl) ;
-}
+} /* end method */
 
 int paramopt::loads(cchar *name,cchar *sp,int sl) noex {
 	return paramopt_loads(this,name,sp,sl) ;
-}
+} /* end method */
 
 int paramopt::load(cchar *name,cchar *vbuf,int vlen) noex {
 	return paramopt_load(this,name,vbuf,vlen) ;
-}
+} /* end method */
 
 int paramopt::loaduniq(cchar *name,cchar *vp,int vl) noex {
 	return paramopt_loaduniq(this,name,vp,vl) ;
-}
+} /* end method */
 
 int paramopt::loadone(cchar *sp,int sl) noex {
 	return paramopt_loadone(this,sp,sl) ;
-}
+} /* end method */
 
 int paramopt::havekey(cchar *name) noex {
 	return paramopt_havekey(this,name) ;
-}
+} /* end method */
 
 int paramopt::haveval(cchar *key,cchar *vp,int vl) noex {
 	return paramopt_haveval(this,key,vp,vl) ;
-}
+} /* end method */
 
 int paramopt::curbegin(PO_CUR *curp) noex {
 	return paramopt_curbegin(this,curp) ;
-}
+} /* end method */
 
 int paramopt::curend(PO_CUR *curp) noex {
 	return paramopt_curend(this,curp) ;
-}
+} /* end method */
 
 int paramopt::curenumkey(PO_CUR *curp,cchar **rpp) noex {
 	return paramopt_curenumkey(this,curp,rpp) ;
-}
+} /* end method */
 
 int paramopt::curenumval(cchar *key,PO_CUR *curp,cchar **rpp) noex {
 	return paramopt_curenumval(this,key,curp,rpp) ;
-}
+} /* end method */
 
 int paramopt::curfetch(cchar *key,PO_CUR *curp,cchar **rpp) noex {
 	return paramopt_curfetch(this,key,curp,rpp) ;
-}
+} /* end method */
 
 int paramopt_count(paramopt *op) noex {
     	int		rs = SR_NOTOPEN ;
@@ -130,7 +131,7 @@ int paramopt_count(paramopt *op) noex {
 
 int paramopt::countvals(cchar *key) noex {
 	return paramopt_countvals(this,key) ;
-}
+} /* end method */
 
 void paramopt::dtor() noex {
 	if (cint rs = finish ; rs < 0) {
