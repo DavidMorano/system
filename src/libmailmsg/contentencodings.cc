d74210e9e23dfbc679d4fb4a554d3349619cacc9
David A.D. Morano <david.a.morano@gmail.com>
2026-09-05 00:59:22 -0400

    update

--- src/libmailmsg/contentencodings.cc
+++ src/libmailmsg/contentencodings.cc
@@ -28,13 +28,13 @@
 *******************************************************************************/
 
 #include	<envstandards.h>	/* must be ordered first to configure */
-#include	<cstddef>		/* |nullptr_t| */
-#include	<cstdlib>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<localmisc.h>
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
 
 #include	"contentencodings.h"
 
