27a816360584d8115c4a891877288adf41b15926
David A.D. Morano <david.a.morano@gmail.com>
2026-09-05 00:58:49 -0400

    restore

---
+++ src/libf/modmisc.hh
@@ -0,0 +1,31 @@
+/* modmisc HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* miscellaneous interfaces */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	MODMISC_INCLUDE
+#define	MODMISC_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* MODMISC_INCLUDE */
+
+
