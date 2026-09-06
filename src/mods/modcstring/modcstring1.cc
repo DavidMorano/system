ab43a356b42f15c3db29ae07f2b9fcfe94952327
David A.D. Morano <david.a.morano@gmail.com>
2026-09-06 01:19:22 -0400

    restore

---
+++ src/mods/modcstring/modcstring1.cc
@@ -0,0 +1,92 @@
+/* modcstring MODULE (implementation) */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* UNIX® kernel support subroutines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-03-26, David A­D­ Morano
+	This was first written to give a little bit to UNIX® what
+	we have in our own circuit-pack OSes!
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Group:
+	cstrlen
+	cmemcpy
+	cmemset
+
+	Description:
+	These are constant-evaluation versions of some standard
+	C-language library string subroutines.
+	subroutines.
+
+	Synopsis:
+	int cstrlen
+	voidp cmemcpy
+
+	Arguments:
+	dp		destination buffer pointer
+	dl		destination buffer length
+	s1		source string (first)
+
+	Returns:
+	>=0		amount of data returned
+	<0		error (system-return)
+
+*******************************************************************************/
+
+module ;
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<cstring>		/* CSTD |strlcpy(3c)| */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"modcstring.hh"
+
+module modcstring ;
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
