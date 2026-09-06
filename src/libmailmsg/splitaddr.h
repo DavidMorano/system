4ed042a8ccc54839b191d860d80efef3f7e70c6d
David A.D. Morano <david.a.morano@gmail.com>
2026-09-06 01:10:36 -0400

    update

--- src/libmailmsg/splitaddr.h
+++ src/libmailmsg/splitaddr.h
@@ -20,9 +20,9 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<clanguage.h>
-#include	<usysbase.h>
-#include	<vechand.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<vechand.h>		/* LIBUC */
 
 
 #define	SPLITADDR	struct splitaddr_head
