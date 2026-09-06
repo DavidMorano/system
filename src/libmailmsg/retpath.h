192414330f326013d4f928929981d96aae81b66b
David A.D. Morano <david.a.morano@gmail.com>
2026-09-06 01:10:28 -0400

    update

--- src/libmailmsg/retpath.h
+++ src/libmailmsg/retpath.h
@@ -20,12 +20,9 @@
 
 
 #include	<envstandards.h>	/* must be ordered first to configure */
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
-#include	<usysrets.h>
-#include	<vecstr.h>
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<vecstr.h>		/* LIBUC */
 
 
 #define	RETPATH		vecstr
