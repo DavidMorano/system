c1d451e58300b31e008a236b6ae429df2790be7f
David A.D. Morano <david.a.morano@gmail.com>
2026-09-06 01:03:31 -0400

    restore

---
+++ src/libmailmsg/mailaddrtypes.cc
@@ -0,0 +1,73 @@
+/* mailaddrtypes SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* Mail_Address_Types */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-12, David A­D­ Morano
+	This code is newly written but modeled after some code that
+	was originally written to get some standardized variable
+	serialization for the Ethernet-Controller (ETHCON) development
+	work.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+  	Name:
+	mailaddrtypes
+
+	Description:
+	This file defines the various mail-address types that I
+	handle in various other code pieces.
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"mailaddrtypes.h"
+
+
+/* local defines */
+
+
+/* imported namespaces */
+
+
+/* local typedefs */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+
