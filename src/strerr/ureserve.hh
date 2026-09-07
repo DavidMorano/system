c17c0fb40d28fcfd0608af661a79f082a02f9eb0
David A.D. Morano <david.a.morano@gmail.com>
2026-09-07 00:24:55 -0400

    restore

---
+++ src/strerr/ureserve.hh
@@ -0,0 +1,31 @@
+/* ureserve HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* LIBU reserved interfaces */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-04-10, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	URESERVE_INCLUDE
+#define	URESERVE_INCLUDE
+#ifdef	__cplusplus
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif /* __cplusplus */
+#endif /* URESERVE_INCLUDE */
+
+
