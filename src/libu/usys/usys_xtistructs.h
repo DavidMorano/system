064a36de4dcd9abdce5b9199360a70f210307c11
David A.D. Morano <david.a.morano@gmail.com>
2026-09-15 00:57:33 -0400

    update

--- src/libu/usys/usys_xtistructs.h
+++ src/libu/usys/usys_xtistructs.h
@@ -31,11 +31,11 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<sys/types.h>		/* POSIX® */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
 
 
 #if	defined(SYSHAS_XTI) && (SYSHAS_XTI > 0)
