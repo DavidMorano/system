b3b81be93acc1d8d7e0890d2c8dbfa2ad8c73ed7
David A.D. Morano <david.a.morano@gmail.com>
2026-09-15 00:59:19 -0400

    restore

---
+++ src/libuc/hasx/hasext.h
@@ -0,0 +1,38 @@
+/* hasext HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* does the c-string have the given extension c-string */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright (c) 1998 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	HASEXT_INCLUDE
+#define	HASEXT_INCLUDE
+
+
+#include	<envstandards.h>	/* MUST be first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
+
+
+EXTERNC_begin
+
+extern bool	hasext	(cchar *,int,cchar *) noex ;
+
+EXTERNC_end
+
+
+#endif /* HASEXT_INCLUDE */
+
+
