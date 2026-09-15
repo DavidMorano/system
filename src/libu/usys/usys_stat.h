46bc23e347ddf948e625a12f46fa28705a75ef0e
David A.D. Morano <david.a.morano@gmail.com>
2026-09-15 00:56:28 -0400

    update

--- src/libu/usys/usys_stat.h
+++ src/libu/usys/usys_stat.h
@@ -34,12 +34,12 @@
 
 
 #include	<envstandards.h>	/* ordered first to configure */
-#include	<sys/types.h>
-#include	<sys/stat.h>
-#include	<clanguage.h>
-#include	<utypedefs.h>
-#include	<utypealiases.h>
-#include	<usysdefs.h>
+#include	<sys/types.h>		/* POSIX® */
+#include	<sys/stat.h>		/* POSIX® */
+#include	<clanguage.h>		/* LIBU */
+#include	<utypedefs.h>		/* LIBU */
+#include	<utypealiases.h>	/* LIBU */
+#include	<usysdefs.h>		/* LIBU */
 
 
 /* missing from some operating systems */
