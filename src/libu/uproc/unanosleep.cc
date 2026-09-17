1e929fc1bbd5327d6752117a0faf41c88a172438
David A.D. Morano <david.a.morano@gmail.com>
2026-09-17 00:19:12 -0400

    restore

---
+++ src/libu/uproc/unanosleep.cc
@@ -0,0 +1,57 @@
+/* unanosleep SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* translation layer interface for UNIX® equivalents */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	This subroutine was written for Rightcore Network Services (RNS).
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+#include	<envstandards.h>	/* MUST be ordered first to configure */
+#include	<sys/types.h>		/* POSIX® */
+#include	<cerrno>		/* CSTD */
+#include	<ctime>			/* CSTD |nanosleep(2)| */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<usyscalls.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"unanosleep.h"
+
+
+/* local defines */
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local variables */
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int u_nanosleep(CTIMESPEC *tsp,TIMESPEC *rtsp) noex {
+	int		rs = SR_FAULT ;
+	if (tsp) {
+	    if ((rs = nanosleep(tsp,rtsp)) < 0) {
+	        rs = (neg errno) ;
+	    }
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (u_nanosleep) */
+
+
