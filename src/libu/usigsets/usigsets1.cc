ae13e65079e34d2232e1657cfdb1a5efeff52d5c
David A.D. Morano <david.a.morano@gmail.com>
2026-09-19 00:54:12 -0400

    update

--- src/libu/usigsets/usigsets1.cc
+++ src/libu/usigsets/usigsets1.cc
@@ -59,35 +59,77 @@ module usigsets ;
 
 /* exported variables */
 
-usigsets	usigset ;
+usigsets	usigseter ;
 
 
 /* exported subroutines */
 
 extern "C" {
     int u_sigsetempty	(sigset_t *sp) noex {
-	return usigset.empty(sp) ;
+	return usigseter.empty(sp) ;
     }
     int u_sigsetfill	(sigset_t *sp) noex {
-	return usigset.fill(sp) ;
+	return usigseter.fill(sp) ;
     }
     int u_sigsetadd	(sigset_t *sp,int si) noex {
-	return usigset.add(sp,si) ;
+	return usigseter.add(sp,si) ;
     }
     int u_sigsetdel	(sigset_t *sp,int si) noex {
-	return usigset.del(sp,si) ;
+	return usigseter.del(sp,si) ;
     }
-    int u_sigsetismem	(sigset_t *sp,int si) noex {
-	return usigset.ismem(sp,si) ;
+    int u_sigsetismem	(const sigset_t *sp,int si) noex {
+	return usigseter.ismem(sp,si) ;
     }
 } /* end extern (C) */
+
+int u_sigmask(int w,usigset *nsp,usigset *osp) noex {
+    int		rs ;
+    if (nsp && osp) {
+        rs = u_sigmask(w,&nsp->ss,&osp->ss) ;
+    } else if (nsp) {
+	sigset_t *tosp = nullptr ;
+        rs = u_sigmask(w,&nsp->ss,tosp) ;
+    } else {
+	sigset_t *tnsp = nullptr ;
+	sigset_t *tosp = nullptr ;
+        rs = u_sigmask(w,tnsp,tosp) ;
+    }
+    return rs ;
+} /* end subroutine */
+
+/* local subroutines */
+int u_sigmask(int w,usigset *nsp,sigset_t *osp) noex {
+    int		rs ;
+    if (nsp) {
+        rs = u_sigmask(w,&nsp->ss,osp) ;
+    } else {
+	sigset_t *tnsp = nullptr ;
+        rs = u_sigmask(w,tnsp,osp) ;
+    }
+    return rs ;
+} /* end subroutine */
+
+int u_sigmask(int w,sigset_t *nsp,usigset *osp) noex {
+    int		rs ;
+    if (osp) {
+        rs = u_sigmask(w,nsp,&osp->ss) ;
+    } else {
+	sigset_t *tosp = nullptr ;
+        rs = u_sigmask(w,nsp,tosp) ;
+    }
+    return rs ;
+} /* end subroutine */
+
+int u_sigwait(usigset *sp,int *rp) noex {
+    return u_sigwait(&sp->ss,rp) ;
+} /* end subroutine */
 
 
 /* local subroutines */
 
 int usigsets::empty(sigset_t *sp) noex {
 	int		rs = SR_FAULT ;
-	if (sp) {
+	if (sp) ylikely {
 	    if ((rs = sigemptyset(sp)) < 0) {
 	        rs = (neg errno) ;
 	    }
@@ -97,7 +139,7 @@ int usigsets::empty(sigset_t *sp) noex {
 
 int usigsets::fill(sigset_t *sp) noex {
 	int		rs = SR_FAULT ;
-	if (sp) {
+	if (sp) ylikely {
 	    if ((rs = sigfillset(sp)) < 0) {
 	        rs = (neg errno) ;
 	    }
@@ -107,9 +149,9 @@ int usigsets::fill(sigset_t *sp) noex {
 
 int usigsets::add(sigset_t *sp,int sn) noex {
 	int		rs = SR_FAULT ;
-	if (sp) {
+	if (sp) ylikely {
 	    rs = SR_INVALID ;
-	    if (sn >= 0) {
+	    if (sn >= 0) ylikely {
 	        if ((rs = sigaddset(sp,sn)) < 0) {
 	            rs = (neg errno) ;
 	        }
@@ -120,9 +162,9 @@ int usigsets::add(sigset_t *sp,int sn) noex {
 
 int usigsets::del(sigset_t *sp,int sn) noex {
 	int		rs = SR_FAULT ;
-	if (sp) {
+	if (sp) ylikely {
 	    rs = SR_INVALID ;
-	    if (sn >= 0) {
+	    if (sn >= 0) ylikely {
 	        if ((rs = sigdelset(sp,sn)) < 0) {
 	            rs = (neg errno) ;
 	        }
@@ -133,9 +175,9 @@ int usigsets::del(sigset_t *sp,int sn) noex {
 
 int usigsets::ismem(const sigset_t *sp,int sn) noex {
 	int		rs = SR_FAULT ;
-	if (sp) {
+	if (sp) ylikely {
 	    rs = SR_INVALID ;
-	    if (sn >= 0) {
+	    if (sn >= 0) ylikely {
 	        if ((rs = sigismember(sp,sn)) < 0) {
 		    rs = (neg errno) ;
 	        }
