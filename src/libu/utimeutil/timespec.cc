91ecbb82a87d0ff1638522e78f7c017a79df5ccd
David A.D. Morano <david.a.morano@gmail.com>
2026-09-19 00:50:00 -0400

    restore

---
+++ src/libu/utimeutil/timespec.cc
@@ -0,0 +1,94 @@
+/* timespec SUPPORT */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* UNIX® TIMESPEC object initialization */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-11-01, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A­D­ Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Name:
+	timespec_load
+
+	Description:
+	These subroutines manipulate TIMESPEC objects.
+
+	Synopsis:
+	int timespec_load(TIMESPEC *tsp,time_t sec,long nsec) noex
+
+	Arguments:
+	tsp		pointer to TIMESPEC
+	sec		seconds
+	nsec		nanoseconds
+
+	Returns:
+	>=0		OK
+	<0		error (system-return)
+
+*******************************************************************************/
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<ctime>			/* CSTD |TIMESPEC| */
+#include	<cstddef>		/* CSTD */
+#include	<cstdlib>		/* CSTD */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+#include	<localmisc.h>		/* LIBU */
+
+#include	"timespec.h"
+
+
+/* local defines */
+
+#ifndef	INTBILLION
+#define	INTBILLION	1000000000
+#endif
+
+
+/* external subroutines */
+
+
+/* external variables */
+
+
+/* local structures */
+
+
+/* forward references */
+
+
+/* local variables */
+
+const int	onebillion = INTBILLION ;
+
+
+/* exported variables */
+
+
+/* exported subroutines */
+
+int timespec_load(TIMESPEC *tsp,time_t sec,long nsec) noex {
+	int		rs = SR_FAULT ;
+	if (tsp) ylikely {
+	    rs = SR_OK ;
+	    while (nsec >= onebillion) {
+	        sec += 1 ;
+	        nsec -= onebillion ;
+	    } /* end while */
+	    tsp->tv_sec = sec ;
+	    tsp->tv_nsec = nsec ;
+	} /* end if (non-null) */
+	return rs ;
+} /* end subroutine (timespec_load) */
+
+
