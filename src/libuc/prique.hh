8aa8d03190ec0e766df61a64ad181efd1077c017
David A.D. Morano <david.a.morano@gmail.com>
2026-09-22 03:36:00 -0400

    restore

---
+++ src/libuc/prique.hh
@@ -0,0 +1,34 @@
+/* prique HEADER */
+/* charset=ISO8859-1 */
+/* lang=C++20 */
+
+/* this object implements a priority queue container of objects */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-23, David A­D­ Morano
+	Originally written for Rightcore Network Services.
+
+	= 2020-05-07, David A-D- Morano
+	I converted this (formerly a header-only file) to a module.
+
+*/
+
+/* Copyright © 1998,2020 David A­D­ Morano.  All rights reserved. */
+
+#ifndef	PRIQUE_INCLUDE
+#define	PRIQUE_INCLUDE
+#ifdef	__cplusplus /* everything is C++ only */
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<clanguage.h>		/* LIBU */
+#include	<usysbase.h>		/* LIBU */
+
+
+#endif	/* __cplusplus */
+#endif /* PRIQUE_INCLUDE */
+
+
