cb4b167e0456ee53708e1e9ccd5f283afa97e67f
David A.D. Morano <david.a.morano@gmail.com>
2026-09-24 00:11:06 -0400

    update

--- src/libu/usysdefs.h
+++ src/libu/usysdefs.h
@@ -43,6 +43,7 @@
 #include	<usysdefs_pf.h>		/* protocol families */
 #include	<usysdefs_fcntl.h>
 #include	<usysdefs_ioctl.h>
+#include	<usysdefs_itimers.h>
 #include	<usysdefs_len.h>
 #include	<usysdefs_lim.h>
 #include	<usysdefs_lock.h>
