49615960af5ff59902b5ea778e403bec7cb330eb
David A.D. Morano <david.a.morano@gmail.com>
2026-09-24 00:11:14 -0400

    update

--- src/libu/xxtostr.h
+++ src/libu/xxtostr.h
@@ -55,34 +55,44 @@
 
 EXTERNC_begin
 
-extern char *itostr(int,		char *) noex ;
-extern char *ltostr(long,		char *) noex ;
-extern char *lltostr(longlong,		char *) noex ;
+extern char *sitostr(sint,		char *) noex ;
+extern char *sltostr(slong,		char *) noex ;
+extern char *slltostr(slonglong,	char *) noex ;
 
 extern char *uitostr(uint,		char *) noex ;
 extern char *ultostr(ulong,		char *) noex ;
 extern char *ulltostr(ulonglong,	char *) noex ;
 
+local inline char *itostr(int v,char *endp) noex {
+    	return sitostr(v,endp) ;
+} /* end subroutine */
+local inline char *ltostr(long v,char *endp) noex {
+    	return sltostr(v,endp) ;
+} /* end subroutine */
+local inline char *lltostr(longlong v,char *endp) noex {
+    	return slltostr(v,endp) ;
+} /* end subroutine */
+
 EXTERNC_end
 
 #ifdef	__cplusplus
 
-extern char *itostr(int,		char *,int) noex ;
-extern char *ltostr(long,		char *,int) noex ;
-extern char *lltostr(longlong,		char *,int) noex ;
+extern char *sitostr(sint,		char *,int) noex ;
+extern char *sltostr(slong,		char *,int) noex ;
+extern char *slltostr(slonglong,	char *,int) noex ;
 
 extern char *uitostr(uint,		char *,int) noex ;
 extern char *ultostr(ulong,		char *,int) noex ;
 extern char *ulltostr(ulonglong,	char *,int) noex ;
 
-inline char *xtostr(int sv,		char *endp,int b = 10) noex {
-    return itostr(sv,endp,b) ;
+inline char *xtostr(sint sv,		char *endp,int b = 10) noex {
+    return sitostr(sv,endp,b) ;
 } /* end */
-inline char *xtostr(long sv,		char *endp,int b = 10) noex {
-    return ltostr(sv,endp,b) ;
+inline char *xtostr(slong sv,		char *endp,int b = 10) noex {
+    return sltostr(sv,endp,b) ;
 } /* end */
-inline char *xtostr(longlong sv,	char *endp,int b = 10) noex {
-    return lltostr(sv,endp,b) ;
+inline char *xtostr(slonglong sv,	char *endp,int b = 10) noex {
+    return slltostr(sv,endp,b) ;
 } /* end */
 
 inline char *xtostr(uint uv,		char *endp,int b = 10) noex {
