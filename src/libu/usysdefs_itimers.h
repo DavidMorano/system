8fe54ac06ba71f93e17dfba51c67062c03a2785a
David A.D. Morano <david.a.morano@gmail.com>
2026-09-24 00:09:27 -0400

    restore

---
+++ src/libu/usysdefs_itimers.h
@@ -0,0 +1,50 @@
+/* usysdefs_itimers HEADER */
+/* charset=ISO8859-1 */
+/* lang=C20 */
+
+/* these are (a multitude) of various UNIX® system pre-processor defines */
+/* version %I% last-modified %G% */
+
+
+/* revision history:
+
+	= 1998-02-09, David A-D- Morano
+	This subroutine was written for Rightcore Network Services.
+
+*/
+
+/* Copyright © 1998 David A-D- Morano.  All rights reserved. */
+
+/*******************************************************************************
+
+	Description:
+	This file defines various preprocessor variables for use
+	in setting up the UNIX®-system default for various buffer
+	sizes.  The real run-time system buffer sizes are (indeed)
+	determined at runtime where they may exist.
+
+	Usage:
+	Among the most important uses for the data contained herein
+	is the BUFSIZE management facility.  It uses the data below
+	to set default buffer sizes for those systems that do not
+	have dynamicly managed buffer size support in the system
+	kernel.
+
+*******************************************************************************/
+
+#ifndef	USYSDEFSITIMERS_INCLUDE
+#define	USYSDEFSITIMERS_INCLUDE
+
+
+#include	<envstandards.h>	/* ordered first to configure */
+#include	<sys/time.h>		/* POSIX® */
+
+
+#ifndef	ITIMER_VIRT
+#define	ITIMER_VIRT	ITIMER_VIRTUAL
+#endif
+
+
+#endif /* USYSDEFSITIMERS_INCLUDE */
+
+
